38ba8012e7cc0536ccf68e0c4643cb89a65fd8ce
Sebastian Breß <sebastian.bress@dfki.de>
2018-12-25 23:22:48 +0100

    more work on the c compiler interface

--- iotdb/impl/CodeGen/CodeGenerator.cpp
+++ iotdb/impl/CodeGen/CodeGenerator.cpp
@@ -1,15 +1,28 @@
 
 #include <CodeGen/CodeGen.hpp>
+#include <CodeGen/C_CodeGen/CodeCompiler.hpp>
 
 namespace iotdb {
-
-
-
 
   class C_CodeGenerator : public CodeGenerator {
     bool addOperator(OperatorPtr, const CodeGenArgs &);
     PipelineStagePtr compile(const CompilerArgs &);
     ~C_CodeGenerator();
   };
+
+  bool C_CodeGenerator::addOperator(OperatorPtr, const CodeGenArgs &){
+
+    return false;
+  }
+  PipelineStagePtr C_CodeGenerator::compile(const CompilerArgs &){
+      CCodeCompiler compiler;
+      CompiledCCodePtr code = compiler.compile("");
+      //code->getFunctionPointer("");
+      return PipelineStagePtr();
+  }
+  C_CodeGenerator::~C_CodeGenerator(){
+
+  }
+
 
 }
