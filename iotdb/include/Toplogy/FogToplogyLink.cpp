5f6c9565ebcdcd34b2b2505c3496fa26848769e0
zeuchste <steffen.zeuch@dfki.de>
2019-01-17 13:35:57 +0100

    add topo header

---
+++ iotdb/include/Toplogy/FogToplogyLink.cpp
@@ -0,0 +1,15 @@
+/*
+ * FogToplogyLink.cpp
+ *
+ *  Created on: Jan 17, 2019
+ *      Author: zeuchste
+ */
+
+#ifndef INCLUDE_TOPLOGY_FOGTOPLOGYLINK_CPP_
+#define INCLUDE_TOPLOGY_FOGTOPLOGYLINK_CPP_
+
+
+
+
+
+#endif /* INCLUDE_TOPLOGY_FOGTOPLOGYLINK_CPP_ */
---
+++ iotdb/include/Toplogy/FogToplogyNode.hpp
@@ -0,0 +1,15 @@
+/*
+ * FogToplogyNode.hpp
+ *
+ *  Created on: Jan 17, 2019
+ *      Author: zeuchste
+ */
+
+#ifndef INCLUDE_TOPLOGY_FOGTOPLOGYNODE_HPP_
+#define INCLUDE_TOPLOGY_FOGTOPLOGYNODE_HPP_
+
+
+
+
+
+#endif /* INCLUDE_TOPLOGY_FOGTOPLOGYNODE_HPP_ */
