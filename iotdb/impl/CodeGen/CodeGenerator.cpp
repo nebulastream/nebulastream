b71c4b90dca7cf8c92f8e1db12381d8e19bff3c6
Sebastian Breß <sebastian.bress@dfki.de>
2019-01-23 01:31:01 +0100

    first draft of a C code based code generator and its respective builder interfaces

--- iotdb/impl/CodeGen/CodeGenerator.cpp
+++ iotdb/impl/CodeGen/CodeGenerator.cpp
@@ -39,9 +39,32 @@ namespace iotdb {
 
   bool addToCode(GeneratedCodePtr code, GeneratedCodePtr code_to_add);
 
+  bool addToCode(GeneratedCodePtr code, GeneratedCodePtr code_to_add) {
+    if (!code || !code_to_add) return false;
+    code->header_and_types_code_block_
+        << code_to_add->header_and_types_code_block_.str();
+    code->fetch_input_code_block_ << code_to_add->fetch_input_code_block_.str();
+    code->declare_variables_code_block_
+        << code_to_add->declare_variables_code_block_.str();
+    code->init_variables_code_block_
+        << code_to_add->init_variables_code_block_.str();
+    code->upper_code_block_.insert(code->upper_code_block_.end(),
+                                   code_to_add->upper_code_block_.begin(),
+                                   code_to_add->upper_code_block_.end());
+    code->lower_code_block_.insert(code->lower_code_block_.begin(),
+                                   code_to_add->lower_code_block_.begin(),
+                                   code_to_add->lower_code_block_.end());
+    code->after_for_loop_code_block_
+        << code_to_add->after_for_loop_code_block_.str();
+    code->create_result_table_code_block_
+        << code_to_add->create_result_table_code_block_.str();
+    code->clean_up_code_block_ << code_to_add->clean_up_code_block_.str();
+    code->kernel_header_and_types_code_block_
+        << code_to_add->kernel_header_and_types_code_block_.str();
+    return true;
+  }
+
   CodeGenerator::~CodeGenerator(){}
-
-
 
   class C_CodeGenerator : public CodeGenerator {
     bool addOperator(OperatorPtr, const CodeGenArgs &);
@@ -66,99 +89,10 @@ namespace iotdb {
 
   }
 
+
+
   CodeGeneratorPtr createCodeGenerator(){
     return CodeGeneratorPtr();
   }
-
-  void generate_LLVM_AST(){
-    // Path to the C file
-    std::string inputPath = "getinmemory.c";
-
-    // Path to clang (e.g. /usr/local/bin/clang)
-//    auto clangPath = llvm::sys::findProgramByName("clang++");
-//    if(clangPath.getError()){
-//        IOTDB_FATAL_ERROR("Could not find clang++ compiler: " + clangPath.getError().message());
-//    }
-
-    // Arguments to pass to the clang driver:
-    //	clang getinmemory.c -lcurl -v
-    std::vector<const char *> args;
-    args.push_back(inputPath.c_str());
-    args.push_back("-o a.exe");
-    args.push_back("-v");		// verbose
-
-                                                // The clang driver needs a DiagnosticsEngine so it can report problems
-    clang::DiagnosticOptions diagnosticOptions;
-    clang::TextDiagnosticPrinter *DiagClient = new clang::TextDiagnosticPrinter(llvm::errs(), &diagnosticOptions);
-    if(!DiagClient)
-      IOTDB_FATAL_ERROR("clang::TextDiagnosticPrinter is NULL!");
-    //clang::IntrusiveRefCntPtr<clang::DiagnosticIDs> DiagID(new clang::DiagnosticIDs());
-
-    llvm::IntrusiveRefCntPtr<clang::DiagnosticIDs> diagIds = new clang::DiagnosticIDs();
-    //auto Diags = std::make_unique<clang::DiagnosticsEngine>(diagIds, &diagnosticOptions, DiagClient);
-  //FIXME: The following line creates a memory leak.
-  // For some odd reason the program crashes when we call the destructor, this needs further investitation
-    clang::DiagnosticsEngine* Diags = new clang::DiagnosticsEngine(diagIds, &diagnosticOptions, DiagClient);
-
-
-
-    // Create the clang driver
-    //const char* const val = clangPath.get().c_str();
-    //auto val2 = Diags.get();
-    clang::driver::Driver TheDriver(CLANG_EXECUTABLE, llvm::sys::getDefaultTargetTriple(), *Diags);
-
-
-    // Create the set of actions to perform
-    auto C = std::unique_ptr<clang::driver::Compilation>(TheDriver.BuildCompilation(args));
-
-    // Print the set of actions
-    TheDriver.PrintActions(*C);
-
-    // This is referenced by `FileMgr` and will be released by `FileMgr` when it
-    // is deleted.
-    clang::IntrusiveRefCntPtr<clang::vfs::InMemoryFileSystem> InMemoryFileSystem(
-        new clang::vfs::InMemoryFileSystem);
-
-    std::unique_ptr<clang::FileManager> FileMgr(
-        new clang::FileManager(clang::FileSystemOptions(), InMemoryFileSystem));
-    // This is passed to `SM` as reference, so the pointer has to be referenced
-    // by `Environment` due to the same reason above.
-//    std::unique_ptr<clang::DiagnosticsEngine> Diagnostics(new clang::DiagnosticsEngine(
-//        clang::IntrusiveRefCntPtr<clang::DiagnosticIDs>(new clang::DiagnosticIDs),
-//        new clang::DiagnosticOptions));
-    // This will be stored as reference, so the pointer has to be stored in
-    // due to the same reason above.
-    std::unique_ptr<clang::SourceManager> VirtualSM(
-        new clang::SourceManager(*Diags, *FileMgr));
-
-
-    clang::VarDecl* vardecl;
-
-    clang::Decl::Kind kind = clang::Decl::Kind::Var;
-
-    (void) vardecl;
-    (void) kind;
-
-    clang::LangOptions lang_opts;
-
-    clang::LangOptions LangOpts;
-    clang::IdentifierTable idents(LangOpts);
-
-    clang::SelectorTable sels;
-    clang::Builtin::Context builtins;
-
-    clang::ASTContext context (LangOpts, *VirtualSM, idents,
-                   sels, builtins);
-
-
-
-#ifdef NON
-#endif
-
-    std::cout << "LLVM initialized!" << std::endl;
-
-
-  }
-
 
 }
