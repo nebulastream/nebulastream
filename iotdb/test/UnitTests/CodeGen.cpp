2b91ca73770b33580e248b79c98048288a4a1a6f
Sebastian Breß <sebastian.bress@dfki.de>
2019-01-23 15:44:35 +0100

    Compilation of C Source Code works

--- iotdb/test/UnitTests/CodeGen.cpp
+++ iotdb/test/UnitTests/CodeGen.cpp
@@ -1,16 +1,24 @@
 
 #include <iostream>
+//#include <CodeGen/C_CodeGen/FunctionBuilder.hpp>
+
+
 
 namespace iotdb {
 
-
+int CodeGenTest();
 
 }
 
 int main(){
 
-  std::cout << "Test Passed!" << std::endl;
-
+  if(!iotdb::CodeGenTest()){
+    std::cout << "Test Passed!" << std::endl;
+    return 1;
+    }else{
+    std::cout << "Test Failed!" << std::endl;
+    return -1;
+    }
   return 0;
 }
 
