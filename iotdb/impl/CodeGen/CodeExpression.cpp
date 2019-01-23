a2fd8b7c1c4b9eb8442be07fbf86a7f6279f8a8d
Sebastian Breß <sebastian.bress@dfki.de>
2019-01-23 16:24:33 +0100

    more work on code generation API

---
+++ iotdb/impl/CodeGen/CodeExpression.cpp
@@ -0,0 +1,11 @@
+
+#include <CodeGen/CodeExpression.hpp>
+
+namespace iotdb{
+
+  CodeExpression::CodeExpression(const std::string& code)
+   : code_(code){}
+
+}
+
+
--- iotdb/include/Core/DataTypes.hpp
+++ iotdb/include/Core/DataTypes.hpp
@@ -16,11 +16,15 @@ typedef std::shared_ptr<DataType> DataTypePtr;
 class ValueType;
 typedef std::shared_ptr<ValueType> ValueTypePtr;
 
+class CodeExpression;
+typedef std::shared_ptr<CodeExpression> CodeExpressionPtr;
+
 class DataType {
 public:
   virtual ValueTypePtr getNullValue() const = 0;
   virtual uint32_t getSizeBytes() const = 0;
   virtual const std::string toString() const = 0;
+  virtual const CodeExpressionPtr getCode() const = 0;
   virtual ~DataType();
 };
 
