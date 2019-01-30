ddf70cf75f469faca9fb6176e6f35afb31530de6
Tobias Behrens <github@tobeas.de>
2019-01-30 14:31:00 +0100

    apply clang-format to Topology

--- iotdb/include/Topology/FogTopologyNode.hpp
+++ iotdb/include/Topology/FogTopologyNode.hpp
@@ -1,36 +1,24 @@
 #ifndef INCLUDE_TOPOLOGY_FOGTOPOLOGYNODE_HPP_
 #define INCLUDE_TOPOLOGY_FOGTOPOLOGYNODE_HPP_
 
-#include <vector>
 #include <memory>
+#include <vector>
 #define INVALID_NODE_ID 101
 
 class FogTopologyNode;
 typedef std::shared_ptr<FogTopologyNode> FogTopologyNodePtr;
 
+class FogTopologyNode {
 
-class FogTopologyNode{
+public:
+  FogTopologyNode() { node_id = INVALID_NODE_ID; }
 
-    public:
-		FogTopologyNode()
-		{
-			node_id = INVALID_NODE_ID;
-		}
+  void setNodeId(size_t id) { node_id = id; }
 
-		void setNodeId(size_t id)
-		{
-			node_id = id;
-		}
+  size_t getNodeId() { return node_id; }
 
-		size_t getNodeId()
-		{
-			return node_id;
-		}
-
-    private:
-        size_t node_id;
-    };
-
-
+private:
+  size_t node_id;
+};
 
 #endif /* INCLUDE_TOPOLOGY_FOGTOPOLOGYNODE_HPP_ */
--- iotdb/include/Topology/FogTopologySensor.hpp
+++ iotdb/include/Topology/FogTopologySensor.hpp
@@ -5,29 +5,17 @@
 
 #define INVALID_NODE_ID 101
 
-class FogTopologySensor{
+class FogTopologySensor {
 
-    public:
-		FogTopologySensor()
-		{
-			sensorID = INVALID_NODE_ID;
-		}
+public:
+  FogTopologySensor() { sensorID = INVALID_NODE_ID; }
 
-		void setSensorId(size_t id)
-		{
-			sensorID = id;
-		}
-		size_t getSensorId()
-		{
-			return sensorID;
-		}
+  void setSensorId(size_t id) { sensorID = id; }
+  size_t getSensorId() { return sensorID; }
 
-
-    private:
-        size_t sensorID;
-    };
-
-
+private:
+  size_t sensorID;
+};
 
 typedef std::shared_ptr<FogTopologySensor> FogTopologySensorPtr;
 
