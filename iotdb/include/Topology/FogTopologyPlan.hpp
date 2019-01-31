4df6adb8db09f8c55fd4de12c32bba18bc2c5729
zeuchste <zeuchste@informatik.hu-berlin.de>
2019-01-31 10:12:20 +0100

    first step in replacin boost matrix with boost graph

--- iotdb/include/Topology/FogTopologyPlan.hpp
+++ iotdb/include/Topology/FogTopologyPlan.hpp
@@ -18,8 +18,23 @@
 #include "Topology/FogTopologyNode.hpp"
 #include "Topology/FogTopologySensor.hpp"
 
+#include <iostream>                  // for std::cout
+#include <utility>                   // for std::pair
+#include <algorithm>                 // for std::for_each
+#include <boost/graph/graph_traits.hpp>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/dijkstra_shortest_paths.hpp>
+#include <boost/graph/graphviz.hpp>
+
+
 #define MAX_NUMBER_OF_NODES 10 // TODO: make this dynamic
 /**
+ * Links:
+ * 		- https://www.boost.org/doc/libs/1_61_0/libs/graph/doc/file_dependency_example.html
+ * 		- https://www.boost.org/doc/libs/1_69_0/libs/graph/doc/index.html
+ * 		- https://www.boost.org/doc/libs/1_49_0/libs/graph/doc/quick_tour.html
+ * 		- https://theboostcpplibraries.com/boost.graph-vertices-and-edges
+ * 		- https://stackoverflow.com/questions/3100146/adding-custom-vertices-to-a-boost-graph
  * TODOs:
  * 		- move functions to impl
  * 	Assumptions
@@ -28,87 +43,125 @@
 
 using namespace std;
 using namespace boost::numeric::ublas;
+using namespace boost;
 
-class Graph {
+struct Vertex {size_t id;};
+struct Edge { size_t id; };
+
+using graph_t  = adjacency_list<listS, vecS, undirectedS, Vertex, Edge >;
+using vertex_t = graph_traits<graph_t>::vertex_descriptor;
+using edge_t   = graph_traits<graph_t>::edge_descriptor;
+
+
+
+class FogGraph {
 public:
-  Graph(int pNumberOfElements) {
-    numberOfRows = numberOfColums = 1;
-    mtx.resize(numberOfRows, numberOfColums);
-    mtx(0, 0) = 0;
-  }
+	void addVertex(size_t id)
+	{
+	    vertex_t u = boost::add_vertex(Vertex{id}, g);
+	}
+	void addEdge(size_t id, vertex_t u, vertex_t v)
+	{
+	    boost::add_edge(u, v, Edge{id}, g);
+	}
 
-  void addNode() {
-    numberOfRows++;
-    numberOfColums++;
-    mtx.resize(numberOfRows, numberOfColums);
-  }
-  // function to add an edge to graph
-  void addLink(size_t nodeID1, size_t nodeID2, size_t linkID) {
-    // TODO: what to do if already exist?
-    mtx(nodeID1, nodeID2) = linkID;
-  }
-
-  size_t getLinkID(size_t nodeID1, size_t nodeID2) { return mtx(nodeID1, nodeID2); }
-
-  void removeLinkID(size_t nodeID1, size_t nodeID2) { mtx(nodeID1, nodeID2) = 0; }
-
-  std::string to_string(matrix<size_t> pMtx) {
-
-    std::stringstream ss;
-
-    // first line
-    ss << " | ";
-    for (size_t v = 0; v < pMtx.size1(); v++) {
-      if (v == 0)
-        ss << "-"
-           << " ";
-      else
-        ss << v << " ";
-    }
-
-    ss << endl;
-    for (size_t v = 0; v < pMtx.size2() * 2 + 3; v++)
-      ss << "-";
-    ss << endl;
-
-    for (size_t u = 0; u < pMtx.size1(); u++) {
-      if (u == 0)
-        ss << "-| ";
-      else
-        ss << u << "| ";
-      for (size_t v = 0; v < pMtx.size2(); v++) {
-        if (pMtx(u, v) == 0)
-          ss << "- ";
-        else
-          ss << pMtx(u, v) << " ";
-      }
-      ss << std::endl;
-    }
-    return ss.str();
-  }
-
-  std::string to_string() { return to_string(mtx); }
-
-  void print(matrix<size_t> pMtx) { std::cout << to_string(pMtx); }
-
-  void print() { print(mtx); }
-
-  void removeRowAndCol() {
-    matrix_slice<matrix<size_t>> ms(mtx, boost::numeric::ublas::slice(0, 1, 3), boost::numeric::ublas::slice(0, 1, 3));
-    print(ms);
-  }
+	void print()
+	{
+		boost::write_graphviz(std::cout, g, [&] (auto& out, auto v) {
+		       out << "[label=\"" << g[v].id << "\"]";
+		      },
+		      [&] (auto& out, auto e) {
+		       out << "[label=\"" << g[e].id << "\"]";
+		    });
+		    std::cout << std::flush;
+	}
 
 private:
-  size_t numberOfRows;
-  size_t numberOfColums;
-  matrix<size_t> mtx;
+    graph_t g;
+
 };
+
+
+
+//class Graph {
+//public:
+//  Graph(int pNumberOfElements) {
+//    numberOfRows = numberOfColums = 1;
+//    mtx.resize(numberOfRows, numberOfColums);
+//    mtx(0, 0) = 0;
+//  }
+//
+//  void addNode() {
+//    numberOfRows++;
+//    numberOfColums++;
+//    mtx.resize(numberOfRows, numberOfColums);
+//  }
+//  // function to add an edge to graph
+//  void addLink(size_t nodeID1, size_t nodeID2, size_t linkID) {
+//    // TODO: what to do if already exist?
+//    mtx(nodeID1, nodeID2) = linkID;
+//  }
+//
+//  size_t getLinkID(size_t nodeID1, size_t nodeID2) { return mtx(nodeID1, nodeID2); }
+//
+//  void removeLinkID(size_t nodeID1, size_t nodeID2) { mtx(nodeID1, nodeID2) = 0; }
+//
+//  std::string to_string(matrix<size_t> pMtx) {
+//
+//    std::stringstream ss;
+//
+//    // first line
+//    ss << " | ";
+//    for (size_t v = 0; v < pMtx.size1(); v++) {
+//      if (v == 0)
+//        ss << "-"
+//           << " ";
+//      else
+//        ss << v << " ";
+//    }
+//
+//    ss << endl;
+//    for (size_t v = 0; v < pMtx.size2() * 2 + 3; v++)
+//      ss << "-";
+//    ss << endl;
+//
+//    for (size_t u = 0; u < pMtx.size1(); u++) {
+//      if (u == 0)
+//        ss << "-| ";
+//      else
+//        ss << u << "| ";
+//      for (size_t v = 0; v < pMtx.size2(); v++) {
+//        if (pMtx(u, v) == 0)
+//          ss << "- ";
+//        else
+//          ss << pMtx(u, v) << " ";
+//      }
+//      ss << std::endl;
+//    }
+//    return ss.str();
+//  }
+//
+//  std::string to_string() { return to_string(mtx); }
+//
+//  void print(matrix<size_t> pMtx) { std::cout << to_string(pMtx); }
+//
+//  void print() { print(mtx); }
+//
+//
+//
+//private:
+//  size_t numberOfRows;
+//  size_t numberOfColums;
+//  matrix<size_t> mtx;
+//  // create a typedef for the Graph type
+//
+//};
 
 class FogTopologyPlan {
 
 public:
   FogTopologyPlan() {
-    linkGraph = new Graph(MAX_NUMBER_OF_NODES);
+//    linkGraph = new Graph(MAX_NUMBER_OF_NODES);
     currentId = 1;
   }
 
@@ -122,7 +175,7 @@ public:
     fogNodes[currentId] = ptr;
     ptr->setNodeId(currentId);
     currentId++;
-    linkGraph->addNode();
+//    linkGraph->addNode();
   }
   void removeFogNode(FogTopologyNodePtr ptr) {
     size_t search_id = ptr->getNodeId();
@@ -140,7 +193,7 @@ public:
     fogSensors[currentId] = ptr;
     ptr->setSensorId(currentId);
     currentId++;
-    linkGraph->addNode();
+//    linkGraph->addNode();
   }
   void removeFogSensor(FogTopologyNodePtr ptr) {
     size_t search_id = ptr->getNodeId();
@@ -161,19 +214,19 @@ public:
   void addFogTopologyLink(size_t pSourceNodeId, size_t pDestNodeId, LinkType type) {
     FogTopologyLinkPtr linkPtr = std::make_shared<FogTopologyLink>(pSourceNodeId, pDestNodeId, type);
     fogLinks[linkPtr->getLinkID()] = linkPtr;
-    linkGraph->addLink(linkPtr->getSourceNodeId(), linkPtr->getDestNodeId(), linkPtr->getLinkID());
-    linkGraph->addLink(linkPtr->getDestNodeId(), linkPtr->getSourceNodeId(), linkPtr->getLinkID());
+//    linkGraph->addLink(linkPtr->getSourceNodeId(), linkPtr->getDestNodeId(), linkPtr->getLinkID());
+//    linkGraph->addLink(linkPtr->getDestNodeId(), linkPtr->getSourceNodeId(), linkPtr->getLinkID());
   }
   void removeFogTopologyLink(FogTopologyLinkPtr linkPtr) {
     size_t search_id = linkPtr->getLinkID();
-    linkGraph->removeLinkID(linkPtr->getSourceNodeId(), linkPtr->getDestNodeId());
+//    linkGraph->removeLinkID(linkPtr->getSourceNodeId(), linkPtr->getDestNodeId());
     fogLinks.erase(linkPtr->getLinkID());
   }
 
   std::string to_string() {
     std::stringstream ss;
 
-    ss << linkGraph->to_string();
+//    ss << linkGraph->to_string();
     ss << "Nodes IDs=";
     for (auto const &it : fogNodes) {
       ss << it.second->getNodeId() << ",";
@@ -196,7 +249,7 @@ private:
   std::map<size_t, FogTopologySensorPtr> fogSensors;
   std::map<size_t, FogTopologyLinkPtr> fogLinks;
   size_t currentId;
-  Graph *linkGraph;
+//  Graph *linkGraph;
 };
 
 #endif /* INCLUDE_TOPOLOGY_FOGTOPOLOGYPLAN_HPP_ */
