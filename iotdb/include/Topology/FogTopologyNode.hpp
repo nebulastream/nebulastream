97c3dac950dae12467e963d27c3d250094af7fb3
zeuchste <steffen.zeuch@dfki.de>
2019-01-31 20:01:51 +0100

    new version of graph

--- iotdb/include/Topology/FogTopologyNode.hpp
+++ iotdb/include/Topology/FogTopologyNode.hpp
@@ -3,22 +3,25 @@
 
 #include <memory>
 #include <vector>
+#include "FogTopologyEntry.hpp"
 #define INVALID_NODE_ID 101
 
-class FogTopologyNode;
-typedef std::shared_ptr<FogTopologyNode> FogTopologyNodePtr;
-
-class FogTopologyNode {
+class FogTopologyNode : public FogTopologyEntry {
 
 public:
   FogTopologyNode() { node_id = INVALID_NODE_ID; }
 
   void setNodeId(size_t id) { node_id = id; }
 
-  size_t getNodeId() { return node_id; }
+  size_t getID() { return node_id; }
+
+  FogNodeType getEntryType(){return Worker;}
+
 
 private:
   size_t node_id;
 };
+
+typedef std::shared_ptr<FogTopologyNode> FogTopologyNodePtr;
 
 #endif /* INCLUDE_TOPOLOGY_FOGTOPOLOGYNODE_HPP_ */
