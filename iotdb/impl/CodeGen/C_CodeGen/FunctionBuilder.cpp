2166a23990b7409212f4e3bdc9b941953c97e698
Sebastian Breß <sebastian.bress@dfki.de>
2019-02-02 12:30:54 +0100

    accessing array elements works now

--- iotdb/impl/CodeGen/C_CodeGen/FunctionBuilder.cpp
+++ iotdb/impl/CodeGen/C_CodeGen/FunctionBuilder.cpp
@@ -368,7 +368,6 @@ namespace iotdb {
 
     enum UnaryOperatorType{ADDRESS_OF_OP,
                            DEREFERENCE_POINTER_OP,
-                           ARRAY_REFERENCE_OP,
                            MEMBER_SELECT_POINTER_OP,
                            MEMBER_SELECT_REFERENCE_OP,
                            PREFIX_INCREMENT_OP,
@@ -385,7 +384,6 @@ namespace iotdb {
       const char* const names[] = {
     "ADDRESS_OF_OP",
     "DEREFERENCE_POINTER_OP",
-    "ARRAY_REFERENCE_OP",
     "MEMBER_SELECT_POINTER_OP",
     "MEMBER_SELECT_REFERENCE_OP",
     "PREFIX_INCREMENT_OP",
@@ -402,7 +400,6 @@ namespace iotdb {
       const char* const names[] = {
         "&",
         "*",
-        "[]",
         "->",
         ".",
         "++",
@@ -416,7 +413,7 @@ namespace iotdb {
       return std::make_shared<CodeExpression>(names[type]);
     }
 
-    enum BinaryOperatorType{EQUAL_OP, UNEQUAL_OP, LESS_THEN_OP,LESS_THEN_EQUAL_OP,GREATER_THEN_OP,GREATER_THEN_EQUAL_OP, PLUS_OP, MINUS_OP, MULTIPLY_OP, DIVISION_OP, MODULO_OP, LOGICAL_AND_OP, LOGICAL_OR_OP, BITWISE_AND_OP, BITWISE_OR_OP, BITWISE_XOR_OP, BITWISE_LEFT_SHIFT_OP, BITWISE_RIGHT_SHIFT_OP, ASSIGNMENT_OP};
+    enum BinaryOperatorType{EQUAL_OP, UNEQUAL_OP, LESS_THEN_OP,LESS_THEN_EQUAL_OP,GREATER_THEN_OP,GREATER_THEN_EQUAL_OP, PLUS_OP, MINUS_OP, MULTIPLY_OP, DIVISION_OP, MODULO_OP, LOGICAL_AND_OP, LOGICAL_OR_OP, BITWISE_AND_OP, BITWISE_OR_OP, BITWISE_XOR_OP, BITWISE_LEFT_SHIFT_OP, BITWISE_RIGHT_SHIFT_OP, ASSIGNMENT_OP, ARRAY_REFERENCE_OP};
 
     const std::string toString(const BinaryOperatorType& type){
       const char* const names[] = {"EQUAL_OP", "UNEQUAL_OP", "LESS_THEN_OP", "LESS_THEN_EQUAL_OP","GREATER_THEN_OP","GREATER_THEN_EQUAL_OP","PLUS_OP",
@@ -431,7 +428,8 @@ namespace iotdb {
                                    "BITWISE_XOR_OP",
                                    "BITWISE_LEFT_SHIFT_OP",
                                    "BITWISE_RIGHT_SHIFT_OP",
-                                   "ASSIGNMENT_OP"};
+                                   "ASSIGNMENT_OP",
+                                   "ARRAY_REFERENCE_OP"};
       return std::string(names[type]);
     }
 
@@ -449,7 +447,7 @@ namespace iotdb {
                                    "|",
                                    "^",
                                    "<<",
-                                   ">>", "="};
+                                   ">>", "=", "[]"};
       return std::make_shared<CodeExpression>(names[type]);
     }
 
@@ -547,8 +545,15 @@ namespace iotdb {
        }
        virtual const CodeExpressionPtr getCode() const{
           CodeExpressionPtr code;
-          code = combine(lhs_->getCode(),toCodeExpression(op_));
-          code = combine(code,rhs_->getCode());
+          if(ARRAY_REFERENCE_OP==op_){
+              code = combine(lhs_->getCode(),std::make_shared<CodeExpression>("["));
+              code = combine(code,rhs_->getCode());
+              code = combine(code,std::make_shared<CodeExpression>("]"));
+            }else{
+              code = combine(lhs_->getCode(),toCodeExpression(op_));
+              code = combine(code,rhs_->getCode());
+            }
+
           std::string ret;
           if(bracket_mode_==BRACKETS){
            ret=std::string("(")+code->code_+std::string(")");
@@ -890,6 +895,9 @@ private:
                 createUserDefinedType(struct_decl_tuple)),
               "tuples");
 
+
+        std::cout << BinaryOperatorStatement(VarRefStatement(var_decl_tuple),ARRAY_REFERENCE_OP,ConstantExprStatement(INT32,"0")).getCode()->code_ << std::endl;
+        std::cout << BinaryOperatorStatement(VarRefStatement(var_decl_tuple),ARRAY_REFERENCE_OP,VarRefStatement(var_decl_i)).getCode()->code_ << std::endl;
 
         FunctionDeclaration main_function = FunctionBuilder::create("compiled_query")
             .returns(createDataType(BasicType(INT32)))
