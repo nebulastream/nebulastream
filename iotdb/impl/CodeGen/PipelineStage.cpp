1117615be69ff79e300e727db85971b684df5e4e
Sebastian Breß <sebastian.bress@dfki.de>
2019-02-03 15:51:37 +0100

    revised code and added very simple test code

--- iotdb/impl/CodeGen/PipelineStage.cpp
+++ iotdb/impl/CodeGen/PipelineStage.cpp
@@ -12,8 +12,12 @@
 
 namespace iotdb {
 
-bool PipelineStage::execute(TupleBuffer buf, WindowState* state) {
+bool PipelineStage::execute(const std::vector<TupleBuffer*>& input_buffers, WindowState* state, TupleBuffer* result_buf) {
   std::cout << "Execute a Pipeline Stage!" << std::endl;
+  uint32_t ret = execute_impl(input_buffers, state, result_buf);
+  if(ret)
+    IOTDB_FATAL_ERROR("Execution of Compled PipelineStage Failed!");
+
   return true;
 }
 
@@ -32,7 +36,7 @@ public:
 
 protected:
 
- TupleBuffer execute_impl(std::vector<TupleBuffer*>& input_buffers) override final;
+ uint32_t execute_impl(const std::vector<TupleBuffer*>& input_buffers, WindowState* state, TupleBuffer* result_buf) override final;
  virtual uint32_t callCFunction(TupleBuffer** tuple_buffers, WindowState* state, TupleBuffer* result_buffer);
 
  CompiledCCodePtr compiled_code_;
@@ -52,22 +56,15 @@ const PipelineStagePtr CPipelineStage::copy() const{
     return PipelineStagePtr(new CPipelineStage(*this));
 }
 
-TupleBuffer CPipelineStage::execute_impl(std::vector<TupleBuffer*>& input_buffers){
-
-  WindowState* state;
-  TupleBuffer result={nullptr,0,0,0};
-
-  uint32_t ret = callCFunction(input_buffers.data(), state, &result);
-  if(!ret)
-    IOTDB_FATAL_ERROR("Execution of Compled PipelineStage Failed!");
-  return result;
+uint32_t CPipelineStage::execute_impl(const std::vector<TupleBuffer*>& input_buffers, WindowState* state, TupleBuffer* result_buf){
+  return callCFunction((TupleBuffer**)input_buffers.data(), state, result_buf);
 }
 
 typedef uint32_t (*SharedCLibPipelineQueryPtr)(TupleBuffer**, WindowState*, TupleBuffer*);
 
 uint32_t CPipelineStage::callCFunction(TupleBuffer** tuple_buffers, WindowState* state, TupleBuffer* result_buffer){
   return (*compiled_code_->getFunctionPointer<SharedCLibPipelineQueryPtr>(
-      "compiled_query"))(tuple_buffers,state, result_buffer);
+      "_Z14compiled_queryPP11TupleBufferP11WindowStateS0_"))(tuple_buffers,state, result_buffer);
 
 }
 
