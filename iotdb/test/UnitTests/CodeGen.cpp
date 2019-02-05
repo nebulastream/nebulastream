1f5de1909bc766f28a72684d2b7685519c835b58
Sebastian Breß <sebastian.bress@dfki.de>
2019-02-05 10:28:10 +0100

    some polishing

--- iotdb/test/UnitTests/CodeGen.cpp
+++ iotdb/test/UnitTests/CodeGen.cpp
@@ -6,17 +6,20 @@
 
 namespace iotdb {
 
+int CodeGenTestCases();
 int CodeGenTest();
 
 }
 
 int main(){
 
+  iotdb::CodeGenTestCases();
+
   if(!iotdb::CodeGenTest()){
     std::cout << "Test Passed!" << std::endl;
     return 0;
     }else{
-    std::cout << "Test Failed!" << std::endl;
+    std::cerr << "Test Failed!" << std::endl;
     return -1;
     }
   return 0;
