ef10536e4a0ba19628d4da71212b1ce9a3d73e6f
Sebastian Breß <sebastian.bress@dfki.de>
2019-02-16 01:27:36 +0100

    moved legacy code from Janis prototype to seperate folder where it is not to be used, it may serve as a source of inspiration though to create a new cool API

--- iotdb/include/API/InputQuery.hpp
+++ iotdb/include/API/InputQuery.hpp
@@ -9,54 +9,10 @@
 #include <API/ParameterTypes.hpp>
 #include <Runtime/DataSource.hpp>
 
-
-//#include "Window.hpp"
-//#include "Aggregation.hpp"
-//#include "JoinPredicate.hpp"
-//#include "Mapper.hpp"
-
 namespace iotdb {
 
 class Operator;
 typedef std::shared_ptr<Operator> OperatorPtr;
-
-//class Predicate;
-//typedef std::shared_ptr<Predicate> PredicatePtr;
-
-//class JoinPredicate;
-//typedef std::shared_ptr<JoinPredicate> JoinPredicatePtr;
-
-//class Aggregation;
-//typedef std::shared_ptr<Aggregation> AggregationPtr;
-
-//class Window;
-//typedef std::shared_ptr<Window> WindowPtr;
-
-//class Mapper;
-//typedef std::shared_ptr<Mapper> MapperPtr;
-
-//struct Attributes{
-//  Attributes(AttributeFieldPtr field1);
-//  Attributes(AttributeFieldPtr field1,AttributeFieldPtr field2);
-//  Attributes(AttributeFieldPtr field1,AttributeFieldPtr field2, AttributeFieldPtr field3);
-//  /** \todo add more constructor cases for more fields */
-//  std::vector<AttributeFieldPtr> attrs;
-//};
-
-//enum SortOrder{ASCENDING,DESCENDING};
-
-//struct SortAttr{
-//    AttributeFieldPtr field;
-//    SortOrder order;
-//};
-
-//struct Sort{
-//  Sort(SortAttr field1);
-//  Sort(SortAttr field1,SortAttr field2);
-//  Sort(SortAttr field1,SortAttr field2, SortAttr field3);
-//  /** \todo add more constructor cases for more fields */
-//  std::vector<SortAttr> param;
-//};
 
 /** \brief the central abstraction for the user to define queries */
 class InputQuery {
