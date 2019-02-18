c22d81bec246f23bddb39a3ad41579733e1870d6
zongxiong.chen <czxczf@gmail.com>
2019-02-18 17:18:22 +0100

    simplify code

--- iotdb/impl/Runtime/BufferManager.cpp
+++ iotdb/impl/Runtime/BufferManager.cpp
@@ -9,17 +9,16 @@ namespace iotdb {
   BufferManager::BufferManager() : capacity(1000) {
 
     std::cout << "Init BufferManager" << std::endl;
-#if USE_LOCK_IMPL
-    std::cout << "USE LOCK IMPL" << std::endl;
-#elif USE_CAS_IMPL
-    std::cout << "USE CAS IMPL" << std::endl;
-#endif
-
     std::cout << "capacity: " << capacity << std::endl;
+#if USE_LOCK_IMPL
     // 1. Lock Version
-    // std::vector<TupleBufferPtr> buffers(capacity);
+    std::cout << "USE LOCK IMPL" << std::endl;
+    std::vector<TupleBufferPtr> buffers(capacity);
+#elif USE_CAS_IMPL
     // 2. CAS Version
+    std::cout << "USE CAS IMPL" << std::endl;
     std::vector<BufferEntry> buffers(capacity);
+#endif
 
     // uint64_t buffer_size = 4096;
     // uint32_t tuple_size_bytes = sizeof(uint64_t);
@@ -35,15 +34,23 @@ namespace iotdb {
       // https://stackoverflow.com/questions/43631415/using-shared-ptr-with-char
       std::shared_ptr<char> buffer(new char[buffer_size], std::default_delete<char []>());
       TupleBufferPtr buf = std::make_shared<TupleBuffer>((void *)(buffer.get()), buffer_size, tuple_size_bytes, num_tuples);
+
+#if USE_LOCK_IMPL
       // 1. Lock Version
-      // buffers[i] = buf;
+      buffers[i] = buf;
+#elif USE_CAS_IMPL
       // 2. CAS Version
       buffers[i].buffer = buf;
+#endif
     }
-    // 1. Lock Versino
-    // buffer_pool.insert(std::make_pair(buffer_size, buffers));
+
+#if USE_LOCK_IMPL
+    // 1. Lock Version
+    buffer_pool.insert(std::make_pair(buffer_size, buffers));
+#elif USE_CAS_IMPL
     // 2. CAS Version
     buffer_pool.insert(std::make_pair(buffer_size, std::move(buffers)));
+#endif
   }
 
   BufferManager::~BufferManager() {
@@ -59,10 +66,10 @@ namespace iotdb {
 
   TupleBufferPtr BufferManager::getBuffer(const uint64_t size) {
 
-    auto found = buffer_pool.find(size);
-    assert(found != buffer_pool.cend() && (std::string("No buffers for size ") + std::to_string(size) + std::string(" in pool. bug?")).c_str());
-    TupleBufferPtr buf = nullptr;
-    auto &buffers = found->second;
+   //  auto found = buffer_pool.find(size);
+   // assert(found != buffer_pool.cend() && (std::string("No buffers for size ") + std::to_string(size) + std::string(" in pool. bug?")).c_str());
+   //  TupleBufferPtr buf = nullptr;
+   //  auto &buffers = found->second;
     // 1. Lock Version
     // std::unique_lock<std::mutex> lock(mutex);
     // while (buffers.empty()) {
@@ -78,28 +85,33 @@ namespace iotdb {
     // cv.notify_all();
 
     // 2. CAS Version
-    do {
-      for (auto &buffer : buffers) {
-        int expected = 0;
-        int desired = 1;
-        if (buffer.used.compare_exchange_weak(expected, desired, std::memory_order_relaxed)) {
-          buf = buffer.buffer;
-          // std::cout << "Thread: " << std::this_thread::get_id() << " GOT Buffer: " << buf.get() << std::endl;
-          break;
-        }
-      }
-    } while (buf == nullptr);
+    // do {
+    //   for (auto &buffer : buffers) {
+    //     int expected = 0;
+    //     int desired = 1;
+    //     if (buffer.used.compare_exchange_weak(expected, desired, std::memory_order_relaxed)) {
+    //       buf = buffer.buffer;
+    //       // std::cout << "Thread: " << std::this_thread::get_id() << " GOT Buffer: " << buf.get() << std::endl;
+    //       break;
+    //     }
+    //   }
+    // } while (buf == nullptr);
 
-    return buf;
+    // return buf;
+    #if USE_LOCK_IMPL
+      return getBufferByLock(size);
+    #elif USE_CAS_IMPL
+      return getBufferByCAS(size);
+    #endif
   }
 
   void BufferManager::releaseBuffer(TupleBufferPtr tupleBuffer) {
 
-    assert(tupleBuffer != nullptr && "tupleBuffer is nullptr");
-    auto found = buffer_pool.find(tupleBuffer->buffer_size);
-    assert(found != buffer_pool.cend() && (std::string("No buffers for size ") + std::to_string(tupleBuffer->buffer_size) + std::string(" in pool. bug?")).c_str());
+    // assert(tupleBuffer != nullptr && "tupleBuffer is nullptr");
+    // auto found = buffer_pool.find(tupleBuffer->buffer_size);
+    // assert(found != buffer_pool.cend() && (std::string("No buffers for size ") + std::to_string(tupleBuffer->buffer_size) + std::string(" in pool. bug?")).c_str());
 
-    auto &buffers = found->second;
+    // auto &buffers = found->second;
     // 1. Lock Version
     // xxx: no need to add lock here
     // buffers.push_back(tupleBuffer);
@@ -110,16 +122,19 @@ namespace iotdb {
     // cv.notify_all();
 
     // 2. CAS Version
-    int expected = 0;
-    for (auto & buffer : buffers) {
-      if (buffer.buffer.get() == tupleBuffer.get()) {
-        buffer.used.exchange(expected);
-        // std::cout << "Thread: " << std::this_thread::get_id() << "Release Buffer: " << tupleBuffer.get() << std::endl;
-        break;
-      }
+    // int expected = 0;
+    // for (auto & buffer : buffers) {
+    //   if (buffer.buffer.get() == tupleBuffer.get()) {
+    //     buffer.used.exchange(expected);
+    //     // std::cout << "Thread: " << std::this_thread::get_id() << "Release Buffer: " << tupleBuffer.get() << std::endl;
+    //     break;
+    //   }
+    #if USE_LOCK_IMPL
+      releaseBufferByLock(tupleBuffer);
+    #elif USE_CAS_IMPL
+      releaseBufferByCAS(tupleBuffer);
+    #endif
     }
-
-  }
 
 #if USE_LOCK_IMPL
   TupleBufferPtr BufferManager::getBufferByLock(const uint64_t size) {
@@ -150,6 +165,7 @@ namespace iotdb {
     auto &buffers = found->second;
     // 1. Lock Version
     // xxx: no need to add lock here
+    std::unique_lock<std::mutex> lock(mutex);
     buffers.push_back(tupleBuffer);
     // sanity checking
     assert(buffers.size() <= capacity && "Buffer size is fixed, bug?");
@@ -157,6 +173,7 @@ namespace iotdb {
     // std::cerr << "Thread: " << std::this_thread::get_id() << " release buffer" << std::endl;
     cv.notify_all();
   }
+
 #elif USE_CAS_IMPL
   TupleBufferPtr BufferManager::getBufferByCAS(const uint64_t size) {
     auto found = buffer_pool.find(size);
