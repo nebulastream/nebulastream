6c63c6dd4ad710b55b9400a404edeb5982679200
zeuchste <steffen.zeuch@dfki.de>
2019-04-03 08:57:24 +0200

    fix interface test

--- iotdb/tests/UnitTests/QueryInterfaceTest.cpp
+++ iotdb/tests/UnitTests/QueryInterfaceTest.cpp
@@ -48,11 +48,10 @@ void createQueryString()
 {
 
     std::stringstream code;
-    code << "Config config = Config::create()."
-            "        withParallelism(1)."
-            "        withPreloading()."
-            "        withBufferSize(1000)."
-            "        withNumberOfPassesOverInput(1);"
+    code << "Config config = Config::create()"
+            ".setBufferCount(2000)"
+            ".setBufferSizeInByte(8*1024)"
+            ".setNumberOfWorker(2);"
          << std::endl;
 
     code << "Schema schema = Schema::create().addField(\"\",INT32);" << std::endl;
