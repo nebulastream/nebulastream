658f7d9c1db438de42c8b181d1b75c20e805b04d
zeuchste <steffen.zeuch@dfki.de>
2019-04-04 13:14:51 +0200

    some changes

--- microexperiment/rdma_lib/Utilities/ConnectionCollection.cpp
+++ microexperiment/rdma_lib/Utilities/ConnectionCollection.cpp
@@ -8,7 +8,6 @@
 #include "Debug.h"
 #include <infinity/infinity.h>
 #include <zconf.h>
-#include "string.h"
 
 VerbsConnection &ConnectionCollection::operator[](int idx) {
     return *connections[idx];
