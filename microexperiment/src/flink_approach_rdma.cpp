8a8511e2788be9da3fea602d2283b823e1c4d67d
zeuchste <steffen.zeuch@dfki.de>
2019-04-10 09:19:50 +0200

    test

--- microexperiment/src/flink_approach_rdma.cpp
+++ microexperiment/src/flink_approach_rdma.cpp
@@ -359,7 +359,7 @@ void runConsumer(std::atomic<size_t>** hashTable, size_t windowSizeInSec,
         index %= WRITE_RECEIVE_BUFFER_COUNT;
 
         volatile char* c = &buffer_ready_sign[index];
-        cout << "C=" <<  c << endl;
+        cout << "C[" << index << "]=" << c << endl;
 
         if (buffer_ready_sign[index] == BUFFER_USED_FLAG || buffer_ready_sign[index] == BUFFER_USED_SENDER_DONE)
         {
