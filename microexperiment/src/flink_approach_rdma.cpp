fa6fb39b76d79139147888f887d312977343a562
zeuchste <steffen.zeuch@dfki.de>
2019-04-11 11:24:21 +0200

    test

--- microexperiment/src/flink_approach_rdma.cpp
+++ microexperiment/src/flink_approach_rdma.cpp
@@ -637,8 +637,9 @@ int main(int argc, char *argv[])
             << " producedBuffers=" << producedBuffers
             << " consumedTuples=" << consumedTuples
             << " consumedBuffers=" << consumedBuffers
-            << " datasetSize(KB)" << producesTuples*sizeof(Tuple)/1024/1024
-            << " bandwidth GB/s" << (producesTuples*sizeof(Tuple)/1024/1024/1024)/elapsed_time
+            << " InputDatasetSize(KB)=" << producesTuples*sizeof(record)/1024/1024
+            << " TransferDatasetSize(KB)=" << producesTuples*sizeof(Tuple)/1024/1024
+            << " TransferBandwidth MB/s" << (producesTuples*sizeof(Tuple)/1024/1024)/elapsed_time
             << endl;
 }
 
