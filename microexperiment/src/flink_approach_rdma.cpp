5f5936ce076c4004391232f11811ff3b795ad058
zeuchste <steffen.zeuch@dfki.de>
2019-04-11 13:18:28 +0200

    test

--- microexperiment/src/flink_approach_rdma.cpp
+++ microexperiment/src/flink_approach_rdma.cpp
@@ -643,6 +643,8 @@ int main(int argc, char *argv[])
     ss << " readInputTuples=" << readInputTuples  << endl;
     ss << " readInputVolume(MB)=" << readInputTuples * sizeof(record) /1024 /1024 << endl;
     ss << " readInputThroughput=" << readInputTuples * sizeof(record)/elapsed_time << endl;
+    ss << " readBandWidth MB/s=" << (readInputTuples*sizeof(record)/1024/1024)/elapsed_time << endl;
+
     ss << " ----------------------------------------------" << endl;
 
     ss << " produced=" << producesTuples << endl;
