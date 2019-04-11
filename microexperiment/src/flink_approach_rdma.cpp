98d30a554c578f0c85494ef0fd1bb431590961d0
zeuchste <steffen.zeuch@dfki.de>
2019-04-11 12:25:53 +0200

    test

--- microexperiment/src/flink_approach_rdma.cpp
+++ microexperiment/src/flink_approach_rdma.cpp
@@ -384,8 +384,8 @@ void runConsumer(std::atomic<size_t>** hashTable, size_t windowSizeInSec,
 //            std::future<void> resultFromDB = std::async(std::launch::async, cosume_window_mem, (Tuple*)recv_buffers[index]->getData(), bufferSizeInTuples,
 //                                        hashTable, windowSizeInSec, campaingCnt, consumerID, produceCnt, bufferSizeInTuples);
 
-            cosume_window_mem((Tuple*)recv_buffers[index]->getData(), bufferSizeInTuples,
-                    hashTable, windowSizeInSec, campaingCnt, consumerID, produceCnt, bufferSizeInTuples);
+//            cosume_window_mem((Tuple*)recv_buffers[index]->getData(), bufferSizeInTuples,
+//                    hashTable, windowSizeInSec, campaingCnt, consumerID, produceCnt, bufferSizeInTuples);
             buffer_ready_sign[index] = BUFFER_READY_FLAG;
             if(is_done)
                 break;
