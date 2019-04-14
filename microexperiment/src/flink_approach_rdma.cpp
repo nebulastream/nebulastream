58715098b4f1765a0f6f95ca4b18c13507c9c529
zeuchste <steffen.zeuch@dfki.de>
2019-04-14 15:53:53 +0200

    test

--- microexperiment/src/flink_approach_rdma.cpp
+++ microexperiment/src/flink_approach_rdma.cpp
@@ -418,8 +418,8 @@ void runConsumerNew(std::atomic<size_t>** hashTable, size_t windowSizeInSec,
         {
             *consumedTuples = total_received_tuples;
             *consumedBuffers = total_received_buffers;
-            cout << "Thread=" << omp_get_thread_num() << " Done sending! Receiving a total of " << total_received_tuples << " tuples and " << total_received_buffers << " buffers"
-                            << " nobufferFound=" << noBufferFound << " startIDX=" << startIdx << " endIDX=" << endIdx << endl;
+//            cout << "Thread=" << omp_get_thread_num() << " Done sending! Receiving a total of " << total_received_tuples << " tuples and " << total_received_buffers << " buffers"
+//                            << " nobufferFound=" << noBufferFound << " startIDX=" << startIdx << " endIDX=" << endIdx << endl;
             return;
         }
     }//end of while
