84c12aab3234201b9b650e377c5b39930b87cd0c
zeuchste <steffen.zeuch@dfki.de>
2019-04-17 14:01:45 +0200

    test

--- microexperiment/src/flink_approach_rdma.cpp
+++ microexperiment/src/flink_approach_rdma.cpp
@@ -597,8 +597,10 @@ void runConsumerPartitioned(std::atomic<size_t>** hashTable, size_t windowSizeIn
     *consumedTuples = consumed;
     *consumedBuffers = total_received_buffers;
     *consumerNoBufferFound = noBufferFound;
-    cout << "Thread=" << omp_get_thread_num() << " Done sending! Receiving a total of " << total_received_tuples << " tuples and " << total_received_buffers << " buffers"
+    stringstream ss;
+    ss<< "Thread=" << omp_get_thread_num() << " Done sending! Receiving a total of " << total_received_tuples << " tuples and " << total_received_buffers << " buffers"
                 << " nobufferFound=" << noBufferFound << " startIDX=" << startIdx << " endIDX=" << endIdx << endl;
+    cout << ss.str();
 }
 
 void runConsumerNew(std::atomic<size_t>** hashTable, size_t windowSizeInSec,
@@ -648,8 +650,10 @@ void runConsumerNew(std::atomic<size_t>** hashTable, size_t windowSizeInSec,
         {
             *consumedTuples = total_received_tuples;
             *consumedBuffers = total_received_buffers;
-            cout << "Thread=" << omp_get_thread_num() << " Done sending! Receiving a total of " << total_received_tuples << " tuples and " << total_received_buffers << " buffers"
+            stringstream ss;
+            cout << "Thread=" << omp_get_thread_num() << " Receiving a total of " << total_received_tuples << " tuples and " << total_received_buffers << " buffers"
                             << " nobufferFound=" << noBufferFound << " startIDX=" << startIdx << " endIDX=" << endIdx << endl;
+            cout << ss.str();
             return;
         }
     }//end of while
