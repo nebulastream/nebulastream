382a3905c654a1369e575f7d63a8c2996f5cfedc
zeuchste <steffen.zeuch@dfki.de>
2019-04-18 16:47:16 +0200

    test

--- microexperiment/src/YSB_NO_PART.cpp
+++ microexperiment/src/YSB_NO_PART.cpp
@@ -743,13 +743,13 @@ int main(int argc, char *argv[])
         stringstream ss;
         ss  << "Producer Thread #" << omp_get_thread_num()  << ": on CPU " << sched_getcpu() << " nodes=";
         int numa_node = -1;
-        get_mempolicy(&numa_node, NULL, 0, (void*)sendBuffers, MPOL_F_NODE | MPOL_F_ADDR);
+        get_mempolicy(&numa_node, NULL, 0, (void*)&sendBuffers, MPOL_F_NODE | MPOL_F_ADDR);
         ss << numa_node << ",";
         get_mempolicy(&numa_node, NULL, 0, (void*)sendBuffers[0]->send_buffer, MPOL_F_NODE | MPOL_F_ADDR);
         ss << numa_node << ",";
-        get_mempolicy(&numa_node, NULL, 0, (void*)buffer_ready_sign, MPOL_F_NODE | MPOL_F_ADDR);
+        get_mempolicy(&numa_node, NULL, 0, (void*)&buffer_ready_sign, MPOL_F_NODE | MPOL_F_ADDR);
         ss << numa_node << ",";
-        get_mempolicy(&numa_node, NULL, 0, (void*)region_tokens, MPOL_F_NODE | MPOL_F_ADDR);
+        get_mempolicy(&numa_node, NULL, 0, (void*)&region_tokens, MPOL_F_NODE | MPOL_F_ADDR);
         ss << numa_node << ",";
         cout << ss.str() << endl;
 
