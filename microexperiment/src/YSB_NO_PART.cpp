b639207da3a3b758fcb530c086a9265772fe4f40
zeuchste <steffen.zeuch@dfki.de>
2019-04-18 13:20:20 +0200

    test

--- microexperiment/src/YSB_NO_PART.cpp
+++ microexperiment/src/YSB_NO_PART.cpp
@@ -588,6 +588,37 @@ void printHT(std::atomic<size_t>** hashTable, size_t campaingCnt)
     myfile.close();
 
 }
+short CorePin(int coreID)
+{
+  short status=0;
+  int nThreads = std::thread::hardware_concurrency();
+  //std::cout<<nThreads;
+  cpu_set_t set;
+  std::cout<<"\nPinning to Core:"<<coreID<<"\n";
+  CPU_ZERO(&set);
+
+  if(coreID == -1)
+  {
+    status=-1;
+    std::cout<<"CoreID is -1"<<"\n";
+    return status;
+  }
+
+  if(coreID > nThreads)
+  {
+    std::cout<<"Invalid CORE ID"<<"\n";
+    return status;
+  }
+
+  CPU_SET(coreID,&set);
+  if(sched_setaffinity(0, sizeof(cpu_set_t), &set) < 0)
+  {
+    std::cout<<"Unable to Set Affinity"<<"\n";
+    return -1;
+  }
+  return 1;
+}
+
 namespace po = boost::program_options;
 int main(int argc, char *argv[])
 {
@@ -677,7 +708,10 @@ int main(int argc, char *argv[])
     #pragma omp for
     for(size_t i = 0; i < numberOfConnections; i++)
     {
-        std::cout << "Thread #" << omp_get_thread_num()  << ": on CPU " << sched_getcpu() << "\n";
+//        std::cout << "Thread #" << omp_get_thread_num()  << ": on CPU " << sched_getcpu() << "\n";
+        CorePin(0);
+        while(true)
+            ;
        //setupRDMAProducer(connections[i], bufferSizeInTups);
     }
 }//end of pragma
