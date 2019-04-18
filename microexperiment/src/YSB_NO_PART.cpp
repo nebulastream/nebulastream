779a69bb2018507fcceabe2b3bc4dfee7ac8f339
zeuchste <steffen.zeuch@dfki.de>
2019-04-18 15:45:46 +0200

    test

--- microexperiment/src/YSB_NO_PART.cpp
+++ microexperiment/src/YSB_NO_PART.cpp
@@ -519,7 +519,7 @@ void setupRDMAProducer(VerbsConnection* connection, size_t bufferSizeInTuples, s
 
     ss << "Thread #" << omp_get_thread_num()  << ": on CPU " << sched_getcpu() << "\n";
 
-//    std::vector<TupleBuffer> sendBuffers;
+    std::vector<TupleBuffer> sendBuffers;
     for(size_t i = 0; i < NUM_SEND_BUFFERS; i++)
         sendBuffers.emplace_back(TupleBuffer(*connection, bufferSizeInTuples));
 
