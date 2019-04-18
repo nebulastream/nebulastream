69cae6b333513db190b078f581b2c51e08cba8b2
zeuchste <steffen.zeuch@dfki.de>
2019-04-18 17:39:51 +0200

    test

--- microexperiment/src/YSB_NO_PART.cpp
+++ microexperiment/src/YSB_NO_PART.cpp
@@ -1,3 +1,5 @@
+#include <string>
+
 #include <iostream>
 #include <cstring>
 #include <omp.h>
@@ -721,6 +723,10 @@ int main(int argc, char *argv[])
 {
     auto outer_thread_id = omp_get_thread_num();
     numa_run_on_node(outer_thread_id);
+    numa_set_preferred(outer_thread_id);
+    std::string str = std::to_string(outer_thread_id);
+    struct bitmask *bm = numa_parse_nodestring(str.c_str());
+    numa_bind(bm);
 
     TupleBuffer** sendBuffers = new TupleBuffer*[NUM_SEND_BUFFERS];
     for(size_t i = 0; i < NUM_SEND_BUFFERS; i++)
