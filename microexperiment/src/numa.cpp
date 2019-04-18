c8fb8a812d3a9778dc7761b64be57677f7a25552
zeuchste <steffen.zeuch@dfki.de>
2019-04-18 18:40:01 +0200

    test

--- microexperiment/src/numa.cpp
+++ microexperiment/src/numa.cpp
@@ -38,7 +38,6 @@ int main() {
     void* ptr_to_check2 = bla2;
 
     status[1];
-    ret_code;
     status[0]=-1;
     ret_code=move_pages(0 /*self memory */, 1, &ptr_to_check2,
        NULL, status, 0);
