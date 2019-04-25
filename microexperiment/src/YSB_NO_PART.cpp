468466791019ab373e12cd406c901b6242337ec6
zeuchste <steffen.zeuch@dfki.de>
2019-04-25 20:02:44 +0200

    test

--- microexperiment/src/YSB_NO_PART.cpp
+++ microexperiment/src/YSB_NO_PART.cpp
@@ -431,9 +431,17 @@ size_t runConsumerOneOnOne(Tuple* buffer, size_t bufferSizeInTuples, std::atomic
                     {
 //                        while(true)
 //                        {
-//                            sharedHTConnection->read_blocking(ht_sign_ready_buffer, ready_token);
+                            sharedHTConnection->read_blocking(ht_sign_ready_buffer, ready_token);
 ////                            cout << "read value is id="<< consumerID << ht_sign_ready[consumerID] << endl;
-//                            if(ht_sign_ready[consumerID] == BUFFER_READY_FLAG)
+                            if(ht_sign_ready[consumerID] == BUFFER_READY_FLAG)
+                            {
+                              cout << "ready" << endl;
+
+                            }
+                            else
+                            {
+                                cout << "not ready" << endl;
+                            }
 //                            {
 //                                break;
 //                            }
