d3388b479efa7ed7f75313dbcf04103b8126d52e
zeuchste <steffen.zeuch@dfki.de>
2019-04-28 13:37:08 +0200

    test

--- microexperiment/src/LM_Ponly.cpp
+++ microexperiment/src/LM_Ponly.cpp
@@ -1081,7 +1081,10 @@ int main(int argc, char *argv[])
             cout << "thread in critical = " << omp_get_thread_num() << endl;
             if(numberOfConnections == 1)
             {
-//                conInfos[omp_get_thread_num()] = setupProducerOnly(connections[0], bufferSizeInTups, campaingCnt, rank, numberOfProducer);
+                conInfos[omp_get_thread_num()] = setupProducerOnly(connections[0], bufferSizeInTups, campaingCnt, rank, numberOfProducer);
+                cout << "connections[0]=" << connections[0] << endl;
+                cout << "coninfo=" <<conInfos[omp_get_thread_num()] << endl;
+                cout << "con= " <<conInfos[omp_get_thread_num()]->con << endl;
                 conInfos[omp_get_thread_num()]->con = connections[0];
             }
             else if(numberOfConnections == 2)
