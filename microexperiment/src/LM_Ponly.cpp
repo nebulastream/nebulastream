6172a43b7ce200fb621f784f563b7de358029642
zeuchste <steffen.zeuch@dfki.de>
2019-04-29 18:28:19 +0200

    test

--- microexperiment/src/LM_Ponly.cpp
+++ microexperiment/src/LM_Ponly.cpp
@@ -608,7 +608,7 @@ int main(int argc, char *argv[])
     if((rank == 0 || rank == 2) && numberOfNodes != 2)
     {
         cout << "connecting 0 and 2" << endl;
-        SimpleInfoProvider info(target_rank, "mlx5_0", 1, PORT2, ip);//was 3
+        SimpleInfoProvider info(target_rank, "mlx5_1", 1, PORT2, ip);//was 3
         connections[1] = new VerbsConnection(&info);
         cout << "connection established rank 0 and 2" << endl;
     }
