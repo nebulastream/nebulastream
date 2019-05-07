269f415a50e90e298cba2cb6036cf2e4a6b90851
zeuchste <steffen.zeuch@dfki.de>
2019-05-07 21:28:15 +0200

    test

--- microexperiment/src/Part_PC.cpp
+++ microexperiment/src/Part_PC.cpp
@@ -1068,7 +1068,7 @@ int main(int argc, char *argv[])
     else if(rank == 1)//cloud 42
         target_rank = 0;
     else if(rank == 2)//
-        target_rank = 1;
+        target_rank = 3;
     else if(rank == 3)
         target_rank = 0;
     else
