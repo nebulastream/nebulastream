4bd2a750e38f944d11732c86dff6543e8a5e3840
zeuchste <steffen.zeuch@dfki.de>
2019-05-09 11:30:13 +0200

    test

--- microexperiment/src/Part_PC_Distinct.cpp
+++ microexperiment/src/Part_PC_Distinct.cpp
@@ -121,8 +121,8 @@ public:
         requestToken = connection.create_request_token();
         requestToken->setCompleted(true);
         tups = (Tuple*)(send_buffer->getAddress());
-        numberOfTuples = (size_t*) (send_buffer->getAddressWithOffset((bufferSizeInTuples * sizeof(Tuple)/sizeof(uint64_t))+1));
-        cout << "remaining=" << (send_buffer->getRemainingSizeInBytes((bufferSizeInTuples * sizeof(Tuple)/sizeof(uint64_t))+1)) << endl;;
+        numberOfTuples = (size_t*) send_buffer->getAddressWithOffset( ((bufferSizeInTuples+1) * sizeof(Tuple)/sizeof(uint64_t))) ;
+        cout << "remaining=" << (send_buffer->getRemainingSizeInBytes(((bufferSizeInTuples+1) * sizeof(Tuple)/sizeof(uint64_t)))) << endl;;
         *numberOfTuples = 0;
     }
 
