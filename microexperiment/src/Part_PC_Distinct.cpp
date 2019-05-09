418a1443cc1347d01968838e8ad5abab54193de9
zeuchste <steffen.zeuch@dfki.de>
2019-05-09 13:17:56 +0200

    test

--- microexperiment/src/Part_PC_Distinct.cpp
+++ microexperiment/src/Part_PC_Distinct.cpp
@@ -1463,7 +1463,9 @@ int main(int argc, char *argv[])
           {
              auto inner_thread_id = omp_get_thread_num();
              size_t i = inner_thread_id;
-             size_t share = NUM_SEND_BUFFERS/(numberOfConsumer/numaNodes);
+//             size_t share = NUM_SEND_BUFFERS/(numberOfConsumer/numaNodes);
+             size_t share = NUM_SEND_BUFFERS/(numberOfConsumer);
+
              size_t startIdx = i* share;
              size_t endIdx = (i+1)*share;
              if(i == numberOfConsumer -1)
