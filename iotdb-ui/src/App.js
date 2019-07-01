478f7104389761bcef685c08e7003d35dd6894a8
ankitgit <ankit.chaudhary@tu-berlin.de>
2019-07-01 21:30:01 +0200

    Initial commit to the start page

--- iotdb-ui/src/App.js
+++ iotdb-ui/src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Breadcrumb, BreadcrumbItem, Alert } from 'reactstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <div>
+          <Breadcrumb>
+            <BreadcrumbItem active><h1>IotDB WebInterface</h1></BreadcrumbItem>
+          </Breadcrumb>
+        </div>
+        <div>
+          <Alert color="primary">
+            This is a primary alert — check it out!
+          </Alert>
+        </div>
+
+      </div>
   );
 }
 
