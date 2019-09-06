e2140a2b54980e9dba267c3df337de020de259a8
ankitgit <ankit.chaudhary@tu-berlin.de>
2019-09-06 14:50:21 +0200

    Added operator numbers and additional info for topology graph

--- iotdb/impl/Optimizer/impl/BottomUp.cpp
+++ iotdb/impl/Optimizer/impl/BottomUp.cpp
@@ -6,13 +6,20 @@ using namespace std;
 
 FogExecutionPlan BottomUp::initializeExecutionPlan(InputQuery inputQuery, FogTopologyPlanPtr fogTopologyPlan) {
 
-    FogExecutionPlan executionGraph;
-    const OperatorPtr &sinkOperator = inputQuery.getRoot();
-    const vector<OperatorPtr> &sourceOperators = getSourceOperators(sinkOperator);
-    const deque<FogTopologyEntryPtr> &sourceNodes = getSourceNodes(fogTopologyPlan);
-    placeOperators(executionGraph, fogTopologyPlan, sourceOperators, sourceNodes);
-    completeExecutionGraphWithFogTopology(executionGraph, fogTopologyPlan);
-    return executionGraph;
+  FogExecutionPlan executionGraph;
+  const OperatorPtr &sinkOperator = inputQuery.getRoot();
+  const string &streamName = inputQuery.source_stream.getName();
+  const vector<OperatorPtr> &sourceOperators = getSourceOperators(sinkOperator);
+  const deque<FogTopologyEntryPtr> &sourceNodes = getSourceNodes(fogTopologyPlan, streamName);
+
+  if (sourceNodes.empty()) {
+    std::cout << "Unable to find the target source";
+    throw Exception("No source found in the topology");
+  }
+
+  placeOperators(executionGraph, fogTopologyPlan, sourceOperators, sourceNodes);
+  completeExecutionGraphWithFogTopology(executionGraph, fogTopologyPlan);
+  return executionGraph;
 }
 
 
--- iotdb/include/API/InputQuery.hpp
+++ iotdb/include/API/InputQuery.hpp
@@ -11,53 +11,52 @@
 #include <iostream>
 #include <string>
 
-
 namespace iotdb {
 
-    class Operator;
+class Operator;
 
-    typedef std::shared_ptr<Operator> OperatorPtr;
+typedef std::shared_ptr<Operator> OperatorPtr;
 
-    /**
-     * Interface to create a query.
-     */
-    class InputQuery {
-    public:
+/**
+ * Interface to create a query.
+ */
+class InputQuery {
+ public:
 
-        InputQuery &operator=(const InputQuery &query);
+  InputQuery &operator=(const InputQuery &query);
 
-        InputQuery(const InputQuery &);
+  InputQuery(const InputQuery &);
 
-        ~InputQuery();
+  ~InputQuery();
 
-        /**
-        * Creates a query from a particualr stream.
-        * @param sourceStream name of the stream to query.
-        * @param schema @deprecated will be removed when we have the catalog.
-        * @return the input query
-        */
-        static InputQuery from(Stream& stream);
+  /**
+  * Creates a query from a particualr stream.
+  * @param sourceStream name of the stream to query.
+  * @param schema @deprecated will be removed when we have the catalog.
+  * @return the input query
+  */
+  static InputQuery from(Stream &stream);
 
-        /**
-         * Selects a field from the input schema and place it in the output schema.
-         * @param field
-         * @return query
-         */
-        InputQuery &select(const Field &field);
+  /**
+   * Selects a field from the input schema and place it in the output schema.
+   * @param field
+   * @return query
+   */
+  InputQuery &select(const Field &field);
 
-        /**
-         * Selects two fields from the input schema and place them in the output schema.
-         * @param field
-         * @return query
-         */
-        InputQuery &select(const Field &field1, const Field &field2);
+  /**
+   * Selects two fields from the input schema and place them in the output schema.
+   * @param field
+   * @return query
+   */
+  InputQuery &select(const Field &field1, const Field &field2);
 
-        /**
-         * Filter records according to the predicate.
-         * @param predicate
-         * @return query
-         */
-        InputQuery &filter(const UserAPIExpression& predicate);
+  /**
+   * Filter records according to the predicate.
+   * @param predicate
+   * @return query
+   */
+  InputQuery &filter(const UserAPIExpression &predicate);
 
         /**
          * Map records to the resultField by the predicate.
@@ -67,62 +66,65 @@ namespace iotdb {
          */
         InputQuery &map(const AttributeField& resultField, const Predicate predicate);
 
-        /**
-         * Unify two queries.
-         * All records are contained in the result stream
-         * @return query
-         */
-        InputQuery &combine(const InputQuery &sub_query);
+  /**
+   * Unify two queries.
+   * All records are contained in the result stream
+   * @return query
+   */
+  InputQuery &combine(const InputQuery &sub_query);
 
-        /**
-         * Joins two streams according to the join predicate.
-         * @param sub_query right query.
-         * @param joinPred join predicate.
-         * @return query.
-         */
-        InputQuery &join(const InputQuery &sub_query, const JoinPredicatePtr &joinPred);
+  /**
+   * Joins two streams according to the join predicate.
+   * @param sub_query right query.
+   * @param joinPred join predicate.
+   * @return query.
+   */
+  InputQuery &join(const InputQuery &sub_query, const JoinPredicatePtr &joinPred);
 
+  /**
+   * Creates a window aggregation.
+   * @param windowType Window definition.
+   * @param aggregation Window aggregation function.
+   * @return query.
+   */
+  InputQuery &window(const WindowTypePtr windowType, const WindowAggregation &aggregation);
 
-        /**
-         * Creates a window aggregation.
-         * @param windowType Window definition.
-         * @param aggregation Window aggregation function.
-         * @return query.
-         */
-        InputQuery &window(const WindowTypePtr windowType, const WindowAggregation &aggregation);
+  /**
+  * Registers the query as a source in the catalog.
+  * @param name the name for the result stream.
+  */
+  InputQuery &to(const std::string &name);
 
-        /**
-        * Registers the query as a source in the catalog.
-        * @param name the name for the result stream.
-        */
-        InputQuery & to(const std::string &name);
+  /**
+   * Adds a file sink, which writes all stream records to the destination file.
+   * @param file_name
+   */
+  InputQuery &writeToFile(const std::string &file_name);
 
-        /**
-         * Adds a file sink, which writes all stream records to the destination file.
-         * @param file_name
-         */
-        InputQuery & writeToFile(const std::string &file_name);
+  /**
+   * Adds a print sink, which prints all query results to the output stream.
+   */
+  InputQuery &print(std::ostream & = std::cout);
 
-        /**
-         * Adds a print sink, which prints all query results to the output stream.
-         */
-        InputQuery & print(std::ostream & = std::cout);
+  // helper operators
+  OperatorPtr getRoot() const { return root; };
 
-        // helper operators
-        OperatorPtr getRoot() const { return root; };
+  Stream source_stream;
 
-    private:
-        InputQuery(Stream& source_stream);
-
-        OperatorPtr root;
-        Stream source_stream;
-    };
+  int getNextOperatorId() {
+    operatorIdCounter++;
+    return this->operatorIdCounter;
+  }
+ private:
+  InputQuery(Stream &source_stream);
+  int operatorIdCounter = 0;
+  OperatorPtr root;
+};
 
 /* this function **executes** the code provided by the user and returns an InputQuery Object */
-    const InputQuery createQueryFromCodeString(const std::string &);
+const InputQuery createQueryFromCodeString(const std::string &);
 
-    typedef std::shared_ptr<InputQuery> InputQueryPtr;
-
+typedef std::shared_ptr<InputQuery> InputQueryPtr;
 
 } // namespace iotdb
 
