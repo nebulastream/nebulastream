ff4e72bc490aeb40d9fd7bcea7f459196e408cf3
zeuchste <steffen.zeuch@dfki.de>
2019-11-04 19:33:23 +0100

    change Dispatcher

--- iotdb/include/NodeEngine/NodeEngine.hpp
+++ iotdb/include/NodeEngine/NodeEngine.hpp
@@ -1,9 +1,7 @@
 #ifndef NODE_ENGINE_H
 #define NODE_ENGINE_H
 
-//#include "../Runtime/CompiledDummyPlan.hpp"
 #include "NodeProperties.hpp"
-//#include "json.hpp"
 #include <CodeGen/QueryExecutionPlan.hpp>
 #include <API/Config.hpp>
 #include <NodeEngine/ThreadPool.hpp>
@@ -15,82 +13,80 @@
 #include <vector>
 #include <zmq.hpp>
 
-
 namespace iotdb {
 using JSON = nlohmann::json;
 
 class NodeEngine {
-public:
+ public:
 
-    /**
-     * @brief Create a node engine and gather node information
-     * and initialize Dispatcher, BufferManager and ThreadPool
-     */
-    NodeEngine()
-{
-        props = new NodeProperties();
-        init();
-}
+  /**
+   * @brief Create a node engine and gather node information
+   * and initialize Dispatcher, BufferManager and ThreadPool
+   */
+  NodeEngine() {
+    props = new NodeProperties();
+    init();
+  }
 
-    /**
-     * @brief deploy a new query plan to via the dispatcher
-     * @param new query plan
-     */
-    void deployQuery(QueryExecutionPlanPtr ptr);
+  /**
+   * @brief deploy a new query plan to via the dispatcher
+   * @param new query plan
+   */
+  void deployQuery(QueryExecutionPlanPtr ptr);
 
-    /**
-     * @brief undeploy an existing query plan to via the dispatcher
-     * @param query plan to deploy
-     */
-    void undeployQuery(QueryExecutionPlanPtr qep);
+  /**
+   * @brief undeploy an existing query plan to via the dispatcher
+   * @param query plan to deploy
+   */
+  void undeployQuery(QueryExecutionPlanPtr qep);
 
-    /**
-     * @brief change config of running node eninge
-     * @param config with params:
-     * numberOfThreads, numberOfBuffers, bufferSize
-     */
-    void applyConfig(Config& conf);
+  /**
+   * @brief change config of running node eninge
+   * @param config with params:
+   * numberOfThreads, numberOfBuffers, bufferSize
+   */
+  void applyConfig(Config& conf);
 
-    /**
-     * @brief start thread pool
-     */
+  /**
+   * @brief start thread pool
+   */
 
-    void start();
-    /**
-     * @brief deploy all queries and start thread pool
-     */
-    void startWithRedeploy();
+  void start();
+  /**
+   * @brief deploy all queries and start thread pool
+   */
+  void startWithRedeploy();
 
-    /**
-     * @brief stop thread pool
-     */
-    void stop();
+  /**
+   * @brief stop thread pool
+   */
+  void stop();
 
-    /**
-     * @brief undeploy all queries and stop thread pool
-     */
-    void stopWithUndeploy();
+  /**
+   * @brief undeploy all queries and stop thread pool
+   */
+  void stopWithUndeploy();
 
-    /**
-         * @brief undeploy all queries and delete all qeps
-         */
-    void resetQEPs();
+  /**
+   * @brief undeploy all queries and delete all qeps
+   */
+  void resetQEPs();
 
-    JSON getNodePropertiesAsJSON();
-    NodeProperties* getNodeProperties();
+  JSON getNodePropertiesAsJSON();
+  NodeProperties* getNodeProperties();
 
-private:
-    /**
-     * @brief initialize Dispatcher, BufferManager and ThreadPool
-     */
-    void init();
+ private:
+  /**
+   * @brief initialize Dispatcher, BufferManager and ThreadPool
+   */
+  void init();
 
-    NodeProperties* props;
-    std::vector<QueryExecutionPlanPtr> qeps;
+  NodeProperties* props;
+  std::vector<QueryExecutionPlanPtr> qeps;
 
 };
 
 typedef std::shared_ptr<NodeEngine> NodeEnginePtr;
 
-} // namespace iotdb
+}  // namespace iotdb
 #endif // NODE_ENGINE_H
--- iotdb/include/NodeEngine/NodeProperties.hpp
+++ iotdb/include/NodeEngine/NodeProperties.hpp
@@ -1,7 +1,3 @@
-/**\brief:
- *         This class captures the properties of a node which are send to and updated continuously.
- *
- */
 
 #ifndef _METRICS_H
 #define _METRICS_H
@@ -21,139 +17,145 @@
 namespace iotdb {
 using JSON = nlohmann::json;
 
+/**
+ * \brief: This class captures the properties of a node which are send to and updated continuously.
+ */
 class NodeProperties {
-public:
+ public:
 
-    /**
-     * @brief create a new NodeProperties object
-     * @param default 0 cpus
-     */
-    NodeProperties():nbrProcessors(0) {};
+  /**
+   * @brief create a new NodeProperties object
+   * @param default 0 cpus
+   */
+  NodeProperties()
+      : nbrProcessors(0) {
+  }
+  ;
 
-    ~NodeProperties() {}
+  ~NodeProperties() {
+  }
 
-    /**
-     * @brief print cpu,network,memory, and filesstem stats on cout
-     */
-    void print();
+  /**
+   * @brief print cpu,network,memory, and filesstem stats on cout
+   */
+  void print();
 
-    /**
-     * @brief dumbs all metrics into a string
-     * @param indent not set
-     * @return serialized json object as string
-     */
-    std::string dump(int setw=-1);
+  /**
+   * @brief dumbs all metrics into a string
+   * @param indent not set
+   * @return serialized json object as string
+   */
+  std::string dump(int setw = -1);
 
-    /**
-     * @brief set existing serialized node property into nodeProperties.
-     * Load entire _metrics and all sub jsons
-     * @param char pointer to serialized JSON
-     */
-    void set(const char *metricsBuffer);
+  /**
+   * @brief set existing serialized node property into nodeProperties.
+   * Load entire _metrics and all sub jsons
+   * @param char pointer to serialized JSON
+   */
+  void set(const char *metricsBuffer);
 
-    /**
-     * @brief load existing serialized node property
-     * @param char pointer to serialized JSON
-     */
-    JSON load();
+  /**
+   * @brief load existing serialized node property
+   * @param char pointer to serialized JSON
+   */
+  JSON load();
 
-    /**
-     * @brief gather cpu information from /proc/stat
-     * Gathered stats for each cpu:
-     * name, user, nice, system, idle, iowait, irq,
-     * softirq, steal, guest, guest_nice
-     */
-    void readCpuStats();
+  /**
+   * @brief gather cpu information from /proc/stat
+   * Gathered stats for each cpu:
+   * name, user, nice, system, idle, iowait, irq,
+   * softirq, steal, guest, guest_nice
+   */
+  void readCpuStats();
 
-    /**
-     * @brief gather memory information from sysinfo
-     * Gathered stats:
-     * totalram, totalswap, freeram, sharedram, bufferram,
-     * freeswap, totalhigh, freehigh, procs, mem_unit,
-     * loads_1min, loads_5min, loads_15min
-     */
-    void readMemStats();
+  /**
+   * @brief gather memory information from sysinfo
+   * Gathered stats:
+   * totalram, totalswap, freeram, sharedram, bufferram,
+   * freeswap, totalhigh, freehigh, procs, mem_unit,
+   * loads_1min, loads_5min, loads_15min
+   */
+  void readMemStats();
 
-    /**
-     * @brief gather disk information from statvfs
-     * Gathered stats:
-     * f_bsize, f_frsize, f_blocks, f_bfree, f_bavail
-     */
-    void readDiskStats();
+  /**
+   * @brief gather disk information from statvfs
+   * Gathered stats:
+   * f_bsize, f_frsize, f_blocks, f_bfree, f_bavail
+   */
+  void readDiskStats();
 
-    /**
-     * @brief gather network information from hostname and ifaddrs
-     * Gathered stats for each device:
-     * hostV4, hostV6, tx_packets, tx_bytes, tx_dropped,
-     * rx_packets, rx_bytes, rx_dropped
-     * todo: add bandwidth measurements here
-     */
-    void readNetworkStats();
+  /**
+   * @brief gather network information from hostname and ifaddrs
+   * Gathered stats for each device:
+   * hostV4, hostV6, tx_packets, tx_bytes, tx_dropped,
+   * rx_packets, rx_bytes, rx_dropped
+   * todo: add bandwidth measurements here
+   */
+  void readNetworkStats();
 
-    /**
-     * @brief get CPU stats
-     * @return cpu stats as string
-     */
-    std::string getCpuStats();
+  /**
+   * @brief get CPU stats
+   * @return cpu stats as string
+   */
+  std::string getCpuStats();
 
-    /**
-     * @brief get memory stats
-     * @return memory stats as string
-     */
-    std::string getMemStats();
+  /**
+   * @brief get memory stats
+   * @return memory stats as string
+   */
+  std::string getMemStats();
 
-    /**
-     * @brief get disk stats
-     * @return disk stats as string
-     */
-    std::string getDiskStats();
+  /**
+   * @brief get disk stats
+   * @return disk stats as string
+   */
+  std::string getDiskStats();
 
-    /**
-     * @brief get network stats
-     * @return network stats as string
-     */
-    std::string getNetworkStats();
+  /**
+   * @brief get network stats
+   * @return network stats as string
+   */
+  std::string getNetworkStats();
 
-    /**
-     * @brief metric summary
-     * @return all stats in one JSON file
-     */
-    std::string getMetric();
+  /**
+   * @brief metric summary
+   * @return all stats in one JSON file
+   */
+  std::string getMetric();
 
-    /**
-     * @brief get CPU stats
-     * @return cpu stats as string
-     */
-    std::string getClientName();
+  /**
+   * @brief get CPU stats
+   * @return cpu stats as string
+   */
+  std::string getClientName();
 
-    /**
-     * @brief get CPU stats
-     * @return cpu stats as string
-     */
-    std::string getClientPort();
+  /**
+   * @brief get CPU stats
+   * @return cpu stats as string
+   */
+  std::string getClientPort();
 
-    /**
-     * @brief set client name manually (for testing locally)
-     * @param client name as string
-     */
-    void setClientName(std::string clientname);
+  /**
+   * @brief set client name manually (for testing locally)
+   * @param client name as string
+   */
+  void setClientName(std::string clientname);
 
-    /**
-     * @brief set client port manually (for testing locally)
-     * @return client port as string
-     */
-    void setClientPort(std::string clientPort);
+  /**
+   * @brief set client port manually (for testing locally)
+   * @return client port as string
+   */
+  void setClientPort(std::string clientPort);
 
-
-private:
-    long nbrProcessors;
-    JSON _metrics;
-    JSON _mem;
-    JSON _disk;
-    JSON _cpus;
-    JSON _nets;
-    std::string clientName;
-    std::string clientPort;
+ private:
+  long nbrProcessors;
+  JSON _metrics;
+  JSON _mem;
+  JSON _disk;
+  JSON _cpus;
+  JSON _nets;
+  std::string clientName;
+  std::string clientPort;
 };
 
 typedef std::shared_ptr<NodeProperties> NodePropertiesPtr;
