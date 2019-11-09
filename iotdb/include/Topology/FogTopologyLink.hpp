c78820042eb4df97a449ef00217179827520c5e3
Ankit Chaudhary <my007id@gmail.com>
2019-11-09 12:29:35 +0100

    176: post review changes

--- iotdb/include/Topology/FogTopologyLink.hpp
+++ iotdb/include/Topology/FogTopologyLink.hpp
@@ -7,66 +7,56 @@
 #include "Util/ErrorHandling.hpp"
 
 namespace iotdb {
-#define NOT_EXISTING_LINK_ID INT_MAX
+#define NOT_EXISTING_LINK_ID std::numeric_limits<size_t>::max()
 
 enum LinkType { NodeToNode, NodeToSensor, SensorToNode };
 
-static size_t linkID = 1;
-
 class FogTopologyLink {
 
-  public:
-    FogTopologyLink(FogTopologyEntryPtr pSourceNode, FogTopologyEntryPtr pDestNode)
-    {
-        linkId = NOT_EXISTING_LINK_ID;
-        sourceNode = pSourceNode;
-        destNode = pDestNode;
+ public:
+  explicit FogTopologyLink(FogTopologyEntryPtr pSourceNode, FogTopologyEntryPtr pDestNode) {
+    linkId = NOT_EXISTING_LINK_ID;
+    sourceNode = pSourceNode;
+    destNode = pDestNode;
+  }
+
+  void setLinkID(size_t pLinkID) { linkId = pLinkID; }
+
+  size_t getId() { return linkId; }
+
+  FogTopologyEntryPtr getSourceNode() { return sourceNode; }
+
+  size_t getSourceNodeId() { return sourceNode->getId(); }
+
+  FogTopologyEntryPtr getDestNode() { return destNode; }
+
+  size_t getDestNodeId() { return destNode->getId(); }
+
+  LinkType getLinkType() {
+    if (sourceNode->getEntryType() == Worker && destNode->getEntryType() == Worker) {
+      return NodeToNode;
+    } else if (sourceNode->getEntryType() == Sensor && destNode->getEntryType() == Worker) {
+      return SensorToNode;
+    } else if (sourceNode->getEntryType() == Worker && destNode->getEntryType() == Sensor) {
+      return NodeToSensor;
     }
+    IOTDB_FATAL_ERROR("Unrecognized LinkType!");
+  }
 
-    void setLinkID(size_t pLinkID) { linkId = pLinkID; }
-
-    size_t getId() { return linkId; }
-
-    FogTopologyEntryPtr getSourceNode() { return sourceNode; }
-
-    size_t getSourceNodeId() { return sourceNode->getId(); }
-
-    FogTopologyEntryPtr getDestNode() { return destNode; }
-
-    size_t getDestNodeId() { return destNode->getId(); }
-
-    LinkType getLinkType()
-    {
-        if (sourceNode->getEntryType() == Worker && destNode->getEntryType() == Worker) {
-            return NodeToNode;
-        }
-        else if (sourceNode->getEntryType() == Sensor && destNode->getEntryType() == Worker) {
-            return SensorToNode;
-        }
-        else if (sourceNode->getEntryType() == Worker && destNode->getEntryType() == Sensor) {
-            return NodeToSensor;
-        }
-        IOTDB_FATAL_ERROR("Unrecognized LinkType!");
+  std::string getLinkTypeString() {
+    switch (getLinkType()) {
+      case NodeToNode:return "NodeToNode";
+      case SensorToNode:return "SensorToNode";
+      case NodeToSensor:return "NodeToSensor";
     }
+    IOTDB_FATAL_ERROR("String for LinkType not found!");
+  }
 
-    std::string getLinkTypeString()
-    {
-        switch (getLinkType()) {
-        case NodeToNode:
-            return "NodeToNode";
-        case SensorToNode:
-            return "SensorToNode";
-        case NodeToSensor:
-            return "NodeToSensor";
-        }
-        IOTDB_FATAL_ERROR("String for LinkType not found!");
-    }
+ private:
+  size_t linkId;
 
-  private:
-    size_t linkId;
-
-    FogTopologyEntryPtr sourceNode;
-    FogTopologyEntryPtr destNode;
+  FogTopologyEntryPtr sourceNode;
+  FogTopologyEntryPtr destNode;
 };
 
 typedef std::shared_ptr<FogTopologyLink> FogTopologyLinkPtr;
