71e17298cf8e90d001718a24c09899f389f00dff
x.chatziliadis <x.chatziliadis@tu-berlin.de>
2019-11-17 15:29:46 +0100

    WIP (Tests for BoostSerialization are working)

--- iotdb/include/API/Schema.hpp
+++ iotdb/include/API/Schema.hpp
@@ -14,28 +14,31 @@ typedef std::shared_ptr<AttributeField> AttributeFieldPtr;
 typedef std::vector<AttributeFieldPtr> VecAttributeFieldPtr;
 
 class Schema {
-  public:
-    Schema();
-    static Schema create();
+ public:
+  Schema();
+  static Schema create();
 
-    Schema& copyFields(Schema const& schema);
-    Schema& addField(AttributeFieldPtr field);
-    Schema& addField(const std::string& name, const BasicType&);
-    Schema& addField(const std::string& name, DataTypePtr data);
-    Schema& addField(const std::string& name, uint32_t size);
+  Schema &copyFields(Schema const &schema);
+  Schema &addField(AttributeFieldPtr field);
+  Schema &addField(const std::string &name, const BasicType &);
+  Schema &addField(const std::string &name, DataTypePtr data);
+  Schema &addField(const std::string &name, uint32_t size);
 
-    // Schema &addFixSizeField(const std::string name, const APIDataType data_type);
-    // Schema &addVarSizeField(const std::string name, const APIDataType data_type, const size_t data_size);
-    AttributeFieldPtr get(const std::string name);
-    const AttributeFieldPtr operator[](uint32_t index) const;
+  // Schema &addFixSizeField(const std::string name, const APIDataType data_type);
+  // Schema &addVarSizeField(const std::string name, const APIDataType data_type, const size_t data_size);
+  AttributeFieldPtr get(const std::string name);
+  const AttributeFieldPtr operator[](uint32_t index) const;
 
-    size_t getSize() const;
-    size_t getSchemaSize() const;
-    const std::string toString() const;
+  size_t getSize() const;
+  size_t getSchemaSize() const;
+  const std::string toString() const;
 
-    std::vector<AttributeFieldPtr> fields;
+  std::vector<AttributeFieldPtr> fields;
 
-    template <class Archive> void serialize(Archive& ar, const unsigned int version) {}
+  template<class Archive>
+  void serialize(Archive &ar, const unsigned int version) {
+    ar & fields;
+  }
 };
 } // namespace iotdb
 #endif // API_SCHEMA_H
