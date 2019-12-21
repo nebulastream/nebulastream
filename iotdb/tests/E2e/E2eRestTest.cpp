3b2c3685add08395896d78aab5db52da2f4409eb
zeuchste <steffen.zeuch@dfki.de>
2019-12-21 14:11:01 +0100

    update test

--- iotdb/tests/E2e/E2eRestTest.cpp
+++ iotdb/tests/E2e/E2eRestTest.cpp
@@ -42,7 +42,8 @@ class E2eRestTest : public testing::Test {
   }
 
   static void TearDownTestCase() {
-    std::cout << "Tear down ActorCoordinatorWorkerTest test class." << std::endl;
+    std::cout << "Tear down ActorCoordinatorWorkerTest test class."
+              << std::endl;
 
   }
 
@@ -77,26 +78,23 @@ class E2eRestTest : public testing::Test {
 };
 
 TEST_F(E2eRestTest, testExecutingValidUserQuery) {
-
-
   cout << " start coordinator" << endl;
-   bp::child coordinatorProc("../nesCoordinator");
-   cout << "started coordinator with pid = " << coordinatorProc.id() << endl;
-   bp::child workerProc("../nesWorker");
-   cout << "started worker with pid = " << workerProc.id() << endl;
-   coordinatorPid = workerProc.id();
-   workerPid = coordinatorProc.id();
-   sleep(3);
-  std::string outputFilePath = "blob.txt";
-//  string body =
-//      "{\"userQuery\" : \"Schema schema = Schema::create().addField(\\\"id\\\", BasicType::UINT32).addField(\\\"value\\\", BasicType::UINT64);Stream stream = Stream(\\\"default\\\", schema);InputQuery inputQuery = InputQuery::from(stream).writeToFile(\\\""
-//          + outputFilePath
-//          + "\\\"); return inputQuery;\",\"strategyName\" : \"BottomUp\"}";
-//
+  bp::child coordinatorProc("./nesCoordinator");
+  cout << "started coordinator with pid = " << coordinatorProc.id() << endl;
+  sleep(2);
+  bp::child workerProc("./nesWorker");
+  cout << "started worker with pid = " << workerProc.id() << endl;
+  coordinatorPid = workerProc.id();
+  workerPid = coordinatorProc.id();
+  sleep(3);
+//  std::string outputFilePath = "blob.txt";
+
   std::stringstream ss;
   ss << "{\"userQuery\" : \"Schema schema = Schema::create().addField(\\\"id\\\", BasicType::UINT32).addField(\\\"value\\\", BasicType::UINT64);";
-  ss << "Stream stream = Stream(\\\"default\\\", schema);InputQuery inputQuery = InputQuery::from(stream).print(std::cout);";
-  ss << "return inputQuery;\",\"strategyName\" : \"BottomUp\"}";
+  ss << "Stream stream = Stream(\\\"default\\\", schema); InputQuery inputQuery = InputQuery::from(stream).print(std::cout);";
+  ss << " return inputQuery;\",\"strategyName\" : \"BottomUp\"}";
+  ss << endl;
+  cout << "string submit=" << ss.str();
   string body = ss.str();
 
   web::json::value json_return;
@@ -124,82 +122,18 @@ TEST_F(E2eRestTest, testExecutingValidUserQuery) {
   std::cout << "Query ID: " << queryId << std::endl;
   EXPECT_TRUE(!queryId.empty());
 
-  ifstream my_file(outputFilePath);
-  EXPECT_TRUE(my_file.good());
+//  ifstream my_file(outputFilePath);
+//  EXPECT_TRUE(my_file.good());
 
-  int response = remove(outputFilePath.c_str());
-
-  EXPECT_TRUE(response == 0);
-  sleep(3);
+//  int response = remove(outputFilePath.c_str());
+//
+//  EXPECT_TRUE(response == 0);
+  sleep(2);
   cout << "Killing worker process->PID: " << workerPid << endl;
   workerProc.terminate();
-
+  sleep(2);
   cout << "Killing coordinator process->PID: " << coordinatorPid << endl;
   coordinatorProc.terminate();
 
-
 }
-
-//TEST_F(E2eRestTest, testExecutingValidUserQuery) {
-//  return;
-//    int pidC, pidW;
-//    bool terminated = false;
-//    // first we fork the process
-//    if (pidC = fork()) {
-//        sleep(2);
-//        if (pidW = fork()) {
-//            sleep(2);
-//
-//            std::string outputFilePath = "blob.txt";
-//            string body = "{\"userQuery\" : \"Schema schema = Schema::create().addField(\\\"id\\\", BasicType::UINT32).addField(\\\"value\\\", BasicType::UINT64);Stream stream = Stream(\\\"default\\\", schema);InputQuery inputQuery = InputQuery::from(stream).writeToFile(\\\"" + outputFilePath +"\\\"); return inputQuery;\",\"strategyName\" : \"BottomUp\"}";
-//            web::json::value json_return;
-//
-//            web::http::client::http_client client("http://localhost:8081/v1/iotdb/service/execute-query");
-//            client.request(web::http::methods::POST, U("/"), body)
-//                .then([](const web::http::http_response& response) {
-//                  cout << "get first then" << endl;
-//                  return response.extract_json();
-//                })
-//                .then([&json_return](const pplx::task<web::json::value>& task) {
-//                  try {
-//                      cout << "set return" << endl;
-//                      json_return = task.get();
-//                  }
-//                  catch (const web::http::http_exception& e) {
-//                    cout << "error while setting return" << endl;
-//                      std::cout << "error " << e.what() << std::endl;
-//                  }
-//                })
-//                .wait();
-//
-//            std::cout << "try to acc return" << std::endl;
-//
-//            string queryId = json_return.at("queryId").as_string();
-//            std::cout << "Query ID: " << queryId << std::endl;
-//            EXPECT_TRUE(!queryId.empty());
-//
-//            ifstream my_file(outputFilePath);
-//            EXPECT_TRUE(my_file.good());
-//
-//            int response = remove(outputFilePath.c_str());
-//
-//            EXPECT_TRUE(response == 0);
-//
-//            cout << "Killing worker process->PID: " << pidW << endl;
-//            kill(pidW, SIGKILL);
-//
-//            cout << "Killing coordinator process->PID: " << pidC << endl;
-//            kill(pidC, SIGKILL);
-//        } else {
-//            cout << "start worker" << endl;
-//            const char executableC[] = "../nesWorker";
-//            execlp(executableC, executableC, NULL);
-//        }
-//    } else {
-//      cout << " start coordinator" << endl;
-//        const char executableC[] = "../nesCoordinator";
-//        execlp(executableC, executableC, NULL);
-//    }
-//      sleep(5);
-//}
 }
