42e574728355fe45be91cceb9d72f6fafb8d3918
zeuchste <steffen.zeuch@dfki.de>
2019-12-21 16:50:28 +0100

    solved

--- iotdb/impl/Services/OptimizerService.cpp
+++ iotdb/impl/Services/OptimizerService.cpp
@@ -1,6 +1,8 @@
 #include "Services/OptimizerService.hpp"
 #include <Topology/FogTopologyManager.hpp>
 #include <Optimizer/FogOptimizer.hpp>
+#include <CodeGen/QueryPlanBuilder.hpp>
+
 
 using namespace iotdb;
 using namespace web;
@@ -13,8 +15,14 @@ json::value OptimizerService::getExecutionPlanAsJson(InputQueryPtr inputQuery, s
 FogExecutionPlan OptimizerService::getExecutionPlan(InputQueryPtr inputQuery, string optimizationStrategyName) {
   FogTopologyManager &fogTopologyManager = FogTopologyManager::getInstance();
   const FogTopologyPlanPtr &topologyPlan = fogTopologyManager.getTopologyPlan();
-
+  cout << "topo=" << topologyPlan->getTopologyPlanString() << endl;
   FogOptimizer queryOptimizer;
+
+  QueryPlanBuilder queryPlanBuilder;
+  const json::value &basePlan = queryPlanBuilder.getBasePlan(inputQuery);
+
+  cout << "query plan=" << basePlan << endl;
+
 
   return queryOptimizer.prepareExecutionGraph(optimizationStrategyName, inputQuery, topologyPlan);
 }
