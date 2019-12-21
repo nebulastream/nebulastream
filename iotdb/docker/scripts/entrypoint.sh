2dbea835ec235663e37f0fd6e2e699d26cd0bb27
zeuchste <steffen.zeuch@dfki.de>
2019-12-21 21:59:46 +0100

    travis change

--- iotdb/docker/scripts/entrypoint.sh
+++ iotdb/docker/scripts/entrypoint.sh
@@ -7,10 +7,7 @@ then
     mkdir -p /iotdb/build
     cd /iotdb/build
     cmake -DCMAKE_BUILD_TYPE=Release -DBoost_NO_SYSTEM_PATHS=TRUE -DBoost_INCLUDE_DIR="/usr/include" -DBoost_LIBRARY_DIR="/usr/lib/x86_64-linux-gnu" -DCPPRESTSDK_DIR="/usr/lib/x86_64-linux-gnu/cmake/" ..
-    make -j -w 
-    ln -s /iotdb/build/nesCoordinator /iotdb/build/tests/nesCoordinator
-    ln -s /iotdb/build/nesWorker /iotdb/build/tests/nesWorker
-    make test_debug
+    make -j -w && ln -s /iotdb/build/nesCoordinator /iotdb/build/tests/nesCoordinator && ln -s /iotdb/build/nesWorker /iotdb/build/tests/nesWorker && make test_debug
 else
     exec $@
 fi
