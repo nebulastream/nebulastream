5382a5dfd63a2026810ed0a728b24cd915d6035e
zeuchste <steffen.zeuch@dfki.de>
2019-12-22 14:02:37 +0100

    new build4

--- iotdb/docker/scripts/entrypoint.sh
+++ iotdb/docker/scripts/entrypoint.sh
@@ -7,7 +7,7 @@ then
     mkdir -p /iotdb/build
     cd /iotdb/build
     cmake -DCMAKE_BUILD_TYPE=Release -DBoost_NO_SYSTEM_PATHS=TRUE -DBoost_INCLUDE_DIR="/usr/include" -DBoost_LIBRARY_DIR="/usr/lib/x86_64-linux-gnu" -DCPPRESTSDK_DIR="/usr/lib/x86_64-linux-gnu/cmake/" -DCMAKE_CXX_FLAGS_RELEASE:STRING=" -Wno-unused-variable -Wno-unused-parameter -Wno-ignored-qualifiers -Wno-sign-compare " ..
-    make -j3
+    make -j4
     #&& make test_debug
 else
     exec $@
