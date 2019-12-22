dc32956024610eee849303edace2a0ed4cc145fb
zeuchste <steffen.zeuch@dfki.de>
2019-12-22 15:11:22 +0100

    travis change 2

--- iotdb/docker/scripts/entrypoint.sh
+++ iotdb/docker/scripts/entrypoint.sh
@@ -7,7 +7,7 @@ then
     mkdir -p /iotdb/build
     cd /iotdb/build
     cmake -DCMAKE_BUILD_TYPE=Release -DBoost_NO_SYSTEM_PATHS=TRUE -DBoost_INCLUDE_DIR="/usr/include" -DBoost_LIBRARY_DIR="/usr/lib/x86_64-linux-gnu" -DCPPRESTSDK_DIR="/usr/lib/x86_64-linux-gnu/cmake/" -DCMAKE_CXX_FLAGS_RELEASE:STRING=" -Wno-unused-variable -Wno-unused-parameter -Wno-ignored-qualifiers -Wno-sign-compare " ..
-    make -j4 && make test_debug
+    make -j4 && echo "start coordinaotr" && ./nesCoordinator #&& make test_debug
 else
     exec $@
 fi
