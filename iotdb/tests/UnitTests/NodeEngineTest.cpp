381e20f2ad64c9b001cb216f4d0d1f8cdadfbc3d
zeuchste <steffen.zeuch@dfki.de>
2019-12-27 11:50:23 +0100

    improved version of tests

--- iotdb/tests/UnitTests/NodeEngineTest.cpp
+++ iotdb/tests/UnitTests/NodeEngineTest.cpp
@@ -1,4 +1,3 @@
-
 #include <cassert>
 #include <iostream>
 
@@ -10,74 +9,89 @@
 #include <API/Types/DataTypes.hpp>
 #include <SourceSink/SourceCreator.hpp>
 #include <SourceSink/SinkCreator.hpp>
+#include <sstream>
+using namespace std;
 
 #define DEBUG_OUTPUT
 namespace iotdb {
 
+std::string expectedOutput =
+    "+----------------------------------------------------+\n"
+        "|sum:UINT32|\n"
+        "+----------------------------------------------------+\n"
+        "|10|\n"
+        "+----------------------------------------------------+";
+std::string filePath = "file.txt";
+
 class CompiledTestQueryExecutionPlan : public HandCodedQueryExecutionPlan {
  public:
-uint64_t count;
-uint64_t sum;
-CompiledTestQueryExecutionPlan()
-    : HandCodedQueryExecutionPlan(),
-      count(0),
-      sum(0) {
-}
-
-void setDataSource(DataSourcePtr source) {
-  sources.push_back(source);
-}
-void setDataSink(DataSinkPtr sink) {
-  sinks.push_back(sink);
-}
-bool firstPipelineStage(const TupleBuffer&) {
-  return false;
-}
-
-bool executeStage(uint32_t pipeline_stage_id, const TupleBufferPtr buf) {
-  uint64_t* tuples = (uint64_t*) buf->getBuffer();
-
-  IOTDB_INFO("Test: Start execution");
-
-  for (size_t i = 0; i < buf->getNumberOfTuples(); ++i) {
-    count++;
-    sum += tuples[i];
+  uint64_t count;
+  uint64_t sum;
+  CompiledTestQueryExecutionPlan()
+      : HandCodedQueryExecutionPlan(),
+        count(0),
+        sum(0) {
   }
 
-  IOTDB_INFO("Test: query result = Processed Block:" << buf->getNumberOfTuples() << " count: " << count
-            << "sum: " << sum )
-  assert(sum == 10);
+  void setDataSource(DataSourcePtr source) {
+    sources.push_back(source);
+  }
+  void setDataSink(DataSinkPtr sink) {
+    sinks.push_back(sink);
+  }
+  bool firstPipelineStage(const TupleBuffer&) {
+    return false;
+  }
 
-  DataSinkPtr sink = this->getSinks()[0];
+  bool executeStage(uint32_t pipeline_stage_id, const TupleBufferPtr buf) {
+    uint64_t* tuples = (uint64_t*) buf->getBuffer();
+
+    IOTDB_INFO("Test: Start execution");
+
+    for (size_t i = 0; i < buf->getNumberOfTuples(); ++i) {
+      count++;
+      sum += tuples[i];
+    }
+
+    IOTDB_INFO(
+        "Test: query result = Processed Block:" << buf->getNumberOfTuples() << " count: " << count << "sum: " << sum)
+    assert(sum == 10);
+
+    DataSinkPtr sink = this->getSinks()[0];
 //  sink->getSchema().getSchemaSize();
-  TupleBufferPtr outputBuffer = BufferManager::instance().getBuffer();
-  u_int32_t* arr = (u_int32_t*) outputBuffer->getBuffer();
-  arr[0] = sum;
-  outputBuffer->setNumberOfTuples(1);
-  outputBuffer->setTupleSizeInBytes(4);
+    TupleBufferPtr outputBuffer = BufferManager::instance().getBuffer();
+    u_int32_t* arr = (u_int32_t*) outputBuffer->getBuffer();
+    arr[0] = sum;
+    outputBuffer->setNumberOfTuples(1);
+    outputBuffer->setTupleSizeInBytes(4);
 //  ysbRecordResult* outputBufferPtr = (ysbRecordResult*)outputBuffer->buffer;
-  sink->writeData(outputBuffer);
-  return true;
-}
+    sink->writeData(outputBuffer);
+    return true;
+  }
 };
 typedef std::shared_ptr<CompiledTestQueryExecutionPlan> CompiledTestQueryExecutionPlanPtr;
 
 /**
  * @brief test for the engine
  * TODO: add more test cases
+ *  - More complex queries
+ *  - concurrent queries
+ *  - long running queries
+ *
  */
 class EngineTest : public testing::Test {
  public:
   static void SetUpTestCase() {
 #ifdef DEBUG_OUTPUT
     setupLogging();
+    remove(filePath.c_str());
 #endif
     IOTDB_INFO("Setup EngineTest test class.");
   }
   static void TearDownTestCase() {
+    remove(filePath.c_str());
     std::cout << "Tear down EngineTest class." << std::endl;
   }
-
 
  protected:
   static void setupLogging() {
@@ -96,15 +110,38 @@ class EngineTest : public testing::Test {
         new log4cxx::ConsoleAppender(layoutPtr));
 
     // set log level
-     logger->setLevel(log4cxx::Level::getDebug());
+    logger->setLevel(log4cxx::Level::getDebug());
 //    logger->setLevel(log4cxx::Level::getInfo());
 
-    // add appenders and other will inherit the settings
+// add appenders and other will inherit the settings
     logger->addAppender(file);
     logger->addAppender(console);
   }
 
 };
+
+void testOutput() {
+  ifstream testFile(filePath.c_str());
+  EXPECT_TRUE(testFile.good());
+  std::ifstream ifs(filePath.c_str());
+  std::string content((std::istreambuf_iterator<char>(ifs)),
+                      (std::istreambuf_iterator<char>()));
+
+  EXPECT_EQ(content, expectedOutput);
+  ifs.close();
+  int response = remove(filePath.c_str());
+  EXPECT_TRUE(response == 0);
+}
+
+CompiledTestQueryExecutionPlanPtr setupQEP() {
+  CompiledTestQueryExecutionPlanPtr qep(new CompiledTestQueryExecutionPlan());
+  DataSourcePtr source = createTestSourceWithoutSchema();
+  Schema sch = Schema::create().addField("sum", BasicType::UINT32);
+  DataSinkPtr sink = createBinaryFileSinkWithSchema(sch, filePath);
+  qep->setDataSource(source);
+  qep->setDataSink(sink);
+  return qep;
+}
 
 TEST_F(EngineTest, start_stop_engine_empty) {
   NodeEngine* ptr = new NodeEngine();
@@ -113,38 +150,32 @@ TEST_F(EngineTest, start_stop_engine_empty) {
 }
 
 TEST_F(EngineTest, deploy_start_stop_test) {
-  CompiledTestQueryExecutionPlanPtr qep(new CompiledTestQueryExecutionPlan());
-  DataSourcePtr source = createTestSourceWithoutSchema();
-  Schema sch = Schema::create().addField("sum", 4);
-//  Schema& sch = Schema::create().addField("sum", BasicType::UINT32);
-  DataSinkPtr sink = createBinaryFileSinkWithSchema(sch, "file.txt");
-  qep->setDataSource(source);
-  qep->setDataSink(sink);
+  CompiledTestQueryExecutionPlanPtr qep = setupQEP();
 
   NodeEngine* ptr = new NodeEngine();
   ptr->deployQuery(qep);
   ptr->start();
-  sleep(100);
-//  ptr->stop();
+  sleep(1);
+  ptr->stop();
+
+  testOutput();
 }
-#ifdef 0
+
 
 TEST_F(EngineTest, start_deploy_stop_test) {
-  CompiledTestQueryExecutionPlanPtr qep(new CompiledTestQueryExecutionPlan());
-  DataSourcePtr source = createTestSourceWithoutSchema();
-  qep->setDataSource(source);
+  CompiledTestQueryExecutionPlanPtr qep = setupQEP();
 
   NodeEngine* ptr = new NodeEngine();
   ptr->start();
   ptr->deployQuery(qep);
   sleep(1);
   ptr->stop();
+
+  testOutput();
 }
 
 TEST_F(EngineTest, start_deploy_undeploy_stop_test) {
-  CompiledTestQueryExecutionPlanPtr qep(new CompiledTestQueryExecutionPlan());
-  DataSourcePtr source = createTestSourceWithoutSchema();
-  qep->setDataSource(source);
+  CompiledTestQueryExecutionPlanPtr qep = setupQEP();
 
   NodeEngine* ptr = new NodeEngine();
   ptr->start();
@@ -152,49 +183,50 @@ TEST_F(EngineTest, start_deploy_undeploy_stop_test) {
   sleep(1);
   ptr->undeployQuery(qep);
   ptr->stop();
+
+  testOutput();
 }
 
 TEST_F(EngineTest, startWithRedeploy_test) {
-  CompiledTestQueryExecutionPlanPtr qep(new CompiledTestQueryExecutionPlan());
-  DataSourcePtr source = createTestSourceWithoutSchema();
-  qep->setDataSource(source);
+  CompiledTestQueryExecutionPlanPtr qep = setupQEP();
 
   NodeEngine* ptr = new NodeEngine();
   ptr->start();
   ptr->deployQuery(qep);
   sleep(1);
   ptr->stop();
+  sleep(1);
   ptr->startWithRedeploy();
+
+  testOutput();
 }
 
 TEST_F(EngineTest, stopWithRedeploy_test) {
-  CompiledTestQueryExecutionPlanPtr qep(new CompiledTestQueryExecutionPlan());
-  DataSourcePtr source = createTestSourceWithoutSchema();
-  qep->setDataSource(source);
+  CompiledTestQueryExecutionPlanPtr qep = setupQEP();
 
   NodeEngine* ptr = new NodeEngine();
   ptr->start();
   ptr->deployQuery(qep);
   sleep(1);
   ptr->stopWithUndeploy();
+
+  testOutput();
 }
 
 TEST_F(EngineTest, resetQEP_test) {
-  CompiledTestQueryExecutionPlanPtr qep(new CompiledTestQueryExecutionPlan());
-  DataSourcePtr source = createTestSourceWithoutSchema();
-  qep->setDataSource(source);
+  CompiledTestQueryExecutionPlanPtr qep = setupQEP();
 
   NodeEngine* ptr = new NodeEngine();
   ptr->start();
   ptr->deployQuery(qep);
   sleep(1);
   ptr->resetQEPs();
+
+  testOutput();
 }
 
 TEST_F(EngineTest, change_dop_with_restart_test) {
-  CompiledTestQueryExecutionPlanPtr qep(new CompiledTestQueryExecutionPlan());
-  DataSourcePtr source = createTestSourceWithoutSchema();
-  qep->setDataSource(source);
+  CompiledTestQueryExecutionPlanPtr qep = setupQEP();
 
   NodeEngine* ptr = new NodeEngine();
   ptr->setDOPWithRestart(2);
@@ -202,12 +234,12 @@ TEST_F(EngineTest, change_dop_with_restart_test) {
   ptr->deployQuery(qep);
   sleep(1);
   ptr->stop();
+
+  testOutput();
 }
 
 TEST_F(EngineTest, change_dop_without_restart_test) {
-  CompiledTestQueryExecutionPlanPtr qep(new CompiledTestQueryExecutionPlan());
-  DataSourcePtr source = createTestSourceWithoutSchema();
-  qep->setDataSource(source);
+  CompiledTestQueryExecutionPlanPtr qep = setupQEP();
 
   NodeEngine* ptr = new NodeEngine();
   ptr->setDOPWithoutRestart(2);
@@ -217,9 +249,7 @@ TEST_F(EngineTest, change_dop_without_restart_test) {
   ptr->stop();
   ptr->start();
   EXPECT_EQ(ptr->getDOP(), size_t(2));
+
+  testOutput();
 }
-
-
-
-#endif
 }
