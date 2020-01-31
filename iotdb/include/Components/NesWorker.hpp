8aff56ff9f5518b79f019ea45326abe380ca4332
zeuchste <steffen.zeuch@dfki.de>
2020-01-31 21:20:41 +0100

    seperate into two files

--- iotdb/include/Components/NesWorker.hpp
+++ iotdb/include/Components/NesWorker.hpp
@@ -12,140 +12,55 @@ namespace NES {
 
 class NesWorker {
  public:
-  NesWorker() {
-    workerCfg.load<io::middleman>();
-    actor_system system { workerCfg };
-    client = system.spawn<NES::WorkerActor>(workerCfg.ip,
-                                            workerCfg.publish_port,
-                                            workerCfg.receive_port);
-    connected = false;
+  /**
+   * @brief default constructor
+   * @note this will create the worker actor using the default worker config
+   */
+  NesWorker();
 
-    connect();
-  }
+  /**
+   * @brief method to connect the worker to the coordinator
+   * @param host ip as string
+   * @param port as uint
+   * @return bool indicating success
+   */
+  bool connect(std::string host, uint16_t port);
 
-  bool connect(std::string host, uint16_t port) {
-    WorkerActorConfig workerCfg;
-    workerCfg.host = host;
-    workerCfg.publish_port = port;
-    workerCfg.load<io::middleman>();
+  /*
+   * @brief connect to coordinator using the default worker config
+   * @return bool indicating success
+   */
+  bool connect();
 
-    actor_system actorSystem { workerCfg };
-    scoped_actor self { actorSystem };
-    bool &con = this->connected;
-    self->request(client, task_timeout, connect_atom::value, workerCfg.host,
-                  workerCfg.publish_port).receive(
-        [&con, &workerCfg](const bool &c) mutable {
-          con = c;
-          NES_DEBUG(
-              "NESWORKER: connected successfully to host " << workerCfg.host << " port=" << workerCfg.publish_port)
-        }
-        ,
-        [=](const error &er) {
-          string error_msg = to_string(er);
-          NES_ERROR("NESWORKER: ERROR while try to connect " << error_msg)
-        });
-    return con;
-  }
+  /**
+   * @brief disconnect from coordinator
+   * @return bool indicating success
+   */
+  bool disconnect();
 
-  bool connect() {
-    WorkerActorConfig workerCfg;
-    workerCfg.load<io::middleman>();
-    actor_system actorSystem { workerCfg };
-    scoped_actor self { actorSystem };
-    bool &con = this->connected;
-    self->request(client, task_timeout, connect_atom::value, workerCfg.host,
-                  workerCfg.publish_port).receive(
-        [&con, &workerCfg](const bool &c) mutable {
-          con = c;
-          NES_DEBUG(
-              "NESWORKER: connected successfully to host " << workerCfg.host << " port=" << workerCfg.publish_port)
-        }
-        ,
-        [=](const error &er) {
-          string error_msg = to_string(er);
-          NES_ERROR("NESWORKER: ERROR while try to connect " << error_msg)
-        });
-    return con;
-  }
+  /**
+   * @brief method to register logical stream with the coordinator
+   * @param name of the logical stream
+   * @param path tot the file containing the schema
+   */
+  bool registerLogicalStream(std::string name, std::string path);
 
-  bool disconnect() {
-    WorkerActorConfig workerCfg;
-    workerCfg.load<io::middleman>();
-    actor_system actorSystem { workerCfg };
-    scoped_actor self { actorSystem };
-    bool &con = this->connected;
-
-    self->request(client, task_timeout, disconnect_atom::value).receive(
-        [&con](const bool &dc) mutable {
-          con = false;
-          NES_DEBUG("NESWORKER: disconnected successfully")
-        }
-        ,
-        [=](const error &er) {
-          string error_msg = to_string(er);
-          NES_ERROR("NESWORKER: ERROR while try to disconnect " << error_msg)
-        });
-    return !con;
-  }
-
-  bool registerLogicalStream(std::string name, std::string path) {
-    WorkerActorConfig workerCfg;
-    workerCfg.load<io::middleman>();
-    actor_system actorSystem { workerCfg };
-    scoped_actor self { actorSystem };
-
-    bool success = false;
-
-    self->request(client, task_timeout, register_log_stream_atom::value, name,
-                  path).receive(
-        [&success](const bool &dc) mutable {
-          NES_DEBUG("NESWORKER: register log stream successful")
-          success = true;
-        }
-        ,
-        [=,&success](const error &er) {
-          string error_msg = to_string(er);
-          NES_ERROR(
-              "NESWORKER: ERROR while try to register stream" << error_msg)
-          success = false;
-        });
-    return success;
-  }
-
+  /**
+   * @brief method to register physical stream with the coordinator
+   * @param sourceType as string
+   * @param sourceConf as string
+   * @param pyhsical stream name as string
+   * @param logical stream name as string
+   */
   bool registerPhysicalStream(std::string sourceType, std::string sourceConf,
                               std::string physicalStreamName,
-                              std::string logicalStreamName) {
-    WorkerActorConfig workerCfg;
-    workerCfg.load<io::middleman>();
-    actor_system actorSystem { workerCfg };
-    scoped_actor self { actorSystem };
-
-    bool success = false;
-
-    self->request(client, task_timeout, register_phy_stream_atom::value,
-                  sourceType, sourceConf, physicalStreamName, logicalStreamName)
-        .receive(
-        [&success](const bool &dc) mutable {
-          NES_DEBUG("NESWORKER: register log stream successful")
-          success = true;
-        }
-        ,
-        [=,&success](const error &er) {
-          string error_msg = to_string(er);
-          NES_ERROR(
-              "NESWORKER: ERROR while try to register stream" << error_msg)
-          success = false;
-        });
-    return success;
-  }
+                              std::string logicalStreamName);
 
  private:
-  static void setupLogging();
   bool connected;
   infer_handle_from_class_t<NES::WorkerActor> client;
   WorkerActorConfig workerCfg;
   PhysicalStreamConfig defaultConf;
-
 };
 typedef std::shared_ptr<NesWorker> NesWorkerPtr;
 
