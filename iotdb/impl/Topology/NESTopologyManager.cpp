38b7bc32362408cfd5825b4f454a14f74db63436
Ankit Chaudhary <my007id@gmail.com>
2020-02-04 15:32:47 +0100

    305 : added link capacity and latency as fixed values

--- iotdb/impl/Topology/NESTopologyManager.cpp
+++ iotdb/impl/Topology/NESTopologyManager.cpp
@@ -3,144 +3,149 @@
 namespace NES {
 
 NESTopologyCoordinatorNodePtr NESTopologyManager::createNESCoordinatorNode(size_t id,
-    const std::string ipAddr, CPUCapacity cpuCapacity) {
-  return currentPlan->createNESCoordinatorNode(id, ipAddr, cpuCapacity);
+                                                                           const std::string ipAddr,
+                                                                           CPUCapacity cpuCapacity) {
+    return currentPlan->createNESCoordinatorNode(id, ipAddr, cpuCapacity);
 }
 
 NESTopologyWorkerNodePtr NESTopologyManager::createNESWorkerNode(
     size_t id, const std::string ipAddr, CPUCapacity cpuCapacity) {
-  return currentPlan->createNESWorkerNode(id, ipAddr, cpuCapacity);
+    return currentPlan->createNESWorkerNode(id, ipAddr, cpuCapacity);
 }
 
 NESTopologySensorNodePtr NESTopologyManager::createNESSensorNode(size_t id, std::string ip,
-    CPUCapacity cpuCapacity) {
-  return currentPlan->createNESSensorNode(id, ip, cpuCapacity);
+                                                                 CPUCapacity cpuCapacity) {
+    return currentPlan->createNESSensorNode(id, ip, cpuCapacity);
 }
 
 bool NESTopologyManager::removeNESWorkerNode(NESTopologyWorkerNodePtr ptr) {
-  return currentPlan->removeNESWorkerNode(std::move(ptr));
+    return currentPlan->removeNESWorkerNode(std::move(ptr));
 }
 
 bool NESTopologyManager::removeNESSensorNode(NESTopologySensorNodePtr ptr) {
-  return currentPlan->removeNESSensorNode(std::move(ptr));
+    return currentPlan->removeNESSensorNode(std::move(ptr));
 }
 
 bool NESTopologyManager::removeNESNode(NESTopologyEntryPtr ptr) {
-  return currentPlan->removeNESNode(std::move(ptr));
+    return currentPlan->removeNESNode(std::move(ptr));
 }
 
-NESTopologyLinkPtr NESTopologyManager::createNESTopologyLink(
-    NESTopologyEntryPtr pSourceNode, NESTopologyEntryPtr pDestNode) {
-  return currentPlan->createNESTopologyLink(pSourceNode, pDestNode);
+NESTopologyLinkPtr NESTopologyManager::createNESTopologyLink(NESTopologyEntryPtr pSourceNode,
+                                                             NESTopologyEntryPtr pDestNode,
+                                                             size_t pLinkCapacity,
+                                                             size_t pLinkLatency) {
+
+    return currentPlan->createNESTopologyLink(pSourceNode, pDestNode, pLinkCapacity, pLinkLatency);
 }
 
 bool NESTopologyManager::removeNESTopologyLink(NESTopologyLinkPtr linkPtr) {
-  return currentPlan->removeNESTopologyLink(linkPtr);
+    return currentPlan->removeNESTopologyLink(linkPtr);
 }
 
 void NESTopologyManager::printNESTopologyPlan() {
-  NES_DEBUG(getNESTopologyPlanString())
+    NES_DEBUG(getNESTopologyPlanString())
 }
 
 std::string NESTopologyManager::getNESTopologyPlanString() {
-  return currentPlan->getTopologyPlanString();
+    return currentPlan->getTopologyPlanString();
 }
 
 NESTopologyPlanPtr NESTopologyManager::getNESTopologyPlan() {
-  return currentPlan;
+    return currentPlan;
 }
 
 NESTopologyEntryPtr NESTopologyManager::getRootNode() {
-  return currentPlan->getRootNode();
-}
-;
+    return currentPlan->getRootNode();
+};
 
 json::value NESTopologyManager::getNESTopologyGraphAsJson() {
 
-    const NESTopologyGraphPtr &nesGraphPtr = getNESTopologyPlan()->getNESTopologyGraph();
-    const std::vector<NESTopologyLinkPtr> &allEdges = nesGraphPtr->getAllEdges();
-    const std::vector<NESVertex> &allVertex = nesGraphPtr->getAllVertex();
+    const NESTopologyGraphPtr& nesGraphPtr = getNESTopologyPlan()->getNESTopologyGraph();
+    const std::vector<NESTopologyLinkPtr>& allEdges = nesGraphPtr->getAllEdges();
+    const std::vector<NESVertex>& allVertex = nesGraphPtr->getAllVertex();
 
-  auto result = json::value::object();
-  std::vector<json::value> edges { };
-  std::vector<json::value> vertices { };
-  for (u_int i = 0; i < allEdges.size(); i++) {
-    const NESTopologyLinkPtr &edge = allEdges[i];
-    const NESTopologyEntryPtr &sourceNode = edge->getSourceNode();
-    const NESTopologyEntryPtr &destNode = edge->getDestNode();
-    auto edgeInfo = json::value::object();
-    const auto source = "Node-" + std::to_string(sourceNode->getId());
-    const auto dest = "Node-" + std::to_string(destNode->getId());
-    edgeInfo["source"] = json::value::string(source);
-    edgeInfo["target"] = json::value::string(dest);
-    edges.push_back(edgeInfo);
-  }
-
-  for (u_int i = 0; i < allVertex.size(); i++) {
-    const NESVertex &vertex = allVertex[i];
-    auto vertexInfo = json::value::object();
-    const std::string id = "Node-" + std::to_string(vertex.ptr->getId());
-    const std::string nodeType = vertex.ptr->getEntryTypeString();
-    int cpuCapacity = vertex.ptr->getCpuCapacity();
-    int remainingCapacity = vertex.ptr->getRemainingCpuCapacity();
-
-    vertexInfo["id"] = json::value::string(id);
-    vertexInfo["title"] = json::value::string(id);
-    vertexInfo["nodeType"] = json::value::string(nodeType);
-    vertexInfo["capacity"] = json::value::string(std::to_string(cpuCapacity));
-    vertexInfo["remainingCapacity"] = json::value::string(
-        std::to_string(remainingCapacity));
-
-    if (nodeType == "Sensor") {
-      NESTopologySensorNodePtr ptr = std::static_pointer_cast<
-          NESTopologySensorNode>(vertex.ptr);
-      //TODO: ankit please check if this ok for you
-      vertexInfo["physicalStreamName"] = json::value::string(
-          ptr->getPhysicalStreamName());
+    auto result = json::value::object();
+    std::vector<json::value> edges{};
+    std::vector<json::value> vertices{};
+    for (u_int i = 0; i < allEdges.size(); i++) {
+        const NESTopologyLinkPtr& edge = allEdges[i];
+        const NESTopologyEntryPtr& sourceNode = edge->getSourceNode();
+        const NESTopologyEntryPtr& destNode = edge->getDestNode();
+        auto edgeInfo = json::value::object();
+        const auto source = "Node-" + std::to_string(sourceNode->getId());
+        const auto dest = "Node-" + std::to_string(destNode->getId());
+        edgeInfo["source"] = json::value::string(source);
+        edgeInfo["target"] = json::value::string(dest);
+        edgeInfo["linkCapacity"] = json::value::string(std::to_string(edge->getLinkCapacity()));
+        edgeInfo["linkLatency"] = json::value::string(std::to_string(edge->getLinkLatency()));
+        edges.push_back(edgeInfo);
     }
 
-    vertices.push_back(vertexInfo);
-  }
+    for (u_int i = 0; i < allVertex.size(); i++) {
+        const NESVertex& vertex = allVertex[i];
+        auto vertexInfo = json::value::object();
+        const std::string id = "Node-" + std::to_string(vertex.ptr->getId());
+        const std::string nodeType = vertex.ptr->getEntryTypeString();
+        int cpuCapacity = vertex.ptr->getCpuCapacity();
+        int remainingCapacity = vertex.ptr->getRemainingCpuCapacity();
 
-  result["nodes"] = json::value::array(vertices);
-  result["edges"] = json::value::array(edges);
-  return result;
+        vertexInfo["id"] = json::value::string(id);
+        vertexInfo["title"] = json::value::string(id);
+        vertexInfo["nodeType"] = json::value::string(nodeType);
+        vertexInfo["capacity"] = json::value::string(std::to_string(cpuCapacity));
+        vertexInfo["remainingCapacity"] = json::value::string(
+            std::to_string(remainingCapacity));
+
+        if (nodeType == "Sensor") {
+            NESTopologySensorNodePtr ptr = std::static_pointer_cast<
+                NESTopologySensorNode>(vertex.ptr);
+            //TODO: ankit please check if this ok for you
+            vertexInfo["physicalStreamName"] = json::value::string(
+                ptr->getPhysicalStreamName());
+        }
+
+        vertices.push_back(vertexInfo);
+    }
+
+    result["nodes"] = json::value::array(vertices);
+    result["edges"] = json::value::array(edges);
+    return result;
 }
 
 std::vector<json::value> NESTopologyManager::getChildrenNode(
     NESTopologyEntryPtr nesParentNode) {
 
-    const NESTopologyGraphPtr &nesGraphPtr = getNESTopologyPlan()->getNESTopologyGraph();
-    const std::vector<NESTopologyLinkPtr> &edgesToNode = nesGraphPtr
+    const NESTopologyGraphPtr& nesGraphPtr = getNESTopologyPlan()->getNESTopologyGraph();
+    const std::vector<NESTopologyLinkPtr>& edgesToNode = nesGraphPtr
         ->getAllEdgesToNode(nesParentNode);
 
-  std::vector<json::value> children = { };
+    std::vector<json::value> children = {};
 
-  if (edgesToNode.empty()) {
-    return {};
-  }
-
-  for (NESTopologyLinkPtr edge : edgesToNode) {
-    const NESTopologyEntryPtr &sourceNode = edge->getSourceNode();
-    if (sourceNode) {
-      auto child = json::value::object();
-      const auto label = std::to_string(sourceNode->getId()) + "-"
-          + sourceNode->getEntryTypeString();
-      child["id"] = json::value::string(label);
-      const std::vector<json::value> &grandChildren = getChildrenNode(
-          sourceNode);
-      if (!grandChildren.empty()) {
-        child["children"] = json::value::array(grandChildren);
-      }
-      children.push_back(child);
+    if (edgesToNode.empty()) {
+        return {};
     }
-  }
-  return children;
+
+    for (NESTopologyLinkPtr edge : edgesToNode) {
+        const NESTopologyEntryPtr& sourceNode = edge->getSourceNode();
+        if (sourceNode) {
+            auto child = json::value::object();
+            const auto label = std::to_string(sourceNode->getId()) + "-"
+                + sourceNode->getEntryTypeString();
+            child["id"] = json::value::string(label);
+            const std::vector<json::value>& grandChildren = getChildrenNode(
+                sourceNode);
+            if (!grandChildren.empty()) {
+                child["children"] = json::value::array(grandChildren);
+            }
+            children.push_back(child);
+        }
+    }
+    return children;
 }
 
 void NESTopologyManager::resetNESTopologyPlan() {
-  currentPlan.reset(new NESTopologyPlan());
-  StreamCatalog::instance().reset();
+    currentPlan.reset(new NESTopologyPlan());
+    StreamCatalog::instance().reset();
 }
 
 }
--- iotdb/include/Topology/NESTopologyLink.hpp
+++ iotdb/include/Topology/NESTopologyLink.hpp
@@ -9,69 +9,106 @@ namespace NES {
 #define NOT_EXISTING_LINK_ID std::numeric_limits<size_t>::max()
 
 enum LinkType {
-  NodeToNode,
-  NodeToSensor,
-  SensorToNode
+    NodeToNode,
+    NodeToSensor,
+    SensorToNode
 };
 
-//FIXME: add docu here
+/**
+ * @brief This class is used for representing the links between two NES topology nodes. The direction is represented by
+ * source and destination nodes.
+ *
+ * The link can be of type: node to node, node to sensor, and sensor to sensor.
+ */
 class NESTopologyLink {
 
- public:
-  explicit NESTopologyLink(size_t pLinkId, NESTopologyEntryPtr pSourceNode,
-                           NESTopologyEntryPtr pDestNode) {
-    linkId = pLinkId;
-    sourceNode = pSourceNode;
-    destNode = pDestNode;
-  }
+  public:
 
-  /**
-   * @brief method to get the id of the node
-   * @return id as a size_t
-   */
-  size_t getId();
+    explicit NESTopologyLink(size_t pLinkId, NESTopologyEntryPtr pSourceNode, NESTopologyEntryPtr pDestNode,
+                             size_t pLinkCapacity, size_t pLinkLatency) {
+        linkId = pLinkId;
+        sourceNode = pSourceNode;
+        destNode = pDestNode;
+        linkCapacity = pLinkCapacity;
+        linkLatency = pLinkLatency;
+    }
 
-  /**
-   * @brief get the source node of a link
-   * @return NESTopologyEntryPtr to the source node
-   */
-  NESTopologyEntryPtr getSourceNode();
+    /**
+     * @brief method to get the id of the node
+     * @return id as a size_t
+     */
+    size_t getId();
 
-  /**
-   * @brief method to get the id of the source of a lin
-   * @return size_t id of the source node
-   */
-  size_t getSourceNodeId();
+    /**
+     * @brief get the source node of a link
+     * @return NESTopologyEntryPtr to the source node
+     */
+    NESTopologyEntryPtr getSourceNode();
 
-  /**
-   * @brief get the destination node of a link
-   * @return NESTopologyEntryPtr to the destination node
-   */
-  NESTopologyEntryPtr getDestNode();
+    /**
+     * @brief method to get the id of the source of a lin
+     * @return size_t id of the source node
+     */
+    size_t getSourceNodeId();
 
-  /**
-   * @brief get the id of the destination node of the link
-   * @return size_t of destination node
-   */
-  size_t getDestNodeId();
+    /**
+     * @brief get the destination node of a link
+     * @return NESTopologyEntryPtr to the destination node
+     */
+    NESTopologyEntryPtr getDestNode();
 
-  /**
-   * @brief get the type of the link
-   * @return LinkType
-   */
-  LinkType getLinkType();
+    /**
+     * @brief get the id of the destination node of the link
+     * @return size_t of destination node
+     */
+    size_t getDestNodeId();
 
-  /**
-   * @brief get the type of the link as a string
-   * @return link type as string
-   */
-  std::string getLinkTypeString();
+    /**
+     * @brief get the type of the link
+     * @return LinkType
+     */
+    LinkType getLinkType();
 
- private:
-  size_t linkId;
+    /**
+     * @brief get the type of the link as a string
+     * @return link type as string
+     */
+    std::string getLinkTypeString();
 
-  NESTopologyEntryPtr sourceNode;
-  NESTopologyEntryPtr destNode;
+    size_t getLinkLatency() const;
+
+    size_t getLinkCapacity() const;
+
+    void updateLinkLatency(size_t linkLatency);
+
+    void updateLinkCapacity(size_t linkCapacity);
+
+  private:
+
+    /**
+     * @brief unique identifier of the link.
+     */
+    size_t linkId;
+
+    /**
+     * @brief source node pointer
+     */
+    NESTopologyEntryPtr sourceNode;
+
+    /**
+     * @brief destination node pointer
+     */
+    NESTopologyEntryPtr destNode;
+
+    /**
+     * @brief link latency (lower is better)
+     */
+    size_t linkLatency;
+
+    /**
+     * @brief link capacity (higher is better)
+     */
+    size_t linkCapacity;
 };
 
 typedef std::shared_ptr<NESTopologyLink> NESTopologyLinkPtr;
