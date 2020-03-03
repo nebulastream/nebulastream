c0d73ee7b2a2ed83a72c4059e029ec4e41a710c7
Philipp Grulich <philippgrulich@hotmail.de>
2020-03-03 13:54:03 +0100

    #234 add comment to QueryExecutionPlan.cpp

--- iotdb/impl/QueryCompiler/QueryExecutionPlan.cpp
+++ iotdb/impl/QueryCompiler/QueryExecutionPlan.cpp
@@ -10,78 +10,78 @@ QueryExecutionPlan::QueryExecutionPlan() : sources(), stages() {
 
 QueryExecutionPlan::QueryExecutionPlan(std::vector<DataSourcePtr> sources,
                                        std::vector<PipelineStagePtr> stages,
-                                       std::map<DataSource *, uint32_t> sourceToStage,
+                                       std::map<DataSource*, uint32_t> sourceToStage,
                                        std::map<uint32_t, uint32_t> stageToDest)
-    : sources(std::move(sources)), stages(std::move(stages)), sourceToStage(std::move(sourceToStage)), stageToDest(std::move(stageToDest)) {
+    : sources(std::move(sources)),
+      stages(std::move(stages)),
+      sourceToStage(std::move(sourceToStage)),
+      stageToDest(std::move(stageToDest)) {
 }
 
 QueryExecutionPlan::~QueryExecutionPlan() {
-  NES_DEBUG("destroy qep")
-  sources.clear();
-  stages.clear();
-  sourceToStage.clear();
-  stageToDest.clear();
+    NES_DEBUG("destroy qep")
+    sources.clear();
+    stages.clear();
+    sourceToStage.clear();
+    stageToDest.clear();
 }
 
 void QueryExecutionPlan::stop() {
-  for (auto &stage: stages) {
-    stage->stop();
-  }
+    NES_DEBUG("QueryExecutionPlan: stop");
+    for (auto& stage: stages) {
+        stage->stop();
+    }
 }
 
 void QueryExecutionPlan::setup() {
-  for (auto &stage: stages) {
-    stage->setup();
-  }
+    NES_DEBUG("QueryExecutionPlan: setup");
+    for (auto& stage: stages) {
+        stage->setup();
+    }
 }
 
 void QueryExecutionPlan::start() {
-  for (auto &stage: stages) {
-    stage->start();
-  }
+    NES_DEBUG("QueryExecutionPlan: start");
+    for (auto& stage: stages) {
+        stage->start();
+    }
 }
 
 void QueryExecutionPlan::addDataSource(DataSourcePtr source) {
-  sources.push_back(source);
+    sources.push_back(source);
 }
 
 const std::vector<DataSourcePtr> QueryExecutionPlan::getSources() const { return sources; }
 
 void QueryExecutionPlan::addDataSink(DataSinkPtr sink) {
-  sinks.push_back(sink);
+    sinks.push_back(sink);
 }
 
 const std::vector<DataSinkPtr> QueryExecutionPlan::getSinks() const { return sinks; }
 
-
-
-
-
 void QueryExecutionPlan::addPipelineStage(PipelineStagePtr pipelineStage) {
-  stages.push_back(pipelineStage);
+    stages.push_back(pipelineStage);
 }
 
 void QueryExecutionPlan::print() {
-  for (auto source : sources) {
-    NES_INFO("Source:" << source)
-    NES_INFO(
-        "\t Generated Buffers=" << source->getNumberOfGeneratedBuffers())
-    NES_INFO("\t Generated Tuples=" << source->getNumberOfGeneratedTuples())
-    NES_INFO("\t Schema=" << source->getSourceSchemaAsString())
-  }
-  for (auto sink : sinks) {
-    NES_INFO("Sink:" << sink)
-    NES_INFO("\t Generated Buffers=" << sink->getNumberOfSentBuffers())
-    NES_INFO("\t Generated Tuples=" << sink->getNumberOfSentTuples())
-  }
+    for (auto source : sources) {
+        NES_INFO("Source:" << source)
+        NES_INFO(
+            "\t Generated Buffers=" << source->getNumberOfGeneratedBuffers())
+        NES_INFO("\t Generated Tuples=" << source->getNumberOfGeneratedTuples())
+        NES_INFO("\t Schema=" << source->getSourceSchemaAsString())
+    }
+    for (auto sink : sinks) {
+        NES_INFO("Sink:" << sink)
+        NES_INFO("\t Generated Buffers=" << sink->getNumberOfSentBuffers())
+        NES_INFO("\t Generated Tuples=" << sink->getNumberOfSentTuples())
+    }
 }
 
 bool QueryExecutionPlan::executeStage(uint32_t pipeline_stage_id, const NES::TupleBufferPtr buf) {
-  return false;
+    return false;
 }
 
-uint32_t QueryExecutionPlan::stageIdFromSource(DataSource *source) { return sourceToStage[source]; };
-
-
+uint32_t QueryExecutionPlan::stageIdFromSource(DataSource* source) { return sourceToStage[source]; };
 
 } // namespace NES
