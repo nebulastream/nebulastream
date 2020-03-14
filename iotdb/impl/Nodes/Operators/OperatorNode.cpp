ddb746d4f18d4482a826cb14ba60f1f9b6dcf674
Philipp Grulich <philippgrulich@hotmail.de>
2020-03-14 07:19:34 +0100

    #433 rename to parents and children

--- iotdb/impl/Nodes/Operators/OperatorNode.cpp
+++ iotdb/impl/Nodes/Operators/OperatorNode.cpp
@@ -4,7 +4,7 @@ OperatorNode::OperatorNode() {}
 
 SchemaPtr OperatorNode::getResultSchema() const {
     // todo we assume that all previous operators have the same output schema otherwise this plan is not valid
-    return this->predecessors[0]->as<OperatorNode>()->getResultSchema();
+    return this->parents[0]->as<OperatorNode>()->getResultSchema();
 }
 
 }
