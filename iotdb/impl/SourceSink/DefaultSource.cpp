324a9695c643724fd4fcf87c82260cd53a603bc1
Philipp Grulich <philippgrulich@hotmail.de>
2020-03-22 09:47:32 +0100

    #397 generate correct data for data source.

--- iotdb/impl/SourceSink/DefaultSource.cpp
+++ iotdb/impl/SourceSink/DefaultSource.cpp
@@ -5,31 +5,55 @@
 
 namespace NES {
 
-DefaultSource::DefaultSource(const Schema &schema,
+DefaultSource::DefaultSource(const Schema& schema,
                              const uint64_t numbersOfBufferToProduce,
                              size_t frequency)
     :
     GeneratorSource(schema, numbersOfBufferToProduce) {
-  this->gatheringInterval = frequency;
+    this->gatheringInterval = frequency;
 }
 
 TupleBufferPtr DefaultSource::receiveData() {
-  // 10 tuples of size one
-  TupleBufferPtr buf = BufferManager::instance().getBuffer();
-  size_t tupleCnt = 10;
-  auto layout = createRowLayout(std::make_shared<Schema>(schema));
+    // 10 tuples of size one
+    TupleBufferPtr buf = BufferManager::instance().getBuffer();
+    size_t tupleCnt = 10;
+    auto layout = createRowLayout(std::make_shared<Schema>(schema));
 
-  assert(buf->getBuffer() != nullptr);
+    assert(buf->getBuffer() != nullptr);
+    auto fields = schema.fields;
+    for (uint64_t recordIndex = 0; recordIndex < tupleCnt; recordIndex++) {
+        for (uint64_t fieldIndex = 0; fieldIndex < fields.size();
+             fieldIndex++) {
+            auto dataType = fields[fieldIndex]->getDataType();
+            if (dataType->isEqual(createDataType(UINT8))) {
+                layout->getValueField<uint8_t>(recordIndex, fieldIndex)->write(buf, 1);
+            } else if (dataType->isEqual(createDataType(UINT16))) {
+                layout->getValueField<uint16_t>(recordIndex, fieldIndex)->write(buf, 1);
+            } else if (dataType->isEqual(createDataType(UINT32))) {
+                layout->getValueField<uint32_t>(recordIndex, fieldIndex)->write(buf, 1);
+            } else if (dataType->isEqual(createDataType(UINT64))) {
+                layout->getValueField<uint64_t>(recordIndex, fieldIndex)->write(buf, 1);
+            } else if (dataType->isEqual(createDataType(INT8))) {
+                layout->getValueField<int8_t>(recordIndex, fieldIndex)->write(buf, 1);
+            } else if (dataType->isEqual(createDataType(INT16))) {
+                layout->getValueField<int16_t>(recordIndex, fieldIndex)->write(buf, 1);
+            } else if (dataType->isEqual(createDataType(INT32))) {
+                layout->getValueField<int32_t>(recordIndex, fieldIndex)->write(buf, 1);
+            } else if (dataType->isEqual(createDataType(INT64))) {
+                layout->getValueField<int64_t>(recordIndex, fieldIndex)->write(buf, 1);
+            } else if (dataType->isEqual(createDataType(FLOAT32))) {
+                layout->getValueField<float>(recordIndex, fieldIndex)->write(buf, 1);
+            } else if (dataType->isEqual(createDataType(FLOAT64))) {
+                layout->getValueField<double>(recordIndex, fieldIndex)->write(buf, 1);
+            }else{
+                NES_DEBUG("This data source only generates data for numeric fields")
+            }
 
-  for (uint64_t recordIndex = 0; recordIndex < tupleCnt; recordIndex++) {
-    for (uint64_t fieldIndex = 0; fieldIndex < this->schema.getSize();
-        fieldIndex++) {
-        layout->getValueField<uint64_t>(recordIndex, fieldIndex)->write(buf, 1);
+        }
     }
-  }
-  buf->setTupleSizeInBytes(sizeof(uint64_t));
-  buf->setNumberOfTuples(tupleCnt);
-  return buf;
+    buf->setTupleSizeInBytes(schema.getSchemaSize());
+    buf->setNumberOfTuples(tupleCnt);
+    return buf;
 }
 }
 BOOST_CLASS_EXPORT(NES::DefaultSource);
