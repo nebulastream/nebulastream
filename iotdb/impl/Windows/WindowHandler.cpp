6e49157c36d183214cc0113a5a44ce0cdbde3870
Ventura Del Monte <venturadelmonte@gmail.com>
2020-03-27 12:05:08 +0100

    Add missing packages

--- iotdb/impl/Windows/WindowHandler.cpp
+++ iotdb/impl/Windows/WindowHandler.cpp
@@ -25,62 +25,6 @@ bool WindowHandler::setup(QueryExecutionPlanPtr queryExecutionPlan, uint32_t pip
     return true;
 }
 
-template<class FinalAggregateType, class PartialAggregateType>
-void WindowHandler::aggregateWindows(
-    WindowSliceStore<PartialAggregateType>* store,
-    WindowDefinitionPtr windowDefinition,
-    TupleBufferPtr tupleBuffer) {
-
-    // For event time we use the maximal records ts as watermark.
-    // For processing time we use the current wall clock as watermark.
-    // TODO we should add a allowed lateness to support out of order events
-    auto windowTimeType = windowDefinition->windowType->getTimeCharacteristic();
-    auto watermark = windowTimeType == TimeCharacteristic::ProcessingTime ? getTsFromClock() : store->getMaxTs();
-
-    // create result vector of windows
-    auto windows = std::make_shared<std::vector<WindowState>>();
-    // the window type addes result windows to the windows vectors
-    windowDefinition->windowType->triggerWindows(windows, store->getLastWatermark(), watermark);
-    // allocate partial final aggregates for each window
-    auto partialFinalAggregates = std::vector<PartialAggregateType>(windows->size());
-    // iterate over all slices and update the partial final aggregates
-    auto slices = store->getSliceMetadata();
-    auto partialAggregates = store->getPartialAggregates();
-    NES_DEBUG("WindowHandler: trigger " << windows->size() << " windows, on " << slices.size() << " slices");
-    for (uint64_t sliceId = 0; sliceId < slices.size(); sliceId++) {
-        for (uint64_t windowId = 0; windowId < windows->size(); windowId++) {
-            auto window = (*windows)[windowId];
-            // A slice is contained in a window if the window starts before the slice and ends after the slice
-            if (window.getStartTs() <= slices[sliceId].getStartTs() &&
-                window.getEndTs() >= slices[sliceId].getEndTs()) {
-                //TODO Because of this condition we currently only support SUM aggregations
-                if (Sum* sumAggregation = dynamic_cast<Sum*>(windowDefinition->windowAggregation.get())) {
-                    if (partialFinalAggregates.size() <= windowId) {
-                        // initial the partial aggregate
-                        partialFinalAggregates[windowId] = partialAggregates[sliceId];
-                    } else {
-                        // update the partial aggregate
-                        partialFinalAggregates[windowId] =
-                            sumAggregation->combine<PartialAggregateType>(partialFinalAggregates[windowId],
-                                                                          partialAggregates[sliceId]);
-                    }
-                }
-            }
-        }
-    }
-    // calculate the final aggregate
-    auto intBuffer = static_cast<FinalAggregateType*> (tupleBuffer->getBuffer());
-    for (uint64_t i = 0; i < partialFinalAggregates.size(); i++) {
-        //TODO Because of this condition we currently only support SUM aggregations
-        if (Sum* sumAggregation = dynamic_cast<Sum*>(windowDefinition->windowAggregation.get())) {
-            intBuffer[tupleBuffer->getNumberOfTuples()] =
-                sumAggregation->lower<FinalAggregateType, PartialAggregateType>(partialFinalAggregates[i]);
-        }
-        tupleBuffer->setNumberOfTuples(tupleBuffer->getNumberOfTuples() + 1);
-    }
-    store->setLastWatermark(watermark);
-};
-
 void WindowHandler::trigger() {
     while(running) {
         // we currently assume processing time and only want to check for new window results every 1 second
