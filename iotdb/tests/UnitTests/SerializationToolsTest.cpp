c8016e42a0589e0a5a148eda4811b89a4f03438c
Johannes Russ <Johannes.Russ@dfki.de>
2020-04-07 16:10:05 +0200

    Removed last unneeded test, first working version after refactoring with SchemaPtr, missing rebase with master

--- iotdb/tests/UnitTests/SerializationToolsTest.cpp
+++ iotdb/tests/UnitTests/SerializationToolsTest.cpp
@@ -41,9 +41,6 @@ class SerializationToolsTest : public testing::Test {
 
 /* Test serialization for Schema  */
 TEST_F(SerializationToolsTest, serialize_deserialize_schema) {
-  schema = Schema::create()->addField("id", BasicType::UINT32)->addField(
-      "value", BasicType::UINT64);
-  stream = std::make_shared<Stream>("default", schema);
   string sschema = SerializationTools::ser_schema(schema);
   SchemaPtr dschema = SerializationTools::parse_schema(sschema);
   EXPECT_TRUE(dschema->equals(schema));
