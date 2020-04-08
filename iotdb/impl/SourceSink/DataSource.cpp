84f2a95141af9a68d76db3e11244bbfb24a3d0eb
Johannes Russ <Johannes.Russ@dfki.de>
2020-04-08 15:22:09 +0200

    include minor fixes of the last rebase after #359

--- iotdb/impl/SourceSink/DataSource.cpp
+++ iotdb/impl/SourceSink/DataSource.cpp
@@ -16,11 +16,11 @@ BOOST_CLASS_EXPORT_IMPLEMENT(NES::DataSource);
 
 namespace NES {
 
-DataSource::DataSource(SchemaPtr pSschema)
+DataSource::DataSource(SchemaPtr pSchema)
     :
     running(false),
     thread(),
-    schema(pSschema),
+    schema(pSchema),
     generatedTuples(0),
     generatedBuffers(0),
     numBuffersToProcess(UINT64_MAX),
