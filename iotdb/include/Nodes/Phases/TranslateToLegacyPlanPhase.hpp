2bf4e889d586cc59f6ae00dfee34f263f51b83eb
Philipp Grulich <philippgrulich@hotmail.de>
2020-04-09 21:59:19 +0200

    #507 add documentation

--- iotdb/include/Nodes/Phases/TranslateToLegacyPlanPhase.hpp
+++ iotdb/include/Nodes/Phases/TranslateToLegacyPlanPhase.hpp
@@ -2,7 +2,6 @@
 #define NES_INCLUDE_NODES_PHASES_TRANSLATETOLEGECYPLAN_HPP_
 
 #include <memory>
-#include <QueryCompiler/QueryCompiler.hpp>
 
 namespace NES {
 
@@ -21,13 +20,33 @@ typedef std::shared_ptr<UserAPIExpression> UserAPIExpressionPtr;
 class Operator;
 typedef std::shared_ptr<Operator> OperatorPtr;
 
-
+/**
+ * @brief Translates a logical query plan to the legacy operator tree
+ */
 class TranslateToLegacyPlanPhase {
   public:
+    /**
+     * @brief Factory method to create a translator phase.
+     */
     static TranslateToLegacyPlanPhasePtr create();
+    /**
+     * @brief Translates a operator node and all its children to the legacy representation.
+     * @param operatorNode
+     * @return Legacy Operator Tree
+     */
     OperatorPtr transform(OperatorNodePtr operatorNode);
   private:
+    /**
+     * @brief Translates an individual operator to its legacy representation.
+     * @param operatorNode
+     * @return Legacy Operator
+     */
     OperatorPtr transformIndividualOperator(OperatorNodePtr operatorNode);
+    /**
+     * @brief Translates an expression to a legacy user api expression.
+     * @param expression node
+     * @return UserAPIExpressionPtr
+     */
     UserAPIExpressionPtr transformExpression(ExpressionNodePtr node);
 };
 
