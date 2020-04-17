c235d9d01fe015d96836ce0420fd74ae8b826bc7
Philipp Grulich <philippgrulich@hotmail.de>
2020-04-17 09:24:15 +0200

    #487 add translation for map operator

--- iotdb/impl/Nodes/Operators/LogicalOperators/MapLogicalOperatorNode.cpp
+++ iotdb/impl/Nodes/Operators/LogicalOperators/MapLogicalOperatorNode.cpp
@@ -1,14 +1,13 @@
 #include <Nodes/Operators/LogicalOperators/MapLogicalOperatorNode.hpp>
-#include <API/UserAPIExpression.hpp>
+#include <Nodes/Expressions/FieldAssignmentExpressionNode.hpp>
 #include <API/Types/AttributeField.hpp>
 
-
 namespace NES {
-MapLogicalOperatorNode::MapLogicalOperatorNode(const ExpressionNodePtr mapExpression) : mapExpression(mapExpression) {
-}
+
+MapLogicalOperatorNode::MapLogicalOperatorNode(const FieldAssignmentExpressionNodePtr mapExpression) : mapExpression(mapExpression) {}
 
 bool MapLogicalOperatorNode::equal(const NodePtr rhs) const {
-    if(this->isIdentical(rhs)) {
+    if (this->isIdentical(rhs)) {
         return true;
     }
     if (rhs->instanceOf<MapLogicalOperatorNode>()) {
@@ -24,7 +23,11 @@ const std::string MapLogicalOperatorNode::toString() const {
     return ss.str();
 }
 
-LogicalOperatorNodePtr createMapLogicalOperatorNode(const ExpressionNodePtr mapExpression) {
+FieldAssignmentExpressionNodePtr MapLogicalOperatorNode::getMapExpression() {
+    return mapExpression;
+}
+
+LogicalOperatorNodePtr createMapLogicalOperatorNode(const FieldAssignmentExpressionNodePtr mapExpression) {
     return std::make_shared<MapLogicalOperatorNode>(mapExpression);
 }
 }
--- iotdb/impl/Nodes/Phases/TranslateToLegacyPlanPhase.cpp
+++ iotdb/impl/Nodes/Phases/TranslateToLegacyPlanPhase.cpp
@@ -3,14 +3,25 @@
 #include <Nodes/Node.hpp>
 #include <Nodes/Phases/TranslateToLegacyPlanPhase.hpp>
 
-#include <Nodes/Expressions/LogicalExpressions/AndExpressionNode.hpp>
-#include <Nodes/Expressions/LogicalExpressions/EqualsExpressionNode.hpp>
-#include <Nodes/Expressions/LogicalExpressions/LessEqualsExpressionNode.hpp>
-#include <Nodes/Expressions/LogicalExpressions/NegateExpressionNode.hpp>
 #include <Nodes/Operators/LogicalOperators/FilterLogicalOperatorNode.hpp>
+#include <Nodes/Operators/LogicalOperators/MapLogicalOperatorNode.hpp>
 #include <Nodes/Operators/LogicalOperators/SinkLogicalOperatorNode.hpp>
 #include <Nodes/Operators/LogicalOperators/SourceLogicalOperatorNode.hpp>
 #include <Operators/Operator.hpp>
+#include <Nodes/Expressions/FieldAssignmentExpressionNode.hpp>
+#include <Nodes/Expressions/LogicalExpressions/AndExpressionNode.hpp>
+#include <Nodes/Expressions/LogicalExpressions/EqualsExpressionNode.hpp>
+#include <Nodes/Expressions/LogicalExpressions/LessEqualsExpressionNode.hpp>
+#include <Nodes/Expressions/LogicalExpressions/OrExpressionNode.hpp>
+#include <Nodes/Expressions/LogicalExpressions/LessExpressionNode.hpp>
+#include <Nodes/Expressions/LogicalExpressions/GreaterExpressionNode.hpp>
+#include <Nodes/Expressions/LogicalExpressions/GreaterEqualsExpressionNode.hpp>
+#include <Nodes/Expressions/ArithmeticalExpressions/ArithmeticalExpressionNode.hpp>
+#include <Nodes/Expressions/ArithmeticalExpressions/AddExpressionNode.hpp>
+#include <Nodes/Expressions/ArithmeticalExpressions/SubExpressionNode.hpp>
+#include <Nodes/Expressions/ArithmeticalExpressions/MulExpressionNode.hpp>
+#include <Nodes/Expressions/ArithmeticalExpressions/DivExpressionNode.hpp>
+
 namespace NES {
 
 TranslateToLegacyPlanPhasePtr TranslateToLegacyPlanPhase::create() {
@@ -29,9 +40,24 @@ OperatorPtr TranslateToLegacyPlanPhase::transformIndividualOperator(OperatorNode
         // we can assume that the legacy expression is always a predicate.
         auto legacyPredicate = std::dynamic_pointer_cast<Predicate>(legacyExpression);
         if (legacyPredicate == nullptr) {
-            NES_FATAL_ERROR("TranslateToLegacyPhase: Error during translating expression");
+            NES_FATAL_ERROR("TranslateToLegacyPhase: Error during translating filter expression");
         }
         return createFilterOperator(legacyPredicate);
+    } else if (operatorNode->instanceOf<MapLogicalOperatorNode>()) {
+        // Translate map operator node.
+        auto mapOperatorNode = operatorNode->as<MapLogicalOperatorNode>();
+        auto mapExpression = mapOperatorNode->getMapExpression();
+        // Translate to the legacy representation
+        auto legacyFieldAccess = transformExpression(mapExpression->getField());
+        auto legacyAssignment = transformExpression(mapExpression->getAssignment());
+        // Cast to the proper type
+        auto legacyPredicate = std::dynamic_pointer_cast<Predicate>(legacyAssignment);
+        auto legacyField = std::dynamic_pointer_cast<Field>(legacyAssignment);
+        if (legacyPredicate == nullptr || legacyField == nullptr) {
+            NES_FATAL_ERROR("TranslateToLegacyPhase: Error during translating map expression");
+        }
+        // Create legacy map operator
+        return createMapOperator(legacyField->getAttributeField(), legacyPredicate);
 
     } else if (operatorNode->instanceOf<SinkLogicalOperatorNode>()) {
         // Translate sink operator node.
@@ -60,7 +86,13 @@ OperatorPtr TranslateToLegacyPlanPhase::transform(OperatorNodePtr operatorNode) 
  * To this end we first cast the expression node in the right subtype and then translate it.
  */
 UserAPIExpressionPtr TranslateToLegacyPlanPhase::transformExpression(ExpressionNodePtr expression) {
-    if (expression->instanceOf<ConstantValueExpressionNode>()) {
+    if (expression->instanceOf<LogicalExpressionNode>()) {
+        // Translate logical expressions to the legacy representation
+        return transformLogicalExpressions(expression);
+    } else if (expression->instanceOf<ArithmeticalExpressionNode>()) {
+        // Translate arithmetical expressions to the legacy representation
+        return transformArithmeticalExpressions(expression);
+    } else if (expression->instanceOf<ConstantValueExpressionNode>()) {
         // Translate constant value expression node.
         auto constantValueExpression = expression->as<ConstantValueExpressionNode>();
         auto value = constantValueExpression->getConstantValue();
@@ -71,26 +103,91 @@ UserAPIExpressionPtr TranslateToLegacyPlanPhase::transformExpression(ExpressionN
         auto fieldName = fieldReadExpression->getFieldName();
         auto value = fieldReadExpression->getStamp();
         return Field(AttributeField(fieldName, value).copy()).copy();
-    } else if (expression->instanceOf<AndExpressionNode>()) {
+    }
+    NES_FATAL_ERROR(
+        "TranslateToLegacyPhase: No transformation implemented for this expression node: " << expression->toString());
+    NES_NOT_IMPLEMENTED;
+}
+
+UserAPIExpressionPtr TranslateToLegacyPlanPhase::transformArithmeticalExpressions(ExpressionNodePtr expression) {
+    if (expression->instanceOf<AddExpressionNode>()) {
+        // Translate add expression node.
+        auto addExpressionNode = expression->as<AddExpressionNode>();
+        auto legacyLeft = transformExpression(addExpressionNode->getLeft());
+        auto legacyRight = transformExpression(addExpressionNode->getRight());
+        return Predicate(BinaryOperatorType::PLUS_OP, legacyLeft, legacyRight).copy();
+    } else if (expression->instanceOf<SubExpressionNode>()) {
+        // Translate sub expression node.
+        auto subExpressionNode = expression->as<SubExpressionNode>();
+        auto legacyLeft = transformExpression(subExpressionNode->getLeft());
+        auto legacyRight = transformExpression(subExpressionNode->getRight());
+        return Predicate(BinaryOperatorType::MINUS_OP, legacyLeft, legacyRight).copy();
+    } else if (expression->instanceOf<MulExpressionNode>()) {
+        // Translate mul expression node.
+        auto mulExpressionNode = expression->as<MulExpressionNode>();
+        auto legacyLeft = transformExpression(mulExpressionNode->getLeft());
+        auto legacyRight = transformExpression(mulExpressionNode->getRight());
+        return Predicate(BinaryOperatorType::MULTIPLY_OP, legacyLeft, legacyRight).copy();
+    } else if (expression->instanceOf<DivExpressionNode>()) {
+        // Translate div expression node.
+        auto divExpressionNode = expression->as<DivExpressionNode>();
+        auto legacyLeft = transformExpression(divExpressionNode->getLeft());
+        auto legacyRight = transformExpression(divExpressionNode->getRight());
+        return Predicate(BinaryOperatorType::DIVISION_OP, legacyLeft, legacyRight).copy();
+    }
+    NES_FATAL_ERROR(
+        "TranslateToLegacyPhase: No transformation implemented for this arithmetical expression node: "
+            << expression->toString());
+    NES_NOT_IMPLEMENTED;
+}
+
+UserAPIExpressionPtr TranslateToLegacyPlanPhase::transformLogicalExpressions(ExpressionNodePtr expression) {
+    if (expression->instanceOf<AndExpressionNode>()) {
         // Translate and expression node.
         auto andExpressionNode = expression->as<AndExpressionNode>();
         auto legacyLeft = transformExpression(andExpressionNode->getLeft());
         auto legacyRight = transformExpression(andExpressionNode->getRight());
         return Predicate(BinaryOperatorType::LOGICAL_AND_OP, legacyLeft, legacyRight).copy();
+    } else if (expression->instanceOf<OrExpressionNode>()) {
+        // Translate or expression node.
+        auto orExpressionNode = expression->as<OrExpressionNode>();
+        auto legacyLeft = transformExpression(orExpressionNode->getLeft());
+        auto legacyRight = transformExpression(orExpressionNode->getRight());
+        return Predicate(BinaryOperatorType::LOGICAL_OR_OP, legacyLeft, legacyRight).copy();
+    } else if (expression->instanceOf<LessExpressionNode>()) {
+        // Translate less expression node.
+        auto lessExpressionNode = expression->as<LessExpressionNode>();
+        auto legacyLeft = transformExpression(lessExpressionNode->getLeft());
+        auto legacyRight = transformExpression(lessExpressionNode->getRight());
+        return Predicate(BinaryOperatorType::LESS_THEN_OP, legacyLeft, legacyRight).copy();
     } else if (expression->instanceOf<LessEqualsExpressionNode>()) {
-        // Translate less then expression node.
+        // Translate less equals expression node.
         auto andExpressionNode = expression->as<LessEqualsExpressionNode>();
         auto legacyLeft = transformExpression(andExpressionNode->getLeft());
         auto legacyRight = transformExpression(andExpressionNode->getRight());
-        return Predicate(BinaryOperatorType::LESS_THEN_OP, legacyLeft, legacyRight).copy();
+        return Predicate(BinaryOperatorType::LESS_THEN_EQUAL_OP, legacyLeft, legacyRight).copy();
+    } else if (expression->instanceOf<GreaterExpressionNode>()) {
+        // Translate greater expression node.
+        auto greaterExpressionNode = expression->as<GreaterExpressionNode>();
+        auto legacyLeft = transformExpression(greaterExpressionNode->getLeft());
+        auto legacyRight = transformExpression(greaterExpressionNode->getRight());
+        return Predicate(BinaryOperatorType::GREATER_THEN_OP, legacyLeft, legacyRight).copy();
+    } else if (expression->instanceOf<GreaterEqualsExpressionNode>()) {
+        // Translate greater equals expression node.
+        auto greaterEqualsExpressionNode = expression->as<GreaterEqualsExpressionNode>();
+        auto legacyLeft = transformExpression(greaterEqualsExpressionNode->getLeft());
+        auto legacyRight = transformExpression(greaterEqualsExpressionNode->getRight());
+        return Predicate(BinaryOperatorType::GREATER_THEN_EQUAL_OP, legacyLeft, legacyRight).copy();
     } else if (expression->instanceOf<EqualsExpressionNode>()) {
         // Translate equals expression node.
-        auto andExpressionNode = expression->as<EqualsExpressionNode>();
-        auto legacyLeft = transformExpression(andExpressionNode->getLeft());
-        auto legacyRight = transformExpression(andExpressionNode->getRight());
+        auto equalsExpressionNode = expression->as<EqualsExpressionNode>();
+        auto legacyLeft = transformExpression(equalsExpressionNode->getLeft());
+        auto legacyRight = transformExpression(equalsExpressionNode->getRight());
         return Predicate(BinaryOperatorType::EQUAL_OP, legacyLeft, legacyRight).copy();
     }
-    NES_FATAL_ERROR("TranslateToLegacyPhase: No transformation implemented for this expression node: " << expression);
+    NES_FATAL_ERROR(
+        "TranslateToLegacyPhase: No transformation implemented for this logical expression node: "
+            << expression->toString());
     NES_NOT_IMPLEMENTED;
 }
 
