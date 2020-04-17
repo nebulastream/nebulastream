117d7e244fd90efa7873595320ffaed5423761ef
Ventura Del Monte <venturadelmonte@gmail.com>
2020-04-17 16:10:01 +0200

    Initialize BufferManager in ZMQ tests

--- iotdb/tests/UnitTests/ZMQTest.cpp
+++ iotdb/tests/UnitTests/ZMQTest.cpp
@@ -13,6 +13,7 @@
 #include <NodeEngine/TupleBuffer.hpp>
 #include <SourceSink/SinkCreator.hpp>
 #include <SourceSink/SourceCreator.hpp>
+
 using namespace NES;
 
 #ifndef LOCAL_HOST
@@ -24,102 +25,103 @@ using namespace NES;
 #endif
 
 class ZMQTest : public testing::Test {
- public:
-  /* Will be called before any test in this class are executed. */
-  static void SetUpTestCase() {
-    std::cout << "Setup ZMQTest test class." << std::endl;
-  }
+public:
+    /* Will be called before any test in this class are executed. */
+    static void SetUpTestCase() {
+        NES_DEBUG("Setup ZMQTest test class.");
+        BufferManager::instance().configure(1024, 1024);
+    }
 
-  /* Will be called before a test is executed. */
-  void SetUp() {
-    std::cout << "Setup ZMQTest test case." << std::endl;
-    NES::setupLogging("ZMQTest.log", NES::LOG_DEBUG);
+    /* Will be called before a test is executed. */
+    void SetUp() {
+        NES_DEBUG("Setup ZMQTest test case.");
+        NES::setupLogging("ZMQTest.log", NES::LOG_DEBUG);
 
-    address = std::string("tcp://") + std::string(LOCAL_HOST) + std::string(":")
-        + std::to_string(LOCAL_PORT);
+        address = std::string("tcp://") + std::string(LOCAL_HOST) + std::string(":")
+                  + std::to_string(LOCAL_PORT);
 
-    test_data = { { 0, 100, 1, 99, 2, 98, 3, 97 } };
-    test_data_size = test_data.size() * sizeof(uint32_t);
-    tupleCnt = 8;
-    //    test_data_size = 4096;
-    test_schema = Schema::create()->addField("KEY", UINT32)->addField("VALUE",
-                                                                    UINT32);
-    BufferManager::instance();
-    //    BufferManager::instance().setBufferSize(test_data_size);
-  }
+        test_data = {{0, 100, 1, 99, 2, 98, 3, 97}};
+        test_data_size = test_data.size() * sizeof(uint32_t);
+        tupleCnt = 8;
+        //    test_data_size = 4096;
+        test_schema = Schema::create()->addField("KEY", UINT32)->addField("VALUE",
+                                                                          UINT32);
 
-  /* Will be called before a test is executed. */
-  void TearDown() {
-    std::cout << "Setup ZMQTest test case." << std::endl;
-  }
+        //    BufferManager::instance().setBufferSize(test_data_size);
+    }
 
-  /* Will be called after all tests in this class are finished. */
-  static void TearDownTestCase() {
-    std::cout << "Tear down ZMQTest test class." << std::endl;
-  }
+    /* Will be called before a test is executed. */
+    void TearDown() {
+        NES_DEBUG("Setup ZMQTest test case.");
+    }
 
-  size_t tupleCnt;
-  std::string address;
+    /* Will be called after all tests in this class are finished. */
+    static void TearDownTestCase() {
+        NES_DEBUG("Tear down ZMQTest test class." );
+    }
 
-  SchemaPtr test_schema;
-  size_t test_data_size;
-  std::array<uint32_t, 8> test_data;
+    size_t tupleCnt;
+    std::string address;
+
+    SchemaPtr test_schema;
+    size_t test_data_size;
+    std::array<uint32_t, 8> test_data;
 };
 
 /* - ZeroMQ Data Source ---------------------------------------------------- */
 TEST_F(ZMQTest, ZmqSourceReceiveData) {
 
-  // Create ZeroMQ Data Source.
-  auto test_schema = Schema::create()->addField("KEY", UINT32)->addField("VALUE",
-                                                                       UINT32);
-  auto zmq_source = createZmqSource(test_schema, LOCAL_HOST, LOCAL_PORT);
-  std::cout << zmq_source->toString() << std::endl;
- // BufferManager::instance().resizeFixedBufferSize(test_data_size);
+    // Create ZeroMQ Data Source.
+    auto test_schema = Schema::create()->addField("KEY", UINT32)->addField("VALUE",
+                                                                           UINT32);
+    auto zmq_source = createZmqSource(test_schema, LOCAL_HOST, LOCAL_PORT);
+    std::cout << zmq_source->toString() << std::endl;
+    // BufferManager::instance().resizeFixedBufferSize(test_data_size);
 
-  // Start thread for receiving the data.
-  bool receiving_finished = false;
-  auto receiving_thread = std::thread([&]() {
-    // Receive data.
-    auto tuple_buffer = zmq_source->receiveData();
+    // Start thread for receiving the data.
+    bool receiving_finished = false;
+    auto receiving_thread = std::thread([&]() {
+        // Receive data.
+        auto tuple_buffer = zmq_source->receiveData();
 
-    // Test received data.
-    size_t sum = 0;
-    uint32_t *tuple = (uint32_t*) tuple_buffer->getBuffer();
-    for (size_t i = 0; i != 8; ++i) {
-      sum += *(tuple++);
+        // Test received data.
+        size_t sum = 0;
+        uint32_t *tuple = (uint32_t *) tuple_buffer->getBuffer();
+        for (size_t i = 0; i != 8; ++i) {
+            sum += *(tuple++);
+        }
+        size_t expected = 400;
+        EXPECT_EQ(sum, expected);
+
+        // BufferManager::instance().releaseFixedSizeBuffer(tuple_buffer);
+        receiving_finished = true;
+    });
+    size_t tupCnt = 8;
+    // Wait until receiving is complete.
+
+    // Open Publisher
+    zmq::context_t context(1);
+    zmq::socket_t socket(context, ZMQ_PUSH);
+    socket.connect(address.c_str());
+    while (!receiving_finished) {
+
+        // Send data from here.
+        zmq::message_t message_tupleCnt(8);
+        memcpy(message_tupleCnt.data(), &tupCnt, 8);
+        socket.send(message_tupleCnt, ZMQ_SNDMORE);
+
+        zmq::message_t message_data(test_data_size);
+        memcpy(message_data.data(), test_data.data(), test_data_size);
+        socket.send(message_data);
     }
-    size_t expected = 400;
-    EXPECT_EQ(sum, expected);
-
-   // BufferManager::instance().releaseFixedSizeBuffer(tuple_buffer);
-    receiving_finished = true;
-  });
-  size_t tupCnt = 8;
-  // Wait until receiving is complete.
-
-  // Open Publisher
-  zmq::context_t context(1);
-  zmq::socket_t socket(context, ZMQ_PUSH);
-  socket.connect(address.c_str());
-  while (!receiving_finished) {
-
-    // Send data from here.
-    zmq::message_t message_tupleCnt(8);
-    memcpy(message_tupleCnt.data(), &tupCnt, 8);
-    socket.send(message_tupleCnt, ZMQ_SNDMORE);
-
-    zmq::message_t message_data(test_data_size);
-    memcpy(message_data.data(), test_data.data(), test_data_size);
-    socket.send(message_data);
-  }
-  receiving_thread.join();
+    receiving_thread.join();
 }
 
 /* - ZeroMQ Data Sink ------------------------------------------------------ */
 TEST_F(ZMQTest, ZmqSinkSendData) {
 
-  return;
-  //FIXME: this test makes no sense, redo it
+    return;
+    //FIXME: this test makes no sense, redo it
 /**
   // Create ZeroMQ Data Sink.
   auto test_schema = Schema::create()->addField("KEY", UINT32)->addField("VALUE",
