071d029119c199cbc059d28d177a05e995ff9eb4
zeuchste <steffen.zeuch@dfki.de>
2020-04-25 17:45:12 +0200

    remove singleton thread pool

--- iotdb/include/NodeEngine/ThreadPool.hpp
+++ iotdb/include/NodeEngine/ThreadPool.hpp
@@ -16,87 +16,89 @@ namespace NES {
  */
 
 class ThreadPool {
- public:
-  /**
-   * @brief Singleton implementation of Thread Pool
-   */
-  static ThreadPool& instance();
+  public:
 
-  /**
-   * @brief start the Thread pool
-   * 1.) check if thread pool is already running,
-   *    - if yes, return false
-   *    - if not set to running to true
-   * 2.) spawn n threads and bind them to the running routine (routine that probes queue for runable tasks)
-   * @return indicate if start succeed
-   */
-  bool start();
+    /**
+     * @brief default constructor
+     */
+    ThreadPool();
 
-  /**
-     * @brief stop the Thread pool
+    /**
+     * @brief default destructor
+     */
+    ~ThreadPool();
+
+    /**
+     * @brief start the Thread pool
      * 1.) check if thread pool is already running,
-     *    - if no, return false
-     *    - if yes set to running to false
-     * 2.) waking up all sleeping threads, during their next getWork,
-     * they will recognize that the execution should terminate and exit running routine
-     * 3.) join all threads, i.e., wait till all threads return
-     * @return indicate if stop succeed
+     *    - if yes, return false
+     *    - if not set to running to true
+     * 2.) spawn n threads and bind them to the running routine (routine that probes queue for runable tasks)
+     * @return indicate if start succeed
      */
-  bool stop();
+    bool start();
 
-  /**
-     * @brief running routine of threads, in this routine, threads repeatedly execute the following steps
-     * 1.) Check if running is still true
-     * 2.) If yes, request work from dispatcher (blocking until tasks get available)
-     * 3.) If task is valid, execute the task and completeWork
-     * 4.) Repeat
+    /**
+       * @brief stop the Thread pool
+       * 1.) check if thread pool is already running,
+       *    - if no, return false
+       *    - if yes set to running to false
+       * 2.) waking up all sleeping threads, during their next getWork,
+       * they will recognize that the execution should terminate and exit running routine
+       * 3.) join all threads, i.e., wait till all threads return
+       * @return indicate if stop succeed
+       */
+    bool stop();
+
+    /**
+       * @brief running routine of threads, in this routine, threads repeatedly execute the following steps
+       * 1.) Check if running is still true
+       * 2.) If yes, request work from dispatcher (blocking until tasks get available)
+       * 3.) If task is valid, execute the task and completeWork
+       * 4.) Repeat
+       */
+    void runningRoutine();
+
+    /**
+     * @brief set the number of threads in the thread pool
+     * @note this effect will take place after the next restart
+     * @param number of threads
      */
-  void runningRoutine();
+    void setNumberOfThreadsWithoutRestart(size_t size);
 
-  /**
-   * @brief set the number of threads in the thread pool
-   * @note this effect will take place after the next restart
-   * @param number of threads
-   */
-  void setNumberOfThreadsWithoutRestart(size_t size);
+    /**
+      * @brief set the number of threads in the thread pool
+      * @param number of threads
+      * @caution this will restart the engine
+      */
+    void setNumberOfThreadsWithRestart(size_t size);
 
-  /**
-    * @brief set the number of threads in the thread pool
-    * @param number of threads
-    * @caution this will restart the engine
-    */
-   void setNumberOfThreadsWithRestart(size_t size);
+    /**
+     * @brief get the current number of threads in thread pool
+     * @return number of current threads
+     */
+    size_t getNumberOfThreads();
+
+    /**
+     *@brief restart the node engine
+     */
+    void restart();
+
+  private:
 
 
-  /**
-   * @brief get the current number of threads in thread pool
-   * @return number of current threads
-   */
-  size_t getNumberOfThreads();
+    //indicating if the thread pool is running, used for multi-thread execution
+    std::atomic<bool> running;
 
-  /**
-   *@brief restart the node engine
-   */
-  void restart();
+    std::atomic<size_t> numThreads;
 
- private:
-  /* implement singleton semantics: no construction,
-   * copying or destruction of Thread Pool objects
-   * outside of the class
-   * Default thread count is 1
-   */
-  ThreadPool();
-  ~ThreadPool();
+    std::vector<std::thread> threads;
 
-  //indicating if the thread pool is running, used for multi-thread execution
-  std::atomic<bool> running;
-
-  std::atomic<size_t> numThreads;
-
-  std::vector<std::thread> threads;
-
-  std::mutex reconfigLock;
+    std::mutex reconfigLock;
 };
+
+typedef std::shared_ptr<ThreadPool> ThreadPoolPtr;
+
 }  // namespace NES
 
 #endif /* THREADPOOL_H_ */
