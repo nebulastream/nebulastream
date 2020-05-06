8ad6c1c46004867b13530381f0aa88993f205fa5
Philipp Grulich <philippgrulich@hotmail.de>
2020-05-06 23:25:50 +0200

    #482 add documentation

--- iotdb/include/QueryCompiler/PipelineExecutionContext.hpp
+++ iotdb/include/QueryCompiler/PipelineExecutionContext.hpp
@@ -7,19 +7,43 @@ class BufferManager;
 typedef std::shared_ptr<BufferManager> BufferManagerPtr;
 
 class TupleBuffer;
+
+/**
+ * @brief The PipelineExecutionContext is passed to a compiled pipeline and offers basic functionality to interact with the runtime.
+ * Via the context, the compile code is able to allocate new TupleBuffers and to emit tuple buffers to the runtime.
+ */
 class PipelineExecutionContext {
 
   public:
+    /**
+     * @brief The PipelineExecutionContext is passed to the compiled pipeline and enables interaction with the NES runtime.
+     * @param bufferManager a reference to the buffer manager to enable allocation from within the pipeline
+     * @param emitFunctionHandler an handler to receive the emitted buffers from the pipeline.
+     */
     explicit PipelineExecutionContext(BufferManagerPtr bufferManager,
-                                      std::function<void(TupleBuffer&)> emitFunction);
+                                      std::function<void(TupleBuffer&)> emitFunctionHandler);
 
+    /**
+     * @brief Allocates a new tuple buffer.
+     * @return TupleBuffer
+     */
     TupleBuffer allocateTupleBuffer();
 
+    /**
+     * @brief Emits a output tuple buffer to the runtime. Internally we call the emit function which is a callback to the correct handler.
+     * @param outputBuffer the output tuple buffer that is passed to the runtime
+     */
     void emitBuffer(TupleBuffer& outputBuffer);
 
   private:
+    /**
+     * @brief Reference to the buffer manager to enable buffer allocation
+     */
     BufferManagerPtr bufferManager;
-    std::function<void(TupleBuffer&)> emitFunction;
+    /**
+     * @brief The emit function handler to react on an emitted tuple buffer.
+     */
+    std::function<void(TupleBuffer&)> emitFunctionHandler;
 };
 
 typedef std::unique_ptr<PipelineExecutionContext> QueryExecutionContextPtr;
