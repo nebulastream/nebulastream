5d8e311b408f0ddbc04456bdc8b679dcdfee9c4f
zeuchste <steffen.zeuch@dfki.de>
2020-05-09 13:58:27 +0200

    #651 first working version with hello worl

--- iotdb/nesWorkerStarter.cpp
+++ iotdb/nesWorkerStarter.cpp
@@ -32,9 +32,81 @@ const string logo = "/********************************************************\n
                     " *\n"
                     " ********************************************************/";
 
+/*** DEBUG CODE ***/
+
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include <grpcpp/grpcpp.h>
+
+#ifdef BAZEL_BUILD
+#include "examples/protos/helloworld.grpc.pb.h"
+#else
+#include <GRPC/helloworld.grpc.pb.h>
+
+#endif
+
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::Status;
+using helloworld::HelloRequest;
+using helloworld::HelloReply;
+using helloworld::Greeter;
+
+
+
+
+class GreeterClient {
+  public:
+    GreeterClient(std::shared_ptr<Channel> channel)
+        : stub_(Greeter::NewStub(channel)) {}
+
+    // Assembles the client's payload, sends it and presents the response back
+    // from the server.
+    std::string SayHello(const std::string& user) {
+        // Data we are sending to the server.
+        HelloRequest request;
+        request.set_name(user);
+
+        // Container for the data we expect from the server.
+        HelloReply reply;
+
+        // Context for the client. It could be used to convey extra information to
+        // the server and/or tweak certain RPC behaviors.
+        ClientContext context;
+
+        // The actual RPC.
+        Status status = stub_->SayHello(&context, request, &reply);
+
+        // Act upon its status.
+        if (status.ok()) {
+            return reply.message();
+        } else {
+            std::cout << status.error_code() << ": " << status.error_message()
+                      << std::endl;
+            return "RPC failed";
+        }
+    }
+
+  private:
+    std::unique_ptr<Greeter::Stub> stub_;
+};
+
 int main(int argc, char** argv) {
     NES::setupLogging("nesWorkerStarter.log", NES::LOG_DEBUG);
     cout << logo << endl;
+
+
+    GreeterClient greeter(grpc::CreateChannel(
+        "0.0.0.0:50051", grpc::InsecureChannelCredentials()));
+    std::string user("world");
+    std::string reply = greeter.SayHello(user);
+    std::cout << "Greeter received: " << reply << std::endl;
+
+    cout << "READY" << endl;
+
+
     namespace po = boost::program_options;
     po::options_description desc("Nes Worker Options");
     uint16_t actorPort = 0;
