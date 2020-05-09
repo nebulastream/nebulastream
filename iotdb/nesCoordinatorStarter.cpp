191fb8a36d5b5dd08ab36052417b0afdcd406493
zeuchste <steffen.zeuch@dfki.de>
2020-05-09 13:58:27 +0200

    #651 first working version with hello worl

--- iotdb/nesCoordinatorStarter.cpp
+++ iotdb/nesCoordinatorStarter.cpp
@@ -28,9 +28,72 @@ const string logo = "/********************************************************\n
                     " *\n"
                     " ********************************************************/";
 
+
+/*** DEBUG CODE ***/
+
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include <grpcpp/grpcpp.h>
+#include <grpcpp/health_check_service_interface.h>
+#include <grpcpp/ext/proto_server_reflection_plugin.h>
+#include <GRPC/helloworld.grpc.pb.h>
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+using helloworld::HelloRequest;
+using helloworld::HelloReply;
+using helloworld::Greeter;
+
+// Logic and data behind the server's behavior.
+class GreeterServiceImpl final : public Greeter::Service {
+    Status SayHello(ServerContext* context, const HelloRequest* request,
+                    HelloReply* reply) override {
+        std::string prefix("Hello ");
+        reply->set_message(prefix + request->name());
+        return Status::OK;
+    }
+};
+void RunServer() {
+    std::string server_address("0.0.0.0:50051");
+    GreeterServiceImpl service;
+
+    ServerBuilder builder;
+    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+    builder.RegisterService(&service);
+    std::unique_ptr<Server> server(builder.BuildAndStart());
+    std::cout << "Server listening on " << server_address << std::endl;
+    server->Wait();
+//
+//    grpc::EnableDefaultHealthCheckService(true);
+//    //    grpc::reflection::InitProtoReflectionServerBuilderPlugin();
+//    ServerBuilder builder;
+//    // Listen on the given address without any authentication mechanism.
+//    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+//    // Register "service" as the instance through which we'll communicate with
+//
+//    // clients. In this case it corresponds to an *synchronous* service.
+//    builder.RegisterService(&service);
+//    // Finally assemble the server.
+//    std::unique_ptr<Server> server(builder.BuildAndStart());
+    std::cout << "Server listening on " << server_address << std::endl;
+
+    // Wait for the server to shutdown. Note that some other thread must be
+    // responsible for shutting down the server for this call to ever return.
+    server->Wait();
+}
+
+
+
+
 int main(int argc, const char* argv[]) {
     NES::setupLogging("nesCoordinatorStarter.log", NES::LOG_DEBUG);
     cout << logo << endl;
+
+    RunServer();
 
     // Initializing defaults
     uint16_t port = 8081;
