e3a8be638ef524d4219789ad54b8a6e841250706
zeuchste <steffen.zeuch@dfki.de>
2020-05-11 14:44:01 +0200

    worker client implemented

---
+++ iotdb/impl/GRPC/WorkerRPCClient.cpp
@@ -0,0 +1,185 @@
+#include <GRPC/WorkerRPCClient.hpp>
+#include <Util/Logger.hpp>
+#include <Actors/ExecutableTransferObject.hpp>
+
+namespace NES {
+
+bool WorkerRPCClient::deployQuery(std::string address, std::string executableTransferObject)
+{
+    NES_DEBUG(
+        "WorkerRPCClient::deployQuery address=" << address << " eto=" << executableTransferObject);
+
+    DeployQueryRequest request;
+    request.set_eto(executableTransferObject);
+
+    DeployQueryReply reply;
+
+    ClientContext context;
+
+    std::shared_ptr<::grpc::Channel> chan = grpc::CreateChannel(address, grpc::InsecureChannelCredentials());
+
+    std::unique_ptr<WorkerRPCService::Stub> workerStub = WorkerRPCService::NewStub(chan);
+
+    Status status = workerStub->DeployQuery(&context, request, &reply);
+
+    if (status.ok()) {
+        NES_DEBUG("WorkerRPCClient::deployQuery: status ok return success=" << reply.success());
+        return reply.success();
+    } else {
+        NES_DEBUG(" WorkerRPCClient::deployQuery "
+                  "error=" << status.error_code() << ": "
+                           << status.error_message());
+        throw Exception("Error while WorkerRPCClient::deployQuery");
+    }
+}
+
+bool WorkerRPCClient::undeployQuery(std::string address, std::string queryId)
+{
+    NES_DEBUG(
+        "WorkerRPCClient::undeployQuery address=" << address << " queryId=" << queryId);
+
+    UndeployQueryRequest request;
+    request.set_queryid(queryId);
+
+    UndeployQueryReply reply;
+
+    ClientContext context;
+
+    std::shared_ptr<::grpc::Channel> chan = grpc::CreateChannel(address, grpc::InsecureChannelCredentials());
+
+    std::unique_ptr<WorkerRPCService::Stub> workerStub = WorkerRPCService::NewStub(chan);
+
+    Status status = workerStub->UndeployQuery(&context, request, &reply);
+
+    if (status.ok()) {
+        NES_DEBUG("WorkerRPCClient::undeployQuery: status ok return success=" << reply.success());
+        return reply.success();
+    } else {
+        NES_DEBUG(" WorkerRPCClient::undeployQuery "
+                  "error=" << status.error_code() << ": "
+                           << status.error_message());
+        throw Exception("Error while WorkerRPCClient::undeployQuery");
+    }
+}
+
+
+bool WorkerRPCClient::registerQuery(std::string address, std::string executableTransferObject)
+{
+    NES_DEBUG(
+        "WorkerRPCClient::registerQuery address=" << address << " eto=" << executableTransferObject);
+
+    RegisterQueryRequest request;
+    request.set_eto(executableTransferObject);
+
+    RegisterQueryReply reply;
+
+    ClientContext context;
+
+    std::shared_ptr<::grpc::Channel> chan = grpc::CreateChannel(address, grpc::InsecureChannelCredentials());
+
+    std::unique_ptr<WorkerRPCService::Stub> workerStub = WorkerRPCService::NewStub(chan);
+
+    Status status = workerStub->RegisterQuery(&context, request, &reply);
+
+    if (status.ok()) {
+        NES_DEBUG("WorkerRPCClient::registerQuery: status ok return success=" << reply.success());
+        return reply.success();
+    } else {
+        NES_DEBUG(" WorkerRPCClient::registerQuery "
+                  "error=" << status.error_code() << ": "
+                           << status.error_message());
+        throw Exception("Error while WorkerRPCClient::registerQuery");
+    }
+}
+
+bool WorkerRPCClient::unregisterQuery(std::string address, std::string queryId)
+{
+    NES_DEBUG(
+        "WorkerRPCClient::unregisterQuery address=" << address << " queryId=" << queryId);
+
+    UnregisterQueryRequest request;
+    request.set_queryid(queryId);
+
+    UnregisterQueryReply reply;
+
+    ClientContext context;
+
+    std::shared_ptr<::grpc::Channel> chan = grpc::CreateChannel(address, grpc::InsecureChannelCredentials());
+
+    std::unique_ptr<WorkerRPCService::Stub> workerStub = WorkerRPCService::NewStub(chan);
+
+    Status status = workerStub->UnregisterQuery(&context, request, &reply);
+
+    if (status.ok()) {
+        NES_DEBUG("WorkerRPCClient::unregisterQuery: status ok return success=" << reply.success());
+        return reply.success();
+    } else {
+        NES_DEBUG(" WorkerRPCClient::unregisterQuery "
+                  "error=" << status.error_code() << ": "
+                           << status.error_message());
+        throw Exception("Error while WorkerRPCClient::unregisterQuery");
+    }
+}
+
+
+bool WorkerRPCClient::startQuery(std::string address, std::string queryId)
+{
+    NES_DEBUG(
+        "WorkerRPCClient::startQuery address=" << address << " queryId=" << queryId);
+
+    StartQueryRequest request;
+    request.set_queryid(queryId);
+
+    StartQueryReply reply;
+
+    ClientContext context;
+
+    std::shared_ptr<::grpc::Channel> chan = grpc::CreateChannel(address, grpc::InsecureChannelCredentials());
+
+    std::unique_ptr<WorkerRPCService::Stub> workerStub = WorkerRPCService::NewStub(chan);
+
+    Status status = workerStub->StartQuery(&context, request, &reply);
+
+    if (status.ok()) {
+        NES_DEBUG("WorkerRPCClient::startQuery: status ok return success=" << reply.success());
+        return reply.success();
+    } else {
+        NES_DEBUG(" WorkerRPCClient::startQuery "
+                  "error=" << status.error_code() << ": "
+                           << status.error_message());
+        throw Exception("Error while WorkerRPCClient::startQuery");
+    }
+}
+
+
+bool WorkerRPCClient::stopQuery(std::string address, std::string queryId)
+{
+    NES_DEBUG(
+        "WorkerRPCClient::stopQuery address=" << address << " queryId=" << queryId);
+
+    StopQueryRequest request;
+    request.set_queryid(queryId);
+
+    StopQueryReply reply;
+
+    ClientContext context;
+
+    std::shared_ptr<::grpc::Channel> chan = grpc::CreateChannel(address, grpc::InsecureChannelCredentials());
+
+    std::unique_ptr<WorkerRPCService::Stub> workerStub = WorkerRPCService::NewStub(chan);
+
+    Status status = workerStub->StopQuery(&context, request, &reply);
+
+    if (status.ok()) {
+        NES_DEBUG("WorkerRPCClient::stopQuery: status ok return success=" << reply.success());
+        return reply.success();
+    } else {
+        NES_DEBUG(" WorkerRPCClient::stopQuery "
+                  "error=" << status.error_code() << ": "
+                           << status.error_message());
+        throw Exception("Error while WorkerRPCClient::stopQuery");
+    }
+}
+
+
+}
---
+++ iotdb/include/GRPC/WorkerRPCClient.hpp
@@ -0,0 +1,75 @@
+#ifndef NES_INCLUDE_GRPC_WORKERRPCCLIENT_HPP_
+#define NES_INCLUDE_GRPC_WORKERRPCCLIENT_HPP_
+
+#include <GRPC/WorkerRPCService.grpc.pb.h>
+#include <GRPC/WorkerRPCService.pb.h>
+
+#include <Services/CoordinatorService.hpp>
+
+#include <grpcpp/grpcpp.h>
+#include <Catalogs/PhysicalStreamConfig.hpp>
+#include <Topology/NESTopologyEntry.hpp>
+#include <string>
+
+using namespace std;
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::Status;
+
+namespace NES {
+class WorkerRPCClient {
+
+    /**
+    * @brief deploy registers and starts a query
+    * @param queryId
+    * @param new query plan as eto
+    * @return true if succeeded, else false
+    */
+    bool deployQuery(std::string address, std::string executableTransferObject);
+
+    /**
+     * @brief undeploy stops and undeploy a query
+     * @param queryId to undeploy
+     * @return true if succeeded, else false
+     */
+    bool undeployQuery(std::string address, std::string queryId);
+
+    /**
+    * @brief gregisters a query
+    * @param queryId
+    * @param query plan to register as eto
+    * @return true if succeeded, else false
+    */
+    bool registerQuery(std::string address, std::string executableTransferObject);
+
+    /**
+     * @brief ungregisters a query
+     * @param queryIdto unregister query
+     * @return true if succeeded, else false
+     */
+    bool unregisterQuery(std::string address, std::string queryId);
+
+    /**
+     * @brief method to start a already deployed query
+     * @note if query is not deploy, false is returned
+     * @param queryId to start
+     * @return bool indicating success
+     */
+    bool startQuery(std::string address, std::string queryId);
+
+    /**
+     * @brief method to stop a query
+     * @param queryId to stop
+     * @return bool indicating success
+     */
+    bool stopQuery(std::string address, std::string queryId);
+
+  private:
+    CoordinatorServicePtr coordinatorServicePtr;
+    std::unique_ptr<WorkerRPCService::Stub> workerStub;
+
+};
+
+}
+
+#endif //NES_INCLUDE_GRPC_WORKERRPCCLIENT_HPP_
