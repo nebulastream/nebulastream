5b08c34c1b6bd8ccd119a84790efcdb7f70b2a18
arianeziehn <arianeziehn@googlemail.com>
2020-05-19 16:32:08 +0200

    #674 remove boost uuid
    fix generateId : 100% tests passed, 0 tests failed out of 36

--- iotdb/impl/Util/UtilityFunctions.cpp
+++ iotdb/impl/Util/UtilityFunctions.cpp
@@ -176,7 +176,7 @@ std::string UtilityFunctions::generateQueryId() {
     for (i = 0; i < 12; i++) {
         ss << dis(gen);
     };
-    NES_DEBUG("UtilityFunctions: generateId: " + ss.str());
+    NES_DEBUG("UtilityFunctions: generateQueryId: " + ss.str());
     return ss.str();
 }
 
@@ -186,8 +186,8 @@ std::string UtilityFunctions::generateId() {
 
     std::uniform_int_distribution<int> dist(0, 15);
 
-    const char *v = "0123456789";
-    const bool dash[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+    const char *v = "0123456789abcdef";
+    const bool dash[] = { 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0 };
 
     std::string res;
     for (int i = 0; i < 16; i++) {
@@ -195,6 +195,8 @@ std::string UtilityFunctions::generateId() {
         res += v[dist(rng)];
         res += v[dist(rng)];
     }
+
+    NES_DEBUG("UtilityFunctions: generateId: " + res);
     return res;
 
 }
