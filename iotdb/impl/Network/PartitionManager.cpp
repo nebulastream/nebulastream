18ad8b11399470e68eb93ef1df18415747d35c9b
x.chatziliadis <x.chatziliadis@tu-berlin.de>
2020-05-19 16:21:34 +0200

    #656 Ventura comments addressed for PR - bugfix

--- iotdb/impl/Network/PartitionManager.cpp
+++ iotdb/impl/Network/PartitionManager.cpp
@@ -5,7 +5,7 @@
 namespace NES::Network {
 
 uint64_t PartitionManager::registerSubpartition(NesPartition partition) {
-    unique_lock<std::mutex> lock(partitionCounterMutex);
+    std::unique_lock<std::mutex> lock(partitionCounterMutex);
     NES_INFO("PartitionManager: Registering " << partition.toString());
 
     //check if partition is present
@@ -19,7 +19,7 @@ uint64_t PartitionManager::registerSubpartition(NesPartition partition) {
 }
 
 uint64_t PartitionManager::unregisterSubpartition(NesPartition partition) {
-    unique_lock<std::mutex> lock(partitionCounterMutex);
+    std::unique_lock<std::mutex> lock(partitionCounterMutex);
     NES_INFO("PartitionManager: Unregistering " << partition.toString());
 
     // if partition is contained, decrement counter
@@ -38,18 +38,18 @@ uint64_t PartitionManager::unregisterSubpartition(NesPartition partition) {
 }
 
 uint64_t PartitionManager::getSubpartitionCounter(NesPartition partition) {
-    unique_lock<std::mutex> lock(partitionCounterMutex);
+    std::unique_lock<std::mutex> lock(partitionCounterMutex);
     return partitionCounter.at(partition);
 }
 
 uint64_t PartitionManager::deletePartition(NesPartition partition) {
-    unique_lock<std::mutex> lock(partitionCounterMutex);
+    std::unique_lock<std::mutex> lock(partitionCounterMutex);
     NES_INFO("PartitionManager: Deleting " << partition.toString());
     return partitionCounter.erase(partition);
 }
 
 void PartitionManager::clear() {
-    unique_lock<std::mutex> lock(partitionCounterMutex);
+    std::unique_lock<std::mutex> lock(partitionCounterMutex);
     NES_INFO("PartitionManager: Clearing registered partitions");
     partitionCounter.clear();
 }
