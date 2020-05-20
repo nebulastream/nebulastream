f7466242006f47fc1d15e6513d4d83a9709f669b
Ankit Chaudhary <my007id@gmail.com>
2020-05-20 19:31:43 +0200

    #338 : added version header file

---
+++ iotdb/include/Version/version.hpp
@@ -0,0 +1,7 @@
+#ifndef __VERSION_HPP__
+#define __VERSION_HPP__
+#define NES_VERSION_MAJOR 0
+#define NES_VERSION_MINOR 0
+#define NES_VERSION_PATCH 2
+constexpr char const* NES_VERSION      = "0.0.2-e38f4494";
+#endif
