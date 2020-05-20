547e6142dda1611a3b1bed7df7bedfc04e7eac49
Ankit Chaudhary <my007id@gmail.com>
2020-05-20 19:31:17 +0200

    #338 : added version template file, added new target named release for upgrading the version and releasing a new tag

---
+++ iotdb/cmake/GetSemanticVersion.cmake
@@ -0,0 +1,97 @@
+find_package(Git)
+
+if (Git_FOUND)
+
+    # Get last tag from git
+    execute_process(COMMAND ${GIT_EXECUTABLE} describe --abbrev=0 --tags
+            WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+            OUTPUT_VARIABLE ${PROJECT_NAME}_VERSION_STRING
+            OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+    # Get partial versions into a list
+    string(REGEX MATCHALL "-.*$|[0-9]+" ${PROJECT_NAME}_PARTIAL_VERSION_LIST
+            ${${PROJECT_NAME}_VERSION_STRING})
+
+    # Set the last released version number
+    list(GET ${PROJECT_NAME}_PARTIAL_VERSION_LIST
+            0 ${PROJECT_NAME}_VERSION_MAJOR)
+    list(GET ${PROJECT_NAME}_PARTIAL_VERSION_LIST
+            1 ${PROJECT_NAME}_VERSION_MINOR)
+    list(GET ${PROJECT_NAME}_PARTIAL_VERSION_LIST
+            2 ${PROJECT_NAME}_VERSION_PATCH)
+
+    #Increment patch number to indicate next free version
+    if (RELEASE_MAJOR)
+        math(EXPR ${PROJECT_NAME}_VERSION_MAJOR ${${PROJECT_NAME}_VERSION_MAJOR}+1)
+        set(${PROJECT_NAME}_VERSION_MINOR 0)
+        set(${PROJECT_NAME}_VERSION_PATCH 0)
+    elseif (RELEASE_MINOR)
+        math(EXPR ${PROJECT_NAME}_VERSION_MINOR ${${PROJECT_NAME}_VERSION_MINOR}+1)
+        set(${PROJECT_NAME}_VERSION_PATCH 0)
+    else ()
+        math(EXPR ${PROJECT_NAME}_VERSION_PATCH ${${PROJECT_NAME}_VERSION_PATCH}+1)
+    endif ()
+
+    # Get current branch name
+    execute_process(COMMAND ${GIT_EXECUTABLE} rev-parse --abbrev-ref HEAD
+            WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+            OUTPUT_VARIABLE ${PROJECT_NAME}_BRANCH_NAME
+            OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+    if (${${PROJECT_NAME}_BRANCH_NAME} STREQUAL "master")
+        # Set project version string
+        set(${PROJECT_NAME}_VERSION ${${PROJECT_NAME}_VERSION_MAJOR}.${${PROJECT_NAME}_VERSION_MINOR}.${${PROJECT_NAME}_VERSION_PATCH})
+    else ()
+        # Get current commit SHA from git
+        execute_process(COMMAND ${GIT_EXECUTABLE} rev-parse --short HEAD
+                WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+                OUTPUT_VARIABLE ${PROJECT_NAME}_VERSION_GIT_SHA
+                OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+        # Set project version string with last commit hash
+        set(${PROJECT_NAME}_VERSION ${${PROJECT_NAME}_VERSION_MAJOR}.${${PROJECT_NAME}_VERSION_MINOR}.${${PROJECT_NAME}_VERSION_PATCH}-${${PROJECT_NAME}_VERSION_GIT_SHA})
+    endif ()
+
+    # Unset the list
+    unset(${PROJECT_NAME}_PARTIAL_VERSION_LIST)
+else ()
+    message(WARNING "Unable to find git configured. Using UNKNOWN as version")
+    set(${PROJECT_NAME}_VERSION "UNKNOWN")
+endif ()
+
+message("NES Version: " ${${PROJECT_NAME}_VERSION})
+
+#if (Git_FOUND)
+#    if (${${PROJECT_NAME}_BRANCH_NAME} STREQUAL "master")
+#        if (RELEASE_MAJOR)
+#            math(EXPR ${PROJECT_NAME}_VERSION_MAJOR ${${PROJECT_NAME}_VERSION_MAJOR} + 1)
+#            set(${PROJECT_NAME}_VERSION_MINOR 0)
+#            set(${PROJECT_NAME}_VERSION_PATCH 0)
+#        elseif (RELEASE_MINOR)
+#            math(EXPR ${PROJECT_NAME}_VERSION_MINOR ${${PROJECT_NAME}_VERSION_MINOR} + 1)
+#            set(${PROJECT_NAME}_VERSION_PATCH 0)
+#        else ()
+#            math(EXPR ${PROJECT_NAME}_VERSION_PATCH ${${PROJECT_NAME}_VERSION_PATCH} + 1)
+#        endif ()
+#
+#        set(${PROJECT_NAME}_VERSION ${${PROJECT_NAME}_VERSION_MAJOR}.${${PROJECT_NAME}_VERSION_MINOR}
+#                .${${PROJECT_NAME}_VERSION_PATCH})
+#    else ()
+#        set(${PROJECT_NAME}_VERSION ${${PROJECT_NAME}_VERSION_MAJOR}.${${PROJECT_NAME}_VERSION_MINOR}
+#                .${${PROJECT_NAME}_VERSION_PATCH}.${${PROJECT_NAME}_VERSION_GIT_SHA})
+#    endif ()
+#
+#    # Save version to file (which will be used when Git is not available
+#    # or VERSION_UPDATE_FROM_GIT is disabled)
+#    file(WRITE ${CMAKE_SOURCE_DIR}/VERSION ${${PROJECT_NAME}_VERSION_STRING_FULL}
+#            "*" ${${PROJECT_NAME}_VERSION_STRING}
+#            "*" ${${PROJECT_NAME}_VERSION_MAJOR}
+#            "*" ${${PROJECT_NAME}_VERSION_MINOR}
+#            "*" ${${PROJECT_NAME}_VERSION_PATCH}
+#            "*" ${${PROJECT_NAME}_VERSION_GIT_SHA})
+#else ()
+#
+#endif ()
+
+
+# Set project version (without the preceding 'v')
