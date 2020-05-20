37e2ca633e2016052ebb4c8f43be08b6ec1b9bbe
Ankit Chaudhary <my007id@gmail.com>
2020-05-20 23:12:11 +0200

    #338 : adjusted the git workflow and scripts

--- iotdb/docker/scripts/entrypoint-nes-build.sh
+++ iotdb/docker/scripts/entrypoint-nes-build.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-[[ ! -f /iotdb/CMakeLists.txt ]] && echo "Please mount source code at /iotdb point. Run [docker run -v <path-to-iotdb>:/iotdb -d <iotdb-image>]" && exit 1
+[[ ! -f /IotDB/iotdb/CMakeLists.txt ]] && echo "Please mount source code at /IotDB point. Run [docker run -v <path-to-iotdb>:/IotDB -d <iotdb-image>]" && exit 1
 
 if [ $# -eq 0 ]
 then
-    mkdir -p /iotdb/build
-    cd /iotdb/build
+    mkdir -p /IotDB/iotdb/build
+    cd /IotDB/iotdb/build
     cmake -DCMAKE_BUILD_TYPE=Release -DGRPC_AVAILABLE=1 -DZMQ_AVAILABLE=1 -DCPPZMQ_AVAILABLE=1 -DNES_BUILDING_ON_CI=1 -DBoost_NO_SYSTEM_PATHS=TRUE -DBoost_INCLUDE_DIR="/usr/include" -DBoost_LIBRARY_DIR="/usr/lib/x86_64-linux-gnu" -DCPPRESTSDK_DIR="/usr/lib/x86_64-linux-gnu/cmake/" -DCMAKE_CXX_FLAGS_RELEASE:STRING="-w -Wno-unused-variable -Wno-unused-parameter -Wno-ignored-qualifiers -Wno-sign-compare " ..
     make -j4
-    cd /iotdb/build/tests
+    cd /IotDB/iotdb/build/tests
     ln -s ../nesCoordinator .
    	ln -s ../nesWorker .
     make test_debug
     result=$?
-    rm -rf /iotdb/build
+    rm -rf /IotDB/iotdb/build
     exit $result
 else
     exec $@
--- iotdb/docker/scripts/entrypoint-nes-release.sh
+++ iotdb/docker/scripts/entrypoint-nes-release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir -p /iotdb/build
-cd /iotdb/build
+mkdir -p /IoTDB/iotdb/build
+cd /IoTDB/iotdb/build
 cmake -DCMAKE_BUILD_TYPE=Release -DGRPC_AVAILABLE=1 -DZMQ_AVAILABLE=1 -DCPPZMQ_AVAILABLE=1 -DNES_BUILDING_ON_CI=1 -DBoost_NO_SYSTEM_PATHS=TRUE -DBoost_INCLUDE_DIR="/usr/include" -DBoost_LIBRARY_DIR="/usr/lib/x86_64-linux-gnu" -DCPPRESTSDK_DIR="/usr/lib/x86_64-linux-gnu/cmake/" -DCMAKE_CXX_FLAGS_RELEASE:STRING="-w -Wno-unused-variable -Wno-unused-parameter -Wno-ignored-qualifiers -Wno-sign-compare " ..
 make release
