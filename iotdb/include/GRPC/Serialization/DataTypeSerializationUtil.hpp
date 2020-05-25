9677e24da822ea1e597b98203b072f0139c611c8
Philipp Grulich <philippgrulich@hotmail.de>
2020-05-25 18:58:12 +0200

    #509 add documentation

--- iotdb/include/GRPC/Serialization/DataTypeSerializationUtil.hpp
+++ iotdb/include/GRPC/Serialization/DataTypeSerializationUtil.hpp
@@ -11,11 +11,40 @@ typedef std::shared_ptr<DataType> DataTypePtr;
 class SerializableDataType;
 class SerializableDataValue;
 
+/**
+ * @brief The DataTypeSerializationUtil offers functionality to serialize and de-serialize data types and value types to a
+ * corresponding protobuffer object.
+ */
 class DataTypeSerializationUtil {
   public:
+    /**
+     * @brief Serializes a data type and all its children to a SerializableDataType object.
+     * @param dataType The data type.
+     * @param serializedDataType The corresponding protobuff object, which is used to capture the state of the object.
+     * @return the modified serializedDataType
+     */
     static SerializableDataType* serializeDataType(DataTypePtr dataType, SerializableDataType* serializedDataType);
+
+    /**
+    * @brief De-serializes the SerializableDataType and all its children to a DataTypePtr
+    * @param serializedDataType the serialized data type.
+    * @return DataTypePtr
+    */
     static DataTypePtr deserializeDataType(SerializableDataType* serializedDataType);
+
+    /**
+     * @brief Serializes a value type and all its children to a SerializableDataValue object.
+     * @param valueType The data value type.
+     * @param serializedDataValue The corresponding protobuff object, which is used to capture the state of the object.
+     * @return the modified serializedDataValue
+     */
     static SerializableDataValue* serializeDataValue(ValueTypePtr valueType, SerializableDataValue* serializedDataValue);
+
+    /**
+    * @brief De-serializes the SerializableDataValue and all its children to a ValueTypePtr
+    * @param serializedDataValue the serialized data value type.
+    * @return ValueTypePtr
+    */
     static ValueTypePtr deserializeDataValue(SerializableDataValue* serializedDataValue);
 };
 }// namespace NES
