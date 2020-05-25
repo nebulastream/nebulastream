0cf6d2854327ecda77b7d722b815ffccecee9658
Philipp Grulich <philippgrulich@hotmail.de>
2020-05-25 19:14:28 +0200

    #509 add documentation

--- iotdb/include/GRPC/Serialization/ExpressionSerializationUtil.hpp
+++ iotdb/include/GRPC/Serialization/ExpressionSerializationUtil.hpp
@@ -10,10 +10,28 @@ typedef std::shared_ptr<ExpressionNode> ExpressionNodePtr;
 
 class SerializableExpression;
 
+/**
+ * @brief The ExpressionSerializationUtil offers functionality to serialize and de-serialize expression nodes to the
+ * corresponding protobuffer object.
+ */
 class ExpressionSerializationUtil {
   public:
+    /**
+    * @brief Serializes a expression node and all its children to a SerializableDataType object.
+    * @param expressionNode The root expression node to serialize.
+    * @param serializedExpression The corresponding protobuff object, which is used to capture the state of the object.
+    * @return the modified serializedExpression
+    */
     static SerializableExpression* serializeExpression(ExpressionNodePtr expressionNode, SerializableExpression* serializedExpression);
+
+    /**
+     * @brief De-serializes the SerializableExpression and all its children to a corresponding ExpressionNodePtr
+     * @param serializedExpression the serialized expression.
+     * @return ExpressionNodePtr
+     */
     static ExpressionNodePtr deserializeExpression(SerializableExpression* serializedExpression);
+
+  private:
     static void serializeLogicalExpressions(ExpressionNodePtr sharedPtr, SerializableExpression* pExpression);
     static void serializeArithmeticalExpressions(ExpressionNodePtr expression, SerializableExpression* serializedExpression);
     static ExpressionNodePtr deserializeLogicalExpressions(SerializableExpression* serializedExpression);
