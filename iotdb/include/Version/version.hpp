af5050e1bf5bb8f8589f6e2612ef26a7ca750bc5
NES-CI <nebulastream@dima.tu-berlin.de>
2020-05-27 15:35:20 +0000

    GIT-CI: Updating NES version to 0.0.7

--- iotdb/include/Version/version.hpp
+++ iotdb/include/Version/version.hpp
@@ -2,6 +2,6 @@
 #define __VERSION_HPP__
 #define NES_VERSION_MAJOR 0
 #define NES_VERSION_MINOR 0
-#define NES_VERSION_PATCH 6
-constexpr char const* NES_VERSION      = "0.0.6";
+#define NES_VERSION_PATCH 7
+constexpr char const* NES_VERSION      = "0.0.7";
 #endif
