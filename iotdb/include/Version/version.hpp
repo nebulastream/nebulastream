01f3d6a0315aa7afef43f07a28361555147d4ac3
Ankit Chaudhary <ankit.chaudhary@tu-berlin.de>
2020-05-27 12:50:26 +0200

    #709 : fixed cmake check for git repos

--- iotdb/include/Version/version.hpp
+++ iotdb/include/Version/version.hpp
@@ -3,5 +3,5 @@
 #define NES_VERSION_MAJOR 0
 #define NES_VERSION_MINOR 0
 #define NES_VERSION_PATCH 4
-constexpr char const* NES_VERSION      = "0.0.4-7bf1897e-SNAPSHOT";
+constexpr char const* NES_VERSION      = "0.0.4-c4df9402-SNAPSHOT";
 #endif
