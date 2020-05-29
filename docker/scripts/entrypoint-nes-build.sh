5fc2987c186ba854cbf150c83e20c63ffc0971ef
Ankit Chaudhary <ankit.chaudhary@tu-berlin.de>
2020-05-29 11:54:42 +0200

    #165: updated docker scripts and removed iotdb name from the project

--- docker/scripts/entrypoint-nes-build.sh
+++ docker/scripts/entrypoint-nes-build.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-[[ ! -f /IoTDB/iotdb/CMakeLists.txt ]] && echo "Please mount source code at /IoTDB point. Run [docker run -v <path-to-iotdb>:/IoTDB -d <iotdb-image>]" && exit 1
+[[ ! -f /nebulastream/CMakeLists.txt ]] && echo "Please mount source code at /nebulastream point. Run [docker run -v <path-to-nes>:/nebulastream -d <nes-image>]" && exit 1
 
 if [ $# -eq 0 ]
 then
-    mkdir -p /IoTDB/iotdb/build
-    cd /IoTDB/iotdb/build
+    mkdir -p /nebulastream/build
+    cd /nebulastream/build
     cmake -DCMAKE_BUILD_TYPE=Release -DGRPC_AVAILABLE=1 -DZMQ_AVAILABLE=1 -DCPPZMQ_AVAILABLE=1 -DNES_BUILDING_ON_CI=1 -DBoost_NO_SYSTEM_PATHS=TRUE -DBoost_INCLUDE_DIR="/usr/include" -DBoost_LIBRARY_DIR="/usr/lib/x86_64-linux-gnu" -DCPPRESTSDK_DIR="/usr/lib/x86_64-linux-gnu/cmake/" -DCMAKE_CXX_FLAGS_RELEASE:STRING="-w -Wno-unused-variable -Wno-unused-parameter -Wno-ignored-qualifiers -Wno-sign-compare " ..
     make -j4
-    cd /IoTDB/iotdb/build/tests
+    cd /nebulastream/build/tests
     ln -s ../nesCoordinator .
    	ln -s ../nesWorker .
     make test_debug
     result=$?
-    rm -rf /IoTDB/iotdb/build
+    rm -rf /nebulastream/build
     exit $result
 else
     exec $@
