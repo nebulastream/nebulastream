42a98b4ba85c51b59321280984d010162c7dd2eb
Ventura Del Monte <venturadelmonte@gmail.com>
2020-06-03 15:54:04 +0200

    [#738] Reduce number of parallel jobs for git submodule fetching when installing gRPC

--- scripts/dependencies/ubuntu/install.sh
+++ scripts/dependencies/ubuntu/install.sh
@@ -20,7 +20,7 @@ sudo apt-get update -qq && sudo apt-get install -qq \
   tar
 
 git clone --branch v1.28.1 https://github.com/grpc/grpc.git && \
-  cd grpc && git submodule update --init && mkdir -p build && cd build && cmake .. -DCMAKE_BUILD_TYPE=Release && \
+  cd grpc && git submodule update --init --jobs 1 && mkdir -p build && cd build && cmake .. -DCMAKE_BUILD_TYPE=Release && \
   make -j && sudo make install && cd .. && cd .. && rm -rf grpc && \
   git clone --single-branch --branch v4.3.2 https://github.com/zeromq/libzmq.git && cd libzmq && \
   mkdir -p build && cd build && cmake .. -DCMAKE_BUILD_TYPE=Release && make -j && sudo make install && \
