0bfbfa282c26991be576ad5c5e22f10293008f1e
Ankit Chaudhary <ankit.chaudhary@tu-berlin.de>
2020-06-08 20:22:38 +0200

    #616: updated rest server interface

--- src/REST/RestServer.cpp
+++ src/REST/RestServer.cpp
@@ -7,16 +7,11 @@
 
 namespace NES {
 
-RestServer::RestServer(std::string host, u_int16_t port,
-                       NesCoordinatorPtr coordinator,
-                       QueryCatalogPtr queryCatalog,
-                       StreamCatalogPtr streamCatalog,
-                       TopologyManagerPtr topologyManager) {
-    this->host = host;
-    this->port = port;
-    this->coordinator = coordinator;
-    this->queryCatalog = queryCatalog;
-    restEngine = std::make_shared<RestEngine>(streamCatalog, coordinator, queryCatalog, topologyManager);
+RestServer::RestServer(std::string host, u_int16_t port, NesCoordinatorPtr coordinator, QueryCatalogPtr queryCatalog,
+                       StreamCatalogPtr streamCatalog, TopologyManagerPtr topologyManager, GlobalExecutionPlanPtr executionPlan)
+    : host(host), port(port), coordinator(coordinator), queryCatalog(queryCatalog) {
+
+    restEngine = std::make_shared<RestEngine>(streamCatalog, coordinator, queryCatalog, topologyManager, executionPlan);
     InterruptHandler::hookSIGINT();
 }
 
