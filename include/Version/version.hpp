579d4f1c52073c89266f8117f6305f3b233c823d
Ankit Chaudhary <ankit.chaudhary@tu-berlin.de>
2020-06-09 13:22:25 +0200

    #718: updated docker base image to ubuntu 20.04

--- include/Version/version.hpp
+++ include/Version/version.hpp
@@ -3,5 +3,5 @@
 #define NES_VERSION_MAJOR 0
 #define NES_VERSION_MINOR 0
 #define NES_VERSION_PATCH 23
-constexpr char const* NES_VERSION      = "0.0.23";
+constexpr char const* NES_VERSION      = "0.0.23-a8f12203-SNAPSHOT";
 #endif
