d7ed10d8b445b56fb9f369233dc6de0bded7bc73
Philipp Grulich <philippgrulich@hotmail.de>
2020-06-11 11:34:01 +0200

    added types

---
+++ include/DataTypes/Numeric.hpp
@@ -0,0 +1,9 @@
+#ifndef NES_INCLUDE_DATATYPES_NUMERIC_HPP_
+#define NES_INCLUDE_DATATYPES_NUMERIC_HPP_
+#include <DataTypes/DataType.hpp>
+
+class Numeric : DataType {
+
+};
+
+#endif//NES_INCLUDE_DATATYPES_NUMERIC_HPP_
