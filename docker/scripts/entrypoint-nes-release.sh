0d5277cf5b97c570738cc11673662f507ff7aa00
Ankit Chaudhary <ankit.chaudhary@tu-berlin.de>
2020-06-12 16:31:17 +0200

    #616 : enabled clang format

--- docker/scripts/entrypoint-nes-release.sh
+++ docker/scripts/entrypoint-nes-release.sh
@@ -16,6 +16,5 @@ git config --local core.sshcommand "/usr/bin/ssh -i \"~/.ssh/id_rsa\" -o \"UserK
 mkdir -p /nebulastream/build
 cd /nebulastream/build
 cmake -DCMAKE_BUILD_TYPE=Release -DGRPC_AVAILABLE=1 -DZMQ_AVAILABLE=1 -DCPPZMQ_AVAILABLE=1 -DNES_BUILDING_ON_CI=1 -DBoost_NO_SYSTEM_PATHS=TRUE -DBoost_INCLUDE_DIR="/usr/include" -DBoost_LIBRARY_DIR="/usr/lib/x86_64-linux-gnu" -DCPPRESTSDK_DIR="/usr/lib/x86_64-linux-gnu/cmake/" -DCMAKE_CXX_FLAGS_RELEASE:STRING="-w -Wno-unused-variable -Wno-unused-parameter -Wno-ignored-qualifiers -Wno-sign-compare " ..
-# Enable this line when llvm is upgraded
-#make format
+make format
 make release
