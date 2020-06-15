dae39cc7fa9ae5a595d6305ae6b485884bc9d794
x.chatziliadis <x.chatziliadis@tu-berlin.de>
2020-06-15 12:55:38 +0200

    #776 addressing PR comments

--- include/Version/version.hpp
+++ include/Version/version.hpp
@@ -2,6 +2,6 @@
 #define __VERSION_HPP__
 #define NES_VERSION_MAJOR 0
 #define NES_VERSION_MINOR 0
-#define NES_VERSION_PATCH 33
-constexpr char const* NES_VERSION      = "0.0.33-a15670d1-SNAPSHOT";
+#define NES_VERSION_PATCH 35
+constexpr char const* NES_VERSION      = "0.0.35-90e72907-SNAPSHOT";
 #endif
