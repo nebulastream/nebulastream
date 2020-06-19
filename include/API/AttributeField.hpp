1e6e749f616818386ebd70593208d63cb064495a
Philipp Grulich <philippgrulich@hotmail.de>
2020-06-20 01:11:35 +0200

    #444 update doc

--- include/API/AttributeField.hpp
+++ include/API/AttributeField.hpp
@@ -12,16 +12,28 @@ typedef std::shared_ptr<DataType> DataTypePtr;
 class AttributeField;
 typedef std::shared_ptr<AttributeField> AttributeFieldPtr;
 
+/**
+ * @brief Represents a typed field in a schema.
+ */
 class AttributeField {
   public:
     AttributeField() = default;
     AttributeField(const std::string& name, DataTypePtr dataType);
+
+    /**
+     * @brief Factory method to create a new field
+     * @param name name of the field
+     * @param dataType data type
+     * @return AttributeFieldPtr
+     */
     static AttributeFieldPtr create(std::string name, DataTypePtr dataType);
-    std::string name;
-    DataTypePtr dataType;
+
     DataTypePtr getDataType() const;
     const std::string toString() const;
     bool isEqual(AttributeFieldPtr attr);
+
+    std::string name;
+    DataTypePtr dataType;
 };
 
 }// namespace NES
