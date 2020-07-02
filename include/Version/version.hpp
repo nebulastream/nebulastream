d27871e0e5a537287bcb609bfd67aef3eda71103
NES-CI <nebulastream@dima.tu-berlin.de>
2020-07-02 17:24:29 +0000

    GIT-CI: Updating NES version to 0.0.47

--- include/Version/version.hpp
+++ include/Version/version.hpp
@@ -2,9 +2,6 @@
 #define __VERSION_HPP__
 #define NES_VERSION_MAJOR 0
 #define NES_VERSION_MINOR 0
-<<<<<<< HEAD
-#define NES_VERSION_PATCH 46 constexpr char const* NES_VERSION = "0.0.46";
-=======
-#define NES_VERSION_PATCH 45 constexpr char const* NES_VERSION = "0.0.45-86048d8b-SNAPSHOT";
->>>>>>> #623 : fixed the bug in path traversal for topdown and bottomup
+#define NES_VERSION_PATCH 47
+constexpr char const* NES_VERSION = "0.0.47";
 #endif
