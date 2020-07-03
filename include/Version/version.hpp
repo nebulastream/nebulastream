9d500aca3bef015056d5604f1c52220c13ab8cb0
Ankit Chaudhary <ankit.chaudhary@tu-berlin.de>
2020-07-03 17:27:33 +0200

    #671: fixed formatting error

--- include/Version/version.hpp
+++ include/Version/version.hpp
@@ -2,6 +2,6 @@
 #define __VERSION_HPP__
 #define NES_VERSION_MAJOR 0
 #define NES_VERSION_MINOR 0
-#define NES_VERSION_PATCH 51
-constexpr char const* NES_VERSION = "0.0.51";
+#define NES_VERSION_PATCH 50
+constexpr char const* NES_VERSION = "0.0.50-14c32e0c-SNAPSHOT";
 #endif
