8d4962f83615207c924eccc0fb4f2498a87a2d08
arianeziehn <arianeziehn@googlemail.com>
2020-07-06 12:34:00 +0200

    #743 update Pattern.cpp Error constructor

--- src/API/Pattern.cpp
+++ src/API/Pattern.cpp
@@ -4,15 +4,14 @@
 #include <Nodes/Operators/LogicalOperators/Sources/LogicalStreamSourceDescriptor.hpp>
 #include <Plans/Query/QueryPlan.hpp>
 #include <iostream>
+#include <utility>
 
 
 namespace NES {
 
-Pattern::Pattern(const Pattern& query) : Query (query)  {
+//Pattern::Pattern(const Pattern& query) : Query (query)  {}
 
-}
-
-Pattern::Pattern(QueryPlanPtr queryplan) :Query(queryPlan){}
+Pattern::Pattern(QueryPlanPtr queryPlan) :Query(queryPlan){}
 
 Pattern Pattern::from(const std::string sourceStreamName) {
     NES_DEBUG("Pattern: create query for input stream " << sourceStreamName);
@@ -22,30 +21,15 @@ Pattern Pattern::from(const std::string sourceStreamName) {
     return Pattern(queryPlan);
 }
 
-Pattern& Pattern::filter(const ExpressionNodePtr filterExpression) {
-    NES_DEBUG("Pattern: add filter operator to query");
-    OperatorNodePtr op = createFilterLogicalOperatorNode(filterExpression);
-    queryPlan->appendOperator(op);
-    return *this;
-}
 
-Pattern& Pattern::map(const FieldAssignmentExpressionNodePtr mapExpression) {
-    NES_DEBUG("Pattern: add map operator to query");
-    OperatorNodePtr op = createMapLogicalOperatorNode(mapExpression);
-    queryPlan->appendOperator(op);
-    return *this;
-}
-
-Pattern& Pattern::sink(const SinkDescriptorPtr sinkDescriptor) {
+/*Pattern& Pattern::sink(const SinkDescriptorPtr sinkDescriptor) {
     NES_DEBUG("Pattern: add sink operator to query");
     OperatorNodePtr op = createSinkLogicalOperatorNode(sinkDescriptor);
     queryPlan->appendOperator(op);
     return *this;
-}
+}*/
 
-QueryPlanPtr Pattern::getQueryPlan() {
-    return queryPlan;
-}
+
 const std::string& Pattern::getPatternName() const {
     return patternName;
 }
