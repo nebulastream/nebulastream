c1ee0b5a04f6f13c5d9e5b9d1122aa64eba9a275
Ankit Chaudhary <ankit.chaudhary@tu-berlin.de>
2020-07-08 21:27:52 +0200

    #810: added Query processor service

--- src/Services/QueryProcessorService.cpp
+++ src/Services/QueryProcessorService.cpp
@@ -1,1 +1,21 @@
-#include "QueryProcessorService.hpp"
+#include <Services/QueryProcessorService.hpp>
+#include <Catalogs/QueryCatalog.hpp>
+#include <Util/Logger.hpp>
+
+namespace NES {
+
+QueryProcessorService::QueryProcessorService(QueryCatalogPtr queryCatalog) : queryCatalog(queryCatalog) {
+    NES_INFO("QueryProcessorService()");
+}
+
+int QueryProcessorService::operator()() {
+    
+    while (true){
+        const std::vector<QueryPlanPtr> queryBatch = queryCatalog->getQueriesToSchedule();
+        if(queryBatch.empty()){
+            //process the queries using a-query-at-a-time model
+        }
+    }
+}
+
+}
