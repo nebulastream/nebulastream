2ef9e61dff67e2d25285d6e1df8ff440b96f4157
NES-CI <nebulastream@dima.tu-berlin.de>
2020-07-10 14:14:13 +0000

    GIT-CI: Updating NES version to 0.0.62

--- src/Util/SensorBus.cpp
+++ src/Util/SensorBus.cpp
@@ -14,7 +14,7 @@ namespace NES {
  * @param buffer the buffer to fill or read from
  * @return output of ioctl (less than zero in cases of failure)
  */
-int SensorBus::rawI2CRdrw(int file, uint8_t address, uint8_t readWrite, uint8_t size, unsigned char *buffer) {
+int SensorBus::rawI2CRdrw(int file, uint8_t address, uint8_t readWrite, uint8_t size, unsigned char* buffer) {
     // envelope data for ioctl
     struct i2c_smbus_ioctl_data ioctlData;
     // the actual data to send over i2c
@@ -23,7 +23,7 @@ int SensorBus::rawI2CRdrw(int file, uint8_t address, uint8_t readWrite, uint8_t 
     // the return status
     int returnStatus;
 
-    if(size > I2C_SMBUS_BLOCK_MAX) {
+    if (size > I2C_SMBUS_BLOCK_MAX) {
         // TODO: replace with NES_ERROR
         return -1;
     }
@@ -33,16 +33,16 @@ int SensorBus::rawI2CRdrw(int file, uint8_t address, uint8_t readWrite, uint8_t 
     // (See i2c_smbus_xfer_emulated CASE:I2C_SMBUS_I2C_BLOCK_DATA)
     smbusData.block[0] = size;
 
-    if(readWrite != I2C_SMBUS_READ) {
+    if (readWrite != I2C_SMBUS_READ) {
         // prepare for reading
         memcpy(&smbusData.block[1], buffer, size);
     }
 
     // prepare ioctlData
-    ioctlData.read_write = readWrite; // type of operation
-    ioctlData.command = address; // chip address to read
+    ioctlData.read_write = readWrite;// type of operation
+    ioctlData.command = address;     // chip address to read
     ioctlData.size = I2C_SMBUS_I2C_BLOCK_DATA;
-    ioctlData.data = &smbusData; // buffer with data or buffer to fill (depends on type of rw)
+    ioctlData.data = &smbusData;// buffer with data or buffer to fill (depends on type of rw)
 
     returnStatus = ioctl(file, I2C_SMBUS, &ioctlData);
     if (returnStatus < 0) {
@@ -59,17 +59,17 @@ int SensorBus::rawI2CRdrw(int file, uint8_t address, uint8_t readWrite, uint8_t 
     return returnStatus;
 }
 
-bool SensorBus::read(int file, int address, int size, unsigned char *buffer) {
+bool SensorBus::read(int file, int address, int size, unsigned char* buffer) {
     // less that zero indicates errors
     return !(SensorBus::rawI2CRdrw(file, address, I2C_SMBUS_READ, size, buffer) < 0);
 }
 
-bool SensorBus::write(int file, int address, int size, unsigned char *buffer) {
+bool SensorBus::write(int file, int address, int size, unsigned char* buffer) {
     // less that zero indicates errors
     return !(SensorBus::rawI2CRdrw(file, address, I2C_SMBUS_WRITE, size, buffer) < 0);
 }
 
-bool SensorBus::initBus(int& file, const char *filename, int address) {
+bool SensorBus::initBus(int& file, const char* filename, int address) {
     if ((file = open(filename, O_RDWR)) < 0) {
         // Failed to open bus
         // TODO: use proper logging
