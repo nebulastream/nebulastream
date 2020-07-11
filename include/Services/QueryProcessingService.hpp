1cba7d3644abf08e6e83b2b093ef45d41cf19017
Ankit Chaudhary <ankit.chaudhary@tu-berlin.de>
2020-07-11 23:56:18 +0200

    #810: refactor query service to accept requests for stopping the queries and starting the queries

--- include/Services/QueryProcessingService.hpp
+++ include/Services/QueryProcessingService.hpp
@@ -14,19 +14,18 @@ typedef std::shared_ptr<TypeInferencePhase> TypeInferencePhasePtr;
 class StreamCatalog;
 typedef std::shared_ptr<StreamCatalog> StreamCatalogPtr;
 
-class QueryDeployer;
-typedef std::shared_ptr<QueryDeployer> QueryDeployerPtr;
-
-class WorkerRPCClient;
-typedef std::shared_ptr<WorkerRPCClient> WorkerRPCClientPtr;
-
 class QueryService;
 typedef std::shared_ptr<QueryService> QueryServicePtr;
 
+class GlobalExecutionPlan;
+typedef std::shared_ptr<GlobalExecutionPlan> GlobalExecutionPlanPtr;
+
+class NESTopologyPlan;
+typedef std::shared_ptr<NESTopologyPlan> NESTopologyPlanPtr;
+
 class QueryProcessingService {
   public:
-    explicit QueryProcessingService(GlobalExecutionPlanPtr globalExecutionPlan, NESTopologyPlanPtr nesTopologyPlan, QueryCatalogPtr queryCatalog,
-                                      StreamCatalogPtr streamCatalog, QueryDeployerPtr queryDeployer, WorkerRPCClientPtr workerRPCClient);
+    explicit QueryProcessingService(GlobalExecutionPlanPtr globalExecutionPlan, NESTopologyPlanPtr nesTopologyPlan, QueryCatalogPtr queryCatalog, StreamCatalogPtr streamCatalog);
     int operator()();
 
     /**
@@ -36,15 +35,12 @@ class QueryProcessingService {
     bool isQueryProcessorRunning() const;
 
   private:
-
     bool queryProcessorRunning = true;
     GlobalExecutionPlanPtr globalExecutionPlan;
     QueryCatalogPtr queryCatalog;
     TypeInferencePhasePtr typeInferencePhase;
     QueryRewritePhasePtr queryRewritePhase;
     QueryPlacementPhasePtr queryPlacementPhase;
-    QueryDeployerPtr queryDeployer;
-    WorkerRPCClientPtr workerRPCClient;
     QueryServicePtr queryService;
 };
 }// namespace NES
