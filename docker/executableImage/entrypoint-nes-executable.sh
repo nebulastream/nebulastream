d24c926f8c71f1e9fa1b9a24077018476c0a5b98
eleicha <laura.mons@web.de>
2020-07-16 11:14:02 +0200

    #842 changes to enable coninous release

--- docker/executableImage/entrypoint-nes-executable.sh
+++ docker/executableImage/entrypoint-nes-executable.sh
@@ -2,9 +2,9 @@
 
 if [ $# -eq 0 ]
 then
-    /opt/local/nebula-stream/nesCoordinator --rest_host=0.0.0.0 --rest_port=8081 --actor_port=12346 &
+    /opt/local/nebula-stream/nesCoordinator --serverIp=0.0.0.0 --restPort=8081 --coordinatorPort=12346 &
     sleep 5s
-    /opt/local/nebula-stream/nesWorker --actor_port=12346 --sourceType=CSVSource --sourceConfig=/opt/local/nebula-stream/exdra.csv --numberOfBuffersToProduce=100 --sourceFrequency=1 --physicalStreamName=test_stream --logicalStreamName=exdra
+    /opt/local/nebula-stream/nesWorker --coordinatorPort=12346 --sourceType=CSVSource --sourceConfig=/opt/local/nebula-stream/exdra.csv --numberOfBuffersToProduce=100 --sourceFrequency=1 --physicalStreamName=test_stream --logicalStreamName=exdra
 else
     exec $@
 fi
---
+++ docker/executableImage/entrypoint-prepare-nes-package.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+[[ ! -f /nebulastream/CMakeLists.txt ]] && echo "Please mount source code at /nebulastream point. Run [docker run -v <path-to-nes>:/nebulastream -d <nes-image>]" && exit 1
+
+if [ $# -eq 0 ]
+then
+    #cd /home/eleicha/Documents/DFKI/Code/nebulastream/build
+    mkdir -p /nebulastream/build
+    cd /nebulastream/build
+    export CC=/usr/bin/clang
+    export CXX=/usr/bin/clang++
+    if test -f *.deb; then
+        rm *.deb
+    fi
+    cmake -DCMAKE_BUILD_TYPE=Release -DBoost_NO_SYSTEM_PATHS=TRUE -DBoost_INCLUDE_DIR="/usr/include" -DBoost_LIBRARY_DIR="/usr/lib/x86_64-linux-gnu" -DCPPRESTSDK_DIR="/usr/lib/x86_64-linux-gnu/cmake/" -DCMAKE_CXX_FLAGS_RELEASE:STRING=" -Wno-unused-variable -Wno-unused-parameter -Wno-ignored-qualifiers -Wno-sign-compare " ..
+    make -j4
+    cpack
+    cd ..
+    if test -f *.deb; then 
+        rm docker/executableImage/resources/*.deb
+    fi
+    cp build/*.deb docker/executableImage/resources
+    result=$?
+    rm -rf /nebulastream/build
+    exit $result
+else
+    exec $@
+fi
