7179ff33ac8dc6e0b0b71a7cc77292e57249dcb5
Ankit Chaudhary <ankit.chaudhary@tu-berlin.de>
2020-07-22 16:59:34 +0200

    #810: added query request queue

--- include/WorkQueue/QueryRequestQueue.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef NES_QUERYREQUESTQUEUE_HPP
-#define NES_QUERYREQUESTQUEUE_HPP
-
-namespace NES {
-class QueryRequestQueue {
-
-};
-}// namespace NES
-#endif//NES_QUERYREQUESTQUEUE_HPP
---
+++ include/WorkQueues/QueryRequestQueue.hpp
@@ -0,0 +1,63 @@
+#ifndef NES_QUERYREQUESTQUEUE_HPP
+#define NES_QUERYREQUESTQUEUE_HPP
+
+#include <iostream>
+#include <memory>
+#include <vector>
+
+namespace NES {
+
+class QueryRequestQueue;
+typedef std::shared_ptr<QueryRequestQueue> QueryRequestQueuePtr;
+
+class QueryCatalogEntry;
+typedef std::shared_ptr<QueryCatalogEntry> QueryCatalogEntryPtr;
+
+class QueryRequestQueue {
+
+  public:
+
+    QueryRequestQueuePtr create();
+
+    /**
+     * @brief Add query request into processing queue
+     * @param queryCatalogEntry: the query request in form of query catalog entry
+     * @return true if successfully added to the queue
+     */
+    bool add(QueryCatalogEntry queryCatalogEntry);
+
+    /**
+     * @brief Get a batch of query catalog entries to be processed.
+     * Note: This method returns only a copy of the
+     * @return a vector of query catalog entry to schedule
+     */
+    std::vector<QueryCatalogEntry> getNextBatch();
+
+    /**
+     * @brief Check if there are new request available
+     * @return true if there are new requests
+     */
+    bool isNewRequestAvailable() const;
+
+    /**
+     * @brief Change status of new request availability
+     * @param newRequestAvailable: bool indicating if the request is available
+     */
+    void setNewRequestAvailable(bool newRequestAvailable);
+
+    /**
+     * @brief This method will force trigger the availabilityTrigger conditional variable in order to
+     * interrupt the wait.
+     */
+    void insertPoisonPill();
+
+  private:
+    explicit QueryRequestQueue();
+
+    std::mutex queryRequest;
+    std::condition_variable availabilityTrigger;
+    bool newRequestAvailable;
+    std::deque<QueryCatalogEntryPtr> schedulingQueue;
+};
+}// namespace NES
+#endif//NES_QUERYREQUESTQUEUE_HPP
