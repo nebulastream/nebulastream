dfd082902f023ace5070cfcf08353d97b4028577
zeuchste <steffen.zeuch@dfki.de>
2020-07-23 14:42:02 +0200

    version

--- include/Version/version.hpp
+++ include/Version/version.hpp
@@ -2,6 +2,11 @@
 #define __VERSION_HPP__
 #define NES_VERSION_MAJOR 0
 #define NES_VERSION_MINOR 0
+<<<<<<< HEAD
 #define NES_VERSION_PATCH 74
 constexpr char const* NES_VERSION = "0.0.74";
+=======
+#define NES_VERSION_PATCH 71
+constexpr char const* NES_VERSION      = "0.0.71-7ac48ce0-SNAPSHOT";
+>>>>>>> version
 #endif
