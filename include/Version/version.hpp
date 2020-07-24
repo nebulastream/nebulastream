5462affc4c3af38ec3c43f56147a5e5eea11efc2
Ankit Chaudhary <ankit.chaudhary@tu-berlin.de>
2020-07-24 13:30:14 +0200

    #842: updated the version info

--- include/Version/version.hpp
+++ include/Version/version.hpp
@@ -2,6 +2,6 @@
 #define __VERSION_HPP__
 #define NES_VERSION_MAJOR 0
 #define NES_VERSION_MINOR 0
-#define NES_VERSION_PATCH 76
-constexpr char const* NES_VERSION = "0.0.76";
+#define NES_VERSION_PATCH 77
+constexpr char const* NES_VERSION      = "0.0.77-7b231374-SNAPSHOT";
 #endif
