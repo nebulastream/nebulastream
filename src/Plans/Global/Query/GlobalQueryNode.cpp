50f916d2675a4d732adf56d95666e21ef860ca04
Ankit Chaudhary <ankit.chaudhary@tu-berlin.de>
2020-07-29 19:50:49 +0200

    #735: added method to retreive nodes from global query plan

--- src/Plans/Global/Query/GlobalQueryNode.cpp
+++ src/Plans/Global/Query/GlobalQueryNode.cpp
@@ -1,1 +1,16 @@
+#include <Nodes/Operators/OperatorNode.hpp>
 #include <Plans/Global/Query/GlobalQueryNode.hpp>
+
+namespace NES {
+
+template<class NodeType>
+void GlobalQueryNode::getNodesWithTypeHelper(std::vector<GlobalQueryNodePtr>& foundNodes) {
+    if (logicalOperators[0]->instanceOf<NodeType>()) {
+        foundNodes.push_back(std::shared_ptr<GlobalQueryNode>(this));
+    }
+    for (auto& successor : this->children) {
+        successor->as<GlobalQueryNode>()->getNodesWithTypeHelper<GlobalQueryNode>(foundNodes);
+    }
+}
+
+}// namespace NES
