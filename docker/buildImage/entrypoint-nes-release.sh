6cd1e15fb38cb49ce09d4246ed79cdc93dc20eca
Ankit Chaudhary <ankit.chaudhary@tu-berlin.de>
2020-07-30 11:19:10 +0200

    #885: removed implicit version update and changed the version target command to update the version file

--- docker/buildImage/entrypoint-nes-release.sh
+++ docker/buildImage/entrypoint-nes-release.sh
@@ -16,5 +16,6 @@ git config --local core.sshcommand "/usr/bin/ssh -i \"~/.ssh/id_rsa\" -o \"UserK
 mkdir -p /nebulastream/build
 cd /nebulastream/build
 cmake -DCMAKE_BUILD_TYPE=Release -DBoost_NO_SYSTEM_PATHS=TRUE -DBoost_INCLUDE_DIR="/usr/include" -DBoost_LIBRARY_DIR="/usr/lib/x86_64-linux-gnu" -DCPPRESTSDK_DIR="/usr/lib/x86_64-linux-gnu/cmake/" -DCMAKE_CXX_FLAGS_RELEASE:STRING="-w -Wno-unused-variable -Wno-unused-parameter -Wno-ignored-qualifiers -Wno-sign-compare " ..
+make version
 make format
 make release
