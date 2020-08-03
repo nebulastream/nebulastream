4af05a49b6e877412a1f7cf9b3be5bd61c75849c
Nils Schubert <nilslpschubert@gmail.com>
2020-08-03 18:16:12 +0200

    Integrated google benchmark into NES by creating benachmark folder and writing CMakeLists.txt similar to tests/CMakeLists.txt.

---
+++ benchmark/BenchmarkSuite/SandBoxBenchmark.h
@@ -0,0 +1,15 @@
+//
+// Created by nils on 8/3/20.
+//
+
+#ifndef NES_BENCHMARK_SANDBOXBENCHMARK_H_
+#define NES_BENCHMARK_SANDBOXBENCHMARK_H_
+
+class SandBoxBenchmark {
+};
+
+
+void run_benchmark(benchmark::State& state);
+
+
+#endif//NES_BENCHMARK_SANDBOXBENCHMARK_H_
