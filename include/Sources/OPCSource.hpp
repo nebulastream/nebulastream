bfc40baa6eb9f50f457988c5cf7578cbef55f2f1
eleicha <laura.mons@web.de>
2020-08-06 10:36:38 +0200

    OPCSource first version

---
+++ include/Sources/OPCSource.hpp
@@ -0,0 +1,137 @@
+//#ifndef OPCSOURCE_HPP
+//#define OPCSOURCE_HPP
+
+#include <cstdint>
+#include <memory>
+#include <string>
+#include <open62541/client_config_default.h>
+#include <open62541/client_highlevel.h>
+#include <open62541/plugin/log_stdout.h>
+
+#include <Sources/DataSource.hpp>
+
+namespace NES {
+class TupleBuffer;
+/**
+ * @brief this class provide a zmq as data source
+ */
+class OPCSource : public DataSource {
+
+  public:
+    /**
+     * @brief constructor for the zmq source
+     * @param schema of the input buffer
+     * @param host name of the source queue
+     * @param port of the source queue
+     */
+    OPCSource(SchemaPtr schema,
+        BufferManagerPtr bufferManager,
+        QueryManagerPtr queryManager,
+        const std::string& url,
+        UA_UInt16 nsIndex,
+        char *nsId);
+
+    OPCSource(SchemaPtr schema,
+        BufferManagerPtr bufferManager,
+        QueryManagerPtr queryManager,
+        const std::string& url,
+        UA_UInt16 nsIndex,
+        char *nsId,
+        const std::string& user,
+        const std::string& password);
+    
+    //ToDo: Implement historical read.
+    
+    /*OPCSource(SchemaPtr schema,
+        BufferManagerPtr bufferManager,
+        QueryManagerPtr queryManager,
+        const std::string& url,
+        UA_Int64 startTime);
+
+    OPCSource(SchemaPtr schema,
+        BufferManagerPtr bufferManager,
+        QueryManagerPtr queryManager,
+        const std::string& url,
+        const std::string& user,
+        const std::string& password,
+        UA_Int64 startTime);*/
+    /**
+     * @brief destructor of OPC sink that disconnects the queue before deconstruction
+     * @note if queue cannot be disconnected, an assertion is raised
+     */
+    ~OPCSource();
+
+    /**
+     * @brief blocking method to receive a buffer from the OPC source
+     * @return TupleBufferPtr containing thre received buffer
+     */
+    std::optional<TupleBuffer> receiveData() override;
+
+    /**
+     * @brief override the toString method for the zmq source
+     * @return returns string describing the zmq source
+     */
+    const std::string toString() const override;
+
+
+    /**
+     * @brief The port of the ZMQ
+     * @return ZMQ port
+     */
+    const std::string& getUrl() const;
+
+    UA_UInt16 getNsIndex() const;
+
+    char getNsId() const;
+
+    const std::string& getUser() const;
+
+    const std::string& getPassword() const;
+
+    UA_StatusCode getRetval() const;
+
+    UA_Client getClient() const;
+
+    bool getConnected() const;
+
+    /**
+     * @brief Get source type
+     */
+    SourceType getType() const override;
+
+  private:
+    /**
+     * @brief default constructor required for boost serialization
+     */
+    OPCSource();
+
+    /**
+     * @brief method to connect zmq using the host and port specified before
+     * check if already connected, if not connect try to connect, if already connected return
+     * @return bool indicating if connection could be established
+     */
+    bool connect();
+
+    bool disconnected();
+
+    /**
+     * @brief method for serialization, all listed variable below are added to the
+     * serialization/deserialization process
+     */
+    friend class DataSource;
+    std::string url;
+    UA_UInt16 nsIndex;
+    char *nsId;
+    std::string& user;
+    std::string& password;
+    UA_StatusCode retval;
+    UA_Client *client;
+    bool connected;
+    bool withPwd;
+
+};
+
+typedef std::shared_ptr<OPCSource> OPCSourcePtr;
+}// namespace NES
+
+//#endif// OPCSOURCE_HPP
---
+++ src/Sources/OPCSource.cpp
@@ -0,0 +1,176 @@
+//#ifdef ENABLE_OPC_BUILD
+#include <Sources/OPCSource.hpp>
+#include <open62541/client_config_default.h>
+#include <open62541/client_highlevel.h>
+#include <open62541/plugin/log_stdout.h>
+
+#include <cassert>
+#include <cstdint>
+#include <cstring>
+#include <memory>
+#include <sstream>
+#include <string>
+
+#include <NodeEngine/QueryManager.hpp>
+#include <NodeEngine/BufferManager.hpp>
+#include <Util/Logger.hpp>
+
+namespace NES {
+
+OPCSource::OPCSource(SchemaPtr schema, BufferManagerPtr bufferManager, QueryManagerPtr queryManager, const std::string& url, UA_UInt16 nsIndex, char *nsId)
+    : DataSource(schema, bufferManager, queryManager), 
+    client(UA_Client_new()),
+    UA_ClientConfig_setDefault(UA_Client_getConfig(client)),
+    retval(UA_StatusCode_Good),
+    connected(false),
+    withPwd(false){
+
+    NES_DEBUG(
+        "OPCSOURCE  " << this << ": Init OPC Source to " << url << " without user and password.");
+}
+
+OPCSource::OPCSource(SchemaPtr schema, BufferManagerPtr bufferManager, QueryManagerPtr queryManager, const std::string& url, UA_UInt16 nsIndex, char *nsId, const std::string& user, const std::string& password)
+    : DataSource(schema, bufferManager, queryManager), 
+    client(UA_Client_new()),
+    UA_ClientConfig_setDefault(UA_Client_getConfig(client)),
+    retval(UA_STATUSCODE_GOOD),
+    connected(false),
+    withPwd(true),
+    user(user),
+    password(password){
+
+    NES_DEBUG(
+        "OPCSOURCE  " << this << ": Init OPC Source to " << url << " with user and password.");
+}
+
+OPCSource::~OPCSource() {
+    NES_DEBUG("OPCSource::~OPCSource()");
+    bool success = NES::OPCSource::disconnect();
+    if (success) {
+        NES_DEBUG("OPCSOURCE  " << this << ": Destroy OPC Source");
+    } else {
+        NES_ERROR("OPCSOURCE  " << this << ": Destroy OPC Source failed cause it could not be disconnected");
+        assert(0);
+    }
+    NES_DEBUG("OPCSOURCE  " << this << ": Destroy OPC Source");
+}
+
+std::optional<TupleBuffer> OPCSource::receiveData() {
+    NES_DEBUG("OPCSource  " << this << ": receiveData ");
+    if (connect()) {
+
+        UA_Variant *val = UA_Variant_new();
+
+        retval = UA_Client_readValueAttribute(client, UA_NODEID_STRING(nsIndex, *nsId), val);
+        auto buffer = bufferManager->getBufferBlocking();
+        NES_DEBUG("OPCSource  " << this << ": got buffer ");
+
+        if(retval == UA_STATUSCODE_GOOD) {
+
+            std::memcpy(buffer.getBuffer(), *(UA_Int32*)val->data, buffer.getBufferSize());
+            buffer.setNumberOfTuples(1);
+            UA_Variant_delete(val);
+
+            return buffer;
+        }else{
+            NES_ERROR("OPCSOURCE error: Could not retrieve data. Further inspection needed.");
+            return std::nullopt;
+        }
+    } else {
+        NES_ERROR("OPCSOURCE: Not connected!");
+        return std::nullopt;
+    }
+}
+
+const std::string OPCSource::toString() const {
+    std::stringstream ss;
+    ss << "OPC_SOURCE(";
+    ss << "SCHEMA(" << schema->toString() << "), ";
+    ss << "URL= " << url << ", ";
+    ss << "NODE_INDEX= " << nsIndex << ", ";
+    ss << "NODE_ID= " << nsId << ". ";
+    return ss.str();
+}
+
+bool OPCSource::connect() {
+
+    if (!connected) {
+        NES_DEBUG("OPCSOURCE was !conncect now connect " << this << ": connected");
+        if (withPwd == false)
+        {
+            retval = UA_Client_connect(client, url);
+        }else{
+
+            retval = UA_Client_connect_username(client, url, user, password);
+        }
+        NES_DEBUG("OPCSOURCE use address " << url);
+
+        if (retval != UA_STATUSCODE_GOOD)
+        {
+            UA_Client_delete(client);
+            connected = false;
+            NES_ERROR("OPCSOURCE ERROR with Status Code: " << retval);
+            NES_DEBUG("OPCSOURCE " << this << ": set connected false");
+        }
+    }
+
+    if (connected) {
+        NES_DEBUG("OPCSOURCE  " << this << ": connected");
+    } else {
+        NES_DEBUG("Exception: OPCSOURCE  " << this << ": NOT connected");
+    }
+    return connected;
+}
+
+bool OPCSource::disconnect() {
+    NES_DEBUG("OPCSource::disconnect() connected=" << connected);
+    if (connected) {
+
+        UA_Client_disconnect(client);
+        UA_Client_delete(client);
+        connected = false;
+    }
+    if (!connected) {
+        NES_DEBUG("OPCSOURCE  " << this << ": disconnected");
+    } else {
+        NES_DEBUG("OPCSOURCE  " << this << ": NOT disconnected");
+    }
+    return !connected;
+}
+
+SourceType OPCSource::getType() const { return OPC_SOURCE; }
+
+const std::string& OPCSource::getUrl() const {
+    return url;
+}
+
+UA_UInt16 OPCSource::getNsIndex() const {
+    return nsIndex;
+}
+
+char OPCSource::getNsId() const {
+    return nsId;
+}
+
+const std::string OPCSource::getUser() const {
+    return user;
+}
+
+const std::string OPCSource::getPassword() const {
+    return password;
+}
+
+UA_StatusCode OPCSource::getRetval() const {
+    return retval;
+}
+
+UA_Client OPCSource::getClient() const {
+    return client;
+}
+
+bool OPCSource::getConnected const {
+    return connected;
+}
+
+}// namespace NES
+//#endif
