32ef8cff96530e70600304a741d3fc3b0d010524
Ankit Chaudhary <ankit.chaudhary@tu-berlin.de>
2020-08-13 14:35:37 +0200

    #777: change query id to integer

--- src/QueryCompiler/GeneratedQueryExecutionPlanBuilder.cpp
+++ src/QueryCompiler/GeneratedQueryExecutionPlanBuilder.cpp
@@ -10,7 +10,7 @@ GeneratedQueryExecutionPlanBuilder::GeneratedQueryExecutionPlanBuilder() {
     setBufferManager(nullptr);
     setQueryManager(nullptr);
     setCompiler(nullptr);
-    setQueryId(-1);
+    setQueryId(0);
 }
 
 GeneratedQueryExecutionPlanBuilder GeneratedQueryExecutionPlanBuilder::create() {
@@ -49,7 +49,7 @@ QueryExecutionPlanPtr GeneratedQueryExecutionPlanBuilder::build() {
     NES_ASSERT(queryManager, "GeneratedQueryExecutionPlanBuilder: Invalid queryManager");
     NES_ASSERT(!sources.empty(), "GeneratedQueryExecutionPlanBuilder: Invalid number of sources");
     NES_ASSERT(!sinks.empty(), "GeneratedQueryExecutionPlanBuilder: Invalid number of sinks");
-    NES_ASSERT(queryId != -1, "GeneratedQueryExecutionPlanBuilder: Invalid compiler");
+    NES_ASSERT(queryId == 0, "GeneratedQueryExecutionPlanBuilder: Invalid compiler");
     NES_ASSERT(queryCompiler, "GeneratedQueryExecutionPlanBuilder: Invalid compiler or no stages");
 
     if (stages.empty() && !leaves.empty()) {
