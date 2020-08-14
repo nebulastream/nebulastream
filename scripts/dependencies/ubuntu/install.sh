5f9f94e6dc080e3d919aaf295b4ae945f000adaa
x.chatziliadis <x.chatziliadis@tu-berlin.de>
2020-08-14 12:02:46 +0200

    #931 changing of docker scripts

--- scripts/dependencies/ubuntu/install.sh
+++ scripts/dependencies/ubuntu/install.sh
@@ -11,6 +11,7 @@ sudo apt-get update -qq && sudo apt-get install -qq \
   liblog4cxx-dev \
   libcpprest-dev \
   libssl-dev \
+  libzmqpp-dev \
   clang-format \
   librdkafka1 \
   librdkafka++1 \
@@ -21,13 +22,7 @@ sudo apt-get update -qq && sudo apt-get install -qq \
 
 git clone --branch v1.28.1 https://github.com/grpc/grpc.git && \
   cd grpc && git submodule update --init --jobs 1 && mkdir -p build && cd build && cmake .. -DCMAKE_BUILD_TYPE=Release && \
-  make -j && sudo make install && cd .. && cd .. && rm -rf grpc && \
-  git clone --single-branch --branch v4.3.2 https://github.com/zeromq/libzmq.git && cd libzmq && \
-  mkdir -p build && cd build && cmake .. -DCMAKE_BUILD_TYPE=Release && make -j && sudo make install && \
-  cd .. && cd .. && rm -rf libzmq && \
-  git clone --single-branch --branch v4.6.0 https://github.com/zeromq/cppzmq.git && cd cppzmq && \
-  mkdir -p build && cd build && cmake .. -DCMAKE_BUILD_TYPE=Release && make -j && sudo make install && \
-  cd .. && cd .. && rm -rf cppzmq
+  make -j && sudo make install && cd .. && cd .. && rm -rf grpc
 
 
 
