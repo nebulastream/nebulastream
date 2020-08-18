cdab8e69507d760077df77d33d1b5b5fb7c4b1aa
Ankit Chaudhary <ankit.chaudhary@tu-berlin.de>
2020-08-18 14:37:09 +0200

    #777: inverted query id to unsigned integer

--- include/API/QueryId.hpp
+++ include/API/QueryId.hpp
@@ -6,7 +6,7 @@
 /**
  * @brief this alias represent a locally running query execution plan
  */
-using QueryId = int64_t;
+using QueryId = uint64_t;
 static constexpr uint64_t INVALID_QUERY_ID = 0;
 
 #endif//NES_INCLUDE_API_QUERYID_HPP_
