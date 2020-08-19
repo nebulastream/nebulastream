3fd3551de890558a0f1941fe844b9138adb1b155
Xenofon Chatziliadis <x.chatziliadis@tu-berlin.de>
2020-08-19 16:44:43 +0200

    #944 parsing of MemoryMetrics added

--- include/Monitoring/MetricValues/MemoryMetrics.hpp
+++ include/Monitoring/MetricValues/MemoryMetrics.hpp
@@ -17,7 +17,20 @@ class MemoryMetrics {
      * @param prefix
      * @return the schema
      */
-    static std::shared_ptr<Schema> getSchema(const std::string& prefix = "");
+    static std::shared_ptr<Schema> getSchema(const std::string& prefix="");
+
+    /**
+     * @brief Parses a MemoryMetrics objects from a given Schema and TupleBuffer.
+     * @param schema
+     * @param buf
+     * @param prefix
+     * @return The MemoryMetrics object
+     */
+    static MemoryMetrics fromBuffer(std::shared_ptr<Schema> schema, TupleBuffer& buf, const std::string& prefix);
+
+    //equality operators
+    bool operator==(const MemoryMetrics& rhs) const;
+    bool operator!=(const MemoryMetrics& rhs) const;
 
     uint64_t TOTAL_RAM;
     uint64_t TOTAL_SWAP;
@@ -34,8 +47,6 @@ class MemoryMetrics {
     uint64_t LOADS_15MIN;
 };
 
-typedef std::shared_ptr<MemoryMetrics> MemoryMetricsPtr;
-
 /**
  * @brief The serialize method to write MemoryMetrics into the given Schema and TupleBuffer. The prefix specifies a string
  * that should be added before each field description in the Schema.
@@ -46,6 +57,8 @@ typedef std::shared_ptr<MemoryMetrics> MemoryMetricsPtr;
  */
 void serialize(MemoryMetrics metrics, std::shared_ptr<Schema> schema, TupleBuffer& buf, const std::string& prefix);
 
+typedef std::shared_ptr<MemoryMetrics> MemoryMetricsPtr;
 }// namespace NES
+
 
 #endif//NES_INCLUDE_MONITORING_METRICVALUES_MEMORYMETRICS_HPP_
