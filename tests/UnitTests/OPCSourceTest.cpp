1d85008cc02399d1a64d2213cce66da2f73df1df
eleicha <laura.mons@web.de>
2020-08-20 09:41:36 +0200

    #844 renamed OPCTest to OPCSourceTest

--- tests/UnitTests/OPCSourceTest.cpp
+++ tests/UnitTests/OPCSourceTest.cpp
@@ -1,121 +1,70 @@
 #include <gtest/gtest.h>
-#ifdef ENABLE_OPC_BUILD
+//#ifdef ENABLE_OPC_BUILD
 #include <string>
 #include <cstring>
 #include <thread>
-#include <memory>
 #include <NodeEngine/QueryManager.hpp>
 #include <NodeEngine/BufferManager.hpp>
 
-#include <open62541/client_config_default.h>
-#include <open62541/client_highlevel.h>
-#include <open62541/client_subscriptions.h>
-#include <open62541/plugin/log_stdout.h>
-
-#include <API/Schema.hpp>
 #include <Util/Logger.hpp>
+#include <Util/TimeMeasurement.hpp>
+#include <Sources/OPCSource.hpp>
 #include <Sources/SourceCreator.hpp>
 
-#include <NodeEngine/NodeEngine.hpp>
-
-
-const std::string url = "opc.tcp://localhost:4840";
+const std::string url = "opc.tcp://eleicha-ThinkPad-T480:53530/OPCUA/SimulationServer";
 
 namespace NES {
 
 class OPCSourceTest : public testing::Test {
   public:
-
-
-    /* Will be called before any test in this class are executed. */
-    static void SetUpTestCase() {
-        NES_DEBUG("Setup OPCSourceTest test class.");
-    }
-
+    BufferManagerPtr bufferManager;
+    QueryManagerPtr queryManager;
     void SetUp() {
-        NES_DEBUG("Setup OPCSourceTest cases.");
         NES::setupLogging("OPCTest.log", NES::LOG_DEBUG);
 
-        test_schema =
-                Schema::create()
-                        ->addField("var", UINT32);
+        schema = Schema::create()
+            ->addField("nsId", 16)
+            ->addField("ndIndex", 16)
+            ->addField("value", 16)
+
+        uint64_t tuple_size = schema->getSchemaSizeInBytes();
+        buffer_size = num_tuples_to_process*tuple_size/num_of_buffers;
+
         bufferManager = std::make_shared<BufferManager>();
         queryManager = std::make_shared<QueryManager>();
 
-        bufferManager->configure(sizeof(uint32_t),1);
-
         ASSERT_GT(buffer_size, 0);
 
+        NES_DEBUG("Setup OPCTest");
     }
-
-    /* Will be called after a test is executed. */
     void TearDown() {
-        NES_DEBUG("Tear down OPCSourceTest");
+        NES_DEBUG("Tear down OPCTest");
     }
-
-    /* Will be called after all tests in this class are finished. */
-    static void TearDownTestCase() {
-        NES_DEBUG("Tear down OPCSourceTest test class." );
-    }
-
-    BufferManagerPtr bufferManager;
-    QueryManagerPtr queryManager;
-    size_t tupleCnt;
-    SchemaPtr test_schema;
-    size_t tuple_size;
-    size_t buffer_size;
 
   protected:
-    UA_NodeId nodeId = UA_NODEID_STRING(1, "the.answer");
-    const std::string& user = "";
-    const std::string& password = "";
+    const std::string url = std::string(url);
+    UA_UInt16 nsIndex = 3;
+    char *nsId = "h1";
 
-
-    const size_t num_of_buffers = 1;
-    const uint64_t num_tuples_to_process = 1;
+    const size_t num_of_buffers = 5;
+    const uint64_t num_tuples_to_process = 100;
+    size_t buffer_size;
+    SchemaPtr schema;
 
 };
 
-TEST_F(OPCSourceTest, OPCSourceInit) {
+TEST_F(OPCTest, OPCSourceInit) {
 
-    auto opcSource = createOPCSource(test_schema, bufferManager, queryManager, url, &nodeId, user, password);
-
+    const DataSourcePtr opcSource = std::make_shared<OPCSource>(schema, bufferManager, queryManager, url, nsIndex, nsId);
     SUCCEED();
 }
 
-TEST_F(OPCSourceTest, OPCSourcePrint) {
+TEST_F(OPCTest, OPCSourceInit) {
 
-    auto opcSource = createOPCSource(test_schema, bufferManager, queryManager, url, &nodeId, user, password);
-
-    std::string expected = "OPC_SOURCE(SCHEMA(var:INTEGER ), URL= opc.tcp://localhost:4840, NODE_INDEX= 3. ";
-
-    EXPECT_EQ(opcSource->toString(), expected);
-
-    std::cout << opcSource->toString() << std::endl;
-
+    const DataSourcePtr opcSource = std::make_shared<OPCSource>(schema, bufferManager, queryManager, url, nsIndex, nsId, user, password);
     SUCCEED();
 }
 
 
-TEST_F(OPCSourceTest, OPCSourceValue) {
-
-    auto test_schema = Schema::create()
-                ->addField("var", UINT32);
-
-    auto opcSource = createOPCSource(test_schema, bufferManager, queryManager, url, &nodeId, user, password);
-
-    auto tuple_buffer = opcSource->receiveData();
-
-    size_t sum = 0;
-    uint32_t *tuple = (uint32_t *) tuple_buffer->getBuffer();
-
-    sum = *tuple;
-    size_t expected = 43;
-
-    EXPECT_EQ(sum, expected);
-
 }
-
-
-}
-#endif
+//#endif
