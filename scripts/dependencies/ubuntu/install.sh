8a30c1f967c8efa51e35c39701c9c1ce18cbfb0b
eleicha <laura.mons@web.de>
2020-09-02 15:19:59 +0200

    #844 added open62541 dependencies to docker image and install.sh script

--- scripts/dependencies/ubuntu/install.sh
+++ scripts/dependencies/ubuntu/install.sh
@@ -20,6 +20,10 @@ sudo apt-get update -qq && sudo apt-get install -qq \
   wget \
   tar
 
+sudo add-apt-repository ppa:open62541-team/ppa -qq && \
+  sudo apt-get update && \
+  sudo apt-get install libopen62541-1-dev -qq
+
 git clone --branch v1.28.1 https://github.com/grpc/grpc.git && \
   cd grpc && git submodule update --init --jobs 1 && mkdir -p build && cd build && cmake .. -DCMAKE_BUILD_TYPE=Release && \
   make -j && sudo make install && cd .. && cd .. && rm -rf grpc
