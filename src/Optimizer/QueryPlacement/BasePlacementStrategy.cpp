14f85a4e1d7cc87490df948fec924451c0e5f8d7
Ankit Chaudhary <ankit.chaudhary@tu-berlin.de>
2020-09-02 13:15:31 +0200

    #750 : refactored base placement strategy

--- src/Optimizer/QueryPlacement/BasePlacementStrategy.cpp
+++ src/Optimizer/QueryPlacement/BasePlacementStrategy.cpp
@@ -143,28 +143,6 @@ TopologyNodePtr BasePlacementStrategy::getTopologyNodeForPinnedOperator(uint64_t
     return candidateTopologyNode;
 }
 
-uint64_t BasePlacementStrategy::addNetworkSinkOperator(QueryPlanPtr queryPlan, TopologyNodePtr parentNesNode) {
-
-    NES_DEBUG("BasePlacementStrategy: found no existing network source operator in the parent execution node");
-    QueryId queryId = queryPlan->getQueryId();
-
-    uint64_t operatorId = UtilityFunctions::getNextOperatorId();
-    uint64_t nextNetworkSourceOperatorId = UtilityFunctions::getNextOperatorId();
-    Network::NodeLocation nodeLocation(parentNesNode->getId(), parentNesNode->getIpAddress(), parentNesNode->getDataPort());
-    Network::NesPartition nesPartition(queryId, nextNetworkSourceOperatorId, nextNetworkSourceOperatorId, 0);
-    const OperatorNodePtr sysSinkOperator = createSinkLogicalOperatorNode(Network::NetworkSinkDescriptor::create(nodeLocation, nesPartition, NSINK_RETRY_WAIT, NSINK_RETRIES));
-    sysSinkOperator->setId(operatorId);
-    queryPlan->appendOperator(sysSinkOperator);
-    return nextNetworkSourceOperatorId;
-}
-
-void BasePlacementStrategy::addNetworkSourceOperator(QueryPlanPtr queryPlan, SchemaPtr inputSchema, uint64_t operatorId) {
-    const Network::NesPartition nesPartition = Network::NesPartition(queryPlan->getQueryId(), operatorId, operatorId, 0);
-    const OperatorNodePtr sysSourceOperator = createSourceLogicalOperatorNode(Network::NetworkSourceDescriptor::create(inputSchema, nesPartition));
-    sysSourceOperator->setId(operatorId);
-    queryPlan->appendOperator(sysSourceOperator);
-}
-
 OperatorNodePtr BasePlacementStrategy::createNetworkSinkOperator(QueryId queryId, uint64_t sourceOperatorId, TopologyNodePtr parentNesNode) {
 
     NES_DEBUG("BasePlacementStrategy: found no existing network source operator in the parent execution node");
