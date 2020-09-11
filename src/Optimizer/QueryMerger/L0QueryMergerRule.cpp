a21360db8dabfabf6d64e5867a990efd56246d0c
Ankit Chaudhary <ankit.chaudhary@tu-berlin.de>
2020-09-11 18:03:27 +0200

    #876: refactored GlobalQueryNode to return list of operators and added logic for merging query plans for L0#

--- src/Optimizer/QueryMerger/L0QueryMergerRule.cpp
+++ src/Optimizer/QueryMerger/L0QueryMergerRule.cpp
@@ -1,6 +1,8 @@
+#include <Nodes/Operators/LogicalOperators/Sources/SourceLogicalOperatorNode.hpp>
 #include <Optimizer/QueryMerger/L0QueryMergerRule.hpp>
-#include <Plans/Global/Query/GlobalQueryPlan.hpp>
 #include <Plans/Global/Query/GlobalQueryNode.hpp>
+#include <Plans/Global/Query/GlobalQueryPlan.hpp>
+#include <Plans/Query/QueryPlan.hpp>
 
 namespace NES {
 
@@ -10,9 +12,39 @@ L0QueryMergerRulePtr L0QueryMergerRule::create() {
     return std::make_shared<L0QueryMergerRule>(L0QueryMergerRule());
 }
 
-void L0QueryMergerRule::apply(GlobalQueryPlanPtr globalQueryPlan) {
+void L0QueryMergerRule::apply(std::vector<QueryPlanPtr> queryPlans, GlobalQueryPlanPtr globalQueryPlan) {
     std::vector<GlobalQueryNodePtr> globalQueryNodeWithSourceOperator = globalQueryPlan->getAllGlobalQueryNodesWithOperatorType<SourceLogicalOperatorNode>();
 
+    for (auto& queryPlan : queryPlans) {
+        bool merged = false;
+        OperatorNodePtr sourceOperator = queryPlan->getOperatorByType<SourceLogicalOperatorNode>()[0];
+        std::string targetLogicalSourceName = sourceOperator->as<SourceLogicalOperatorNode>()->getSourceDescriptor()->getStreamName();
+
+        for (auto& globalQuerySourceNode : globalQueryNodeWithSourceOperator) {
+
+            std::vector<OperatorNodePtr> sourceOperators = globalQuerySourceNode->getOperators();
+            if (sourceOperators.empty()) {
+                //throw exception
+            }
+            std::string logicalSourceName = sourceOperators[0]->as<SourceLogicalOperatorNode>()->getSourceDescriptor()->getStreamName();
+
+            if (logicalSourceName == targetLogicalSourceName) {
+                merged = true;
+            }
+
+            if (merged) {
+                break;
+            }
+        }
+        if (!merged) {
+            globalQueryPlan->addQueryPlan(queryPlan);
+            globalQueryNodeWithSourceOperator = globalQueryPlan->getAllGlobalQueryNodesWithOperatorType<SourceLogicalOperatorNode>();
+        }
+    }
 }
+
+bool operatorContainedInGlobalQueryNode(OperatorNodePtr logicalOperator, )
+
+
 
 }// namespace NES
