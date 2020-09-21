8d5a3b7fb7c82ba17bc1fe3322189f2cec40a67c
Xenofon Chatziliadis <x.chatziliadis@tu-berlin.de>
2020-09-21 18:52:36 +0200

    scripts for the emulation now working

--- emulation/images/entrypoint-prom.sh
+++ emulation/images/entrypoint-prom.sh
@@ -2,4 +2,4 @@
 
 /bin/prometheus-node-exporter > prometheus.log 2>&1 &
 
-sleep 20 && exec /entrypoint.sh $@ > nes-prometheus.log 2>&1 &
+sleep 20 && exec /entrypoint.sh $@ > nes-runtime.log 2>&1 &
