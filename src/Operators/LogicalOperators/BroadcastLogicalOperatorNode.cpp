de0c3836aa8e7ba72bd58a25c0adcb812d0c7e78
Ankit Chaudhary <ankit.chaudhary@tu-berlin.de>
2020-09-24 13:16:37 +0200

    #881: added broadcast logical operator

---
+++ src/Operators/LogicalOperators/BroadcastLogicalOperatorNode.cpp
@@ -0,0 +1,44 @@
+#include <API/AttributeField.hpp>
+#include <API/Schema.hpp>
+#include <Nodes/Expressions/FieldAssignmentExpressionNode.hpp>
+#include <Operators/LogicalOperators/BroadcastLogicalOperatorNode.hpp>
+
+namespace NES {
+
+BroadcastLogicalOperatorNodePtr BroadcastLogicalOperatorNode::create() {
+    return std::shared_ptr<BroadcastLogicalOperatorNode>();
+}
+
+BroadcastLogicalOperatorNode::BroadcastLogicalOperatorNode() {}
+
+bool BroadcastLogicalOperatorNode::isIdentical(NodePtr rhs) const {
+    return rhs->as<BroadcastLogicalOperatorNode>()->getId() == id;
+}
+
+bool BroadcastLogicalOperatorNode::equal(const NodePtr rhs) const {
+    if (rhs->instanceOf<BroadcastLogicalOperatorNode>()) {
+        return true;
+    }
+    return false;
+};
+
+bool BroadcastLogicalOperatorNode::inferSchema() {
+    // infer the default input and output schema
+    OperatorNode::inferSchema();
+    return true;
+}
+
+const std::string BroadcastLogicalOperatorNode::toString() const {
+    std::stringstream ss;
+    ss << "BROADCAST(" << outputSchema->toString() << ")";
+    return ss.str();
+}
+
+OperatorNodePtr BroadcastLogicalOperatorNode::copy() {
+    auto copy = create();
+    copy->setId(id);
+    copy->setInputSchema(inputSchema);
+    copy->setOutputSchema(outputSchema);
+    return copy;
+}
+}// namespace NES
