be943e739eee76b7386c9dd9c0e49515c451d673
Philipp Grulich <philippgrulich@hotmail.de>
2020-10-11 11:05:40 +0200

    #1006 update header

--- src/QueryCompiler/CCodeGenerator/FileBuilder.cpp
+++ src/QueryCompiler/CCodeGenerator/FileBuilder.cpp
@@ -10,9 +10,11 @@ FileBuilder FileBuilder::create(const std::string&) {
     FileBuilder builder;
     builder.declations << "#include <cstdint>" << std::endl;
     builder.declations << "#include <string.h>" << std::endl;
+    builder.declations << "#include <State/StateVariable.hpp>" << std::endl;
     builder.declations << "#include <Windowing/WindowDefinition.hpp>" << std::endl;
     builder.declations << "#include <Windowing/Runtime/WindowHandler.hpp>" << std::endl;
     builder.declations << "#include <Windowing/Runtime/WindowManager.hpp>" << std::endl;
+    builder.declations << "#include <Windowing/Runtime/WindowSliceStore.hpp>" << std::endl;
     builder.declations << "#include <NodeEngine/TupleBuffer.hpp>" << std::endl;
     builder.declations << "#include <NodeEngine/WorkerContext.hpp>" << std::endl;
     builder.declations << "#include <QueryCompiler/PipelineExecutionContext.hpp>" << std::endl;
