f6a8fca4362cf6724d19f5c6640d98821d12f51b
Philipp Grulich <philippgrulich@hotmail.de>
2020-10-13 17:27:46 +0200

    #1058 update factory

--- src/QueryCompiler/GeneratableOperators/Windowing/GeneratableWindowOperator.cpp
+++ src/QueryCompiler/GeneratableOperators/Windowing/GeneratableWindowOperator.cpp
@@ -1,7 +1,10 @@
 
 #include <QueryCompiler/GeneratableOperators/Windowing/GeneratableWindowOperator.hpp>
 #include <Windowing/Runtime/WindowHandlerFactory.hpp>
+#include <utility>
 namespace NES {
+
+GeneratableWindowOperator::GeneratableWindowOperator(LogicalWindowDefinitionPtr windowDefinition) : WindowLogicalOperatorNode(std::move(windowDefinition)){}
 
 WindowHandlerPtr GeneratableWindowOperator::createWindowHandler() {
     return WindowHandlerFactory::createWindowHandler(windowDefinition);
