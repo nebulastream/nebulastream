761c8bd7f7879d337e5984f300c1ad0e2cfa7013
Ankit Chaudhary <ankit.chaudhary@tu-berlin.de>
2020-11-01 17:16:00 +0100

    #1091: added mising document and some refactoring for ExpressionToZ3ExprUtil class

---
+++ src/Optimizer/Utils/ExpressionToZ3ExprUtil.cpp
@@ -0,0 +1,128 @@
+#include <Nodes/Expressions/ArithmeticalExpressions/AddExpressionNode.hpp>
+#include <Nodes/Expressions/ArithmeticalExpressions/ArithmeticalExpressionNode.hpp>
+#include <Nodes/Expressions/ArithmeticalExpressions/DivExpressionNode.hpp>
+#include <Nodes/Expressions/ArithmeticalExpressions/MulExpressionNode.hpp>
+#include <Nodes/Expressions/ArithmeticalExpressions/SubExpressionNode.hpp>
+#include <Nodes/Expressions/ConstantValueExpressionNode.hpp>
+#include <Nodes/Expressions/ExpressionNode.hpp>
+#include <Nodes/Expressions/FieldAccessExpressionNode.hpp>
+#include <Nodes/Expressions/FieldAssignmentExpressionNode.hpp>
+#include <Nodes/Expressions/LogicalExpressions/AndExpressionNode.hpp>
+#include <Nodes/Expressions/LogicalExpressions/EqualsExpressionNode.hpp>
+#include <Nodes/Expressions/LogicalExpressions/GreaterEqualsExpressionNode.hpp>
+#include <Nodes/Expressions/LogicalExpressions/GreaterExpressionNode.hpp>
+#include <Nodes/Expressions/LogicalExpressions/LessEqualsExpressionNode.hpp>
+#include <Nodes/Expressions/LogicalExpressions/LessExpressionNode.hpp>
+#include <Nodes/Expressions/LogicalExpressions/LogicalExpressionNode.hpp>
+#include <Nodes/Expressions/LogicalExpressions/NegateExpressionNode.hpp>
+#include <Nodes/Expressions/LogicalExpressions/OrExpressionNode.hpp>
+#include <Optimizer/Utils/DataTypeToZ3ExprUtil.hpp>
+#include <Optimizer/Utils/ExpressionToZ3ExprUtil.hpp>
+#include <z3++.h>
+
+namespace NES {
+
+z3::expr ExpressionToZ3ExprUtil::createForExpression(ExpressionNodePtr expression, z3::context& context) {
+
+    NES_DEBUG("Creating Z3 expression for input expression " << expression->toString());
+    if (expression->instanceOf<LogicalExpressionNode>()) {
+        return createForLogicalExpressions(expression, context);
+    } else if (expression->instanceOf<ArithmeticalExpressionNode>()) {
+        return createForArithmeticalExpressions(expression, context);
+    } else if (expression->instanceOf<ConstantValueExpressionNode>()) {
+        auto constantValueExpression = expression->as<ConstantValueExpressionNode>();
+        auto value = constantValueExpression->getConstantValue();
+        return DataTypeToZ3ExprUtil::createForDataValue(value, context);
+    } else if (expression->instanceOf<FieldAccessExpressionNode>()) {
+        auto fieldAccessExpression = expression->as<FieldAccessExpressionNode>();
+        std::string fieldName = fieldAccessExpression->getFieldName();
+        DataTypePtr fieldType = fieldAccessExpression->getStamp();
+        return DataTypeToZ3ExprUtil::createForField(fieldName, fieldType, context);
+    } else if (expression->instanceOf<FieldAssignmentExpressionNode>()) {
+        auto fieldAssignmentExpressionNode = expression->as<FieldAssignmentExpressionNode>();
+        std::string fieldName = fieldAssignmentExpressionNode->getField()->getFieldName();
+        DataTypePtr fieldType = fieldAssignmentExpressionNode->getField()->getStamp();
+        auto filedExpr = DataTypeToZ3ExprUtil::createForField(fieldName, fieldType, context);
+        auto valueExpr = createForExpression(fieldAssignmentExpressionNode->getAssignment(), context);
+        return to_expr(context, Z3_mk_eq(context, filedExpr, valueExpr));
+    }
+    NES_THROW_RUNTIME_ERROR("ExpressionSerializationUtil: could not serialize this expression: " + expression->toString());
+}
+
+z3::expr ExpressionToZ3ExprUtil::createForArithmeticalExpressions(ExpressionNodePtr expression, z3::context& context) {
+    NES_DEBUG("Create Z3 expression for arithmetical expression " << expression->toString());
+    if (expression->instanceOf<AddExpressionNode>()) {
+        auto addExpressionNode = expression->as<AddExpressionNode>();
+        auto left = createForExpression(addExpressionNode->getLeft(), context);
+        auto right = createForExpression(addExpressionNode->getRight(), context);
+        Z3_ast array[] = {left, right};
+        return to_expr(context, Z3_mk_add(context, 2, array));
+    } else if (expression->instanceOf<SubExpressionNode>()) {
+        auto subExpressionNode = expression->as<SubExpressionNode>();
+        auto left = createForExpression(subExpressionNode->getLeft(), context);
+        auto right = createForExpression(subExpressionNode->getRight(), context);
+        Z3_ast array[] = {left, right};
+        return to_expr(context, Z3_mk_sub(context, 2, array));
+    } else if (expression->instanceOf<MulExpressionNode>()) {
+        auto mulExpressionNode = expression->as<MulExpressionNode>();
+        auto left = createForExpression(mulExpressionNode->getLeft(), context);
+        auto right = createForExpression(mulExpressionNode->getRight(), context);
+        Z3_ast array[] = {left, right};
+        return to_expr(context, Z3_mk_mul(context, 2, array));
+    } else if (expression->instanceOf<DivExpressionNode>()) {
+        auto divExpressionNode = expression->as<DivExpressionNode>();
+        auto left = createForExpression(divExpressionNode->getLeft(), context);
+        auto right = createForExpression(divExpressionNode->getRight(), context);
+        return to_expr(context, Z3_mk_div(context, left, right));
+    }
+    NES_THROW_RUNTIME_ERROR("No conversion to Z3 expression implemented for the arithmetical expression node: " + expression->toString());
+}
+
+z3::expr ExpressionToZ3ExprUtil::createForLogicalExpressions(ExpressionNodePtr expression, z3::context& context) {
+    NES_DEBUG("Create Z3 expression node for logical expression " << expression->toString());
+    if (expression->instanceOf<AndExpressionNode>()) {
+        auto andExpressionNode = expression->as<AndExpressionNode>();
+        auto left = createForExpression(andExpressionNode->getLeft(), context);
+        auto right = createForExpression(andExpressionNode->getRight(), context);
+        Z3_ast array[] = {left, right};
+        return to_expr(context, Z3_mk_and(context, 2, array));
+    } else if (expression->instanceOf<OrExpressionNode>()) {
+        auto orExpressionNode = expression->as<OrExpressionNode>();
+        auto left = createForExpression(orExpressionNode->getLeft(), context);
+        auto right = createForExpression(orExpressionNode->getRight(), context);
+        Z3_ast array[] = {left, right};
+        return to_expr(context, Z3_mk_or(context, 2, array));
+    } else if (expression->instanceOf<LessExpressionNode>()) {
+        auto lessExpressionNode = expression->as<LessExpressionNode>();
+        auto left = createForExpression(lessExpressionNode->getLeft(), context);
+        auto right = createForExpression(lessExpressionNode->getRight(), context);
+        return to_expr(context, Z3_mk_lt(context, left, right));
+    } else if (expression->instanceOf<LessEqualsExpressionNode>()) {
+        auto lessEqualsExpressionNode = expression->as<LessEqualsExpressionNode>();
+        auto left = createForExpression(lessEqualsExpressionNode->getLeft(), context);
+        auto right = createForExpression(lessEqualsExpressionNode->getRight(), context);
+        return to_expr(context, Z3_mk_le(context, left, right));
+    } else if (expression->instanceOf<GreaterExpressionNode>()) {
+        auto greaterExpressionNode = expression->as<GreaterExpressionNode>();
+        auto left = createForExpression(greaterExpressionNode->getLeft(), context);
+        auto right = createForExpression(greaterExpressionNode->getRight(), context);
+        return to_expr(context, Z3_mk_gt(context, left, right));
+    } else if (expression->instanceOf<GreaterEqualsExpressionNode>()) {
+        auto greaterEqualsExpressionNode = expression->as<GreaterEqualsExpressionNode>();
+        auto left = createForExpression(greaterEqualsExpressionNode->getLeft(), context);
+        auto right = createForExpression(greaterEqualsExpressionNode->getRight(), context);
+        return to_expr(context, Z3_mk_ge(context, left, right));
+    } else if (expression->instanceOf<EqualsExpressionNode>()) {
+        auto equalsExpressionNode = expression->as<EqualsExpressionNode>();
+        auto left = createForExpression(equalsExpressionNode->getLeft(), context);
+        auto right = createForExpression(equalsExpressionNode->getRight(), context);
+        return to_expr(context, Z3_mk_eq(context, left, right));
+    } else if (expression->instanceOf<NegateExpressionNode>()) {
+        auto equalsExpressionNode = expression->as<NegateExpressionNode>();
+        auto expr = createForExpression(equalsExpressionNode->child(), context);
+        return to_expr(context, Z3_mk_not(context, expr));
+    }
+    NES_THROW_RUNTIME_ERROR("No conversion to Z3 expression possible for the logical expression node: " + expression->toString());
+}
+
+}// namespace NES
