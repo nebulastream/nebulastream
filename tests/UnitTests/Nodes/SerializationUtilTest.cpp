6348157687f6545cae455c3aec10161af821a099
Xenofon Chatziliadis <x.chatziliadis@tu-berlin.de>
2020-11-04 16:31:13 +0100

    bugfix

--- tests/UnitTests/Nodes/SerializationUtilTest.cpp
+++ tests/UnitTests/Nodes/SerializationUtilTest.cpp
@@ -180,7 +180,7 @@ TEST_F(SerializationUtilTest, sourceDescriptorSerialization) {
     }
 
     {
-        auto source = CsvSourceDescriptor::create(schema, "localhost", ",", 0, 10, 10, true);
+        auto source = CsvSourceDescriptor::create(schema, "localhost", ",", 0, 10, 10, true, false);
         auto serializedSourceDescriptor = OperatorSerializationUtil::serializeSourceSourceDescriptor(source, new SerializableOperator_SourceDetails());
         auto deserializedSourceDescriptor = OperatorSerializationUtil::deserializeSourceDescriptor(serializedSourceDescriptor);
         ASSERT_TRUE(source->equal(deserializedSourceDescriptor));
