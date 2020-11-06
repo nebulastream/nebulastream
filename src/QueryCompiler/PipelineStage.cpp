c684de619f9329d3bee33eecff162cff7e8bc0f9
Dwi Prasetyo Adi Nugroho <dwiprasetyo.an@gmail.com>
2020-11-06 12:34:33 +0100

    #1066: Remove code to obtain watermark from field in pipelinestage

--- src/QueryCompiler/PipelineStage.cpp
+++ src/QueryCompiler/PipelineStage.cpp
@@ -50,54 +50,11 @@ bool PipelineStage::execute(TupleBuffer& inputBuffer, WorkerContextRef workerCon
         if (pipelineContext->getWindowDef()->getWindowType()->getTimeCharacteristic()->getType() == Windowing::TimeCharacteristic::ProcessingTime) {
             NES_DEBUG("Execute Pipeline Stage set processing time watermark from buffer=" << inputBuffer.getWatermark());
             windowHandler->updateAllMaxTs(inputBuffer.getWatermark(), inputBuffer.getOriginId());
-        } else {//Event time
-            auto distType = pipelineContext->getWindowDef()->getDistributionType()->getType();
-            std::string tsFieldName = "";
-            if (distType == Windowing::DistributionCharacteristic::Combining) {
-                NES_DEBUG("PipelineStage: process combining window");
-                tsFieldName = "end";
-                if (!pipelineContext->getInputSchema()->has(tsFieldName)) {
-                    NES_ERROR("PipelineStage::execute: Window Operator: key field " << tsFieldName << " does not exist!");
-                    NES_FATAL_ERROR("PipelineStage type error");
-                }
-            } else if (distType == Windowing::DistributionCharacteristic::Slicing || distType == Windowing::DistributionCharacteristic::Complete) {
-                NES_DEBUG("PipelineStage: process slicing window or complete window");
-                tsFieldName = pipelineContext->getWindowDef()->getWindowType()->getTimeCharacteristic()->getField()->name;
-                if (!pipelineContext->getInputSchema()->has(tsFieldName)) {
-                    NES_ERROR("PipelineStage::execute: Window Operator: key field " << tsFieldName << " does not exist!");
-                    NES_FATAL_ERROR("PipelineStage type error");
-                }
-            } else {
-                NES_DEBUG("PipelineStage: process complete window");
-            }
-
-            auto layout = createRowLayout(std::make_shared<Schema>(pipelineContext->getInputSchema()));
-            auto fields = pipelineContext->getInputSchema()->fields;
-            size_t keyIndex = pipelineContext->getInputSchema()->getIndex(tsFieldName);
-
-            for (uint64_t recordIndex = 0; recordIndex < inputBuffer.getNumberOfTuples(); recordIndex++) {
-                auto dataType = fields[keyIndex]->getDataType();
-                auto physicalType = DefaultPhysicalTypeFactory().getPhysicalType(dataType);
-                auto basicPhysicalType = std::dynamic_pointer_cast<BasicPhysicalType>(physicalType);
-                if (basicPhysicalType->getNativeType() == BasicPhysicalType::INT_64) {
-                    auto val = layout->getValueField<int64_t>(recordIndex, keyIndex)->read(inputBuffer);
-                    maxWaterMark = std::max(maxWaterMark, (size_t) val);
-                    NES_DEBUG("PipelineStage: maxWaterMark=" << maxWaterMark << " or " << (size_t) val << " val=" << val);
-
-                } else if (basicPhysicalType->getNativeType() == BasicPhysicalType::UINT_64) {
-                    auto val = layout->getValueField<uint64_t>(recordIndex, keyIndex)->read(inputBuffer);
-                    maxWaterMark = std::max(maxWaterMark, val);
-                    NES_DEBUG("PipelineStage: maxWaterMark=" << maxWaterMark << " or "
-                                                             << "  val=" << val);
-                } else {
-                    NES_NOT_IMPLEMENTED();
-                }
-            }
         }
     }
 
     uint32_t ret = !executablePipeline->execute(inputBuffer, pipelineContext, workerContext);
-
+    maxWaterMark = inputBuffer.getWatermark();
     if (hasWindowHandler() && maxWaterMark != 0) {
         NES_DEBUG("PipelineStage::execute: new max watermark=" << maxWaterMark << " originId=" << inputBuffer.getOriginId());
         pipelineContext->getWindowHandler()->updateAllMaxTs(maxWaterMark, inputBuffer.getOriginId());
