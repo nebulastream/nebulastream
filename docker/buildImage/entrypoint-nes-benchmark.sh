f05f4aa01b487e69c95d0e627398c6ceb27f8ad5
Nils Schubert <n.schubert@campus.tu-berlin.de>
2020-11-06 09:43:29 +0100

    #1136 removed own benchmark docker image and moved entrypoint-nes-benchmark.sh to buildImage

--- docker/buildImage/entrypoint-nes-benchmark.sh
+++ docker/buildImage/entrypoint-nes-benchmark.sh
@@ -6,13 +6,20 @@ if [ $# -eq 0 ]
 then
     mkdir -p /nebulastream/build
     cd /nebulastream/build
+
+
+
     export CC=/usr/bin/clang
     export CXX=/usr/bin/clang++
     cmake -DCMAKE_BUILD_TYPE=Release -DBoost_NO_SYSTEM_PATHS=TRUE -DBoost_INCLUDE_DIR="/usr/include" -DBoost_LIBRARY_DIR="/usr/lib/x86_64-linux-gnu" -DCPPRESTSDK_DIR="/usr/lib/x86_64-linux-gnu/cmake/" -DCMAKE_CXX_FLAGS_RELEASE:STRING="-w -Wno-unused-variable -Wno-unused-parameter -Wno-ignored-qualifiers -Wno-sign-compare " -DNES_USE_OPC=1 -DNES_USE_ADAPTIVE=0 -DNES_BUILD_BENCHMARKS=1 ..
     make -j`nproc --ignore=2`
+    
     cd /nebulastream/benchmark/scripts
+    apt-get install python3-pip -y && pip3 install --user argparse datetime matplotlib pandas seaborn numpy
     echo "Executing python script at $(date)..."
     python3 run_and_plot_benchmarks.py -f ../../build/benchmark/ -nc -b ".*-query-benchmark" -m "Run with docker"
+
+
     result=$?
     rm -rf /nebulastream/build
     exit $result
