57c88e3fd163de2b1b4cdfb7b4929298b0144a13
Ventura Del Monte <venturadelmonte@gmail.com>
2020-11-06 23:40:44 +0100

    [#1140] Add check for license file

---
+++ scripts/build/check-license.py
@@ -0,0 +1,36 @@
+import os
+from __future__ import print_function
+import sys
+
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
+license_text_cpp = """/*
+    Copyright (C) 2020 by the NebulaStream project (https://nebula.stream)
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+"""
+
+if __name__ == "__main__":
+    for subdir, dirs, files in os.walk(sys.argv[1]):
+        for file in files:
+            filename = os.path.join(subdir, file)
+            if filename.endswith(".cpp") or filename.endswith(".hpp") or filename.endswith(".proto"):
+                with open(filename, "r") as fp:
+                    content = fp.read()
+                    if not content.startswith(license_text_cpp):
+                        eprint("File", filename, " lacks the license preamble")
+                        sys.exit(1)
+
+sys.exit(0)
