53b352e8be4415a930571c78045b5950fc33563e
Xenofon Chatziliadis <x.chatziliadis@tu-berlin.de>
2020-11-27 19:13:36 +0100

    new scripts for experiments added

--- emulation/scripts/monitoring_env_setup.py
+++ emulation/scripts/monitoring_env_setup.py
@@ -1,3 +1,18 @@
+#!/usr/bin/env python
+# Copyright (C) 2020 by the NebulaStream project (https://nebula.stream)
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#    https://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 #!/usr/bin/python
 
 from mininet.net import Containernet
@@ -7,29 +22,33 @@ from mininet.link import TCLink
 from mininet.log import info, setLogLevel
 
 #params for the topology
-number_workers = 2
+number_workers = 5
+num_tuples = 10
+num_buffers = 20
 
+enable_influx = True
+nesDir = "/home/xenofon/git/nebulastream/"
+influx_storage = "/home/xenofon/experiments/influx/"
+
+#_________________________________________________________________________________________________________________
 setLogLevel('info')
 
 net = Containernet(controller=Controller)
 info('*** Adding controller\n')
 net.addController('c0')
 
-nesDir = "/home/xenofon/git/nebulastream/"
+if not nesDir:
+    raise RuntimeError("Please specify a nes directory like /git/nebulastream/")
 
-influxdb = net.addDocker('influxdb', dimage="influxdb:1.8.3",
-                         ports=[8086],
-                         port_bindings={8086: 8086},
-                         volumes=[nesDir + "emulation/data/influxdb/influxdb.conf:/etc/influxdb/influxdb.conf:ro",
-                                  nesDir + "emulation/data/influxdb:/var/lib/influxdb"],
-                         dcmd='influxd -config /etc/influxdb/influxdb.conf')
+if enable_influx:
+    influxdb = net.addDocker('influxdb', dimage="influxdb:1.8.3",
+                             ports=[8086],
+                             port_bindings={8086: 8086},
+                             volumes=[nesDir + "emulation/influxdb/influxdb.conf:/etc/influxdb/influxdb.conf:ro",
+                                      influx_storage + ":/var/lib/influxdb"],
+                             dcmd='influxd -config /etc/influxdb/influxdb.conf')
 
 info('*** Adding docker containers\n')
-#crd = net.addDocker('crd', ip='10.15.16.3',
-#                    dimage="nebulastream/nes-executable-image:latest",
-#                    ports=[8081, 12346, 4000, 4001, 4002],
-#                    port_bindings={8081: 8081, 12346: 12346, 4000: 4000, 4001: 4001, 4002: 4002},
-#                    dcmd='/opt/local/nebula-stream/nesCoordinator --serverIp=0.0.0.0')
 
 crd = net.addDocker('crd', ip='10.15.16.3',
                        dimage="nes_prometheus",
@@ -47,8 +66,10 @@ for i in range(0, number_workers):
                                          "path": nesDir + "emulation/images"},
                            ports=[3000, 3001, 9100],
                            port_bindings={3007: 3000, 3008: 3001, 9101: 9100})
-    cmd = '/entrypoint-prom.sh wrk /opt/local/nebula-stream/nesWorker --logLevel=LOG_DEBUG --coordinatorPort=4000 --coordinatorIp=10.15.16.3 --localWorkerIp=' + ip + ' --sourceType=YSBSource --numberOfBuffersToProduce=100 --numberOfTuplesToProducePerBuffer=10 --sourceFrequency=1 --physicalStreamName=ysb' + str(i) + ' --logicalStreamName=ysb'
-    print (cmd)
+    cmd = '/entrypoint-prom.sh wrk /opt/local/nebula-stream/nesWorker --logLevel=LOG_NONE --coordinatorPort=4000 --coordinatorIp=10.15.16.3 --localWorkerIp=' + ip \
+          + ' --sourceType=YSBSource --numberOfBuffersToProduce=' + str(num_buffers) + ' --numberOfTuplesToProducePerBuffer=' + str(num_tuples) + ' --sourceFrequency=1 --physicalStreamName=ysb' \
+          + str(i) + ' --logicalStreamName=ysb'
+    print(cmd)
     workers.append((w, cmd))
 
 info('*** Adding switches\n')
@@ -59,7 +80,7 @@ net.addLink(crd, sw1, cls=TCLink)
 for w in workers:
     net.addLink(w[0], sw1, cls=TCLink)
 
-crd.cmd('/entrypoint-prom.sh crd /opt/local/nebula-stream/nesCoordinator --serverIp=10.15.16.3 --logLevel=LOG_DEBUG')
+crd.cmd('/entrypoint-prom.sh crd /opt/local/nebula-stream/nesCoordinator --restIp=0.0.0.0 --coordinatorIp=10.15.16.3 --logLevel=LOG_NONE')
 for w in workers:
     w[0].cmd(w[1])
 
--- emulation/scripts/monitoring_experiment.py
+++ emulation/scripts/monitoring_experiment.py
@@ -2,15 +2,25 @@ import requests
 import datetime
 import docker
 from influxdb import InfluxDBClient
+from time import sleep
 
 # experiment parameters
-experiment_table = "monitoringEval"
-topology = "1Crd10Wrks"
+experiment_table = "experiment_monitoring_ysb"
+topology = "1Crd5Wrks"
 system = "NES"
-version = "1"
-noIterations = 200
+noIterations = 25
+noIterationsBeforeExecution = 5
 
-def readDockerStats(containerIters, influxTable, topology, system, version, runtime, iteration):
+description = "10Tup-10Buf"
+
+version = "2"
+
+# types are None, "prometheus", "nes"
+types = [None, "prometheus", "nes"]
+monitoring_type = types[2]
+
+
+def readDockerStats(containerIters, influxTable, topology, system, version, runtime, iteration, description, monitoring):
     msrmnts = []
     for cName, cIter in containerIters:
         msrmt = next(cIter)
@@ -28,7 +38,9 @@ def readDockerStats(containerIters, influxTable, topology, system, version, runt
                 "type": nes_type,
                 "topology": topology,
                 "system": system,
-                "version": version
+                "version": version,
+                "description": description,
+                "monitoring": str(monitoring)
             },
             "time": msrmt["read"],
             "fields": {
@@ -46,44 +58,90 @@ def readDockerStats(containerIters, influxTable, topology, system, version, runt
         msrmnts.append(measurementDict)
     return msrmnts
 
+def make_request(request_type):
+    if request_type == "ysb":
+        nes_url = 'http://localhost:8081/v1/nes/query/execute-query'
+        data = '''{
+        \"userQuery\" : \"Query::from(\\\"ysb\\\").windowByKey(Attribute(\\\"campaign_id\\\"), TumblingWindow::of(EventTime(Attribute(\\\"current_ms\\\")), Milliseconds(1)), Sum(Attribute(\\\"user_id\\\"))).sink(FileSinkDescriptor::create(\\\"ysbOut.csv\\\",\\\"CSV_FORMAT\\\",\\\"APPEND\\\"));\",
+        \"strategyName\" : \"BottomUp\"
+        }'''
+        return requests.post(nes_url, data=data)
+    elif request_type == "monitoring_prometheus":
+        nes_url = 'http://localhost:8081/v1/nes/monitoring/metrics/prometheus'
+        return requests.get(nes_url)
+    elif request_type == "monitoring_nes":
+        nes_url = 'http://localhost:8081/v1/nes/monitoring/metrics/'
+        return requests.get(nes_url)
+    else:
+        raise RuntimeError("Unkown request " + request_type)
+
+
+def request_monitoring_data(type):
+    print("Executing monitoring call on " + type)
+    resp = None
+    if type == "prometheus":
+        resp = make_request("monitoring_prometheus")
+    elif type == "nes":
+        resp = make_request("monitoring_nes")
+
+    if resp and resp.status_code == 200:
+        return True
+    else:
+        raise RuntimeError("Response with status code " + str(resp.status_code))
 
 print("Executing monitoring request experiment")
-
 # execute experiment
 msrmnt_batch = []
-prom_url = 'http://localhost:8081/v1/nes/monitoring/metrics/prometheus'
-nes_url = 'http://localhost:8081/v1/nes/monitoring/metrics/'
 docker_client = docker.DockerClient(base_url='unix://var/run/docker.sock')
 
 containers = filter(lambda c: c.name.startswith(("mn.crd", "mn.w")), docker_client.containers.list())
 containerIters = [(c.name, c.stats(decode=True)) for c in containers]
 
-for i in range(1, noIterations + 1):
-    print("Experiment run " + str(i))
-    start = datetime.datetime.now()
-    if system == "NES":
-        response = requests.get(nes_url)
-    elif system == "PROMETHEUS":
-        response = requests.get(prom_url)
-    else:
-        raise RuntimeError("Unkown system " + system)
+start = datetime.datetime.now()
+for i in range(1, noIterationsBeforeExecution):
+    print("\nExecuting iteration " + str(i))
+    if monitoring_type:
+        request_monitoring_data(monitoring_type)
 
+    print("Reading docker stats " + str(i))
+    end = datetime.datetime.now()
+    delta = (end - start)
+    runtimeInMs = int(delta.total_seconds() * 1000)  # milliseconds
+    msrmnt_batch.extend(readDockerStats(containerIters, experiment_table, topology, system, version, runtimeInMs, i, description, monitoring_type))
+    sleep(1)
+
+print("Executing query")
+start = datetime.datetime.now()
+response = make_request("ysb")
+
+for i in range(noIterationsBeforeExecution, noIterationsBeforeExecution + noIterations + 1):
+    print("\nExecuting iteration " + str(i))
+
+    if monitoring_type:
+        request_monitoring_data(monitoring_type)
+
+    print("Reading docker stats " + str(i))
     end = datetime.datetime.now()
     delta = (end - start)
     runtimeInMs = int(delta.total_seconds() * 1000)  # milliseconds
 
     if (response.status_code == 200):
         # print(response.json())
-        msrmnt_batch.extend(
-            readDockerStats(containerIters, experiment_table, topology, system, version, runtimeInMs, i))
-        # sleep(0.5)
+        msrmnt_batch.extend(readDockerStats(containerIters, experiment_table, topology, system, version, runtimeInMs, i, description, monitoring_type))
+        sleep(1)
     else:
         raise RuntimeError("Response with status code " + str(response.status_code))
 
 # influx operations
 print("Experiment finished! Writing to Influx..")
 influx_client = InfluxDBClient(host='localhost', port=8086)
-influx_client.switch_database('mydb')
+
+try:
+    influx_client.create_database('monitoring')
+except:
+    print('Database monitoring already exists')
+
+influx_client.switch_database('monitoring')
 
 print(msrmnt_batch)
 res = influx_client.write_points(msrmnt_batch)
