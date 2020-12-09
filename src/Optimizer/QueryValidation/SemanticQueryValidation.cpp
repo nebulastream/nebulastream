9e8845fb480866f939c5204428dab460109c174e
Mark Sumegi <sumegi.mark@gmail.com>
2020-12-10 00:44:45 +0100

    [#1253] Classes and Tests for Semantic and Syntactic query validation

---
+++ src/Optimizer/QueryValidation/SemanticQueryValidation.cpp
@@ -0,0 +1,51 @@
+/*
+    Copyright (C) 2020 by the NebulaStream project (https://nebula.stream)
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+
+#include <Optimizer/QueryValidation/SemanticQueryValidation.hpp>
+#include <Operators/LogicalOperators/FilterLogicalOperatorNode.hpp>
+#include <Plans/Query/QueryPlan.hpp>
+#include <Util/Logger.hpp>
+#include <string.h>
+#include <z3++.h>
+
+namespace NES {
+
+NES::SemanticQueryValidation::SemanticQueryValidation(SchemaPtr schema){
+    this->schema = schema;
+}
+
+bool NES::SemanticQueryValidation::isSatisfiable(QueryPtr inputQuery) {
+    
+    std::shared_ptr<z3::context> c = std::make_shared<z3::context>();
+    z3::solver s(*c);
+
+    auto filterOperators = inputQuery->getQueryPlan()->getOperatorByType<FilterLogicalOperatorNode>();
+
+    for (int i = 0; i < filterOperators.size(); i++) {
+        filterOperators[i]->getPredicate()->inferStamp(schema);
+    }
+    for (int i = 0; i < filterOperators.size(); i++) {
+        filterOperators[i]->inferZ3Expression(c);
+    }
+    for (int i = 0; i < filterOperators.size(); i++) {
+        s.add(filterOperators[i]->getZ3Expression());
+    }
+
+    return s.check() != z3::unsat;
+}
+
+}// namespace NES
