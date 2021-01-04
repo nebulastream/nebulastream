cf19d04aba0c203c0600693b7952160128a67275
Xenofon Chatziliadis <x.chatziliadis@tu-berlin.de>
2021-01-04 16:22:28 +0100

    modularization of experiments and topology added

--- emulation/images/entrypoint-prom.sh
+++ emulation/images/entrypoint-prom.sh
@@ -14,19 +14,21 @@
 # limitations under the License.
 
 
-#/bin/prometheus-node-exporter --no-collector.arp --no-collector.bcache --no-collector.bonding --no-collector.conntrack \
-#--no-collector.edac --no-collector.entropy --no-collector.filefd --no-collector.hwmon --no-collector.infiniband \
-#--no-collector.ipvs --no-collector.mdadm --no-collector.nfs --no-collector.nfsd --no-collector.pressure \
-#--no-collector.sockstat --no-collector.textfile --no-collector.time --no-collector.timex --no-collector.uname \
-#--no-collector.vmstat --no-collector.xfs --no-collector.zfs --no-collector.systemd --no-collector.cpu \
-#--no-collector.diskstats --no-collector.loadavg --no-collector.netclass --no-collector.netstat --no-collector.stat \
-#> prometheus.log 2>&1 &
+if [ $1 = "prometheus" ]; then
+  echo "Starting prometheus daemon"
+  /bin/prometheus-node-exporter --no-collector.arp --no-collector.bcache --no-collector.bonding --no-collector.conntrack \
+  --no-collector.edac --no-collector.entropy --no-collector.filefd --no-collector.hwmon --no-collector.infiniband \
+  --no-collector.ipvs --no-collector.mdadm --no-collector.nfs --no-collector.nfsd --no-collector.pressure \
+  --no-collector.sockstat --no-collector.textfile --no-collector.time --no-collector.timex --no-collector.uname \
+  --no-collector.vmstat --no-collector.xfs --no-collector.zfs --no-collector.systemd --no-collector.cpu \
+  --no-collector.diskstats --no-collector.loadavg --no-collector.netclass --no-collector.netstat --no-collector.stat \
+  > prometheus.log 2>&1 &
+fi
 
-
-if [ $1 = "crd" ]; then
+if [ $2 = "crd" ]; then
   echo "Executing coordinator script:"
-  sleep 5 && exec /entrypoint.sh ${@:2} > nes-runtime.log 2>&1 &
+  sleep 5 && exec /entrypoint.sh ${@:3} > nes-runtime.log 2>&1 &
 else
   echo "Executing worker script:"
-  sleep 10 && exec /entrypoint.sh ${@:2} > nes-runtime.log 2>&1 &
+  sleep 10 && exec /entrypoint.sh ${@:3} > nes-runtime.log 2>&1 &
 fi
