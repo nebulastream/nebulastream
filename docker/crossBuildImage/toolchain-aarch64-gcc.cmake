a217483fcb1eaf125482ead592739f3c2b00abe1
digiou <digitalbckp@gmail.com>
2021-01-12 21:40:31 +0200

    [#1375] Rewrite cmake for gtest, add nes binary wrappers

--- docker/crossBuildImage/toolchain-aarch64-gcc.cmake
+++ docker/crossBuildImage/toolchain-aarch64-gcc.cmake
@@ -1,6 +1,10 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
 set(TARGET_ABI "linux-gnu")
+SET(CROSS_TARGET ${CMAKE_SYSTEM_PROCESSOR}-${CMAKE_TARGET_ABI})
+
+# specify sysroot for aarch64
+SET(CMAKE_SYSROOT /opt/sysroots/aarch64-linux-gnu)
 
 # specify the cross compiler
 set(CMAKE_C_COMPILER   aarch64-${TARGET_ABI}-gcc)
@@ -18,14 +22,8 @@ set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 
-# If we don't set these, CMake will end up using the host version.
+# If these pass, CMake will end up using the host version.
 find_program(GCC_FULL_PATH aarch64-${TARGET_ABI}-gcc)
 if (NOT GCC_FULL_PATH)
     message(FATAL_ERROR "Cross-compiler aarch64-${TARGET_ABI}-gcc not found")
 endif ()
-get_filename_component(GCC_DIR ${GCC_FULL_PATH} PATH)
-set(CMAKE_LINKER       ${GCC_DIR}/aarch64-${TARGET_ABI}-ld      CACHE FILEPATH "linker")
-set(CMAKE_ASM_COMPILER ${GCC_DIR}/aarch64-${TARGET_ABI}-as      CACHE FILEPATH "assembler")
-set(CMAKE_OBJCOPY      ${GCC_DIR}/aarch64-${TARGET_ABI}-objcopy CACHE FILEPATH "objcopy")
-set(CMAKE_STRIP        ${GCC_DIR}/aarch64-${TARGET_ABI}-strip   CACHE FILEPATH "strip")
-set(CMAKE_CPP          ${GCC_DIR}/aarch64-${TARGET_ABI}-cpp     CACHE FILEPATH "cpp")
