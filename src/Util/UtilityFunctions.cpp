6b4f9ab9db7936559fbe9f3a1a5b35273e50aa21
nabilshafi <nabilshafi@gmail.com>
2021-01-20 05:53:43 +0100

    change schema

--- src/Util/UtilityFunctions.cpp
+++ src/Util/UtilityFunctions.cpp
@@ -306,7 +306,7 @@ std::string UtilityFunctions::printTupleBufferAsCSV(NodeEngine::TupleBuffer& tbu
             else{
                 ss << ",";
                 //Adding time stamp
-                auto time =  std::to_string( std::chrono::duration_cast<std::chrono::nanoseconds>(Clock::now().time_since_epoch()).count());
+                auto time =  std::to_string( std::chrono::duration_cast<std::chrono::milliseconds>(Clock::now().time_since_epoch()).count());
                // outputFile.write((char*)time.c_str(),time.size());
                 ss << time.c_str();
             }
