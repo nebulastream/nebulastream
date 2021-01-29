ada9349d48ef96f1f010902ad0758fd9fc32b998
Nithish Sankaranarayanan <nithishsankaranarayanan@gmail.com>
2021-01-29 14:37:42 +0100

    #1520: Allow hierarchy config

--- emulation/bdapro2/lib/topology.py
+++ emulation/bdapro2/lib/topology.py
@@ -6,21 +6,21 @@ from mininet.log import info
 from mininet.net import Containernet
 from mininet.node import Controller
 
-from lib.util import request_monitoring_data, NodeMessage
+from lib.util import request_monitoring_data, NodeCmd
 
 
 class Topology:
     def __init__(self):
         self.net = None
         self.topology_size: int = 0
-        self.workers: List[NodeMessage] = []
+        self.nodes: List[NodeCmd] = []
         self._stopped: bool = True
 
-    def create_topology(self, topology: Callable[[Containernet], Tuple[Containernet, int, List[NodeMessage]]]):
+    def create_topology(self, topology: Callable[[Containernet], Tuple[Containernet, int, List[NodeCmd]]]):
         net = Containernet(controller=Controller)
         info('*** Adding controller\n')
         net.addController('c0')
-        self.net, self.topology_size, self.workers = topology(net)
+        self.net, self.topology_size, self.nodes = topology(net)
 
     def wait_until_topology_is_complete(self, timeout):
         timeout = timeout // 5
@@ -36,13 +36,13 @@ class Topology:
             except Exception as e:
                 print("Failed to connect to REST service of coordinator: ", e)
 
-            if current_size == len(self.workers):
+            if current_size == len(self.nodes):
                 print(f"Completed: Received monitoring data from coordinator for nodes {current_size}")
                 complete = True
                 break
             else:
                 print(
-                    f"Waiting: Received monitoring data from coordinator for nodes {current_size} but expecting {len(self.workers)}")
+                    f"Waiting: Received monitoring data from coordinator for nodes {current_size} but expecting {len(self.nodes)}")
                 sleep(5)
         return complete
 
@@ -51,8 +51,8 @@ class Topology:
 
     def start_emulation(self):
         self.net.start()
-        self._stopped = True
-        for worker in self.workers:
+        self._stopped = False
+        for worker in self.nodes:
             worker.node.sendCmd(worker.cmd)
             print(f"Executed CMD {worker.cmd} and on Worker {worker.node.name}")
 
