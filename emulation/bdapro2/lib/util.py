9eca112346577dc653629fb4a503d12bca94bdc2
Nithish Sankaranarayanan <nithishsankaranarayanan@gmail.com>
2021-01-29 12:33:24 +0100

    #1520: Graceful exit and requests module to add parent child relationship

--- emulation/bdapro2/lib/util.py
+++ emulation/bdapro2/lib/util.py
@@ -2,6 +2,8 @@ import mininet.node
 import requests
 from attr import dataclass
 
+NES_BASE_URL = 'http://localhost:8081/v1/nes'
+
 
 @dataclass
 class NodeMessage:
@@ -11,9 +13,17 @@ class NodeMessage:
 
 def request_monitoring_data():
     print("Executing monitoring call on topology")
-    nes_url = 'http://localhost:8081/v1/nes/monitoring/metrics/'
-    resp = requests.get(nes_url)
+    monitoring_url = f"{NES_BASE_URL}/monitoring/metrics/"
+    resp = requests.get(monitoring_url)
     if resp and resp.status_code == 200:
         return resp
     else:
         raise RuntimeError("Response with status code " + str(resp.status_code))
+
+
+def add_parent_topology(child_id: int, parent_id: int):
+    print(f"Adding {parent_id} as parent for {child_id}")
+    topology_add_parent_url = f"{NES_BASE_URL}/topology/addParent"
+    resp = requests.post(topology_add_parent_url, {"childId": str(child_id), "parentId": str(parent_id)})
+    if resp.status_code != 200:
+        raise RuntimeError(f"Could not add node {parent_id} as parent for {child_id}:  {str(resp.text)}")
