21d3e0792ef965f59d957886832b068747fb50e3
Dwi Prasetyo Adi Nugroho <dwiprasetyo.an@gmail.com>
2021-02-08 19:20:15 +0100

    [#1295] added tests with hierarchical topology

--- tests/Integration/AllowedLatenessTest.cpp
+++ tests/Integration/AllowedLatenessTest.cpp
@@ -287,7 +287,7 @@ TEST_F(AllowedLatenessTest, testAllowedLateness_MPS_FT_IO_10ms) {
 /*
  * @brief Test allowed lateness using multiple sources, flat topology, in-order stream with 250ms allowed lateness
  */
-TEST_F(AllowedLatenessTest, testAllowedLateness_SMPS_FT_IO_250ms) {
+TEST_F(AllowedLatenessTest, testAllowedLateness_MPS_FT_IO_250ms) {
     string query = "Query::from(\"inOrderStream\")"
                    ".assignWatermark(EventTimeWatermarkStrategyDescriptor::create(Attribute(\"timestamp\"),Milliseconds(250), "
                    "Milliseconds()))"
@@ -379,6 +379,215 @@ TEST_F(AllowedLatenessTest, testAllowedLateness_MPS_FT_OO_250ms) {
     // with allowed lateness=250, {9,1,1900} included in 1000-2000 window, while the 3000-4000 window is not yet closed
     std::vector<Output> expectedOutput = {{1000, 2000, 1, 63},
                                           {2000, 3000, 1, 72}};
+
+    std::vector<Output> actualOutput = testHarness.getOutput<Output>(expectedOutput.size());
+
+    EXPECT_EQ(actualOutput.size(), expectedOutput.size());
+    EXPECT_THAT(actualOutput, ::testing::UnorderedElementsAreArray(expectedOutput));
+}
+
+
+//----Test with Hierarchical Topology----//
+//* Topology:
+//PhysicalNode[id=1, ip=127.0.0.1, resourceCapacity=12, usedResource=0]
+//|--PhysicalNode[id=5, ip=127.0.0.1, resourceCapacity=12, usedResource=0]
+//|  |--PhysicalNode[id=7, ip=127.0.0.1, resourceCapacity=12, usedResource=0]
+//|  |--PhysicalNode[id=6, ip=127.0.0.1, resourceCapacity=12, usedResource=0]
+//|--PhysicalNode[id=2, ip=127.0.0.1, resourceCapacity=1, usedResource=0]
+//|  |--PhysicalNode[id=4, ip=127.0.0.1, resourceCapacity=12, usedResource=0]
+//|  |--PhysicalNode[id=3, ip=127.0.0.1, resourceCapacity=12, usedResource=0]
+/*
+ * @brief Test allowed lateness using multiple sources, hierarchical topology, in-order stream with 0ms allowed lateness
+ */
+TEST_F(AllowedLatenessTest, testAllowedLateness_MPS_HT_IO_0ms) {
+    string query = "Query::from(\"inOrderStream\")"
+                   ".assignWatermark(EventTimeWatermarkStrategyDescriptor::create(Attribute(\"timestamp\"),Milliseconds(0), "
+                   "Milliseconds()))"
+                   ".windowByKey(Attribute(\"id\"), TumblingWindow::of(EventTime(Attribute(\"timestamp\")),Seconds(1)), "
+                   "Sum(Attribute(\"value\")))";
+
+    TestHarness testHarness = TestHarness(query, restPort, rpcPort);
+    testHarness.addNonSourceWorker();
+    testHarness.addNonSourceWorker();
+    testHarness.addCSVSource(inOrderConf, inputSchema, testHarness.getWorkerId(0));
+    testHarness.addCSVSource(inOrderConf, inputSchema, testHarness.getWorkerId(0));
+    testHarness.addCSVSource(inOrderConf, inputSchema, testHarness.getWorkerId(1));
+    testHarness.addCSVSource(inOrderConf, inputSchema, testHarness.getWorkerId(1));
+
+    TopologyPtr topology = testHarness.getTopology();
+    ASSERT_EQ(topology->getRoot()->getChildren().size(), 2);
+    ASSERT_EQ(topology->getRoot()->getChildren()[0]->getChildren().size(), 2);
+    ASSERT_EQ(topology->getRoot()->getChildren()[1]->getChildren().size(), 2);
+
+    std::vector<Output> expectedOutput = {{1000, 2000, 1, 60},
+                                          {2000, 3000, 1, 120},
+                                          {3000, 4000, 1, 84}};
+    std::vector<Output> actualOutput = testHarness.getOutput<Output>(expectedOutput.size());
+
+    EXPECT_EQ(actualOutput.size(), expectedOutput.size());
+    EXPECT_THAT(actualOutput, ::testing::UnorderedElementsAreArray(expectedOutput));
+}
+
+/*
+ * @brief Test allowed lateness using multiple sources, hierarchical topology, in-order stream with 10ms allowed lateness
+ */
+TEST_F(AllowedLatenessTest, testAllowedLateness_MPS_HT_IO_10ms) {
+    string query = "Query::from(\"inOrderStream\")"
+                   ".assignWatermark(EventTimeWatermarkStrategyDescriptor::create(Attribute(\"timestamp\"),Milliseconds(10), "
+                   "Milliseconds()))"
+                   ".windowByKey(Attribute(\"id\"), TumblingWindow::of(EventTime(Attribute(\"timestamp\")),Seconds(1)), "
+                   "Sum(Attribute(\"value\")))";
+
+    TestHarness testHarness = TestHarness(query, restPort, rpcPort);
+    testHarness.addNonSourceWorker();
+    testHarness.addNonSourceWorker();
+    testHarness.addCSVSource(inOrderConf, inputSchema, testHarness.getWorkerId(0));
+    testHarness.addCSVSource(inOrderConf, inputSchema, testHarness.getWorkerId(0));
+    testHarness.addCSVSource(inOrderConf, inputSchema, testHarness.getWorkerId(1));
+    testHarness.addCSVSource(inOrderConf, inputSchema, testHarness.getWorkerId(1));
+
+    TopologyPtr topology = testHarness.getTopology();
+    ASSERT_EQ(topology->getRoot()->getChildren().size(), 2);
+    ASSERT_EQ(topology->getRoot()->getChildren()[0]->getChildren().size(), 2);
+    ASSERT_EQ(topology->getRoot()->getChildren()[1]->getChildren().size(), 2);
+
+    // with allowed lateness=10, the 3000-4000 is closed
+    std::vector<Output> expectedOutput = {{1000, 2000, 1, 60},
+                                          {2000, 3000, 1, 120},
+                                          {3000, 4000, 1, 84}};
+
+    std::vector<Output> actualOutput = testHarness.getOutput<Output>(expectedOutput.size());
+
+    EXPECT_EQ(actualOutput.size(), expectedOutput.size());
+    EXPECT_THAT(actualOutput, ::testing::UnorderedElementsAreArray(expectedOutput));
+}
+
+/*
+ * @brief Test allowed lateness using multiple sources, hierarchical topology, in-order stream with 250ms allowed lateness
+ */
+TEST_F(AllowedLatenessTest, testAllowedLateness_MPS_HT_IO_250ms) {
+    string query = "Query::from(\"inOrderStream\")"
+                   ".assignWatermark(EventTimeWatermarkStrategyDescriptor::create(Attribute(\"timestamp\"),Milliseconds(250), "
+                   "Milliseconds()))"
+                   ".windowByKey(Attribute(\"id\"), TumblingWindow::of(EventTime(Attribute(\"timestamp\")),Seconds(1)), "
+                   "Sum(Attribute(\"value\")))";
+
+    TestHarness testHarness = TestHarness(query, restPort, rpcPort);
+    testHarness.addNonSourceWorker();
+    testHarness.addNonSourceWorker();
+    testHarness.addCSVSource(inOrderConf, inputSchema, testHarness.getWorkerId(0));
+    testHarness.addCSVSource(inOrderConf, inputSchema, testHarness.getWorkerId(0));
+    testHarness.addCSVSource(inOrderConf, inputSchema, testHarness.getWorkerId(1));
+    testHarness.addCSVSource(inOrderConf, inputSchema, testHarness.getWorkerId(1));
+
+    TopologyPtr topology = testHarness.getTopology();
+    ASSERT_EQ(topology->getRoot()->getChildren().size(), 2);
+    ASSERT_EQ(topology->getRoot()->getChildren()[0]->getChildren().size(), 2);
+    ASSERT_EQ(topology->getRoot()->getChildren()[1]->getChildren().size(), 2);
+
+    // with allowed lateness=250, the 3000-4000 window is not yet closed and up to {5,1,1990} included to the 1000-2000 window
+    std::vector<Output> expectedOutput = {{1000, 2000, 1, 60},
+                                          {2000, 3000, 1, 120}};
+
+    std::vector<Output> actualOutput = testHarness.getOutput<Output>(expectedOutput.size());
+
+    EXPECT_EQ(actualOutput.size(), expectedOutput.size());
+    EXPECT_THAT(actualOutput, ::testing::UnorderedElementsAreArray(expectedOutput));
+}
+
+/*
+ * @brief Test allowed lateness using multiple sources, hierarchical topology, out-of-order stream with 0ms allowed lateness
+ */
+TEST_F(AllowedLatenessTest, testAllowedLateness_MPS_HT_OO_0ms) {
+    string query = "Query::from(\"OutOfOrderStream\")"
+                   ".assignWatermark(EventTimeWatermarkStrategyDescriptor::create(Attribute(\"timestamp\"),Milliseconds(0), "
+                   "Milliseconds()))"
+                   ".windowByKey(Attribute(\"id\"), TumblingWindow::of(EventTime(Attribute(\"timestamp\")),Seconds(1)), "
+                   "Sum(Attribute(\"value\")))";
+
+    TestHarness testHarness = TestHarness(query, restPort, rpcPort);
+    testHarness.addNonSourceWorker();
+    testHarness.addNonSourceWorker();
+    testHarness.addCSVSource(outOfOrderConf, inputSchema, testHarness.getWorkerId(0));
+    testHarness.addCSVSource(outOfOrderConf, inputSchema, testHarness.getWorkerId(0));
+    testHarness.addCSVSource(outOfOrderConf, inputSchema, testHarness.getWorkerId(1));
+    testHarness.addCSVSource(outOfOrderConf, inputSchema, testHarness.getWorkerId(1));
+
+    TopologyPtr topology = testHarness.getTopology();
+    ASSERT_EQ(topology->getRoot()->getChildren().size(), 2);
+    ASSERT_EQ(topology->getRoot()->getChildren()[0]->getChildren().size(), 2);
+    ASSERT_EQ(topology->getRoot()->getChildren()[1]->getChildren().size(), 2);
+
+    // with allowed lateness = 0, {6,1,1990} is not included to window 1000-2000
+    std::vector<Output> expectedOutput = {{1000, 2000, 1, 24},
+                                          {2000, 3000, 1, 96},
+                                          {3000, 4000, 1, 88}};
+    std::vector<Output> actualOutput = testHarness.getOutput<Output>(expectedOutput.size());
+
+    EXPECT_EQ(actualOutput.size(), expectedOutput.size());
+    EXPECT_THAT(actualOutput, ::testing::UnorderedElementsAreArray(expectedOutput));
+}
+
+/*
+ * @brief Test allowed lateness using multiple sources, hierarchical topology, out-of-order stream with 10ms allowed lateness
+ */
+TEST_F(AllowedLatenessTest, testAllowedLateness_MPS_HT_OO_10ms) {
+    string query = "Query::from(\"OutOfOrderStream\")"
+                   ".assignWatermark(EventTimeWatermarkStrategyDescriptor::create(Attribute(\"timestamp\"),Milliseconds(10), "
+                   "Milliseconds()))"
+                   ".windowByKey(Attribute(\"id\"), TumblingWindow::of(EventTime(Attribute(\"timestamp\")),Seconds(1)), "
+                   "Sum(Attribute(\"value\")))";
+
+    TestHarness testHarness = TestHarness(query, restPort, rpcPort);
+    testHarness.addNonSourceWorker();
+    testHarness.addNonSourceWorker();
+    testHarness.addCSVSource(outOfOrderConf, inputSchema, testHarness.getWorkerId(0));
+    testHarness.addCSVSource(outOfOrderConf, inputSchema, testHarness.getWorkerId(0));
+    testHarness.addCSVSource(outOfOrderConf, inputSchema, testHarness.getWorkerId(1));
+    testHarness.addCSVSource(outOfOrderConf, inputSchema, testHarness.getWorkerId(1));
+
+    TopologyPtr topology = testHarness.getTopology();
+    ASSERT_EQ(topology->getRoot()->getChildren().size(), 2);
+    ASSERT_EQ(topology->getRoot()->getChildren()[0]->getChildren().size(), 2);
+    ASSERT_EQ(topology->getRoot()->getChildren()[1]->getChildren().size(), 2);
+
+    // with allowed lateness = 10, {6,1,1990} is included to window 1000-2000
+    std::vector<Output> expectedOutput = {{1000, 2000, 1, 48},
+                                          {2000, 3000, 1, 96},
+                                          {3000,4000,1,88}};
+
+    std::vector<Output> actualOutput = testHarness.getOutput<Output>(expectedOutput.size());
+
+    EXPECT_EQ(actualOutput.size(), expectedOutput.size());
+    EXPECT_THAT(actualOutput, ::testing::UnorderedElementsAreArray(expectedOutput));
+}
+
+/*
+ * @brief Test allowed lateness using multiple sources, hierarchical topology, out-of-order stream with 250ms allowed lateness
+ */
+TEST_F(AllowedLatenessTest, testAllowedLateness_MPS_HT_OO_250ms) {
+    string query = "Query::from(\"OutOfOrderStream\")"
+                   ".assignWatermark(EventTimeWatermarkStrategyDescriptor::create(Attribute(\"timestamp\"),Milliseconds(250), "
+                   "Milliseconds()))"
+                   ".windowByKey(Attribute(\"id\"), TumblingWindow::of(EventTime(Attribute(\"timestamp\")),Seconds(1)), "
+                   "Sum(Attribute(\"value\")))";
+
+    TestHarness testHarness = TestHarness(query, restPort, rpcPort);
+    testHarness.addNonSourceWorker();
+    testHarness.addNonSourceWorker();
+    testHarness.addCSVSource(outOfOrderConf, inputSchema, testHarness.getWorkerId(0));
+    testHarness.addCSVSource(outOfOrderConf, inputSchema, testHarness.getWorkerId(0));
+    testHarness.addCSVSource(outOfOrderConf, inputSchema, testHarness.getWorkerId(1));
+    testHarness.addCSVSource(outOfOrderConf, inputSchema, testHarness.getWorkerId(1));
+
+    TopologyPtr topology = testHarness.getTopology();
+    ASSERT_EQ(topology->getRoot()->getChildren().size(), 2);
+    ASSERT_EQ(topology->getRoot()->getChildren()[0]->getChildren().size(), 2);
+    ASSERT_EQ(topology->getRoot()->getChildren()[1]->getChildren().size(), 2);
+
+    // with allowed lateness=250, {9,1,1900} included in 1000-2000 window, while the 3000-4000 window is not yet closed
+    std::vector<Output> expectedOutput = {{1000, 2000, 1, 84},
+                                          {2000, 3000, 1, 96}};
 
     std::vector<Output> actualOutput = testHarness.getOutput<Output>(expectedOutput.size());
 
