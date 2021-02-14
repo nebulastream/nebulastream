46fc1fe44114a678e414267e711ce1535e196868
Nithish Sankaranarayanan <nithishsankaranarayanan@gmail.com>
2021-02-14 10:51:52 +0100

    #1520: WIP Async stable requests

--- emulation/bdapro2/workload.py
+++ emulation/bdapro2/workload.py
@@ -1,6 +1,7 @@
 import math
 import random
 import time
+from concurrent.futures import ProcessPoolExecutor, as_completed
 from datetime import datetime
 from typing import List, Dict, Optional
 
@@ -47,7 +48,7 @@ def submit_query(base_url: str, request: List[Dict[str, str]], header=None):
 
 
 # get a field value in the response_list
-def get_query_id(responses: List[Response]):
+def get_query_id(responses: List[Response]) -> List[int]:
     query_id = []
     for response in responses:
         query_id.append(response.json()["queryId"])
@@ -86,15 +87,24 @@ def _filter_none(response: List[Optional[str]]) -> List[str]:
     return [r for r in response if r is not None]
 
 
+def stable_workload(base_url: str, base_query: str, n_requests: int = 100, niter: int = 1, sleep_time: int = 1):
+    futures = []
+    with ProcessPoolExecutor(max_workers=niter) as executor:
+        for i in range(1, niter + 1):
+            futures.append(executor.submit(launch_requests, base_url, base_query, i, n_requests))
+            time.sleep(sleep_time)
+        for completed in as_completed(futures):
+            iter_number, _, query_start_time = completed.result()
+            print(f"Queries launched in iteration {iter_number} took {time.time() - query_start_time} seconds")
+
+
 def generate_workload(base_url: str, base_query: str, n_requests: int = 100, stable: bool = True, ratio: float = 0.5,
                       niter: int = 1):
+    print(f"Executing Workload.")
     queryids = []
-    for i in range(niter):
-        print(f"Running iteration {i + 1}")
-        query_sets = generate_query(base_query, n_requests, i)
-        start_time = time.time()
-        response_list = submit_query(base_url, query_sets)
-        recent_query_ids = get_query_id(response_list)
+    for i in range(1, niter + 1):
+        print(f"Running iteration {i}")
+        _, recent_query_ids, start_time = launch_requests(base_query, base_url, i, n_requests)
         while number_of_queries_waiting_to_run(base_url) > 0:
             print(f"Waiting until submitted queries are marked as running.")
             time.sleep(1)
@@ -116,7 +126,27 @@ def generate_workload(base_url: str, base_query: str, n_requests: int = 100, sta
         queryids.extend(recent_query_ids)
 
 
+def launch_requests(base_url: str, base_query: str, iteration_number: int, n_requests: int) -> (int, List[int], int):
+    query_sets = generate_query(base_query, n_requests, iteration_number)
+    query_launch_time = time.time()
+    print(f"Submitting queries for iteration {iteration_number} at {query_launch_time}")
+    response_list = submit_query(base_url, query_sets)
+    recent_query_ids = get_query_id(response_list)
+    return iteration_number, recent_query_ids, query_launch_time
+
+
+def init_query(base_url: str, base_query: str):
+    query_sets = generate_query(base_query, 1, 0)
+    response_list = submit_query(base_url, query_sets)
+    get_query_id(response_list)
+    while number_of_queries_waiting_to_run(BASE_QUERY) > 0:
+        print(f"Waiting for first query to run.")
+        time.sleep(2)
+
+
 if __name__ == '__main__':
     NES_BASE_URL = "http://localhost:8081/v1/nes"
     BASE_QUERY = "Query::from(\"" + "ysb" + "\")"
-    generate_workload(NES_BASE_URL, BASE_QUERY, stable=True, n_requests=10, niter=20)
+    init_query(NES_BASE_URL, BASE_QUERY)
+    stable_workload(NES_BASE_URL, BASE_QUERY, n_requests=10, niter=10, sleep_time=1)
+    # generate_workload(NES_BASE_URL, BASE_QUERY, stable=True, n_requests=10, niter=20)
