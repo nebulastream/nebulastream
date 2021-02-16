5e62d7e466c563d6e73b75ef171cb35cf3a75ef2
zeuchste <steffen.zeuch@dfki.de>
2021-02-16 11:30:14 +0100

    [#1562] fix comment

--- include/Windowing/Runtime/WindowManager.hpp
+++ include/Windowing/Runtime/WindowManager.hpp
@@ -30,7 +30,7 @@ namespace NES::Windowing {
 class WindowManager {
 
   public:
-    explicit WindowManager(Windowing::WindowTypePtr windowType, uint64_t allowedLateness, uint64_t id);
+    explicit WindowManager(Windowing::WindowTypePtr windowType, uint64_t allowedLateness);
 
     /**
      * @brief Get the window type for the window manager
