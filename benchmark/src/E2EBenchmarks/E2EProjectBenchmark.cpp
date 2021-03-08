0b5690ea3a2c733bb3b69364758ce2a0ae0a347e
zeuchste <steffen.zeuch@dfki.de>
2021-03-08 08:58:03 +0100

    [#1633] fix benchmarks

--- benchmark/src/E2EBenchmarks/E2EProjectBenchmark.cpp
+++ benchmark/src/E2EBenchmarks/E2EProjectBenchmark.cpp
@@ -26,18 +26,19 @@ using namespace Benchmarking;
 int main() {
     // Number of workerThreads in nodeEngine
     std::vector<uint16_t> allWorkerThreads;
-    BenchmarkUtils::createRangeVector<uint16_t>(allWorkerThreads, 1, 3, 1);
+    BenchmarkUtils::createRangeVector<uint16_t>(allWorkerThreads, 1, 4, 1);
 
     // Number of workerThreads in nodeEngine
     std::vector<uint16_t> allCoordinatorThreads;
-    BenchmarkUtils::createRangeVector<uint16_t>(allCoordinatorThreads, 1, 3, 1);
+    BenchmarkUtils::createRangeVector<uint16_t>(allCoordinatorThreads, 1, 4, 1);
 
     // Number of dataSources
     std::vector<uint16_t> allDataSources;
     BenchmarkUtils::createRangeVector<uint16_t>(allDataSources, 1, 2, 1);
 
     // source mode
-    std::vector<E2EBase::InputOutputMode> allSourceModes{E2EBase::InputOutputMode::CacheMode, E2EBase::InputOutputMode::MemMode};
+    std::vector<E2EBase::InputOutputMode> allSourceModes{E2EBase::InputOutputMode::MemMode};
+//    std::vector<E2EBase::InputOutputMode> allSourceModes{E2EBase::InputOutputMode::CacheMode, E2EBase::InputOutputMode::MemMode};
     //        std::vector<E2EBase::InputOutputMode> allSourceModes {E2EBase::InputOutputMode::MemMode};
     //            std::vector<E2EBase::InputOutputMode> allSourceModes {E2EBase::InputOutputMode::CacheMode};
     //        std::vector<E2EBase::InputOutputMode> allSourceModes {E2EBase::InputOutputMode::FileMode};
