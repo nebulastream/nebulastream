f3c9f47989a7a601e50ef21987b2cd1773309317
Philipp Grulich <philippgrulich@hotmail.de>
2021-03-13 21:54:09 +0100

    #1694 add query plan iterator

---
+++ src/Plans/Utils/QueryPlanIterator.cpp
@@ -0,0 +1,67 @@
+/*
+    Copyright (C) 2020 by the NebulaStream project (https://nebula.stream)
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+#include <Plans/Query/QueryPlan.hpp>
+#include <Plans/Utils/QueryPlanIterator.hpp>
+
+namespace NES {
+
+QueryPlanIterator::QueryPlanIterator(QueryPlanPtr queryPlan) : queryPlan(std::move(queryPlan)){};
+
+QueryPlanIterator::iterator QueryPlanIterator::begin() { return iterator(queryPlan); }
+
+QueryPlanIterator::iterator QueryPlanIterator::end() { return iterator(); }
+
+QueryPlanIterator::iterator::iterator(QueryPlanPtr current) {
+    auto rootOperators = current->getRootOperators();
+    for (long i = rootOperators.size() - 1; i >= 0; i--) {
+        workStack.push(rootOperators[i]);
+    }
+}
+
+QueryPlanIterator::iterator::iterator() = default;
+
+bool QueryPlanIterator::iterator::operator!=(const iterator& other) const {
+    if (workStack.empty() && other.workStack.empty()) {
+        return false;
+    };
+    return true;
+};
+
+NodePtr QueryPlanIterator::iterator::operator*() { return workStack.top(); }
+
+QueryPlanIterator::iterator& QueryPlanIterator::iterator::operator++() {
+    if (workStack.empty()) {
+        NES_DEBUG("Iterator: we reached the end of this iterator and will not do anything.");
+    } else {
+        auto current = workStack.top();
+        workStack.pop();
+        auto children = current->getChildren();
+        for (long i = children.size() - 1; i >= 0; i--) {
+
+            auto child = children[i];
+            NES_ASSERT(child->getParents().size() != 0,
+                       "A child node should have a parend!");
+
+            // check if current node is last parent of child.
+            if(child->getParents().back() == current){
+                workStack.push(child);
+            }
+        }
+    }
+    return *this;
+}
+
+}// namespace NES
