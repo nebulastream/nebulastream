b3002ca4a98c8c6441232490d22a88260b6e4a94
zeuchste <steffen.zeuch@dfki.de>
2021-03-15 20:09:01 +0100

    fix error

--- src/NodeEngine/QueryManager.cpp
+++ src/NodeEngine/QueryManager.cpp
@@ -447,6 +447,7 @@ void QueryManager::addWork(const OperatorId operatorId, TupleBuffer& buf) {
                || taskQueue.size() > upperBound) {
             std::this_thread::sleep_for(std::chrono::milliseconds(100));
             waitCounter++;
+            NES_DEBUG("Waiting");
             //TODO: we have to do this because it could be that a source is stuck here and then the shutdown crashes, so basically we test 100x for 100ms
             // and then release the break
             if (tryCnt++ == 100) {
