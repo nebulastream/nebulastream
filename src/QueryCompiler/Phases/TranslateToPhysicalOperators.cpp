33c1b8803546d65e157ebc64001c2a1e307f02fb
Philipp Grulich <philippgrulich@hotmail.de>
2021-03-20 09:52:48 +0100

    #1714 fix return type

--- src/QueryCompiler/Phases/TranslateToPhysicalOperators.cpp
+++ src/QueryCompiler/Phases/TranslateToPhysicalOperators.cpp
@@ -13,26 +13,24 @@
     See the License for the specific language governing permissions and
     limitations under the License.
 */
+#include <Operators/LogicalOperators/LogicalOperatorNode.hpp>
 #include <Plans/Query/QueryPlan.hpp>
 #include <Plans/Utils/QueryPlanIterator.hpp>
-#include <Operators/LogicalOperators/LogicalOperatorNode.hpp>
+#include <QueryCompiler/Phases/PhysicalOperatorProvider.hpp>
 #include <QueryCompiler/Phases/TranslateToPhysicalOperators.hpp>
 #include <QueryCompiler/QueryCompilerForwardDeclaration.hpp>
-#include <QueryCompiler/Phases/PhysicalOperatorProvider.hpp>
 
 namespace NES {
 namespace QueryCompilation {
 
-TranslateToPhysicalOperatorsPtr TranslateToPhysicalOperators::TranslateToPhysicalOperators::create(PhysicalOperatorProviderPtr provider) {
+TranslateToPhysicalOperatorsPtr
+TranslateToPhysicalOperators::TranslateToPhysicalOperators::create(PhysicalOperatorProviderPtr provider) {
     return std::make_shared<TranslateToPhysicalOperators>(provider);
 }
 
-TranslateToPhysicalOperators::TranslateToPhysicalOperators(PhysicalOperatorProviderPtr provider): provider(provider) {
+TranslateToPhysicalOperators::TranslateToPhysicalOperators(PhysicalOperatorProviderPtr provider) : provider(provider) {}
 
-}
-
-QueryCompilation::PhysicalQueryPlanPtr TranslateToPhysicalOperators::apply(QueryPlanPtr queryPlan) {
-
+QueryPlanPtr TranslateToPhysicalOperators::apply(QueryPlanPtr queryPlan) {
     auto iterator = QueryPlanIterator(queryPlan);
     std::vector<NodePtr> nodes;
     for (auto node : iterator) {
@@ -41,8 +39,8 @@ QueryCompilation::PhysicalQueryPlanPtr TranslateToPhysicalOperators::apply(Query
     for (auto node : nodes) {
         provider->lower(queryPlan, node->as<LogicalOperatorNode>());
     }
+    return queryPlan;
 }
-
 
 }// namespace QueryCompilation
 }// namespace NES
