3cc6aede64047afa3bfa9c9b304e3c390a4b05d2
Nils Schubert <n.schubert@campus.tu-berlin.de>
2021-03-26 12:01:41 +0100

    #1641 improved readRecord() for row layout

--- benchmark/src/Micro-Benchmarks/BenchmarkDynamicMemoryLayout.cpp
+++ benchmark/src/Micro-Benchmarks/BenchmarkDynamicMemoryLayout.cpp
@@ -2605,10 +2605,10 @@ static void BM_WritingNumberOfFieldsColLayoutNewLayout(benchmark::State& state) 
 }
 
 #define REPETITIONS 20
-BENCHMARK(BM_WriteRecordsRowLayoutNewLayout)->Repetitions(REPETITIONS)->ReportAggregatesOnly(true);
-BENCHMARK(BM_WriteRecordsColumnLayoutNewLayout)->Repetitions(REPETITIONS)->ReportAggregatesOnly(true);
-//BENCHMARK(BM_ReadRecordsRowLayoutNewLayout)->Repetitions(REPETITIONS)->ReportAggregatesOnly(true);
-//BENCHMARK(BM_ReadRecordsColumnLayoutNewLayout)->Repetitions(REPETITIONS)->ReportAggregatesOnly(true);
+//BENCHMARK(BM_WriteRecordsRowLayoutNewLayout)->Repetitions(REPETITIONS)->ReportAggregatesOnly(true);
+//BENCHMARK(BM_WriteRecordsColumnLayoutNewLayout)->Repetitions(REPETITIONS)->ReportAggregatesOnly(true);
+BENCHMARK(BM_ReadRecordsRowLayoutNewLayout)->Repetitions(REPETITIONS)->ReportAggregatesOnly(true);
+BENCHMARK(BM_ReadRecordsColumnLayoutNewLayout)->Repetitions(REPETITIONS)->ReportAggregatesOnly(true);
 //
 //BENCHMARK(BM_WriteFieldRowLayoutNewLayout)->Repetitions(REPETITIONS)->ReportAggregatesOnly(true);
 //BENCHMARK(BM_WriteFieldColumnLayoutNewLayout)->Repetitions(REPETITIONS)->ReportAggregatesOnly(true);
