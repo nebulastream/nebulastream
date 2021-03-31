42abf033db3f1a01169e7469487da46e63fae3a6
Philipp Grulich <philippgrulich@hotmail.de>
2021-03-31 14:35:19 +0200

    #1750 add pragma to ignore warning

---
+++ include/Util/DisableWarningsPragma.hpp
@@ -0,0 +1,47 @@
+#ifndef NES_INCLUDE_UTIL_DISABLEWARNINGSPRAGMA_HPP_
+#define NES_INCLUDE_UTIL_DISABLEWARNINGSPRAGMA_HPP_
+
+/**
+ * @brief The following pragma disables particular compiler warnings for a particular code fragment.
+ * For example:
+ *
+ *  DISABLE_WARNING_PUSH
+ *  DISABLE_WARNING_UNREFERENCED_FUNCTION
+        ---
+        code where you want
+        to disable the warnings
+        ---
+ *  DISABLE_WARNING_POP
+ *
+ *  Disables unreferenced function warning.
+ *
+ *  The macro works for GCC, CLANG, and Visual Studio.
+ *  Details here: https://www.fluentcpp.com/2019/08/30/how-to-disable-a-warning-in-cpp
+ *
+ */
+#if defined(_MSC_VER)
+#define DISABLE_WARNING_PUSH           __pragma(warning( push ))
+    #define DISABLE_WARNING_POP            __pragma(warning( pop ))
+    #define DISABLE_WARNING(warningNumber) __pragma(warning( disable : warningNumber ))
+
+    #define DISABLE_WARNING_UNREFERENCED_FORMAL_PARAMETER    DISABLE_WARNING(4100)
+    #define DISABLE_WARNING_UNREFERENCED_FUNCTION            DISABLE_WARNING(4505)
+    // other warnings you want to deactivate...
+
+#elif defined(__GNUC__) || defined(__clang__)
+    #define DO_PRAGMA(X) _Pragma(#X)
+    #define DISABLE_WARNING_PUSH           DO_PRAGMA(GCC diagnostic push)
+    #define DISABLE_WARNING_POP            DO_PRAGMA(GCC diagnostic pop)
+    #define DISABLE_WARNING(warningName)   DO_PRAGMA(GCC diagnostic ignored #warningName)
+    #define DISABLE_WARNING_UNREFERENCED_FORMAL_PARAMETER    DISABLE_WARNING(-Wunused-parameter)
+    #define DISABLE_WARNING_UNREFERENCED_FUNCTION            DISABLE_WARNING(-Wunused-function)
+    // other warnings you want to deactivate...
+#else
+#define DISABLE_WARNING_PUSH
+    #define DISABLE_WARNING_POP
+    #define DISABLE_WARNING_UNREFERENCED_FORMAL_PARAMETER
+    #define DISABLE_WARNING_UNREFERENCED_FUNCTION
+    // other warnings you want to deactivate...
+#endif
+
+#endif//NES_INCLUDE_UTIL_DISABLEWARNINGSPRAGMA_HPP_
