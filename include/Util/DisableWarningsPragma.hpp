a623f2b0619e074268f6815af5c529a725fead7a
Philipp Grulich <philippgrulich@hotmail.de>
2021-03-31 20:48:53 +0200

    #1714 fix pragma

--- include/Util/DisableWarningsPragma.hpp
+++ include/Util/DisableWarningsPragma.hpp
@@ -48,8 +48,8 @@
 #define DISABLE_WARNING_PUSH DO_PRAGMA(GCC diagnostic push)
 #define DISABLE_WARNING_POP DO_PRAGMA(GCC diagnostic pop)
 #define DISABLE_WARNING(warningName) DO_PRAGMA(GCC diagnostic ignored #warningName)
-#define DISABLE_WARNING_UNREFERENCED_FORMAL_PARAMETER DISABLE_WARNING(-Wunused - parameter)
-#define DISABLE_WARNING_UNREFERENCED_FUNCTION DISABLE_WARNING(-Wunused - function)
+#define DISABLE_WARNING_UNREFERENCED_FORMAL_PARAMETER DISABLE_WARNING(-Wunused-parameter)
+#define DISABLE_WARNING_UNREFERENCED_FUNCTION DISABLE_WARNING(-Wunused-function)
 // other warnings you want to deactivate...
 #else
 #define DISABLE_WARNING_PUSH
