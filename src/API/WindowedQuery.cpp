b56c22ced2b314b360695a65fa1d7104b16ffa01
flo <floren-tine@web.de>
2021-04-08 15:08:58 +0200

    [#1728] changes to WindowedQuery

--- src/API/WindowedQuery.cpp
+++ src/API/WindowedQuery.cpp
@@ -18,8 +18,8 @@
 #include <API/Query.hpp>
 #include <Nodes/Expressions/ExpressionNode.hpp>
 #include <Nodes/Expressions/FieldAssignmentExpressionNode.hpp>
-#include <Operators/LogicalOperators/Arity/BinaryOperatorNode.hpp>
-#include <Operators/LogicalOperators/Arity/UnaryOperatorNode.hpp>
+#include <Operators/LogicalOperators/LogicalBinaryOperatorNode.hpp>
+#include <Operators/LogicalOperators/LogicalUnaryOperatorNode.hpp>
 #include <Operators/LogicalOperators/Sinks/SinkLogicalOperatorNode.hpp>
 #include <Operators/LogicalOperators/Sources/LogicalStreamSourceDescriptor.hpp>
 #include <Operators/LogicalOperators/WatermarkAssignerLogicalOperatorNode.hpp>
