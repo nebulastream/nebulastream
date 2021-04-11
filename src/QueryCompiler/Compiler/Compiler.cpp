40eae3afaf2d17ec8732c978a07ba5bdb548a9ed
zeuchste <steffen.zeuch@dfki.de>
2021-04-11 10:01:18 +0200

    [#1764] change to benchmark flags

--- src/QueryCompiler/Compiler/Compiler.cpp
+++ src/QueryCompiler/Compiler/Compiler.cpp
@@ -76,13 +76,17 @@ CompiledCodePtr Compiler::compile(const std::string& source) {
 #endif
 
 #ifdef NES_RELEASE_MODE
-    std::cout << "use release flags" << std::endl;
-    flags = CompilerFlags::createOptimizingCompilerFlags();
-#endif
-
 #ifdef NES_BENCHMARKS_MODE
     std::cout << "use benchmark flags" << std::endl;
     flags = CompilerFlags::createBenchmarkingCompilerFlags();
+#elif
+    std::cout << "use release flags" << std::endl;
+    flags = CompilerFlags::createOptimizingCompilerFlags();
+#endif
+#endif
+
+#ifdef NES_BENCHMARKS_MODE
+
 #endif
 
     flags->addFlag("--shared");
