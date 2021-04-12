c8ee32c94e636adbea75912c5418b918d3d69bfe
zeuchste <steffen.zeuch@dfki.de>
2021-04-12 15:37:00 +0200

    [#1771] check memory source

--- src/QueryCompiler/Compiler/Compiler.cpp
+++ src/QueryCompiler/Compiler/Compiler.cpp
@@ -76,9 +76,11 @@ CompiledCodePtr Compiler::compile(const std::string& source) {
 #endif
 
 #ifdef NES_RELEASE_MODE
-#ifdef NES_BENCHMARKS_MODE
+#ifdef DNES_BENCHMARKS_FLAG_MODE
+    std::cout << "use benchmark flags" << std::endl;
     flags = CompilerFlags::createBenchmarkingCompilerFlags();
 #else
+    std::cout << "use release flags" << std::endl;
     flags = CompilerFlags::createOptimizingCompilerFlags();
 #endif
 #endif
