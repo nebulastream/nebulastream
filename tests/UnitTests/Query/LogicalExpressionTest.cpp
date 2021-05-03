f2848efeb1dfcb0865121f0179ce92df3fa9e223
Julius Huelsmann <juliusHuelsmann@gmail.com>
2021-05-03 09:43:54 +0200

    [#1780] test: Add tests which test logical expressions
    Add tests that test if logical expressions compile from the desingated
    types that construct ExpressionItems.

--- tests/UnitTests/Query/LogicalExpressionTest.cpp
+++ tests/UnitTests/Query/LogicalExpressionTest.cpp
@@ -16,6 +16,7 @@
 
 #include <Util/Logger.hpp>
 #include <Util/UtilityFunctions.hpp>
+#include <Util/TestHarness/TestHarness.hpp>
 
 #include <gtest/gtest.h>
 
@@ -37,9 +38,11 @@ class LogicalExpressionTest : public testing::Test {
         NES_DEBUG("LogicalExpressionTest: Tear down QueryExecutionTest test case.");
     }
 
-    static void TearDownTestCase() { NES_DEBUG("LogicalExpressionTest: Tear down QueryExecutionTest test class."); }
+    static void TearDownTestCase() {
+        NES_DEBUG("LogicalExpressionTest: Tear down QueryExecutionTest test class.");
+    }
 
-    inline static void testBinaryOperator(std::string const &op) noexcept {
+    inline static void testBinaryOperator(std::string const& op) noexcept {
 
         std::vector<std::tuple<std::string, std::string>> pairs{
             {"0", R"(Attribute("value"))"},
@@ -77,28 +80,126 @@ class LogicalExpressionTest : public testing::Test {
             auto const q1 = R"(Query::from("").filter()" + v1 + op + v2 + R"( );)";
             EXPECT_NO_THROW(UtilityFunctions::createQueryFromCodeString(q1));
 
-            auto const q2 = R"(Query::from("").filter()" + v1 + op + v2 + R"( );)";
+            auto const q2 = R"(Query::from("").filter()" + v2 + op + v1 + R"( );)";
             EXPECT_NO_THROW(UtilityFunctions::createQueryFromCodeString(q2));
         }
     }
 };
 
+/// Template that can check that different Operators can be used with all designated data types that construct an
+/// ExpressionItem.
+template<template<typename, auto...> typename CompilesFromArgs, template<typename ...> typename CompilesFromTypes>
+void checkBinary() {
+
+    bool compiles = CompilesFromArgs<ExpressionNodePtr, 0, 0>::value;
+    ASSERT_TRUE(compiles);
+
+    compiles = CompilesFromTypes<ExpressionNodePtr, ExpressionItem, int8_t>::value
+        && CompilesFromTypes<ExpressionNodePtr, int8_t, ExpressionItem>::value;
+    ASSERT_TRUE(compiles);
+
+    compiles = CompilesFromTypes<ExpressionNodePtr, ExpressionItem, uint8_t>::value
+        && CompilesFromTypes<ExpressionNodePtr, uint8_t, ExpressionItem>::value;
+    ASSERT_TRUE(compiles);
+
+    compiles = CompilesFromTypes<ExpressionNodePtr, ExpressionItem, int16_t>::value
+        && CompilesFromTypes<ExpressionNodePtr, int16_t, ExpressionItem>::value;
+    ASSERT_TRUE(compiles);
+
+    compiles = CompilesFromTypes<ExpressionNodePtr, ExpressionItem, uint16_t>::value
+        && CompilesFromTypes<ExpressionNodePtr, uint16_t, ExpressionItem>::value;
+    ASSERT_TRUE(compiles);
+
+    compiles = CompilesFromTypes<ExpressionNodePtr, ExpressionItem, int32_t>::value
+        && CompilesFromTypes<ExpressionNodePtr, int32_t, ExpressionItem>::value;
+    ASSERT_TRUE(compiles);
+
+    compiles = CompilesFromTypes<ExpressionNodePtr, ExpressionItem, uint32_t>::value
+        && CompilesFromTypes<ExpressionNodePtr, uint32_t, ExpressionItem>::value;
+    ASSERT_TRUE(compiles);
+
+    compiles = CompilesFromTypes<ExpressionNodePtr, ExpressionItem, int64_t>::value
+        && CompilesFromTypes<ExpressionNodePtr, int64_t, ExpressionItem>::value;
+    ASSERT_TRUE(compiles);
+
+    compiles = CompilesFromTypes<ExpressionNodePtr, ExpressionItem, uint64_t>::value
+        && CompilesFromTypes<ExpressionNodePtr, uint64_t, ExpressionItem>::value;
+    ASSERT_TRUE(compiles);
+
+    compiles = CompilesFromTypes<ExpressionNodePtr, ExpressionItem, float>::value
+        && CompilesFromTypes<ExpressionNodePtr, float, ExpressionItem>::value;
+    ASSERT_TRUE(compiles);
+
+    compiles = CompilesFromTypes<ExpressionNodePtr, ExpressionItem, double>::value
+        && CompilesFromTypes<ExpressionNodePtr, double, ExpressionItem>::value;
+    ASSERT_TRUE(compiles);
+
+    compiles = CompilesFromTypes<ExpressionNodePtr, ExpressionItem, bool>::value
+        && CompilesFromTypes<ExpressionNodePtr, bool, ExpressionItem>::value;
+    ASSERT_TRUE(compiles);
+
+    compiles = CompilesFromTypes<ExpressionNodePtr, ExpressionItem, char const*>::value
+        && CompilesFromTypes<ExpressionNodePtr, char const*, ExpressionItem>::value;
+    ASSERT_TRUE(compiles);
+
+    compiles = CompilesFromTypes<ExpressionNodePtr, ExpressionItem, ValueTypePtr>::value
+        && CompilesFromTypes<ExpressionNodePtr, ValueTypePtr, ExpressionItem>::value;
+    ASSERT_TRUE(compiles);
+
+    compiles = CompilesFromTypes<ExpressionNodePtr, ExpressionItem, ExpressionNodePtr>::value
+        && CompilesFromTypes<ExpressionNodePtr, ExpressionNodePtr, ExpressionItem>::value;
+    ASSERT_TRUE(compiles);
+}
+
+SETUP_COMPILE_TIME_TESTS(eq, operator==);
+SETUP_COMPILE_TIME_TESTS(neq, operator!=);
+SETUP_COMPILE_TIME_TESTS(land, operator&&);
+SETUP_COMPILE_TIME_TESTS(lor, operator||);
+SETUP_COMPILE_TIME_TESTS(leq, operator<=);
+SETUP_COMPILE_TIME_TESTS(geq, operator>=);
+SETUP_COMPILE_TIME_TESTS(lt, operator<);
+SETUP_COMPILE_TIME_TESTS(gt, operator>);
+
 //TODO: re-enable tests when finishing #1170, #1781
 
-TEST_F(LogicalExpressionTest, DISABLED_testEqualityExpression) { testBinaryOperator("=="); }
+TEST_F(LogicalExpressionTest, DISABLED_testEqualityExpression) {
+    checkBinary<eqCompiles, eqCompilesFromType>();
+    testBinaryOperator("==");
+}
 
-TEST_F(LogicalExpressionTest, DISABLED_testInequalityExpression) { testBinaryOperator("!="); }
+TEST_F(LogicalExpressionTest, DISABLED_testInequalityExpression) {
+    checkBinary<neqCompiles, neqCompilesFromType>();
+    testBinaryOperator("!=");
+}
 
-TEST_F(LogicalExpressionTest, DISABLED_testAndCompile) { testBinaryOperator("&&"); }
+TEST_F(LogicalExpressionTest, DISABLED_testAndCompile) {
+    checkBinary<landCompiles, landCompilesFromType>();
+    testBinaryOperator("&&");
+}
 
-TEST_F(LogicalExpressionTest, DISABLED_testOrExpression) { testBinaryOperator("||"); }
+TEST_F(LogicalExpressionTest, DISABLED_testOrExpression) {
+    checkBinary<lorCompiles, lorCompilesFromType>();
+    testBinaryOperator("||");
+}
 
-TEST_F(LogicalExpressionTest, DISABLED_testLeqExpression) { testBinaryOperator("<="); }
+TEST_F(LogicalExpressionTest, DISABLED_testLeqExpression) {
+    checkBinary<leqCompiles, leqCompilesFromType>();
+    testBinaryOperator("<=");
+}
 
-TEST_F(LogicalExpressionTest, DISABLED_testGeqExpression) { testBinaryOperator(">="); }
+TEST_F(LogicalExpressionTest, DISABLED_testGeqExpression) {
+    checkBinary<geqCompiles, geqCompilesFromType>();
+    testBinaryOperator(">=");
+}
 
-TEST_F(LogicalExpressionTest, DISABLED_testLtExpression) { testBinaryOperator("<"); }
+TEST_F(LogicalExpressionTest, DISABLED_testLtExpression) {
+    checkBinary<ltCompiles, ltCompilesFromType>();
+    testBinaryOperator("<");
+}
 
-TEST_F(LogicalExpressionTest, DISABLED_testGtExpression) { testBinaryOperator(">"); }
+TEST_F(LogicalExpressionTest, DISABLED_testGtExpression) {
+    checkBinary<gtCompiles, gtCompilesFromType>();
+    testBinaryOperator(">");
+}
 
 }
