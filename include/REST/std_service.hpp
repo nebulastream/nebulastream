05e1fc6fd5449646299d3dde5ef03db1efd79764
Vincent Szlang <vincent.szlang@googlemail.com>
2021-05-06 14:34:42 +0200

    #1796 - removed boost headers in std_service.hpp with that also removed unused include statement in ConnectivityController.cpp, QueryController.cpp and TopologyController.cpp. Moreover removed NetworkUtils.hpp as it is no longer being used in NES. Removed NetworkUtils.hpp from CMakeLists.txt.

--- include/REST/std_service.hpp
+++ include/REST/std_service.hpp
@@ -18,21 +18,6 @@
 
 #define BOOST_SPIRIT_THREADSAFE// enable thread safety to json read on property tree!
 
-#include <boost/format.hpp>
-
-#include <boost/log/core.hpp>
-#include <boost/log/expressions.hpp>
-#include <boost/log/support/date_time.hpp>
-#include <boost/log/trivial.hpp>
-#include <boost/log/utility/setup/common_attributes.hpp>
-#include <boost/log/utility/setup/console.hpp>
-
-#include <boost/property_tree/json_parser.hpp>
-#include <boost/property_tree/ptree.hpp>
-
-#include <boost/algorithm/string.hpp>
-#include <boost/asio.hpp>
-
 #include <iostream>
 #include <map>
 #include <regex>
