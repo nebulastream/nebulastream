2d73c25b97b7ffc730e7c6d73520bf70b61ef665
Julius Huelsmann <juliusHuelsmann@gmail.com>
2021-05-14 01:28:38 +0200

    [#1781] fix: invalid read in naming of thread

--- src/Util/ThreadNaming.cpp
+++ src/Util/ThreadNaming.cpp
@@ -37,13 +37,15 @@ void setThreadName(const char* threadNameFmt, ...) {
     va_list args;
     va_start(args, threadNameFmt);
     vsprintf(buffer, threadNameFmt, args);
-    auto sz = std::min<uint64_t>(15, std::strlen(buffer));
+    // We need to copy the null terminator from buffer if any and if strlen(...) < 15 in order to not
+    // display uninitialized memory. Therefore, increase strlen by one.
+    auto sz = std::min<uint64_t>(15, std::strlen(buffer) + 1);
     std::strncpy(resized_buffer, buffer, sz);
+    resized_buffer[sz] = 0;
     std::string thName(resized_buffer);
     //this will add the thread name in the log
     MDC::put("threadName", thName);
 
-    resized_buffer[sz] = 0;
 #ifdef HAS_POSIX_THREAD
     pthread_setname_np(pthread_self(), resized_buffer);
 #endif
