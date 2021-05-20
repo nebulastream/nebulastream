b87febd6131527f6862940e23e54f68210f1cf1c
Philipp Grulich <philippgrulich@hotmail.de>
2021-05-20 08:53:15 +0200

    [#1888] add namespace to code gen

--- src/QueryCompiler/CodeGenerator/CCodeGenerator/FileBuilder.cpp
+++ src/QueryCompiler/CodeGenerator/CCodeGenerator/FileBuilder.cpp
@@ -50,7 +50,8 @@ FileBuilder FileBuilder::create(const std::string&) {
                           "#include <Windowing/WindowAggregations/ExecutableMinAggregation.hpp>\n"
                           "#include <Windowing/WindowAggregations/ExecutableMaxAggregation.hpp>\n"
                           "#include <Windowing/WindowActions/ExecutableSliceAggregationTriggerAction.hpp>\n"
-                          "#include <Windowing/WindowActions/ExecutableCompleteAggregationTriggerAction.hpp>"
+                          "#include <Windowing/WindowActions/ExecutableCompleteAggregationTriggerAction.hpp>\n"
+                          "using namespace NES::QueryCompilation;"
                        << std::endl;
 
     return builder;
