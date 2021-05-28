31708a8c8813c58a0219fdb24e42abcb5ebbd5b4
NES-CI <nebulastream@dima.tu-berlin.de>
2021-05-28 18:22:08 +0000

    GIT-CI: Updating NES version to 0.0.424

--- include/Version/version.hpp
+++ include/Version/version.hpp
@@ -18,6 +18,6 @@
 #define __VERSION_HPP__
 #define NES_VERSION_MAJOR 0
 #define NES_VERSION_MINOR 0
-#define NES_VERSION_PATCH 423
-constexpr char const* NES_VERSION = "0.0.423";
+#define NES_VERSION_PATCH 424
+constexpr char const* NES_VERSION = "0.0.424";
 #endif
