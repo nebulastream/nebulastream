548b0902995b2e08b170b705c9fb5190750e51b3
Julius Huelsmann <juliusHuelsmann@gmail.com>
2021-06-10 23:15:02 +0200

    [#261] c-t: bugprone checks
    Checks:
    bugprone-argument-comment,bugprone-assert-side-effect,bugprone-bad-signal-to-kill-thread,bugprone-bool-pointer-implicit-conversion,bugprone-branch-clone,bugprone-copy-constructor-init,bugprone-dangling-handle,bugprone-dynamic-static-initializers,bugprone-exception-escape,bugprone-fold-init-type,bugprone-forward-declaration-namespace,bugprone-forwarding-reference-overload,bugprone-inaccurate-erase,bugprone-incorrect-roundings,bugprone-infinite-loop,bugprone-integer-division,bugprone-lambda-function-name,bugprone-macro-parentheses,bugprone-macro-repeated-side-effects,bugprone-misplaced-operator-in-strlen-in-alloc,bugprone-misplaced-pointer-arithmetic-in-alloc,bugprone-misplaced-widening-cast,bugprone-move-forwarding-reference,bugprone-multiple-statement-macro,bugprone-narrowing-conversions,bugprone-no-escape,bugprone-not-null-terminated-result,bugprone-parent-virtual-call,bugprone-posix-return,bugprone-reserved-identifier,bugprone-signed-char-misuse,bugprone-sizeof-container,bugprone-sizeof-expression,bugprone-spuriously-wake-up-functions,bugprone-string-constructor,bugprone-string-integer-assignment,bugprone-string-literal-with-embedded-nul,bugprone-suspicious-enum-usage,bugprone-suspicious-include,bugprone-suspicious-memset-usage,bugprone-suspicious-missing-comma,bugprone-suspicious-semicolon,bugprone-suspicious-string-compare,bugprone-swapped-arguments,bugprone-terminating-continue,bugprone-throw-keyword-missing,bugprone-too-small-loop-variable,bugprone-undefined-memory-manipulation,bugprone-undelegated-constructor,bugprone-unhandled-self-assignment,bugprone-unused-raii,bugprone-unused-return-value,bugprone-use-after-move,bugprone-virtual-near-miss

--- tests/util/NESTest.hpp
+++ tests/util/NESTest.hpp
@@ -18,8 +18,8 @@
 #include <gtest/gtest.h>
 #include <typeinfo>
 #define ASSERT_INSTANCE_OF(node, instance)                                                                                       \
-    if (!node->instanceOf<instance>()) {                                                                                         \
-        auto message = node->toString() + " is not of instance " + std::string(typeid(instance).name());                         \
+    if (!(node)->instanceOf<instance>()) {                                                                                         \
+        auto message = (node)->toString() + " is not of instance " + std::string(typeid(instance).name());                         \
         GTEST_FATAL_FAILURE_(message.c_str());                                                                                   \
     }
 #endif//NES_TESTS_UTIL_NESTEST_HPP_
