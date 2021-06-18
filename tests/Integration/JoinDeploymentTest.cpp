827d4b386adad07aa4a6db29b1c4db8cde701583
Dwi Prasetyo Adi Nugroho <dwiprasetyo.an@gmail.com>
2021-06-18 08:32:16 +0200

    [#1922] addded a join deployment test

--- tests/Integration/JoinDeploymentTest.cpp
+++ tests/Integration/JoinDeploymentTest.cpp
@@ -14,7 +14,11 @@
     limitations under the License.
 */
 
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-copy-dtor"
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
+#pragma clang diagnostic pop
 
 #include <Catalogs/LambdaSourceStreamConfig.hpp>
 #include <Components/NesCoordinator.hpp>
@@ -24,8 +28,10 @@
 #include <Configurations/ConfigOptions/WorkerConfig.hpp>
 #include <Plans/Global/Query/GlobalQueryPlan.hpp>
 #include <Plans/Query/QueryId.hpp>
+#include <QueryCompiler/GeneratableTypes/Array.hpp>
 #include <Services/QueryService.hpp>
 #include <Util/Logger.hpp>
+#include <Util/TestHarness/TestHarness.hpp>
 #include <Util/TestUtils.hpp>
 #include <iostream>
 
@@ -1455,5 +1461,82 @@ TEST_F(JoinDeploymentTest, DISABLED_testJoinBenchmarkQuery) {
     bool retStopCord = crd->stopCoordinator(true);
     EXPECT_TRUE(retStopCord);
     NES_INFO("JoinDeploymentTest: Test finished");
+}
+
+/*
+ * @brief Test a join query that uses fixed-array as keys
+ */
+TEST_F(JoinDeploymentTest, testJoinWithFixedCharKey) {
+    struct Window1 {
+        NES::QueryCompilation::Array<char, 8> id1;
+        uint64_t timestamp;
+    };
+
+    struct Window2 {
+        NES::QueryCompilation::Array<char, 8> id2;
+        uint64_t timestamp;
+    };
+
+    auto window1Schema = Schema::create()
+        ->addField("id1", DataTypeFactory::createFixedChar(8))
+        ->addField("timestamp", DataTypeFactory::createUInt64());
+
+    auto window2Schema = Schema::create()
+        ->addField("id2", DataTypeFactory::createFixedChar(8))
+        ->addField("timestamp", DataTypeFactory::createUInt64());
+
+    ASSERT_EQ(sizeof(Window1), window1Schema->getSchemaSizeInBytes());
+    ASSERT_EQ(sizeof(Window2), window2Schema->getSchemaSizeInBytes());
+
+    std::string queryWithJoinOperator =
+        R"(Query::from("window1")
+            .joinWith(Query::from("window2")).where(Attribute("id1")).equalsTo(Attribute("id2")).window(TumblingWindow::of(EventTime(Attribute("timestamp")), Milliseconds(1000)))
+            .project(Attribute("window1window2$start"),Attribute("window1window2$end"), Attribute("window1window2$key"),  Attribute("window1$timestamp"))
+        )";
+    TestHarness testHarness = TestHarness(queryWithJoinOperator, restPort, rpcPort);
+
+    testHarness.addMemorySource("window1", window1Schema, "window1");
+    testHarness.addMemorySource("window2", window2Schema, "window2");
+
+    ASSERT_EQ(testHarness.getWorkerCount(), 2u);
+
+    testHarness.pushElement<Window1>({"aaaaaaa", 1000u}, 0u);
+    testHarness.pushElement<Window2>({"bbbbbbb", 1001u}, 0u);
+    testHarness.pushElement<Window2>({"ccccccc", 1002u}, 0u);
+    testHarness.pushElement<Window2>({"aaaaaaa", 2000u}, 0u);
+    testHarness.pushElement<Window2>({"ddddddd", 2001u}, 0u);
+    testHarness.pushElement<Window2>({"eeeeeee", 2002u}, 0u);
+    testHarness.pushElement<Window2>({"aaaaaaa", 3000u}, 0u);
+
+    testHarness.pushElement<Window2>({"fffffff", 1003u}, 1u);
+    testHarness.pushElement<Window2>({"bbbbbbb", 1011u}, 1u);
+    testHarness.pushElement<Window2>({"ccccccc", 1102u}, 1u);
+    testHarness.pushElement<Window2>({"ccccccc", 1112u}, 1u);
+    testHarness.pushElement<Window2>({"aaaaaaa", 2010u}, 1u);
+    testHarness.pushElement<Window2>({"ddddddd", 2301u}, 1u);
+    testHarness.pushElement<Window2>({"ggggggg", 3100u}, 1u);
+
+    struct Output {
+        uint64_t window1window2$start;
+        uint64_t window1window2$end;
+        NES::QueryCompilation::Array<char, 8> window1window2$key;
+        uint64_t window2$timestamp;
+
+        // overload the == operator to check if two instances are the same
+        bool operator==(Output const& rhs) const {
+            return (window1window2$start == rhs.window1window2$start && window1window2$end == rhs.window1window2$end
+                    && window1window2$key == rhs.window1window2$key && window2$timestamp == rhs.window2$timestamp
+                    );
+        }
+    };
+
+    std::vector<Output> expectedOutput = {{2000, 3000, "ddddddd", 2001},
+                                          {1000, 2000, "ccccccc", 1002},
+                                          {1000, 2000, "ccccccc", 1002},
+                                          {1000, 2000, "bbbbbbb", 1001}};
+    std::vector<Output> actualOutput = testHarness.getOutput<Output>(expectedOutput.size(), "BottomUp");
+
+    EXPECT_EQ(actualOutput.size(), expectedOutput.size());
+    EXPECT_THAT(actualOutput, ::testing::UnorderedElementsAreArray(expectedOutput));
 }
 }// namespace NES
