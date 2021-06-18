14e1fb855f6ac477815546fae16709d811da86d4
Nithish Sankaranarayanan <nithishsankaranarayanan@gmail.com>
2021-06-18 17:52:24 +0530

    [#1808] Multi SubQueries can send message to same source

--- src/Network/OutputChannel.cpp
+++ src/Network/OutputChannel.cpp
@@ -32,13 +32,14 @@ OutputChannel::OutputChannel(zmq::socket_t&& zmqSocket, const ChannelId channelI
 
 std::unique_ptr<OutputChannel> OutputChannel::create(std::shared_ptr<zmq::context_t> const& zmqContext,
                                                      std::string&& socketAddr,
+                                                     QuerySubPlanId querySubPlanId,
                                                      NesPartition nesPartition,
                                                      ExchangeProtocol& protocol,
                                                      std::chrono::seconds waitTime,
                                                      uint8_t retryTimes) {
     std::chrono::seconds backOffTime = waitTime;
     try {
-        ChannelId channelId(nesPartition, Runtime::NesThread::getId());
+        ChannelId channelId(querySubPlanId, nesPartition, Runtime::NesThread::getId());
         zmq::socket_t zmqSocket(*zmqContext, ZMQ_DEALER);
         constexpr int linger = -1;
         NES_DEBUG("OutputChannel: Connecting with zmq-socketopt linger=" << std::to_string(linger) << ", id=" << channelId);
