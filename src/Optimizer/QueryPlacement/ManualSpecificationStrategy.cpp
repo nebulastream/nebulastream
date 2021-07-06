3e4949920c5838120dd36380ee07e327829152fc
Dwi Prasetyo Adi Nugroho <dwiprasetyo.an@gmail.com>
2021-07-06 12:22:55 +0200

    [#2022] add some docs

--- src/Optimizer/QueryPlacement/ManualSpecificationStrategy.cpp
+++ src/Optimizer/QueryPlacement/ManualSpecificationStrategy.cpp
@@ -1,3 +1,19 @@
+/*
+    Copyright (C) 2020 by the NebulaStream project (https://nebula.stream)
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
 #include <Optimizer/QueryPlacement/ManualSpecificationStrategy.hpp>
 #include <Plans/Query/QueryPlan.hpp>
 #include <utility>
@@ -25,11 +41,13 @@ NES::Optimizer::ManualSpecificationStrategy::ManualSpecificationStrategy(NES::Gl
                             std::move(streamCatalog)) {}
 
 bool NES::Optimizer::ManualSpecificationStrategy::updateGlobalExecutionPlan(NES::QueryPlanPtr queryPlan) {
+    // check if user has specify binary mapping for placement
     if (binaryMapping.empty()) {
         NES_ERROR("ManualSpecificationStrategy::updateGlobalExecutionPlan: binary mapping is not set");
         return false;
     }
 
+    // apply the placement from the specified binary mapping
     assignMappingToTopology(topology, queryPlan, this->binaryMapping);
     addNetworkSourceAndSinkOperators(queryPlan);
     runTypeInferencePhase(queryPlan->getQueryId());
