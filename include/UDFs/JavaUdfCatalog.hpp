f523811e00fc8c7501113ee9e75f3860f68a150e
Viktor Rosenfeld <viktor.rosenfeld@tu-berlin.de>
2021-08-05 01:05:10 +0200

    [#2080] Add class documentation of JavaUdfCatalog and JavaUdfImplementation

--- include/UDFs/JavaUdfCatalog.hpp
+++ include/UDFs/JavaUdfCatalog.hpp
@@ -24,18 +24,48 @@
 
 namespace NES {
 
+/**
+ * @brief The Java UDF catalog stores all the data required to execute a Java UDF inside an embedded JVM.
+ *
+ * It provides an API to register and remove UDFs from a client,
+ * to retrieve the implementation data during query rewrite,
+ * and to retrieve a list of registered UDFs for visualization.
+ */
 class JavaUdfCatalog {
   public:
-    // TODO use unique_ptr here to make sure that JavaUdfCatalog takes ownership of the pointer
+    /**
+     * @brief Register the implementation data of a Java UDF.
+     * @param name The name of the UDF as it is used in queries.
+     * @param implementation The implementation data of the UDF.
+     * @throws UdfException If implementation is nullptr or if a UDF under the name is already registered.
+     */
+    // TODO use unique_ptr here to make sure that JavaUdfCatalog takes ownership of the implementation object
     void registerJavaUdf(const std::string& name, JavaUdfImplementationPtr implementation);
+
+    /**
+     * @brief Retrieve the implementation data for a Java UDF.
+     * @param name The name of the UDF as it is used in queries.
+     * @return The implementation data of the UDF, or nullptr if the UDF is not registered.
+     */
     JavaUdfImplementationPtr getUdfImplementation(const std::string& name);
+
+    /**
+     * @brief Remove the UDF from the catalog.
+     * @param name The name of the UDF as it is used in queries.
+     * @return True, if the UDF was registered in the catalog; otherwise, False.
+     *
+     * Removing an unregistered UDF is not an error condition because it could have been removed by another user.
+     * In this case, the user should just be notified.
+     */
     bool removeUdf(const std::string& name);
 
+    /**
+     * @brief Retrieve a list of registered UDFs.
+     * @return A STL container containing the names of the registered UDFs.
+     */
     // TODO Not sure if this is the correct API.
-    // I'd rather return an iterator over the keys.
-    // Or maybe we don't need this function at all.
-    // It's not really used to support UDFs.
-    // It could be used by a visualization tool.
+    // I'd rather return an iterator over the keys. Or maybe we don't need this function at all.
+    // It's not really used to support UDFs. It could be used by a visualization tool.
     // It would be nice to show more information about the UDFs (signature, size of dependencies),
     // but that's not needed right now.
     const std::vector<std::string> listUdfs() const;
