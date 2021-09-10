0c07d4637a0a1d159a7e684c3663e6016a66ed1d
flo <floren-tine@web.de>
2021-09-11 01:16:01 +0200

    [#2076] license preamble

--- tests/UnitTests/ServiceTests/StreamCatalogServiceTest.cpp
+++ tests/UnitTests/ServiceTests/StreamCatalogServiceTest.cpp
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+
 #include <gtest/gtest.h>
 #include <Catalogs/PhysicalStreamConfig.hpp>
 #include <Catalogs/StreamCatalog.hpp>
