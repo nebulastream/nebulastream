3f268c4db74e924e87b10a14cf805b80e2da6bf9
zeuchste <steffen.zeuch@dfki.de>
2021-09-24 14:36:19 +0200

    [#2160] fix build

--- src/Sources/MemorySource.cpp
+++ src/Sources/MemorySource.cpp
@@ -128,7 +128,7 @@ std::optional<Runtime::TupleBuffer> MemorySource::receiveData() {
         case COPY_BUFFER_SIMD_RTE: {
 #ifdef __x86_64__
             buffer = bufferManager->getBufferBlocking();
-            rte_memcpy(buffer.getBuffer(), numaLocalMemoryArea.get() + currentPositionInBytes, buffer.getBufferSize());
+            rte_memcpy(buffer.getBuffer(), numaLocalMemoryArea.getBuffer() + currentPositionInBytes, buffer.getBufferSize());
 #else
             NES_THROW_RUNTIME_ERROR("COPY_BUFFER_SIMD_RTE source mode is not supported.");
 #endif
