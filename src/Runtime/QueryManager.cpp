617588e09ad066599c9105dc5a8c83ab3b5ea92a
zeuchste <steffen.zeuch@dfki.de>
2021-10-04 12:55:29 +0200

    [#2160] add light weight monitoring

--- src/Runtime/QueryManager.cpp
+++ src/Runtime/QueryManager.cpp
@@ -980,6 +980,7 @@ void QueryManager::addWorkForNextPipeline(TupleBuffer& buffer, Execution::Succes
 
 void QueryManager::completedWork(Task& task, WorkerContext& wtx) {
     NES_DEBUG("QueryManager::completedWork: Work for task=" << task.toString());
+    std::cout << "QueryManager::completedWork: Work for task=" << task.toString() << std::endl;
     if (task.isReconfiguration()) {
         return;
     }
