d20bd383176490b6cc29272c73c06f43a8178eb6
Philipp Grulich <philippgrulich@hotmail.de>
2021-10-07 22:37:16 +0200

    introduce record assignment

--- include/QueryCompiler/Phases/OutputBufferAllocationStrategies.hpp
+++ include/QueryCompiler/Phases/OutputBufferAllocationStrategies.hpp
@@ -19,7 +19,14 @@
 
 namespace NES::QueryCompilation {
 
-enum OutputBufferAllocationStrategy : unsigned char {
+enum OutputBufferAssignmentStrategy : uint8_t {
+    // this strategy copies each field individually
+    FIELD_COPY,
+    // This strategy copies the whole record with an singe assignment
+    RECORD_COPY
+};
+
+enum OutputBufferAllocationStrategy : uint8_t{
     /// If all records and all fields match up in input and result buffer we can simply emit the input buffer.
     /// For this no filter can be applied and no new fields can be added.
     /// The only typical operations possible are inplace-maps, e.g. "id = id + 1".
