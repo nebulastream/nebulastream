8f636c5cc17010bcd328e44377bfe3ed2720efae
zeuchste <steffen.zeuch@dfki.de>
2021-10-15 14:36:34 +0200

    [#2252] disable light weight stats

--- tests/Integration/MultiThreadedTest.cpp
+++ tests/Integration/MultiThreadedTest.cpp
@@ -48,7 +48,7 @@ class MultiThreadedTest : public testing::Test {
     static void TearDownTestCase() { NES_INFO("Tear down MultiWorkerTest class."); }
 };
 
-TEST_F(MultiThreadedTest, DISABLED_testFilterQuery) {
+TEST_F(MultiThreadedTest, testFilterQuery) {
     CoordinatorConfigPtr crdConf = CoordinatorConfig::create();
     WorkerConfigPtr wrkConf = WorkerConfig::create();
     SourceConfigPtr srcConf = SourceConfig::create();
