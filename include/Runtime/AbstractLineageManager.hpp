966cf652612a8f0ff86612da28f9848f58eb7075
AKozar <kozar.anastasiia@gmail.com>
2021-10-27 13:39:35 +0200

    [#2264] formatting changed

---
+++ include/Runtime/AbstractLineageManager.hpp
@@ -0,0 +1,53 @@
+/*
+    Copyright (C) 2020 by the NebulaStream project (https://nebula.stream)
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#ifndef NES_ABSTRACTLINEAGEMANAGER_H
+#define NES_ABSTRACTLINEAGEMANAGER_H
+#include <Util/BufferSequenceNumber.hpp>
+#include <cstdio>
+
+namespace NES::Runtime {
+
+/**
+ * @brief The Abstract Lineage Manager class is used to map of all tuples that got their sequence number changed
+ * by stateful operators
+ */
+class AbstractLineageManager {
+  public:
+    virtual ~AbstractLineageManager() noexcept = default;
+
+    /**
+     * @brief Inserts a pair newId, oldId into lineage, where newId is a key
+     * @param newId new sequence number that was created by a stateful operator
+     * @param oldId old sequence number that the tuple had
+     */
+    virtual void insertIntoLineage(BufferSequenceNumber newId, BufferSequenceNumber oldId) = 0;
+
+    /**
+     * @brief Deletes a pair<newId,oldId> from lineage manager
+     * @param id newId of the tuple
+     * @return true in case of a success trimming
+     */
+    virtual bool trimLineage(BufferSequenceNumber id) = 0;
+
+    /**
+     * @brief Return current lineage size
+     * @return Current lineage size
+     */
+    virtual size_t getLineageSize() const = 0;
+};
+}// namespace NES::Runtime
+#endif//NES_ABSTRACTLINEAGEMANAGER_H
--- tests/UnitTests/Runtime/LineageManagerTest.cpp
+++ tests/UnitTests/Runtime/LineageManagerTest.cpp
@@ -23,6 +23,7 @@ namespace NES {
 const size_t buffersInserted = 21;
 const size_t emptyBuffer = 0;
 const size_t oneBuffer = 1;
+const size_t numberOfThreads = 21;
 
 class LineageManagerTest : public testing::Test {
 
@@ -80,15 +81,35 @@ TEST_F(LineageManagerTest, bufferDeletionFromLineageManager) {
 }
 
 /**
-     * @brief test inserts five buffers in different queues concurrently.
+     * @brief test checks that invert function returns null in case id doesn't exist
+*/
+TEST_F(LineageManagerTest, invertNonExistingId) {
+    auto lineageManager = std::make_shared<Runtime::LineageManager>();
+    ASSERT_EQ(lineageManager->invertBuffer(BufferSequenceNumber(0, 0)).isValid(), false);
+}
+
+/**
+     * @brief test check that the invert function returns old id of a buffer
+*/
+TEST_F(LineageManagerTest, invertExistingId) {
+    auto lineageManager = std::make_shared<Runtime::LineageManager>();
+    for (size_t i = 0; i < buffersInserted; i++) {
+        lineageManager->insertIntoLineage(BufferSequenceNumber(i, i), BufferSequenceNumber(i + 1, i + 1));
+        ASSERT_EQ(lineageManager->getLineageSize(), i + 1);
+    }
+    ASSERT_EQ(lineageManager->getLineageSize(), buffersInserted);
+    ASSERT_EQ(lineageManager->invertBuffer(BufferSequenceNumber(0, 0)), BufferSequenceNumber(1, 1));
+}
+
+/**
+     * @brief test inserts buffers in lineage concurrently.
 */
 TEST_F(LineageManagerTest, multithreadInsertionInLineage) {
     auto lineageManager = std::make_shared<Runtime::LineageManager>();
     std::vector<std::thread> t;
-    for (uint32_t i = 0; i < buffersInserted; i++) {
+    for (uint32_t i = 0; i < numberOfThreads; i++) {
         t.emplace_back([lineageManager, i]() {
             lineageManager->insertIntoLineage(BufferSequenceNumber(i, i), BufferSequenceNumber(i + 1, i + 1));
-            ASSERT_EQ(lineageManager->getLineageSize(), i + 1);
         });
     }
     for (auto& thread : t) {
@@ -96,4 +117,26 @@ TEST_F(LineageManagerTest, multithreadInsertionInLineage) {
     }
     ASSERT_EQ(lineageManager->getLineageSize(), buffersInserted);
 }
+
+/**
+     * @brief test deletes buffers from lineage concurrently.
+*/
+TEST_F(LineageManagerTest, multithreadDeletionFromLineage) {
+    auto lineageManager = std::make_shared<Runtime::LineageManager>();
+    std::vector<std::thread> t;
+    for (size_t i = 0; i < numberOfThreads; i++) {
+        lineageManager->insertIntoLineage(BufferSequenceNumber(i, i), BufferSequenceNumber(i + 1, i + 1));
+        ASSERT_EQ(lineageManager->getLineageSize(), i + 1);
+    }
+    ASSERT_EQ(lineageManager->getLineageSize(), buffersInserted);
+    for (uint32_t i = 0; i < numberOfThreads; i++) {
+        t.emplace_back([lineageManager, i]() {
+            lineageManager->trimLineage(BufferSequenceNumber(i, i));
+        });
+    }
+    for (auto& thread : t) {
+        thread.join();
+    }
+    ASSERT_EQ(lineageManager->getLineageSize(), emptyBuffer);
 }
+}// namespace NES
