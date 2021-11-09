22ef2c2f12a579da7d3ff570fdac432fb990b1f1
Dwi Nugroho <dwiprasetyo.an@gmail.com>
2021-11-09 23:32:07 +0100

    [#2187] add client originated query plan serializer

--- include/GRPC/Serialization/QueryPlanSerializationUtil.hpp
+++ include/GRPC/Serialization/QueryPlanSerializationUtil.hpp
@@ -37,14 +37,14 @@ class QueryPlanSerializationUtil {
      * @return the pointer to serialized SerializableQueryPlan
      */
     static void serializeQueryPlan(const QueryPlanPtr& queryPlan, SerializableQueryPlan* serializableQueryPlan);
-//
-//    /**
-//     * @brief Serializes a Query Plan from a client and all its root operators to a SerializableQueryPlan object.
-//     * @param queryPlan: The query plan
-//     * @param serializableQueryPlan: pointer to the serializable query Plan object
-//     * @return the pointer to serialized SerializableQueryPlan
-//     */
-//    static void serializeClientOriginatedQueryPlan(const QueryPlanPtr& queryPlan, SerializableQueryPlan* serializableQueryPlan);
+
+    /**
+     * @brief Serializes a Query Plan from a client and all its root operators to a SerializableQueryPlan object.
+     * @param queryPlan: The query plan
+     * @param serializableQueryPlan: pointer to the serializable query Plan object
+     * @return the pointer to serialized SerializableQueryPlan
+     */
+    static void serializeClientOriginatedQueryPlan(const QueryPlanPtr& queryPlan, SerializableQueryPlan* serializableQueryPlan);
 
     /**
      * @brief De-serializes the SerializableQueryPlan and all its root operators back to a QueryPlanPtr
