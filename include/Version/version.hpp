196c374628e755dcab08851210590af44096e1de
Philipp Grulich <philippgrulich@hotmail.de>
2021-12-05 23:35:00 +0100

    increment version

--- include/Version/version.hpp
+++ include/Version/version.hpp
@@ -17,7 +17,7 @@
 #ifndef __VERSION_HPP__
 #define __VERSION_HPP__
 #define NES_VERSION_MAJOR 0
-#define NES_VERSION_MINOR 0
-#define NES_VERSION_PATCH 581
-constexpr char const* NES_VERSION = "0.0.581";
+#define NES_VERSION_MINOR 1
+#define NES_VERSION_PATCH 0
+constexpr char const* NES_VERSION      = "0.1.0-d422fe5e2-SNAPSHOT";
 #endif
