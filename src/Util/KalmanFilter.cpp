179dc687f70393f0de29218da88d48987f5d6d75
digiou <digitalbckp@gmail.com>
2021-12-15 14:10:29 +0100

    [#1211] Use int for sensor values to match bench repo

--- src/Util/KalmanFilter.cpp
+++ src/Util/KalmanFilter.cpp
@@ -154,16 +154,16 @@ std::chrono::milliseconds KalmanFilter::getNewFrequency() {
 }
 
 void KalmanFilter::updateFromTupleBuffer(Runtime::TupleBuffer& tupleBuffer) {
-    NES_DEBUG("KalmanFilter::updateFromTupleBuffer: using a whole tuple buffer");
+    NES_DEBUG("KalmanFilter::updateFromTupleBuffer: updating from a whole tuple buffer");
     if (tupleBuffer.isValid()) {
         Eigen::VectorXd valueVector(1);
         auto numOfTuples = tupleBuffer.getNumberOfTuples();
         auto records = tupleBuffer.getBuffer<Sensors::SingleSensor>();
         for (uint64_t i = 0; i < numOfTuples; ++i) {
-            valueVector << records[i].sensedValue;
+            valueVector << records[i].value;
             this->update(valueVector);
         }
-        NES_DEBUG("KalmanFilter::updateFromTupleBuffer: consumed the whole buffer");
+        NES_DEBUG("KalmanFilter::updateFromTupleBuffer: consumed a whole buffer");
     }
 }
 
