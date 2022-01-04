ace059acf427ec5bd9432299518d3401e5126e8b
Balint Rueb <rueb@campus.tu-berlin.de>
2022-01-04 12:01:10 +0100

    #2394 address PR comments

--- nes-core/src/Phases/ConvertLogicalToPhysicalSink.cpp
+++ nes-core/src/Phases/ConvertLogicalToPhysicalSink.cpp
@@ -135,7 +135,7 @@ DataSinkPtr ConvertLogicalToPhysicalSink::createDataSink(OperatorId operatorId,
         NES_INFO("ConvertLogicalToPhysicalSink: Creating network sink");
         auto networkSinkDescriptor = sinkDescriptor->as<Network::NetworkSinkDescriptor>();
         return createNetworkSink(schema,
-                                 networkSinkDescriptor->getGlobalOperatorId(),
+                                 networkSinkDescriptor->getOperatorId(),
                                  querySubPlan->getQuerySubPlanId(),
                                  networkSinkDescriptor->getNodeLocation(),
                                  networkSinkDescriptor->getNesPartition(),
