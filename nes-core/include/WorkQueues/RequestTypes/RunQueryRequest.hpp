67b760fc1e5ed5655f770b72f0c1e00143911753
zeuchste <steffen.zeuch@dfki.de>
2022-01-04 21:07:52 +0100

    [#2399] fix build

--- nes-core/include/WorkQueues/RequestTypes/RunQueryRequest.hpp
+++ nes-core/include/WorkQueues/RequestTypes/RunQueryRequest.hpp
@@ -42,7 +42,7 @@ class RunQueryRequest : public Request {
     static RunQueryRequestPtr create(QueryPlanPtr queryPlan, std::string queryPlacementStrategy);
 
     /// Virtual destructor for inheritance
-//    ~RunQueryRequest() override = default;
+    ~RunQueryRequest() = default;
 
     /**
      * @brief Get the query plan to run
