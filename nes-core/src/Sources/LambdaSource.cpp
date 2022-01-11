ebbd64a897e90b14dbaf644916c3ea82d7c55e36
zeuchste <steffen.zeuch@dfki.de>
2022-01-11 17:33:35 +0100

    [#2399] make lambda source non blocking

--- nes-core/src/Sources/LambdaSource.cpp
+++ nes-core/src/Sources/LambdaSource.cpp
@@ -67,11 +67,12 @@ std::optional<Runtime::TupleBuffer> LambdaSource::receiveData() {
     NES_DEBUG("LambdaSource::receiveData called on operatorId=" << operatorId);
     using namespace std::chrono_literals;
 
-    auto buffer = bufferManager->getBufferBlocking();
+//    auto buffer = bufferManager->getBufferBlocking();
 
-    NES_ASSERT2_FMT(numberOfTuplesToProduce * schema->getSchemaSizeInBytes() <= buffer.getBufferSize(),
-                    "value to write is larger than the buffer");
+//    NES_ASSERT2_FMT(numberOfTuplesToProduce * schema->getSchemaSizeInBytes() <= buffer.getBufferSize(),
+//                    "value to write is larger than the buffer");
 
+    Runtime::TupleBuffer buffer;
     generationFunction(buffer, numberOfTuplesToProduce);
     buffer.setNumberOfTuples(numberOfTuplesToProduce);
 
