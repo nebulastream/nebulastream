fa7247d0a37f31ba942896d9c31312b7e9c9c114
Philipp Grulich <philippgrulich@hotmail.de>
2022-01-15 14:23:18 +0100

    [#2212] fix version path

--- nes-core/include/Version/version.hpp
+++ nes-core/include/Version/version.hpp
@@ -18,6 +18,6 @@
 #define __VERSION_HPP__
 #define NES_VERSION_MAJOR 0
 #define NES_VERSION_MINOR 1
-#define NES_VERSION_PATCH 14
-constexpr char const* NES_VERSION = "0.1.14";
+#define NES_VERSION_PATCH 15
+constexpr char const* NES_VERSION      = "0.1.15";
 #endif
