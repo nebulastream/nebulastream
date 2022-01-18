574ecfbe4c83c5b0e9f81981ec7d5dca8c1ccfec
Dwi Nugroho <dwiprasetyo.an@gmail.com>
2022-01-18 22:40:57 +0100

    [#2324] exclude jitify from license check

--- scripts/build/check_license.py
+++ scripts/build/check_license.py
@@ -34,7 +34,7 @@ license_text_cpp = """/*
 """
 
 if __name__ == "__main__":
-    exclude = set(['cmake-build-debug', 'cmake-build-release', 'build', 'yaml'])
+    exclude = set(['cmake-build-debug', 'cmake-build-release', 'build', 'yaml', 'jitify'])
     for subdir, dirs, files in os.walk(sys.argv[1]):
         dirs[:] = [d for d in dirs if d not in exclude]
         for file in files:
