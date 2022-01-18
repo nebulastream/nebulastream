f8eeddeb676b3010941ab0d3d90e7cb7273fd9e6
Ankit Chaudhary <ankit.chaudhary@tu-berlin.de>
2022-01-18 14:56:02 +0100

    #2312:added impl for MemorySourceType

---
+++ nes-core/include/Catalogs/Source/PhysicalSourceTypes/MemorySourceType.hpp
@@ -0,0 +1,80 @@
+/*
+    Copyright (C) 2020 by the NebulaStream project (https://nebula.stream)
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#ifndef NES_INCLUDE_CATALOGS_MEMORY_SOURCE_STREAM_CONFIG_HPP_
+#define NES_INCLUDE_CATALOGS_MEMORY_SOURCE_STREAM_CONFIG_HPP_
+
+#include <Catalogs/Source/PhysicalSourceTypes/PhysicalSourceType.hpp>
+
+namespace NES {
+
+class MemorySourceType;
+using MemorySourceTypePtr = std::shared_ptr<MemorySourceType>;
+
+/**
+ * @brief A stream config for a memory source
+ */
+class MemorySourceType : public PhysicalSourceType {
+  public:
+
+    /**
+     * @brief Factory method of MemorySourceType
+     * @param memoryArea : location from where to read data
+     * @param memoryAreaSize : amount of memory to read
+     * @param numBuffersToProduce : number of buffers to produce
+     * @param gatheringValue : gathering value
+     * @param gatheringMode : gathering mode
+     * @return a constructed MemorySourceType
+     */
+    static MemorySourceTypePtr create(uint8_t* memoryArea,
+                                              size_t memoryAreaSize,
+                                              uint64_t numBuffersToProduce,
+                                              uint64_t gatheringValue,
+                                              const std::string& gatheringMode);
+
+    ~MemorySourceType() noexcept = default;
+
+    /**
+     * @brief The string representation of the object
+     * @return the string representation of the object
+     */
+    std::string toString() override;
+
+    bool equal(const PhysicalSourceTypePtr& other) override;
+
+  private:
+    /**
+     * @brief Create a MemorySourceType using a set of parameters
+     * @param sourceType the type of the source
+     * @param physicalStreamName the name of the physical stream
+     * @param logicalStreamName the name of the logical stream
+     * @param memoryArea the pointer to the memory area
+     * @param memoryAreaSize the size of the memory area
+     */
+    explicit MemorySourceType(uint8_t* memoryArea,
+                                      size_t memoryAreaSize,
+                                      uint64_t numBuffersToProduce,
+                                      uint64_t gatheringValue,
+                                      const std::string& gatheringMode);
+
+    std::shared_ptr<uint8_t> memoryArea;
+    size_t memoryAreaSize;
+    uint64_t numberOfBufferToProduce;
+    uint64_t gatheringValue;
+    std::string gatheringMode;
+};
+}// namespace NES
+#endif// NES_INCLUDE_CATALOGS_MEMORY_SOURCE_STREAM_CONFIG_HPP_
---
+++ nes-core/src/Catalogs/Source/PhysicalSourceTypes/MemorySourceType.cpp
@@ -0,0 +1,70 @@
+/*
+    Copyright (C) 2020 by the NebulaStream project (https://nebula.stream)
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include <Catalogs/Source/PhysicalSourceTypes/MemorySourceType.hpp>
+
+namespace NES {
+
+using namespace Configurations;
+
+namespace detail {
+
+struct MemoryAreaDeleter {
+    void operator()(uint8_t* ptr) const { free(ptr); }
+};
+
+}// namespace detail
+
+MemorySourceType::MemorySourceType(uint8_t* memoryArea,
+                                   size_t memoryAreaSize,
+                                   uint64_t numBuffersToProduce,
+                                   uint64_t gatheringValue,
+                                   const std::string& gatheringMode)
+    : PhysicalSourceType(MEMORY_SOURCE), memoryArea(memoryArea, detail::MemoryAreaDeleter()), memoryAreaSize(memoryAreaSize),
+      numberOfBufferToProduce(numBuffersToProduce), gatheringValue(gatheringValue), gatheringMode(gatheringMode) {}
+
+MemorySourceTypePtr MemorySourceType::create(uint8_t* memoryArea,
+                                             size_t memoryAreaSize,
+                                             uint64_t numBuffersToProcess,
+                                             uint64_t gatheringValue,
+                                             const std::string& gatheringMode) {
+    NES_ASSERT(memoryArea, "invalid memory area");
+    return std::make_shared<MemorySourceType>(
+        MemorySourceType(memoryArea, memoryAreaSize, numBuffersToProcess, gatheringValue, gatheringMode));
+}
+
+std::string MemorySourceType::toString() {
+    std::stringstream ss;
+    ss << "LambdaSourceType => {\n";
+    ss << "MemoryArea :" << memoryArea;
+    ss << "MemoryAreaSize :" << memoryAreaSize;
+    ss << "NumberOfBuffersToProduce :" << numberOfBufferToProduce;
+    ss << "GatheringValue :" << gatheringValue;
+    ss << "GatheringMode :" + gatheringMode;
+    ss << "\n}";
+    return ss.str();
+}
+
+bool MemorySourceType::equal(const PhysicalSourceTypePtr& other) {
+    if (!other->instanceOf<MemorySourceType>()) {
+        return false;
+    }
+    auto otherSourceConfig = other->as<MemorySourceType>();
+    return memoryArea == otherSourceConfig->memoryArea && memoryAreaSize == otherSourceConfig->memoryAreaSize
+        && numberOfBufferToProduce == otherSourceConfig->numberOfBufferToProduce
+        && gatheringValue == otherSourceConfig->gatheringValue && gatheringMode == otherSourceConfig->gatheringMode;
+}
+}// namespace NES
