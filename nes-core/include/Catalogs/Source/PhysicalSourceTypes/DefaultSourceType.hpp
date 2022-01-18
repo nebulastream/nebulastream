24f9f5fa2e99128b7547e460a5bfd5bf9e19692d
Ankit Chaudhary <ankit.chaudhary@tu-berlin.de>
2022-01-18 13:10:12 +0100

    #2312:added impl for DefaultSourceType

---
+++ nes-core/include/Catalogs/Source/PhysicalSourceTypes/DefaultSourceType.hpp
@@ -0,0 +1,79 @@
+/*
+    Copyright (C) 2020 by the NebulaStream project (https://nebula.stream)
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#ifndef NES_DEFAULTSOURCETYPECONFIG_HPP
+#define NES_DEFAULTSOURCETYPECONFIG_HPP
+
+#include <Catalogs/Source/PhysicalSourceTypes/PhysicalSourceType.hpp>
+#include <Util/yaml/rapidyaml.hpp>
+#include <map>
+#include <memory>
+#include <string>
+
+namespace NES {
+
+class DefaultSourceType;
+using DefaultSourceTypePtr = std::shared_ptr<DefaultSourceType>;
+
+/**
+ * @brief Configuration object for default source config
+ * A simple source with default data created inside NES, useful for testing
+ */
+class DefaultSourceType : public PhysicalSourceType {
+
+  public:
+    /**
+     * @brief create a DefaultSourceConfigPtr object
+     * @param sourceConfigMap inputted config options
+     * @return DefaultSourceConfigPtr
+     */
+    static DefaultSourceTypePtr create(std::map<std::string, std::string> sourceConfigMap);
+
+    /**
+     * @brief create a DefaultSourceConfigPtr object
+     * @param sourceConfigMap inputted config options
+     * @return DefaultSourceConfigPtr
+     */
+    static DefaultSourceTypePtr create(ryml::NodeRef yamlConfig);
+
+    /**
+     * @brief create defaultSourceConfig with default values
+     * @return defaultSourceConfig with default values
+     */
+    static DefaultSourceTypePtr create();
+
+    std::string toString() override;
+
+    bool equal(const PhysicalSourceTypePtr& other) override;
+
+  private:
+    /**
+     * @brief constructor to create a new Default source config object using the sourceConfigMap for physicalStreams
+     */
+    explicit DefaultSourceType(std::map<std::string, std::string> sourceConfigMap);
+
+    /**
+     * @brief constructor to create a new Default source config object using the sourceConfigMap for physicalStreams
+     */
+    explicit DefaultSourceType(ryml::NodeRef sourceTypeConfig);
+
+    /**
+     * @brief constructor to create a new Default source config object initialized with default values
+     */
+    DefaultSourceType();
+};
+}// namespace NES
+#endif
