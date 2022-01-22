7bf5f64d3035425e1bc5b25e34730ee3ea89d2fc
stendler <stendler@campus.tu-berlin.de>
2022-01-22 19:21:24 +0100

    benchmark: transform output to tsv

--- benchmark/scripts/query_compiler_benchmark.sh
+++ benchmark/scripts/query_compiler_benchmark.sh
@@ -17,15 +17,21 @@ tests="filterQuery
        externalOperatorTest"
 
 echo >&2 "[DEBUG] Going to run $test_prefix tests in: $*"
+
+# print CSV header
+csv_delimiter="${CSV_DELIMITER:=\t}"
+echo "build-folder${csv_delimiter}query${csv_delimiter}timed_unit${csv_delimiter}time${csv_delimiter}unit"
+
 for test_folder in "$@"; do
   if [ -d "$test_folder" ]; then
-    echo "Build Folder: $test_folder"
+    echo >&2 "Build Folder: $test_folder"
     (
     cd $test_folder
     for testCase in $tests; do
-      echo "Running test: $testCase"
+      echo >&2 "Running test: $testCase"
       LD_PRELOAD="$libnes" sh -c "exec $test_exec --gtest_filter=$test_prefix$testCase" \
-        | grep -A 8 "overall runtime: "
+        | grep -A 8 "overall runtime: " `# filter to collect only the relevant outputs`\
+        | sed -e 's/\t//' -e "s/:[\s]*/$csv_delimiter/" -e "s/ ms/${csv_delimiter}ms/" | awk "{print \"$test_folder$csv_delimiter$testCase$csv_delimiter\""' $0}' `# transform to CSV`
     done
     )
   else
