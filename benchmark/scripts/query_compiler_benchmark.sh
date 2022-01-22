9dacb0b60c31f143f4b82aa2bba9186c4d5877a1
stendler <stendler@campus.tu-berlin.de>
2022-01-22 20:09:03 +0100

    benchmark: add description, usage and example

--- benchmark/scripts/query_compiler_benchmark.sh
+++ benchmark/scripts/query_compiler_benchmark.sh
@@ -1,6 +1,25 @@
 #!/usr/bin/env sh
 
 
+# This script takes cmake build folders as arguments and will run query-compiler-tests in each of them
+# outputting the printed timings in TSV format.
+#
+# Parameters: Paths to cmake build folders
+# STDOUT: TSV including headers
+# STDERR: Log messages
+#
+# ENV configuration options:
+# - DELIMITER: delimiter used for the output (default: "\t" [tabs])
+#              WARNING: Occurrences of the delimiter in the output is not escaped!
+#              Choose a delimiter that will not be in the filtered test log messages (or fix this script).
+#
+# Usage example:
+# /usr/bin/env sh /nebulastream/benchmark/scripts/query_compiler_benchmark.sh "/nebulastream/cmake-build-impro/" \
+#   | sed -i -e "s/\/nebulastream\/cmake-build-impro\//header selection/" \
+#            -e "s/\/nebulastream\/cmake-build-no-impro\//all headers/" \
+#            -e "s/build-folder/variant/" \
+#   > impro-query-compiler-benchmark.tsv
+
 libnes="nes-core/libnes.so"
 test_exec="nes-core/tests/query-compiler-tests --gtest_color=no"
 test_prefix="QueryCompilerTest."
@@ -19,19 +38,19 @@ tests="filterQuery
 echo >&2 "[DEBUG] Going to run $test_prefix tests in: $*"
 
 # print CSV header
-csv_delimiter="${CSV_DELIMITER:=\t}"
-echo "build-folder${csv_delimiter}query${csv_delimiter}timed_unit${csv_delimiter}time${csv_delimiter}unit"
+delimiter="${DELIMITER:=\t}"
+echo "build-folder${delimiter}query${delimiter}timed_unit${delimiter}time${delimiter}unit"
 
 for test_folder in "$@"; do
   if [ -d "$test_folder" ]; then
-    echo >&2 "Build Folder: $test_folder"
+    echo >&2 "[INFO] Build Folder: $test_folder"
     (
     cd $test_folder
     for testCase in $tests; do
-      echo >&2 "Running test: $testCase"
+      echo >&2 "[INFO] Running test: $testCase"
       LD_PRELOAD="$libnes" sh -c "exec $test_exec --gtest_filter=$test_prefix$testCase" \
         | grep -A 8 "overall runtime: " `# filter to collect only the relevant outputs`\
-        | sed -e 's/\t//' -e "s/:[\s]*/$csv_delimiter/" -e "s/ ms/${csv_delimiter}ms/" | awk "{print \"$test_folder$csv_delimiter$testCase$csv_delimiter\""' $0}' `# transform to CSV`
+        | sed -e 's/\t//' -e "s/:[\s]*/$delimiter/" -e "s/ ms/${delimiter}ms/" | awk "{print \"$test_folder$delimiter$testCase$delimiter\""' $0}' `# transform to CSV`
     done
     )
   else
