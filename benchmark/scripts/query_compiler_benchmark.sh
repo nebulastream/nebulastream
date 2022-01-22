ce0c7c3072cd53e4930579c71cd0db1220c63f73
stendler <stendler@campus.tu-berlin.de>
2022-01-22 15:37:11 +0100

    benchmark: accept cmake build folder paths as arguments & update echos

--- benchmark/scripts/query_compiler_benchmark.sh
+++ benchmark/scripts/query_compiler_benchmark.sh
@@ -14,22 +14,22 @@ tests="filterQuery
        tumblingWindowQueryEventTime
        unionQuery
        joinQuery
-       externalOperatorTest
-"
-#test_exec="nes-core/tests/query-compiler-tests --gtest_filter=QueryCompilerTest.filterQuery:QueryCompilerTest/*.filterQuery:QueryCompilerTest.filterQuery/*:*/QueryCompilerTest.filterQuery/*:*/QueryCompilerTest/*.filterQuery --gtest_color=no"
+       externalOperatorTest"
 
-for test_folder in ${TEST_FOLDERS:="../../cmake-build-impro/"}; do
+echo >&2 "[DEBUG] Going to run $test_prefix tests in: $*"
+for test_folder in "$@"; do
   if [ -d "$test_folder" ]; then
+    echo "Build Folder: $test_folder"
     (
     cd $test_folder
     for testCase in $tests; do
-      echo "Running: $testCase"
+      echo "Running test: $testCase"
       LD_PRELOAD="$libnes" sh -c "exec $test_exec --gtest_filter=$test_prefix$testCase" \
         | grep -A 8 "overall runtime: "
     done
     )
   else
-    echo >&2 "Folder '$test_folder' not found. Skipping."
+    echo >&2 "[ERROR] Folder '$test_folder' not found. Skipping."
   fi
 done
 
