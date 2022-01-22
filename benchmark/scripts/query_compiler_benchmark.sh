6cff622aa3cb9e7dc4b39a015f5fb50770166490
stendler <stendler@campus.tu-berlin.de>
2022-01-22 14:58:36 +0100

    benchmark: run each test case individually with gtest_filter
    now a single test case can be excluded, and we also can map the results
    to a corresponding test case.

--- benchmark/scripts/query_compiler_benchmark.sh
+++ benchmark/scripts/query_compiler_benchmark.sh
@@ -3,16 +3,33 @@
 
 libnes="nes-core/libnes.so"
 test_exec="nes-core/tests/query-compiler-tests --gtest_color=no"
+test_prefix="QueryCompilerTest."
+# output from --gtest_list_tests
+tests="filterQuery
+       filterQueryBitmask
+       mapQuery
+       windowQuery
+       windowQueryEventTime
+       tumblingWindowQueryIngestionTime
+       tumblingWindowQueryEventTime
+       unionQuery
+       joinQuery
+       externalOperatorTest
+"
 #test_exec="nes-core/tests/query-compiler-tests --gtest_filter=QueryCompilerTest.filterQuery:QueryCompilerTest/*.filterQuery:QueryCompilerTest.filterQuery/*:*/QueryCompilerTest.filterQuery/*:*/QueryCompilerTest/*.filterQuery --gtest_color=no"
 
 for test_folder in ${TEST_FOLDERS:="../../cmake-build-impro/"}; do
   if [ -d "$test_folder" ]; then
     (
     cd $test_folder
-    LD_PRELOAD="$libnes" sh -c "exec $test_exec" \
-      | grep -A 8 "overall runtime: "
+    for testCase in $tests; do
+      echo "Running: $testCase"
+      LD_PRELOAD="$libnes" sh -c "exec $test_exec --gtest_filter=$test_prefix$testCase" \
+        | grep -A 8 "overall runtime: "
+    done
     )
   else
     echo >&2 "Folder '$test_folder' not found. Skipping."
   fi
 done
+
