01908fa543b4a58df4efcd31569714b6a7357fbe
János Brodbeck <janos.brodbeck@posteo.de>
2022-01-23 13:31:17 +0100

    [#2440] add header file for precompiled header approach

---
+++ nes-core/include/QueryCompiler/Compiler/RuntimeHeaders.hpp
@@ -0,0 +1,60 @@
+/*
+Copyright (C) 2020 by the NebulaStream project (https://nebula.stream)
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    https://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#ifndef NES_INCLUDE_QUERYCOMPILER_COMPILER_RUNTIMEHEADERS_HPP_
+#define NES_INCLUDE_QUERYCOMPILER_COMPILER_RUNTIMEHEADERS_HPP_
+
+#include <Common/ExecutableType/Array.hpp>
+#include <QueryCompiler/Operators/PhysicalOperators/CEP/CEPOperatorHandler/CEPOperatorHandler.hpp>
+#include <Runtime/Execution/ExecutablePipelineStage.hpp>
+#include <Runtime/Execution/PipelineExecutionContext.hpp>
+#include <Runtime/ExecutionResult.hpp>
+#include <Runtime/TupleBuffer.hpp>
+#include <Runtime/WorkerContext.hpp>
+#include <State/StateVariable.hpp>
+#include <Windowing/LogicalWindowDefinition.hpp>
+#include <Windowing/Runtime/WindowManager.hpp>
+#include <Windowing/Runtime/WindowSliceStore.hpp>
+#include <Windowing/Runtime/WindowedJoinSliceListStore.hpp>
+#include <Windowing/WindowActions/BaseExecutableJoinAction.hpp>
+#include <Windowing/WindowActions/ExecutableCompleteAggregationTriggerAction.hpp>
+#include <Windowing/WindowActions/ExecutableNestedLoopJoinTriggerAction.hpp>
+#include <Windowing/WindowActions/ExecutableSliceAggregationTriggerAction.hpp>
+#include <Windowing/WindowAggregations/ExecutableAVGAggregation.hpp>
+#include <Windowing/WindowAggregations/ExecutableCountAggregation.hpp>
+#include <Windowing/WindowAggregations/ExecutableMaxAggregation.hpp>
+#include <Windowing/WindowAggregations/ExecutableMedianAggregation.hpp>
+#include <Windowing/WindowAggregations/ExecutableMinAggregation.hpp>
+#include <Windowing/WindowAggregations/ExecutableSumAggregation.hpp>
+#include <Windowing/WindowHandler/AggregationWindowHandler.hpp>
+#include <Windowing/WindowHandler/JoinHandler.hpp>
+#include <Windowing/WindowHandler/JoinOperatorHandler.hpp>
+#include <Windowing/WindowHandler/WindowOperatorHandler.hpp>
+#include <Windowing/WindowPolicies/ExecutableOnTimeTriggerPolicy.hpp>
+#include <Windowing/WindowPolicies/ExecutableOnWatermarkChangeTriggerPolicy.hpp>
+#include <API/Query.hpp>
+//#include <API/Pattern.hpp>
+#include <API/Schema.hpp>
+#include <Operators/LogicalOperators/Sinks/PrintSinkDescriptor.hpp>
+#include <Operators/LogicalOperators/Sinks/FileSinkDescriptor.hpp>
+#include <Operators/LogicalOperators/Sinks/KafkaSinkDescriptor.hpp>
+#include <Operators/LogicalOperators/Sinks/ZmqSinkDescriptor.hpp>
+#include <Operators/LogicalOperators/Sinks/MQTTSinkDescriptor.hpp>
+#include <Operators/LogicalOperators/Sinks/NullOutputSinkDescriptor.hpp>
+#include <Windowing/Watermark/EventTimeWatermarkStrategyDescriptor.hpp>
+#include <Windowing/Watermark/IngestionTimeWatermarkStrategyDescriptor.hpp>
+#include <Sources/DataSource.hpp>
+#include <cstdint>
+#include <string.h>
+
+#endif//NES_INCLUDE_QUERYCOMPILER_COMPILER_RUNTIMEHEADERS_HPP_
