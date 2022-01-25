55773e5892a833f2796566a241c007c89c2c9549
Mark Sumegi <sumegi.mark@gmail.com>
2022-01-25 22:38:30 +0100

    #1779 rework folder structure after rebase

---
+++ nes-core/include/QueryCompiler/Operators/GeneratableOperators/GeneratableInferModelOperator.hpp
@@ -0,0 +1,49 @@
+/*
+    Copyright (C) 2020 by the NebulaStream project (https://nebula.stream)
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+#ifndef NES_INCLUDE_QUERYCOMPILER_OPERATORS_GENERATABLEOPERATORS_GENERATABLEINFER_MODEL_HPP_
+#define NES_INCLUDE_QUERYCOMPILER_OPERATORS_GENERATABLEOPERATORS_GENERATABLEINFER_MODEL_HPP_
+
+#include <Nodes/Expressions/ExpressionNode.hpp>
+#include <QueryCompiler/Operators/GeneratableOperators/GeneratableOperator.hpp>
+#include <Windowing/JoinForwardRefs.hpp>
+
+namespace NES {
+namespace QueryCompilation {
+namespace GeneratableOperators {
+
+/**
+ * @brief Generates a conditional inferModel operator, which infers an ML model
+ */
+class GeneratableInferModelOperator : public GeneratableOperator {
+  public:
+    static GeneratableOperatorPtr create(SchemaPtr inputSchema, SchemaPtr outputSchema, std::string model, std::vector<ExpressionItemPtr> inputFields, std::vector<ExpressionItemPtr> outputFields, Join::InferModelOperatorHandlerPtr operatorHandler);
+    static GeneratableOperatorPtr create(OperatorId id, SchemaPtr inputSchema, SchemaPtr outputSchema, std::string model, std::vector<ExpressionItemPtr> inputFields, std::vector<ExpressionItemPtr> outputFields, Join::InferModelOperatorHandlerPtr operatorHandler);
+    void generateExecute(CodeGeneratorPtr codegen, PipelineContextPtr context) override;
+    void generateOpen(CodeGeneratorPtr codegen, PipelineContextPtr context) override;
+    [[nodiscard]] std::string toString() const override;
+    OperatorNodePtr copy() override;
+
+  private:
+    GeneratableInferModelOperator(OperatorId id, SchemaPtr inputSchema, SchemaPtr outputSchema, std::string model, std::vector<ExpressionItemPtr> inputFields, std::vector<ExpressionItemPtr> outputFields, Join::InferModelOperatorHandlerPtr operatorHandler);
+    const std::string model;
+    const std::vector<ExpressionItemPtr> inputFields;
+    const std::vector<ExpressionItemPtr> outputFields;
+    Join::InferModelOperatorHandlerPtr operatorHandler;
+};
+}// namespace GeneratableOperators
+}// namespace QueryCompilation
+}// namespace NES
+#endif//NES_INCLUDE_QUERYCOMPILER_OPERATORS_GENERATABLEOPERATORS_GENERATABLEINFER_MODEL_HPP_
---
+++ nes-core/src/Windowing/WindowHandler/InferModelOperatorHandler.cpp
@@ -0,0 +1,60 @@
+/*
+    Copyright (C) 2020 by the NebulaStream project (https://nebula.stream)
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+#include <Windowing/WindowHandler/InferModelOperatorHandler.hpp>
+#include <Runtime/Reconfigurable.hpp>
+#include <Runtime/WorkerContext.hpp>
+#include <Windowing/JoinForwardRefs.hpp>
+#include <Windowing/Runtime/WindowSliceStore.hpp>
+#include <Windowing/Runtime/WindowState.hpp>
+#include <Windowing/WindowActions/BaseExecutableWindowAction.hpp>
+
+
+namespace NES::Join {
+
+InferModelOperatorHandlerPtr InferModelOperatorHandler::create(std::string model) {
+    return std::make_shared<InferModelOperatorHandler>(model);
+}
+
+InferModelOperatorHandler::InferModelOperatorHandler(std::string model) {
+    this->model = model;
+    tfAdapter = TensorflowAdapter::create();
+    tfAdapter->initializeModel(model);
+}
+
+void InferModelOperatorHandler::start(Runtime::Execution::PipelineExecutionContextPtr,
+                                      Runtime::StateManagerPtr stateManager,
+                                uint32_t localStateVariableId) {
+    std::cout << stateManager->getNodeId() << std::endl;
+    std::cout << localStateVariableId << std::endl;
+
+
+    // implement start here
+}
+
+void InferModelOperatorHandler::stop(Runtime::Execution::PipelineExecutionContextPtr) {
+    // implement stop here
+}
+void InferModelOperatorHandler::reconfigure(Runtime::ReconfigurationMessage& task, Runtime::WorkerContext& context) {
+    Runtime::Execution::OperatorHandler::reconfigure(task, context);
+}
+
+void InferModelOperatorHandler::postReconfigurationCallback(Runtime::ReconfigurationMessage& task) {
+    Runtime::Execution::OperatorHandler::postReconfigurationCallback(task);
+}
+const std::string& InferModelOperatorHandler::getModel() const { return model; }
+const TensorflowAdapterPtr& InferModelOperatorHandler::getTensorflowAdapter() const { return tfAdapter; }
+
+}// namespace NES::Join
