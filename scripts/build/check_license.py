51b2392ab0e9db8668b087decf036116cd460906
Philipp Grulich <philippgrulich@hotmail.de>
2022-01-28 10:40:41 +0100

    [#2459] remove licence header

--- scripts/build/check_license.py
+++ scripts/build/check_license.py
@@ -17,8 +17,6 @@ import os
 import sys
 
 license_text_cpp = """/*
-    Copyright (C) 2020 by the NebulaStream project (https://nebula.stream)
-
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
     You may obtain a copy of the License at
