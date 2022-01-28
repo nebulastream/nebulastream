f8db4c2f19783399d917253c598373b6cace324a
Philipp Grulich <philippgrulich@hotmail.de>
2022-01-28 10:46:44 +0100

    [#2459] remove licence header

--- docker/devImage/entrypoint-nes-dev.sh
+++ docker/devImage/entrypoint-nes-dev.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-# Copyright (C) 2020 by the NebulaStream project (https://nebula.stream)
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
