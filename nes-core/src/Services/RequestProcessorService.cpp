54f80d391b7df29d0d2bf21d2ffe65e99267c940
Ankit Chaudhary <ankit.chaudhary@tu-berlin.de>
2022-02-21 16:31:18 +0100

    block deployment till x number of queries are received

--- nes-core/src/Services/RequestProcessorService.cpp
+++ nes-core/src/Services/RequestProcessorService.cpp
@@ -101,7 +101,7 @@ void RequestProcessorService::start() {
 
                 for (const auto& queryRequest : nesRequests) {
                     auto queryId = queryRequest->getQueryId();
-                    if (queryId == 4000) {
+                    if (queryId == 1000) {
                         readyForPlacementAndDeployment = true;
                         break;
                     }
