6b96965ea3d8524036c6bb56ca2f8ae2592c7071
Philipp Grulich <philippgrulich@hotmail.de>
2022-02-23 22:48:32 +0100

    [#2122] fix merge errors

--- nes-compiler/src/Compiler/Util/ExecutablePath.cpp
+++ nes-compiler/src/Compiler/Util/ExecutablePath.cpp
@@ -11,7 +11,7 @@
     See the License for the specific language governing permissions and
     limitations under the License.
 */
-#include <Util/ExecutablePath.hpp>
+#include <Compiler/Util//ExecutablePath.hpp>
 #include <Util/Logger.hpp>
 #include <filesystem>
 #include <vector>
