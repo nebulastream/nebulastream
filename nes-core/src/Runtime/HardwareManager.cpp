68b3933250c20cf1580f3f5f2aae10b04ab39e00
Viktor Rosenfeld <viktor.rosenfeld@tu-berlin.de>
2022-03-10 14:28:41 +0100

    [#2634] Parse output of lscpu depending on hardware architecture

--- nes-core/src/Runtime/HardwareManager.cpp
+++ nes-core/src/Runtime/HardwareManager.cpp
@@ -58,13 +58,22 @@ void readCpuConfig(uint32_t& numa_nodes_count,
                 splits.push_back(s);
             }
         }
-        bool online = splits[5] == "yes";
-        if (!online) {
-            continue;
-        }
         unsigned long cpu = std::stoi(splits[0]);
         unsigned long node = std::stoi(splits[1]);
+#if defined(__x86_64__) || defined(_M_X64)
         unsigned long core = std::stoi(splits[3]);
+        bool online = splits[5] == "yes";
+#elif defined(__aarch64__)
+        unsigned long core = std::stoi(splits[2]);
+        bool online = splits[3] == "yes";
+#else
+#error "Invalid architecture"
+#endif
+        if (!online) {
+            continue;
+        }        if (!online) {
+            continue;
+        }
         if (auto it = cpuMapping.find(node); it != cpuMapping.end()) {
             cpuMapping[node] = HardwareManager::NumaDescriptor(node);
         }
