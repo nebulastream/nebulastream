60e548fa4ec7528216d20056ca47479d0f9732b8
Balint Rueb <rueb@campus.tu-berlin.de>
2022-03-13 12:10:42 +0100

    #2528 addressed PR feedback, fixed spelling mistake

--- nes-core/include/Runtime/NodeEngineBuilder.hpp
+++ nes-core/include/Runtime/NodeEngineBuilder.hpp
@@ -17,32 +17,55 @@
 #include <Runtime/RuntimeForwardRefs.hpp>
 #include <Runtime/NodeEngine.hpp>
 #include <Configurations/Worker/QueryCompilerConfiguration.hpp>
+#include <Configurations/Worker/WorkerConfiguration.hpp>
+//#include <Compiler/LanguageCompiler.hpp>
 namespace NES::Runtime {
 enum class NumaAwarenessFlag { ENABLED, DISABLED };
 class NodeEngineBuilder {
 
   public:
-    static NodeEngineBuilder create();
+    static NodeEngineBuilder create(Configurations::WorkerConfiguration workerConfiguration);
+
+    NodeEngineBuilder() = delete;
 
     NodeEngineBuilder& setHostname(const std::string& hostname);
-    NodeEngineBuilder& setPort(uint16_t port);
-    NodeEngineBuilder& setPhysicalSources(const std::vector<PhysicalSourcePtr>& physicalSources);
-    NodeEngineBuilder& setNumThreads(uint16_t numThreads);
-    NodeEngineBuilder& setBufferSize(uint64_t bufferSize);
-    NodeEngineBuilder& setNumberOfBuffersInGlobalBufferManager(uint64_t numberOfBuffersInGlobalBufferManager);
-    NodeEngineBuilder& setNumberOfBuffersInSourceLocalBufferPool(uint64_t numberOfBuffersInSourceLocalBufferPool);
-    NodeEngineBuilder& setNumberOfBuffersPerWorker(uint64_t numberOfBuffersPerWorker);
-    NodeEngineBuilder& setQueryCompilerConfiguration(const Configurations::QueryCompilerConfiguration& queryCompilerConfiguration);
-    NodeEngineBuilder& setNesWorker(const std::weak_ptr<NesWorker>&& nesWorker);
-    NodeEngineBuilder& setEnableNumaAwareness(NumaAwarenessFlag enableNumaAwareness);
-    NodeEngineBuilder& setWorkerToCoreMapping(const std::string& workerToCoreMapping);
-    NodeEngineBuilder& setNumberOfQueues(uint64_t numberOfQueues);
-    NodeEngineBuilder& setNumberOfThreadsPerQueue(uint64_t numberOfThreadsPerQueue);
-    NodeEngineBuilder& setQueryManagerMode(QueryManager::QueryMangerMode queryManagerMode);
+    NodeEngineBuilder& setNesWorker(std::weak_ptr<NesWorker>&& nesWorker);
+    NodeEngineBuilder& setNodeEngineId(uint64_t nodeEngineId);
+    NodeEngineBuilder& setPartitionManager(Network::PartitionManagerPtr partitionManager);
+    NodeEngineBuilder& setHardwareManager(HardwareManagerPtr hardwareManager);
+    NodeEngineBuilder& setBufferManagers(std::vector<BufferManagerPtr> bufferManagers);
+    NodeEngineBuilder& setQueryManager(QueryManagerPtr queryManager);
+    NodeEngineBuilder& setStateManager(StateManagerPtr stateManager);
+    NodeEngineBuilder& setBufferStorage(BufferStoragePtr bufferStorage);
+    NodeEngineBuilder& setMaterializedViewManager(Experimental::MaterializedView::MaterializedViewManagerPtr materializedViewManager);
+    //NodeEngineBuilder& setLanguageCompiler(std::shared_ptr<Compiler::LanguageCompiler> languageCompiler);
+    NodeEngineBuilder& setJITCompiler(Compiler::JITCompilerPtr jitCompiler );
+    NodeEngineBuilder& setPhaseFactory(QueryCompilation::Phases::PhaseFactoryPtr phaseFactory);
+    NodeEngineBuilder& setCompiler(QueryCompilation::QueryCompilerPtr queryCompiler);
+
+
     NodeEnginePtr build();
 
   private:
-    static QueryCompilation::QueryCompilerOptionsPtr
+    NodeEngineBuilder(Configurations::WorkerConfiguration workerConfiguration);
+    std::string hostname;
+    std::weak_ptr<NesWorker> nesWorker;
+    uint64_t nodeEngineId;
+    Network::PartitionManagerPtr partitionManager;
+    HardwareManagerPtr hardwareManager;
+    std::vector<BufferManagerPtr> bufferManagers;
+    QueryManagerPtr queryManager;
+    StateManagerPtr stateManager;
+    BufferStoragePtr bufferStorage;
+    Experimental::MaterializedView::MaterializedViewManagerPtr materializedViewManager;
+    //std::shared_ptr<Compiler::LanguageCompiler> languageCompiler;
+    Compiler::JITCompilerPtr jitCompiler;
+    QueryCompilation::Phases::PhaseFactoryPtr phaseFactory;
+    QueryCompilation::QueryCompilerPtr queryCompiler;
+
+
+
+        static QueryCompilation::QueryCompilerOptionsPtr
     createQueryCompilationOptions(const Configurations::QueryCompilerConfiguration queryCompilerConfiguration);
 
     /**
@@ -51,25 +74,8 @@ class NodeEngineBuilder {
      */
     static uint64_t getNextNodeEngineId();
 
-    std::string hostname;
-    uint16_t port;
-    bool isPortSet;
-    std::vector<PhysicalSourcePtr> physicalSources;
-    uint16_t numThreads;
-    uint64_t bufferSize;
-    uint64_t numberOfBuffersInGlobalBufferManager;
-    uint64_t numberOfBuffersInSourceLocalBufferPool;
-    uint64_t numberOfBuffersPerWorker;
-    Configurations::QueryCompilerConfiguration queryCompilerConfiguration;
-    bool isQueryCompilerConfigurationSet;
-    std::weak_ptr<NesWorker> nesWorker;
-    NumaAwarenessFlag enableNumaAwareness;
-    bool isNumaAwarenessFlagSet;
-    std::string workerToCoreMapping;
-    uint64_t numberOfQueues;
-    uint64_t numberOfThreadsPerQueue;
-    QueryManager::QueryMangerMode queryManagerMode;
-    bool isQueryManagerModeSet;
+    Configurations::WorkerConfiguration workerConfiguration;
+
 };
 }// namespace NES::Runtime
 #endif//NES_NODEENGINEBUILDER_HPP
