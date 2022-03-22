0b89e98acae97bf657f7a73d0a36f079afea9cbc
NES-CI <nebulastream@dima.tu-berlin.de>
2022-03-22 19:10:00 +0000

    GIT-CI: Updating NES version to 0.1.82

--- nes-core/include/Version/version.hpp
+++ nes-core/include/Version/version.hpp
@@ -17,6 +17,6 @@
 #define NES_VERSION_MAJOR 0
 #define NES_VERSION_MINOR 1
 #define NES_VERSION_PATCH 82
-#define NES_VERSION_POST_FIX SNAPSHOT
-constexpr char const* NES_VERSION      = "0.1.82-SNAPSHOT";
+#define NES_VERSION_POST_FIX 
+constexpr char const* NES_VERSION      = "0.1.82";
 #endif
