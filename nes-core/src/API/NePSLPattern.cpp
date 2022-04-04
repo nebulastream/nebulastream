674f631bb47287b64b1c007a9746c1e956a7725b
Philipp Grulich <philippgrulich@hotmail.de>
2022-04-04 19:03:00 +0200

    [#2690] fix build errors

--- nes-core/src/API/NePSLPattern.cpp
+++ nes-core/src/API/NePSLPattern.cpp
@@ -1,52 +1,80 @@
-//
-// Created by zouni on 06.01.22.
-//
+/*
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    https://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 
 #include <iostream>
-#include "NePSLPattern.h"
-#include "../../nes-core/src/Parsers/NePSL/NesCEPLexer.h"
-#include "../../nes-core//src/Parsers/NePSL/NesCEPParser.h"
-#include "../../nes-core/src/Parsers/NePSL/NesCEPListener.h"
-#include "../../nes-core/src/Parsers/NePSL/NesCEPBaseListener.h"
-
-void processPattern(const std::string* patternString){
-
-    std::cout<<patternString;
-    antlr4::ANTLRInputStream input((std::istream&) patternString);
-    NesCEPLexer lexer(&input);
-    antlr4::CommonTokenStream tokens(&lexer);
-    NesCEPParser parser(&tokens);
-    NesCEPParser::QueryContext* tree=parser.query();
-    std::cout << tree->toStringTree(&parser) << std::endl;
-
-    NesCEPBaseListener listener;
-    antlr4::tree::ParseTreeWalker::DEFAULT.walk(&listener, tree);
-
-}
+#include <API/NePSLPattern.h>
+#include <Parsers/NePSL/gen/NesCEPLexer.h>
+#include <Parsers/NePSL/gen/NesCEPParser.h>
+#include <Parsers/NePSL/gen/NesCEPListener.h>
+#include <Parsers/NePSL/gen/NesCEPBaseListener.h>
+#include <Parsers/NePSL/NesCEPQueryPlanCreator.h>
+#include <API/Query.hpp>
+#include <cstring>
 
 NePSLPattern::NePSLPattern(int id) {
     setId(id);
     setNegated(false);
     setIteration(false);
-    setParent(this);
+
 }
+void NePSLPattern::printPattern() {
+    std::cout<<"subPattern of id: "<<this->getId()<<std::endl;
+    std::cout<<"right: "<<this->getRight()<<std::endl;
+    std::cout<<"left: "<<this->getLeft()<<std::endl;
+    std::cout<<"parent: "<<this->getParent()<<std::endl;
+    try {
+        std::cout<<"eventRight: "<<this->getEventRight()<<std::endl;
+
+    } catch (std::exception exception) {
+        std::cout<<"eventRight: null"<<std::endl;
+    }
+    try {
+        std::cout<<"eventLeft: "<<this->getEventLeft()<<std::endl;
+
+    } catch (std::exception exception) {
+        std::cout<<"eventLeft: null"<<std::endl;
+    }
+    try {
+        std::cout<<"op: "<<this->getOp()<<std::endl;
+
+    } catch (std::exception exception) {
+        std::cout<<"op: null"<<std::endl;
+    }
+    std::cout<<"negated: "<<this->negated<<std::endl;
+    std::cout<<"iteration: "<<this->iteration<<std::endl;
+}
+
 
 int NePSLPattern::getId() const { return id; }
 void NePSLPattern::setId(int id) { NePSLPattern::id = id; }
-NePSLPattern* NePSLPattern::getRight() const { return right; }
-void NePSLPattern::setRight(NePSLPattern* right) { NePSLPattern::right = right; }
-NePSLPattern* NePSLPattern::getLeft() const { return left; }
-void NePSLPattern::setLeft(NePSLPattern* left) { NePSLPattern::left = left; }
-NePSLPattern* NePSLPattern::getOp() const { return op; }
-void NePSLPattern::setOp(NePSLPattern* op) { NePSLPattern::op = op; }
-NePSLPattern* NePSLPattern::getParent() const { return parent; }
-void NePSLPattern::setParent(NePSLPattern* parent) { NePSLPattern::parent = parent; }
+int NePSLPattern::getRight() const { return right; }
+void NePSLPattern::setRight(int right) { NePSLPattern::right = right; }
+int NePSLPattern::getLeft() const { return left; }
+void NePSLPattern::setLeft(int left) { NePSLPattern::left = left; }
+const std::string& NePSLPattern::getOp() const { return op; }
+void NePSLPattern::setOp(const std::string& op) { NePSLPattern::op = op; }
+int NePSLPattern::getParent() const { return parent; }
+void NePSLPattern::setParent(int parent) { NePSLPattern::parent = parent; }
 bool NePSLPattern::isNegated() const { return negated; }
 void NePSLPattern::setNegated(bool negated) { NePSLPattern::negated = negated; }
 bool NePSLPattern::isIteration() const { return iteration; }
 void NePSLPattern::setIteration(bool iteration) { NePSLPattern::iteration = iteration; }
-const std::string& NePSLPattern::getIterStr() const { return iterStr; }
-void NePSLPattern::setIterStr(const std::string& iterStr) { NePSLPattern::iterStr = iterStr; }
-void NePSLPattern::processPattern(const std::string patternString) {
-    std::cout << (patternString);
-}
+const std::string& NePSLPattern::getEventRight() const { return eventRight; }
+void NePSLPattern::setEventRight(const std::string& eventRight) { NePSLPattern::eventRight = eventRight; }
+const std::string& NePSLPattern::getEventLeft() const { return eventLeft; }
+void NePSLPattern::setEventLeft(const std::string& eventLeft) { NePSLPattern::eventLeft = eventLeft; }
+uint64_t NePSLPattern::getIterMin() const { return iterMin; }
+void NePSLPattern::setIterMin(uint64_t iterMin) { NePSLPattern::iterMin = iterMin; }
+uint64_t NePSLPattern::getIterMax() const { return iterMax; }
+void NePSLPattern::setIterMax(uint64_t iterMax) { NePSLPattern::iterMax = iterMax; }
+const NES::Query& NePSLPattern::getQuery() const { return query; }
+void NePSLPattern::setQuery(const NES::Query& query) { NePSLPattern::query = query; }
