e43d089b941c53af8a6e212659cf521c47c31058
mnarihamza <mnari.hamza@hotmail.fr>
2022-04-06 13:06:20 +0200

    [#2690]: exclude the the generated files from the license check_license.py third attempt: create NePSLPattern.cpp as a copy of Query.cpp

--- scripts/build/check_license.py
+++ scripts/build/check_license.py
@@ -38,8 +38,8 @@ if __name__ == "__main__":
                      'rte_memory.h',
                      'apex_memmove.cpp']
     exclude = set(['cmake-build-debug', 'cmake-build-release', 'cmake-build-debug-docker', 'cmake-build-release-docker',
-                   'build', 'yaml', 'jitify', 'magicenum', 'Backward', '/nebulastream/nes-core/include/Parsers/NePSL/gen',
-                   '/nebulastream/nes-core/src/Parsers/NePSL/gen'])
+                   'build', 'yaml', 'jitify', 'magicenum', 'Backward', 'nes-core/include/Parsers/NePSL/gen',
+                   'nes-core/src/Parsers/NePSL/gen'])
     result = True
     print(str(sys.argv))
     for subdir, dirs, files in os.walk(sys.argv[1]):
