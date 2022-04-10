2e834cc498a9dbeb3b03a1e4eeafe1bc13842ea1
mnarihamza <mnari.hamza@hotmail.fr>
2022-04-10 19:31:09 +0200

    [#2690]: removed the antlr generated comments and adjusted the Readme file

--- nes-core/src/Parsers/NePSL/gen/NesCEPParser.cpp
+++ nes-core/src/Parsers/NePSL/gen/NesCEPParser.cpp
@@ -1,8 +1,5 @@
 
 
-// Generated from /home/zouni/CLionProjects/nebulastream/nes-core/src/Parsers/NePSL/NesCEP.g4 by ANTLR 4.9.2
-
-
 #include "Parsers/NePSL/gen/NesCEPListener.h"
 
 #include "Parsers/NePSL/gen/NesCEPParser.h"
