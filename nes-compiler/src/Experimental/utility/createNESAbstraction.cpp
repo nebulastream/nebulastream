f292ca4fb771804a0496fa98f2ee62a4b1db2b0a
alepping <aljoschalepping@gmail.com>
2022-04-15 09:40:12 +0200

    #2748 : Replaced old license preambles with current ones.

--- nes-compiler/src/Experimental/utility/createNESAbstraction.cpp
+++ nes-compiler/src/Experimental/utility/createNESAbstraction.cpp
@@ -1,12 +1,25 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
 #include <Experimental/utility/createNESAbstraction.hpp>
-#include <Experimental/MLIR/NESAbstractionToMLIR.hpp>
 #include <Experimental/NESAbstraction/NESAbstractionNode/NESAbstractionForNode.hpp>
 #include <Experimental/NESAbstraction/NESAbstractionNode/NESAbstractionIfNode.hpp>
 #include <Experimental/NESAbstraction/NESAbstractionNode/NESAbstractionWriteNode.hpp>
 /**
-   * @brief Create a simple NESAbstractionTree used for testing.
-   * @return std::unique_ptr<NESAbstractionTree>
-   */
+  * @brief Create a simple NESAbstractionTree used for testing.
+  * @return std::unique_ptr<NESAbstractionTree>
+  */
 std::shared_ptr<NESAbstractionTree> createSimpleNESAbstractionTree(
     uint64_t numTuples, std::vector<uint64_t> indexes, std::vector<NESAbstractionNode::BasicType> types) {
     // Define the ForNode.
