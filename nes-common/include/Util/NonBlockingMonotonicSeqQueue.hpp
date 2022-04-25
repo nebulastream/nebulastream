769d8d4df2d92fa45e385e3305d66b8cf3780091
Philipp Grulich <philippgrulich@hotmail.de>
2022-04-25 10:07:23 +0200

    [#2540] add NonBlockingMonotonicSeqQueue

--- nes-common/include/Util/NonBlockingMonotonicSeqQueue.hpp
+++ nes-common/include/Util/NonBlockingMonotonicSeqQueue.hpp
@@ -16,7 +16,6 @@
 #define NES_INCLUDE_UTIL_NONBLOCKINGWATERMARKPROCESSOR_HPP_
 
 #include <Util/Logger/Logger.hpp>
-#include <Util/libcuckoo/cuckoohash_map.hh>
 #include <algorithm>
 #include <assert.h>
 #include <atomic>
@@ -25,19 +24,30 @@
 namespace NES::Util {
 
 /**
- * @brief This class implements a non blocking monotonic sequence queue.
+ * @brief This class implements a non blocking monotonic sequence queue,
+ * which is mainly used as a foundation for an efficient watermark processor.
  * This queue receives values of type T with an associated sequence number and
  * returns the value with the highest sequence number in strictly monotonic increasing order.
  *
- * Internally this queue is implemented as a linked-list of blocks and each block stores a list of <seq,T> pairs.
+ * Internally this queue is implemented as a linked-list of blocks and each block stores a array of <seq,T> pairs.
  * |------- |       |------- |
  * | s1, s2 | ----> | s3, s4 |
  * |------- |       | ------ |
  *
+ * The high level flow is the following:
+ * If we receive the following sequence of input pairs <seq,T>:
+ * Note that the events with seq 3 and 5 are received out-of-order.
+ *
+ * [<1,T1>,<2,T2>,<4,T4>,<6,T6>,<3,T3>,<7,T7>,<5,T5>]
+ *
+ * GetCurrentValue will return the following sequence of current values:
+ * [<1,T1>,<2,T2>,<2,T2>,<2,T2>,<2,T2>,<4,T6>,<7,T7>]
+ *
+ *
  * @tparam T
  * @tparam blockSize
  */
-template<class T, uint64_t blockSize = 100>
+template<class T, uint64_t blockSize = 10000>
 class NonBlockingMonotonicSeqQueue {
   private:
     /**
@@ -67,21 +77,25 @@ class NonBlockingMonotonicSeqQueue {
     ~NonBlockingMonotonicSeqQueue() {}
 
     /**
-     * @brief Emplace the next element to the queue and
-     * @param seq
-     * @param value
+     * @brief Emplace a new element to the queue.
+     * This method can be called concurrently.
+     * However, only one element with a given sequence number can be inserted.
+     * @param sequenceNumber of the new element.
+     * @param value of the new value.
+     * @throws RuntimeException if an element with the same sequence number was already inserted.
      */
-    void emplace(uint64_t seq, T value) {
-        // First we emplace the value to its specific block.
-        // After this call it is safe to assume that a block, which contains seq exists.
-        emplaceValueInBlock(seq, value);
-        // We now try to shift the current sequence number
-        shiftCurrent();
+    void emplace(uint64_t sequenceNumber, T value) {
+        // First emplace the value to the specific block of the sequenceNumber.
+        // After this call it is safe to assume that a block, which contains the sequenceNumber exists.
+        emplaceValueInBlock(sequenceNumber, value);
+        // Try to shift the current sequence number
+        shiftCurrentValue();
     }
 
     /**
      * @brief Returns the current value.
-     * @return T
+     * This method is thread save, however it is not guaranteed that current value dose not change concurrently.
+     * @return T value
      */
     auto getCurrentValue() {
         auto currentBlock = std::atomic_load(&head);
@@ -95,22 +109,32 @@ class NonBlockingMonotonicSeqQueue {
     }
 
   private:
+    /**
+     * @brief Emplace a value T to the specific location of the passed sequence number
+     *
+     * The method is split in two phased:
+     * 1. Find the correct block for this sequence number. If the block not yet exists we add a new block to the linked-list.
+     * 2. Place value at the correct slot associated with the sequence number.
+     *
+     * @param seq the sequence number of the value
+     * @param value the value that should be stored.
+     */
     void emplaceValueInBlock(uint64_t seq, T value) {
-        // The block index, which contains the sequence number.
-        auto targetBlockIndex = seq / blockSize;
-        // Find the right block
-        auto currentBlock = std::atomic_load(&head);
         // Each block contains blockSize elements and covers sequence numbers from
         // [blockIndex * blockSize] till [blockIndex * blockSize + blockSize]
-        // if the end seq number is smaller than the searched seq we have to go to the next block.
+        // Calculate the target block index, which contains the sequence number
+        auto targetBlockIndex = seq / blockSize;
+        // Lookup the current block
+        auto currentBlock = std::atomic_load(&head);
+        // if the blockIndex is smaller the target block index we travers the next block
         while (currentBlock->blockIndex < targetBlockIndex) {
             // append new block if the next block is a nullptr
             auto nextBlock = std::atomic_load(&currentBlock->next);
             if (nextBlock == nullptr) {
                 auto newBlock = std::make_shared<Block>(currentBlock->blockIndex + 1);
-                std::atomic_compare_exchange_strong(&currentBlock->next, &nextBlock, newBlock);
-                // we don't if this or another thread succeeds, as we just start over again in the loop
-                // and use what ever is now stored in currentBlock.next.
+                std::atomic_compare_exchange_weak(&currentBlock->next, &nextBlock, newBlock);
+                // we don't care if this or another thread succeeds, as we just start over again in the loop
+                // and use what ever is now stored in currentBlock.next
             } else {
                 // move to the next block
                 currentBlock = nextBlock;
@@ -119,18 +143,25 @@ class NonBlockingMonotonicSeqQueue {
 
         // check if we really found the correct block
         if (!(seq >= currentBlock->blockIndex * blockSize && seq < currentBlock->blockIndex * blockSize + blockSize)) {
-            NES_THROW_RUNTIME_ERROR("The found block is wrong, we expected ");
+            NES_THROW_RUNTIME_ERROR("The found block is wrong");
         }
 
         // Emplace value in block
-        // It is safe to perform this operation without atomics as no other thread will have the same sequence number,
-        // and thus can modify this value.
+        // It is safe to perform this operation without atomics as no other thread can't the same sequence number,
+        // and thus can't modify this value.
+        // Concurrent can also not happen yet as the currentSeq is only modified in shiftCurrent.
         auto seqIndexInBlock = seq - (currentBlock->blockIndex * blockSize);
         currentBlock->log[seqIndexInBlock].seq = seq;
         currentBlock->log[seqIndexInBlock].value = value;
     }
 
-    void shiftCurrent() {
+    /**
+     * @brief This method shifts tries to shift the current value.
+     * To this end, it checks if the next expected sequence number (currentSeq + 1) is already inserted.
+     * If the next sequence number is available it replaces the currentSeq with the next one.
+     * If the next sequence number is in a new block this method also replaces the pointer to the next block.
+     */
+    void shiftCurrentValue() {
         auto checkForUpdate = true;
         while (checkForUpdate) {
             auto currentBlock = std::atomic_load(&head);
@@ -150,7 +181,7 @@ class NonBlockingMonotonicSeqQueue {
                     // this will always be the first element
                     auto& value = nextBlock->log[0];
                     if (value.seq == nextSeqNumber) {
-                        // the next sequence number is still in the current block thus we only have to exchange the currentSeq.
+                        // Modify currentSeq and head
                         if (std::atomic_compare_exchange_strong(&currentSeq, &currentSequenceNumber, nextSeqNumber)) {
                             NES_DEBUG("Swap HEAD: remove " << currentBlock->blockIndex << " - " << currentBlock.use_count());
                             std::atomic_compare_exchange_strong(&head, &currentBlock, nextBlock);
