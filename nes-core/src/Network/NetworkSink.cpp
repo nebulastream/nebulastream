67c6a03b47fc673919e8ea88baa1758cd861839a
AKozar <kozar.anastasiia@gmail.com>
2022-05-10 16:31:14 +0200

    [#2798] adding nesPartition as key to storage

--- nes-core/src/Network/NetworkSink.cpp
+++ nes-core/src/Network/NetworkSink.cpp
@@ -57,7 +57,7 @@ bool NetworkSink::writeData(Runtime::TupleBuffer& inputBuffer, Runtime::WorkerCo
     if (channel) {
         auto success = channel->sendBuffer(inputBuffer, sinkFormat->getSchemaPtr()->getSchemaSizeInBytes());
         if (faultToleranceType == FaultToleranceType::AT_LEAST_ONCE) {
-            workerContext.insertIntoStorage(inputBuffer);
+            workerContext.insertIntoStorage(nesPartition.getPartitionId(), inputBuffer);
         }
         return success;
     }
@@ -101,7 +101,7 @@ void NetworkSink::reconfigure(Runtime::ReconfigurationMessage& task, Runtime::Wo
             NES_ASSERT(channel, "Channel not valid partition " << nesPartition);
             workerContext.storeNetworkChannel(nesPartition.getOperatorId(), std::move(channel));
             workerContext.setObjectRefCnt(this, task.getUserData<uint32_t>());
-            workerContext.createStorage();
+            workerContext.createStorage(nesPartition.getPartitionId());
             NES_DEBUG("NetworkSink: reconfigure() stored channel on " << nesPartition.toString() << " Thread "
                                                                       << Runtime::NesThread::getId() << " ref cnt "
                                                                       << task.getUserData<uint32_t>());
@@ -122,7 +122,7 @@ void NetworkSink::reconfigure(Runtime::ReconfigurationMessage& task, Runtime::Wo
         case Runtime::PropagateEpoch: {
             //on arrival of an epoch barrier trim data in buffer storages in network sinks that belong to one query plan
             NES_DEBUG("Executing PropagateEpoch on qep queryId=" << queryId);
-            workerContext.trimStorage(task.getUserData<uint64_t>());
+            workerContext.trimStorage(task.getUserData<uint64_t>(), nesPartition.getPartitionId());
             break;
         }
         default: {
