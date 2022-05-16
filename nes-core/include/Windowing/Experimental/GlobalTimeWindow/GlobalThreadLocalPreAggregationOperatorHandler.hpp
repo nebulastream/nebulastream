83920053021de4f8241386976f6c8cfc9b53322f
Philipp Grulich <philippgrulich@hotmail.de>
2022-05-16 22:24:42 +0200

    [#2800] move files

---
+++ nes-core/include/Windowing/Experimental/GlobalTimeWindow/GlobalThreadLocalPreAggregationOperatorHandler.hpp
@@ -0,0 +1,93 @@
+#ifndef NES_NES_CORE_INCLUDE_WINDOWING_EXPERIMENTAL_GLOBALTIMEBASEDWINDOW_GLOBALTHREADLOCALPREAGGREGATIONOPERATORHANDLER_HPP_
+#define NES_NES_CORE_INCLUDE_WINDOWING_EXPERIMENTAL_GLOBALTIMEBASEDWINDOW_GLOBALTHREADLOCALPREAGGREGATIONOPERATORHANDLER_HPP_
+
+#include <Runtime/Execution/OperatorHandler.hpp>
+#include <Windowing/Experimental/GlobalTimeWindow/GlobalThreadLocalSliceStore.hpp>
+namespace NES::Experimental {
+class HashMapFactory;
+using HashMapFactoryPtr = std::shared_ptr<HashMapFactory>;
+class LockFreeMultiOriginWatermarkProcessor;
+}// namespace NES::Experimental
+
+namespace NES::Windowing {
+class LogicalWindowDefinition;
+using LogicalWindowDefinitionPtr = std::shared_ptr<LogicalWindowDefinition>;
+}// namespace NES::Windowing
+
+namespace NES::Windowing::Experimental {
+class KeyedThreadLocalSliceStore;
+class GlobalSliceStaging;
+
+/**
+ * @brief The KeyedThreadLocalPreAggregationOperator provides an operator handler to perform slice-based pre-aggregation of tumbling and sliding windows.
+ * This window handler, maintains a slice store for each worker thread and provides them for the aggregation.
+ * For each processed tuple buffer triggerThreadLocalState is called, which checks if the thread-local slice store should be triggered.
+ */
+class GlobalThreadLocalPreAggregationOperatorHandler
+    : public Runtime::Execution::OperatorHandler,
+      public detail::virtual_enable_shared_from_this<GlobalThreadLocalPreAggregationOperatorHandler, false> {
+    using inherited0 = detail::virtual_enable_shared_from_this<GlobalThreadLocalPreAggregationOperatorHandler, false>;
+    using inherited1 = Runtime::Reconfigurable;
+
+  public:
+    GlobalThreadLocalPreAggregationOperatorHandler(const Windowing::LogicalWindowDefinitionPtr& windowDefinition,
+                                                   const std::vector<OriginId> origins,
+                                                   std::weak_ptr<GlobalSliceStaging> weakSliceStagingPtr);
+
+    /**
+     * @brief Initializes the thread local state for the window operator
+     * @param ctx PipelineExecutionContext
+     * @param hashmapFactory HashMapFactoryPtr
+     */
+    void setup(Runtime::Execution::PipelineExecutionContext& ctx, uint64_t entrySize);
+
+    void start(Runtime::Execution::PipelineExecutionContextPtr pipelineExecutionContext,
+               Runtime::StateManagerPtr stateManager,
+               uint32_t localStateVariableId) override;
+
+    void stop(Runtime::Execution::PipelineExecutionContextPtr pipelineExecutionContext) override;
+
+    /**
+     * @brief This method triggers the thread local state and appends all slices,
+     * which end before the current global watermark to the slice staging area.
+     * @param wctx WorkerContext
+     * @param ctx PipelineExecutionContext
+     * @param workerId
+     * @param originId
+     * @param sequenceNumber
+     * @param watermarkTs
+     */
+    void triggerThreadLocalState(Runtime::WorkerContext& wctx,
+                                 Runtime::Execution::PipelineExecutionContext& ctx,
+                                 uint64_t workerId,
+                                 OriginId originId,
+                                 uint64_t sequenceNumber,
+                                 uint64_t watermarkTs);
+
+    /**
+     * @brief Returns the thread local slice store by a specific worker thread id
+     * @param workerId
+     * @return
+     */
+    GlobalThreadLocalSliceStore& getThreadLocalSliceStore(uint64_t workerId);
+
+    /**
+     * @brief Returns the window definition.
+     * @return Windowing::LogicalWindowDefinitionPtr
+     */
+    Windowing::LogicalWindowDefinitionPtr getWindowDefinition();
+
+    ~GlobalThreadLocalPreAggregationOperatorHandler();
+
+  private:
+    uint64_t windowSize;
+    uint64_t windowSlide;
+    std::weak_ptr<GlobalSliceStaging> weakSliceStaging;
+    std::vector<std::unique_ptr<GlobalThreadLocalSliceStore>> threadLocalSliceStores;
+    std::shared_ptr<::NES::Experimental::LockFreeMultiOriginWatermarkProcessor> watermarkProcessor;
+    Windowing::LogicalWindowDefinitionPtr windowDefinition;
+    NES::Experimental::HashMapFactoryPtr factory;
+};
+}// namespace NES::Windowing::Experimental
+
+#endif//NES_NES_CORE_INCLUDE_WINDOWING_EXPERIMENTAL_GLOBALTIMEBASEDWINDOW_GLOBALTHREADLOCALPREAGGREGATIONOPERATORHANDLER_HPP_
---
+++ nes-core/src/Windowing/Experimental/KeyedTimeWindow/KeyedSlidingWindowSinkOperatorHandler.cpp
@@ -0,0 +1,71 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include <Runtime/BufferManager.hpp>
+#include <Runtime/Execution/ExecutablePipelineStage.hpp>
+#include <Runtime/Execution/PipelineExecutionContext.hpp>
+#include <Runtime/ExecutionResult.hpp>
+#include <Runtime/Reconfigurable.hpp>
+#include <Runtime/TupleBuffer.hpp>
+#include <Runtime/WorkerContext.hpp>
+#include <State/StateVariable.hpp>
+#include <Util/Experimental/HashMap.hpp>
+#include <Util/NonBlockingMonotonicSeqQueue.hpp>
+#include <Windowing/Experimental/LockFreeMultiOriginWatermarkProcessor.hpp>
+#include <Windowing/Experimental/LockFreeWatermarkProcessor.hpp>
+#include <Windowing/Experimental/KeyedTimeWindow/KeyedGlobalSliceStore.hpp>
+#include <Windowing/Experimental/KeyedTimeWindow/KeyedSlice.hpp>
+#include <Windowing/Experimental/KeyedTimeWindow/KeyedSlidingWindowSinkOperatorHandler.hpp>
+#include <Windowing/Experimental/KeyedTimeWindow/KeyedThreadLocalSliceStore.hpp>
+#include <Windowing/Experimental/KeyedTimeWindow/SliceStaging.hpp>
+#include <Windowing/Experimental/WindowProcessingTasks.hpp>
+#include <Windowing/LogicalWindowDefinition.hpp>
+#include <Windowing/WindowMeasures/TimeMeasure.hpp>
+#include <Windowing/WindowTypes/WindowType.hpp>
+namespace NES::Windowing::Experimental {
+
+KeyedSlidingWindowSinkOperatorHandler::KeyedSlidingWindowSinkOperatorHandler(
+    const Windowing::LogicalWindowDefinitionPtr& windowDefinition,
+    std::shared_ptr<KeyedGlobalSliceStore>& globalSliceStore)
+    : globalSliceStore(globalSliceStore), windowDefinition(windowDefinition) {
+    windowSize = windowDefinition->getWindowType()->getSize().getTime();
+    windowSlide = windowDefinition->getWindowType()->getSlide().getTime();
+}
+
+void KeyedSlidingWindowSinkOperatorHandler::setup(Runtime::Execution::PipelineExecutionContext&,
+                                                  NES::Experimental::HashMapFactoryPtr hashmapFactory) {
+    this->factory = hashmapFactory;
+}
+
+void KeyedSlidingWindowSinkOperatorHandler::start(Runtime::Execution::PipelineExecutionContextPtr,
+                                                  Runtime::StateManagerPtr,
+                                                  uint32_t) {
+    NES_DEBUG("start KeyedSlidingWindowSinkOperatorHandler");
+}
+
+void KeyedSlidingWindowSinkOperatorHandler::stop(Runtime::Execution::PipelineExecutionContextPtr) {
+    NES_DEBUG("stop KeyedSlidingWindowSinkOperatorHandler");
+    globalSliceStore.reset();
+}
+
+KeyedSlicePtr KeyedSlidingWindowSinkOperatorHandler::createKeyedSlice(WindowTriggerTask* windowTriggerTask) {
+    return std::make_unique<KeyedSlice>(factory, windowTriggerTask->windowStart, windowTriggerTask->windowEnd);
+}
+std::vector<KeyedSliceSharedPtr> KeyedSlidingWindowSinkOperatorHandler::getSlicesForWindow(WindowTriggerTask* windowTriggerTask) {
+    NES_DEBUG("getSlicesForWindow " << windowTriggerTask->windowStart << " - " << windowTriggerTask->windowEnd);
+    return globalSliceStore->getSlicesForWindow(windowTriggerTask->windowStart, windowTriggerTask->windowEnd);
+}
+Windowing::LogicalWindowDefinitionPtr KeyedSlidingWindowSinkOperatorHandler::getWindowDefinition() { return windowDefinition; }
+
+}// namespace NES::Windowing::Experimental
