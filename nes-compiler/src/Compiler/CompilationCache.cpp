1143411a5db6fe193870547d7d35f7a3b3b3c606
zeuchste <steffen.zeuch@dfki.de>
2022-05-25 14:33:52 +0200

    [#2790] add file

---
+++ nes-compiler/src/Compiler/CompilationCache.cpp
@@ -0,0 +1,37 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include <Compiler/CompilationCache.hpp>
+#include <Util/Logger/Logger.hpp>
+#include <Compiler/CompilationRequest.hpp>
+#include <Compiler/CompilationResult.hpp>
+#include <Compiler/SourceCode.hpp>
+namespace NES::Compiler {
+
+bool CompilationCache::exists(std::shared_ptr<SourceCode> code) { return compilationReuseMap.contains(code); };
+
+void CompilationCache::insert(std::pair<std::shared_ptr<SourceCode>, CompilationResult> newEntry) {
+    lock.lock();
+    if (exists(newEntry.first)) {
+        NES_WARNING("Compilation Cache: inserted item already exists");
+    } else {
+        compilationReuseMap.insert(newEntry);
+    }
+}
+
+CompilationResult CompilationCache::get(std::shared_ptr<SourceCode> code) {
+    lock.lock();
+    return compilationReuseMap.find(code)->second;
+}
+}// namespace NES::Compiler
