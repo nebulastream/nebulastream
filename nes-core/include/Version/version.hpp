2f9f2df62d33e4ca22fe64405b8e4f991cee8e85
NES-CI <nebulastream@dima.tu-berlin.de>
2022-06-01 22:40:50 +0000

    GIT-CI: Updating NES version to 0.2.44

--- nes-core/include/Version/version.hpp
+++ nes-core/include/Version/version.hpp
@@ -17,6 +17,6 @@
 #define NES_VERSION_MAJOR 0
 #define NES_VERSION_MINOR 2
 #define NES_VERSION_PATCH 44
-#define NES_VERSION_POST_FIX -SNAPSHOT
-constexpr char const* NES_VERSION      = "0.2.44-SNAPSHOT";
+#define NES_VERSION_POST_FIX 
+constexpr char const* NES_VERSION      = "0.2.44";
 #endif
