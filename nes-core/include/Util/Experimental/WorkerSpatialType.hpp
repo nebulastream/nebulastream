7294d0f6300c7b968eec6ea63e92db8ff2d901a5
fkuzun <felix.s.lang@campus.tu-berlin.de>
2022-06-24 15:41:10 +0200

    [#2703] removed incomplete test and added preambles

--- nes-core/include/Util/Experimental/WorkerSpatialType.hpp
+++ nes-core/include/Util/Experimental/WorkerSpatialType.hpp
@@ -11,7 +11,6 @@
     See the License for the specific language governing permissions and
     limitations under the License.
 */
-
 #ifndef NES_UTIL_EXPERIMENTL_WORKERSPATIALTYPE_HPP
 #define NES_UTIL_EXPERIMENTL_WORKERSPATIALTYPE_HPP
 #include <string>
