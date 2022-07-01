d1a68c2ca70d4e1f5186b4e083fecededf04bffc
flo <floren-tine@web.de>
2022-07-01 14:56:30 +0200

    [#2770] added executable for oatpp tests

--- nes-core/tests/UnitTests/REST/TestsForOatppEndpoints.cpp
+++ nes-core/tests/UnitTests/REST/TestsForOatppEndpoints.cpp
@@ -24,9 +24,9 @@ TEST_F(TestsForOatppEndpoints, testControllerSimpleTest) {
     oatpp::base::Environment::init();
 
     runTests();
-
-    /* Print how much objects were created during app running, and what have left-probably leaked */
-    /* Disable object counting for release builds using '-D OATPP_DISABLE_ENV_OBJECT_COUNTERS' flag for better performance */
+/*
+    // Print how much objects were created during app running, and what have left-probably leaked
+    // Disable object counting for release builds using '-D OATPP_DISABLE_ENV_OBJECT_COUNTERS' flag for better performance
     std::cout << "\nEnvironment:\n";
     std::cout << "objectsCount = " << oatpp::base::Environment::getObjectsCount() << "\n";
     std::cout << "objectsCreated = " << oatpp::base::Environment::getObjectsCreated() << "\n\n";
@@ -34,6 +34,6 @@ TEST_F(TestsForOatppEndpoints, testControllerSimpleTest) {
     OATPP_ASSERT(oatpp::base::Environment::getObjectsCount() == 0);
 
     oatpp::base::Environment::destroy();
-
+*/
     return 0;
 }
