0fde2d90448df6736886ac0be601cfbcd6353b67
flo <floren-tine@web.de>
2022-07-11 11:28:11 +0200

    [#2770] changed back testing to using the oatpp Client

---
+++ nes-core/tests/UnitTests/REST/OatppTests/MyControllerTest.hpp
@@ -0,0 +1,15 @@
+#ifndef NES_NES_CORE_TESTS_UNITTESTS_REST_OATPPTESTS_MYCONTROLLERTEST_HPP_
+#define NES_NES_CORE_TESTS_UNITTESTS_REST_OATPPTESTS_MYCONTROLLERTEST_HPP_
+
+
+#include <oatpp-test/UnitTest.hpp>
+
+class MyControllerTest : public oatpp::test::UnitTest {
+  public:
+
+    MyControllerTest() : UnitTest("TEST[MyControllerTest]" /* Test TAG for logs */){}
+    void onRun() override;
+
+};
+
+#endif//NES_NES_CORE_TESTS_UNITTESTS_REST_OATPPTESTS_MYCONTROLLERTEST_HPP_
