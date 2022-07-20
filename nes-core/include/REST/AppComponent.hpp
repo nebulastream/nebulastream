8aed7ae352cbac92dca2680570e8ec94da2d4daa
Balint Rueb <rueb@campus.tu-berlin.de>
2022-07-20 18:44:50 +0200

    2770 fixed preambles

--- nes-core/include/REST/AppComponent.hpp
+++ nes-core/include/REST/AppComponent.hpp
@@ -1,16 +1,17 @@
 /*
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
 
-    https://www.apache.org/licenses/LICENSE-2.0
+        https://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
 */
+
 
 /***************************************************************************
  *
@@ -45,39 +46,38 @@ class AppComponent {
         :host(host), port(port){
             NES_INFO("Creating AppComponent");
         };
-  private:
-    std::string host;
-    uint16_t port;
 
-  public:
-  /**
-   *  Create ConnectionProvider component which listens on the port
-   */
+    /**
+     *  Create ConnectionProvider component which listens on the port
+     */
     OATPP_CREATE_COMPONENT(std::shared_ptr<oatpp::network::ServerConnectionProvider>, serverConnectionProvider)([&] {
         return oatpp::network::tcp::server::ConnectionProvider::createShared({host, port, oatpp::network::Address::IP_4});
     }());
     /**
    *  Create Router component
    */
-
     OATPP_CREATE_COMPONENT(std::shared_ptr<oatpp::web::server::HttpRouter>, httpRouter)([] {
         return oatpp::web::server::HttpRouter::createShared();
     }());
+
     /**
    *  Create ConnectionHandler component which uses Router component to route requests
    */
-
     OATPP_CREATE_COMPONENT(std::shared_ptr<oatpp::network::ConnectionHandler>, serverConnectionHandler)([] {
         OATPP_COMPONENT(std::shared_ptr<oatpp::web::server::HttpRouter>, router); // get Router component
         return oatpp::web::server::HttpConnectionHandler::createShared(router);
     }());
+
     /**
    *  Create ObjectMapper component to serialize/deserialize DTOs in Contoller's API
    */
-
     OATPP_CREATE_COMPONENT(std::shared_ptr<oatpp::data::mapping::ObjectMapper>, apiObjectMapper)([] {
         return oatpp::parser::json::mapping::ObjectMapper::createShared();
     }());
+
+  private:
+    std::string host;
+    uint16_t port;
 };
 
 #endif//NES_NES_CORE_INCLUDE_REST_APPCOMPONENT_HPP_
