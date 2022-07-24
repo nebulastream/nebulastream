5fc32489471573aaa914f892a75a9794d0bb606e
Balint Rueb <rueb@campus.tu-berlin.de>
2022-07-24 21:29:21 +0200

    #2770 fixed preambles

--- nes-core/tests/UnitTests/REST/Controller/ConnectivityControllerTest.cpp
+++ nes-core/tests/UnitTests/REST/Controller/ConnectivityControllerTest.cpp
@@ -1,7 +1,3 @@
-//
-// Created by balint on 24.07.22.
-//
-
 /*
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
