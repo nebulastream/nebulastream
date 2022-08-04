f47b721547fd58b7a4b1487c882e4c282a568b40
AKozar <kozar.anastasiia@gmail.com>
2022-08-04 17:16:31 +0200

    [#2798] latency commented

--- nes-core/src/Runtime/WorkerContext.cpp
+++ nes-core/src/Runtime/WorkerContext.cpp
@@ -28,26 +28,26 @@ WorkerContext::WorkerContext(uint32_t workerId,
     //we changed from a local pool to a fixed sized pool as it allows us to manage the numbers that are hold in the cache via the paramter
     localBufferPool = bufferManager->createLocalBufferPool(numberOfBuffersPerWorker);
     NES_ASSERT(localBufferPool != nullptr, "Local buffer is not allowed to be null");
-    statisticsFile.open("latency" + std::to_string(workerId) + ".csv", std::ios::out);
+//    statisticsFile.open("latency" + std::to_string(workerId) + ".csv", std::ios::out);
     storageFile.open("storage" + std::to_string(workerId) + ".csv", std::ios::out);
-    statisticsFile << "time, latency\n";
+//    statisticsFile << "time, latency\n";
     storageFile << "time, numberOfBuffers\n";
 }
 
 WorkerContext::~WorkerContext() {
     localBufferPool->destroy();
-    statisticsFile.flush();
-    statisticsFile.close();
+//    statisticsFile.flush();
+//    statisticsFile.close();
     storageFile.flush();
     storageFile.close();
 }
 
-size_t WorkerContext::getStorageSize() {
-    auto size = 0;
-    for (auto iteratorPartitionId : this->storage) {
-        size += iteratorPartitionId.second.size();
+size_t WorkerContext::getStorageSize(Network::NesPartition nesPartitionId) {
+    auto iteratorPartitionId = this->storage.find(nesPartitionId);
+    if (iteratorPartitionId != this->storage.end()) {
+        return iteratorPartitionId->second.size();
     }
-    return size;
+    return 0;
 }
 
 void WorkerContext::printStatistics(Runtime::TupleBuffer& inputBuffer) {
@@ -99,7 +99,7 @@ void WorkerContext::insertIntoStorage(Network::NesPartition nesPartitionId, NES:
     auto ts = std::chrono::system_clock::now();
     auto timeNow = std::chrono::system_clock::to_time_t(ts);
     storageFile << std::put_time(std::localtime(&timeNow), "%Y-%m-%d %X") << ",";
-    storageFile << getStorageSize() << "\n";
+    storageFile << getStorageSize(nesPartitionId) << "\n";
 }
 
 void WorkerContext::trimStorage(Network::NesPartition nesPartitionId, uint64_t timestamp) {
