d3eca7a9de6d9512bc451544ed00483290e47783
stnoah <noahstoecker99@gmail.com>
2022-08-06 13:22:18 +0200

    added test file

---
+++ nes-core/tests/Integration/BasicTest.cpp
@@ -0,0 +1,199 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+#include <Catalogs/Source/PhysicalSource.hpp>
+#include <Catalogs/Source/PhysicalSourceTypes/CSVSourceType.hpp>
+#include <Common/DataTypes/DataTypeFactory.hpp>
+#include <Components/NesCoordinator.hpp>
+#include <Components/NesWorker.hpp>
+#include <Configurations/Coordinator/CoordinatorConfiguration.hpp>
+#include <Configurations/Worker/WorkerConfiguration.hpp>
+#include <NesBaseTest.hpp>
+#include <Operators/LogicalOperators/Sinks/SinkLogicalOperatorNode.hpp>
+#include <Plans/Global/Query/SharedQueryPlan.hpp>
+#include <Plans/Query/QueryId.hpp>
+#include <Services/QueryService.hpp>
+#include <Sinks/Mediums/SinkMedium.hpp>
+#include <Util/Logger/Logger.hpp>
+#include <Util/TestUtils.hpp>
+#include <chrono>
+#include <cpprest/filestream.h>
+#include <cpprest/http_client.h>
+#include <gtest/gtest.h>
+#include <thread>
+
+using namespace utility;
+using namespace std;
+using namespace web;
+// Common features like URIs.
+using namespace web::http;
+// Common HTTP functionality
+using namespace web::http::client;
+// HTTP client features
+using namespace concurrency::streams;
+
+namespace NES {
+
+using namespace Configurations;
+const int timestamp = 1644426604;
+const uint64_t numberOfTupleBuffers = 4;
+
+class BasicTest : public Testing::NESBaseTest {
+  public:
+    std::string ipAddress = "127.0.0.1";
+    CoordinatorConfigurationPtr coordinatorConfig;
+    WorkerConfigurationPtr workerConfig;
+    CSVSourceTypePtr csvSourceTypeInfinite;
+    CSVSourceTypePtr csvSourceTypeFinite;
+    SchemaPtr inputSchema;
+    Runtime::BufferManagerPtr bufferManager;
+
+    static void SetUpTestCase() {
+        NES::Logger::setupLogging("BasicTest.log", NES::LogLevel::LOG_DEBUG);
+        NES_INFO("Setup BasicTest test class.");
+    }
+
+    void SetUp() override {
+        Testing::NESBaseTest::SetUp();
+
+        bufferManager = std::make_shared<Runtime::BufferManager>(1024, 1);
+
+        coordinatorConfig = CoordinatorConfiguration::create();
+        coordinatorConfig = CoordinatorConfiguration::create();
+        coordinatorConfig->rpcPort = *rpcCoordinatorPort;
+        coordinatorConfig->restPort = *restPort;
+
+        workerConfig = WorkerConfiguration::create();
+        workerConfig->coordinatorPort = *rpcCoordinatorPort;
+
+        csvSourceTypeInfinite = CSVSourceType::create();
+        csvSourceTypeInfinite->setFilePath(std::string(TEST_DATA_DIRECTORY) + "window-out-of-order.csv");
+        csvSourceTypeInfinite->setNumberOfTuplesToProducePerBuffer(numberOfTupleBuffers);
+        csvSourceTypeInfinite->setNumberOfBuffersToProduce(0);
+
+        csvSourceTypeFinite = CSVSourceType::create();
+        csvSourceTypeFinite->setFilePath(std::string(TEST_DATA_DIRECTORY) + "window-out-of-order.csv");
+        csvSourceTypeFinite->setNumberOfTuplesToProducePerBuffer(numberOfTupleBuffers - 1);
+        csvSourceTypeFinite->setNumberOfBuffersToProduce(numberOfTupleBuffers);
+
+        inputSchema = Schema::create()
+                          ->addField("value", DataTypeFactory::createUInt64())
+                          ->addField("id", DataTypeFactory::createUInt64())
+                          ->addField("timestamp", DataTypeFactory::createUInt64());
+    }
+};
+
+/*
+ * @brief test timestamp of watermark processor
+ */
+TEST_F(BasicTest, firstTest) {
+    NES_INFO("BasicTest: Start coordinator");
+    NesCoordinatorPtr crd = std::make_shared<NesCoordinator>(coordinatorConfig);
+    crd->getSourceCatalogService()->registerLogicalSource("window", inputSchema);
+    uint64_t port = crd->startCoordinator(/**blocking**/ false);
+    EXPECT_NE(port, 0UL);
+    NES_INFO("BasicTest: Coordinator started successfully");
+
+    //Setup Worker
+    NES_INFO("BasicTest: Start worker 1");
+    auto physicalSource1 = PhysicalSource::create("window", "x1", csvSourceTypeInfinite);
+    workerConfig->physicalSources.add(physicalSource1);
+
+    NesWorkerPtr wrk1 = std::make_shared<NesWorker>(std::move(workerConfig));
+    bool retStart1 = wrk1->start(/**blocking**/ false, /**withConnect**/ true);
+    EXPECT_TRUE(retStart1);
+    NES_INFO("BasicTest: Worker1 started successfully");
+
+    NesWorkerPtr wrk2 = std::make_shared<NesWorker>(std::move(workerConfig));
+    bool retStart2 = wrk2->start(/**blocking**/ false, /**withConnect**/ true);
+    EXPECT_TRUE(retStart2);
+    NES_INFO("BasicTest: Worker2 started successfully");
+
+    QueryServicePtr queryService = crd->getQueryService();
+    QueryCatalogServicePtr queryCatalogService = crd->getQueryCatalogService();
+
+    std::string outputFilePath = getTestResourceFolder() / "testUpstreamBackup.out";
+    remove(outputFilePath.c_str());
+
+    // The query contains a watermark assignment with 50 ms allowed lateness
+    NES_INFO("BasicTest: Submit query");
+    string query =
+        "Query::from(\"window\").sink(FileSinkDescriptor::create(\"" + outputFilePath + R"(", "CSV_FORMAT", "APPEND"));)";
+
+    QueryId queryId =
+        queryService->validateAndQueueAddQueryRequest(query, "BottomUp", FaultToleranceType::NONE, LineageType::IN_MEMORY);
+
+    /*GlobalQueryPlanPtr globalQueryPlan = crd->getGlobalQueryPlan();
+    EXPECT_TRUE(TestUtils::waitForQueryToStart(queryId, queryCatalogService));
+    EXPECT_TRUE(TestUtils::checkCompleteOrTimeout(wrk1, queryId, globalQueryPlan, 1));
+    EXPECT_TRUE(TestUtils::checkCompleteOrTimeout(crd, queryId, globalQueryPlan, 1));*/
+
+    /*auto querySubPlanIds = crd->getNodeEngine()->getSubQueryIds(queryId);
+    for (auto querySubPlanId : querySubPlanIds) {
+        auto sinks = crd->getNodeEngine()->getExecutableQueryPlan(querySubPlanId)->getSinks();
+        for (auto& sink : sinks) {
+            auto buffer1 = bufferManager->getUnpooledBuffer(timestamp);
+            buffer1->setWatermark(timestamp);
+            buffer1->setSequenceNumber(1);
+            sink->updateWatermark(buffer1.value());
+            auto buffer2 = bufferManager->getUnpooledBuffer(timestamp);
+            buffer2->setWatermark(timestamp);
+            buffer2->setOriginId(1);
+            buffer2->setSequenceNumber(1);
+            sink->updateWatermark(buffer2.value());
+            auto currentTimestamp = sink->getCurrentEpochBarrier();
+            while (currentTimestamp == 0) {
+                NES_INFO("BasicTest: current timestamp: " << currentTimestamp);
+                std::this_thread::sleep_for(std::chrono::milliseconds(100));
+                currentTimestamp = sink->getCurrentEpochBarrier();
+            }
+            EXPECT_TRUE(currentTimestamp == timestamp);
+        }
+    }*/
+
+    /*NES_INFO("BasicTest: Remove query");
+    queryService->validateAndQueueStopQueryRequest(queryId);
+    EXPECT_TRUE(TestUtils::checkStoppedOrTimeout(queryId, queryCatalogService));*/
+
+    for (int i= 1; i<= 50; i++) {
+        //std::cout << "\n" << i;
+        NES_INFO("XDXDXDXDXD");
+        NES_INFO(wrk1->getWorkerConfiguration()->numberOfSlots.getValue())
+
+
+        QueryCatalogEntryPtr qptr1 = crd->getQueryCatalogService()->getEntryForQuery(queryId);
+        NES_INFO(qptr1->getQueryString());
+        QueryCatalogPtr qqp = crd->getGlobalQueryPlan().get
+    }
+
+    NES_INFO("BasicTest: Stop worker 1");
+    bool retStopWrk1 = wrk1->stop(true);
+    EXPECT_TRUE(retStopWrk1);
+
+    NES_INFO("BasicTest: Stop worker 2");
+    bool retStopWrk2 = wrk2->stop(true);
+    EXPECT_TRUE(retStopWrk2);
+
+    NES_INFO("BasicTest: Stop Coordinator");
+    bool retStopCord = crd->stopCoordinator(true);
+    EXPECT_TRUE(retStopCord);
+    NES_INFO("BasicTest: Test finished");
+}
+/*
+ * @brief test message passing between sink-coordinator-sources
+ */
+
+/*
+ * @brief test if upstream backup doesn't fail
+ */
+}// namespace NES
