b7a4607c36c7ebcff539a9d0fd901db8b6d202a2
Philipp Grulich <philippgrulich@hotmail.de>
2022-08-08 12:56:09 +0200

    [#2902] fix

--- nes-execution-engine/include/Experimental/Babelfish/IRSerialization.hpp
+++ nes-execution-engine/include/Experimental/Babelfish/IRSerialization.hpp
@@ -14,7 +14,6 @@
 #ifndef NES_NES_EXECUTION_ENGINE_SRC_EXPERIMENTAL_BABELFISH_IRSERIALIZATION_HPP_
 #define NES_NES_EXECUTION_ENGINE_SRC_EXPERIMENTAL_BABELFISH_IRSERIALIZATION_HPP_
 #include <cpprest/json.h>
-
 #include <Experimental/NESIR/BasicBlocks/BasicBlock.hpp>
 #include <Experimental/NESIR/BasicBlocks/BasicBlockInvocation.hpp>
 #include <Experimental/NESIR/NESIR.hpp>
