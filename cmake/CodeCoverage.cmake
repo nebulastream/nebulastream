db9e7d6d881a7c083f20243578ebe89cb53ee18b
Ventura Del Monte <venturadelmonte@gmail.com>
2022-08-12 12:10:39 +0200

    [#1195] Add shared libraries when generating CC

--- cmake/CodeCoverage.cmake
+++ cmake/CodeCoverage.cmake
@@ -367,8 +367,8 @@ function(target_code_coverage TARGET_NAME)
                         COMMAND
                         ${LLVM_COV_PATH} export $<TARGET_FILE:${TARGET_NAME}> ${SO_OBJECTS}
                         -instr-profile=${target_code_coverage_COVERAGE_TARGET_NAME}.profdata
-                        -format="text" ${EXCLUDE_REGEX} >
-                        ${CMAKE_COVERAGE_OUTPUT_DIRECTORY}/${target_code_coverage_COVERAGE_TARGET_NAME}.json
+                        -format="lcov" ${EXCLUDE_REGEX} >
+                        ${CMAKE_COVERAGE_OUTPUT_DIRECTORY}/${target_code_coverage_COVERAGE_TARGET_NAME}.lcov
                         DEPENDS ccov-processing-${target_code_coverage_COVERAGE_TARGET_NAME})
 
                 # Generates HTML output of the coverage information for perusal
