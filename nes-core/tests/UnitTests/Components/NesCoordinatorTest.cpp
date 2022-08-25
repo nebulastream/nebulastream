76f4f7733ec5745da3b774002979bbc2702b87b9
Viktor Rosenfeld <viktor.rosenfeld@tu-berlin.de>
2022-08-25 18:35:46 +0200

    [#2948] Test that internal worker uses the correct configuration

---
+++ nes-core/tests/UnitTests/Components/NesCoordinatorTest.cpp
@@ -0,0 +1,47 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include <NesBaseTest.hpp>
+#include <Configurations/Coordinator/CoordinatorConfiguration.hpp>
+#include <Components/NesCoordinator.hpp>
+#include <Components/NesWorker.hpp>
+
+namespace NES {
+
+class NesCoordinatorTest : public Testing::NESBaseTest {
+  public:
+    static void SetUpTestCase() {
+        NES::Logger::setupLogging("CoordinatorTest.log", NES::LogLevel::LOG_DEBUG);
+    }
+
+    void SetUp() override { Testing::NESBaseTest::SetUp(); }
+};
+
+// Test that the worker configuration from the coordinator configuration is passed to the internal worker.
+TEST_F(NesCoordinatorTest, internalWorkerUsesConfigurationFromCoordinatorConfiguration) {
+    // given
+    auto configuration = CoordinatorConfiguration::create();
+    configuration->rpcPort = *rpcCoordinatorPort;
+    configuration->restPort = *restPort;
+    configuration->worker.numWorkerThreads = 92;
+    // when
+    auto coordinator = std::make_shared<NesCoordinator>(configuration);
+    coordinator->startCoordinator(false);
+    // then
+    ASSERT_EQ(92, coordinator->getNesWorker()->getWorkerConfiguration()->numWorkerThreads.getValue());
+}
+
+
+
+}
