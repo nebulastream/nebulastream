bd44fd35a0d4a8a1d637ef4b4e9cb488477d3414
Balint Rueb <rueb@campus.tu-berlin.de>
2022-08-29 13:13:44 +0200

    #2840 added all GET endpoints, began adding POST endpoints

---
+++ nes-core/include/REST/DTOs/QueryControllerSubmitQueryRequest.hpp
@@ -0,0 +1,48 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#ifndef NES_NES_CORE_INCLUDE_REST_DTOS_NES_NES_CORE_INCLUDE_REST_DTOS_QUERYCONTROLLERSUBMITQUERYREQUEST_HPP_
+#define NES_NES_CORE_INCLUDE_REST_DTOS_NES_NES_CORE_INCLUDE_REST_DTOS_QUERYCONTROLLERSUBMITQUERYREQUEST_HPP_
+
+#include <oatpp/core/data/mapping/type/Object.hpp>
+#include <oatpp/core/macro/codegen.hpp>
+
+/* Begin DTO code-generation */
+#include OATPP_CODEGEN_BEGIN(DTO)
+
+namespace NES {
+const std::string DEFAULT_TOLERANCE_TYPE = "NONE";
+const std::string DEFAULT_LINEAGE_MODE = "NONE";
+namespace REST {
+namespace DTO {
+
+/**
+ * Message Data-Transfer-Object
+ */
+class QueryControllerSubmitQueryRequest : public oatpp::DTO {
+    DTO_INIT(QueryControllerSubmitQueryRequest, DTO /* Extends */)
+    DTO_FIELD(String, userQuery, "userQuery");
+    DTO_FIELD(String, placement, "strategyName");
+    DTO_FIELD(String, faultTolerance, "faultTolerance") = DEFAULT_TOLERANCE_TYPE;
+    DTO_FIELD(String, lineage, "lineage") = DEFAULT_LINEAGE_MODE;
+};
+
+}// namespace DTO
+}// namespace REST
+}// namespace NES
+
+/* End DTO code-generation */
+#include OATPP_CODEGEN_END(DTO)
+
+#endif//NES_NES_CORE_INCLUDE_REST_DTOS_NES_NES_CORE_INCLUDE_REST_DTOS_QUERYCONTROLLERSUBMITQUERYREQUEST_HPP_
---
+++ nes-core/tests/Integration/REST/Controller/QueryControllerTest.cpp
@@ -0,0 +1,74 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+#include <API/Query.hpp>
+#include <Compiler/CPPCompiler/CPPCompiler.hpp>
+#include <Compiler/JITCompilerBuilder.hpp>
+#include <NesBaseTest.hpp>
+#include <Plans/Utils/PlanIdGenerator.hpp>
+#include <REST/ServerTypes.hpp>
+#include <Services/QueryParsingService.hpp>
+#include <Util/Logger/Logger.hpp>
+#include <Util/TestUtils.hpp>
+#include <cpr/cpr.h>
+#include <gtest/gtest.h>
+#include <memory>
+
+namespace NES {
+class QueryControllerTest : public Testing::NESBaseTest {
+  public:
+    static void SetUpTestCase() {
+        NES::Logger::setupLogging("QueryControllerTest.log", NES::LogLevel::LOG_DEBUG);
+        NES_INFO("Setup QueryControllerTest test class.");
+    }
+
+    static void TearDownTestCase() { NES_INFO("Tear down QueryControllerTest test class."); }
+};
+
+TEST_F(QueryControllerTest, testExecuteQueryMalformedBody) {
+    NES_INFO("TestsForOatppEndpoints: Start coordinator");
+    CoordinatorConfigurationPtr coordinatorConfig = CoordinatorConfiguration::create();
+    coordinatorConfig->rpcPort = *rpcCoordinatorPort;
+    coordinatorConfig->restPort = *restPort;
+    coordinatorConfig->restServerType = ServerType::Oatpp;
+    auto coordinator = std::make_shared<NesCoordinator>(coordinatorConfig);
+    ASSERT_EQ(coordinator->startCoordinator(false), *rpcCoordinatorPort);
+    NES_INFO("QueryControllerTest: Coordinator started successfully");
+    bool success = TestUtils::checkRESTServerStartedOrTimeout(coordinatorConfig->restPort.getValue(), 5);
+
+    if (!success) {
+        FAIL() << "Rest server failed to start";
+    }
+    std::string body = "{\"malformed\": \"test\"}";
+    auto response   = cpr::Post(cpr::Url{BASE_URL + std::to_string(*restPort) + "/v1/nes/query/execute-query"},
+                              cpr::Header{{"Content-Type", "application/json"}}, cpr::Body{body},
+                              cpr::ConnectTimeout{3000}, cpr::Timeout{3000});
+    EXPECT_EQ(response.status_code, 500l);
+
+    std::string queryString =
+        R"(Query::from("default_logical").filter(Attribute("value") < 42).sink(PrintSinkDescriptor::create()); )";
+    auto cppCompiler = Compiler::CPPCompiler::create();
+    auto jitCompiler = Compiler::JITCompilerBuilder().registerLanguageCompiler(cppCompiler).build();
+    auto queryParsingService = QueryParsingService::create(jitCompiler);
+    auto queryCatalogService = coordinator->getQueryCatalogService();
+    QueryPtr query = queryParsingService->createQueryFromCodeString(queryString);
+    QueryId queryId = PlanIdGenerator::getNextQueryId();
+    const QueryPlanPtr queryPlan = query->getQueryPlan();
+    queryPlan->setQueryId(queryId);
+    auto catalogEntry = queryCatalogService->createNewEntry(queryString, queryPlan, "BottomUp");
+    cpr::Response re = cpr::Get(cpr::Url{BASE_URL + std::to_string(*restPort) + "/v1/nes/queryCatalog/allRegisteredQueries"});
+    //TODO:: compare content of response to expected values. To be added once json library found #2950
+    EXPECT_EQ(re.status_code, 200l);
+    std::cout << re.text;
+}
+} // namespace NES
