4392a8405ea9cc064418e75fded741a92f9c36cd
Balint Rueb <rueb@campus.tu-berlin.de>
2022-08-29 15:57:05 +0200

    #2840 added POST and DELETE endpoints

--- nes-core/include/REST/OatppController/QueryController.hpp
+++ nes-core/include/REST/OatppController/QueryController.hpp
@@ -19,6 +19,7 @@
 #include <REST/DTOs/QueryControllerOptimizationPhasesResponse.hpp>
 #include <REST/DTOs/QueryControllerSubmitQueryRequest.hpp>
 #include <REST/DTOs/QueryControllerSubmitQueryResponse.hpp>
+#include <REST/DTOs/QueryControllerStopQueryResponse.hpp>
 #include <REST/Handlers/ErrorHandler.hpp>
 #include <REST/OatppController/BaseRouterPrefix.hpp>
 #include <Runtime/QueryStatistics.hpp>
@@ -31,6 +32,7 @@
 #include <exception>
 
 #include OATPP_CODEGEN_BEGIN(ApiController)
+#include "GRPC/Serialization/QueryPlanSerializationUtil.hpp"
 #include "SerializableQueryPlan.pb.h"
 
 namespace NES {
@@ -153,6 +155,7 @@ class QueryController : public oatpp::web::server::api::ApiController {
         //No sense in duplicating functionality, so "query-status" endpoint is not implemented.
        return errorHandler->handleError(Status::CODE_301, "Requests for the status of a query are now served at /queryCatalogController/status?queryId=" + std::to_string(queryId));
     }
+
     ENDPOINT("POST", "/execute-query", submitQuery, BODY_DTO(Object<DTO::QueryControllerSubmitQueryRequest>, request)) {
         try {
             if(!validatePlacementStrategy(request->placement)){
@@ -194,47 +197,76 @@ class QueryController : public oatpp::web::server::api::ApiController {
     ENDPOINT("POST", "/execute-query-ex", submitQueryProtobuf, BODY_STRING(String, request)) {
         try {
             std::shared_ptr<SubmitQueryRequest> protobufMessage = std::make_shared<SubmitQueryRequest>();
-            if (validateProtobufMessage(protobufMessage, request)) {
+            auto optional = validateProtobufMessage(protobufMessage, request);
+            if (optional.has_value()) {
+                    return optional.value();
+            }
+            SerializableQueryPlan* queryPlanSerialized = protobufMessage->mutable_queryplan();
+            QueryPlanPtr queryPlan(QueryPlanSerializationUtil::deserializeQueryPlan(queryPlanSerialized));
+            auto* context = protobufMessage->mutable_context();
+            std::string faultToleranceString = DEFAULT_TOLERANCE_TYPE;
+            std::string lineageString = DEFAULT_TOLERANCE_TYPE;
+            if (context->contains("faultTolerance")) {
+                if (!validateFaultToleranceType(context->at("faultTolerance").value())) {
+                    std::string errorMessage = "Invalid fault tolerance Type provided: " + context->at("faultTolerance").value() +
+                        ". Valid Fault Tolerance Types are: 'AT_MOST_ONCE', 'AT_LEAST_ONCE', 'EXACTLY_ONCE', 'NONE'.";
+                    return errorHandler->handleError(Status::CODE_400, errorMessage);
+                } else {
+                    faultToleranceString = context->at("faultTolerance").value();
+                }
+            }
+            if (context->contains("lineage")) {
+                if (!validateLineageMode(lineageString = context->at("lineage").value())) {
+                    NES_ERROR("QueryController: handlePost -execute-query: Invalid Lineage Type provided: " + lineageString);
+                        std::string errorMessage = "Invalid Lineage Mode Type provided: " + lineageString
+                                                 + ". Valid Lineage Modes are: 'IN_MEMORY', 'PERSISTENT', 'REMOTE', 'NONE'.";
+                    return errorHandler->handleError(Status::CODE_400, errorMessage);
+                } else {
+                    lineageString = context->at("lineage").value();
+                }
+            }
+            std::string* queryString = protobufMessage->mutable_querystring();
+            std::string placementStrategy = context->at("placement").value();
+            auto faultToleranceMode = stringToFaultToleranceTypeMap(faultToleranceString);
+            auto lineageMode = stringToLineageTypeMap(lineageString);
+            QueryId queryId = queryService->addQueryRequest(*queryString,
+                                                            queryPlan,
+                                                            placementStrategy,
+                                                            faultToleranceMode,
+                                                            lineageMode);
 
-            }
-            if(!validatePlacementStrategy(request->placement)){
-                std::string errorMessage =  "Invalid Placement Strategy: " + request->placement +
-                    ". Further info can be found at https://docs.nebula.stream/cpp/class_n_e_s_1_1_placement_strategy.html";
-                return errorHandler->handleError(Status::CODE_400,errorMessage);
-            }
-            if(!validateFaultToleranceType(request->faultTolerance)){
-                std::string errorMessage = "Invalid fault tolerance Type provided: " + request->faultTolerance +
-                    ". Valid Fault Tolerance Types are: 'AT_MOST_ONCE', 'AT_LEAST_ONCE', 'EXACTLY_ONCE', 'NONE'.";
-                return errorHandler->handleError(Status::CODE_400, errorMessage);
-            }
-            auto faultToleranceMode = stringToFaultToleranceTypeMap(request->faultTolerance);
-            auto lineageMode = stringToLineageTypeMap(request->lineage);
-            NES_DEBUG("QueryController: handlePost -execute-query: Params: userQuery= "
-                      << request->userQuery.getValue("null") << ", strategyName= " << request->placement.getValue("null")
-                      << ", faultTolerance= " << request->faultTolerance.getValue("null") << ", lineage= " << request->lineage.getValue("null"));
-            QueryId queryId = queryService->validateAndQueueAddQueryRequest(request->userQuery,
-                                                                            request->placement,
-                                                                            faultToleranceMode,
-                                                                            lineageMode);
             //Prepare the response
             auto dto = DTO::QueryControllerSubmitQueryResponse::createShared();
             dto->queryId = queryId;
-            return createDtoResponse(Status::CODE_200, dto);
-        }
-        catch (const InvalidQueryException& exc) {
-            NES_ERROR("QueryController: handlePost -execute-query: Exception occurred during submission of a query "
-                      "user request:"
-                      << exc.what());
+            return createDtoResponse(Status::CODE_201, dto);
+        } catch (const std::exception& exc) {
+            NES_ERROR(
+                "QueryController: handlePost -execute-query-ex: Exception occurred while building the query plan for "
+                "user request:"
+                << exc.what());
             return errorHandler->handleError(Status::CODE_400, exc.what());
-        }
-        catch (std::exception& e) {
-            std::string errorMessage = "Potentially incorrect or missing key words for user query (use 'userQuery') or placement strategy (use 'strategyName')."
-                                       "For more info check: https://docs.nebula.stream/docs/clients/rest-api/ \n";
-            return errorHandler->handleError(Status::CODE_500, errorMessage + e.what());
+        } catch (...) {
+            NES_ERROR("RestServer: unknown exception.");
+            return errorHandler->handleError(Status::CODE_500, "unknown exception");
         }
     }
 
-
+    ENDPOINT("DELETE", "/stop-query", stopQuery, QUERY(UInt64, queryId, "queryId")){
+        try{
+            bool success = queryService->validateAndQueueStopQueryRequest(queryId);
+            Status status = success ? Status::CODE_202 : Status::CODE_400;  //QueryController catches InvalidQueryStatus exception, but this is never thrown since it was commented out
+            auto dto = DTO::QueryControllerStopQueryResponse::createShared();
+            dto->success = success;
+            return createDtoResponse(status,dto);
+        }
+        catch (QueryNotFoundException e) {
+            return errorHandler->handleError(Status::CODE_404, "No query with given ID: " + std::to_string(queryId));
+        }
+        catch (...) {
+            NES_ERROR("RestServer: unknown exception.");
+            return errorHandler->handleError(Status::CODE_500, "unknown exception");
+        }
+    }
 
   private:
 
@@ -277,6 +309,14 @@ class QueryController : public oatpp::web::server::api::ApiController {
             return errorHandler->handleError(Status::CODE_400,errorMessage);
         }
         return std::nullopt;
+    }
+
+    bool validateLineageMode(const std::string& lineageModeString) {
+        auto lineageMode = stringToLineageTypeMap(lineageModeString);
+        if (lineageMode == LineageType::INVALID) {
+            return false;
+        }
+        return true;
     }
 
     QueryServicePtr queryService;
