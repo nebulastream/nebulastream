bfadd194d86cd886b44d28abf8a96c7bf13eeedd
Balint <rueb@campus.tu-berlin.de>
2022-09-11 12:22:38 +0200

    #2843 added test skeleton

---
+++ nes-core/tests/Integration/REST/Controller/UdfCatalogControllerTest.cpp
@@ -0,0 +1,38 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include <API/Query.hpp>
+#include <Compiler/CPPCompiler/CPPCompiler.hpp>
+#include <Compiler/JITCompilerBuilder.hpp>
+#include <NesBaseTest.hpp>
+#include <Plans/Utils/PlanIdGenerator.hpp>
+#include <REST/ServerTypes.hpp>
+#include <Services/QueryParsingService.hpp>
+#include <Util/Logger/Logger.hpp>
+#include <Util/TestUtils.hpp>
+#include <cpr/cpr.h>
+#include <gtest/gtest.h>
+#include <memory>
+
+namespace NES {
+class QueryCatalogControllerTest : public Testing::NESBaseTest {
+  public:
+    static void SetUpTestCase() {
+        NES::Logger::setupLogging("ConnectivityControllerTest.log", NES::LogLevel::LOG_DEBUG);
+        NES_INFO("Setup TopologyControllerTest test class.");
+    }
+
+    static void TearDownTestCase() { NES_INFO("Tear down ConnectivityControllerTest test class."); }
+};
+}// namespace NES
