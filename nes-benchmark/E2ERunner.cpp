092655ac0abe95b3399e6df0f2e5e2e9593c7ccc
Nils Schubert <n.schubert@campus.tu-berlin.de>
2022-10-05 11:33:52 +0200

    #2999 fixed license

--- nes-benchmark/E2ERunner.cpp
+++ nes-benchmark/E2ERunner.cpp
@@ -1,15 +1,15 @@
 /*
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
 
-    https://www.apache.org/licenses/LICENSE-2.0
+        https://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
 */
 
 
@@ -44,6 +44,7 @@ class BenchmarkRunner : public NES::Exceptions::ErrorListener {
         NES_FATAL_ERROR(fatalErrorMessage.str());
         std::cerr << fatalErrorMessage.str() << std::endl;
     }
+
     void onFatalException(std::shared_ptr<std::exception> exceptionPtr, std::string callStack) override {
         std::ostringstream fatalExceptionMessage;
         fatalExceptionMessage << "onFatalException: exception=[" << exceptionPtr->what() << "] callstack=\n" << callStack;
