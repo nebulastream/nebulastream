07cf1a1d28ea9839bad08122951864a1871db9e8
Nils Schubert <n.schubert@campus.tu-berlin.de>
2022-10-08 12:11:48 +0200

    #1779 created own test class for MlHeuristicPlacement

--- nes-core/tests/UnitTests/Optimizer/QueryPlacement/QueryPlacementTest.cpp
+++ nes-core/tests/UnitTests/Optimizer/QueryPlacement/QueryPlacementTest.cpp
@@ -12,12 +12,10 @@
     limitations under the License.
 */
 
-#include "z3++.h"
 #include <API/QueryAPI.hpp>
 #include <Catalogs/Source/LogicalSource.hpp>
 #include <Catalogs/Source/PhysicalSource.hpp>
 #include <Catalogs/Source/PhysicalSourceTypes/CSVSourceType.hpp>
-#include "Catalogs/Source/PhysicalSourceTypes/MQTTSourceType.hpp"
 #include <Catalogs/Source/SourceCatalog.hpp>
 #include <Catalogs/Source/SourceCatalogEntry.hpp>
 #include <Catalogs/UDF/UdfCatalog.hpp>
@@ -39,7 +37,6 @@
 #include <Optimizer/Phases/TypeInferencePhase.hpp>
 #include <Optimizer/QueryMerger/Z3SignatureBasedPartialQueryMergerRule.hpp>
 #include <Optimizer/QueryPlacement/BasePlacementStrategy.hpp>
-#include <Optimizer/QueryPlacement/ILPStrategy.hpp>
 #include <Optimizer/QueryPlacement/ManualPlacementStrategy.hpp>
 #include <Optimizer/QueryPlacement/PlacementStrategyFactory.hpp>
 #include <Plans/Global/Execution/ExecutionNode.hpp>
@@ -53,27 +50,11 @@
 #include <Topology/TopologyNode.hpp>
 #include <Util/Logger/Logger.hpp>
 #include <gtest/gtest.h>
-#include <utility>
-#include <Optimizer/Phases/SignatureInferencePhase.hpp>
 #include <Optimizer/QueryMerger/Z3SignatureBasedCompleteQueryMergerRule.hpp>
 #include <z3++.h>
 
 
-If some tests fail then declare iris schema in them
-
-
-        Questions:
-    - The FilterOperator with the selectivity is used by Seungmi. I can try to fix it by deleting it and then see what I can do
-    - remove the public interface and just set it by a default of 1
-
-
-    - Talk about: "nes-core/tests/UnitTests/Optimizer/QueryValidation/SemanticQueryValidationTest.cpp" Line 239 - 242
-                   - Create a new class in QueryPlacement UnitTests
-                   - if this takes longer than couple of minutes then create new issue
-
-
-
-                   using namespace NES;
+using namespace NES;
 using namespace z3;
 using namespace Configurations;
 
@@ -196,72 +177,6 @@ class QueryPlacementTest : public Testing::TestWithErrorHandling<testing::Test> 
         typeInferencePhase = Optimizer::TypeInferencePhase::create(sourceCatalog, udfCatalog);
     }
 
-    void topologyGenerator() {
-        topology = Topology::create();
-
-        std::vector<int> parents = {-1, 0, 0, 0, 1, 2, 3, 3, 4, 5, 5, 6, 7};
-        std::vector<int> resources = {20, 20, 20, 20, 21, 22, 23, 23, 24, 25, 25, 26, 27};
-
-        std::vector<int> tf_enabled_nodes = {1, 2, 3, 4, 5, 6, 7, 8, 11, 12};
-        std::vector<int> low_throughput_sources = {11};
-        std::vector<int> ml_hardwares = {};
-
-        std::vector<int> sources {8, 9, 10, 11, 12};
-
-        std::vector<TopologyNodePtr> nodes;
-        for (int i = 0; i < (int) resources.size(); i++) {
-            nodes.push_back(TopologyNode::create(i, "localhost", 123, 124, resources[i]));
-            if(i == 0) {
-                topology->setAsRoot(nodes[i]);
-            } else {
-                topology->addNewTopologyNodeAsChild(nodes[parents[i]], nodes[i]);
-            }
-            if(std::count(tf_enabled_nodes.begin(), tf_enabled_nodes.end(), i)) {
-                nodes[i]->addNodeProperty("tf_installed", true);
-            }
-            if(std::count(low_throughput_sources.begin(), low_throughput_sources.end(), i)) {
-                nodes[i]->addNodeProperty("low_throughput_source", true);
-            }
-            if(std::count(ml_hardwares.begin(), ml_hardwares.end(), i)) {
-                nodes[i]->addNodeProperty("ml_hardware", true);
-            }
-        }
-
-//        std::string schema = R"(Schema::create()->addField(createField("id", UINT64))
-//                           ->addField(createField("SepalLengthCm", FLOAT32))
-//                           ->addField(createField("SepalWidthCm", FLOAT32))
-//                           ->addField(createField("PetalLengthCm", FLOAT32))
-//                           ->addField(createField("PetalWidthCm", FLOAT32))
-//                           ->addField(createField("SpeciesCode", UINT64));)";
-
-        const std::string streamName = "iris";
-        SchemaPtr irisSchema = Schema::create()
-            ->addField(createField("id", UINT64))
-            ->addField(createField("SepalLengthCm", FLOAT32))
-            ->addField(createField("SepalWidthCm", FLOAT32))
-            ->addField(createField("PetalLengthCm", FLOAT32))
-            ->addField(createField("PetalWidthCm", FLOAT32))
-            ->addField(createField("SpeciesCode", UINT64))
-            ->addField(createField("CreationTime", UINT64));
-
-        sourceCatalog = std::make_shared<Catalogs::Source::SourceCatalog>(queryParsingService);
-        sourceCatalog->addLogicalSource(streamName, irisSchema);
-        auto logicalSource = sourceCatalog->getLogicalSource(streamName);
-
-        CSVSourceTypePtr csvSourceType = CSVSourceType::create();
-        csvSourceType->setGatheringInterval(0);
-        csvSourceType->setNumberOfTuplesToProducePerBuffer(0);
-        auto physicalSource = PhysicalSource::create(streamName, "test2", csvSourceType);
-
-        for (int source : sources) {
-            Catalogs::Source::SourceCatalogEntryPtr streamCatalogEntry = std::make_shared<Catalogs::Source::SourceCatalogEntry>(physicalSource, logicalSource, nodes[source]);
-            sourceCatalog->addPhysicalSource(streamName, streamCatalogEntry);
-        }
-
-        globalExecutionPlan = GlobalExecutionPlan::create();
-        typeInferencePhase = Optimizer::TypeInferencePhase::create(sourceCatalog, udfCatalog);
-    }
-
     static void assignDataModificationFactor(QueryPlanPtr queryPlan) {
         QueryPlanIterator queryPlanIterator = QueryPlanIterator(std::move(queryPlan));
 
@@ -334,39 +249,7 @@ TEST_F(QueryPlacementTest, testPlacingQueryWithBottomUpStrategy) {
     }
 }
 
-/* Test query placement with Ml heuristic strategy  */
-#ifdef TFDEF
-TEST_F(QueryPlacementTest, testPlacingQueryWithMlHeuristicStrategy) {
 
-    topologyGenerator();
-    Query query = Query::from("iris")
-                      .inferModel("../../../test_data/iris_95acc.tflite",
-                                  {Attribute("SepalLengthCm"), Attribute("SepalWidthCm"), Attribute("PetalLengthCm"), Attribute("PetalWidthCm")},
-                                  {Attribute("iris0", FLOAT32), Attribute("iris1", FLOAT32), Attribute("iris2", FLOAT32)})
-                      .filter(Attribute("iris0") < 3.0, 1)
-                      .project(Attribute("iris1"), Attribute("iris2"))
-                      .sink(PrintSinkDescriptor::create());
-
-    QueryPlanPtr queryPlan = query.getQueryPlan();
-
-    auto queryReWritePhase = Optimizer::QueryRewritePhase::create(false);
-    queryPlan = queryReWritePhase->execute(queryPlan);
-    typeInferencePhase->execute(queryPlan);
-
-    auto topologySpecificQueryRewrite =
-        Optimizer::TopologySpecificQueryRewritePhase::create(topology, sourceCatalog, Configurations::OptimizerConfiguration());
-    topologySpecificQueryRewrite->execute(queryPlan);
-    typeInferencePhase->execute(queryPlan);
-
-    auto sharedQueryPlan = SharedQueryPlan::create(queryPlan);
-    auto queryId = sharedQueryPlan->getSharedQueryId();
-    auto queryPlacementPhase =
-        Optimizer::QueryPlacementPhase::create(globalExecutionPlan, topology, typeInferencePhase, z3Context, false);
-    queryPlacementPhase->execute(NES::PlacementStrategy::MlHeuristic, sharedQueryPlan);
-    std::vector<ExecutionNodePtr> executionNodes = globalExecutionPlan->getExecutionNodesByQueryId(queryId);
-
-}
-#endif
 
 /* Test query placement with top down strategy  */
 TEST_F(QueryPlacementTest, testPlacingQueryWithTopDownStrategy) {
