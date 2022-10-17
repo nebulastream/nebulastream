fd841aae03668f35c49cf3aa5d4851aeb8805d3b
Philipp Grulich <philippgrulich@hotmail.de>
2022-10-17 12:55:35 +0200

    [#3061] add Mod expression

---
+++ nes-runtime/src/Execution/Expressions/Functions/ModExpression.cpp
@@ -0,0 +1,23 @@
+#include <Execution/Expressions/Functions/ModExpression.hpp>
+#include <Nautilus/Interface/FunctionCall.hpp>
+#include <cmath>
+
+namespace NES::Runtime::Execution::Expressions {
+
+ModExpression::ModExpression(const NES::Runtime::Execution::Expressions::ExpressionPtr& leftSubExpression,
+                             const NES::Runtime::Execution::Expressions::ExpressionPtr& rightSubExpression)
+    : leftSubExpression(leftSubExpression), rightSubExpression(rightSubExpression) {}
+
+double calculateMod(double x, double y){
+    return std::fmod(x,y);
+}
+
+
+Value<> ModExpression::execute(NES::Nautilus::Record& record) const {
+    Value leftValue = leftSubExpression->execute(record);
+    Value rightValue = rightSubExpression->execute(record);
+    Value<> result = FunctionCall<>("calculateMod", calculateMod, leftValue.as<Double>(), rightValue.as<Double>());
+    return result;
+}
+
+}// namespace NES::Runtime::Execution::Expressions
