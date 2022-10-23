2d7734c0529a717f2e4ae36b3896c87cfe2f38a0
tieumyvv <104427452+tieumyvv@users.noreply.github.com>
2022-10-23 23:19:40 +0200

    Add cbrt function/tests

---
+++ nes-runtime/tests/UnitTests/Execution/Expressions/Functions/CbrtExpressionTest.cpp
@@ -0,0 +1,100 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include <Execution/Expressions/Functions/CbrtExpression.hpp>
+#include <TestUtils/ExpressionWrapper.hpp>
+#include <Util/Logger/Logger.hpp>
+#include <gtest/gtest.h>
+#include <memory>
+
+namespace NES::Runtime::Execution::Expressions {
+
+class CbrtExpressionTest : public testing::Test {
+  public:
+    /* Will be called before any test in this class are executed. */
+    static void SetUpTestCase() {
+        NES::Logger::setupLogging("CbrtExpressionTest.log", NES::LogLevel::LOG_DEBUG);
+        std::cout << "Setup CbrtExpressionTest test class." << std::endl;
+    }
+
+    /* Will be called before a test is executed. */
+    void SetUp() override { std::cout << "Setup TraceTest test case." << std::endl; }
+
+    /* Will be called before a test is executed. */
+    void TearDown() override { std::cout << "Tear down TraceTest test case." << std::endl; }
+
+    /* Will be called after all tests in this class are finished. */
+    static void TearDownTestCase() { std::cout << "Tear down TraceTest test class." << std::endl; }
+};
+
+TEST_F(CbrtExpressionTest, evaluateCbrtExpressionInteger) {
+    auto expression = BinaryExpressionWrapper<CbrtExpression>();
+    // Int8
+    {
+        auto resultValue = expression.eval(Value<Int8>((int8_t) 17), Value<Int8>((int8_t) 4));
+        ASSERT_EQ(resultValue, (float) 1);
+        ASSERT_TRUE(resultValue->getTypeIdentifier()->isType<Double>());
+    }
+
+    // Int32
+    {
+        auto resultValue = expression.eval(Value<Int32>((int32_t) 17), Value<Int32>((int32_t) 4));
+        ASSERT_EQ(resultValue, (float) 1);
+        ASSERT_TRUE(resultValue->getTypeIdentifier()->isType<Double>());
+    }
+    // Int64
+    {
+        auto resultValue = expression.eval(Value<Int64>((int64_t) 17), Value<Int64>((int64_t) 4));
+        ASSERT_EQ(resultValue, (float) 1);
+        ASSERT_TRUE(resultValue->getTypeIdentifier()->isType<Double>());
+    }
+}
+
+TEST_F(CbrtExpressionTest, evaluateCbrtExpressionFloat) {
+    auto expression = BinaryExpressionWrapper<CbrtExpression>();
+    // Float
+    {
+        auto resultValue = expression.eval(Value<Float>((float) 4), Value<Float>((float) 4));
+        ASSERT_EQ(resultValue, (float) 0);
+        ASSERT_TRUE(resultValue->getTypeIdentifier()->isType<Double>());
+    }
+    // Float
+    {
+        auto resultValue = expression.eval(Value<Float>((float) 17), Value<Float>((float) 4));
+        ASSERT_EQ(resultValue, (float) 1);
+        ASSERT_TRUE(resultValue->getTypeIdentifier()->isType<Double>());
+    }
+    // Double
+    {
+        auto resultValue = expression.eval(Value<Double>((double) 17), Value<Double>((double) 4));
+        ASSERT_EQ(resultValue, (float) 1);
+        ASSERT_TRUE(resultValue->getTypeIdentifier()->isType<Double>());
+    }
+    // Double
+    {
+        auto resultValue = expression.eval(Value<Double>((double) 4), Value<Double>((double) 4));
+        ASSERT_EQ(resultValue, (double) 0);
+        ASSERT_TRUE(resultValue->getTypeIdentifier()->isType<Double>());
+    }
+}
+
+/**
+ * @brief If we execute the expression on a boolean it should throw an exception.
+ */
+TEST_F(CbrtExpressionTest, evaluateCbrtExpressionOnWrongType) {
+    auto expression = BinaryExpressionWrapper<CbrtExpression>();
+    ASSERT_ANY_THROW(expression.eval(Value<Boolean>(true), Value<Boolean>(false)););
+}
+
+}// namespace NES::Runtime::Execution::Expressions
