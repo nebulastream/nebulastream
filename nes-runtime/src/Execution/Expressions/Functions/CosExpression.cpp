46f15d7a5931d9192e486607ee5c18c8146dc599
khartago <rayen0799@live.fr>
2022-10-24 12:24:23 +0000

    #3039 Add cos(x) expression

--- nes-runtime/src/Execution/Expressions/Functions/CosExpression.cpp
+++ nes-runtime/src/Execution/Expressions/Functions/CosExpression.cpp
@@ -1,4 +1,4 @@
-/*Copyright LICENSE nebulastream
+/*Copyright LICENSE NebulaStream
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
