06578fc35498b11f9ae70ba9da6d1f7b7c8cd590
khartago <rayen0799@live.fr>
2022-10-24 09:11:00 +0000

    #3039 Add cos(x) expression

---
+++ nes-runtime/tests/UnitTests/Execution/Expressions/Functions/CosExpressionTest.cpp
@@ -0,0 +1,86 @@
+#include <Execution/Expressions/Functions/CosExpression.hpp>
+#include <TestUtils/ExpressionWrapper.hpp>
+#include <Util/Logger/Logger.hpp>
+#include <gtest/gtest.h>
+#include <memory>
+
+namespace NES::Runtime::Execution::Expressions {
+
+class CosExpressionTest : public testing::Test {
+  public:
+    /* Will be called before any test in this class are executed. */
+    static void SetUpTestCase() {
+        NES::Logger::setupLogging("CosExpressionTest.log", NES::LogLevel::LOG_DEBUG);
+        std::cout << "Setup CosExpressionTest test class." << std::endl;
+    }
+
+    /* Will be called before a test is executed. */
+    void SetUp() override { std::cout << "Setup TraceTest test case." << std::endl; }
+
+    /* Will be called before a test is executed. */
+    void TearDown() override { std::cout << "Tear down TraceTest test case." << std::endl; }
+
+    /* Will be called after all tests in this class are finished. */
+    static void TearDownTestCase() { std::cout << "Tear down TraceTest test class." << std::endl; }
+};
+
+TEST_F(CosExpressionTest, evaluateModExpressionInteger) {
+    auto expression = UnaryExpressionWrapper<CosExpression>();
+    // Int8
+    {
+        auto resultValue = expression.eval(Value<Int8>((int8_t) 4));
+        ASSERT_EQ(resultValue, (float) 1);
+        ASSERT_TRUE(resultValue->getTypeIdentifier()->isType<Double>());
+    }
+
+    // Int32
+    {
+        auto resultValue = expression.eval(Value<Int32>((int32_t) 4));
+        ASSERT_EQ(resultValue, (float) 1);
+        ASSERT_TRUE(resultValue->getTypeIdentifier()->isType<Double>());
+    }
+    // Int64
+    {
+        auto resultValue = expression.eval(Value<Int64>((int64_t) 4));
+        ASSERT_EQ(resultValue, (float) 1);
+        ASSERT_TRUE(resultValue->getTypeIdentifier()->isType<Double>());
+    }
+}
+
+TEST_F(CosExpressionTest, evaluateModExpressionFloat) {
+    auto expression = UnaryExpressionWrapper<CosExpression>();
+    // Float
+    {
+        auto resultValue = expression.eval(Value<Float>((float) 4));
+        ASSERT_EQ(resultValue, (float) 0);
+        ASSERT_TRUE(resultValue->getTypeIdentifier()->isType<Double>());
+    }
+    // Float
+    {
+        auto resultValue = expression.eval(Value<Float>((float) 4));
+        ASSERT_EQ(resultValue, (float) 1);
+        ASSERT_TRUE(resultValue->getTypeIdentifier()->isType<Double>());
+    }
+    // Double
+    {
+        auto resultValue = expression.eval(Value<Double>((double) 4));
+        ASSERT_EQ(resultValue, (float) 1);
+        ASSERT_TRUE(resultValue->getTypeIdentifier()->isType<Double>());
+    }
+    // Double
+    {
+        auto resultValue = expression.eval(Value<Double>((double) 4));
+        ASSERT_EQ(resultValue, (double) 0);
+        ASSERT_TRUE(resultValue->getTypeIdentifier()->isType<Double>());
+    }
+}
+
+/**
+ * @brief If we execute the expression on a boolean it should throw an exception.
+ */
+TEST_F(CosExpressionTest, evaluateModExpressionOnWrongType) {
+    auto expression = UnaryExpressionWrapper<CosExpression>();
+    ASSERT_ANY_THROW(expression.eval(Value<Boolean>(false)););
+}
+
+}// namespace NES::Runtime::Execution::Expressions
