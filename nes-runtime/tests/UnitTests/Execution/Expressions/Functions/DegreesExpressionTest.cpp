d625b69b258ef90e69ce6d21b52674d98ba482af
dbpro6 <dbpro6@cloud-48.dima.tu-berlin.de>
2022-10-25 23:20:20 +0000

    [#3056] add Degree and Pi Expression

---
+++ nes-runtime/tests/UnitTests/Execution/Expressions/Functions/DegreesExpressionTest.cpp
@@ -0,0 +1,82 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+#include <Execution/Expressions/Functions/DegreesExpression.hpp>
+#include <TestUtils/ExpressionWrapper.hpp>
+#include <Util/Logger/Logger.hpp>
+#include <gtest/gtest.h>
+#include <memory>
+#include <cmath>
+
+namespace NES::Runtime::Execution::Expressions {
+
+class DegreesExpressionTest : public testing::Test {
+  public:
+    /* Will be called before any test in this class are executed. */
+    static void SetUpTestCase() {
+        NES::Logger::setupLogging("DegreesExpressionTest.log", NES::LogLevel::LOG_DEBUG);
+        std::cout << "Setup DegreesExpressionTest test class." << std::endl;
+    }
+    /* Will be called before a test is executed. */
+    void SetUp() override { std::cout << "Setup TraceTest test case." << std::endl; }
+    /* Will be called before a test is executed. */
+    void TearDown() override { std::cout << "Tear down TraceTest test case." << std::endl; }
+    /* Will be called after all tests in this class are finished. */
+    static void TearDownTestCase() { std::cout << "Tear down TraceTest test class." << std::endl; }
+};
+
+TEST_F(DegreesExpressionTest, evaluateDegreesExpressionInteger) {
+    auto expression = UnaryExpressionWrapper<DegreesExpression>();
+    // Int8
+    {
+        auto resultValue = expression.eval(Value<Int8>((int8_t) M_PI));
+        ASSERT_EQ(resultValue, 180);
+        ASSERT_TRUE(resultValue->getTypeIdentifier()->isType<Double>());
+    }
+    // Int16
+    {
+        auto resultValue = expression.eval(Value<Int16>((int16_t) M_PI_2));
+        ASSERT_EQ(resultValue, 90);
+        ASSERT_TRUE(resultValue->getTypeIdentifier()->isType<Double>());
+    }
+    // Int32
+    {
+        auto resultValue = expression.eval(Value<Int32>((int32_t) M_PI_4));
+        ASSERT_EQ(resultValue, 45);
+        ASSERT_TRUE(resultValue->getTypeIdentifier()->isType<Double>());
+    }
+    // Int64
+    {
+        auto resultValue = expression.eval(Value<Int64>((int64_t) M_PI));
+        ASSERT_EQ(resultValue, 180);
+        ASSERT_TRUE(resultValue->getTypeIdentifier()->isType<Double>());
+    }
+}
+
+TEST_F(DegreesExpressionTest, evaluateDegreesExpressionFloat) {
+    auto expression = UnaryExpressionWrapper<DegreesExpression>();
+    // Float
+    {
+        auto resultValue = expression.eval(Value<Float>((float) M_PI));
+        //TODO: We currently receive a Double as result here #3112
+      //  ASSERT_EQ(resultValue, 180);
+        ASSERT_TRUE(resultValue->getTypeIdentifier()->isType<Double>());
+    }
+    // Double
+    {
+        auto resultValue = expression.eval(Value<Double>((double) 2*M_PI));
+        ASSERT_EQ(resultValue, 360.00);
+        ASSERT_TRUE(resultValue->getTypeIdentifier()->isType<Double>());
+    }
+}
+}
