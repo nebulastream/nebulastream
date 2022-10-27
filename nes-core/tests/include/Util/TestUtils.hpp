b87bf4ccfaa362ed6f31dea1662421c0f631f41a
Balint <rueb@campus.tu-berlin.de>
2022-10-27 12:39:16 +0200

    #3097 added config function for server type

--- nes-core/tests/include/Util/TestUtils.hpp
+++ nes-core/tests/include/Util/TestUtils.hpp
@@ -149,6 +149,10 @@ template<typename T>
 
 [[nodiscard]] std::string enableMonitoring(bool prefix = false) { return configOption(ENABLE_MONITORING_CONFIG, true, prefix); }
 
+[[nodiscard]] std::string restServerType(string sType) {
+    return "--serverType=" + SERVER_TYPE + sType;
+}
+
 // 2884: Fix configuration to disable distributed window rule
 [[nodiscard]] std::string disableDistributedWindowingOptimization() {
     return "--optimizer.performDistributedWindowOptimization=false";
