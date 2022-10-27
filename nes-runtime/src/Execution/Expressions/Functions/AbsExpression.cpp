823c6aa84f6e4ea3664bafdbaf5fa8070f67f4b1
QSchlegel <34195172+QSchlegel@users.noreply.github.com>
2022-10-27 21:47:33 +0200

    Update AbsExpression.cpp

--- nes-runtime/src/Execution/Expressions/Functions/AbsExpression.cpp
+++ nes-runtime/src/Execution/Expressions/Functions/AbsExpression.cpp
@@ -2,7 +2,9 @@
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
     You may obtain a copy of the License at
+
         https://www.apache.org/licenses/LICENSE-2.0
+
     Unless required by applicable law or agreed to in writing, software
     distributed under the License is distributed on an "AS IS" BASIS,
     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,16 +16,10 @@
 #include <Nautilus/Interface/FunctionCall.hpp>
 #include <cmath>
 
-
-
 namespace NES::Runtime::Execution::Expressions {
-
-
 
 AbsExpression::AbsExpression(const NES::Runtime::Execution::Expressions::ExpressionPtr& subExpression)
     : subExpression(subExpression) {}
-
-
 
 /**
   * @brief This method computes the absolute value of n.
@@ -33,8 +29,6 @@ AbsExpression::AbsExpression(const NES::Runtime::Execution::Expressions::Express
   */
 
 double calculateAbs(double n) { return std::abs(n); }
-
-
 
 Value<> AbsExpression::execute(NES::Nautilus::Record& record) const {
 
