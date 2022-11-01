955c96a0ed262e370004c24868e43e25d054cfb2
Sumalvico <niklas.sumalvico@gmail.com>
2022-11-01 11:03:21 +0100

    [#3034] add atan expression

---
+++ nes-runtime/include/Execution/Expressions/Functions/AtanExpression.hpp
@@ -0,0 +1,41 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#ifndef NES_NES_RUNTIME_INCLUDE_EXECUTION_EXPRESSIONS_FUNCTIONS_ATANEXPRESSION_HPP_
+#define NES_NES_RUNTIME_INCLUDE_EXECUTION_EXPRESSIONS_FUNCTIONS_ATANEXPRESSION_HPP_
+
+#include <Execution/Expressions/Expression.hpp>
+#include <Nautilus/Interface/DataTypes/Value.hpp>
+
+namespace NES::Runtime::Execution::Expressions {
+<<<<<<< HEAD
+/**
+ * @brief This expression atan the Expression
+ */
+=======
+>>>>>>> 9dba02e38e206994e098fa09cf72ef4d08b8b272
+
+class AtanExpression : public Expression {
+  public:
+    AtanExpression(const ExpressionPtr& leftSubExpression);
+    Value<> execute(Record& record) const override;
+
+  private:
+    const  ExpressionPtr leftSubExpression;
+};
+
+}// namespace NES::Runtime::Execution::Expressions
+
+
+#endif//NES_NES_RUNTIME_INCLUDE_EXECUTION_EXPRESSIONS_FUNCTIONS_ATANEXPRESSION_HPP_
