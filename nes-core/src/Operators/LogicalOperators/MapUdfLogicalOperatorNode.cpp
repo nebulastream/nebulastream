32c9ee7090f7684ce8ab40422cc8f01b7d90dd54
Viktor Rosenfeld <viktor.rosenfeld@tu-berlin.de>
2022-11-04 14:28:13 +0100

    [#3069] Add new class MapUdfLogicalOperatorNode

---
+++ nes-core/src/Operators/LogicalOperators/MapUdfLogicalOperatorNode.cpp
@@ -0,0 +1,60 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include <Operators/LogicalOperators/MapUdfLogicalOperatorNode.hpp>
+#include <Catalogs/UDF/JavaUdfDescriptor.hpp>
+#include <sstream>
+
+namespace NES {
+
+MapUdfLogicalOperatorNode::MapUdfLogicalOperatorNode(OperatorId id, const Catalogs::UDF::JavaUdfDescriptorPtr javaUdfDescriptor)
+    : OperatorNode(id), LogicalUnaryOperatorNode(id), javaUdfDescriptor(javaUdfDescriptor) {}
+
+Catalogs::UDF::JavaUdfDescriptorPtr MapUdfLogicalOperatorNode::getJavaUdfDescriptor() const {
+    return javaUdfDescriptor;
+}
+
+bool MapUdfLogicalOperatorNode::inferSchema(Optimizer::TypeInferencePhaseContext& typeInferencePhaseContext) {
+    return LogicalUnaryOperatorNode::inferSchema(typeInferencePhaseContext);
+}
+
+void MapUdfLogicalOperatorNode::inferStringSignature() {
+
+}
+
+std::string MapUdfLogicalOperatorNode::toString() const {
+    std::stringstream ss;
+    ss << "MAP_JAVA_UDF(" << id << ")";
+    return ss.str();
+}
+
+OperatorNodePtr MapUdfLogicalOperatorNode::copy() {
+    return std::make_shared<MapUdfLogicalOperatorNode>(*this);
+}
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunused-parameter"
+bool MapUdfLogicalOperatorNode::equal(const NodePtr& other) const {
+    return false;
+}
+#pragma clang diagnostic pop
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunused-parameter"
+bool MapUdfLogicalOperatorNode::isIdentical(const NodePtr& other) const {
+    return false;
+}
+#pragma clang diagnostic pop
+
+}
