961043a7a7c6b93d8240ea6c08c599ad82b8950f
Philipp Grulich <philippgrulich@hotmail.de>
2022-11-05 09:22:02 +0100

    [#3056] fix int conversion

--- nes-runtime/src/Execution/Expressions/Functions/DegreesExpression.cpp
+++ nes-runtime/src/Execution/Expressions/Functions/DegreesExpression.cpp
@@ -16,29 +16,29 @@
 #include <cmath>
 namespace NES::Runtime::Execution::Expressions {
 DegreesExpression::DegreesExpression(const NES::Runtime::Execution::Expressions::ExpressionPtr& SubExpression)
-    : SubExpression(SubExpression){}
-double calculateDegrees(double x) { return (x*180)/M_PI; }
+    : SubExpression(SubExpression) {}
+double calculateDegrees(double x) { return (x * 180.0) / M_PI; }
 
 Value<> DegreesExpression::execute(NES::Nautilus::Record& record) const {
     // Evaluate the left sub expression and retrieve the value.
     Value subValue = SubExpression->execute(record);
     //check the type and then call the function.
-    if (subValue->isType<Int8>() ) {
+    if (subValue->isType<Int8>()) {
         // call the calculateMod function with the correct type
         return FunctionCall<>("calculateDegrees", calculateDegrees, subValue.as<Int8>());
-    } else if (subValue->isType<Int16>() ) {
+    } else if (subValue->isType<Int16>()) {
         return FunctionCall<>("calculateDegrees", calculateDegrees, subValue.as<Int16>());
-    } else if (subValue->isType<Int32>() ) {
+    } else if (subValue->isType<Int32>()) {
         return FunctionCall<>("calculateDegrees", calculateDegrees, subValue.as<Int32>());
-    } else if (subValue->isType<Int64>() ) {
+    } else if (subValue->isType<Int64>()) {
         return FunctionCall<>("calculateDegrees", calculateDegrees, subValue.as<Int64>());
-    } else if (subValue->isType<Float>() ) {
+    } else if (subValue->isType<Float>()) {
         return FunctionCall<>("calculateDegrees", calculateDegrees, subValue.as<Float>());
-    } else if (subValue->isType<Double>() ) {
+    } else if (subValue->isType<Double>()) {
         return FunctionCall<>("calculateDegrees", calculateDegrees, subValue.as<Double>());
     } else {
         // If no type was applicable we throw an exception.
         NES_THROW_RUNTIME_ERROR("This expression is only defined on numeric input arguments that are either Integer or Float.");
     }
 }
-}
+}// namespace NES::Runtime::Execution::Expressions
