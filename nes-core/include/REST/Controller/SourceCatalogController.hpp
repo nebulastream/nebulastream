e69761868d8f0b3002a5c737c2487ca83bd889ee
eleicha <laura.mons@web.de>
2022-11-08 18:38:17 +0100

    #3141 added cors to SourceCatalogController.hpp

--- nes-core/include/REST/Controller/SourceCatalogController.hpp
+++ nes-core/include/REST/Controller/SourceCatalogController.hpp
@@ -126,6 +126,7 @@ class SourceCatalogController : public oatpp::web::server::api::ApiController {
         }
     }
 
+    ADD_CORS(addLogicalSource)
     ENDPOINT("POST", "/addLogicalSource", addLogicalSource, BODY_STRING(String, request)) {
 
         NES_DEBUG("SourceCatalogController: addLogicalSource: REST received request to add new Logical Source.");
@@ -167,6 +168,7 @@ class SourceCatalogController : public oatpp::web::server::api::ApiController {
         }
     }
 
+    ADD_CORS(addLogicalSourceEx)
     ENDPOINT("POST", "/addLogicalSource-ex", addLogicalSourceEx, BODY_STRING(String, request)) {
 
         NES_DEBUG("SourceCatalogController: addLogicalSource: REST received request to add new Logical Source.");
@@ -211,6 +213,7 @@ class SourceCatalogController : public oatpp::web::server::api::ApiController {
         }
     }
 
+    ADD_CORS(updateLogicalSource)
     ENDPOINT("POST", "/updateLogicalSource", updateLogicalSource, BODY_STRING(String, request)) {
 
         NES_DEBUG("SourceCatalogController: updateLogicalSource: REST received request to update the given Logical Source.");
@@ -253,6 +256,7 @@ class SourceCatalogController : public oatpp::web::server::api::ApiController {
         }
     }
 
+    ADD_CORS(updateLogicalSourceEx)
     ENDPOINT("POST", "/updateLogicalSource-ex", updateLogicalSourceEx, BODY_STRING(String, request)) {
 
         NES_DEBUG("SourceCatalogController: updateLogicalSource: REST received request to update the given Logical Source.");
@@ -300,6 +304,7 @@ class SourceCatalogController : public oatpp::web::server::api::ApiController {
         }
     }
 
+    ADD_CORS(deleteLogicalSource)
     ENDPOINT("DELETE", "/deleteLogicalSource", deleteLogicalSource, QUERY(String, logicalSourceName, "logicalSourceName")) {
         NES_DEBUG("SourceCatalogController: deleteLogicalSource: REST received request to delete the given Logical Source.");
         try {
