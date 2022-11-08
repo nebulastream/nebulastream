d7f5df96018d42bd16f6c301c8d5fa8c4bebcda8
dbpro15 <paul.pohlitz@gmx.de>
2022-11-08 14:49:43 +0000

    [#3047] fixes unit tests

--- nes-runtime/tests/UnitTests/Execution/Expressions/Functions/Log10ExpressionTest.cpp
+++ nes-runtime/tests/UnitTests/Execution/Expressions/Functions/Log10ExpressionTest.cpp
@@ -44,26 +44,26 @@ namespace NES::Runtime::Execution::Expressions {
         auto expression = UnaryExpressionWrapper<Log10Expression>();
         // Int8
         {
-            auto resultValue = expression.eval(Value<Int8>((int8_t) 90));
-            ASSERT_EQ(resultValue, 1.95424250944);
+            auto resultValue = expression.eval(Value<Int8>((int8_t) 100));
+            ASSERT_EQ(resultValue, (double) 2.0);
             ASSERT_TRUE(resultValue->getTypeIdentifier()->isType<Double>());
         }
         // Int16
         {
-            auto resultValue = expression.eval(Value<Int16>((int16_t) 90));
-            ASSERT_EQ(resultValue, 1.95424250944);
+            auto resultValue = expression.eval(Value<Int16>((int16_t) 100));
+            ASSERT_EQ(resultValue, (double) 2.0);
             ASSERT_TRUE(resultValue->getTypeIdentifier()->isType<Double>());
         }
         // Int32
         {
-            auto resultValue = expression.eval(Value<Int32>((int32_t) 90));
-            ASSERT_EQ(resultValue, 1.95424250944);
+            auto resultValue = expression.eval(Value<Int32>((int32_t) 100));
+            ASSERT_EQ(resultValue, (double) 2.0);
             ASSERT_TRUE(resultValue->getTypeIdentifier()->isType<Double>());
         }
         // Int64
         {
-            auto resultValue = expression.eval(Value<Int64>((int64_t) 90));
-            ASSERT_EQ(resultValue, 1.95424250944);
+            auto resultValue = expression.eval(Value<Int64>((int64_t) 100));
+            ASSERT_EQ(resultValue, (double) 2.0);
             ASSERT_TRUE(resultValue->getTypeIdentifier()->isType<Double>());
         }
     }
@@ -72,14 +72,14 @@ namespace NES::Runtime::Execution::Expressions {
         auto expression = UnaryExpressionWrapper<Log10Expression>();
         // Float
         {
-            auto resultValue = expression.eval(Value<Float>((float) 90));
-            ASSERT_EQ(resultValue, 1.95424250944);
+            auto resultValue = expression.eval(Value<Float>((float) 100));
+            ASSERT_EQ(resultValue, (double) 2.0);
             ASSERT_TRUE(resultValue->getTypeIdentifier()->isType<Double>());
         }
         // Double
         {
-            auto resultValue = expression.eval(Value<Double>((double) 90));
-            ASSERT_EQ(resultValue, 1.95424250944);
+            auto resultValue = expression.eval(Value<Double>((double) 100));
+            ASSERT_EQ(resultValue, (double) 2.0);
             ASSERT_TRUE(resultValue->getTypeIdentifier()->isType<Double>());
         }
     }
