6674e722a97a3077db0161dddb6863d66db93dc0
Philipp Grulich <philippgrulich@hotmail.de>
2022-11-11 16:52:10 +0100

    [#3089] first step towards modular runtime

--- nes-core/include/QueryCompiler/Phases/NautilusPhaseFactory.hpp
+++ nes-core/include/QueryCompiler/Phases/NautilusPhaseFactory.hpp
@@ -5,22 +5,27 @@
 #ifndef NES_NES_RUNTIME_INCLUDE_QUERYCOMPILER_NAUTILUSPHASEFACTORY_HPP_
 #define NES_NES_RUNTIME_INCLUDE_QUERYCOMPILER_NAUTILUSPHASEFACTORY_HPP_
 #include <QueryCompiler/Phases/PhaseFactory.hpp>
-namespace NES::Runtime::QueryCompiler::Phases {
+
+namespace NES::QueryCompilation::Phases {
 
 /**
  * @brief The default phase factory creates a default set of phases.
  */
-class NautilusPhaseFactory : public QueryCompilation::Phases::PhaseFactory {
+class NautilusPhaseFactory : public PhaseFactory {
   public:
     virtual ~NautilusPhaseFactory() = default;
     static QueryCompilation::Phases::PhaseFactoryPtr create();
     QueryCompilation::LowerLogicalToPhysicalOperatorsPtr
+
     createLowerLogicalQueryPlanPhase(QueryCompilation::QueryCompilerOptionsPtr options) override;
     QueryCompilation::PipeliningPhasePtr createPipeliningPhase(QueryCompilation::QueryCompilerOptionsPtr options) override;
+
     QueryCompilation::AddScanAndEmitPhasePtr
     createAddScanAndEmitPhase(QueryCompilation::QueryCompilerOptionsPtr options) override;
+
     QueryCompilation::LowerPhysicalToGeneratableOperatorsPtr
     createLowerPhysicalToGeneratableOperatorsPhase(QueryCompilation::QueryCompilerOptionsPtr options) override;
+
     QueryCompilation::CodeGenerationPhasePtr createCodeGenerationPhase(QueryCompilation::QueryCompilerOptionsPtr options,
                                                                        Compiler::JITCompilerPtr jitCompiler) override;
     QueryCompilation::LowerToExecutableQueryPlanPhasePtr
@@ -29,8 +34,9 @@ class NautilusPhaseFactory : public QueryCompilation::Phases::PhaseFactory {
     createBufferOptimizationPhase(QueryCompilation::QueryCompilerOptionsPtr options) override;
     QueryCompilation::PredicationOptimizationPhasePtr
     createPredicationOptimizationPhase(QueryCompilation::QueryCompilerOptionsPtr options) override;
+
 };
 
-}// namespace NES::Runtime::QueryCompiler::Phases
+}// namespace NES::QueryCompilation::Phases
 
 #endif//NES_NES_RUNTIME_INCLUDE_QUERYCOMPILER_NAUTILUSPHASEFACTORY_HPP_
