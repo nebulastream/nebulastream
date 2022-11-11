b06d992a7cc0a20f7b490d30b1e93d65d7b1d7c0
zeuchste <steffen.zeuch@dfki.de>
2022-11-11 18:16:52 +0100

    [#2942] add missing files

---
+++ nes-benchmark/src/DataGeneration/YSBDataGenerator.cpp
@@ -0,0 +1,65 @@
+#include <API/Schema.hpp>
+#include <DataGeneration/YSBDataGenerator.h>
+#include <Runtime/MemoryLayout/DynamicTupleBuffer.hpp>
+#include <Runtime/MemoryLayout/MemoryLayout.hpp>
+#include <Runtime/TupleBuffer.hpp>
+#include <fstream>
+#include <utility>
+
+namespace NES::Benchmark::DataGeneration {
+
+YSBDataGenerator::YSBDataGenerator(Runtime::BufferManagerPtr bufferManager)
+    : DataGenerator(std::move(bufferManager)) {}
+
+std::string YSBDataGenerator::getName() { return "YSB"; }
+
+std::vector<Runtime::TupleBuffer> YSBDataGenerator::createData(uint64_t numberOfBuffers, uint64_t bufferSize) {
+    std::cout << "YSB Mode source mode" << std::endl;
+
+    std::vector<Runtime::TupleBuffer> createdBuffers;
+    createdBuffers.reserve(numberOfBuffers);
+
+    auto memoryLayout = getMemoryLayout(bufferSize);
+    for (uint64_t currentBuffer = 0; currentBuffer < numberOfBuffers; currentBuffer++) {
+        auto buffer = allocateBuffer();
+        auto dynamicBuffer = Runtime::MemoryLayouts::DynamicTupleBuffer(memoryLayout, buffer);
+        for (uint64_t currentRecord = 0; currentRecord < dynamicBuffer.getCapacity(); currentRecord++) {
+            auto ts = std::chrono::duration_cast<std::chrono::milliseconds>(
+                          std::chrono::high_resolution_clock::now().time_since_epoch())
+                          .count();
+            auto campaign_id = rand() % 1000;
+            auto event_type = currentRecord % 3;
+            dynamicBuffer[currentRecord]["user_id"].write<uint64_t>(1);
+            dynamicBuffer[currentRecord]["page_id"].write<uint64_t>(0);
+            dynamicBuffer[currentRecord]["campaign_id"].write<uint64_t>(campaign_id);
+            dynamicBuffer[currentRecord]["ad_type"].write<uint64_t>(0);
+            dynamicBuffer[currentRecord]["event_type"].write<uint64_t>(event_type);
+            dynamicBuffer[currentRecord]["current_ms"].write<uint64_t>(ts);
+            dynamicBuffer[currentRecord]["ip"].write<uint64_t>(0x01020304);
+            dynamicBuffer[currentRecord]["d1"].write<uint64_t>(1);
+            dynamicBuffer[currentRecord]["d2"].write<uint64_t>(1);
+            dynamicBuffer[currentRecord]["d3"].write<uint32_t>(1);
+            dynamicBuffer[currentRecord]["d4"].write<uint16_t>(1);
+        }
+        dynamicBuffer.setNumberOfTuples(dynamicBuffer.getCapacity());
+        createdBuffers.emplace_back(buffer);
+    }
+    NES_INFO("Created all buffers!");
+    return createdBuffers;
+}
+SchemaPtr YSBDataGenerator::getSchema() {
+    return Schema::create()
+        ->addField("user_id", UINT64)
+        ->addField("page_id", UINT64)
+        ->addField("campaign_id", UINT64)
+        ->addField("ad_type", UINT64)
+        ->addField("event_type", UINT64)
+        ->addField("current_ms", UINT64)
+        ->addField("ip", UINT64)
+        ->addField("d1", UINT64)
+        ->addField("d2", UINT64)
+        ->addField("d3", UINT32)
+        ->addField("d4", UINT16);
+}
+
+}// namespace NES::Benchmark::DataGeneration
