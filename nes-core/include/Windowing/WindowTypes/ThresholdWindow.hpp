0e3af12cd004ef5e2300ce95914a96f8485b561c
arianeziehn <arianeziehn@googlemail.com>
2022-11-14 15:22:20 +0100

    [#3135] add minium count support to threshold window

--- nes-core/include/Windowing/WindowTypes/ThresholdWindow.hpp
+++ nes-core/include/Windowing/WindowTypes/ThresholdWindow.hpp
@@ -15,6 +15,7 @@
 #ifndef NES_CORE_INCLUDE_WINDOWING_WINDOWTYPES_THRESHOLDWINDOW_HPP_
 #define NES_CORE_INCLUDE_WINDOWING_WINDOWTYPES_THRESHOLDWINDOW_HPP_
 
+#include <Windowing/Runtime/ThresholdWindowState.hpp>
 #include <Windowing/WindowMeasures/TimeMeasure.hpp>
 #include <Windowing/WindowTypes/ContentBasedWindowType.hpp>
 
@@ -22,7 +23,9 @@ namespace NES::Windowing {
 
 class ThresholdWindow : public ContentBasedWindowType {
   public:
-    static WindowTypePtr of(ExpressionNodePtr predicate);//TODO #3135: allow specification of minimum-count
+    static WindowTypePtr of(ExpressionNodePtr predicate);
+
+    static WindowTypePtr of(ExpressionNodePtr predicate, uint64_t miniumCount);
 
     /**
     * @brief Returns true, because this a threshold window
@@ -36,10 +39,21 @@ class ThresholdWindow : public ContentBasedWindowType {
 
     [[nodiscard]] const ExpressionNodePtr& getPredicate() const;
 
+    uint64_t getMiniumCount();
+
+    /**
+     * @brief Triggers windows if they marked as closed
+     * @param windows vector of windows
+     */
+    void triggerWindows(std::vector<ThresholdWindowState>& windows);
+
   private:
     explicit ThresholdWindow(ExpressionNodePtr predicate);
+    ThresholdWindow(ExpressionNodePtr predicate, uint64_t minCount);
 
     ExpressionNodePtr predicate;
+    uint64_t miniumCount = 0;
+    bool isClosed = false;
 };
 
 }// namespace NES::Windowing
