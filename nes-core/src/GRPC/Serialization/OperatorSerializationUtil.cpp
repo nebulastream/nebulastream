eacbe836519b5c82133749c05d8ee0463d155735
arianeziehn <arianeziehn@googlemail.com>
2022-11-16 14:38:45 +0100

    [#3135] fix typo in minimumCount

--- nes-core/src/GRPC/Serialization/OperatorSerializationUtil.cpp
+++ nes-core/src/GRPC/Serialization/OperatorSerializationUtil.cpp
@@ -525,7 +525,7 @@ OperatorSerializationUtil::serializeWindowOperator(const WindowOperatorNodePtr& 
         auto thresholdWindowDetails = SerializableOperator_WindowDetails_ThresholdWindow();
         ExpressionSerializationUtil::serializeExpression(thresholdWindow->getPredicate(),
                                                          thresholdWindowDetails.mutable_predicate());
-        thresholdWindowDetails.set_miniumcount(thresholdWindow->getMiniumCount());
+        thresholdWindowDetails.set_miniumcount(thresholdWindow->getMinimumCount());
         windowDetails.mutable_windowtype()->PackFrom(thresholdWindowDetails);
     }
 
