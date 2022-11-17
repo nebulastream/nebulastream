ae92da4a0ca504c8d1ba925959a9179d011cac4a
Nils Schubert <n.schubert@campus.tu-berlin.de>
2022-11-17 09:54:36 +0100

    2501 checkpoint

--- nes-runtime/include/Execution/Operators/Streaming/Aggregations/Join/BloomFilter.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by nils on 17.11.22.
-//
-
-#include "BloomFilter.hpp"
---
+++ nes-runtime/src/Execution/Operators/Streaming/Aggregations/Join/BloomFilter.cpp
@@ -0,0 +1,28 @@
+/*
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+
+#include <Execution/Operators/Streaming/Aggregations/Join/BloomFilter.hpp>
+#include <Util/Logger/Logger.hpp>
+#include <cmath>
+
+namespace NES::Runtime::Execution::Operators {
+
+BloomFilter::BloomFilter(uint64_t entries, double falsePositiveRate) {
+    NES_ASSERT2_FMT(falsePositiveRate < 1.0 && falsePositiveRate > 0.0, "BloomFilter false positive rate has to be in the range of (0.0, 1.0)!");
+
+    double numerator = std::log(falsePositiveRate);
+    double denominator = std::pow(std::log(2), 2); 0.480453013918201;// ln(2)^2
+}
+} // namespace NES::Runtime::Execution::Operators
