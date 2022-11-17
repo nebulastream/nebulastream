74ff04ad594d86fe1453df9c560c7945aa47d65a
Nils Schubert <n.schubert@campus.tu-berlin.de>
2022-11-17 08:39:17 +0100

    2501 checkpoint

---
+++ nes-runtime/include/Execution/Operators/Streaming/Aggregations/Join/BloomFilter.hpp
@@ -0,0 +1,22 @@
+
+#ifndef NES_BLOOMFILTER_HPP
+#define NES_BLOOMFILTER_HPP
+
+namespace NES::Runtime::Execution::Operators {
+
+
+
+class alignas(64) BloomFilter {
+
+
+
+
+  private:
+    uint32_t noBits;
+    uint16_t noHashes;
+    uint8_t
+
+};
+
+}
+#endif//NES_BLOOMFILTER_HPP
