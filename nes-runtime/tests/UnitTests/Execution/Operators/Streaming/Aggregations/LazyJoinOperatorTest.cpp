1f07967dc71bbf3d430004b3870fa1483fdcc403
Nils Schubert <n.schubert.1@tu-berlin.de>
2022-11-25 13:54:32 +0100

    2501 started with LazyJoinTest

--- nes-runtime/tests/UnitTests/Execution/Operators/Streaming/Aggregations/LazyJoinOperatorTest.cpp
+++ nes-runtime/tests/UnitTests/Execution/Operators/Streaming/Aggregations/LazyJoinOperatorTest.cpp
@@ -12,3 +12,10 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+namespace NES::Runtime::Execution::Operators {
+
+class LazyJoinOperatorTest
+
+
+
+} // namespace NES::Runtime::Execution::Operators
