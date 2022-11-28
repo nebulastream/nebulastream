25fdaf17225beedc37aa633e0f2bb9a5d84a7faa
arianeziehn <arianeziehn@googlemail.com>
2022-11-28 10:49:58 +0100

    [#3262] add threading to threshold window

--- nes-runtime/src/Execution/Operators/ThresholdWindow/ThresholdWindow.cpp
+++ nes-runtime/src/Execution/Operators/ThresholdWindow/ThresholdWindow.cpp
@@ -17,12 +17,14 @@
 #include <Nautilus/Interface/DataTypes/Integer/Int.hpp>
 #include <Nautilus/Interface/FunctionCall.hpp>
 #include <Nautilus/Interface/Record.hpp>
+#include <mutex>
 
 namespace NES::Runtime::Execution::Operators {
 
 extern "C" void addToSumAggregate(void* state, int64_t valueToAdd) {
     auto handler = (ThresholdWindowOperatorHandler*) state;
     handler->sum = handler->sum + valueToAdd;
+    handler->recordCount++;
 }
 
 extern "C" void setIsWindowOpen(void* state, bool isWindowOpen) {
@@ -35,6 +37,11 @@ extern "C" bool getIsWindowOpen(void* state) {
     return handler->isWindowOpen;
 }
 
+extern "C" bool getMinCountFulfilled(void* state) {
+    auto handler = (ThresholdWindowOperatorHandler*) state;
+    return handler->recordCount >= handler->minCount;
+}
+
 extern "C" void setSumAggregate(void* state, int64_t valueToSet) {
     auto handler = (ThresholdWindowOperatorHandler*) state;
     handler->sum = valueToSet;
@@ -45,17 +52,24 @@ extern "C" int64_t getSumAggregate(void* state) {
     return handler->sum;
 }
 
+extern "C" void lockWindowHandler(void* state) {
+    auto handler = (ThresholdWindowOperatorHandler*) state;
+    std::lock_guard<std::mutex> lock(handler->mutex);
+}
+
 void NES::Runtime::Execution::Operators::ThresholdWindow::execute(ExecutionContext& ctx, Record& record) const {
     // Evaluate the threshold condition
     auto val = predicateExpression->execute(record);
     auto handler = ctx.getGlobalOperatorHandler(operatorHandlerIndex);
+    FunctionCall("lockWindowHandler", lockWindowHandler, handler);
     if (val) {
         auto aggregatedValue = aggregatedFieldAccessExpression->execute(record);
         FunctionCall("addToSumAggregate", addToSumAggregate, handler, aggregatedValue.as<Int64>());
         FunctionCall("setIsWindowOpen", setIsWindowOpen, handler, Value<Boolean>(true));
     } else {
         auto isWindowOpen = FunctionCall("getIsWindowOpen", getIsWindowOpen, handler);
-        if (isWindowOpen) {
+        auto minCountFulfilled = FunctionCall("getMinCountFulfilled", getMinCountFulfilled, handler);
+        if (isWindowOpen && minCountFulfilled) {
             auto sumAggregation = FunctionCall("getSumAggregate", getSumAggregate, handler);
             auto aggregationResult = Record({{aggregationResultFieldIdentifier, sumAggregation}});
             FunctionCall("setSumAggregate", setSumAggregate, handler, Value<Int64>((int64_t) 0));
