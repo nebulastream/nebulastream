a158686508c5e84ea5f2bbdc86aad61eac1b686e
Ankit Chaudhary <ankit.chaudhary@tu-berlin.de>
2022-11-30 18:52:08 +0100

    #3075: added constraint classes and configuration class

---
+++ nes-common/include/Configurations/ConfigurationKeys.hpp
@@ -0,0 +1,28 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#ifndef NES_CONFIGURATIONKEYS_HPP
+#define NES_CONFIGURATIONKEYS_HPP
+
+#include <string>
+
+namespace NES {
+
+const std::string TENSER_FLOW_SUPPORT = "TENSER_FLOW";
+const std::string JAVA_UDF_SUPPORT = "JAVA_UDF";
+const std::string MOBILITY_SUPPORT = "MOBILITY";
+
+}// namespace NES
+
+#endif//NES_CONFIGURATIONKEYS_HPP
