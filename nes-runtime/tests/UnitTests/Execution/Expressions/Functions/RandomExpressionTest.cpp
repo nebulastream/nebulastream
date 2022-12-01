7725a92a5498a8f3d56d2cf98399f883b50c205d
dbpro12 <h.muratev@web.de>
2022-12-01 12:38:13 +0000

    3050_Random_V2.1 Ich habe die Lizensen und CMake Datei überarbeitet bzw. hinzugefügt.

--- nes-runtime/tests/UnitTests/Execution/Expressions/Functions/RandomExpressionTest.cpp
+++ nes-runtime/tests/UnitTests/Execution/Expressions/Functions/RandomExpressionTest.cpp
@@ -1,5 +1,4 @@
 /*
-<<<<<<< HEAD
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
     You may obtain a copy of the License at
@@ -20,70 +19,73 @@
 
 namespace NES::Runtime::Execution::Expressions {
 
-class RandomExpressionTest : public testing::Test {
-  public:
-    /* Will be called before any test in this class are executed. */
-    static void SetUpTestCase() {
-        NES::Logger::setupLogging("RandomExpressionTest.log", NES::LogLevel::LOG_DEBUG);
-        std::cout << "Setup RandomExpressionTest test class." << std::endl;
-    }
+        class RandomExpressionTest : public testing::Test {
+          public:
+            /* Will be called before any test in this class are executed. */
+            static void SetUpTestCase() {
+                NES::Logger::setupLogging("RandomExpressionTest.log", NES::LogLevel::LOG_DEBUG);
+                std::cout << "Setup RandomExpressionTest test class." << std::endl;
+            }
 
-    /* Will be called before a test is executed. */
-    void SetUp() override { std::cout << "Setup TraceTest test case." << std::endl; }
+            /* Will be called before a test is executed. */
+            void SetUp() override { std::cout << "Setup TraceTest test case." << std::endl; }
 
-    /* Will be called before a test is executed. */
-    void TearDown() override { std::cout << "Tear down TraceTest test case." << std::endl; }
+            /* Will be called before a test is executed. */
+            void TearDown() override { std::cout << "Tear down TraceTest test case." << std::endl; }
 
-    /* Will be called after all tests in this class are finished. */
-    static void TearDownTestCase() { std::cout << "Tear down TraceTest test class." << std::endl; }
-};
-TEST_F(RandomExpressionTest, evaluatePiExpressionInteger) {
-    auto expression = UnaryExpressionWrapper<RandomExpression>();
-    // Int8
-    {
-        auto resultValue = expression.eval(Value<Int8>((int8_t) 1));
-        ASSERT_EQ(resultValue, resultValue > 0 and resultValue < 1);
-        ASSERT_TRUE(resultValue->getTypeIdentifier()->isType<Double>());
-    }
-    // Int16
-    {
-        auto resultValue = expression.eval(Value<Int16>((int16_t) 2));
-        ASSERT_EQ(resultValue, resultValue > 0 and resultValue < 1);
-        ASSERT_TRUE(resultValue->getTypeIdentifier()->isType<Double>());
-    }
-    // Int32
-    {
-        auto resultValue = expression.eval(Value<Int32>((int32_t) 3));
-        ASSERT_EQ(resultValue, resultValue > 0 and resultValue < 1);
-        ASSERT_TRUE(resultValue->getTypeIdentifier()->isType<Double>());
-    }
-    // Int64
-    {
-        auto resultValue = expression.eval(Value<Int64>((int64_t) 4));
-        ASSERT_EQ(resultValue, resultValue > 0 and resultValue < 1);
-        ASSERT_TRUE(resultValue->getTypeIdentifier()->isType<Double>());
-    }
-}
-TEST_F(RandomExpressionTest, evaluatePiExpressionFloat) {
-    auto expression = UnaryExpressionWrapper<RandomExpression>();
-    // Float
-    {
-        auto resultValue = expression.eval(Value<Float>((float) 5));
-        ASSERT_EQ(resultValue, resultValue > 0 and resultValue < 1);
-        ASSERT_TRUE(resultValue->getTypeIdentifier()->isType<Double>());
-    }
-    // Double
-    {
-        auto resultValue = expression.eval(Value<Double>((double) 6));
-        ASSERT_EQ(resultValue, resultValue > 0 and resultValue < 1);
-        ASSERT_TRUE(resultValue->getTypeIdentifier()->isType<Double>());
-    }
-}
-/**
+            /* Will be called after all tests in this class are finished. */
+            static void TearDownTestCase() { std::cout << "Tear down TraceTest test class." << std::endl; }
+        };
+
+        TEST_F(RandomExpressionTest, evaluatePiExpressionInteger) {
+            auto expression = UnaryExpressionWrapper<RandomExpression>();
+            // Int8
+            {
+                auto resultValue = expression.eval(Value<Int8>((int8_t) 1));
+                ASSERT_EQ(resultValue, resultValue > 0 and resultValue < 1);
+                ASSERT_TRUE(resultValue->getTypeIdentifier()->isType<Double>());
+            }
+            // Int16
+            {
+                auto resultValue = expression.eval(Value<Int16>((int16_t) 2));
+                ASSERT_EQ(resultValue, resultValue > 0 and resultValue < 1);
+                ASSERT_TRUE(resultValue->getTypeIdentifier()->isType<Double>());
+            }
+            // Int32
+            {
+                auto resultValue = expression.eval(Value<Int32>((int32_t) 3));
+                ASSERT_EQ(resultValue, resultValue > 0 and resultValue < 1);
+                ASSERT_TRUE(resultValue->getTypeIdentifier()->isType<Double>());
+            }
+            // Int64
+            {
+                auto resultValue = expression.eval(Value<Int64>((int64_t) 4));
+                ASSERT_EQ(resultValue, resultValue > 0 and resultValue < 1);
+                ASSERT_TRUE(resultValue->getTypeIdentifier()->isType<Double>());
+            }
+        }
+
+        TEST_F(RandomExpressionTest, evaluatePiExpressionFloat) {
+            auto expression = UnaryExpressionWrapper<RandomExpression>();
+            // Float
+            {
+                auto resultValue = expression.eval(Value<Float>((float) 5));
+                ASSERT_EQ(resultValue, resultValue > 0 and resultValue < 1);
+                ASSERT_TRUE(resultValue->getTypeIdentifier()->isType<Double>());
+            }
+            // Double
+            {
+                auto resultValue = expression.eval(Value<Double>((double) 6));
+                ASSERT_EQ(resultValue, resultValue > 0 and resultValue < 1);
+                ASSERT_TRUE(resultValue->getTypeIdentifier()->isType<Double>());
+            }
+        }
+        /**
         * @brief If we execute the expression on a boolean it should throw an exception.
         */
-TEST_F(RandomExpressionTest, evaluateSinExpressionOnWrongType) {
-    auto expression = UnaryExpressionWrapper<RandomExpression>();
-    ASSERT_ANY_THROW(expression.eval(Value<Boolean>(true)););
-}
-}
+        TEST_F(RandomExpressionTest, evaluateSinExpressionOnWrongType) {
+            auto expression = UnaryExpressionWrapper<RandomExpression>();
+            ASSERT_ANY_THROW(expression.eval(Value<Boolean>(true)););
+        }
+        }
+
