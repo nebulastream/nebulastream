b74ffcd49dbdadfbd20190e03d66e21cf2fef35f
alepping <aljoschalepping@gmail.com>
2022-12-05 18:49:52 +0100

    #3169 : We can now detect increasing and decreasing loops. Also counted loop detection now covers more counted loop shapes.

--- nes-runtime/include/Nautilus/IR/Operations/Loop/LoopInfo.hpp
+++ nes-runtime/include/Nautilus/IR/Operations/Loop/LoopInfo.hpp
@@ -29,13 +29,14 @@ class DefaultLoopInfo : public LoopInfo {};
 
 class CountedLoopInfo : public LoopInfo {
   public:
-    OperationWPtr lowerBound;
-    OperationWPtr upperBound;
-    OperationWPtr stepSize;
+    uint64_t lowerBound;
+    uint64_t upperBound;
+    uint64_t stepSize;
     std::vector<OperationPtr> loopInitialIteratorArguments;
     std::vector<OperationPtr> loopBodyIteratorArguments;
-    OperationPtr loopBodyInductionVariable;
+    // OperationPtr loopBodyInductionVariable;
     BasicBlockPtr loopBodyBlock;
+    BasicBlockPtr loopFalseBlock;
     BasicBlockPtr loopEndBlock;
     bool isCountedLoop() override { return true; }
 };
