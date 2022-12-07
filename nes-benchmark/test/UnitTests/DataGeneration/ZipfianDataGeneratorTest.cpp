8cd52611d31e89ff8b28653923e175a428dba3c4
Niklas Tantow <n.tantow@campus.tu-berlin.de>
2022-12-07 23:37:04 +0100

    #3281 started with ZipfianDataGeneratorTest

---
+++ nes-benchmark/test/UnitTests/DataGeneration/ZipfianDataGeneratorTest.cpp
@@ -0,0 +1,81 @@
+/*
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#include <DataGeneration/ZipfianDataGenerator.hpp>
+#include <Util/Logger/Logger.hpp>
+#include <gtest/gtest.h>
+#include <API/Schema.hpp>
+
+namespace NES::Benchmark::DataGeneration {
+    class ZipfianDataGeneratorTest : public testing::Test {
+      public:
+        /* Will be called before any test in this class are executed. */
+        static void SetUpTestCase() {
+            NES::Logger::setupLogging("ZipfianDataGeneratorTest.log", NES::LogLevel::LOG_DEBUG);
+            std::cout << "Setup ZipfianDataGeneratorTest test class." << std::endl;
+        }
+
+        /* Will be called before a test is executed. */
+        void SetUp() override { std::cout << "Setup ZipfianDataGeneratorTest test case." << std::endl; }
+
+        /* Will be called before a test is executed. */
+        void TearDown() override { std::cout << "Tear down ZipfianDataGeneratorTest test case." << std::endl; }
+
+        /* Will be called after all tests in this class are finished. */
+        static void TearDownTestCase() { std::cout << "Tear down ZipfianDataGeneratorTest test class." << std::endl; }
+    };
+
+    TEST_F(ZipfianDataGeneratorTest, getSchemaTest) {
+        auto zipfianDataGenerator = std::make_shared<ZipfianDataGenerator>(/* alpha */ 0, /* minValue */ 0, /* maxValue */ 1000);
+        auto schemaDefault = zipfianDataGenerator->getSchema();
+        auto expectedSchema = NES::Schema::create()
+                                  ->addField(createField("id", NES::UINT64))
+                                  ->addField(createField("value", NES::UINT64))
+                                  ->addField(createField("payload", NES::UINT64))
+                                  ->addField(createField("timestamp", NES::UINT64));
+
+        // TODO compare schema correctly
+        ASSERT_EQ(schemaDefault, expectedSchema);
+    }
+
+    TEST_F(ZipfianDataGeneratorTest, getNameTest) {
+        auto zipfianDataGenerator = std::make_shared<ZipfianDataGenerator>(/* alpha */ 0, /* minValue */ 0, /* maxValue */ 1000);
+        auto nameDefault = zipfianDataGenerator->getName();
+        auto expectedName = "Zipfian";
+
+        ASSERT_EQ(nameDefault, expectedName);
+    }
+
+    TEST_F(ZipfianDataGeneratorTest, toStringTest) {
+        auto zipfianDataGenerator = std::make_shared<ZipfianDataGenerator>(/* alpha */ 0, /* minValue */ 0, /* maxValue */ 1000);
+        auto stringDefault = zipfianDataGenerator->toString();
+
+        // TODO expected string
+        auto expectedString = ;
+
+        ASSERT_EQ(stringDefault, expectedString);
+    }
+
+    TEST_F(ZipfianDataGeneratorTest, createDataTest) {
+        auto zipfianDataGenerator = std::make_shared<ZipfianDataGenerator>(/* alpha */ 0, /* minValue */ 0, /* maxValue */ 1000);
+        // TODO data default
+        auto dataDefault = zipfianDataGenerator->createData(100, sizeof(char));
+
+        // TODO expected Data
+        // how to expect random data?
+        auto expectedData = ;
+
+        ASSERT_EQ(dataDefault, expectedData);
+    }
+}//namespace NES::Benchmark::DataGeneration
