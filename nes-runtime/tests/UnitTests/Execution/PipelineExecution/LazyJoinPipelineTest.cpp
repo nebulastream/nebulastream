a8dd0c249cd74568d787c8733e84c219aba6e5cb
Nils Schubert <n.schubert.1@tu-berlin.de>
2022-12-09 17:17:31 +0100

    #2501 join sink does not work if we have multiple tuples per buffer

--- nes-runtime/tests/UnitTests/Execution/PipelineExecution/LazyJoinPipelineTest.cpp
+++ nes-runtime/tests/UnitTests/Execution/PipelineExecution/LazyJoinPipelineTest.cpp
@@ -11,8 +11,85 @@
     See the License for the specific language governing permissions and
     limitations under the License.
 */
+#include <API/AttributeField.hpp>
+#include <API/Schema.hpp>
+#include <Exceptions/ErrorListener.hpp>
+#include <Execution/Pipelines/ExecutablePipelineProvider.hpp>
+#include <Runtime/BufferManager.hpp>
+#include <Runtime/WorkerContext.hpp>
+#include <TestUtils/AbstractPipelineExecutionTest.hpp>
+#include <Util/Logger/Logger.hpp>
+#include <cstring>
+#include <gtest/gtest.h>
+#include <iostream>
+#include <string>
 
 namespace NES::Runtime::Execution {
-    
+
+
+class TestRunner : public NES::Exceptions::ErrorListener {
+  public:
+    void onFatalError(int signalNumber, std::string callStack) override {
+        std::ostringstream fatalErrorMessage;
+        fatalErrorMessage << "onFatalError: signal [" << signalNumber << "] error [" << strerror(errno) << "] callstack "
+                          << callStack;
+
+        NES_FATAL_ERROR(fatalErrorMessage.str());
+        std::cerr << fatalErrorMessage.str() << std::endl;
+    }
+
+    void onFatalException(std::shared_ptr<std::exception> exceptionPtr, std::string callStack) override {
+        std::ostringstream fatalExceptionMessage;
+        fatalExceptionMessage << "onFatalException: exception=[" << exceptionPtr->what() << "] callstack=\n" << callStack;
+
+        NES_FATAL_ERROR(fatalExceptionMessage.str());
+        std::cerr << fatalExceptionMessage.str() << std::endl;
+    }
+};
+
+
+class LazyJoinPipelineTest : public testing::Test, public AbstractPipelineExecutionTest {
+
+  public:
+
+    ExecutablePipelineProvider* provider;
+    BufferManagerPtr bufferManager;
+    WorkerContextPtr workerContext;
+
+    /* Will be called before any test in this class are executed. */
+    static void SetUpTestCase() {
+        NES::Logger::setupLogging("LazyJoinPipelineTest.log", NES::LogLevel::LOG_DEBUG);
+        std::cout << "Setup LazyJoinPipelineTest test class." << std::endl;
+    }
+
+    /* Will be called before a test is executed. */
+    void SetUp() override {
+        std::cout << "Setup LazyJoinPipelineTest test case." << std::endl;
+        provider = ExecutablePipelineProviderRegistry::getPlugin(this->GetParam()).get();
+        bufferManager = std::make_shared<Runtime::BufferManager>();
+        workerContext = std::make_shared<WorkerContext>(0, bufferManager, 100);
+    }
+
+    /* Will be called before a test is executed. */
+    void TearDown() override { std::cout << "Tear down LazyJoinPipelineTest test case." << std::endl; }
+
+    /* Will be called after all tests in this class are finished. */
+    static void TearDownTestCase() { std::cout << "Tear down LazyJoinPipelineTest test class." << std::endl; }
+};
+
+
+TEST_P(LazyJoinPipelineTest, lazyJoinPipeline) {
+//    erstmal die unit test umschrieben und den joinsink, so dass nicht immer ein ganzer buffer benutzt wird für einen tuple
+    NES_NOT_IMPLEMENTED();
+}
+
+
+
+INSTANTIATE_TEST_CASE_P(testIfCompilation,
+                        LazyJoinPipelineTest,
+                        ::testing::Values("PipelineInterpreter", "PipelineCompiler"),
+                        [](const testing::TestParamInfo<LazyJoinPipelineTest::ParamType>& info) {
+                            return info.param;
+                        });
 
 } // namespace NES::Runtime::Execution
