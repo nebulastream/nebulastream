b1e0e93cc7694e7749a28b0486ce2f92e1d5646e
Adrian Michalke <adrian.michalke@yahoo.de>
2022-12-13 18:53:17 +0100

    [#3144] Add various tests

---
+++ nes-runtime/tests/testData/JavaUdfTestData/BooleanMapFunction.java
@@ -0,0 +1,24 @@
+//package stream.nebula.udf;
+
+import java.io.Serializable;
+
+
+public class BooleanMapFunction implements MapFunction<Boolean, Boolean> {
+    
+    // This field is used to verify that we store the actual instance in the UDF descriptor.
+    public boolean instanceVariable = false;
+
+    public BooleanMapFunction(){
+        this.instanceVariable = false;
+    }
+
+    public BooleanMapFunction(boolean instanceVariable) {
+        this.instanceVariable = instanceVariable;
+    }
+
+    @Override
+    public Boolean map(Boolean value) {
+        return value && instanceVariable;
+    }
+   
+}
