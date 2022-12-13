b3352693a37e8df86d214324b38658ea94ab7ca2
alepping <aljoschalepping@gmail.com>
2022-12-13 11:25:52 +0100

    #3169 : Started to work on value scoping.

--- nes-runtime/src/Nautilus/IR/BasicBlocks/BasicBlockArgument.cpp
+++ nes-runtime/src/Nautilus/IR/BasicBlocks/BasicBlockArgument.cpp
@@ -20,5 +20,9 @@ std::ostream& operator<<(std::ostream& os, const BasicBlockArgument& argument) {
     os << argument.identifier;
     return os;
 }
+
+OperationPtr BasicBlockArgument::getBaseOperation() { return baseOperation; }
+void BasicBlockArgument::setBaseOperation(OperationPtr baseOperation) { this->baseOperation = baseOperation; }
+
 std::string BasicBlockArgument::toString() { return identifier; }
 }// namespace NES::Nautilus::IR::Operations
