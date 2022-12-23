2739c1135adada155f089ef771a80f5acf375293
BieVic <victorbieszka@gmail.com>
2022-12-23 14:20:10 +0100

    [#3334] add wasmtime compiler & runtime

---
+++ nes-runtime/include/Nautilus/Backends/WASM/WASMCompiler.hpp
@@ -0,0 +1,160 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#ifndef NES_RUNTIME_INCLUDE_NAUTILUS_BACKENDS_WASM_WASMCOMPILER_HPP_
+#define NES_RUNTIME_INCLUDE_NAUTILUS_BACKENDS_WASM_WASMCOMPILER_HPP_
+
+#include <Nautilus/Backends/WASM/Mapping.hpp>
+#include <Nautilus/IR/BasicBlocks/BasicBlock.hpp>
+#include <Nautilus/IR/IRGraph.hpp>
+#include <Nautilus/IR/Operations/AddressOperation.hpp>
+#include <Nautilus/IR/Operations/ArithmeticOperations/AddOperation.hpp>
+#include <Nautilus/IR/Operations/ArithmeticOperations/DivOperation.hpp>
+#include <Nautilus/IR/Operations/ArithmeticOperations/MulOperation.hpp>
+#include <Nautilus/IR/Operations/ArithmeticOperations/SubOperation.hpp>
+#include <Nautilus/IR/Operations/BranchOperation.hpp>
+#include <Nautilus/IR/Operations/CastOperation.hpp>
+#include <Nautilus/IR/Operations/ConstBooleanOperation.hpp>
+#include <Nautilus/IR/Operations/ConstFloatOperation.hpp>
+#include <Nautilus/IR/Operations/ConstIntOperation.hpp>
+#include <Nautilus/IR/Operations/FunctionOperation.hpp>
+#include <Nautilus/IR/Operations/IfOperation.hpp>
+#include <Nautilus/IR/Operations/LoadOperation.hpp>
+#include <Nautilus/IR/Operations/LogicalOperations/AndOperation.hpp>
+#include <Nautilus/IR/Operations/LogicalOperations/CompareOperation.hpp>
+#include <Nautilus/IR/Operations/LogicalOperations/NegateOperation.hpp>
+#include <Nautilus/IR/Operations/LogicalOperations/OrOperation.hpp>
+#include <Nautilus/IR/Operations/Loop/LoopOperation.hpp>
+#include <Nautilus/IR/Operations/Operation.hpp>
+#include <Nautilus/IR/Operations/ProxyCallOperation.hpp>
+#include <Nautilus/IR/Operations/ReturnOperation.hpp>
+#include <Nautilus/IR/Operations/StoreOperation.hpp>
+#include <binaryen-c.h>
+#include <map>
+#include <memory>
+
+namespace NES::Nautilus::Backends::WASM {
+
+class WASMCompiler {
+    using BinaryenExpressions = Mapping<std::string, BinaryenExpressionRef>;
+    using RelooperBlocks = Mapping<std::string, RelooperBlockRef>;
+    using BlockExpressions = Mapping<std::string, BinaryenExpressionRef>;
+    using ProxyFunctions = Mapping<std::string, std::vector<BinaryenType>>;
+
+  public:
+    WASMCompiler();
+    std::pair<size_t, char*> lower(const std::shared_ptr<IR::IRGraph>& ir);
+
+    std::vector<std::string> getProxyFunctionNames() {
+        std::vector<std::string> proxyFunctionNames;
+        for (const auto& proxyName : proxyFunctions.getMapping()) {
+            proxyFunctionNames.push_back(proxyName.first);
+        }
+        return proxyFunctionNames;
+    };
+
+  private:
+    /**
+     * Binaryen module which generates the final wasm binary code
+     */
+    BinaryenModuleRef wasm;
+    /**
+     * The Relooper instance used for creating structured CFGs
+     */
+    RelooperRef relooper;
+    /**
+     * Create RelooperBlock from BinaryenBlocks and store them
+     */
+    RelooperBlocks relooperBlocks;
+    /**
+     * Save locals (variables) generated during compilation of expressions.
+     * The whole list of locals ist needed in the last compilation step, when generating the execute function
+     */
+    Mapping<std::string, BinaryenType> localVariables;
+    /**
+     * Local variables mapped to BinaryenIndex.
+     */
+    Mapping<std::string, BinaryenExpressionRef> localVarMapping;
+    /**
+     * Contains all expressions that are already used in another expression. That way we prevent adding duplicate
+     * expressions on the stack. For example, in
+     * 0_0 = 7 :i64
+     * 0_1 = 0_103 + 0_0 :i64
+     * 0_0 would end up in consumed as 0_1 already "defines" it
+     */
+    std::map<std::string, BinaryenExpressionRef> consumed;
+    /**
+     * Contains branches between blocks, which need to be added after all blocks are created
+     */
+    std::vector<std::tuple<std::string, std::string, BinaryenExpressionRef>> blockLinking;
+    /**
+     * Stores created blocks before we link them via branches
+     */
+    BlockExpressions blocks;
+    /**
+     * Current block over which we iterate
+     */
+    IR::BasicBlockPtr currentBlock;
+    /**
+     * Proxy functions used. Contains the proxy function name as key and a vector with the parameter &
+     * return types as value => {param_1, ..., param_n, return}
+     */
+    ProxyFunctions proxyFunctions;
+
+    const char* memoryName = "memory";
+    const char* proxyFunctionModule = "ProxyFunction";
+
+    /**
+     * Function arguments and locals share te same index space in Webassembly. When creating new locals, we need to track
+     * the index.
+     */
+    int localsIndex;
+
+    void generateWASM(const IR::BasicBlockPtr& basicBlock, BinaryenExpressions& expressions);
+    void generateWASM(const IR::Operations::OperationPtr& operation, BinaryenExpressions& module);
+    void generateWASM(const std::shared_ptr<IR::Operations::FunctionOperation>& funcOp);
+    void generateWASM(const std::shared_ptr<IR::Operations::ConstIntOperation>& constIntOp, BinaryenExpressions& module);
+    void generateWASM(const std::shared_ptr<IR::Operations::ConstFloatOperation>& constFloatOp, BinaryenExpressions& module);
+    void generateWASM(const std::shared_ptr<IR::Operations::ConstBooleanOperation>& constBooleanOp, BinaryenExpressions& module);
+    void generateWASM(const std::shared_ptr<IR::Operations::AddOperation>& addOp, BinaryenExpressions& module);
+    void generateWASM(const std::shared_ptr<IR::Operations::SubOperation>& subOp, BinaryenExpressions& module);
+    void generateWASM(const std::shared_ptr<IR::Operations::MulOperation>& mulOp, BinaryenExpressions& module);
+    void generateWASM(const std::shared_ptr<IR::Operations::DivOperation>& divOp, BinaryenExpressions& module);
+    void generateWASM(const std::shared_ptr<IR::Operations::StoreOperation>& storeOp, BinaryenExpressions& module);
+    void generateWASM(const std::shared_ptr<IR::Operations::LoadOperation>& loadOp, BinaryenExpressions& module);
+    void generateWASM(const std::shared_ptr<IR::Operations::AddressOperation>& addressOp, BinaryenExpressions& module);
+    void generateWASM(const std::shared_ptr<IR::Operations::IfOperation>& ifOp, BinaryenExpressions& module);
+    void generateWASM(const std::shared_ptr<IR::Operations::CompareOperation>& compareOp, BinaryenExpressions& module);
+    void generateWASM(const std::shared_ptr<IR::Operations::BranchOperation>& branchOp, BinaryenExpressions& module);
+    void generateWASM(const std::shared_ptr<IR::Operations::ReturnOperation>& returnOp, BinaryenExpressions& module);
+    void generateWASM(const std::shared_ptr<IR::Operations::ProxyCallOperation>& proxyCallOp, BinaryenExpressions& module);
+    void generateWASM(const std::shared_ptr<IR::Operations::OrOperation>& yieldOperation, BinaryenExpressions& module);
+    void generateWASM(const std::shared_ptr<IR::Operations::AndOperation>& yieldOperation, BinaryenExpressions& module);
+    void generateWASM(const std::shared_ptr<IR::Operations::NegateOperation>& yieldOperation, BinaryenExpressions& module);
+    void generateWASM(const std::shared_ptr<IR::Operations::CastOperation>& castOperation, BinaryenExpressions& module);
+    void generateWASM(const std::shared_ptr<IR::Operations::LoopOperation>& loopOp, BinaryenExpressions& module);
+
+    static BinaryenOp convertToInt32Comparison(IR::Operations::CompareOperation::Comparator comparisonType);
+    static BinaryenOp convertToInt64Comparison(IR::Operations::CompareOperation::Comparator comparisonType);
+    static BinaryenOp convertToFloat32Comparison(IR::Operations::CompareOperation::Comparator comparisonType);
+    static BinaryenOp convertToFloat64Comparison(IR::Operations::CompareOperation::Comparator comparisonType);
+
+    void generateBasicBlock(IR::Operations::BasicBlockInvocation& blockInvocation, BinaryenExpressions blockArgs);
+    void generateBody(BinaryenExpressions expressions);
+    BinaryenExpressionRef generateCFG();
+    static BinaryenType getBinaryenType(const IR::Types::StampPtr& stampPtr);
+};
+}// namespace NES::Nautilus::Backends::WASM
+
+#endif//NES_RUNTIME_INCLUDE_NAUTILUS_BACKENDS_WASM_WASMCOMPILER_HPP_
