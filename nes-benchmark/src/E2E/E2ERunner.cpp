9934c37ed47af5bc7258fda765e34806b5b7fdaa
Nils Schubert <n.schubert.1@tu-berlin.de>
2023-01-02 18:28:59 +0100

    3332 next up is getting unit tests for different yaml files to go green.

--- nes-benchmark/src/E2E/E2ERunner.cpp
+++ nes-benchmark/src/E2E/E2ERunner.cpp
@@ -12,117 +12,40 @@
     limitations under the License.
 */
 
-#include <E2E/Configurations/E2EBenchmarkConfig.hpp>
-#include <Exceptions/ErrorListener.hpp>
-#include <Util/Logger/Logger.hpp>
-
-#include <E2E/E2ESingleRun.hpp>
-#include <cstring>
-#include <filesystem>
+#include <E2E/E2ERunner.hpp>
 #include <fstream>
 
-const std::string logo = "/********************************************************\n"
-                         " *     _   _   ______    _____\n"
-                         " *    | \\ | | |  ____|  / ____|\n"
-                         " *    |  \\| | | |__    | (___\n"
-                         " *    |     | |  __|    \\___ \\     Benchmark Runner\n"
-                         " *    | |\\  | | |____   ____) |\n"
-                         " *    |_| \\_| |______| |_____/\n"
-                         " *\n"
-                         " ********************************************************/";
+namespace NES::Benchmark {
 
-class BenchmarkRunner : public NES::Exceptions::ErrorListener {
-  public:
-    void onFatalError(int signalNumber, std::string callStack) override {
-        std::ostringstream fatalErrorMessage;
-        fatalErrorMessage << "onFatalError: signal [" << signalNumber << "] error [" << strerror(errno) << "] callstack "
-                          << callStack;
+    E2EBenchmarkConfig parseYamlConfig(std::string configPath, std::string logPath) {
+        NES::Logger::setupLogging(logPath, E2EBenchmarkConfig::getLogLevel(configPath));
+        E2EBenchmarkConfig e2EBenchmarkConfig;
 
-        NES_FATAL_ERROR(fatalErrorMessage.str());
-        std::cerr << fatalErrorMessage.str() << std::endl;
-    }
+        try {
+            e2EBenchmarkConfig = E2EBenchmarkConfig::createBenchmarks(configPath);
+            NES_INFO("E2ERunner: Created the following experiments: " << e2EBenchmarkConfig.toString());
+        } catch (std::exception& e) {
+            NES_THROW_RUNTIME_ERROR("E2ERunner: Error while creating benchmarks!");
 
-    void onFatalException(std::shared_ptr<std::exception> exceptionPtr, std::string callStack) override {
-        std::ostringstream fatalExceptionMessage;
-        fatalExceptionMessage << "onFatalException: exception=[" << exceptionPtr->what() << "] callstack=\n" << callStack;
-
-        NES_FATAL_ERROR(fatalExceptionMessage.str());
-        std::cerr << fatalExceptionMessage.str() << std::endl;
-    }
-};
-
-int main(int argc, const char* argv[]) {
-
-    std::cout << logo << std::endl;
-
-    // Activating and installing error listener
-    auto runner = std::make_shared<BenchmarkRunner>();
-    NES::Exceptions::installGlobalErrorListener(runner);
-
-    if (argc > 3 || argc == 0) {
-        std::cerr << "Error: Only --configPath= and --logPath= are allowed as a command line argument!\nExiting now..."
-                  << std::endl;
-        return -1;
-    }
-
-    // Iterating through the arguments
-    std::unordered_map<std::string, std::string> argMap;
-    for (int i = 0; i < argc; ++i) {
-        auto pathArg = std::string(argv[i]);
-        if (pathArg.find("--configPath") != std::string::npos) {
-            argMap["configPath"] = pathArg.substr(pathArg.find("=") + 1, pathArg.length() - 1);
         }
-        if (pathArg.find("--logPath") != std::string::npos) {
-            argMap["logPath"] = pathArg.substr(pathArg.find("=") + 1, pathArg.length() - 1);
-        }
+
+        return e2EBenchmarkConfig;
     }
 
-    if (argMap.size() < 2) {
-        std::cerr << "Error: Missing --configPath or --logPath could not been found in arguments!" << std::endl;
-        return -1;
-    }
-
-    std::string configPath = argMap["configPath"];
-    std::string logPath = argMap["logPath"];
-    // Reading and parsing the config yaml file
-    std::cout << "parsed yaml config: " << configPath << std::endl;
-    if (configPath.empty() || !std::filesystem::exists(configPath)) {
-        std::cerr << "No yaml file provided or the file does not exist!" << std::endl;
-        return -1;
-    }
-
-    NES::Logger::setupLogging(logPath, NES::Benchmark::E2EBenchmarkConfig::getLogLevel(configPath));
-    NES::Benchmark::E2EBenchmarkConfig e2EBenchmarkConfig;
-
-    try {
-        e2EBenchmarkConfig = NES::Benchmark::E2EBenchmarkConfig::createBenchmarks(configPath);
-        NES_INFO("E2ERunner: Created the following experiments: " << e2EBenchmarkConfig.toString());
-    } catch (std::exception& e) {
-        NES_ERROR("E2ERunner: Error while creating benchmarks!");
-        return -1;
-    }
-
-    // Writing the csv header to the output file
-    std::ofstream ofs;
-    ofs.open(e2EBenchmarkConfig.getConfigOverAllRuns().outputFile->getValue(), std::ofstream::out | std::ofstream::app);
-    ofs << "BM_NAME,NES_VERSION,SchemaSizeInB"
-        << ",Time,ProcessedTasks,ProcessedBuffers,ProcessedTuples,LatencySum,QueueSizeSum"
-        << ",ThroughputInTuplesPerSec,ThroughputInTasksPerSec,ThroughputInBuffersPerSec,ThroughputInMiBPerSec"
-        << ",WorkerThreads,SourceCnt,BufferSizeInB,InputType,DataProviderMode,Query"
-        << "\n";
-    ofs.close();
-
-    int rpcPort = 8000, restPort = 9000;
-    auto configOverAllRuns = e2EBenchmarkConfig.getConfigOverAllRuns();
-    for (auto& configPerRun : e2EBenchmarkConfig.getAllConfigPerRuns()) {
-        rpcPort += 23;
-        restPort += 23;
-
-        NES::Benchmark::E2ESingleRun singleRun(configPerRun, e2EBenchmarkConfig.getConfigOverAllRuns(), rpcPort, restPort);
+    void runSingleRun(E2EBenchmarkConfigPerRun& configPerRun, E2EBenchmarkConfigOverAllRuns& configOverallRuns, int rpcPort, int restPort) {
+        E2ESingleRun singleRun(configPerRun, configOverallRuns, rpcPort, restPort);
         singleRun.run();
-
-        NES_INFO("Done with single experiment run!");
     }
 
-    return 0;
+    void writeHeaderToCsvFile(E2EBenchmarkConfig& e2EBenchmarkConfig) {
+        // Writing the csv header to the output file
+        std::ofstream ofs;
+        ofs.open(e2EBenchmarkConfig.getConfigOverAllRuns().outputFile->getValue(), std::ofstream::out | std::ofstream::app);
+        ofs << "BenchmarkName,NES_VERSION,SchemaSize,timestamp,processedTasks,processedBuffers,processedTuples,latencySum,"
+               "queueSizeSum,availGlobalBufferSum,availFixedBufferSum,"
+               "tuplesPerSecond,tasksPerSecond,bufferPerSecond,mebiBPerSecond,"
+               "numberOfWorkerOfThreads,numberOfDeployedQueries,numberOfSources,bufferSizeInBytes,inputType,dataProviderMode,queryString"
+            << std::endl;
+        ofs.close();
+    }
 }
