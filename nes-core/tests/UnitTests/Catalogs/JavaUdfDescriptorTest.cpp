db757ab254f52e9f69c46c234669d8bf295efa37
Viktor Rosenfeld <viktor.rosenfeld@tu-berlin.de>
2023-01-04 14:11:40 +0100

    [#3308] Use builder to construct JavaUdfDecriptor instances in test code

--- nes-core/tests/UnitTests/Catalogs/JavaUdfDescriptorTest.cpp
+++ nes-core/tests/UnitTests/Catalogs/JavaUdfDescriptorTest.cpp
@@ -17,6 +17,7 @@
 
 using namespace std::string_literals;
 
+#include <Util/JavaUdfDescriptorBuilder.hpp>
 #include <Catalogs/UDF/JavaUdfDescriptor.hpp>
 #include <Common/DataTypes/DataTypeFactory.hpp>
 #include <Exceptions/UdfException.hpp>
@@ -27,38 +28,44 @@ namespace NES::Catalogs::UDF {
 class JavaUdfDescriptorTest : public Testing::NESBaseTest {
   protected:
     static void SetUpTestCase() { NES::Logger::setupLogging("UdfTest.log", NES::LogLevel::LOG_DEBUG); }
-
-    const std::string className{"some_package.class_name"};
-    const std::string methodName{"udf_method"};
-    const JavaSerializedInstance serializedInstance{1};// byte-array containing 1 byte
-    const JavaUdfByteCodeList byteCodeList{{"some_package.class_name"s, JavaByteCode{1}}};
-    const SchemaPtr outputSchema = std::make_shared<Schema>()->addField("attribute", DataTypeFactory::createUInt64());
 };
 
+// The following tests check that constructing a Java UDF descriptor with invalid data is not possible.
+// In order to eliminate the need to change every construction of a JavaUdfDescriptor instance when adding fields to the
+// JavaUdfDescriptor class, the construction is done using a builder pattern.
+// By default, the JavaUdfDescriptorBuilder contains valid data for all fields required for the Java UDF descriptor,
+// e.g., class name, method name, serialized instance, bytecode list, and others.
+
 TEST_F(JavaUdfDescriptorTest, NoExceptionIsThrownForValidData) {
-    EXPECT_NO_THROW(JavaUdfDescriptor(className, methodName, serializedInstance, byteCodeList, outputSchema));
+    EXPECT_NO_THROW(JavaUdfDescriptorBuilder::createDefaultJavaUdfDescriptor());
 }
 
 TEST_F(JavaUdfDescriptorTest, TheFullyQualifiedNameMustNotBeEmpty) {
-    EXPECT_THROW(JavaUdfDescriptor(""s, methodName, serializedInstance, byteCodeList, outputSchema), UdfException);
+    EXPECT_THROW(JavaUdfDescriptorBuilder{}
+                     .setClassName("") // empty name
+                     .build(),
+                 UdfException);
 }
 
 TEST_F(JavaUdfDescriptorTest, TheMethodNameMustNotBeEmtpy) {
-    EXPECT_THROW(JavaUdfDescriptor(className, ""s, serializedInstance, byteCodeList, outputSchema), UdfException);
+    EXPECT_THROW(JavaUdfDescriptorBuilder{}
+                     .setMethodName("") // empty name
+                     .build(),
+                 UdfException);
 }
 
 TEST_F(JavaUdfDescriptorTest, TheInstanceMustNotBeEmpty) {
-    // when
-    auto emptyInstance = JavaSerializedInstance{};// empty byte array
-    // then
-    EXPECT_THROW(JavaUdfDescriptor(className, methodName, emptyInstance, byteCodeList, outputSchema), UdfException);
+    EXPECT_THROW(JavaUdfDescriptorBuilder{}
+                     .setInstance(JavaSerializedInstance{}) // empty byte array
+                     .build(),
+                 UdfException);
 }
 
 TEST_F(JavaUdfDescriptorTest, TheListOfByteCodeDefinitionsMustNotBeEmpty) {
-    // when
-    auto emptyByteCodeList = JavaUdfByteCodeList{};// empty list
-    // then
-    EXPECT_THROW(JavaUdfDescriptor(className, methodName, serializedInstance, emptyByteCodeList, outputSchema), UdfException);
+    EXPECT_THROW(JavaUdfDescriptorBuilder{}
+                     .setByteCodeList(JavaUdfByteCodeList{}) // empty list
+                     .build(),
+                 UdfException);
 }
 
 TEST_F(JavaUdfDescriptorTest, TheListOfByteCodeDefinitionsMustContainTheFullyQualifiedNameOfTheUdfClass) {
@@ -66,54 +73,71 @@ TEST_F(JavaUdfDescriptorTest, TheListOfByteCodeDefinitionsMustContainTheFullyQua
     auto unknownClassName = "some_other_package.some_other_class_name"s;
     auto byteCodeList = JavaUdfByteCodeList{{"some_package.unknown_class_name"s, JavaByteCode{1}}};
     // then
-    EXPECT_THROW(JavaUdfDescriptor(unknownClassName, methodName, serializedInstance, byteCodeList, outputSchema), UdfException);
+    EXPECT_THROW(JavaUdfDescriptorBuilder{}
+                     .setClassName(unknownClassName)
+                     .setByteCodeList(byteCodeList)
+                     .build(),
+                 UdfException);
 }
 
 TEST_F(JavaUdfDescriptorTest, TheListOfByteCodeDefinitionsMustNotContainEmptyByteCode) {
     // when
+    auto className = "some_package.my_udf"s;
     auto byteCodeListWithEmptyByteCode = JavaUdfByteCodeList{{className, JavaByteCode{}}};// empty byte array
     // then
-    EXPECT_THROW(JavaUdfDescriptor(className, methodName, serializedInstance, byteCodeListWithEmptyByteCode, outputSchema),
+    EXPECT_THROW(JavaUdfDescriptorBuilder{}
+                     .setClassName(className)
+                     .setByteCodeList(byteCodeListWithEmptyByteCode)
+                     .build(),
                  UdfException);
 }
 
 TEST_F(JavaUdfDescriptorTest, TheOutputSchemaMustNotBeEmpty) {
-    // when
-    auto emptyOutputSchema = std::make_shared<Schema>();// empty list
-    // then
-    EXPECT_THROW(JavaUdfDescriptor(className, methodName, serializedInstance, byteCodeList, emptyOutputSchema), UdfException);
+    EXPECT_THROW(JavaUdfDescriptorBuilder{}
+                     .setOutputSchema(std::make_shared<Schema>()) // empty list
+                     .build(), UdfException);
 }
+
+// The following tests verify the equality logic of the Java UDF descriptor.
 
 TEST_F(JavaUdfDescriptorTest, Equality) {
     // when
-    auto descriptor1 = JavaUdfDescriptor{className, methodName, serializedInstance, byteCodeList, outputSchema};
-    auto descriptor2 = JavaUdfDescriptor{className, methodName, serializedInstance, byteCodeList, outputSchema};
+    auto descriptor1 = JavaUdfDescriptorBuilder::createDefaultJavaUdfDescriptor();
+    auto descriptor2 = JavaUdfDescriptorBuilder::createDefaultJavaUdfDescriptor();
     // then
-    ASSERT_TRUE(descriptor1 == descriptor2);
+    ASSERT_TRUE(*descriptor1 == *descriptor2); // Compare pointed-to variables, not the pointers.
 }
 
 TEST_F(JavaUdfDescriptorTest, InEquality) {
     // when
-    auto descriptor = JavaUdfDescriptor{className, methodName, serializedInstance, byteCodeList, outputSchema};
+    auto descriptor = JavaUdfDescriptorBuilder::createDefaultJavaUdfDescriptor();
     // Check a different class name. In this case, the byte code list must contain the byte code for the different class name.
     auto differentClassName = "different_class_name"s;
-    auto descriptorWithDifferentClassName =
-        JavaUdfDescriptor{differentClassName, methodName, serializedInstance, {{differentClassName, {1}}}, outputSchema};
+    auto descriptorWithDifferentClassName = JavaUdfDescriptorBuilder{}
+                                                .setClassName(differentClassName)
+                                                .setByteCodeList({{differentClassName, {1}}})
+                                                .build();
     EXPECT_FALSE(descriptor == descriptorWithDifferentClassName);
     // Check a different method name.
-    auto descriptorWithDifferentMethodName =
-        JavaUdfDescriptor{className, "different_method_name", serializedInstance, byteCodeList, outputSchema};
+    auto descriptorWithDifferentMethodName = JavaUdfDescriptorBuilder{}
+                                                 .setMethodName("different_method_name")
+                                                 .build();
     EXPECT_FALSE(descriptor == descriptorWithDifferentMethodName);
     // Check a different serialized instance (internal state of the UDF).
-    auto descriptorWithDifferentSerializedInstance = JavaUdfDescriptor{className, methodName, {2}, byteCodeList, outputSchema};
+    auto descriptorWithDifferentSerializedInstance = JavaUdfDescriptorBuilder{}
+                                                         .setInstance({2})
+                                                         .build();
     EXPECT_FALSE(descriptor == descriptorWithDifferentSerializedInstance);
     // Check a different byte code definition of the UDF class.
-    auto descriptorWithDifferentByteCode =
-        JavaUdfDescriptor{className, methodName, serializedInstance, {{className, {2}}}, outputSchema};
+    auto descriptorWithDifferentByteCode = JavaUdfDescriptorBuilder{}
+                                               .setByteCodeList({{descriptor->getClassName(), {2}}})
+                                               .build();
     EXPECT_FALSE(descriptor == descriptorWithDifferentByteCode);
     // Check a different byte code list, i.e., additional dependencies.
-    auto descriptorWithDifferentByteCodeList =
-        JavaUdfDescriptor{className, methodName, serializedInstance, {{className, {1}}, {differentClassName, {1}}}, outputSchema};
+    auto descriptorWithDifferentByteCodeList = JavaUdfDescriptorBuilder{}
+                                                   .setByteCodeList({{descriptor->getClassName(), {1}},
+                                                                     {differentClassName, {1}}})
+                                                   .build();
     EXPECT_FALSE(descriptor == descriptorWithDifferentByteCodeList);
     // The output schema is ignored because it can be derived from the method signature.
     // We trust the Java client to do this correctly; it would cause errors otherwise during query execution.
