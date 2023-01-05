35240dac0704c073b5ac69b33c936e703c37b389
NES-CI <nebulastream@dima.tu-berlin.de>
2023-01-05 22:50:45 +0000

    GIT-CI: Updating NES version to 0.4.19

--- nes-core/tests/UnitTests/Catalogs/JavaUdfDescriptorTest.cpp
+++ nes-core/tests/UnitTests/Catalogs/JavaUdfDescriptorTest.cpp
@@ -17,10 +17,10 @@
 
 using namespace std::string_literals;
 
-#include <Util/JavaUdfDescriptorBuilder.hpp>
 #include <Catalogs/UDF/JavaUdfDescriptor.hpp>
 #include <Common/DataTypes/DataTypeFactory.hpp>
 #include <Exceptions/UdfException.hpp>
+#include <Util/JavaUdfDescriptorBuilder.hpp>
 #include <Util/Logger/Logger.hpp>
 
 namespace NES::Catalogs::UDF {
@@ -42,28 +42,28 @@ TEST_F(JavaUdfDescriptorTest, NoExceptionIsThrownForValidData) {
 
 TEST_F(JavaUdfDescriptorTest, TheFullyQualifiedNameMustNotBeEmpty) {
     EXPECT_THROW(JavaUdfDescriptorBuilder{}
-                     .setClassName("") // empty name
+                     .setClassName("")// empty name
                      .build(),
                  UdfException);
 }
 
 TEST_F(JavaUdfDescriptorTest, TheMethodNameMustNotBeEmtpy) {
     EXPECT_THROW(JavaUdfDescriptorBuilder{}
-                     .setMethodName("") // empty name
+                     .setMethodName("")// empty name
                      .build(),
                  UdfException);
 }
 
 TEST_F(JavaUdfDescriptorTest, TheInstanceMustNotBeEmpty) {
     EXPECT_THROW(JavaUdfDescriptorBuilder{}
-                     .setInstance(JavaSerializedInstance{}) // empty byte array
+                     .setInstance(JavaSerializedInstance{})// empty byte array
                      .build(),
                  UdfException);
 }
 
 TEST_F(JavaUdfDescriptorTest, TheListOfByteCodeDefinitionsMustNotBeEmpty) {
     EXPECT_THROW(JavaUdfDescriptorBuilder{}
-                     .setByteCodeList(JavaUdfByteCodeList{}) // empty list
+                     .setByteCodeList(JavaUdfByteCodeList{})// empty list
                      .build(),
                  UdfException);
 }
@@ -73,11 +73,7 @@ TEST_F(JavaUdfDescriptorTest, TheListOfByteCodeDefinitionsMustContainTheFullyQua
     auto unknownClassName = "some_other_package.some_other_class_name"s;
     auto byteCodeList = JavaUdfByteCodeList{{"some_package.unknown_class_name"s, JavaByteCode{1}}};
     // then
-    EXPECT_THROW(JavaUdfDescriptorBuilder{}
-                     .setClassName(unknownClassName)
-                     .setByteCodeList(byteCodeList)
-                     .build(),
-                 UdfException);
+    EXPECT_THROW(JavaUdfDescriptorBuilder{}.setClassName(unknownClassName).setByteCodeList(byteCodeList).build(), UdfException);
 }
 
 TEST_F(JavaUdfDescriptorTest, TheListOfByteCodeDefinitionsMustNotContainEmptyByteCode) {
@@ -85,17 +81,15 @@ TEST_F(JavaUdfDescriptorTest, TheListOfByteCodeDefinitionsMustNotContainEmptyByt
     auto className = "some_package.my_udf"s;
     auto byteCodeListWithEmptyByteCode = JavaUdfByteCodeList{{className, JavaByteCode{}}};// empty byte array
     // then
-    EXPECT_THROW(JavaUdfDescriptorBuilder{}
-                     .setClassName(className)
-                     .setByteCodeList(byteCodeListWithEmptyByteCode)
-                     .build(),
+    EXPECT_THROW(JavaUdfDescriptorBuilder{}.setClassName(className).setByteCodeList(byteCodeListWithEmptyByteCode).build(),
                  UdfException);
 }
 
 TEST_F(JavaUdfDescriptorTest, TheOutputSchemaMustNotBeEmpty) {
     EXPECT_THROW(JavaUdfDescriptorBuilder{}
-                     .setOutputSchema(std::make_shared<Schema>()) // empty list
-                     .build(), UdfException);
+                     .setOutputSchema(std::make_shared<Schema>())// empty list
+                     .build(),
+                 UdfException);
 }
 
 TEST_F(JavaUdfDescriptorTest, TheInputClassNameMustNotBeEmpty) {
@@ -119,7 +113,7 @@ TEST_F(JavaUdfDescriptorTest, Equality) {
     auto descriptor1 = JavaUdfDescriptorBuilder::createDefaultJavaUdfDescriptor();
     auto descriptor2 = JavaUdfDescriptorBuilder::createDefaultJavaUdfDescriptor();
     // then
-    ASSERT_TRUE(*descriptor1 == *descriptor2); // Compare pointed-to variables, not the pointers.
+    ASSERT_TRUE(*descriptor1 == *descriptor2);// Compare pointed-to variables, not the pointers.
 }
 
 TEST_F(JavaUdfDescriptorTest, InEquality) {
@@ -127,40 +121,27 @@ TEST_F(JavaUdfDescriptorTest, InEquality) {
     auto descriptor = JavaUdfDescriptorBuilder::createDefaultJavaUdfDescriptor();
     // Check a different class name. In this case, the byte code list must contain the byte code for the different class name.
     auto differentClassName = "different_class_name"s;
-    auto descriptorWithDifferentClassName = JavaUdfDescriptorBuilder{}
-                                                .setClassName(differentClassName)
-                                                .setByteCodeList({{differentClassName, {1}}})
-                                                .build();
+    auto descriptorWithDifferentClassName =
+        JavaUdfDescriptorBuilder{}.setClassName(differentClassName).setByteCodeList({{differentClassName, {1}}}).build();
     EXPECT_FALSE(*descriptor == *descriptorWithDifferentClassName);
     // Check a different method name.
-    auto descriptorWithDifferentMethodName = JavaUdfDescriptorBuilder{}
-                                                 .setMethodName("different_method_name")
-                                                 .build();
+    auto descriptorWithDifferentMethodName = JavaUdfDescriptorBuilder{}.setMethodName("different_method_name").build();
     EXPECT_FALSE(*descriptor == *descriptorWithDifferentMethodName);
     // Check a different serialized instance (internal state of the UDF).
-    auto descriptorWithDifferentSerializedInstance = JavaUdfDescriptorBuilder{}
-                                                         .setInstance({2})
-                                                         .build();
+    auto descriptorWithDifferentSerializedInstance = JavaUdfDescriptorBuilder{}.setInstance({2}).build();
     EXPECT_FALSE(*descriptor == *descriptorWithDifferentSerializedInstance);
     // Check a different byte code definition of the UDF class.
-    auto descriptorWithDifferentByteCode = JavaUdfDescriptorBuilder{}
-                                               .setByteCodeList({{descriptor->getClassName(), {2}}})
-                                               .build();
+    auto descriptorWithDifferentByteCode =
+        JavaUdfDescriptorBuilder{}.setByteCodeList({{descriptor->getClassName(), {2}}}).build();
     EXPECT_FALSE(*descriptor == *descriptorWithDifferentByteCode);
     // Check a different byte code list, i.e., additional dependencies.
-    auto descriptorWithDifferentByteCodeList = JavaUdfDescriptorBuilder{}
-                                                   .setByteCodeList({{descriptor->getClassName(), {1}},
-                                                                     {differentClassName, {1}}})
-                                                   .build();
+    auto descriptorWithDifferentByteCodeList =
+        JavaUdfDescriptorBuilder{}.setByteCodeList({{descriptor->getClassName(), {1}}, {differentClassName, {1}}}).build();
     EXPECT_FALSE(*descriptor == *descriptorWithDifferentByteCodeList);
     // Check a different input type.
-    auto descriptorWithDifferentInputType = JavaUdfDescriptorBuilder{}
-                                                .setInputClassName("different_input_type")
-                                                .build();
+    auto descriptorWithDifferentInputType = JavaUdfDescriptorBuilder{}.setInputClassName("different_input_type").build();
     // Check a different return type.
-    auto descriptorWithDifferentOutputType = JavaUdfDescriptorBuilder{}
-                                                .setOutputClassName("different_output_type")
-                                                .build();
+    auto descriptorWithDifferentOutputType = JavaUdfDescriptorBuilder{}.setOutputClassName("different_output_type").build();
     EXPECT_FALSE(*descriptor == *descriptorWithDifferentOutputType);
     // The output schema is ignored because it can be derived from the method signature.
     // We trust the Java client to do this correctly; it would cause errors otherwise during query execution.
