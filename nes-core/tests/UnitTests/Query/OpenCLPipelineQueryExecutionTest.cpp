f5799ea8eba9f404732acb5e8b9798d38866e962
Viktor Rosenfeld <viktor.rosenfeld@tu-berlin.de>
2023-01-21 01:39:18 +0100

    [#3434] OpenCL pipeline test

--- nes-core/tests/UnitTests/Query/OpenCLPipelineQueryExecutionTest.cpp
+++ nes-core/tests/UnitTests/Query/OpenCLPipelineQueryExecutionTest.cpp
@@ -1,37 +1,52 @@
 /*
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-        https://www.apache.org/licenses/LICENSE-2.0
+    https://www.apache.org/licenses/LICENSE-2.0
 
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 */
 
 #include <API/QueryAPI.hpp>
+#include <API/Schema.hpp>
 #include <Catalogs/Source/PhysicalSource.hpp>
 #include <Catalogs/Source/PhysicalSourceTypes/DefaultSourceType.hpp>
+#include <Catalogs/Source/SourceCatalog.hpp>
 #include <Catalogs/UDF/UdfCatalog.hpp>
-#include <Configurations/Worker/WorkerConfiguration.hpp>
 #include <NesBaseTest.hpp>
+#include <Network/NetworkChannel.hpp>
+#include <Operators/LogicalOperators/FilterLogicalOperatorNode.hpp>
 #include <Optimizer/Phases/TypeInferencePhase.hpp>
 #include <QueryCompiler/Operators/PhysicalOperators/PhysicalExternalOperator.hpp>
+#include <QueryCompiler/QueryCompilationRequest.hpp>
 #include <Runtime/Execution/ExecutablePipelineStage.hpp>
+#include <Runtime/Execution/PipelineExecutionContext.hpp>
+#include <Runtime/FixedSizeBufferPool.hpp>
+#include <Runtime/LocalBufferPool.hpp>
+#include <Runtime/MemoryLayout/ColumnLayout.hpp>
+#include <Runtime/MemoryLayout/ColumnLayoutField.hpp>
+#include <Runtime/MemoryLayout/RowLayout.hpp>
+#include <Runtime/MemoryLayout/RowLayoutField.hpp>
 #include <Runtime/NodeEngineBuilder.hpp>
+#include <Runtime/WorkerContext.hpp>
 #include <Sources/SourceCreator.hpp>
+#include <Topology/TopologyNode.hpp>
+#include <Util/Logger/Logger.hpp>
 #include <Util/TestQuery.hpp>
 #include <Util/TestQueryCompiler.hpp>
 #include <Util/TestSink.hpp>
 #include <Util/TestUtils.hpp>
-#include <Operators/LogicalOperators/FilterLogicalOperatorNode.hpp>
-#include <Runtime/TupleBuffer.hpp>
-#include <Runtime/Execution/PipelineExecutionContext.hpp>
+#include <utility>
 
 using namespace NES;
+using Runtime::TupleBuffer;
+
+#define NUMBER_OF_TUPLE 10
 
 class OpenCLQueryExecutionTest : public Testing::TestWithErrorHandling<testing::Test> {
   public:
@@ -43,17 +58,30 @@ class OpenCLQueryExecutionTest : public Testing::TestWithErrorHandling<testing::
                          unsigned numberOfTuples) {
         // Insert input data into the tuple buffer.
         auto idField = Runtime::MemoryLayouts::RowLayoutField<int32_t, true>::create(0, memoryLayout, tupleBuffer);
-        auto valueField = Runtime::MemoryLayouts::RowLayoutField<int32_t, true>::create(0, memoryLayout, tupleBuffer);
+        auto valueField = Runtime::MemoryLayouts::RowLayoutField<int32_t, true>::create(1, memoryLayout, tupleBuffer);
         for (auto i = 0u; i < numberOfTuples; ++i) {
             idField[i] = i;
             valueField[i] = 1;
         }
         tupleBuffer.setNumberOfTuples(numberOfTuples);
     }
+
+    void cleanUpPlan(Runtime::Execution::ExecutableQueryPlanPtr plan) {
+        // TODO Why is this necessary?
+        std::for_each(plan->getSources().begin(), plan->getSources().end(), [plan](auto source) {
+            plan->notifySourceCompletion(source, Runtime::QueryTerminationType::Graceful);
+        });
+        std::for_each(plan->getPipelines().begin(), plan->getPipelines().end(), [plan](auto pipeline) {
+            plan->notifyPipelineCompletion(pipeline, Runtime::QueryTerminationType::Graceful);
+        });
+        std::for_each(plan->getSinks().begin(), plan->getSinks().end(), [plan](auto sink) {
+            plan->notifySinkCompletion(sink, Runtime::QueryTerminationType::Graceful);
+        });
+        ASSERT_TRUE(plan->stop());
+    }
 };
 
 class SimpleOpenCLPipelineStage : public Runtime::Execution::ExecutablePipelineStage {
-
     class InputRecord {
       public:
         [[maybe_unused]] int32_t id;
@@ -68,6 +96,7 @@ class SimpleOpenCLPipelineStage : public Runtime::Execution::ExecutablePipelineS
         [[maybe_unused]] int32_t new2;
     };
 
+  public:
     uint32_t setup(Runtime::Execution::PipelineExecutionContext& pipelineExecutionContext) override {
         // Get kernel source
         // Get OpenCL platform and device
@@ -77,14 +106,15 @@ class SimpleOpenCLPipelineStage : public Runtime::Execution::ExecutablePipelineS
         return ExecutablePipelineStage::setup(pipelineExecutionContext);
     }
 
-    ExecutionResult execute([[maybe_unused]] Runtime::TupleBuffer& buffer,
-                            [[maybe_unused]] Runtime::Execution::PipelineExecutionContext& ctx,
-                            [[maybe_unused]] Runtime::WorkerContext& wc) override {
+    ExecutionResult execute(Runtime::TupleBuffer& buffer,
+                            Runtime::Execution::PipelineExecutionContext& ctx,
+                            Runtime::WorkerContext& wc) override {
         // Obtain an output buffer.
         auto outputBuffer = wc.allocateTupleBuffer();
 
         // This is a map kernel, so the number of output tuples == number of input tuples.
-        outputBuffer.setNumberOfTuples(buffer.getNumberOfTuples());
+        auto numberOfOutputTuples = buffer.getNumberOfTuples();
+        outputBuffer.setNumberOfTuples(numberOfOutputTuples);
 
         // TODO Copy memory buffers
         // TODO Setup kernel call
@@ -102,6 +132,7 @@ class SimpleOpenCLPipelineStage : public Runtime::Execution::ExecutablePipelineS
     }
 };
 
+// Test the execution of an external operator on a source with a custom structure
 TEST_F(OpenCLQueryExecutionTest, simpleOpenCLKernel) {
     // Create an external operator with the custom OpenCL pipeline stage.
     auto customPipelineStage = std::make_shared<SimpleOpenCLPipelineStage>();
@@ -118,8 +149,8 @@ TEST_F(OpenCLQueryExecutionTest, simpleOpenCLKernel) {
     // Create a node engine to execute the OpenCL pipeline.
     // TODO Is the query status listener necessary?
     auto nodeEngine = Runtime::NodeEngineBuilder::create(workerConfiguration)
-                          .setQueryStatusListener(std::make_shared<DummyQueryListener>())
-                          .build();
+                     .setQueryStatusListener(std::make_shared<DummyQueryListener>())
+                     .build();
 
     // Create the input schema with two uint32 fields.
     // TODO Why do the fields have to be prefixed with test$? If this is omitted, type inference below fails.
@@ -166,7 +197,7 @@ TEST_F(OpenCLQueryExecutionTest, simpleOpenCLKernel) {
                                               Catalogs::UDF::UdfCatalog::create());
     auto queryPlan = typeInferencePhase->execute(query.getQueryPlan());
     auto filterOperator = queryPlan->getOperatorByType<FilterLogicalOperatorNode>()[0];
-//    filterOperator->insertBetweenThisAndParentNodes(externalOperator);
+    filterOperator->insertBetweenThisAndParentNodes(externalOperator);
 
     // Create an executable query plan.
     auto request = QueryCompilation::QueryCompilationRequest::create(queryPlan, nodeEngine);
@@ -176,31 +207,58 @@ TEST_F(OpenCLQueryExecutionTest, simpleOpenCLKernel) {
     ASSERT_EQ(plan->getStatus(), Runtime::Execution::ExecutableQueryPlanStatus::Created);
 
     // The plan should have two pipelines.
-//    ASSERT_EQ(plan->getPipelines().size(), 2u);
+    ASSERT_EQ(plan->getPipelines().size(), 2u);
 
     // Create a worker context to execute the pipeline.
     // TODO What are these magic numbers?
     auto workerContext = Runtime::WorkerContext(1, nodeEngine->getBufferManager(), 4);
 
     // Get an input buffer from the buffer manager.
-    auto buffer = nodeEngine->getBufferManager()->getBufferBlocking();
-    // !! converts to bool.
-    // TupleBuffer has operator!() which tests if the internal ptr is set.
-    // TODO This is bad design, it should be a explanatory method name, e.g., isValid.
-    ASSERT_TRUE(!!buffer);
+    // The buffer object has to be destroyed before nodeEngine->stop() is called, otherwise the following error occurs.
+    // [LocalBufferPool.cpp:70] [destroy] NES Fatal Error on numberOfReservedBuffers == exclusiveBufferCount message: one or more buffers were not returned to the pool: 3 but expected 4
+    {
+        auto buffer = nodeEngine->getBufferManager()->getBufferBlocking();
+        // !! converts to bool.
+        // TupleBuffer has operator!() which tests if the internal ptr is set.
+        // TODO This is bad design, it should be a explanatory method name, e.g., isValid.
+        ASSERT_TRUE(!!buffer);
 
-    // Fill the input buffer.
-    auto memoryLayout = Runtime::MemoryLayouts::RowLayout::create(inputSchema, nodeEngine->getBufferManager()->getBufferSize());
-    fillInputBuffer(buffer, memoryLayout, numberOfTuples);
+        // Fill the input buffer.
+        auto inputMemoryLayout = Runtime::MemoryLayouts::RowLayout::create(inputSchema, nodeEngine->getBufferManager()->getBufferSize());
+        fillInputBuffer(buffer, inputMemoryLayout, numberOfTuples);
 
-    // Start the query plan.
-    plan->setup();
-//    ASSERT_TRUE(plan->setup());
-//    ASSERT_EQ(plan->getStatus(), Runtime::Execution::ExecutableQueryPlanStatus::Deployed);
-//    ASSERT_TRUE(plan->start(nodeEngine->getStateManager()));
-//    ASSERT_EQ(plan->getStatus(), Runtime::Execution::ExecutableQueryPlanStatus::Running);
-//    // Execute the second pipeline, which is the custom pipeline?
-//    // TODO Why is it the second?
-//    ASSERT_EQ(plan->getPipelines()[1]->execute(buffer, workerContext), ExecutionResult::Ok);
+        // Start the query plan.
+        ASSERT_TRUE(plan->setup());
+        ASSERT_EQ(plan->getStatus(), Runtime::Execution::ExecutableQueryPlanStatus::Deployed);
+        ASSERT_TRUE(plan->start(nodeEngine->getStateManager()));
+        ASSERT_EQ(plan->getStatus(), Runtime::Execution::ExecutableQueryPlanStatus::Running);
+        // Execute the second pipeline, which is the custom pipeline?
+        // TODO Why is it the second?
+        ASSERT_EQ(plan->getPipelines()[1]->execute(buffer, workerContext), ExecutionResult::Ok);
 
+        // There should be one output buffer with 5 tuples.
+        EXPECT_EQ(testSink->getNumberOfResultBuffers(), 1u);
+        auto resultBuffer = testSink->get(0);
+        EXPECT_EQ(resultBuffer.getNumberOfTuples(), 5u);
+
+        // Compare results
+        auto outputMemoryLayout = Runtime::MemoryLayouts::RowLayout::create(outputSchema, nodeEngine->getBufferManager()->getBufferSize());
+        auto idField = Runtime::MemoryLayouts::RowLayoutField<int32_t, true>::create(0, outputMemoryLayout, resultBuffer);
+        auto valueField = Runtime::MemoryLayouts::RowLayoutField<int32_t, true>::create(1, outputMemoryLayout, resultBuffer);
+        auto new1Field = Runtime::MemoryLayouts::RowLayoutField<int32_t, true>::create(2, outputMemoryLayout, resultBuffer);
+        auto new2Field = Runtime::MemoryLayouts::RowLayoutField<int32_t, true>::create(3, outputMemoryLayout, resultBuffer);
+        for (auto i = 0u; i < 5u; ++i) {
+            EXPECT_EQ(idField[i], i);
+            EXPECT_EQ(valueField[i], 1);
+            EXPECT_EQ(new1Field[i], idField[i] * 2);
+            EXPECT_EQ(new2Field[i], idField[i] + 2);
+        }
+    }
+
+    // Cleanup
+    cleanUpPlan(plan);
+    testSink->cleanupBuffers();
+    // TODO Why is this necessary?
+    ASSERT_EQ(testSink->getNumberOfResultBuffers(), 0U);
+    ASSERT_TRUE(nodeEngine->stop());
 }
