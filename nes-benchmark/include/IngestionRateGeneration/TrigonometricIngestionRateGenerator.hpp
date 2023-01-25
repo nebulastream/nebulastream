20f53f7505d2f04ebb468b178c298f47bdd28004
Niklas Tantow <n.tantow@campus.tu-berlin.de>
2023-01-25 20:49:59 +0100

    #3333 added functions for class TrigonometricIngestionRateGenerator

---
+++ nes-benchmark/include/IngestionRateGeneration/TrigonometricIngestionRateGenerator.hpp
@@ -0,0 +1,59 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#ifndef NES_TRIGONOMETRICINGESTIONRATEGENERATOR_HPP
+#define NES_TRIGONOMETRICINGESTIONRATEGENERATOR_HPP
+
+#include <IngestionRateGeneration/IngestionRateGenerator.hpp>
+
+namespace NES::Benchmark::IngestionRateGeneration {
+class TrigonometricIngestionRateGenerator : public IngestionRateGenerator {
+  public:
+    /**
+     * @brief constructor for a uniform ingestion rate generator
+     * @param ingestionRateInBuffers
+     * @param ingestionRateCnt
+     */
+    explicit TrigonometricIngestionRateGenerator(std::string ingestionRateDistribution, uint64_t ingestionRateInBuffers, uint64_t ingestionRateCnt, uint64_t numberOfPeriods);
+
+    /**
+      * @brief creates a vector of length ingestionRateCnt and fills it with values that are distributed according to ingestionRateDistribution
+      * @return predefinedIngestionRates
+      */
+    std::vector<std::uint64_t> generateIngestionRates() override;
+
+  private:
+    /**
+     * @brief calculates the sine of x. Sine has a period length of ingestionRateInBuffers/numberOfPeriods
+     * @param x
+     * @return value
+     */
+    long double getSinValue(uint64_t x);
+
+    /**
+     * @brief calculates the cosine of x. Cosine has a period length of ingestionRateInBuffers/numberOfPeriods
+     * @param x
+     * @return value
+     */
+    long double getCosValue(uint64_t x);
+
+    IngestionRateDistribution ingestionRateDistribution;
+    uint64_t ingestionRateInBuffers;
+    uint64_t ingestionRateCnt;
+    uint64_t numberOfPeriods;
+    std::vector<uint64_t> predefinedIngestionRates;
+};
+}// namespace NES::Benchmark::IngestionRateGeneration
+
+#endif//NES_TRIGONOMETRICINGESTIONRATEGENERATOR_HPP
