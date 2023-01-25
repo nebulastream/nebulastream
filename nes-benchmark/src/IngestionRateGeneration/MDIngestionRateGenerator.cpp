6b44ed9ec238b45db18a4b6b81601242eec75efc
Niklas Tantow <n.tantow@campus.tu-berlin.de>
2023-01-25 18:36:20 +0100

    #3333 added class MDIngestionRateGenerator

---
+++ nes-benchmark/src/IngestionRateGeneration/MDIngestionRateGenerator.cpp
@@ -0,0 +1,36 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include <IngestionRateGeneration/MDIngestionRateGenerator.hpp>
+
+namespace NES::Benchmark::IngestionRateGeneration {
+MDIngestionRateGenerator::MDIngestionRateGenerator(std::string ingestionRateDistribution, uint64_t ingestionRateCnt)
+    : IngestionRateGenerator(), ingestionRateDistribution(ingestionRateDistribution), ingestionRateCnt(ingestionRateCnt) {}
+
+std::vector<std::uint64_t> MDIngestionRateGenerator::generateIngestionRates() {
+    for (uint64_t i = 0; i < ingestionRateCnt; ++i) {
+        if (ingestionRateDistribution == "M1" || ingestionRateDistribution == "m1") {
+            predefinedIngestionRates.push_back(m1Values[i % 18]);
+        } else if (ingestionRateDistribution == "M2" || ingestionRateDistribution == "m2") {
+            predefinedIngestionRates.push_back(m2Values[i % 18]);
+        } else if (ingestionRateDistribution == "D1" || ingestionRateDistribution == "d1") {
+            predefinedIngestionRates.push_back(d1Values[i % 30]);
+        } else if (ingestionRateDistribution == "D2" || ingestionRateDistribution == "d2") {
+            predefinedIngestionRates.push_back(d2Values[i % 30]);
+        }
+    }
+
+    return predefinedIngestionRates;
+}
+}// namespace NES::Benchmark::IngestionRateGeneration
