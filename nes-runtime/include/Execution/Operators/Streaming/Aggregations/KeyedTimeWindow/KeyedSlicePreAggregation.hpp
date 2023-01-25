4c61af16ee0da434a81cb34d5309cf372400af3e
Philipp Grulich <philippgrulich@hotmail.de>
2023-01-25 16:48:47 +0100

    [#3417] start with keyed aggregation
    Signed-off-by: Philipp Grulich <philippgrulich@hotmail.de>

---
+++ nes-runtime/include/Execution/Operators/Streaming/Aggregations/KeyedTimeWindow/KeyedSlicePreAggregation.hpp
@@ -0,0 +1,52 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#ifndef NES_NES_RUNTIME_INCLUDE_EXECUTION_OPERATORS_STREAMING_AGGREGATIONS_KEYEDPREAGGREGATIONOPERATOR_HPP_
+#define NES_NES_RUNTIME_INCLUDE_EXECUTION_OPERATORS_STREAMING_AGGREGATIONS_KEYEDPREAGGREGATIONOPERATOR_HPP_
+#include <Execution/Aggregation/AggregationFunction.hpp>
+#include <Execution/Expressions/Expression.hpp>
+#include <Execution/Operators/ExecutableOperator.hpp>
+#include <Nautilus/Interface/Hash/HashFunction.hpp>
+namespace NES::Runtime::Execution::Operators {
+
+/**
+* @brief KeyedSlicePreAggregation operator that performs the pre-aggregation step for a keyed window aggregation.
+*/
+class KeyedSlicePreAggregation : public ExecutableOperator {
+  public:
+    /**
+    * @brief Creates a KeyedSlicePreAggregation operator
+    */
+    KeyedSlicePreAggregation(uint64_t operatorHandlerIndex,
+                             Expressions::ExpressionPtr timestampExpression,
+                             const std::vector<Expressions::ExpressionPtr>& keyExpressions,
+                             const std::vector<Expressions::ExpressionPtr>& aggregationExpressions,
+                             const std::vector<std::shared_ptr<Aggregation::AggregationFunction>>& aggregationFunctions);
+    void setup(ExecutionContext& executionCtx) const override;
+    void open(ExecutionContext& ctx, RecordBuffer& recordBuffer) const override;
+    void execute(ExecutionContext& ctx, Record& record) const override;
+    void close(ExecutionContext& ctx, RecordBuffer& recordBuffer) const override;
+
+  private:
+    const uint64_t operatorHandlerIndex;
+    const Expressions::ExpressionPtr timestampExpression;
+    const std::vector<Expressions::ExpressionPtr> keyExpressions;
+    const std::vector<Expressions::ExpressionPtr> aggregationExpressions;
+    const std::vector<std::shared_ptr<Aggregation::AggregationFunction>> aggregationFunctions;
+    const std::unique_ptr<Nautilus::Interface::HashFunction> hashFunction;
+};
+
+}// namespace NES::Runtime::Execution::Operators
+
+#endif//NES_NES_RUNTIME_INCLUDE_EXECUTION_OPERATORS_STREAMING_AGGREGATIONS_KEYEDPREAGGREGATIONOPERATOR_HPP_
