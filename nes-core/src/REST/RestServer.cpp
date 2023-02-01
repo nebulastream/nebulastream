6b68e833257474f7aa6358dbc2d16a4035cc0c4b
fkuzun <felix.s.lang@campus.tu-berlin.de>
2023-02-01 18:21:58 +0100

    [#3447] added exception handling to test

--- nes-core/src/REST/RestServer.cpp
+++ nes-core/src/REST/RestServer.cpp
@@ -163,7 +163,7 @@ void RestServer::run() {
 
     /* Run server */
     server.run([this]() -> bool {
-        NES_DEBUG("checking if stop request has arrived for rest server");
+        NES_DEBUG("checking if stop request has arrived for rest server listening on port " << port);
         std::unique_lock lock(mutex);
         return !stopRequested;
     });
