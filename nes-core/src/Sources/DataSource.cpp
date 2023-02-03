41a8efd8519fc4304a57b713a5ab424f48240c69
alepping <aljoschalepping@gmail.com>
2023-02-03 20:43:16 +0100

    #3467 : Added pushRecord, and readRecord utility functions to DynamicTupleBuffer. Refactored DynamicMemoryLayoutTests.

--- nes-core/src/Sources/DataSource.cpp
+++ nes-core/src/Sources/DataSource.cpp
@@ -11,6 +11,7 @@
     See the License for the specific language governing permissions and
     limitations under the License.
 */
+#include <Runtime/MemoryLayout/DynamicTupleBuffer.hpp>
 #include <Runtime/Execution/ExecutablePipeline.hpp>
 #include <Runtime/Execution/ExecutablePipelineStage.hpp>
 #include <Runtime/Execution/ExecutableQueryPlan.hpp>
