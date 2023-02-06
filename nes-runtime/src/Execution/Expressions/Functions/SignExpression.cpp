5cd2faae8338acce011148519ea0c135fce4812b
dbpro16 <j.mellerh@gmail.com>
2023-02-06 09:56:18 +0000

    added Trunc and Sign expressions #3060 #3052

--- nes-runtime/src/Execution/Expressions/Functions/SignExpression.cpp
+++ nes-runtime/src/Execution/Expressions/Functions/SignExpression.cpp
@@ -11,7 +11,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-#include <Execution/Expressions/SignExpression.hpp>
+#include <Execution/Expressions/Functions/SignExpression.hpp>
 #include <Nautilus/Interface/FunctionCall.hpp>
 #include <cmath>
 
--- nes-runtime/src/Execution/Expressions/Functions/TruncExpression.cpp
+++ nes-runtime/src/Execution/Expressions/Functions/TruncExpression.cpp
@@ -11,7 +11,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-#include <Execution/Expressions/TruncExpression.hpp>
+#include <Execution/Expressions/Functions/TruncExpression.hpp>
 #include <Nautilus/Interface/FunctionCall.hpp>
 #include <cmath>
 
