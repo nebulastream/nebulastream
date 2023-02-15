19daea32cd9d5df63f63ebf77e29181f5a0a9f2c
juliane <juliane.verwiebe@jevernet.de>
2023-02-15 15:23:04 +0100

    change include statements and add licence

--- nes-runtime/tests/UnitTests/Execution/PipelineExecution/SelectivityRuntimeTest.cpp
+++ nes-runtime/tests/UnitTests/Execution/PipelineExecution/SelectivityRuntimeTest.cpp
@@ -1,8 +1,19 @@
-//
-// Created by Juliane on 13.01.2023.
-//
-#include "Execution/Expressions/LogicalExpressions/GreaterThanExpression.hpp"
-#include "Execution/Expressions/LogicalExpressions/LessThanExpression.hpp"
+/*
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#include <Execution/Expressions/LogicalExpressions/GreaterThanExpression.hpp>
+#include <Execution/Expressions/LogicalExpressions/LessThanExpression.hpp>
 #include <API/Schema.hpp>
 #include <Execution/Expressions/ConstantIntegerExpression.hpp>
 #include <Execution/Expressions/LogicalExpressions/EqualsExpression.hpp>
