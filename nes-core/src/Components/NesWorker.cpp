787d87042946502ce4ba137644640106a34e9a4d
AKozar <kozar.anastasiia@gmail.com>
2023-02-16 17:12:22 +0100

    lambda source

--- nes-core/src/Components/NesWorker.cpp
+++ nes-core/src/Components/NesWorker.cpp
@@ -177,7 +177,6 @@ bool NesWorker::start(bool blocking, bool withConnect) {
         auto now_ms = std::chrono::time_point_cast<std::chrono::milliseconds>(now);
         auto epoch = now_ms.time_since_epoch();
         auto value = std::chrono::duration_cast<std::chrono::milliseconds>(epoch);
-        buffer.setWatermark(value.count());
         for (auto u = 0u; u < numberOfTuplesToProduce; ++u) {
             records[u].a = u;
             records[u].b = u % 2;
