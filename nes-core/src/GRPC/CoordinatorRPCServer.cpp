5f6d4f23bc35ef1e1c8e86b347a52c187fcbe172
fkuzun <felix.s.lang@campus.tu-berlin.de>
2023-02-17 19:10:32 +0100

    [#3514] check spatial type when unregistering

--- nes-core/src/GRPC/CoordinatorRPCServer.cpp
+++ nes-core/src/GRPC/CoordinatorRPCServer.cpp
@@ -92,7 +92,10 @@ Status
 CoordinatorRPCServer::UnregisterWorker(ServerContext*, const UnregisterWorkerRequest* request, UnregisterWorkerReply* reply) {
     NES_DEBUG("CoordinatorRPCServer::UnregisterNode: request =" << request);
 
-    bool success = topologyManagerService->removeGeoLocation(request->workerid());
+    auto spatialType = topologyManagerService->findNodeWithId(request->workerid())->getSpatialNodeType();
+    bool success = topologyManagerService->removeGeoLocation(request->workerid()) ||
+        spatialType == NES::Spatial::Experimental::SpatialType::NO_LOCATION ||
+        spatialType == NES::Spatial::Experimental::SpatialType::INVALID;
     if (success) {
         if (!topologyManagerService->unregisterNode(request->workerid())) {
             NES_ERROR("CoordinatorRPCServer::UnregisterNode: Worker was not removed");
