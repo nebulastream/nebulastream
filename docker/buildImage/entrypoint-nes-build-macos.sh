2eae0733ad6bfe4506c2a6580d13096a26b0e2d2
Philipp Grulich <philippgrulich@hotmail.de>
2023-03-05 14:10:13 +0100

    [#3564] split build and tests
    Signed-off-by: Philipp Grulich <philippgrulich@hotmail.de>

--- docker/buildImage/entrypoint-nes-build-macos.sh
+++ docker/buildImage/entrypoint-nes-build-macos.sh
@@ -31,8 +31,8 @@ echo "Build dir=$build_dir"
 if [ $# -eq 0 ]
 then
     # Build NES
-    mkdir -p /nebulastream/build
-    cd /nebulastream/build
+    mkdir -p build
+    cd build
     cmake --fresh -B "$build_dir/" -DCMAKE_BUILD_TYPE=Release -DBoost_NO_SYSTEM_PATHS=TRUE -DNES_SELF_HOSTING=1 -DNES_USE_OPC=0 -DNES_ENABLE_EXPERIMENTAL_EXECUTION_ENGINE=1 -DNES_ENABLE_EXPERIMENTAL_EXECUTION_MLIR=1 -DNES_USE_MQTT=1 -DNES_USE_ADAPTIVE=0 -DNES_USE_TF=1 -DNES_USE_S2=1 ..
     cmake --build "$build_dir/" -j4
     # Check if build was successful
