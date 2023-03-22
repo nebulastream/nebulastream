e903dfb005738f50cbcb86f087f6e142d76a09a9
arianeziehn <ariane.ziehn@campus.tu-berlin.de>
2023-03-22 15:23:50 +0100

    [#3130] Add TimeStamp Extension Fix AddExpression for Time Stamp Data Type Improve Tests given Review Comments

--- nes-runtime/tests/UnitTests/Nautilus/Interface/CustomDataTypeTest.cpp
+++ nes-runtime/tests/UnitTests/Nautilus/Interface/CustomDataTypeTest.cpp
@@ -11,7 +11,6 @@
     See the License for the specific language governing permissions and
     limitations under the License.
 */
-
 #include <Nautilus/IR/Types/IntegerStamp.hpp>
 #include <Nautilus/Interface/DataTypes/Integer/Int.hpp>
 #include <Nautilus/Interface/DataTypes/InvocationPlugin.hpp>
@@ -29,12 +28,22 @@ class CustomDataTypeTest : public Testing::NESBaseTest {
   public:
     /* Will be called before any test in this class are executed. */
     static void SetUpTestCase() {
-        NES::Logger::setupLogging("CustomDataTypeTest.log", NES::LogLevel::LOG_DEBUG);
-        NES_INFO("Setup CustomDataTypeTest test class.");
+        NES::Logger::setupLogging("TextTypeTest.log", NES::LogLevel::LOG_DEBUG);
+        NES_DEBUG("Setup TextTypeTest test class.");
+    }
+
+    /* Will be called before a test is executed. */
+    void SetUp() override {
+        Testing::NESBaseTest::SetUp();
+        bm = std::make_shared<Runtime::BufferManager>();
+        wc = std::make_shared<Runtime::WorkerContext>(0, bm, 1024);
+        NES_DEBUG("Setup TextTypeTest test case.")
     }
 
     /* Will be called after all tests in this class are finished. */
-    static void TearDownTestCase() { NES_INFO("Tear down CustomDataTypeTest test class."); }
+    static void TearDownTestCase() { NES_DEBUG("Tear down TextTypeTest test class."); }
+    std::shared_ptr<Runtime::BufferManager> bm;
+    std::shared_ptr<Runtime::WorkerContext> wc;
 };
 
 class CustomType : public Any {
@@ -78,34 +87,98 @@ TEST_F(CustomDataTypeTest, customCustomDataTypeTest) {
     NES_DEBUG(c3.value);
 }
 
-TEST_F(CustomDataTypeTest, customTimeStampTypeTest) {
+TEST_F(CustomDataTypeTest, customTimeStampTypeBaseTest) {
     long ms = 1666798551744;// Wed Oct 26 2022 15:35:51
     std::chrono::hours dur(ms);
-    auto c1 = Value<TimeStamp>(TimeStamp((int64_t) dur.count()));
-    ASSERT_EQ(cast<IR::Types::IntegerStamp>(c1.value->getType())->getNumberOfBits(), 64);
-    ASSERT_EQ(c1.getValue().toString(), std::to_string(ms));
+    auto c1 = Value<TimeStamp>(TimeStamp((uint64_t) dur.count()));
+    EXPECT_EQ(cast<IR::Types::IntegerStamp>(c1.value->getType())->getNumberOfBits(), 64);
+    EXPECT_EQ(c1->getMilliSeconds(), (uint64_t) ms);
 
-    const TimeStamp c2 = TimeStamp((int64_t) dur.count());
+    const TimeStamp c2 = TimeStamp((uint64_t) dur.count());
     auto c3 = c1 + c2;
-    ASSERT_EQ(c3.getValue().toString(), std::to_string(3333597103488));
-    const TimeStamp c4 = TimeStamp((int64_t) dur.count() - 1000);
-    /* tests the functions greater and less than, equals */
-    ASSERT_EQ((c1 > c4), true);
-    ASSERT_EQ((c4 < c1), true);
-    ASSERT_EQ((c1 == c2), true);
-    ASSERT_EQ((c1 == c3), false);
+    EXPECT_EQ(c3.as<TimeStamp>()->getMilliSeconds(), (uint64_t) 3333597103488);
+    const TimeStamp c4 = TimeStamp((uint64_t) dur.count() - 1000);
+    /** tests the functions greater and less than, equals */
+    EXPECT_EQ((c1 > c4), true);
+    EXPECT_EQ((c4 < c1), true);
+    EXPECT_EQ((c1 == c2), true);
+    EXPECT_EQ((c1 == c3), false);
+    /** tests the functions get a day part, seconds to year */
     auto seconds = c1->getSeconds();
-    ASSERT_EQ(seconds->toString(), std::to_string(51));
+    EXPECT_EQ(seconds->toString(), std::to_string(51));
     auto minutes = c1->getMinutes();
-    ASSERT_EQ(minutes->toString(), std::to_string(35));
+    EXPECT_EQ(minutes->toString(), std::to_string(35));
     auto hours = c1->getHours();
-    ASSERT_EQ(hours->toString(), std::to_string(15));
+    EXPECT_EQ(hours->toString(), std::to_string(15));
     auto days = c1->getDays();
-    ASSERT_EQ(days->toString(), std::to_string(26));
+    EXPECT_EQ(days->toString(), std::to_string(26));
     auto months = c1->getMonths();
-    ASSERT_EQ(months->toString(), std::to_string(10));
+    EXPECT_EQ(months->toString(), std::to_string(10));
     auto years = c1->getYears();
-    ASSERT_EQ(years->toString(), std::to_string(2022));
+    EXPECT_EQ(years->toString(), std::to_string(2022));
+}
+
+TEST_F(CustomDataTypeTest, customTimeStampTypeExtendions) {
+    /** Test für century functions*/
+    long ms = 1666798551744;// Wed Oct 26 2022 15:35:51
+    std::chrono::milliseconds dur(ms);
+    auto c1 = Value<TimeStamp>(TimeStamp((uint64_t) dur.count()));
+    auto centuryc1 = c1->century();
+    EXPECT_EQ(centuryc1, (uint64_t) 21);
+    long ms2 = 570207551;// Wed Jan 07 1970 14:23:27
+    std::chrono::milliseconds dur1(ms2);
+    auto c2 = Value<TimeStamp>(TimeStamp((uint64_t) dur1.count()));
+    auto centuryc2 = c2->century();
+    EXPECT_EQ(centuryc2, (uint64_t) 20);
+    /** Test für age functions*/
+    auto age = c2->difference(c1);
+    EXPECT_EQ(age, (uint64_t) 1666228344193);
+    auto age2 = c1->difference(c2);
+    EXPECT_EQ(age2, (uint64_t) 1666228344193);
+    /** Test für weekdayName */
+    auto weekdayName = c1->getWeekdayName();
+    EXPECT_EQ(weekdayName, Value<Text>("Wednesday"));
+    long ms3 = 1666000551744;//Mon Oct 17 2022 09:55:51
+    std::chrono::milliseconds dur2(ms3);
+    auto c3 = Value<TimeStamp>(TimeStamp((uint64_t) dur2.count()));
+    auto weekdayNameMon = c3->getWeekdayName();
+    EXPECT_EQ(weekdayNameMon, Value<Text>("Monday"));
+}
+
+TEST_F(CustomDataTypeTest, customTimeStampTypeConstructurTest) {
+    //Test String Constructor
+    auto textValue = Value<Text>("1970-01-07T14:23:27");
+    auto c3 = Value<TimeStamp>(TimeStamp((Value<Text>) textValue));
+    EXPECT_EQ(c3->getMilliSeconds(), (uint64_t) 566607000L);
+    //
+    auto textValue1 = Value<Text>("1970-01-07");
+    auto c4 = Value<TimeStamp>(TimeStamp((Value<Text>) textValue1));
+    EXPECT_EQ(c4->getMilliSeconds(), (uint64_t) 514800000L);
+}
+
+TEST_F(CustomDataTypeTest, customTimeStampTypeIntervalTest) {
+    std::chrono::milliseconds dur(3600000L);
+    auto dayParts = Value<Text>("1970 years 1 day 1 hour ");
+    auto c1 = Value<TimeStamp>(TimeStamp((uint64_t) dur.count()));
+    auto interval = c1->interval();
+    EXPECT_EQ(interval, dayParts);
+
+    std::chrono::milliseconds dur1(570207551L);
+    auto c2 = Value<TimeStamp>(TimeStamp((uint64_t) dur1.count()));
+    auto interval1 = c2->interval();
+    auto dayParts2 = Value<Text>("1970 years 7 days 14 hours 23 minutes 27 seconds");
+    EXPECT_EQ(interval1, dayParts2);
+
+    std::chrono::milliseconds duris(0L);
+    auto c3 = Value<TimeStamp>(TimeStamp((uint64_t) duris.count()));
+    auto dayParts3 = Value<Text>("1970 years 1 day ");
+    auto interval2 = c3->interval();
+    EXPECT_TRUE((Boolean) interval2->equals(dayParts3));
+
+    auto c4 = Value<TimeStamp>(TimeStamp((uint64_t) duris.count()));
+    auto textzero = Value<Text>("1970 years 1 day und ganz viel Quatsch");
+    auto intervall1 = c4->interval();
+    EXPECT_FALSE((Boolean) intervall1->equals(textzero));
 }
 
 }// namespace NES::Nautilus
