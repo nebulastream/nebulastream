3204bc2a237e7e6b0f123d584ccb5a05e9215b23
Nils Schubert <n.schubert.1@tu-berlin.de>
2023-03-23 17:21:08 +0100

    [3620] first step

---
+++ nes-approx/include/Synopses/SynopsesArguments.hpp
@@ -0,0 +1,37 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#ifndef NES_SYNOPSESARGUMENTS_HPP
+#define NES_SYNOPSESARGUMENTS_HPP
+
+#include <stdint.h>
+
+namespace NES::ASP {
+
+class SynopsesArguments {
+
+    enum class Type : uint8_t {
+        SRSWR,
+
+    };
+
+  private:
+    size_t width;
+    size_t height;
+    size_t numberOf
+};
+
+} // namespace NES::ASP
+
+#endif//NES_SYNOPSESARGUMENTS_HPP
