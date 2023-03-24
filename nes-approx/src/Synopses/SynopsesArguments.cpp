a6534ac43410e3a9656f56399b1b5d0e3b09aa68
Nils Schubert <n.schubert.1@tu-berlin.de>
2023-03-24 12:24:00 +0100

    [3620] continued with first skeleton work

--- nes-approx/src/Synopses/SynopsesArguments.cpp
+++ nes-approx/src/Synopses/SynopsesArguments.cpp
@@ -16,4 +16,29 @@
 
 namespace NES::ASP {
 
+SynopsesArguments::SynopsesArguments(Type type, size_t width, size_t height, size_t windowSize, std::string fieldName,
+                                     Runtime::Execution::Aggregation::AggregationFunctionPtr aggregationFunction)
+    : type(type), width(width), height(height), windowSize(windowSize), fieldName(fieldName),
+      aggregationFunction(std::move(aggregationFunction)) {}
+
+SynopsesArguments SynopsesArguments::createArguments(Runtime::Execution::Aggregation::AggregationFunctionPtr aggregationFunction,
+                                                     std::string fieldName, Type type, size_t width, size_t height,
+                                                     size_t windowSize) {
+    return SynopsesArguments(type, width, height, windowSize, fieldName, aggregationFunction);
+}
+
+size_t SynopsesArguments::getWidth() const { return width; }
+
+size_t SynopsesArguments::getHeight() const { return height; }
+
+size_t SynopsesArguments::getWindowSize() const { return windowSize; }
+
+Runtime::Execution::Aggregation::AggregationFunctionPtr SynopsesArguments::getAggregationFunction() const {
+    return aggregationFunction;
+}
+
+SynopsesArguments::Type SynopsesArguments::getType() const { return type; }
+
+const std::string& SynopsesArguments::getFieldName() const { return fieldName; }
+
 } // namespace NES::ASP
