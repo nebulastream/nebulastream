46767ad316d9a62d23df6609e0d12eb0a4ad0033
Ankit Chaudhary <ankit.chaudhary@tu-berlin.de>
2023-04-13 10:33:08 +0200

    #3624: fixed infer model deployment test and addressed review comments

--- nes-runtime/src/Execution/Operators/Streaming/InferModel/TensorflowAdapter.cpp
+++ nes-runtime/src/Execution/Operators/Streaming/InferModel/TensorflowAdapter.cpp
@@ -13,7 +13,6 @@
 */
 #include <Execution/Operators/Streaming/InferModel/TensorflowAdapter.hpp>
 #include <Util/Logger/Logger.hpp>
-#include <cstdarg>
 #include <fstream>
 #include <iostream>
 
@@ -26,14 +25,14 @@ namespace NES::Runtime::Execution::Operators {
 
 TensorflowAdapterPtr TensorflowAdapter::create() { return std::make_shared<TensorflowAdapter>(); }
 
-void TensorflowAdapter::initializeModel(std::string model) {
-    NES_DEBUG2("INITIALIZING MODEL:  {}", model);
-    std::ifstream input(model, std::ios::in | std::ios::binary);
+void TensorflowAdapter::initializeModel(std::string pathToModel) {
+    NES_DEBUG2("INITIALIZING MODEL:  {}", pathToModel);
+    std::ifstream input(pathToModel, std::ios::in | std::ios::binary);
     std::string bytes((std::istreambuf_iterator<char>(input)), (std::istreambuf_iterator<char>()));
     input.close();
     NES_INFO2("MODEL SIZE: {}", std::to_string(bytes.size()));
     TfLiteInterpreterOptions* options = TfLiteInterpreterOptionsCreate();
-    interpreter = TfLiteInterpreterCreate(TfLiteModelCreateFromFile(model.c_str()), options);
+    interpreter = TfLiteInterpreterCreate(TfLiteModelCreateFromFile(pathToModel.c_str()), options);
     TfLiteInterpreterAllocateTensors(interpreter);
     this->inputTensor = TfLiteInterpreterGetInputTensor(interpreter, 0);
     this->tensorSize = (int) (TfLiteTensorByteSize(inputTensor));
@@ -60,131 +59,11 @@ void TensorflowAdapter::infer() {
 
     //Copy value to the output
     TfLiteTensorCopyToBuffer(outputTensor, outputData, outputSize);
+}
 
-
-    /* //Prepare output tensor
-    const TfLiteTensor* outputTensor = TfLiteInterpreterGetOutputTensor(interpreter, 0);
-    int outputSize = (int) (TfLiteTensorByteSize(outputTensor));
-    output = (float*) malloc(outputSize);
-
-    //Copy value to the output
-    TfLiteTensorCopyToBuffer(outputTensor, output, outputSize);
-
-
-
-    //Prepare input parameters based on data type
-    if (modelInput[0]->isType<Nautilus::Int32>()) {
-        //create input for tensor
-        *//*auto* inputData = (<int>*) malloc(inputSize);
-        for (uint32_t i = 0; i < modelInput.size(); ++i) {
-            inputData[i] = modelInput.at(i).as<Nautilus::Int32>();
-        }*//*
-
-        //Copy input tensor
-        TfLiteTensorCopyFromBuffer(inputTensor, input, inputSize);
-        //Invoke tensor model and perform inference
-        TfLiteInterpreterInvoke(interpreter);
-
-        //Release allocated memory for input
-        free(inputData);
-
-        //Clear allocated memory to output
-        if (output != nullptr) {
-            free(output);
-        }
-
-        //Prepare output tensor
-        const TfLiteTensor* outputTensor = TfLiteInterpreterGetOutputTensor(interpreter, 0);
-        int outputSize = (int) (TfLiteTensorByteSize(outputTensor));
-        output = (float*) malloc(outputSize);
-
-        //Copy value to the output
-        TfLiteTensorCopyToBuffer(outputTensor, output, outputSize);
-
-    } else if (modelInput[0]->isType<Nautilus::Float>()) {
-        //create input for tensor
-        auto* inputData = (float*) malloc(inputSize);
-        for (uint32_t i = 0; i < modelInput.size(); ++i) {
-            inputData[i] = modelInput.at(i).as<Nautilus::Float>();
-        }
-
-        //Copy input tensor
-        TfLiteTensorCopyFromBuffer(inputTensor, inputData, inputSize);
-        //Invoke tensor model and perform inference
-        TfLiteInterpreterInvoke(interpreter);
-
-        //Release allocated memory for input
-        free(inputData);
-
-        //Clear allocated memory to output
-        if (output != nullptr) {
-            free(output);
-        }
-
-        //Prepare output tensor
-        const TfLiteTensor* outputTensor = TfLiteInterpreterGetOutputTensor(interpreter, 0);
-        int outputSize = (int) (TfLiteTensorByteSize(outputTensor));
-        output = (float*) malloc(outputSize);
-
-        //Copy value to the output
-        TfLiteTensorCopyToBuffer(outputTensor, output, outputSize);
-
-    } else if (modelInput[0]->isType<Nautilus::Double>()) {
-        //create input for tensor
-        auto* inputData = (double*) malloc(inputSize);
-        for (uint32_t i = 0; i < modelInput.size(); ++i) {
-            inputData[i] = modelInput.at(i).as<Nautilus::Double>();
-        }
-
-        //Copy input tensor
-        TfLiteTensorCopyFromBuffer(inputTensor, inputData, inputSize);
-        //Invoke tensor model and perform inference
-        TfLiteInterpreterInvoke(interpreter);
-
-        //Release allocated memory for input
-        free(inputData);
-
-        //Clear allocated memory to output
-        if (output != nullptr) {
-            free(output);
-        }
-
-        //Prepare output tensor
-        const TfLiteTensor* outputTensor = TfLiteInterpreterGetOutputTensor(interpreter, 0);
-        int outputSize = (int) (TfLiteTensorByteSize(outputTensor));
-        output = (float*) malloc(outputSize);
-
-        //Copy value to the output
-        TfLiteTensorCopyToBuffer(outputTensor, output, outputSize);
-
-    } else if (modelInput[0]->isType<Nautilus::Boolean>()) {
-        //create input for tensor
-        auto* inputData = (bool*) malloc(inputSize);
-        for (uint32_t i = 0; i < modelInput.size(); ++i) {
-            inputData[i] = modelInput.at(i).as<Nautilus::Boolean>();
-        }
-
-        //Copy input tensor
-        TfLiteTensorCopyFromBuffer(inputTensor, inputData, inputSize);
-        //Invoke tensor model and perform inference
-        TfLiteInterpreterInvoke(interpreter);
-
-        //Release allocated memory for input
-        free(inputData);
-
-        //Clear allocated memory to output
-        if (output != nullptr) {
-            free(output);
-        }
-
-        //Prepare output tensor
-        const TfLiteTensor* outputTensor = TfLiteInterpreterGetOutputTensor(interpreter, 0);
-        int outputSize = (int) (TfLiteTensorByteSize(outputTensor));
-        output = (float*) malloc(outputSize);
-
-        //Copy value to the output
-        TfLiteTensorCopyToBuffer(outputTensor, output, outputSize);
-    }*/
+TensorflowAdapter::~TensorflowAdapter() {
+    free(inputData);
+    free(outputData);
 }
 
 }// namespace NES::Runtime::Execution::Operators
