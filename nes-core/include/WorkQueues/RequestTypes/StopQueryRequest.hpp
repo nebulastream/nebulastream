5ebb8753cbdf0497633c06b87ee50c78bab03049
eleicha <laura.mons@web.de>
2023-04-13 18:37:09 +0200

    #3634 added Stop request and tests

--- nes-core/include/WorkQueues/RequestTypes/StopQueryRequest.hpp
+++ nes-core/include/WorkQueues/RequestTypes/StopQueryRequest.hpp
@@ -15,19 +15,71 @@
 #ifndef NES_CORE_INCLUDE_WORKQUEUES_REQUESTTYPES_STOPQUERYREQUEST_HPP_
 #define NES_CORE_INCLUDE_WORKQUEUES_REQUESTTYPES_STOPQUERYREQUEST_HPP_
 
+#include <WorkQueues/RequestTypes/AbstractRequest.hpp>
 #include <WorkQueues/RequestTypes/Request.hpp>
+#include <WorkQueues/StorageHandles/StorageHandle.hpp>
+
+namespace NES::Optimizer {
+class TypeInferencePhase;
+using TypeInferencePhasePtr = std::shared_ptr<TypeInferencePhase>;
+
+class QueryRewritePhase;
+using QueryRewritePhasePtr = std::shared_ptr<QueryRewritePhase>;
+
+class QueryPlacementPhase;
+using QueryPlacementPhasePtr = std::shared_ptr<QueryPlacementPhase>;
+
+class GlobalQueryPlanUpdatePhase;
+using GlobalQueryPlanUpdatePhasePtr = std::shared_ptr<GlobalQueryPlanUpdatePhase>;
+}// namespace NES::Optimizer
 
 namespace NES {
+
+class GlobalQueryPlan;
+using GlobalQueryPlanPtr = std::shared_ptr<GlobalQueryPlan>;
+
+class QueryCatalogService;
+using QueryCatalogServicePtr = std::shared_ptr<QueryCatalogService>;
+
+class QueryDeploymentPhase;
+using QueryDeploymentPhasePtr = std::shared_ptr<QueryDeploymentPhase>;
+
+class QueryUndeploymentPhase;
+using QueryUndeploymentPhasePtr = std::shared_ptr<QueryUndeploymentPhase>;
+
+class GlobalExecutionPlan;
+using GlobalExecutionPlanPtr = std::shared_ptr<GlobalExecutionPlan>;
+
+class Topology;
+using TopologyPtr = std::shared_ptr<Topology>;
+
+class WorkerRPCClient;
+using WorkerRPCClientPtr = std::shared_ptr<WorkerRPCClient>;
 
 class StopQueryRequest;
 using StopQueryRequestPtr = std::shared_ptr<StopQueryRequest>;
 
+namespace Catalogs {
+
+namespace Source {
+class SourceCatalog;
+using SourceCatalogPtr = std::shared_ptr<SourceCatalog>;
+}// namespace Source
+
+namespace UDF {
+class UdfCatalog;
+using UdfCatalogPtr = std::shared_ptr<UdfCatalog>;
+}// namespace UDF
+
+}// namespace Catalogs
+
 /**
  * @brief This request is used for stopping a running query in NES cluster
  */
-class StopQueryRequest : public Request {
+class StopQueryRequest : public Request, public AbstractRequest {
 
   public:
+    //todo: remove method after Request Restructuring was successfully completed
     /**
      * @brief Create instance of  StopQueryRequest
      * @param queryId : the id of query to be stopped
@@ -35,15 +87,42 @@ class StopQueryRequest : public Request {
      */
     static StopQueryRequestPtr create(QueryId queryId);
 
+    static StopQueryRequestPtr
+    create(QueryId queryId, size_t maxRetries, const WorkerRPCClientPtr& workerRpcClient, bool queryReconfiguration);
+
+    void executeRequestLogic(const StorageHandlePtr storageHandle) override;
+
+    void preRollbackHandle(std::exception ex, StorageHandlePtr storageHandle) override;
+    void postRollbackHandle(std::exception ex, StorageHandlePtr storageHandle) override;
+    void rollBack(std::exception& ex, StorageHandlePtr storageHandle) override;
+    void preExecution(StorageHandlePtr storageHandle, std::vector<StorageHandleResourceType> requiredResources) override;
+    void postExecution(StorageHandlePtr storageHandle, std::vector<StorageHandleResourceType> requiredResources) override;
+
     std::string toString() override;
 
     ~StopQueryRequest() override = default;
 
-    QueryId getQueryId() const;
+    const WorkerRPCClientPtr& getWorkerRpcClient() const;
+
+    const QueryId& getQueryId() const;
 
   private:
-    explicit StopQueryRequest(QueryId queryId);
+    StopQueryRequest(QueryId queryId, size_t maxRetries, const WorkerRPCClientPtr& workerRpcClient, bool queryReconfiguration);
+    //todo: remove constructor after Request Restructuring was successfully completed
+    StopQueryRequest(QueryId queryId);
+    const WorkerRPCClientPtr& workerRpcClient;
+    GlobalExecutionPlanPtr globalExecutionPlan;
+    TopologyPtr topology;
+    QueryCatalogServicePtr queryCatalogService;
+    GlobalQueryPlanPtr globalQueryPlan;
     QueryId queryId;
+    Catalogs::Source::SourceCatalogPtr sourceCatalog;
+    QueryDeploymentPhasePtr queryDeploymentPhase;
+    QueryUndeploymentPhasePtr queryUndeploymentPhase;
+    Optimizer::TypeInferencePhasePtr typeInferencePhase;
+    Optimizer::QueryPlacementPhasePtr queryPlacementPhase;
+    Catalogs::UDF::UdfCatalogPtr udfCatalog;
+    bool queryReconfiguration;
 };
 }// namespace NES
 
--- nes-core/src/WorkQueues/RequestTypes/StopQueryRequest.cpp
+++ nes-core/src/WorkQueues/RequestTypes/StopQueryRequest.cpp
@@ -12,18 +12,150 @@
     limitations under the License.
 */
 
+#include <Catalogs/Query/QueryCatalogEntry.hpp>
+#include <Exceptions/QueryDeploymentException.hpp>
+#include <Exceptions/QueryNotFoundException.hpp>
+#include <Exceptions/QueryPlacementException.hpp>
+#include <Optimizer/Phases/QueryPlacementPhase.hpp>
+#include <Optimizer/Phases/TypeInferencePhase.hpp>
+#include <Phases/QueryDeploymentPhase.hpp>
+#include <Phases/QueryUndeploymentPhase.hpp>
+#include <Plans/Global/Query/GlobalQueryPlan.hpp>
+#include <Plans/Global/Query/SharedQueryPlan.hpp>
+#include <Services/QueryCatalogService.hpp>
+#include <Services/TopologyManagerService.hpp>
+#include <Util/RequestType.hpp>
 #include <WorkQueues/RequestTypes/StopQueryRequest.hpp>
 #include <string>
 namespace NES {
 
-StopQueryRequest::StopQueryRequest(QueryId queryId) : queryId(queryId) {}
-
+//todo: remove method after Request Restructuring was successfully completed
+StopQueryRequest::StopQueryRequest(QueryId queryId)
+    : AbstractRequest(0, {}), workerRpcClient(getWorkerRpcClient()), queryId(queryId), queryReconfiguration(false) {}
+//todo: remove constructor after Request Restructuring was successfully completed
 StopQueryRequestPtr StopQueryRequest::create(QueryId queryId) {
     return std::make_shared<StopQueryRequest>(StopQueryRequest(queryId));
 }
 
+StopQueryRequest::StopQueryRequest(QueryId queryId,
+                                   size_t maxRetries,
+                                   const WorkerRPCClientPtr& workerRpcClient,
+                                   bool queryReconfiguration)
+    : AbstractRequest(maxRetries, {}), workerRpcClient(std::move(workerRpcClient)), queryId(queryId),
+      queryReconfiguration(queryReconfiguration) {}
+
+StopQueryRequestPtr StopQueryRequest::create(QueryId queryId,
+                                             size_t maxRetries,
+                                             const WorkerRPCClientPtr& workerRpcClient,
+                                             bool queryReconfiguration) {
+    return std::make_shared<StopQueryRequest>(StopQueryRequest(queryId, maxRetries, workerRpcClient, queryReconfiguration));
+}
+
+void StopQueryRequest::preExecution(StorageHandlePtr storageHandle, [[maybe_unused]] std::vector<StorageHandleResourceType> requiredResources) {
+    NES_TRACE2("Acquire locks.");
+    queryCatalogService = storageHandle->getQueryCatalogHandle();
+    globalQueryPlan = storageHandle->getGlobalQueryPlanHandle();
+    globalExecutionPlan = storageHandle->getGlobalExecutionPlanHandle();
+    topology = storageHandle->getTopologyHandle();
+    sourceCatalog = storageHandle->getSourceCatalogHandle();
+    udfCatalog = storageHandle->getUdfCatalogHandle();
+    typeInferencePhase = Optimizer::TypeInferencePhase::create(sourceCatalog, udfCatalog);
+    queryPlacementPhase = Optimizer::QueryPlacementPhase::create(globalExecutionPlan,
+                                                                 topology,
+                                                                 typeInferencePhase,
+                                                                 queryReconfiguration);
+    queryUndeploymentPhase =
+        QueryUndeploymentPhase::create(topology, globalExecutionPlan, workerRpcClient);
+}
+
+void StopQueryRequest::executeRequestLogic(const StorageHandlePtr storageHandle) {
+    NES_TRACE2("Start Stop Request logic.");
+    try {
+        //mark single query for hard stop
+        auto markedForHardStopSuccessful = queryCatalogService->checkAndMarkForHardStop(queryId);
+        if (!markedForHardStopSuccessful) {
+            std::exception e = std::runtime_error("Failed to mark query for hard stop");
+            throw e;
+        }
+        //remove single query from global query plan
+        globalQueryPlan->removeQuery(queryId, RequestType::Stop);
+        auto sharedQueryId = globalQueryPlan->getSharedQueryId(queryId);
+        //todo: here shared query plan could be updated or stopped need to check this and act accordingly
+        //undeploy SQP
+        auto sharedQueryPlan = globalQueryPlan->getSharedQueryPlan(sharedQueryId);
+        bool undeploymentSuccessful = queryUndeploymentPhase->execute(sharedQueryId, sharedQueryPlan->getStatus());
+        if (!undeploymentSuccessful) {
+            std::exception e = std::runtime_error("Failed to undeploy query");
+            throw e;
+        }
+        if (SharedQueryPlanStatus::Stopped == sharedQueryPlan->getStatus()) {
+            //Mark all contained queryIdAndCatalogEntryMapping as stopped
+            for (auto& involvedQueryIds : sharedQueryPlan->getQueryIds()) {
+                queryCatalogService->updateQueryStatus(involvedQueryIds, QueryStatus::STOPPED, "Hard Stopped");
+            }
+        } else if (SharedQueryPlanStatus::Updated == sharedQueryPlan->getStatus()) {
+            //3.3.2. Perform placement of updated shared query plan
+            auto queryPlan = sharedQueryPlan->getQueryPlan();
+            NES_DEBUG2("QueryProcessingService: Performing Operator placement for shared query plan");
+            //todo: Where to get the placement strategy? Currently it is supplied with the run query request
+            //could we move it to the SQP? Since one SQP should have the same placement strategy, right?
+            //otherwise maybe add it to an individual query plan?
+            bool placementSuccessful = queryPlacementPhase->execute(PlacementStrategy::TopDown, sharedQueryPlan);
+            if (!placementSuccessful) {
+                throw QueryPlacementException(sharedQueryId,
+                                              "QueryProcessingService: Failed to perform query placement for "
+                                              "query plan with shared query id: "
+                                                  + std::to_string(sharedQueryId));
+            }
+
+            //3.3.3. Perform deployment of re-placed shared query plan
+            bool deploymentSuccessful = queryDeploymentPhase->execute(sharedQueryPlan);
+            if (!deploymentSuccessful) {
+                throw QueryDeploymentException(sharedQueryId,
+                                               "QueryRequestProcessingService: Failed to deploy query with global query Id "
+                                                   + std::to_string(sharedQueryId));
+            }
+
+            //Update the shared query plan as deployed
+            sharedQueryPlan->setStatus(SharedQueryPlanStatus::Deployed);
+        }
+    } catch (std::exception& e) {
+        handleError(e, storageHandle);
+    }
+}
+
+void StopQueryRequest::postExecution(StorageHandlePtr storageHandle, [[maybe_unused]] std::vector<StorageHandleResourceType> requiredResources) {
+    NES_TRACE2("Release locks.");
+    queryCatalogService.reset();
+    globalQueryPlan.reset();
+    globalExecutionPlan.reset();
+    topology.reset();
+    sourceCatalog.reset();
+    udfCatalog.reset();
+    typeInferencePhase.reset();
+    queryPlacementPhase.reset();
+    queryUndeploymentPhase.reset();
+    storageHandle.reset();
+}
+
 std::string StopQueryRequest::toString() { return "StopQueryRequest { QueryId: " + std::to_string(queryId) + "}"; }
 
-QueryId StopQueryRequest::getQueryId() const { return queryId; }
-
+/*todo: add error handling for:
+ * 1. [16:06:55.369061] [E] [thread 107422] [QueryCatalogService.cpp:324] [addUpdatedQueryPlan] QueryCatalogService: Query Catalog does not contains the input queryId 0
+ * 2. [16:49:07.569624] [E] [thread 109653] [RuntimeException.cpp:31] [RuntimeException] GlobalQueryPlan: Can not add query plan with invalid id. at /home/eleicha/Documents/DFKI/Code/nebulastream/nes-core/src/Plans/Global/Query/GlobalQueryPlan.cpp:33 addQueryPlan
+*/
+void StopQueryRequest::preRollbackHandle(std::exception ex, [[maybe_unused]] StorageHandlePtr storageHandle) {
+    NES_TRACE2("Error: {}", ex.what());
+    //todo: nothing
+}
+void StopQueryRequest::postRollbackHandle(std::exception ex, [[maybe_unused]] StorageHandlePtr storageHandle) {
+    NES_TRACE2("Error: {}", ex.what());
+    //todo: call fail query request
+}
+void StopQueryRequest::rollBack(std::exception& ex, [[maybe_unused]] StorageHandlePtr storageHandle) {
+    NES_TRACE2("Error: {}", ex.what());
+    //todo: need to add instanceOf to errors to handle failures correctly
+}
+const WorkerRPCClientPtr& StopQueryRequest::getWorkerRpcClient() const { return workerRpcClient; }
+const QueryId& StopQueryRequest::getQueryId() const { return queryId; }
 }// namespace NES
