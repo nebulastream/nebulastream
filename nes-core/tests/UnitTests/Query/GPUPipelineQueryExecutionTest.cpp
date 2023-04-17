25ef963589666b10eec0caab8d0d844f9fe95650
Adrian Michalke <adrian.michalke@yahoo.de>
2023-04-17 12:44:50 +0200

    [#3609] address comments

--- nes-core/tests/UnitTests/Query/GPUPipelineQueryExecutionTest.cpp
+++ nes-core/tests/UnitTests/Query/GPUPipelineQueryExecutionTest.cpp
@@ -19,7 +19,7 @@
 #include <Catalogs/Source/PhysicalSource.hpp>
 #include <Catalogs/Source/PhysicalSourceTypes/DefaultSourceType.hpp>
 #include <Catalogs/Source/SourceCatalog.hpp>
-#include <Catalogs/UDF/UdfCatalog.hpp>
+#include <Catalogs/UDF/UDFCatalog.hpp>
 #include <NesBaseTest.hpp>
 #include <Network/NetworkChannel.hpp>
 #include <Operators/LogicalOperators/FilterLogicalOperatorNode.hpp>
