83c9a20d4ea569651e0a0cf6f4528d7d658e47ee
Niklas Tantow <n.tantow@campus.tu-berlin.de>
2023-05-12 17:34:06 +0200

    #3224 finished with all functions which generate dependencies

---
+++ nes-benchmark/include/DataGeneration/NEXMarkGeneration/DependencyGenerator.hpp
@@ -0,0 +1,109 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+    
+        https://www.apache.org/licenses/LICENSE-2.0
+    
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#ifndef NES_DEPENDENCYGENERATOR_HPP
+#define NES_DEPENDENCYGENERATOR_HPP
+
+#include <cstdint>
+#include <vector>
+#include <API/Schema.hpp>
+#include <random>
+
+namespace NES::Benchmark::DataGeneration::NEXMarkGeneration {
+
+class DependencyGenerator {
+  public:
+    /**
+     * @brief returns the single instance of the class
+     * @param numberOfRecords
+     * @return DependencyGenerator instance
+     */
+    static DependencyGenerator& getInstance(uint64_t numberOfRecords);
+
+    /**
+     * @brief getter for persons
+     * @return persons
+     */
+    std::vector<uint64_t>& getPersons();
+
+    /**
+     * @brief getter for auctions
+     * @return auctions
+     */
+    std::vector<std::tuple<uint64_t, uint64_t, uint64_t>>& getAuctions();
+
+    /**
+     * @brief getter for bids
+     * @return bids
+     */
+    std::vector<std::tuple<uint64_t, uint64_t, uint64_t>>& getBids();
+
+  private:
+    /**
+     * @brief constructor of a DependencyGenerator
+     * @param numberOfRecords
+     */
+    explicit DependencyGenerator(uint64_t numberOfRecords);
+
+    /**
+     * @brief copy constructor and move assignment operator are deleted to prevent creation of multiple instances of the DependencyGenerator.
+     * Note that the move constructor and move assignment operator are deleted implicitly by the compiler.
+     */
+    DependencyGenerator(const DependencyGenerator&) = delete;
+    DependencyGenerator& operator=(const DependencyGenerator&) = delete;
+
+    /**
+     * @brief creates a timestamp for every person and adds it to 'persons'
+     * @param curTime
+     * @param numPersons
+     */
+    void generatePersonDependencies(uint64_t* curTime, uint64_t numPersons);
+
+    /**
+     * @brief creates start and end timestamps as well as a random seller id and adds the element to 'auctions'
+     * @param curTime
+     * @param numOpenAuctions
+     */
+    void generateOpenAuctionDependencies(uint64_t* curTime, uint64_t numOpenAuctions);
+
+    /**
+     * @brief creates a timestamp as well as a random auction id and bidder id and adds the element to 'bids'
+     * @param curTime
+     * @param numBids
+     */
+    void generateBidDependencies(uint64_t* curTime, uint64_t numBids);
+
+    /**
+     * @brief increments the current time up to 60 seconds
+     * @param curTimeInSec
+     * @return incremented time
+     */
+    uint64_t incrementTime(uint64_t curTimeInSec);
+
+    /**
+     * @brief a persons element is a timestamp
+     */
+    std::vector<uint64_t> persons;
+    /**
+     * @brief an auctions element consists of seller id, start timestamp and end timestamp
+     */
+    std::vector<std::tuple<uint64_t, uint64_t, uint64_t>> auctions;
+    /**
+     * @brief a bids element consists of auction id, bidder id and timestamp
+     */
+    std::vector<std::tuple<uint64_t, uint64_t, uint64_t>> bids;
+};
+} // namespace NES::Benchmark::DataGeneration::NEXMarkGeneration
+
+#endif//NES_DEPENDENCYGENERATOR_HPP
---
+++ nes-benchmark/src/DataGeneration/NEXMarkGeneration/DependencyGenerator.cpp
@@ -0,0 +1,123 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include <DataGeneration/NEXMarkGeneration/DependencyGenerator.hpp>
+
+namespace NES::Benchmark::DataGeneration::NEXMarkGeneration {
+
+DependencyGenerator::DependencyGenerator(uint64_t numberOfRecords) {
+    // using a seed to generate a predictable sequence of values for deterministic behavior
+    std::mt19937 generator(103984);
+    std::uniform_int_distribution<uint64_t> uniformPersonGenerationDistribution(0, 9);
+    std::uniform_int_distribution<uint64_t> uniformOpenAuctionGenerationDistribution(0, 2);
+    std::uniform_int_distribution<uint64_t> uniformBidGenerationDistribution(0, 20);
+
+    auto timeInSec = 0UL;
+
+    // first generate some persons and open auctions that can be bid on
+    for (auto i = 0; i < 50; ++i) {
+        generatePersonDependencies(&timeInSec, 1);
+    }
+    for (auto i = 0; i < 50; ++i) {
+        generateOpenAuctionDependencies(&timeInSec, 1);
+    }
+
+    // now generate approximately numberOfRecords many auctions as well as corresponding persons and bids
+    for (auto i = 0UL; i < numberOfRecords; ++i) {
+        // create on average one person per ten auctions
+        if (uniformPersonGenerationDistribution(generator) == 0) {
+            generatePersonDependencies(&timeInSec, 1);
+        }
+
+        // create on average one open auction
+        auto numOpenAuctions = uniformOpenAuctionGenerationDistribution(generator);
+        generateOpenAuctionDependencies(&timeInSec, numOpenAuctions);
+
+        // create on average ten bids per auction
+        auto numBids = uniformBidGenerationDistribution(generator);
+        generateBidDependencies(&timeInSec, numBids);
+    }
+}
+
+DependencyGenerator& DependencyGenerator::getInstance(uint64_t numberOfRecords) {
+    // C++11 guarantees that static local variables are initialized in a thread-safe manner
+    static DependencyGenerator instance(numberOfRecords);
+    return instance;
+}
+
+void DependencyGenerator::generatePersonDependencies(uint64_t* curTime, uint64_t numPersons) {
+    *curTime = incrementTime(*curTime);
+
+    for (uint64_t i = 0; i < numPersons; ++i) {
+        persons.emplace_back(*curTime);
+    }
+}
+
+void DependencyGenerator::generateOpenAuctionDependencies(uint64_t* curTime, uint64_t numOpenAuctions) {
+    *curTime = incrementTime(*curTime);
+    std::random_device rndDevice;
+    std::mt19937 generator(rndDevice());
+
+    // generate a random end time that is between two and twenty-six hours after the start time
+    std::uniform_int_distribution<uint64_t> uniformEndTimeDistribution(2 * 60 * 60, 26 * 60 * 60 - 1);
+
+    // generate a random seller id from the pool of persons
+    auto maxSellerId = persons.size() - 1;
+    std::uniform_int_distribution<uint64_t> uniformPersonIdDistribution(0, maxSellerId);
+
+    for (uint64_t i = 0; i < numOpenAuctions; ++i) {
+        auto sellerId = uniformPersonIdDistribution(generator);
+        auto endTime = *curTime + uniformEndTimeDistribution(generator);
+        std::tuple<uint64_t, uint64_t, uint64_t> auctionRecord = std::make_tuple(sellerId, *curTime, endTime);
+        auctions.emplace_back(auctionRecord);
+    }
+}
+
+void DependencyGenerator::generateBidDependencies(uint64_t* curTime, uint64_t numBids) {
+    *curTime = incrementTime(*curTime);
+    std::random_device rndDevice;
+    std::mt19937 generator(rndDevice());
+
+    // generate a random auction id from the pool of auctions
+    auto maxAuctionId = auctions.size() - 1;
+    std::uniform_int_distribution<uint64_t> uniformAuctionIdDistribution(0, maxAuctionId);
+
+    // generate a random seller id from the pool of persons
+    auto maxBidderId = persons.size() - 1;
+    std::uniform_int_distribution<uint64_t> uniformPersonIdDistribution(0, maxBidderId);
+
+    for (uint64_t i = 0; i < numBids; ++i) {
+        auto auctionId = uniformAuctionIdDistribution(generator);
+        auto bidderId = uniformPersonIdDistribution(generator);
+        std::tuple<uint64_t, uint64_t, uint64_t> bidRecord = std::make_tuple(auctionId, bidderId, *curTime);
+        bids.emplace_back(bidRecord);
+    }
+}
+
+uint64_t DependencyGenerator::incrementTime(uint64_t curTimeInSec) {
+    // generate a random new time that is at most one minute in the future
+    std::random_device rndDevice;
+    std::mt19937 generator(rndDevice());
+    std::uniform_int_distribution<uint64_t> uniformTimeDistribution(0, 59);
+
+    return curTimeInSec + uniformTimeDistribution(generator);
+}
+
+std::vector<uint64_t>& DependencyGenerator::getPersons() { return persons; }
+
+std::vector<std::tuple<uint64_t, uint64_t, uint64_t>>& DependencyGenerator::getAuctions() { return auctions; }
+
+std::vector<std::tuple<uint64_t, uint64_t, uint64_t>>& DependencyGenerator::getBids() { return bids; }
+
+} // namespace NES::Benchmark::DataGeneration::NEXMarkGeneration
