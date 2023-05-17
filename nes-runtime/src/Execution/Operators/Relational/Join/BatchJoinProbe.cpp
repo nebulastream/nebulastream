3dce519fd10ff8bbc692120588466ca346d098c5
Nils Schubert <n.schubert.1@tu-berlin.de>
2023-05-17 23:46:52 +0200

    [#3735] fixed compilation error

--- nes-runtime/src/Execution/Operators/Relational/Join/BatchJoinProbe.cpp
+++ nes-runtime/src/Execution/Operators/Relational/Join/BatchJoinProbe.cpp
@@ -21,7 +21,7 @@
 #include <Nautilus/Interface/FunctionCall.hpp>
 #include <Nautilus/Interface/Hash/HashFunction.hpp>
 #include <Nautilus/Interface/HashMap/ChainedHashMap/ChainedHashMapRef.hpp>
-#include <Nautilus/Interface/List/ListRef.hpp>
+#include <Nautilus/Interface/SequentialData/SequentialDataRef.hpp>
 #include <utility>
 
 namespace NES::Runtime::Execution::Operators {
