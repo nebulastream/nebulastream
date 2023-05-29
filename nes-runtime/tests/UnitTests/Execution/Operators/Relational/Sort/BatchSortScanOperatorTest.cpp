33a356a3cc95b2a4f006e752e3109069ec901018
Adrian Michalke <adrian.michalke@yahoo.de>
2023-05-29 09:44:43 +0200

    [#3773] Random values (const-seed) to sort

--- nes-runtime/tests/UnitTests/Execution/Operators/Relational/Sort/BatchSortScanOperatorTest.cpp
+++ nes-runtime/tests/UnitTests/Execution/Operators/Relational/Sort/BatchSortScanOperatorTest.cpp
@@ -27,6 +27,8 @@
 #include <Util/Logger/Logger.hpp>
 #include <gtest/gtest.h>
 #include <memory>
+#include <random>
+#include <tuple>
 
 namespace NES::Runtime::Execution::Operators {
 
@@ -51,8 +53,9 @@ void fillState(uint64_t numberOfRecord, std::shared_ptr<BatchSortOperatorHandler
     auto state = handler->getState();
     auto entrySize = sizeof(TypeParam);
     auto stateRef = Nautilus::Interface::PagedVectorRef(Value<MemRef>((int8_t*) state), entrySize);
+    std::mt19937 randomGenerator(42);
     for (size_t j = 0; j < numberOfRecord; j++) {
-        Value<> val((TypeParam) (numberOfRecord - j));
+        Value<> val((TypeParam) (randomGenerator()));
         auto entry = stateRef.allocateEntry();
         entry.store(val);
     }
@@ -65,21 +68,24 @@ TYPED_TEST_SUITE(BatchSortScanOperatorTest, TestTypes);
  * @brief Tests if the sort operator collects records with multiple fields
  */
 TYPED_TEST(BatchSortScanOperatorTest, SortOperatorMultipleFieldsTest) {
+    using cType = typename TypeParam::first_type;
+    using nType = typename TypeParam::second_type;
+
     std::shared_ptr<BufferManager> bm = std::make_shared<BufferManager>();
     std::shared_ptr<WorkerContext> wc = std::make_shared<WorkerContext>(0, bm, 100);
 
     auto numberOfRecords = 100;
-    auto entrySize = sizeof(TypeParam);
+    auto entrySize = sizeof(cType);
     auto handler = std::make_shared<BatchSortOperatorHandler>(entrySize);
     auto pipelineContext = MockedPipelineExecutionContext({handler});
     handler->setup(pipelineContext);
-    fillState<TypeParam>(numberOfRecords, handler);
+    fillState<cType>(numberOfRecords, handler);
 
-    auto type = Util::getPhysicalTypePtr<TypeParam>();
+    auto type = Util::getPhysicalTypePtr<cType>();
     auto dataTypes = std::vector<PhysicalTypePtr>{type};
     std::vector<Record::RecordFieldIdentifier> fieldNames = {"f1"};
 
-    auto sortScanOperator = BatchSortScan(0, fieldNames, dataTypes);
+    auto sortScanOperator = BatchSortScan(0, fieldNames, dataTypes, {0});
     auto collector = std::make_shared<CollectOperator>();
     sortScanOperator.setChild(collector);
     auto ctx = ExecutionContext(Value<MemRef>(nullptr), Value<MemRef>((int8_t*) &pipelineContext));
@@ -90,9 +96,14 @@ TYPED_TEST(BatchSortScanOperatorTest, SortOperatorMultipleFieldsTest) {
 
     ASSERT_EQ(collector->records.size(), 100);
     ASSERT_EQ(collector->records[0].numberOfFields(), 1);
+
     // Records should be in ascending order
-    for (int i = 0; i < numberOfRecords; i++) {
-        ASSERT_EQ(collector->records[i].read("f1"), i + 1);
+    auto prev = collector->records[0].read("f1").as<nType>();
+    for (int i = 1; i < numberOfRecords; i++){
+        auto cur = collector->records[i].read("f1").as<nType>();
+        std::cout << prev << " " << cur << std::endl;
+        ASSERT_LE(prev, cur);
+        prev = cur;
     }
 }
 
