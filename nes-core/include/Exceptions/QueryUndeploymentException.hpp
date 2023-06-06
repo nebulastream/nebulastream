4c69e7a5e689cc7c6eea7f612c5df4e8f8ebb74a
fkuzun <felix.s.lang@campus.tu-berlin.de>
2023-06-06 16:31:10 +0000

    [#3817] added query undeployment exception

--- nes-core/include/Exceptions/QueryUndeploymentException.hpp
+++ nes-core/include/Exceptions/QueryUndeploymentException.hpp
@@ -15,16 +15,43 @@
 #ifndef NES_CORE_INCLUDE_EXCEPTIONS_QUERYUNDEPLOYMENTEXCEPTION_HPP_
 #define NES_CORE_INCLUDE_EXCEPTIONS_QUERYUNDEPLOYMENTEXCEPTION_HPP_
 
+#include <Common/Identifiers.hpp>
 #include <Exceptions/RequestExecutionException.hpp>
+#include <optional>
 #include <stdexcept>
 #include <string>
 
 namespace NES {
+
+/**
+ * @brief this exception indicates an error during the undeployment of a reqeust
+ */
 class QueryUndeploymentException : public std::runtime_error, public RequestExecutionException {
   public:
+    /**
+     * @brief constructor
+     * @param message message containing information about the error
+     */
     explicit QueryUndeploymentException(const std::string& message);
 
+    /**
+     * @brief constructor
+     * @param sharedQueryId the shared query id of the query that was being undeployed when the exception occurred
+     * @param message message containing information about the error
+     */
+    QueryUndeploymentException(SharedQueryId sharedQueryId, const std::string& message);
+
+    /**
+     * @brief get the shared query id of the query that was to be undeployed, if it is known
+     * @return: an optional containing the shared query id or nullopt if it was no supplies at request creation
+     */
+    std::optional<SharedQueryId> getSharedQueryId();
+
     const char* what() const noexcept override;
+
+  private:
+    std::optional<SharedQueryId> sharedQueryId;
+
 };
 }// namespace NES
 #endif// NES_CORE_INCLUDE_EXCEPTIONS_QUERYUNDEPLOYMENTEXCEPTION_HPP_
