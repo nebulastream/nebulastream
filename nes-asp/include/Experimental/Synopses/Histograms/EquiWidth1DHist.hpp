a70efa393902ff0f56aff61ddc92ae9105fcdced
Nils Schubert <n.schubert.1@tu-berlin.de>
2023-06-08 16:19:02 +0200

    [#3784] Added comments

--- nes-asp/include/Experimental/Synopses/Histograms/EquiWidth1DHist.hpp
+++ nes-asp/include/Experimental/Synopses/Histograms/EquiWidth1DHist.hpp
@@ -20,10 +20,15 @@
 #include <Execution/Expressions/ReadFieldExpression.hpp>
 
 namespace NES::ASP {
+
+/**
+ * @brief Implementation of a 1-D equi-width histogram
+ */
 class EquiWidth1DHist : public AbstractSynopsis {
-
 public:
-
+    /**
+     * @brief This class acts as a simple storage container for the local state, which contains the memref to the bins
+     */
     class LocalBinsOperatorState : public Runtime::Execution::Operators::OperatorState {
       public:
         explicit LocalBinsOperatorState(const Nautilus::Interface::Fixed2DArrayRef& binsRef) : bins(binsRef) {}
@@ -31,19 +36,57 @@ public:
         Nautilus::Interface::Fixed2DArrayRef bins;
     };
 
+    /**
+     * @brief Constructor for an equi-width-1d histogram
+     * @param aggregationConfig
+     * @param entrySize
+     * @param minValue
+     * @param maxValue
+     * @param numberOfBins
+     * @param lowerBinBoundString
+     * @param upperBinBoundString
+     * @param readBinDimension
+     */
     EquiWidth1DHist(Parsing::SynopsisAggregationConfig& aggregationConfig, const uint64_t entrySize,
                     const int64_t minValue, const int64_t maxValue, const uint64_t numberOfBins,
                     const std::string& lowerBinBoundString, const std::string& upperBinBoundString,
                     std::unique_ptr<Runtime::Execution::Expressions::ReadFieldExpression> readBinDimension);
 
+    /**
+     * @brief Adds the record to the histogram, by first calculating the position and then calling the aggregation function
+     * @param handlerIndex
+     * @param ctx
+     * @param record
+     * @param pState
+     */
     void addToSynopsis(uint64_t handlerIndex, Runtime::Execution::ExecutionContext &ctx, Nautilus::Record record,
                        Runtime::Execution::Operators::OperatorState *pState) override;
 
+    /**
+     * @brief Performs the lower operation on all bins and writes each bin into a record
+     * @param handlerIndex
+     * @param ctx
+     * @param bufferManager
+     * @return Vector of TupleBuffers, which contain the records of the bins
+     */
     std::vector<Runtime::TupleBuffer> getApproximate(uint64_t handlerIndex, Runtime::Execution::ExecutionContext &ctx,
                                                      Runtime::BufferManagerPtr bufferManager) override;
 
+    /**
+     * @brief Sets the histogram up, by calling the setup method of the operator handler, as well as resetting the
+     * values in the bins with the aggregation function
+     * @param handlerIndex
+     * @param ctx
+     */
     void setup(uint64_t handlerIndex, Runtime::Execution::ExecutionContext &ctx) override;
 
+    /**
+     * @brief Allows the histogram to store something in the state of the synopsis operator, for example a memref to bins
+     * @param handlerIndex
+     * @param op
+     * @param ctx
+     * @param buffer
+     */
     void storeLocalOperatorState(uint64_t handlerIndex, const Runtime::Execution::Operators::Operator* op,
                                  Runtime::Execution::ExecutionContext &ctx,
                                  Runtime::Execution::RecordBuffer buffer) override;
