0a5750177e82959c868e5b21111c6cc85863ba00
Nils Schubert <n.schubert.1@tu-berlin.de>
2023-06-12 12:04:18 +0200

    [#3779] Checkpoint

--- nes-asp/tests/UnitTests/Experimental/Synopses/RandomSampleWithoutReplacementTest.cpp
+++ nes-asp/tests/UnitTests/Experimental/Synopses/RandomSampleWithoutReplacementTest.cpp
@@ -66,6 +66,12 @@ class RandomSampleWithoutReplacementTest : public Testing::NESBaseTest {
         executionContext = std::make_unique<Runtime::Execution::ExecutionContext>(
             Nautilus::Value<Nautilus::MemRef>((int8_t*) workerContext.get()),
             Nautilus::Value<Nautilus::MemRef>((int8_t*) pipelineContext.get()));
+        inputSchema = Schema::create(Schema::MemoryLayoutType::ROW_LAYOUT)
+            ->addField(idString, BasicType::INT64)
+            ->addField(aggregationString, BasicType::FLOAT64)
+            ->addField(timestampFieldName, BasicType::UINT64);
+
+        entrySize = inputSchema->getSchemaSizeInBytes();
     }
 
     Runtime::BufferManagerPtr bufferManager;
@@ -77,9 +83,11 @@ class RandomSampleWithoutReplacementTest : public Testing::NESBaseTest {
     const std::string timestampFieldName = "ts";
     const uint64_t numberOfSamples = 3;
     const uint64_t minValue = 0;
+    const uint64_t handlerIndex = 0;
     const uint64_t maxValue = 5;
 
-    const uint64_t handlerIndex = 0;
+    uint64_t entrySize;
+    SchemaPtr inputSchema;
     std::shared_ptr<RandomSampleWithoutReplacementOperatorHandler> opHandler;
     std::unique_ptr<Runtime::Execution::ExecutionContext> executionContext;
     Runtime::WorkerContextPtr workerContext;
@@ -117,12 +125,6 @@ std::vector<Nautilus::Record> getInputData(Schema& inputSchema) {
 
 TEST_F(RandomSampleWithoutReplacementTest, sampleTestCount) {
     auto aggregationType = Parsing::Aggregation_Type::COUNT;
-    const auto entrySize = sizeof(uint64_t);
-
-    const auto inputSchema = Schema::create(Schema::MemoryLayoutType::ROW_LAYOUT)
-        ->addField(idString, BasicType::UINT64)
-        ->addField(aggregationString, BasicType::INT64)
-        ->addField(timestampFieldName, BasicType::UINT64);
     auto outputSchema = Benchmarking::getOutputSchemaFromTypeAndInputSchema(aggregationType, *inputSchema,
                                                                             idString, aggregationString,
                                                                             idString, approximateString);
@@ -144,8 +146,9 @@ TEST_F(RandomSampleWithoutReplacementTest, sampleTestCount) {
     }
 
     // Creating query keys for all histograms
-    std::vector<Nautilus::Value<>> queryKeys = {Nautilus::Value<>(0), Nautilus::Value<>(1), Nautilus::Value<>(2),
-                                                Nautilus::Value<>(3), Nautilus::Value<>(4),
+    std::vector<Nautilus::Value<>> queryKeys = {Nautilus::Value<>((int64_t)0), Nautilus::Value<>((int64_t)1),
+                                                Nautilus::Value<>((int64_t)2), Nautilus::Value<>((int64_t)3),
+                                                Nautilus::Value<>((int64_t)4),
     };
 
     auto approximateBuffers = randomSample.getApproximate(handlerIndex, *executionContext, queryKeys, bufferManager);
@@ -153,20 +156,217 @@ TEST_F(RandomSampleWithoutReplacementTest, sampleTestCount) {
                                                                                               outputSchema);
 
     // Checking for correctness
-    EXPECT_EQ(dynamicBuffer[0][idString].read<uint64_t>(), 0);
-    EXPECT_EQ(dynamicBuffer[0][approximateString].read<int64_t>(), 1);
+    EXPECT_EQ(dynamicBuffer[0][idString].read<int64_t>(), 0);
+    EXPECT_EQ(dynamicBuffer[0][approximateString].read<double_t>(), 1 + 1.0/3);
 
-    EXPECT_EQ(dynamicBuffer[1][idString].read<uint64_t>(), 1);
-    EXPECT_EQ(dynamicBuffer[1][approximateString].read<int64_t>(), 0);
+    EXPECT_EQ(dynamicBuffer[1][idString].read<int64_t>(), 1);
+    EXPECT_EQ(dynamicBuffer[1][approximateString].read<double_t>(), 0);
 
-    EXPECT_EQ(dynamicBuffer[2][idString].read<uint64_t>(), 2);
-    EXPECT_EQ(dynamicBuffer[2][approximateString].read<int64_t>(), 1);
+    EXPECT_EQ(dynamicBuffer[2][idString].read<int64_t>(), 2);
+    EXPECT_EQ(dynamicBuffer[2][approximateString].read<double_t>(), 1 + 1.0/3);
 
-    EXPECT_EQ(dynamicBuffer[3][idString].read<uint64_t>(), 3);
-    EXPECT_EQ(dynamicBuffer[3][approximateString].read<int64_t>(), 0);
+    EXPECT_EQ(dynamicBuffer[3][idString].read<int64_t>(), 3);
+    EXPECT_EQ(dynamicBuffer[3][approximateString].read<double_t>(), 0);
 
-    EXPECT_EQ(dynamicBuffer[4][idString].read<uint64_t>(), 4);
-    EXPECT_EQ(dynamicBuffer[4][approximateString].read<int64_t>(), 1);
+    EXPECT_EQ(dynamicBuffer[4][idString].read<int64_t>(), 4);
+    EXPECT_EQ(dynamicBuffer[4][approximateString].read<double_t>(), 1 + 1.0/3);
 }
+
+TEST_F(RandomSampleWithoutReplacementTest, sampleTestMin) {
+    auto aggregationType = Parsing::Aggregation_Type::MIN;
+    auto outputSchema = Benchmarking::getOutputSchemaFromTypeAndInputSchema(aggregationType, *inputSchema,
+                                                                            idString, aggregationString,
+                                                                            idString, approximateString);
+
+    // Creating aggregation config and the histogram
+    auto aggregationConfig = Parsing::SynopsisAggregationConfig::create(aggregationType, idString, aggregationString,
+                                                                        approximateString, timestampFieldName,
+                                                                        inputSchema, outputSchema);
+    RandomSampleWithoutReplacement randomSample(aggregationConfig, numberOfSamples, entrySize);
+
+    // Setting up the synopsis and creating the local operator state
+    randomSample.setup(handlerIndex, *executionContext);
+    auto sampleMemRef = Nautilus::Value<Nautilus::MemRef>((int8_t*) opHandler->getPagedVectorRef());
+    auto samples = Nautilus::Interface::PagedVectorRef(sampleMemRef, entrySize, numberOfSamples);
+
+    // Inserting records
+    for (auto& record : getInputData(*inputSchema)) {
+        randomSample.addToSynopsis(handlerIndex, *executionContext, record, nullptr);
+    }
+
+    // Creating query keys for all histograms
+    std::vector<Nautilus::Value<>> queryKeys = {Nautilus::Value<>((int64_t)0), Nautilus::Value<>((int64_t)1),
+                                                Nautilus::Value<>((int64_t)2), Nautilus::Value<>((int64_t)3),
+                                                Nautilus::Value<>((int64_t)4),
+    };
+
+    auto approximateBuffers = randomSample.getApproximate(handlerIndex, *executionContext, queryKeys, bufferManager);
+    auto dynamicBuffer = Runtime::MemoryLayouts::DynamicTupleBuffer::createDynamicTupleBuffer(approximateBuffers[0],
+                                                                                              outputSchema);
+
+    // Checking for correctness
+    EXPECT_EQ(dynamicBuffer[0][idString].read<int64_t>(), 0);
+    EXPECT_EQ(dynamicBuffer[0][approximateString].read<double_t>(), 42.0);
+
+    EXPECT_EQ(dynamicBuffer[1][idString].read<int64_t>(), 1);
+    EXPECT_EQ(dynamicBuffer[1][approximateString].read<double_t>(), std::numeric_limits<int64_t>::max());
+
+    EXPECT_EQ(dynamicBuffer[2][idString].read<int64_t>(), 2);
+    EXPECT_EQ(dynamicBuffer[2][approximateString].read<double_t>(), 1234.0);
+
+    EXPECT_EQ(dynamicBuffer[3][idString].read<int64_t>(), 3);
+    EXPECT_EQ(dynamicBuffer[3][approximateString].read<double_t>(), std::numeric_limits<int64_t>::max());
+
+    EXPECT_EQ(dynamicBuffer[4][idString].read<int64_t>(), 4);
+    EXPECT_EQ(dynamicBuffer[4][approximateString].read<double_t>(), 101.0);
+}
+
+TEST_F(RandomSampleWithoutReplacementTest, sampleTestMax) {
+    auto aggregationType = Parsing::Aggregation_Type::MAX;
+    auto outputSchema = Benchmarking::getOutputSchemaFromTypeAndInputSchema(aggregationType, *inputSchema,
+                                                                            idString, aggregationString,
+                                                                            idString, approximateString);
+
+    // Creating aggregation config and the histogram
+    auto aggregationConfig = Parsing::SynopsisAggregationConfig::create(aggregationType, idString, aggregationString,
+                                                                        approximateString, timestampFieldName,
+                                                                        inputSchema, outputSchema);
+    RandomSampleWithoutReplacement randomSample(aggregationConfig, numberOfSamples, entrySize);
+
+    // Setting up the synopsis and creating the local operator state
+    randomSample.setup(handlerIndex, *executionContext);
+    auto sampleMemRef = Nautilus::Value<Nautilus::MemRef>((int8_t*) opHandler->getPagedVectorRef());
+    auto samples = Nautilus::Interface::PagedVectorRef(sampleMemRef, entrySize, numberOfSamples);
+
+    // Inserting records
+    for (auto& record : getInputData(*inputSchema)) {
+        randomSample.addToSynopsis(handlerIndex, *executionContext, record, nullptr);
+    }
+
+    // Creating query keys for all histograms
+    std::vector<Nautilus::Value<>> queryKeys = {Nautilus::Value<>((int64_t)0), Nautilus::Value<>((int64_t)1),
+                                                Nautilus::Value<>((int64_t)2), Nautilus::Value<>((int64_t)3),
+                                                Nautilus::Value<>((int64_t)4),
+    };
+
+    auto approximateBuffers = randomSample.getApproximate(handlerIndex, *executionContext, queryKeys, bufferManager);
+    auto dynamicBuffer = Runtime::MemoryLayouts::DynamicTupleBuffer::createDynamicTupleBuffer(approximateBuffers[0],
+                                                                                              outputSchema);
+
+    // Checking for correctness
+    EXPECT_EQ(dynamicBuffer[0][idString].read<int64_t>(), 0);
+    EXPECT_EQ(dynamicBuffer[0][approximateString].read<double_t>(), 42.0);
+
+    EXPECT_EQ(dynamicBuffer[1][idString].read<int64_t>(), 1);
+    EXPECT_EQ(dynamicBuffer[1][approximateString].read<double_t>(), std::numeric_limits<int64_t>::min());
+
+    EXPECT_EQ(dynamicBuffer[2][idString].read<int64_t>(), 2);
+    EXPECT_EQ(dynamicBuffer[2][approximateString].read<double_t>(), 1234.0);
+
+    EXPECT_EQ(dynamicBuffer[3][idString].read<int64_t>(), 3);
+    EXPECT_EQ(dynamicBuffer[3][approximateString].read<double_t>(), std::numeric_limits<int64_t>::min());
+
+    EXPECT_EQ(dynamicBuffer[4][idString].read<int64_t>(), 4);
+    EXPECT_EQ(dynamicBuffer[4][approximateString].read<double_t>(), 101.0);
+}
+
+TEST_F(RandomSampleWithoutReplacementTest, sampleTestSum) {
+    auto aggregationType = Parsing::Aggregation_Type::SUM;
+    auto outputSchema = Benchmarking::getOutputSchemaFromTypeAndInputSchema(aggregationType, *inputSchema,
+                                                                            idString, aggregationString,
+                                                                            idString, approximateString);
+
+    // Creating aggregation config and the histogram
+    auto aggregationConfig = Parsing::SynopsisAggregationConfig::create(aggregationType, idString, aggregationString,
+                                                                        approximateString, timestampFieldName,
+                                                                        inputSchema, outputSchema);
+    RandomSampleWithoutReplacement randomSample(aggregationConfig, numberOfSamples, entrySize);
+
+    // Setting up the synopsis and creating the local operator state
+    randomSample.setup(handlerIndex, *executionContext);
+    auto sampleMemRef = Nautilus::Value<Nautilus::MemRef>((int8_t*) opHandler->getPagedVectorRef());
+    auto samples = Nautilus::Interface::PagedVectorRef(sampleMemRef, entrySize, numberOfSamples);
+
+    // Inserting records
+    for (auto& record : getInputData(*inputSchema)) {
+        randomSample.addToSynopsis(handlerIndex, *executionContext, record, nullptr);
+    }
+
+    // Creating query keys for all histograms
+    std::vector<Nautilus::Value<>> queryKeys = {Nautilus::Value<>((int64_t)0), Nautilus::Value<>((int64_t)1),
+                                                Nautilus::Value<>((int64_t)2), Nautilus::Value<>((int64_t)3),
+                                                Nautilus::Value<>((int64_t)4),
+    };
+
+    auto approximateBuffers = randomSample.getApproximate(handlerIndex, *executionContext, queryKeys, bufferManager);
+    auto dynamicBuffer = Runtime::MemoryLayouts::DynamicTupleBuffer::createDynamicTupleBuffer(approximateBuffers[0],
+                                                                                              outputSchema);
+
+    // Checking for correctness
+    EXPECT_EQ(dynamicBuffer[0][idString].read<int64_t>(), 0);
+    EXPECT_EQ(dynamicBuffer[0][approximateString].read<double_t>(), 56.0);
+
+    EXPECT_EQ(dynamicBuffer[1][idString].read<int64_t>(), 1);
+    EXPECT_EQ(dynamicBuffer[1][approximateString].read<double_t>(), 0);
+
+    EXPECT_EQ(dynamicBuffer[2][idString].read<int64_t>(), 2);
+    EXPECT_EQ(dynamicBuffer[2][approximateString].read<double_t>(), 1645 + 1.0/3);
+
+    EXPECT_EQ(dynamicBuffer[3][idString].read<int64_t>(), 3);
+    EXPECT_EQ(dynamicBuffer[3][approximateString].read<double_t>(), 0);
+
+    EXPECT_EQ(dynamicBuffer[4][idString].read<int64_t>(), 4);
+    EXPECT_EQ(dynamicBuffer[4][approximateString].read<double_t>(), 134 + 2.0/3);
+}
+
+TEST_F(RandomSampleWithoutReplacementTest, sampleTestAverage) {
+    auto aggregationType = Parsing::Aggregation_Type::AVERAGE;
+    auto outputSchema = Benchmarking::getOutputSchemaFromTypeAndInputSchema(aggregationType, *inputSchema,
+                                                                            idString, aggregationString,
+                                                                            idString, approximateString);
+
+    // Creating aggregation config and the histogram
+    auto aggregationConfig = Parsing::SynopsisAggregationConfig::create(aggregationType, idString, aggregationString,
+                                                                        approximateString, timestampFieldName,
+                                                                        inputSchema, outputSchema);
+    RandomSampleWithoutReplacement randomSample(aggregationConfig, numberOfSamples, entrySize);
+
+    // Setting up the synopsis and creating the local operator state
+    randomSample.setup(handlerIndex, *executionContext);
+    auto sampleMemRef = Nautilus::Value<Nautilus::MemRef>((int8_t*) opHandler->getPagedVectorRef());
+    auto samples = Nautilus::Interface::PagedVectorRef(sampleMemRef, entrySize, numberOfSamples);
+
+    // Inserting records
+    for (auto& record : getInputData(*inputSchema)) {
+        randomSample.addToSynopsis(handlerIndex, *executionContext, record, nullptr);
+    }
+
+    // Creating query keys for all histograms
+    std::vector<Nautilus::Value<>> queryKeys = {Nautilus::Value<>((int64_t)0), Nautilus::Value<>((int64_t)1),
+                                                Nautilus::Value<>((int64_t)2), Nautilus::Value<>((int64_t)3),
+                                                Nautilus::Value<>((int64_t)4),
+    };
+
+    auto approximateBuffers = randomSample.getApproximate(handlerIndex, *executionContext, queryKeys, bufferManager);
+    auto dynamicBuffer = Runtime::MemoryLayouts::DynamicTupleBuffer::createDynamicTupleBuffer(approximateBuffers[0],
+                                                                                              outputSchema);
+
+    // Checking for correctness
+    EXPECT_EQ(dynamicBuffer[0][idString].read<int64_t>(), 0);
+    EXPECT_EQ(dynamicBuffer[0][approximateString].read<double_t>(), 42.0);
+
+    EXPECT_EQ(dynamicBuffer[1][idString].read<int64_t>(), 1);
+    EXPECT_EQ(dynamicBuffer[1][approximateString].read<double_t>(), 0);
+
+    EXPECT_EQ(dynamicBuffer[2][idString].read<int64_t>(), 2);
+    EXPECT_EQ(dynamicBuffer[2][approximateString].read<double_t>(), 1234.0);
+
+    EXPECT_EQ(dynamicBuffer[3][idString].read<int64_t>(), 3);
+    EXPECT_EQ(dynamicBuffer[3][approximateString].read<double_t>(), 0);
+
+    EXPECT_EQ(dynamicBuffer[4][idString].read<int64_t>(), 4);
+    EXPECT_EQ(dynamicBuffer[4][approximateString].read<double_t>(), 101.0);
+}
+
 
 } // namespace NES::ASP
