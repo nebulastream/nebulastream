193ae356dc8a9de8d6e314ee2af80ac7b5044fde
Niklas Tantow <n.tantow@campus.tu-berlin.de>
2023-06-14 15:29:59 +0200

    #3224 added class for UniformIntDistributions

---
+++ nes-benchmark/include/DataGeneration/NEXMarkGeneration/UniformIntDistributions.hpp
@@ -0,0 +1,152 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#ifndef NES_UNIFORMINTDISTRIBUTIONS_HPP
+#define NES_UNIFORMINTDISTRIBUTIONS_HPP
+
+#include <cstdint>
+#include <random>
+
+namespace NES::Benchmark::DataGeneration::NEXMarkGeneration {
+class UniformIntDistributions {
+  public:
+    /**
+     * @brief constructor of UniformIntDistribution - initializes generator with seed
+     */
+    UniformIntDistributions();
+
+    /**
+     * @brief generates a random boolean
+     * @return either 0 or 1
+     */
+    uint8_t generateRandomBoolean();
+
+    /**
+     * @brief generates a random reserve (value between 1000 and 2000)
+     * @return random reserve
+     */
+    uint16_t generateRandomReserve();
+
+    /**
+     * @brief generates a random category (value between 0 and 302)
+     * @return random category
+     */
+    uint16_t generateRandomCategory();
+
+    /**
+     * @brief generates a random quantity (value between 1 and 10)
+     * @return random quantity
+     */
+    uint8_t generateRandomQuantity();
+
+    /**
+     * @brief generates the index for a random firstname
+     * @return random index
+     */
+    uint16_t generateRandomFirstname();
+
+    /**
+     * @brief generates the index for a random lastname
+     * @return random index
+     */
+    uint16_t generateRandomLastname();
+
+    /**
+     * @brief generates the index for a random email
+     * @return random index
+     */
+    uint8_t generateRandomEmail();
+    /**
+     * @brief generates the index for a random city
+     * @return random index
+     */
+    uint16_t generateRandomCity();
+
+    /**
+     * @brief generates the index for a random country
+     * @return random index
+     */
+    uint8_t generateRandomCountry();
+
+    /**
+     * @brief generates the index for a random province
+     * @return random index
+     */
+    uint8_t generateRandomProvince();
+
+    /**
+     * @brief generates the index for a random education
+     * @return random index
+     */
+    uint8_t generateRandomEducation();
+
+    /**
+     * @brief generates a random zipcode (value between 1 and 99999)
+     * @return random zipcode
+     */
+    uint32_t generateRandomZipcode();
+
+    /**
+     * @brief generates a random number between 1 and 99
+     * @return random number
+     */
+    uint8_t generateRandomOneHundred();
+
+    /**
+     * @brief generates a random credit card (value between 1000 and 9999)
+     * @return random credit card
+     */
+    uint16_t generateRandomCreditcard();
+
+    /**
+     * @brief generates a random income (value between 40000 and 69999)
+     * @return random income
+     */
+    uint32_t generateRandomIncome();
+
+    /**
+     * @brief generates a random age (value between 30 and 44)
+     * @return random age
+     */
+    uint8_t generateRandomAge();
+
+    /**
+     * @brief generates a random number of interests (value between 0 and 4)
+     * @return random number of interests
+     */
+    uint8_t generateRandomInterest();
+
+    /**
+     * @brief generates a random phone number prefix (value between 10 and 999)
+     * @return random phone number prefix
+     */
+    uint16_t generateRandomPhonePrefix();
+
+    /**
+     * @brief generates a random phone number suffix (value between 123457 and 9987653)
+     * @return random phone number suffix
+     */
+    uint32_t generateRandomPhoneSuffix();
+
+    /**
+     * @brief decides whether the country is the US or random country from the country vector
+     * @return true or false
+     */
+    bool isCountryUS();
+  private:
+    std::mt19937 generator;
+};
+} //namespace NES::Benchmark::DataGeneration::NEXMarkGeneration
+
+#endif//NES_UNIFORMINTDISTRIBUTIONS_HPP
---
+++ nes-benchmark/src/DataGeneration/NEXMarkGeneration/UniformIntDistributions.cpp
@@ -0,0 +1,123 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include <DataGeneration/NEXMarkGeneration/UniformIntDistributions.hpp>
+#include <DataGeneration/NEXMarkGeneration/PersonDataPool.hpp>
+
+namespace NES::Benchmark::DataGeneration::NEXMarkGeneration {
+
+// using seed to generate a predictable sequence of values for deterministic behavior
+UniformIntDistributions::UniformIntDistributions() : generator(42) {}
+
+uint8_t UniformIntDistributions::generateRandomBoolean() {
+    std::uniform_int_distribution<uint8_t> uniformBooleanDistribution(0, 1);
+    return uniformBooleanDistribution(generator);
+}
+
+uint16_t UniformIntDistributions::generateRandomReserve() {
+    std::uniform_int_distribution<uint16_t> uniformReserveDistribution(1000, 2000);
+    return uniformReserveDistribution(generator);
+}
+
+uint16_t UniformIntDistributions::generateRandomCategory() {
+    std::uniform_int_distribution<uint16_t> uniformCategoryDistribution(0, 302);
+    return uniformCategoryDistribution(generator);
+}
+
+uint8_t UniformIntDistributions::generateRandomQuantity() {
+    std::uniform_int_distribution<uint8_t> uniformQuantityDistribution(1, 10);
+    return uniformQuantityDistribution(generator);
+}
+
+uint16_t UniformIntDistributions::generateRandomFirstname() {
+    std::uniform_int_distribution<uint16_t> uniformFirstnameDistribution(0, PersonDataPool().firstnames.size() - 1);
+    return uniformFirstnameDistribution(generator);
+}
+
+uint16_t UniformIntDistributions::generateRandomLastname() {
+    std::uniform_int_distribution<uint16_t> uniformLastnameDistribution(0, PersonDataPool().lastnames.size() - 1);
+    return uniformLastnameDistribution(generator);
+}
+
+uint8_t UniformIntDistributions::generateRandomEmail() {
+    std::uniform_int_distribution<uint8_t> uniformEmailDistribution(0, PersonDataPool().emails.size() - 1);
+    return uniformEmailDistribution(generator);
+}
+
+uint16_t UniformIntDistributions::generateRandomCity() {
+    std::uniform_int_distribution<uint16_t> uniformCityDistribution(0, PersonDataPool().cities.size() - 1);
+    return uniformCityDistribution(generator);
+}
+
+uint8_t UniformIntDistributions::generateRandomCountry() {
+    std::uniform_int_distribution<uint8_t> uniformCountryDistribution(0, PersonDataPool().countries.size() - 1);
+    return uniformCountryDistribution(generator);
+}
+
+uint8_t UniformIntDistributions::generateRandomProvince() {
+    std::uniform_int_distribution<uint8_t> uniformProvinceDistribution(0, PersonDataPool().provinces.size() - 1);
+    return uniformProvinceDistribution(generator);
+}
+
+uint8_t UniformIntDistributions::generateRandomEducation() {
+    std::uniform_int_distribution<uint8_t> uniformEducationDistribution(0, PersonDataPool().education.size() - 1);
+    return uniformEducationDistribution(generator);
+}
+
+uint32_t UniformIntDistributions::generateRandomZipcode() {
+    std::uniform_int_distribution<uint32_t> uniformZipcodeDistribution(1, 99999);
+    return uniformZipcodeDistribution(generator);
+}
+
+uint8_t UniformIntDistributions::generateRandomOneHundred() {
+    std::uniform_int_distribution<uint8_t> uniformHundredDistribution(1, 99);
+    return uniformHundredDistribution(generator);
+}
+
+uint16_t UniformIntDistributions::generateRandomCreditcard() {
+    std::uniform_int_distribution<uint16_t> uniformCreditcardDistribution(1000, 9999);
+    return uniformCreditcardDistribution(generator);
+}
+
+uint32_t UniformIntDistributions::generateRandomIncome() {
+    std::uniform_int_distribution<uint32_t> uniformIncomeDistribution(40000, 69999);
+    return uniformIncomeDistribution(generator);
+}
+
+uint8_t UniformIntDistributions::generateRandomAge() {
+    std::uniform_int_distribution<uint8_t> uniformAgeDistribution(30, 44);
+    return uniformAgeDistribution(generator);
+}
+
+uint8_t UniformIntDistributions::generateRandomInterest() {
+    std::uniform_int_distribution<uint8_t> uniformInterestDistribution(0, 4);
+    return uniformInterestDistribution(generator);
+}
+
+uint16_t UniformIntDistributions::generateRandomPhonePrefix() {
+    std::uniform_int_distribution<uint16_t> uniformPhonePrefixDistribution(10, 999);
+    return uniformPhonePrefixDistribution(generator);
+}
+
+uint32_t UniformIntDistributions::generateRandomPhoneSuffix() {
+    std::uniform_int_distribution<uint32_t> uniformPhoneSuffixDistribution(123457, 9987653);
+    return uniformPhoneSuffixDistribution(generator);
+}
+
+bool UniformIntDistributions::isCountryUS() {
+    std::uniform_int_distribution<uint8_t> uniformCountryUSDistribution(0, 3);
+    return uniformCountryUSDistribution(generator) != 0;
+}
+
+} //namespace NES::Benchmark::DataGeneration::NEXMarkGeneration
