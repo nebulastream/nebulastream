1701364ace411513aa76bf12ad095aebe28b8b80
Adrian Michalke <adrian.michalke@yahoo.de>
2023-06-26 10:38:44 +0200

    [#3773] docs: add licensing DuckDB

--- nes-runtime/include/Execution/Operators/Relational/Sort/BatchSortEncode.hpp
+++ nes-runtime/include/Execution/Operators/Relational/Sort/BatchSortEncode.hpp
@@ -12,6 +12,31 @@
     limitations under the License.
 */
 
+/**
+ * The function RadixSortMSD is taken from DuckDB (MIT license):
+ * MIT License
+ *
+ * Copyright 2018-2023 Stichting DuckDB Foundation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #ifndef NES_RUNTIME_INCLUDE_EXECUTION_OPERATORS_RELATIONAL_BATCHSORTENCODE_HPP_
 #define NES_RUNTIME_INCLUDE_EXECUTION_OPERATORS_RELATIONAL_BATCHSORTENCODE_HPP_
 
@@ -27,7 +52,7 @@ namespace NES::Runtime::Execution::Operators {
 
 /**
  * @brief Our radix sort sorts in chunks of bytes. To perform our radix sort, we need to encode the sort column.
- * Inspired by DuckDB's radix sort implementation.
+ * Encoding is taken from DuckDB with modification (C++11 features and adaption to Nautilus)
  * https://github.com/duckdb/duckdb/blob/e9f6ba553a108769f7c34a6af9354f3044e338e8/src/include/duckdb/common/radix.hpp
  */
 
