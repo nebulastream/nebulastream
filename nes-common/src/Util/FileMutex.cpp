fd2243861f44e73466a507536de1745873ffa5a6
Florentine Seuffert <floren-tine@web.de>
2023-06-29 15:46:37 +0200

    [#3903] adds variable declaration in NES_ERROR_OR_THROW_RUNTIME definition

--- nes-common/src/Util/FileMutex.cpp
+++ nes-common/src/Util/FileMutex.cpp
@@ -30,9 +30,6 @@ FileMutex::FileMutex(const std::string filePath) : fileName(filePath) {
     if (fd == -1 && errno == EEXIST) {
         fd = open(filePath.c_str(), O_RDWR);
     }
-/*    std::stringstream message;
-    message << "Invalid file " << filePath << " " << strerror(errno);
-    NES_ASSERT2_FMT(fd != -1, message.str());*/
     NES_ASSERT2_FMT(fd != -1, "Invalid file " << filePath << " " << strerror(errno));
 }
 
