48c0b16531c282ae9aae886a31f48fdbbd5615b0
hoangmip <ph.hoangmi@gmail.com>
2023-07-03 11:49:30 +0000

    #3910 added adrians review

--- nes-runtime/include/Execution/Operators/Relational/PythonUDF/MapPythonUDF.hpp
+++ nes-runtime/include/Execution/Operators/Relational/PythonUDF/MapPythonUDF.hpp
@@ -1,19 +1,19 @@
 /*
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
 
-    https://www.apache.org/licenses/LICENSE-2.0
+        https://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
 */
-#ifdef MIS_PYTHON_UDF_ENABLED
-#ifndef NES_MAPPYTHONUDF_HPP
-#define NES_MAPPYTHONUDF_HPP
+#ifdef NAUTILUS_PYTHON_UDF_ENABLED
+#ifndef NES_RUNTIME_INCLUDE_EXECUTION_OPERATORS_RELATIONAL_PYTHONUDF_MAPPYTHONUDF_HPP_
+#define NES_RUNTIME_INCLUDE_EXECUTION_OPERATORS_RELATIONAL_PYTHONUDF_MAPPYTHONUDF_HPP_
 
 #include <API/AttributeField.hpp>
 #include <API/Schema.hpp>
@@ -44,5 +44,5 @@ class MapPythonUDF  : public ExecutableOperator {
 
 }// namespace NES::Runtime::Execution::Operators
 
-#endif //NES_MAPPYTHONUDF_HPP
-#endif
+#endif //NES_RUNTIME_INCLUDE_EXECUTION_OPERATORS_RELATIONAL_PYTHONUDF_MAPPYTHONUDF_HPP_
+#endif //NAUTILUS_PYTHON_UDF_ENABLED
