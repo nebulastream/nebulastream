276869497e8c7a1f881a5dc0fd4f63b9b0170b6f
maseiler <matthiasseiler@outlook.com>
2023-07-19 10:29:46 +0200

    Add read benchmark

--- nes-benchmark/src/MicroBenchmarks/MemoryLayout/BenchmarkMemoryLayout.cpp
+++ nes-benchmark/src/MicroBenchmarks/MemoryLayout/BenchmarkMemoryLayout.cpp
@@ -93,6 +93,8 @@ BenchmarkMemoryLayout::BenchmarkMemoryLayout(size_t numBuffers, size_t bufferSiz
     o << std::setw(2) << arguments << std::endl;
 }
 
+// TODO refactor. Read benchmark basically copypasta of write benchmarks with additional call
+
 std::tuple<uint8_t> RECORD_1 = {65};
 template<class... Args>
 void BM_MemoryLayout_Write_1(benchmark::State& state, Args&&... args) {
@@ -107,6 +109,7 @@ void BM_MemoryLayout_Write_1(benchmark::State& state, Args&&... args) {
     double totalTime = 0;
     for (auto _ : state) {
         auto buffer = CompressedDynamicTupleBuffer(memoryLayoutPtr, bench.bufferManager->getBufferBlocking());
+        buffer.readRecordFromBuffer<uint8_t>(0);
         auto start = std::chrono::high_resolution_clock::now();
         for (size_t i = 0; i < buffer.getCapacity() - 1; i++)
             buffer.pushRecordToBuffer(RECORD_1);
@@ -224,60 +227,247 @@ void BM_MemoryLayout_Write_16(benchmark::State& state, Args&&... args) {
     state.counters["TotalTime"] = (double) totalTime / (double) state.iterations();
 }
 
+template<class... Args>
+void BM_MemoryLayout_Read_1(benchmark::State& state, Args&&... args) {
+    // parse arguments
+    std::tuple args_tuple = std::make_tuple(std::move(args)...);
+    size_t numBuffers = std::get<0>(args_tuple);
+    size_t bufferSize = std::get<1>(args_tuple);
+    size_t isRowLayout = std::get<2>(args_tuple);
+
+    MemoryLayoutPtr memoryLayoutPtr = createMemoryLayout(isRowLayout, 1, bufferSize);
+    BenchmarkMemoryLayout bench = BenchmarkMemoryLayout(numBuffers, bufferSize);
+    double totalTime = 0;
+    for (auto _ : state) {
+        auto buffer = CompressedDynamicTupleBuffer(memoryLayoutPtr, bench.bufferManager->getBufferBlocking());
+        buffer.readRecordFromBuffer<uint8_t>(0);
+        for (size_t i = 0; i < buffer.getCapacity() - 1; i++)
+            buffer.pushRecordToBuffer(RECORD_1);
+
+        auto start = std::chrono::high_resolution_clock::now();
+        for (size_t i = 0; i < buffer.getCapacity() - 1; i++)
+            buffer.readRecordFromBuffer<uint8_t>(i);
+        auto end = std::chrono::high_resolution_clock::now();
+        totalTime += std::chrono::duration_cast<std::chrono::duration<double>>(end - start).count();
+    }
+    state.counters["TotalTime"] = (double) totalTime / (double) state.iterations();
+}
+
+template<class... Args>
+void BM_MemoryLayout_Read_2(benchmark::State& state, Args&&... args) {
+    // parse arguments
+    std::tuple args_tuple = std::make_tuple(std::move(args)...);
+    size_t numBuffers = std::get<0>(args_tuple);
+    size_t bufferSize = std::get<1>(args_tuple);
+    size_t isRowLayout = std::get<2>(args_tuple);
+
+    MemoryLayoutPtr memoryLayoutPtr = createMemoryLayout(isRowLayout, 2, bufferSize);
+    BenchmarkMemoryLayout bench = BenchmarkMemoryLayout(numBuffers, bufferSize);
+    double totalTime = 0;
+    for (auto _ : state) {
+        auto buffer = CompressedDynamicTupleBuffer(memoryLayoutPtr, bench.bufferManager->getBufferBlocking());
+        for (size_t i = 0; i < buffer.getCapacity() - 1; i++)
+            buffer.pushRecordToBuffer(RECORD_2);
+
+        auto start = std::chrono::high_resolution_clock::now();
+        for (size_t i = 0; i < buffer.getCapacity() - 1; i++)
+            buffer.readRecordFromBuffer<uint8_t, uint8_t>(i);
+        auto end = std::chrono::high_resolution_clock::now();
+        totalTime += std::chrono::duration_cast<std::chrono::duration<double>>(end - start).count();
+    }
+    state.counters["TotalTime"] = (double) totalTime / (double) state.iterations();
+}
+
+template<class... Args>
+void BM_MemoryLayout_Read_4(benchmark::State& state, Args&&... args) {
+    // parse arguments
+    std::tuple args_tuple = std::make_tuple(std::move(args)...);
+    size_t numBuffers = std::get<0>(args_tuple);
+    size_t bufferSize = std::get<1>(args_tuple);
+    size_t isRowLayout = std::get<2>(args_tuple);
+
+    MemoryLayoutPtr memoryLayoutPtr = createMemoryLayout(isRowLayout, 4, bufferSize);
+    BenchmarkMemoryLayout bench = BenchmarkMemoryLayout(numBuffers, bufferSize);
+    double totalTime = 0;
+    for (auto _ : state) {
+        auto buffer = CompressedDynamicTupleBuffer(memoryLayoutPtr, bench.bufferManager->getBufferBlocking());
+        for (size_t i = 0; i < buffer.getCapacity() - 1; i++)
+            buffer.pushRecordToBuffer(RECORD_4);
+
+        auto start = std::chrono::high_resolution_clock::now();
+        for (size_t i = 0; i < buffer.getCapacity() - 1; i++)
+            buffer.readRecordFromBuffer<uint8_t, uint8_t, uint8_t, uint8_t>(i);
+        auto end = std::chrono::high_resolution_clock::now();
+        totalTime += std::chrono::duration_cast<std::chrono::duration<double>>(end - start).count();
+    }
+    state.counters["TotalTime"] = (double) totalTime / (double) state.iterations();
+}
+
+template<class... Args>
+void BM_MemoryLayout_Read_8(benchmark::State& state, Args&&... args) {
+    // parse arguments
+    std::tuple args_tuple = std::make_tuple(std::move(args)...);
+    size_t numBuffers = std::get<0>(args_tuple);
+    size_t bufferSize = std::get<1>(args_tuple);
+    size_t isRowLayout = std::get<2>(args_tuple);
+
+    MemoryLayoutPtr memoryLayoutPtr = createMemoryLayout(isRowLayout, 8, bufferSize);
+    BenchmarkMemoryLayout bench = BenchmarkMemoryLayout(numBuffers, bufferSize);
+    double totalTime = 0;
+    for (auto _ : state) {
+        auto buffer = CompressedDynamicTupleBuffer(memoryLayoutPtr, bench.bufferManager->getBufferBlocking());
+        for (size_t i = 0; i < buffer.getCapacity() - 1; i++)
+            buffer.pushRecordToBuffer(RECORD_8);
+
+        auto start = std::chrono::high_resolution_clock::now();
+        for (size_t i = 0; i < buffer.getCapacity() - 1; i++)
+            buffer.readRecordFromBuffer<uint8_t, uint8_t, uint8_t, uint8_t, uint8_t, uint8_t, uint8_t, uint8_t>(i);
+        auto end = std::chrono::high_resolution_clock::now();
+        totalTime += std::chrono::duration_cast<std::chrono::duration<double>>(end - start).count();
+    }
+    state.counters["TotalTime"] = (double) totalTime / (double) state.iterations();
+}
+
+template<class... Args>
+void BM_MemoryLayout_Read_16(benchmark::State& state, Args&&... args) {
+    // parse arguments
+    std::tuple args_tuple = std::make_tuple(std::move(args)...);
+    size_t numBuffers = std::get<0>(args_tuple);
+    size_t bufferSize = std::get<1>(args_tuple);
+    size_t isRowLayout = std::get<2>(args_tuple);
+
+    MemoryLayoutPtr memoryLayoutPtr = createMemoryLayout(isRowLayout, 16, bufferSize);
+    BenchmarkMemoryLayout bench = BenchmarkMemoryLayout(numBuffers, bufferSize);
+    double totalTime = 0;
+    for (auto _ : state) {
+        auto buffer = CompressedDynamicTupleBuffer(memoryLayoutPtr, bench.bufferManager->getBufferBlocking());
+        for (size_t i = 0; i < buffer.getCapacity() - 1; i++)
+            buffer.pushRecordToBuffer(RECORD_16);
+
+        auto start = std::chrono::high_resolution_clock::now();
+        for (size_t i = 0; i < buffer.getCapacity() - 1; i++)
+            buffer.readRecordFromBuffer<uint8_t,
+                                        uint8_t,
+                                        uint8_t,
+                                        uint8_t,
+                                        uint8_t,
+                                        uint8_t,
+                                        uint8_t,
+                                        uint8_t,
+                                        uint8_t,
+                                        uint8_t,
+                                        uint8_t,
+                                        uint8_t,
+                                        uint8_t,
+                                        uint8_t,
+                                        uint8_t,
+                                        uint8_t>(i);
+        auto end = std::chrono::high_resolution_clock::now();
+        totalTime += std::chrono::duration_cast<std::chrono::duration<double>>(end - start).count();
+    }
+    state.counters["TotalTime"] = (double) totalTime / (double) state.iterations();
+}
+
 // ------------------------------------------------------------
 // Benchmark Parameters
 // have to be changed by hand (except compression algorithm) ...
 // ------------------------------------------------------------
 // TODO? use environment variables for all variables?
-size_t NUM_BENCHMARK_ITERATIONS = 1000;
+size_t NUM_BENCHMARK_ITERATIONS = 100; // TODO 1000
 size_t NUM_BUFFERS = 1;
 size_t BUFFER_SIZE = std::atoll(std::getenv("BUF_SIZE"));
 
 // Note: only change above values and none from below!
 
 // ------------------------------------------------------------
-// RowLayout
+// RowLayout, Write
 // ------------------------------------------------------------
-BENCHMARK_CAPTURE(BM_MemoryLayout_Write_1, RowLayout1, NUM_BUFFERS, BUFFER_SIZE, true)
+BENCHMARK_CAPTURE(BM_MemoryLayout_Write_1, RowWrite1, NUM_BUFFERS, BUFFER_SIZE, true)
     ->UseManualTime()
     ->Iterations(NUM_BENCHMARK_ITERATIONS);
 
-BENCHMARK_CAPTURE(BM_MemoryLayout_Write_2, RowLayout2, NUM_BUFFERS, BUFFER_SIZE, true)
+BENCHMARK_CAPTURE(BM_MemoryLayout_Write_2, RowWrite2, NUM_BUFFERS, BUFFER_SIZE, true)
     ->UseManualTime()
     ->Iterations(NUM_BENCHMARK_ITERATIONS);
 
-BENCHMARK_CAPTURE(BM_MemoryLayout_Write_4, RowLayout4, NUM_BUFFERS, BUFFER_SIZE, true)
+BENCHMARK_CAPTURE(BM_MemoryLayout_Write_4, RowWrite4, NUM_BUFFERS, BUFFER_SIZE, true)
     ->UseManualTime()
     ->Iterations(NUM_BENCHMARK_ITERATIONS);
 
-BENCHMARK_CAPTURE(BM_MemoryLayout_Write_8, RowLayout8, NUM_BUFFERS, BUFFER_SIZE, true)
+BENCHMARK_CAPTURE(BM_MemoryLayout_Write_8, RowWrite8, NUM_BUFFERS, BUFFER_SIZE, true)
     ->UseManualTime()
     ->Iterations(NUM_BENCHMARK_ITERATIONS);
 
-BENCHMARK_CAPTURE(BM_MemoryLayout_Write_16, RowLayout16, NUM_BUFFERS, BUFFER_SIZE, true)
+BENCHMARK_CAPTURE(BM_MemoryLayout_Write_16, RowWrite16, NUM_BUFFERS, BUFFER_SIZE, true)
     ->UseManualTime()
     ->Iterations(NUM_BENCHMARK_ITERATIONS);
 
 // ------------------------------------------------------------
-// Column Layout
+// Column Layout, Write
 // ------------------------------------------------------------
-BENCHMARK_CAPTURE(BM_MemoryLayout_Write_1, ColumnLayout1, NUM_BUFFERS, BUFFER_SIZE, false)
+BENCHMARK_CAPTURE(BM_MemoryLayout_Write_1, ColWrite1, NUM_BUFFERS, BUFFER_SIZE, false)
     ->UseManualTime()
     ->Iterations(NUM_BENCHMARK_ITERATIONS);
 
-BENCHMARK_CAPTURE(BM_MemoryLayout_Write_2, ColumnLayout2, NUM_BUFFERS, BUFFER_SIZE, false)
+BENCHMARK_CAPTURE(BM_MemoryLayout_Write_2, ColWrite2, NUM_BUFFERS, BUFFER_SIZE, false)
     ->UseManualTime()
     ->Iterations(NUM_BENCHMARK_ITERATIONS);
 
-BENCHMARK_CAPTURE(BM_MemoryLayout_Write_4, ColumnLayout4, NUM_BUFFERS, BUFFER_SIZE, false)
+BENCHMARK_CAPTURE(BM_MemoryLayout_Write_4, ColWrite4, NUM_BUFFERS, BUFFER_SIZE, false)
     ->UseManualTime()
     ->Iterations(NUM_BENCHMARK_ITERATIONS);
 
-BENCHMARK_CAPTURE(BM_MemoryLayout_Write_8, ColumnLayout8, NUM_BUFFERS, BUFFER_SIZE, false)
+BENCHMARK_CAPTURE(BM_MemoryLayout_Write_8, ColWrite8, NUM_BUFFERS, BUFFER_SIZE, false)
     ->UseManualTime()
     ->Iterations(NUM_BENCHMARK_ITERATIONS);
 
-BENCHMARK_CAPTURE(BM_MemoryLayout_Write_16, ColumnLayout16, NUM_BUFFERS, BUFFER_SIZE, false)
+BENCHMARK_CAPTURE(BM_MemoryLayout_Write_16, ColWrite16, NUM_BUFFERS, BUFFER_SIZE, false)
+    ->UseManualTime()
+    ->Iterations(NUM_BENCHMARK_ITERATIONS);
+
+// ------------------------------------------------------------
+// RowLayout, Read
+// ------------------------------------------------------------
+BENCHMARK_CAPTURE(BM_MemoryLayout_Read_1, RowRead1, NUM_BUFFERS, BUFFER_SIZE, true)
+    ->UseManualTime()
+    ->Iterations(NUM_BENCHMARK_ITERATIONS);
+
+BENCHMARK_CAPTURE(BM_MemoryLayout_Read_2, RowRead2, NUM_BUFFERS, BUFFER_SIZE, true)
+    ->UseManualTime()
+    ->Iterations(NUM_BENCHMARK_ITERATIONS);
+
+BENCHMARK_CAPTURE(BM_MemoryLayout_Read_4, RowRead4, NUM_BUFFERS, BUFFER_SIZE, true)
+    ->UseManualTime()
+    ->Iterations(NUM_BENCHMARK_ITERATIONS);
+
+BENCHMARK_CAPTURE(BM_MemoryLayout_Read_8, RowRead8, NUM_BUFFERS, BUFFER_SIZE, true)
+    ->UseManualTime()
+    ->Iterations(NUM_BENCHMARK_ITERATIONS);
+
+BENCHMARK_CAPTURE(BM_MemoryLayout_Read_16, RowRead16, NUM_BUFFERS, BUFFER_SIZE, true)
+    ->UseManualTime()
+    ->Iterations(NUM_BENCHMARK_ITERATIONS);
+
+// ------------------------------------------------------------
+// Column Layout, Read
+// ------------------------------------------------------------
+BENCHMARK_CAPTURE(BM_MemoryLayout_Read_1, ColRead1, NUM_BUFFERS, BUFFER_SIZE, false)
+    ->UseManualTime()
+    ->Iterations(NUM_BENCHMARK_ITERATIONS);
+
+BENCHMARK_CAPTURE(BM_MemoryLayout_Read_2, ColRead2, NUM_BUFFERS, BUFFER_SIZE, false)
+    ->UseManualTime()
+    ->Iterations(NUM_BENCHMARK_ITERATIONS);
+
+BENCHMARK_CAPTURE(BM_MemoryLayout_Read_4, ColRead4, NUM_BUFFERS, BUFFER_SIZE, false)
+    ->UseManualTime()
+    ->Iterations(NUM_BENCHMARK_ITERATIONS);
+
+BENCHMARK_CAPTURE(BM_MemoryLayout_Read_8, ColRead8, NUM_BUFFERS, BUFFER_SIZE, false)
+    ->UseManualTime()
+    ->Iterations(NUM_BENCHMARK_ITERATIONS);
+
+BENCHMARK_CAPTURE(BM_MemoryLayout_Read_16, ColRead16, NUM_BUFFERS, BUFFER_SIZE, false)
     ->UseManualTime()
     ->Iterations(NUM_BENCHMARK_ITERATIONS);
 
