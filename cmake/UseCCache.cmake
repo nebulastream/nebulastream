f657435fc8848bfc6f28c9f0b1f7c645357884b7
Adrian Michalke <adrian.michalke@yahoo.de>
2023-07-24 14:31:43 +0200

    [#4022] set -fpch-validate-input-files-content and create own cmake for PCHs

--- cmake/UseCCache.cmake
+++ cmake/UseCCache.cmake
@@ -1,16 +1,5 @@
 
 
-if (NES_ENABLE_PRECOMPILED_HEADERS)
-    set(CMAKE_PCH_INSTANTIATE_TEMPLATES ON)
-    if (NES_USE_CCACHE)
-        # Need to set these to enable interplay between ccache and precompiled headers
-        # https://ccache.dev/manual/4.8.html#_precompiled_headers
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Xclang -fno-pch-timestamp")
-        set(ENV{CCACHE_SLOPPINESS} "pch_defines,time_macros,include_file_ctime,include_file_mtime")
-        message("CCACHE_SLOPPINESS: $ENV{CCACHE_SLOPPINESS}")
-    endif ()
-endif ()
-
 if (NES_USE_CCACHE)
     find_program(CCACHE_PROGRAM ccache)
     if (CCACHE_PROGRAM)
---
+++ cmake/UsePrecompiledHeaders.cmake
@@ -0,0 +1,15 @@
+
+
+if (NES_ENABLE_PRECOMPILED_HEADERS)
+    set(CMAKE_PCH_INSTANTIATE_TEMPLATES ON)
+    if (NES_USE_CCACHE)
+        # Need to set these to enable interplay between ccache and precompiled headers
+        # https://ccache.dev/manual/4.8.html#_precompiled_headers
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Xclang -fno-pch-timestamp -fpch-validate-input-files-content")
+        set(ENV{CCACHE_SLOPPINESS} "pch_defines,time_macros,include_file_ctime,include_file_mtime")
+        message(STATUS "Set CCACHE_SLOPPINESS: $ENV{CCACHE_SLOPPINESS}")
+    endif ()
+    message(STATUS "Using Precompiled Headers")
+elseif()
+    message(STATUS "Not using Precompiled Headers")
+endif ()
