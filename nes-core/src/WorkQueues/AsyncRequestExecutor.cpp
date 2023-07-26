9d0db911e0d43a989a7ec28ae4baba618e70aaea
fkuzun <felix.s.lang@campus.tu-berlin.de>
2023-07-26 20:10:15 +0200

    [#4033] started implementing loop

---
+++ nes-core/src/WorkQueues/AsyncRequestExecutor.cpp
@@ -0,0 +1,51 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+#include <WorkQueues/AsyncRequestExecutor.hpp>
+#include <WorkQueues/RequestTypes/AbstractRequest.hpp>
+#include <Util/ThreadNaming.hpp>
+
+NES::Experimental::AsyncRequestExecutor::AsyncRequestExecutor(uint32_t numOfThreads) : running(true) {
+    for (uint32_t i = 0; i < numOfThreads; ++i) {
+        std::promise<bool> promise;
+        completionFutures.emplace_back(promise.get_future());
+        //auto thread = std::thread(runThread, i, std::move(promise));
+        auto thread = std::thread([this, i, promise = std::move(promise)]() mutable {
+          try {
+              setThreadName("RequestExecThr-%d", i);
+              runningRoutine();
+              promise.set_value(true);
+          } catch (std::exception const& ex) {
+              promise.set_exception(std::make_exception_ptr(ex));
+          }
+        });
+        thread.detach();
+    }
+}
+
+
+void NES::Experimental::AsyncRequestExecutor::runningRoutine() {
+    while (true) {
+        std::unique_lock lock(workMutex);
+        while (asyncRequestQueue.empty()) {
+            cv.wait(lock);
+        }
+        if (running) {
+            AbstractRequestPtr abstractRequest = asyncRequestQueue.front();
+            asyncRequestQueue.pop_front();
+            lock.unlock();
+
+            abstractRequest
+        }
+    }
+}
