062882ff5e025bd60d30b3d31439a11762485296
Moritz Ruge <moritz.ruge@gmail.com>
2023-08-03 16:18:27 +0200

    #3861 - changed Yaml config object to a configuration object class, query and delete functionality, corresponding tests, and indirect adjustements

--- nes-common/include/Configurations/ScalarOption.hpp
+++ nes-common/include/Configurations/ScalarOption.hpp
@@ -140,6 +140,7 @@ void ScalarOption<T>::parseFromString(std::string identifier, std::map<std::stri
 using StringOption = ScalarOption<std::string>;
 using IntOption = ScalarOption<int64_t>;
 using UIntOption = ScalarOption<uint64_t>;
+using DoubleOption = ScalarOption<double>;
 using SizeTOption = ScalarOption<size_t>;
 using BoolOption = ScalarOption<bool>;
 
