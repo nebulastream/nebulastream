c318fce0e3cac8f115f9045d9f899b64591ac670
Sumalvico <niklas.sumalvico@gmail.com>
2023-08-03 14:05:03 +0200

    NebulaSQLHelper

--- nes-core/include/Parsers/NebulaSQL/NebulaSQLHelper.hpp
+++ nes-core/include/Parsers/NebulaSQL/NebulaSQLHelper.hpp
@@ -1,15 +1,15 @@
 /*
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
 
-    https://www.apache.org/licenses/LICENSE-2.0
+        https://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
 */
 
 #ifndef NES_CORE_INCLUDE_PARSERS_NEBULASQL_NEBULASQLHELPER_HPP_
@@ -23,48 +23,48 @@ limitations under the License.
 
 namespace NES::Parsers {
 
-        /**
-* @brief This class represents the results from parsing the ANTLR AST tree
-* Attributes of this class represent the different clauses and a merge into a query after parsing the AST
-*/
-        class NebulaSQLHelper {
-          private:
-            std::vector<ExpressionNodePtr> projectionFields;
-            std::vector<std::string> sources;
-            SinkDescriptorPtr sinkDescriptor;
-            std::vector<ExpressionNodePtr> whereClauses;
-            std::map<int32_t, std::string> sourceList;
-            std::map<int32_t, NebulaSQLOperatorNode> operatorList;// contains the operators from the PATTERN clause
-            std::list<ExpressionNodePtr> expressionList;
-            std::list<SinkDescriptorPtr> sinkList; // INTO
-            std::pair<std::string, int32_t> window;// WITHIN
+/**
+ * @brief This class represents the results from parsing the ANTLR AST tree
+ * Attributes of this class represent the different clauses and a merge into a query after parsing the AST
+ */
+class NebulaSQLHelper {
+  public:
+    //Constructors
+    NebulaSQLHelper() = default;
+    // Getter and Setter
+    const std::map<int32_t, std::string>& getSources() const;
+    void setSources(const std::map<int32_t, std::string>& sources);
+    const std::map<int32_t, NebulaSQLOperatorNode>& getOperatorList() const;
+    void setOperatorList(const std::map<int32_t, NebulaSQLOperatorNode>& operatorList);
+    const std::list<ExpressionNodePtr>& getExpressions() const;
+    void setExpressions(const std::list<ExpressionNodePtr>& expressions);
+    const std::vector<ExpressionNodePtr>& getProjectionFields() const;
+    void setProjectionFields(const std::vector<ExpressionNodePtr>& projectionFields);
+    const std::list<SinkDescriptorPtr>& getSinks() const;
+    void setSinks(const std::list<SinkDescriptorPtr>& sinks);
+    const std::pair<std::string, int>& getWindow() const;
+    void setWindow(const std::pair<std::string, int>& window);
 
+    /**
+     * @brief inserts a new source into the source map (FROM-Clause)
+     * @param sourcePair <K,V> containing the nodeId and the streamName
+     */
+    void addSource(std::pair<int32_t, std::basic_string<char>> sourcePair);
+    /**
+     * @brief inserts a new ExpressionItem (Projection Attribute) to the  list of specified output attributes
+     * @param expressionItem
+     */
+    void addProjectionField(ExpressionNodePtr expressionNode);
 
-          public:
-            void setProjectionFields(const std::vector<ExpressionNodePtr>& projectionFields);
-            void addProjectionField(ExpressionNodePtr expressionNode);
+  private:
+    std::map<int32_t, std::string> sourceList;
+    std::map<int32_t, NebulaSQLOperatorNode> operatorList;// contains the operators from the HELPER clause
+    std::list<ExpressionNodePtr> expressionList;
+    std::vector<ExpressionNodePtr> projectionFields;
+    std::list<SinkDescriptorPtr> sinkList; // INTO
+    std::pair<std::string, int32_t> window;// WITHIN
+};
 
-            const std::vector<std::string>& getSources() const;
-            void addSource(const std::string& source);
-
-            const SinkDescriptorPtr getSinkDescriptor() const;
-            void setSink(SinkDescriptorPtr sink);
-
-            const std::vector<ExpressionNodePtr>& getWhereClauses() const;
-            void addWhereClause(ExpressionNodePtr expression);
-
-            const std::vector<ExpressionNodePtr>& getProjectionFields() const;
-
-            uint64_t getLimit() const;
-            const std::string& getNewName() const;
-            const FieldAssignmentExpressionNodePtr& getMapExpression() const;
-            const WatermarkStrategyDescriptorPtr& getWatermarkStrategieDescriptor() const;
-            const NES::Windowing::WindowTypePtr getWindowType() const;
-            void addExpression(ExpressionNodePtr expressionNode);
-            //void addArithmeticBinaryExpression(ExpressionNodePtr sharedPtr) const;
-            const std::pair<std::string, int32_t>& getWindow() const;
-            void setWindow(const std::pair<std::string, int32_t>& window);
-        };
-    }// namespace NES::Parsers
+}// namespace NES::Parsers
 
 #endif// NES_CORE_INCLUDE_PARSERS_NEBULASQL_NEBULASQLHELPER_HPP_
--- nes-core/src/Parsers/NebulaSQL/NebulaSQLHelper.cpp
+++ nes-core/src/Parsers/NebulaSQL/NebulaSQLHelper.cpp
@@ -1,15 +1,15 @@
 /*
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
 
-    https://www.apache.org/licenses/LICENSE-2.0
+        https://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
 */
 
 #include <Parsers/NebulaSQL/NebulaSQLOperatorNode.hpp>
@@ -17,57 +17,25 @@ limitations under the License.
 
 namespace NES::Parsers {
 
-        const std::vector<std::string>& NebulaSQLHelper::getSources() const { return this->sources; }
-        void NebulaSQLHelper::addSource(const std::string& source) {
-            this->sources.push_back(source);
-        }
+//Getter and Setter for the map/list entries of each clause
+const std::map<int32_t, std::string>& NebulaSQLHelper::getSources() const { return this->sourceList; }
+void NebulaSQLHelper::setSources(const std::map<int32_t, std::string>& sources) { this->sourceList = sources; }
+const std::map<int32_t, NebulaSQLOperatorNode>& NebulaSQLHelper::getOperatorList() const { return this->operatorList; }
+void NebulaSQLHelper::setOperatorList(const std::map<int32_t, NebulaSQLOperatorNode>& operatorList) {
+    this->operatorList = operatorList;
+}
+const std::list<ExpressionNodePtr>& NebulaSQLHelper::getExpressions() const { return this->expressionList; }
+void NebulaSQLHelper::setExpressions(const std::list<ExpressionNodePtr>& expressions) { this->expressionList = expressions; }
+const std::vector<ExpressionNodePtr>& NebulaSQLHelper::getProjectionFields() const { return this->projectionFields; }
+void NebulaSQLHelper::setProjectionFields(const std::vector<ExpressionNodePtr>& projectionFields) {
+    this->projectionFields = projectionFields;
+}
+const std::list<SinkDescriptorPtr>& NebulaSQLHelper::getSinks() const { return this->sinkList; }
+void NebulaSQLHelper::setSinks(const std::list<SinkDescriptorPtr>& sinks) { this->sinkList = sinks; }
+const std::pair<std::string, int32_t>& NebulaSQLHelper::getWindow() const { return this->window; }
+void NebulaSQLHelper::setWindow(const std::pair<std::string, int32_t>& window) { this->window = window; }
 
-        const SinkDescriptorPtr NebulaSQLHelper::getSinkDescriptor() const {
-            return this->sinkDescriptor;
-        }
-        void NebulaSQLHelper::setSink(SinkDescriptorPtr sink) {
-            this->sinkDescriptor = sink;
-        }
+void NebulaSQLHelper::addSource(std::pair<int32_t, std::basic_string<char>> sourcePair) { this->sourceList.insert(sourcePair); }
+void NebulaSQLHelper::addProjectionField(ExpressionNodePtr expressionNode) { this->projectionFields.push_back(expressionNode); }
 
-        const std::vector<ExpressionNodePtr>& NebulaSQLHelper::getWhereClauses() const {
-            return whereClauses;
-        }
-        void NebulaSQLHelper::addWhereClause(ExpressionNodePtr expression) {
-            whereClauses.push_back(expression);
-        }
-        void NebulaSQLHelper::setProjectionFields(const std::vector<ExpressionNodePtr>& projectionFields) {
-            this->projectionFields = projectionFields;
-        }
-        void NebulaSQLHelper::addProjectionField(ExpressionNodePtr expressionNode) { this->projectionFields.push_back(expressionNode); }
-        const std::vector<ExpressionNodePtr>& NebulaSQLHelper::getProjectionFields() const { return this->projectionFields; }
-        uint64_t NebulaSQLHelper::getLimit() const { return 0; }
-
-        const std::string& NebulaSQLHelper::getNewName() const {
-            // TODO: Implement logic to return the new name
-            static const std::string placeholder = ""; // Temporary placeholder
-            return placeholder;
-        }
-        const FieldAssignmentExpressionNodePtr& NebulaSQLHelper::getMapExpression() const {
-            // TODO: Implement logic to return the map expression
-            static const FieldAssignmentExpressionNodePtr placeholder = nullptr; // Temporary placeholder
-            return placeholder;
-        }
-        const WatermarkStrategyDescriptorPtr& NebulaSQLHelper::getWatermarkStrategieDescriptor() const {
-            // TODO: Implement logic to return the watermark strategy descriptor
-            static const WatermarkStrategyDescriptorPtr placeholder = nullptr; // Temporary placeholder
-            return placeholder;
-        }
-        /*
-        void NebulaSQLHelper::addArithmeticBinaryExpression(ExpressionNodePtr sharedPtr) const {
-            // TODO: Implement logic to handle the addition of an arithmetic binary expression
-        }
-        */
-        const NES::Windowing::WindowTypePtr NebulaSQLHelper::getWindowType() const { return NES::Windowing::WindowTypePtr(); }
-        const std::pair<std::string, int32_t>& NebulaSQLHelper::getWindow() const { return this->window; }
-        void NebulaSQLHelper::setWindow(const std::pair<std::string, int32_t>& window) { this->window = window; }
-        void NebulaSQLHelper::addExpression(ExpressionNodePtr expressionNode) {
-            auto pos = this->expressionList.begin();
-            this->expressionList.insert(pos, expressionNode);
-        }
-
-        }// namespace NES::Parsers
+}// namespace NES::Parsers
