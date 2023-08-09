f3cbaa6908f927489717d3d485c388e97455ead0
Florian Schmeller <26191888+fschmllr@users.noreply.github.com>
2023-08-09 19:54:44 +0200

    [#3993] Add class to provide tools for kernel compilation

---
+++ nes-runtime/include/Nautilus/Backends/Experimental/Vectorization/KernelCompiler.hpp
@@ -0,0 +1,37 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#ifndef NES_NES_RUNTIME_INCLUDE_NAUTILUS_BACKENDS_EXPERIMENTAL_VECTORIZATION_KERNELCOMPILER_HPP_
+#define NES_NES_RUNTIME_INCLUDE_NAUTILUS_BACKENDS_EXPERIMENTAL_VECTORIZATION_KERNELCOMPILER_HPP_
+
+#include <Nautilus/Backends/Experimental/Vectorization/KernelExecutable.hpp>
+#include <Nautilus/Tracing/Trace/ExecutionTrace.hpp>
+
+namespace NES::Runtime::Execution::Operators {
+    class Operator;
+} // namespace NES::Runtime::Execution::Operators;
+
+namespace NES::Nautilus::Backends {
+
+/**
+ * @brief The `KernelCompiler` class is responsible for creating the execution trace of a vectorized Nautilus operator pipeline.
+ */
+class KernelCompiler {
+public:
+    static std::shared_ptr<Nautilus::Tracing::ExecutionTrace> createTraceFromNautilusOperator(const std::shared_ptr<Runtime::Execution::Operators::Operator>& nautilusOperator);
+};
+
+} // namespace NES::Nautilus::Backends
+
+#endif // NES_NES_RUNTIME_INCLUDE_NAUTILUS_BACKENDS_EXPERIMENTAL_VECTORIZATION_KERNELCOMPILER_HPP_
