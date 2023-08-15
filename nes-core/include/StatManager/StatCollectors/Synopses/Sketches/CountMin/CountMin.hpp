e3b9a5b820114371c434fa06117876f7d7b0f6de
Moritz Ruge <moritz.ruge@gmail.com>
2023-08-15 17:28:15 +0200

    #3861 - adressed Nils feedback

--- nes-core/include/StatManager/StatCollectors/Synopses/Sketches/CountMin/CountMin.hpp
+++ nes-core/include/StatManager/StatCollectors/Synopses/Sketches/CountMin/CountMin.hpp
@@ -24,16 +24,81 @@ namespace NES::Experimental::Statistics {
   class CountMin : public Sketch {
 
     public:
+
+      /**
+       * @brief gets the error of the sketch
+       * @return mError
+       */
       [[nodiscard]] double getError() const;
+
+      /**
+       * @brief gets the probability that the error exceeds the parametrized error
+       * @return mProb
+       */
       [[nodiscard]] double getProb() const;
+
+      /**
+       * @brief gets the sketch data
+       * @return mDataPointer
+       */
       uint32_t** getDataPointer();
+
+      /**
+       * @brief sets the pointer to the sketches data
+       * @param DataPointer a pointer to a sketche
+       */
       void setDataPointer(uint32_t** DataPointer);
+
+      /**
+       * @brief returns the H3 object of the CountMin sketch, which contains a number of hash functions equal to the
+       * number of rows of the CountMin sketch
+       * @return a pointer to an H3 object
+       */
       H3* getClassOfHashFunctions();
+
+      /**
+       * @brief sets the H3 object of a CountMin sketch
+       * @param ClassOfHashingFunctions an H3 object
+       */
       void setClassOfHashFunctions(H3* ClassOfHashingFunctions);
+
+      /**
+       * @brief the constructor of a CountMin sketch
+       * @param config a general purpose configuration object of type StatCollectorConfig, which contains information on
+       * the type of StatCollector which is to be created, over which stream and field to construct the statCollector,
+       * duration, frequency, and more
+       */
       CountMin(const StatCollectorConfig& config);
+
+      /**
+       * @brief updates the CountMin sketch once per row using the key
+       * @param key a key that is hashed once per row, updating exactly one counter of the CountMin sketch per row
+       */
       void update(uint32_t key) override;
+
+      /**
+       * @brief checks whether two CountMin sketches are initialized equally (depth, width, and H3)
+       * @param rightSketch a unique pointer to a StatCollector which is a sketch in this case.
+       * @param statCollection a boolean which signifies if we want to two sketches to be equal for statistic collection.
+       * In this case we also have to check that the duration and frequency are the same
+       * @return true if the two CountMin sketches are equally initilized
+       */
       bool equal(const std::unique_ptr<StatCollector>& rightSketch, bool statCollection) override;
+
+      /**
+       * takes two unique StatCollector pointers to sketches and returns a unique StatCollector pointer to another sketch,
+       * which is the result of merging the two input sketches
+       * @param rightSketch a second unique statCollector pointer to a CountMin sketch
+       * @param statCollection a boolean wether this is being done for statisitic collection
+       * @return a unique statCollector pointer to the merged result of the input
+       */
       std::unique_ptr<StatCollector> merge(std::unique_ptr<StatCollector> rightSketch, bool statCollection) override;
+
+      /**
+       * @brief the function approximates how many times the CountMin Sketch observed the key
+       * @param key the key whose frequency we want to approximate
+       * @return an approximation of the frequency of the key
+       */
       uint32_t pointQuery(uint32_t key);
 
     private:
--- nes-core/include/StatManager/StatCollectors/Synopses/Sketches/Sketches.hpp
+++ nes-core/include/StatManager/StatCollectors/Synopses/Sketches/Sketches.hpp
@@ -19,15 +19,65 @@
 
 namespace NES::Experimental::Statistics {
 
+  /**
+   * @brief an abstract class that inherits from the general class statCollector, and further defines the more detailed
+   * interface for all sketches.
+   */
   class Sketch : public StatCollector {
 
   public:
+    /**
+     * @brief gets the depth of the sketch
+     * @return depth
+     */
     [[nodiscard]]uint32_t getDepth() const;
+
+    /**
+     * @brief gets the width of the sketch
+     * @return width
+     */
     [[nodiscard]]uint32_t getWidth() const;
+
+    /**
+     * @brief the constructor of the abstract class sketch
+     * @param config a config object of type statCollectorConfig, which defines behavior such as the type of sketch, the
+     * duration for which the statistic is to be collected, how ofter a statistic is to be collected, and more
+     * @param depth the depth of the sketch
+     * @param width the width of the sketch
+     */
     Sketch(const StatCollectorConfig& config, const uint32_t depth, const uint32_t width);
+
+    /**
+     * @brief a pure virtual function that is inherited from the statCollector and which has to be defined by every sketch.
+     * The update function modifies the sketch according to the key
+     * @param key the key which modifies the sketch
+     */
     void update(uint32_t key) override = 0;
+
+    /**
+     * @brief a pure virtual function, inherited from the statCollector, which needs to be implemented by every sketch.
+     * equal() checks if two sketches are parametrized equally, which is especially important such that two or
+     * more statCollectors can be merged. They do not need to be identical for true to be returned in terms of the
+     * values in the sketch
+     * @param rightStatCollector the other statCollector to be compared to
+     * @param statCollection a boolean parameter, which tightens the conditions, whether two statCollectors are equal
+     * or not. Within the context of statCollection, they of course need to also be build over the same duration,
+     * frequency, and over the same field.
+     * @return true if the two sketches are initialized similarly, such that they can be merged
+     */
     bool equal(const StatCollectorPtr& rightSketch, bool statCollection) override = 0;
+
+    /**
+     * @brief a pure virtual function, inherited from the statCollector, which needs to be implemented by every sketch.
+     * It checks whether the two sketches are constructed in the same manner (equal()), and if so combines the two
+     * @param rightStatCollector the second statCollector
+     * @param statCollection a boolean parameter, which tightens the conditions, whether two statCollectors are equal
+     * or not. Within the context of statCollection, they of course need to also be build over the same duration,
+     * frequency, and over the same field.
+     * @return a unique statCollectorPtr to the results of the two merged statCollectors.
+     */
     StatCollectorPtr merge(StatCollectorPtr rightSketch, bool statCollection) override = 0;
+
   private:
     uint32_t depth;
     uint32_t width;
