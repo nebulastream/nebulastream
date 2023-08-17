ac9806a9f59dc9736622f1614f7e90897ab08da2
Florian Schmeller <26191888+fschmllr@users.noreply.github.com>
2023-08-17 16:06:16 +0200

    [#4148] Compile pipeline to CUDA kernel for Nautilus Kernel operator

---
+++ nes-runtime/include/Execution/Operators/Experimental/Vectorization/Kernel.hpp
@@ -0,0 +1,51 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+#ifndef NES_RUNTIME_INCLUDE_EXECUTION_OPERATORS_EXPERIMENTAL_VECTORIZATION_KERNEL_HPP_
+#define NES_RUNTIME_INCLUDE_EXECUTION_OPERATORS_EXPERIMENTAL_VECTORIZATION_KERNEL_HPP_
+
+#include <Execution/Operators/Experimental/Vectorization/VectorizableOperator.hpp>
+
+#include <Nautilus/Backends/Experimental/Vectorization/KernelExecutable.hpp>
+#include <Nautilus/Util/CompilationOptions.hpp>
+
+namespace NES::Runtime::Execution::Operators {
+
+/**
+ * @brief The Kernel operator is a Nautilus operator that upon calling its `execute` method executes a kernel function.
+ * The kernel function is compiled into a shared library object and exposed by a wrapper function.
+ */
+class Kernel : public VectorizableOperator {
+public:
+    struct Descriptor {
+        std::shared_ptr<Operator> pipeline;
+        Nautilus::CompilationOptions compileOptions;
+        uint64_t inputSchemaSize;
+    };
+
+    /**
+     * @brief Constructor.
+     * @param descriptor the kernel descriptor
+     */
+    explicit Kernel(Descriptor descriptor);
+
+    void execute(ExecutionContext& ctx, RecordBuffer& recordBuffer) const override;
+
+private:
+    Descriptor descriptor;
+    std::unique_ptr<Backends::KernelExecutable> kernelExecutable;
+};
+
+} // namespace NES::Runtime::Execution::Operators
+
+#endif // NES_RUNTIME_INCLUDE_EXECUTION_OPERATORS_EXPERIMENTAL_VECTORIZATION_KERNEL_HPP_
---
+++ nes-runtime/src/Execution/Operators/Experimental/Vectorization/Kernel.cpp
@@ -0,0 +1,63 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include <Execution/Operators/Experimental/Vectorization/Kernel.hpp>
+#include <Execution/RecordBuffer.hpp>
+#include <Nautilus/Interface/FunctionCall.hpp>
+#include <Nautilus/Backends/Experimental/Vectorization/KernelExecutable.hpp>
+#include <Nautilus/Backends/Experimental/Vectorization/CUDA/CUDAKernelCompiler.hpp>
+#include <Util/DumpHelper.hpp>
+
+namespace NES::Runtime::Execution::Operators {
+
+void invokeKernel(void* kernelExecutable, void* recordBuffer) {
+    auto executable = static_cast<Backends::KernelExecutable*>(kernelExecutable);
+    auto kernelWrapper = executable->getKernelWrapperFunction();
+    kernelWrapper(recordBuffer);
+}
+
+Kernel::Kernel(Descriptor descriptor)
+    : descriptor(descriptor)
+    , kernelExecutable(nullptr)
+{
+
+}
+
+void Kernel::setup(ExecutionContext& ctx) const {
+    auto cudaKernelCompiler = Nautilus::Backends::CUDA::CUDAKernelCompiler("cudaKernelWrapper");
+    auto compileOptions = descriptor.compileOptions;
+    auto dumpHelper = DumpHelper::create(compileOptions.getIdentifier(), compileOptions.isDumpToConsole(), compileOptions.isDumpToFile());
+    auto executable = cudaKernelCompiler.compile(descriptor.pipeline, compileOptions, dumpHelper);
+    // TODO(#4148) Assign kernel executable
+
+    Operator::setup(ctx);
+    auto pipeline = descriptor.pipeline;
+    pipeline->setup(ctx);
+}
+
+void Kernel::open(ExecutionContext& ctx, RecordBuffer& recordBuffer) const {
+    Operator::open(ctx, recordBuffer);
+    auto pipeline = descriptor.pipeline;
+    pipeline->open(ctx, recordBuffer);
+}
+
+void Kernel::execute(ExecutionContext& ctx, RecordBuffer& recordBuffer) const {
+    auto kernelExecutableRef = Value<MemRef>((int8_t*) kernelExecutable.get());
+    auto recordBufferRef = recordBuffer.getReference();
+    Nautilus::FunctionCall("invokeKernel", invokeKernel, kernelExecutableRef, recordBufferRef);
+    auto vectorizedChild = std::dynamic_pointer_cast<const VectorizableOperator>(child);
+    vectorizedChild->execute(ctx, recordBuffer);
+}
+
+} // namespace NES::Runtime::Execution::Operators
