78f4e76128f6e1f9878beefc5e0b91cbe9bd8f3e
Florian Schmeller <26191888+fschmllr@users.noreply.github.com>
2023-08-17 15:33:57 +0200

    [#4148] Make KernelCompiler an interface for compiling kernels

--- nes-runtime/src/Nautilus/Backends/Experimental/Vectorization/KernelCompiler.cpp
+++ nes-runtime/src/Nautilus/Backends/Experimental/Vectorization/KernelCompiler.cpp
@@ -18,33 +18,16 @@
 #include <Execution/Operators/Experimental/Vectorization/VectorizableOperator.hpp>
 #include <Execution/Operators/Operator.hpp>
 #include <Execution/RecordBuffer.hpp>
+#include <Nautilus/CodeGen/CodeGenerator.hpp>
 #include <Nautilus/Tracing/TraceContext.hpp>
 
 namespace NES::Nautilus::Backends {
 
-std::shared_ptr<Nautilus::Tracing::ExecutionTrace> KernelCompiler::createTraceFromNautilusOperator(const std::shared_ptr<Runtime::Execution::Operators::Operator>& nautilusOperator) {
-    auto pipelineExecutionContextRef = Nautilus::Value<Nautilus::MemRef>((int8_t*) nullptr);
-    pipelineExecutionContextRef.ref = Nautilus::Tracing::ValueRef(INT32_MAX, 0, NES::Nautilus::IR::Types::StampFactory::createAddressStamp());
-
-    auto workerContextRef = Nautilus::Value<Nautilus::MemRef>((int8_t*) nullptr);
-    workerContextRef.ref = Nautilus::Tracing::ValueRef(INT32_MAX, 1, NES::Nautilus::IR::Types::StampFactory::createAddressStamp());
-
-    auto memRef = Nautilus::Value<Nautilus::MemRef>(std::make_unique<Nautilus::MemRef>(Nautilus::MemRef(0)));
-    memRef.ref = Nautilus::Tracing::ValueRef(INT32_MAX, 2, NES::Nautilus::IR::Types::StampFactory::createAddressStamp());
-    auto recordBuffer = Runtime::Execution::RecordBuffer(memRef);
-
-    auto schemaSize = Nautilus::Value<Nautilus::UInt64>((uint64_t)0);
-    schemaSize.ref = Nautilus::Tracing::ValueRef(INT32_MAX, 3, NES::Nautilus::IR::Types::StampFactory::createUInt64Stamp());
-
-    auto vectorizedOperator = std::dynamic_pointer_cast<Runtime::Execution::Operators::VectorizableOperator>(nautilusOperator);
-
-    return Nautilus::Tracing::traceFunction([&]() {
-        auto traceContext = Nautilus::Tracing::TraceContext::get();
-        traceContext->addTraceArgument(recordBuffer.getReference().ref);
-        traceContext->addTraceArgument(schemaSize.ref);
-        auto ctx = Runtime::Execution::ExecutionContext(workerContextRef, pipelineExecutionContextRef);
-        vectorizedOperator->execute(ctx, recordBuffer);
-    });
+std::unique_ptr<KernelExecutable> KernelCompiler::compile(const std::shared_ptr<Runtime::Execution::Operators::Operator>& nautilusOperator, const CompilationOptions& options, const DumpHelper& dumpHelper) {
+    auto trace = createTrace(nautilusOperator);
+    auto ir = createIR(trace);
+    auto codeGenerator = createCodeGenerator(ir);
+    return createExecutable(std::move(codeGenerator), options, dumpHelper);
 }
 
 } // namespace NES::Nautilus::Backends
