7761dbee477ca5424fe944b84953698f20fff2d6
Florian Schmeller <26191888+fschmllr@users.noreply.github.com>
2023-08-17 16:01:18 +0200

    [#4148] Implement KernelCompiler interface for CUDA

---
+++ nes-runtime/include/Nautilus/Backends/Experimental/Vectorization/CUDA/CUDAKernelCompiler.hpp
@@ -0,0 +1,48 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#ifndef NES_RUNTIME_INCLUDE_NAUTILUS_BACKENDS_CUDA_CUDAKERNELCOMPILER_HPP_
+#define NES_RUNTIME_INCLUDE_NAUTILUS_BACKENDS_CUDA_CUDAKERNELCOMPILER_HPP_
+
+#include <Nautilus/Backends/Experimental/Vectorization/KernelCompiler.hpp>
+#include <Nautilus/IR/BasicBlocks/BasicBlock.hpp>
+
+namespace NES::Nautilus::Backends::CUDA {
+
+class CUDAKernelCompiler : public KernelCompiler {
+public:
+    /**
+     * @brief Constructor.
+     * @param kernelWrapperName the name of the kernel wrapper function
+     */
+    explicit CUDAKernelCompiler(std::string kernelWrapperName);
+
+private:
+    std::shared_ptr<Nautilus::Tracing::ExecutionTrace> createTrace(const std::shared_ptr<Runtime::Execution::Operators::Operator>& nautilusOperator) override;
+
+    std::shared_ptr<IR::IRGraph> createIR(const std::shared_ptr<Nautilus::Tracing::ExecutionTrace>& trace) override;
+
+    std::unique_ptr<CodeGen::CodeGenerator> createCodeGenerator(const std::shared_ptr<IR::IRGraph>& irGraph) override;
+
+    std::unique_ptr<KernelExecutable> createExecutable(std::unique_ptr<CodeGen::CodeGenerator> codeGenerator, const CompilationOptions& options, const DumpHelper& dumpHelper) override;
+
+    std::shared_ptr<CodeGen::CodeGenerator> getCudaKernelWrapper(const IR::BasicBlockPtr& functionBasicBlock);
+
+    std::string kernelWrapperName;
+    std::string kernelName;
+};
+
+} // namespace NES::Nautilus::Backends::CUDA
+
+#endif // NES_RUNTIME_INCLUDE_NAUTILUS_BACKENDS_CUDA_CUDAKERNELCOMPILER_HPP_
