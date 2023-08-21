fee1f029ad185ec077a8cf99476d48db3a420606
Florian Schmeller <26191888+fschmllr@users.noreply.github.com>
2023-08-21 19:49:11 +0200

    [#3987] Make StagingHandler responsible for buffer life-time

--- nes-runtime/src/Execution/Operators/Experimental/Vectorization/StagingHandler.cpp
+++ nes-runtime/src/Execution/Operators/Experimental/Vectorization/StagingHandler.cpp
@@ -14,22 +14,34 @@
 
 #include <Execution/Operators/Experimental/Vectorization/StagingHandler.hpp>
 
+#include <Runtime/BufferManager.hpp>
 #include <Runtime/TupleBuffer.hpp>
+#include <Runtime/Execution/PipelineExecutionContext.hpp>
 
 namespace NES::Runtime::Execution::Operators {
 
-StagingHandler::StagingHandler(std::unique_ptr<TupleBuffer> tupleBuffer, uint64_t stageBufferCapacity)
-    : tupleBuffer(std::move(tupleBuffer))
-    , stageBufferCapacity(stageBufferCapacity)
+StagingHandler::StagingHandler(uint64_t stageBufferSize, uint64_t schemaSize)
+    : stageBufferSize(stageBufferSize)
+    , stageBufferCapacity(stageBufferSize / schemaSize)
+    , tupleBuffer(nullptr)
     , currentWritePosition(0)
 {
-
+    NES_ASSERT(schemaSize < stageBufferSize, "Stage buffer is too small for schema");
 }
 
-void StagingHandler::start(Runtime::Execution::PipelineExecutionContextPtr, Runtime::StateManagerPtr, uint32_t) {
+void StagingHandler::start(Runtime::Execution::PipelineExecutionContextPtr ctx, Runtime::StateManagerPtr, uint32_t) {
+    auto bufferManager = ctx->getBufferManager();
+    NES_INFO("Requesting unpooled buffer of size {} from buffer manager", stageBufferSize);
+    auto bufferOpt = bufferManager->getUnpooledBuffer(stageBufferSize);
+    NES_ASSERT(bufferOpt, "Request for unpooled buffer failed");
+    auto buffer = bufferOpt.value();
+    tupleBuffer = std::make_unique<Runtime::TupleBuffer>(buffer);
 }
 
 void StagingHandler::stop(Runtime::QueryTerminationType, Runtime::Execution::PipelineExecutionContextPtr) {
+    if (tupleBuffer) {
+        tupleBuffer->release();
+    }
 }
 
 void StagingHandler::reset() {
@@ -49,9 +61,10 @@ uint64_t StagingHandler::getCurrentWritePosition() const {
 }
 
 void StagingHandler::incrementWritePosition() {
-    // TODO Modification to the write position depends on the schema.
     currentWritePosition = currentWritePosition + 1;
-    tupleBuffer->setNumberOfTuples(currentWritePosition);
+    if (tupleBuffer) {
+        tupleBuffer->setNumberOfTuples(currentWritePosition);
+    }
 }
 
 } // namespace NES::Runtime::Execution::Operators
