896da13d40eefbb8d7bcb0cd3cebd3a49a93809c
Florian Schmeller <26191888+fschmllr@users.noreply.github.com>
2023-09-18 19:33:05 +0200

    [#4148] Set kernel executable to compile result

--- nes-runtime/include/Execution/Operators/Experimental/Vectorization/Kernel.hpp
+++ nes-runtime/include/Execution/Operators/Experimental/Vectorization/Kernel.hpp
@@ -44,7 +44,7 @@ public:
 
 private:
     Descriptor descriptor;
-    std::unique_ptr<Backends::KernelExecutable> kernelExecutable;
+    mutable std::unique_ptr<Backends::KernelExecutable> kernelExecutable;
 };
 
 } // namespace NES::Runtime::Execution::Operators
