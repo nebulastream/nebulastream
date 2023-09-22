d5ede28afc152f2cce6cec430bc0710009d14078
arianeziehn <ariane.ziehn@campus.tu-berlin.de>
2023-09-22 13:50:55 +0200

    [#NebulaSQL] gen files for new grammar

--- nes-core/include/Parsers/NebulaSQL/gen/NebulaSQLParser.h
+++ nes-core/include/Parsers/NebulaSQL/gen/NebulaSQLParser.h
@@ -1,12 +1,13 @@
 
-// Generated from /Users/sumalvico/CLionProjects/nebulastream/nes-core/src/Parsers/NebulaSQL/gen/NebulaSQL.g4 by ANTLR 4.12.0
+// Generated from CLionProjects/nebulastream/nes-core/src/Parsers/NebulaSQL/gen/NebulaSQL.g4 by ANTLR 4.9.2
 
 #pragma once
 
 
-#include "antlr4-runtime.h"
+#include <antlr4-runtime.h>
 
 
+namespace NES::Parsers {
 
 
 class  NebulaSQLParser : public antlr4::Parser {
@@ -68,20 +69,13 @@ public:
   };
 
   explicit NebulaSQLParser(antlr4::TokenStream *input);
+  ~NebulaSQLParser();
 
-  NebulaSQLParser(antlr4::TokenStream *input, const antlr4::atn::ParserATNSimulatorOptions &options);
-
-  ~NebulaSQLParser() override;
-
-  std::string getGrammarFileName() const override;
-
-  const antlr4::atn::ATN& getATN() const override;
-
-  const std::vector<std::string>& getRuleNames() const override;
-
-  const antlr4::dfa::Vocabulary& getVocabulary() const override;
-
-  antlr4::atn::SerializedATNView getSerializedATN() const override;
+  virtual std::string getGrammarFileName() const override;
+  virtual const antlr4::atn::ATN& getATN() const override { return _atn; };
+  virtual const std::vector<std::string>& getTokenNames() const override { return _tokenNames; }; // deprecated: use vocabulary instead.
+  virtual const std::vector<std::string>& getRuleNames() const override;
+  virtual antlr4::dfa::Vocabulary& getVocabulary() const override;
 
 
     /**
@@ -2081,8 +2075,7 @@ public:
   NonReservedContext* nonReserved();
 
 
-  bool sempred(antlr4::RuleContext *_localctx, size_t ruleIndex, size_t predicateIndex) override;
-
+  virtual bool sempred(antlr4::RuleContext *_localctx, size_t ruleIndex, size_t predicateIndex) override;
   bool queryTermSempred(QueryTermContext *_localctx, size_t predicateIndex);
   bool identifierSempred(IdentifierContext *_localctx, size_t predicateIndex);
   bool strictIdentifierSempred(StrictIdentifierContext *_localctx, size_t predicateIndex);
@@ -2091,11 +2084,23 @@ public:
   bool primaryExpressionSempred(PrimaryExpressionContext *_localctx, size_t predicateIndex);
   bool numberSempred(NumberContext *_localctx, size_t predicateIndex);
 
-  // By default the static state used to implement the parser is lazily initialized during the first
-  // call to the constructor. You can call this function if you wish to initialize the static state
-  // ahead of time.
-  static void initialize();
-
 private:
+  static std::vector<antlr4::dfa::DFA> _decisionToDFA;
+  static antlr4::atn::PredictionContextCache _sharedContextCache;
+  static std::vector<std::string> _ruleNames;
+  static std::vector<std::string> _tokenNames;
+
+  static std::vector<std::string> _literalNames;
+  static std::vector<std::string> _symbolicNames;
+  static antlr4::dfa::Vocabulary _vocabulary;
+  static antlr4::atn::ATN _atn;
+  static std::vector<uint16_t> _serializedATN;
+
+
+  struct Initializer {
+    Initializer();
+  };
+  static Initializer _init;
 };
 
+}  // namespace NES::Parsers
