eca67b429ac4e8dae785e1fe5befd0c965b1b6ff
capsfan95 <33393154+capsfan95@users.noreply.github.com>
2023-10-16 13:17:32 +0200

    4258add stat request param objects (#4261)
    * [#4258] [#4258] added request Param Obj classes with documentation
    
    * [#4258] [4258] fixed formatting
    
    * [#4258] [#4258] PR Feedback
    
    * [#4258] [#4258] fixed formatting
    
    * [#4258] [#4258] formatting again
    
    * [#4258] [#4258] one last formatting thing
    
    * [#4258] added request Param Obj classes with documentation
    
    * [#4258] fixed formatting
    
    * [#4258] PR Feedback
    
    * [#4258] fixed formatting
    
    * [#4258] [#4258] formatting again
    
    * [#4258] [#4258] one last formatting thing
    
    * [#4258] flipped test logic
    
    ---------
    
    Co-authored-by: Moritz Ruge <moritz.ruge@gmail.com>

---
+++ nes-core/include/Statistics/Requests/RequestParamObj.hpp
@@ -0,0 +1,74 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#ifndef NES_NES_CORE_INCLUDE_STATISTICS_REQUESTS_REQUESTPARAMOBJ_HPP_
+#define NES_NES_CORE_INCLUDE_STATISTICS_REQUESTS_REQUESTPARAMOBJ_HPP_
+
+#include <string>
+#include <memory>
+#include <vector>
+
+namespace NES {
+
+namespace Experimental::Statistics {
+/**
+ * @brief The abstract class from which all requests inherit
+ */
+class RequestParamObj {
+  public:
+    RequestParamObj(const std::string& logicalSourceName,
+                    const std::vector<std::string>& physicalSourceNames,
+                    const std::string& fieldName,
+                    const std::string& expression)
+        : logicalSourceName(logicalSourceName), physicalSourceNames(physicalSourceNames), fieldName(fieldName), expression(expression) {}
+
+    /**
+     * @return returns the logicalStreamName of the request
+     */
+    std::string getLogicalSourceName() const {
+        return logicalSourceName;
+    }
+
+    /**
+     * @return returns the physicalSourceName(s) of the request
+     */
+    const std::vector<std::string>& getPhysicalSourceNames() const {
+        return physicalSourceNames;
+    }
+
+    /**
+     * @return returns the fieldName of the request
+     */
+    std::string getFieldName() const {
+        return fieldName;
+    }
+
+    /**
+     * @return returns the expression, defining which stats to create, probe/query, or delete
+     */
+    std::string getExpression() const {
+        return expression;
+    }
+
+  private:
+    std::string logicalSourceName;
+    std::vector<std::string> physicalSourceNames;
+    std::string fieldName;
+    std::string expression;
+};
+}
+
+}
+
+#endif //NES_NES_CORE_INCLUDE_STATISTICS_REQUESTS_REQUESTPARAMOBJ_HPP_
