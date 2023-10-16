8575f04838ea582f11a8dceb16e491e31d329b7f
capsfan95 <33393154+capsfan95@users.noreply.github.com>
2023-10-16 13:17:32 +0200

    4258add stat request param objects (#4261)
    * [#4258] [#4258] added request Param Obj classes with documentation
    
    * [#4258] [4258] fixed formatting
    
    * [#4258] [#4258] PR Feedback
    
    * [#4258] [#4258] fixed formatting
    
    * [#4258] [#4258] formatting again
    
    * [#4258] [#4258] one last formatting thing
    
    * [#4258] added request Param Obj classes with documentation
    
    * [#4258] fixed formatting
    
    * [#4258] PR Feedback
    
    * [#4258] fixed formatting
    
    * [#4258] [#4258] formatting again
    
    * [#4258] [#4258] one last formatting thing
    
    * [#4258] flipped test logic
    
    ---------
    
    Co-authored-by: Moritz Ruge <moritz.ruge@gmail.com>

---
+++ nes-core/include/Statistics/Requests/BuildRequestParamObj.hpp
@@ -0,0 +1,40 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#ifndef NES_NES_CORE_INCLUDE_STATISTICS_REQUESTS_BUILDREQUESTPARAMOBJ_HPP_
+#define NES_NES_CORE_INCLUDE_STATISTICS_REQUESTS_BUILDREQUESTPARAMOBJ_HPP_
+
+#include <string>
+#include <memory>
+#include <vector>
+#include <Statistics/Requests/RequestParamObj.hpp>
+
+namespace NES {
+namespace Experimental::Statistics {
+
+/**
+ * @brief the inherited class that defines what is needed to build Statistics
+ */
+class BuildRequestParamObj : public RequestParamObj {
+  public:
+    BuildRequestParamObj(const std::string& logicalSourceName,
+                         const std::vector<std::string>& physicalSourceNames,
+                         const std::string& fieldName,
+                         const std::string& expression)
+        : RequestParamObj(logicalSourceName, physicalSourceNames, fieldName, expression) {}
+		};
+	}
+}
+
+#endif //NES_NES_CORE_INCLUDE_STATISTICS_REQUESTS_BUILDREQUESTPARAMOBJ_HPP_
---
+++ nes-core/include/Statistics/Requests/DeleteRequestParamObj.hpp
@@ -0,0 +1,54 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#ifndef NES_NES_CORE_INCLUDE_STATISTICS_REQUESTS_DELETEREQUESTPARAMOBJ_HPP_
+#define NES_NES_CORE_INCLUDE_STATISTICS_REQUESTS_DELETEREQUESTPARAMOBJ_HPP_
+
+#include <string>
+#include <memory>
+#include <vector>
+#include <Statistics/Requests/RequestParamObj.hpp>
+
+namespace NES {
+
+namespace Experimental::Statistics {
+
+/**
+ * @brief the inherited class that defines what is needed to delete Statistics
+ */
+class DeleteRequestParamObj : public RequestParamObj {
+  public:
+    DeleteRequestParamObj(const std::string &logicalSourceName,
+                          const std::vector<std::string> &physicalSourceNames,
+                          const std::string &fieldName,
+                          const std::string &expression,
+                          const time_t endTime)
+        : RequestParamObj(logicalSourceName, physicalSourceNames, fieldName, expression), endTime(endTime) {}
+
+    /**
+     * @return returns the latest possible time for which we wat to keep statCollectors.
+     * Everything older is deleted
+     */
+    time_t getEndTime() const {
+        return endTime;
+    }
+
+  private:
+    time_t endTime;
+};
+}
+
+}
+
+#endif //NES_NES_CORE_INCLUDE_STATISTICS_REQUESTS_DELETEREQUESTPARAMOBJ_HPP_
