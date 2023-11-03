e041f03e00c4e025e87b63bff78c2823b7e9852b
NES-CI <nebulastream@dima.tu-berlin.de>
2023-11-03 08:30:09 +0000

    GIT-CI: Updating NES version to 0.5.241-SNAPSHOT

--- nes-common/include/Version/version.hpp
+++ nes-common/include/Version/version.hpp
@@ -17,8 +17,8 @@
 #define NES_NES_CORE_INCLUDE_VERSION_VERSION_HPP_
 #define NES_VERSION_MAJOR 0
 #define NES_VERSION_MINOR 5
-#define NES_VERSION_PATCH 240
-#define NES_VERSION_POST_FIX 
-constexpr char const* NES_VERSION      = "0.5.240";
+#define NES_VERSION_PATCH 241
+#define NES_VERSION_POST_FIX -SNAPSHOT
+constexpr char const* NES_VERSION      = "0.5.241-SNAPSHOT";
 #endif // NES_NES_CORE_INCLUDE_VERSION_VERSION_HPP_
 // clang-format on
