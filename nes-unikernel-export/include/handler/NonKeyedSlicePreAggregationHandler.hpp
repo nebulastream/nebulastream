9494883bd5214bbf0f64c02fa959fd1aeb8f8f54
Lukas Schwerdtfeger <lukas.schwerdtfeger@gmail.com>
2023-11-16 16:17:33 +0100

    Unikernel-Export: Add Dummy Handler for MergingHandler

---
+++ nes-unikernel-export/include/handler/NonKeyedSlicePreAggregationHandler.hpp
@@ -0,0 +1,44 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#ifndef NES_RUNTIME_INCLUDE_EXECUTION_OPERATORS_STREAMING_AGGREGATIONS_NONKEYEDTIMEWINDOW_NONKEYEDSLICEPREAGGREGATIONHANDLER_HPP_
+#define NES_RUNTIME_INCLUDE_EXECUTION_OPERATORS_STREAMING_AGGREGATIONS_NONKEYEDTIMEWINDOW_NONKEYEDSLICEPREAGGREGATIONHANDLER_HPP_
+#include <Identifiers.hpp>
+#include <Runtime/Execution/OperatorHandler.hpp>
+#include <vector>
+namespace NES::Runtime::Execution::Operators {
+
+class MultiOriginWatermarkProcessor;
+class State;
+/**
+ * @brief The GlobalThreadLocalPreAggregationOperatorHandler provides an operator handler to perform slice-based pre-aggregation
+ * of global non-keyed tumbling and sliding windows.
+ * This operator handler, maintains a slice store for each worker thread and provides them for the aggregation.
+ * For each processed tuple buffer triggerThreadLocalState is called, which checks if the thread-local slice store should be triggered.
+ * This is decided by the current watermark timestamp.
+ */
+class NonKeyedSlicePreAggregationHandler : public OperatorHandler {
+  public:
+    /**
+     * @brief Creates the operator handler with a specific window definition, a set of origins, and access to the slice staging object.
+     * @param windowDefinition logical window definition
+     * @param origins the set of origins, which can produce data for the window operator
+     */
+    NonKeyedSlicePreAggregationHandler(uint64_t windowSize, uint64_t windowSlide, const std::vector<OriginId>& origins);
+
+    ~NonKeyedSlicePreAggregationHandler();
+
+};
+}// namespace NES::Runtime::Execution::Operators
+#endif// NES_RUNTIME_INCLUDE_EXECUTION_OPERATORS_STREAMING_AGGREGATIONS_NONKEYEDTIMEWINDOW_NONKEYEDSLICEPREAGGREGATIONHANDLER_HPP_
