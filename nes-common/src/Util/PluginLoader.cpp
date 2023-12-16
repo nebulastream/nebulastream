ad33ffb67878c268d833528efed243e6622c7938
NES-CI <nebulastream@dima.tu-berlin.de>
2023-12-16 20:20:43 +0000

    GIT-CI: Updating NES version to 0.6.5

--- nes-common/src/Util/PluginLoader.cpp
+++ nes-common/src/Util/PluginLoader.cpp
@@ -34,9 +34,8 @@ void PluginLoader::loadDefaultPlugins() {
 void PluginLoader::loadPlugins(const std::filesystem::path& dirPath) {
     try {
         for (const auto& entry : std::filesystem::directory_iterator(dirPath)) {
-            if (entry.is_regular_file() &&
-                entry.path().filename().string().starts_with("libnes-") &&
-                (entry.path().extension() == ".so" || entry.path().extension() == ".dylib")) {
+            if (entry.is_regular_file() && entry.path().filename().string().starts_with("libnes-")
+                && (entry.path().extension() == ".so" || entry.path().extension() == ".dylib")) {
                 loadPlugin(entry.path().string());
             }
         }
