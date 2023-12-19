0d9bf6d86b23517ae3894fee38ff95399c151d22
Lukas Schwerdtfeger <lukas.schwerdtfeger@gmail.com>
2023-12-19 16:53:00 +0100

    Export: Remove ConvertLogicalToPhysicalSource/Sink and LowerToExecutableQueryPlanPhase

--- nes-execution/src/QueryCompiler/Phases/Translations/LowerToExecutableQueryPlanPhase.cpp
+++ nes-execution/src/QueryCompiler/Phases/Translations/LowerToExecutableQueryPlanPhase.cpp
@@ -33,7 +33,6 @@
 #include <Operators/LogicalOperators/Sources/MQTTSourceDescriptor.hpp>
 #include <Operators/LogicalOperators/Sources/MemorySourceDescriptor.hpp>
 #include <Operators/LogicalOperators/Sources/MonitoringSourceDescriptor.hpp>
-#include <Operators/LogicalOperators/Sources/NoOpSourceDescriptor.h>
 #include <Operators/LogicalOperators/Sources/SenseSourceDescriptor.hpp>
 #include <Operators/LogicalOperators/Sources/SourceDescriptorPlugin.hpp>
 #include <Operators/LogicalOperators/Sources/StaticDataSourceDescriptor.hpp>
@@ -305,9 +304,6 @@ SourceDescriptorPtr LowerToExecutableQueryPlanPhase::createSourceDescriptor(Sche
               magic_enum::enum_name(sourceType));
 
     switch (sourceType) {
-#ifdef UNIKERNEL_EXPORT
-        case SourceType::NOOP_SOURCE: return NoOpSourceDescriptor::create(schema, logicalSourceName);
-#else
         case SourceType::DEFAULT_SOURCE: {
             auto defaultSourceType = physicalSourceType->as<DefaultSourceType>();
             return DefaultSourceDescriptor::create(
@@ -399,7 +395,6 @@ SourceDescriptorPtr LowerToExecutableQueryPlanPhase::createSourceDescriptor(Sche
                                                  kafkaSourceType->getNumberOfBuffersToProduce()->getValue(),
                                                  kafkaSourceType->getBatchSize()->getValue());
         }
-#endif
         default: {
             // check if a plugin can create the correct source descriptor
             for (const auto& plugin : SourceDescriptorPluginRegistry::getPlugins()) {
