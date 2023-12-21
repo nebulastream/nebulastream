dc6006bdfd6d15291028a27ff6778c1f29c04081
NES-CI <nebulastream@dima.tu-berlin.de>
2023-12-21 11:52:26 +0000

    GIT-CI: Updating NES version to 0.6.11-SNAPSHOT

--- nes-common/include/Version/version.hpp
+++ nes-common/include/Version/version.hpp
@@ -17,8 +17,8 @@
 #define NES_NES_CORE_INCLUDE_VERSION_VERSION_HPP_
 #define NES_VERSION_MAJOR 0
 #define NES_VERSION_MINOR 6
-#define NES_VERSION_PATCH 10
-#define NES_VERSION_POST_FIX 
-constexpr char const* NES_VERSION      = "0.6.10";
+#define NES_VERSION_PATCH 11
+#define NES_VERSION_POST_FIX -SNAPSHOT
+constexpr char const* NES_VERSION      = "0.6.11-SNAPSHOT";
 #endif // NES_NES_CORE_INCLUDE_VERSION_VERSION_HPP_
 // clang-format on
