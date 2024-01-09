8ba3ebf828de46d657ea6b9bf806eaa21636da31
AKozar <kozar.anastasiia@gmail.com>
2024-01-09 11:48:40 +0100

    10k nodes

--- nes-core/tests/Integration/UpstreamBackupTest.cpp
+++ nes-core/tests/Integration/UpstreamBackupTest.cpp
@@ -645,7 +645,7 @@ TEST_F(UpstreamBackupTest, testDecisionTime) {
     QueryServicePtr queryService = crd->getQueryService();
     QueryCatalogServicePtr queryCatalogService = crd->getQueryCatalogService();
     uint64_t var = 1;
-    while (var < 300) {
+    while (var < 10000) {
         auto workerConfig = WorkerConfiguration::create();
         workerConfig->numberOfBuffersInSourceLocalBufferPool = 128;
         workerConfig->numberOfBuffersInGlobalBufferManager = 1024;
