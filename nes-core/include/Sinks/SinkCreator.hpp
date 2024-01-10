02a69e870119485155fe98ed183c6853ced6778c
AKozar <kozar.anastasiia@gmail.com>
2024-01-10 13:02:09 +0100

    increase wait times

--- nes-core/include/Sinks/SinkCreator.hpp
+++ nes-core/include/Sinks/SinkCreator.hpp
@@ -267,7 +267,7 @@ DataSinkPtr createNetworkSink(const SchemaPtr& schema,
                               std::chrono::milliseconds waitTime,
                               FaultToleranceType::Value faultToleranceType = FaultToleranceType::NONE,
                               uint64_t numberOfOrigins = 1,
-                              uint8_t retryTimes = 500,
+                              uint8_t retryTimes = 5000000,
                               bool isBuffering = false);
 
 /**
