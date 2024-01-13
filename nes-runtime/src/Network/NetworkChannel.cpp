242ee077c601e98bb9bddb20cbb9cf86e7d073ff
AKozar <kozar.anastasiia@gmail.com>
2024-01-13 11:16:02 +0100

    ingestion rate

--- nes-runtime/src/Network/NetworkChannel.cpp
+++ nes-runtime/src/Network/NetworkChannel.cpp
@@ -51,10 +51,12 @@ std::unique_ptr<T> createNetworkChannel(std::shared_ptr<zmq::context_t> const& z
         ChannelId channelId(nesPartition, Runtime::NesThread::getId());
         zmq::socket_t zmqSocket(*zmqContext, ZMQ_DEALER);
         zmqSocket.set(zmq::sockopt::linger, linger);
+        const int zmqSize = 10000;
         // Sets the timeout for receive operation on the socket. If the value is 0, zmq_recv(3) will return immediately,
         // with a EAGAIN error if there is no message to receive. If the value is -1, it will block until a message is available.
         // For all other values, it will wait for a message for that amount of time before returning with an EAGAIN error.
         zmqSocket.set(zmq::sockopt::rcvtimeo, rcvtimeo);
+        zmqContext->set(zmq::ctxopt::max_sockets, zmqSize);
         // set the high watermark: this zmqSocket will accept only highWaterMark messages and then it ll block
         // until more space is available
         if (highWaterMark > 0) {
