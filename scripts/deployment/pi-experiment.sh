2065dc78670c160f7a502c803dd0fce83daaf6d7
digiou <digitalbckp@gmail.com>
2024-01-19 11:47:51 +0100

    Add full experiment in sh scripts

--- scripts/deployment/pi-experiment.sh
+++ scripts/deployment/pi-experiment.sh
@@ -1,36 +1,61 @@
 #!/bin/bash
 
-# Default experiment type
-experiment_type=${1:-fixed}
+# Function to run the experiment based on the experiment type
+run_experiment() {
+    local experiment_type=$1
+
+    echo "Running experiment type: $experiment_type"
+
+    # Run the corresponding Ansible playbook
+    if [ "$experiment_type" == "fixed" ]; then
+        echo "Running 'fixed' experiment..."
+        ansible-playbook start-pi.yml
+    elif [ "$experiment_type" == "chameleon" ]; then
+        echo "Running 'chameleon' experiment..."
+        ansible-playbook start-pi-chameleon.yml
+    else
+        echo "Unknown experiment type: $experiment_type"
+        exit 1
+    fi
+
+    # Sleep for a minute
+    echo "Sleeping for 6 seconds..."
+    sleep 6
+
+    # Send query to coordinator
+    echo "Sending query on coordinator..."
+    ansible-playbook pi-avg-query.yml
+
+    echo "Sleeping for 2 mins..."
+    sleep 120
+
+    # Stop everything
+    echo "Stopping everything..."
+    ansible-playbook stop-pi.yml
+
+    # Sleep for 6 seconds
+    echo "Sleeping for 6 seconds..."
+    sleep 6
+
+    echo "Experiment completed."
+}
+
+# Number of iterations
+iterations=${1:-5}
 
 echo "Deploying on pis..."
 ansible-playbook deploy-pi.yml
 
-# Run the corresponding Ansible playbook
-if [ "$experiment_type" == "fixed" ]; then
-    echo "Running 'fixed' experiment..."
-    ansible-playbook start-pi.yml
-elif [ "$experiment_type" == "chameleon" ]; then
-    echo "Running 'chameleon' experiment..."
-    ansible-playbook start-pi-chameleon.yml
-else
-    echo "Unknown experiment type: $experiment_type"
-    exit 1
-fi
+echo "Starting fixed iterations..."
+for (( i=1; i<=iterations; i++ ))
+do
+  run_experiment "fixed"
+done
 
-# Sleep for a minute
-echo "Sleeping for 60 seconds..."
-sleep 60
+echo "Starting Chameleon iterations..."
+for (( i=1; i<=iterations; i++ ))
+do
+  run_experiment "chameleon"
+done
 
-# Send query to coordinator
-echo "Sending query on coordinator..."
-ansible-playbook pi-avg-query.yml
-
-echo "Sleeping for 5 mins..."
-sleep 300
-
-# Send query to coordinator
-echo "Stopping everything..."
-ansible-playbook stop-pi.yml
-
-echo "Experiment completed."
+echo "All experiments completed."
