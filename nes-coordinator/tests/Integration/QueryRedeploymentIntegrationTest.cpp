d2d91f849bdbb8f580568dcf98d78bf156641c06
fkuzun <felix.s.lang@campus.tu-berlin.de>
2024-01-21 14:58:08 +0100

    WIP

--- nes-coordinator/tests/Integration/QueryRedeploymentIntegrationTest.cpp
+++ nes-coordinator/tests/Integration/QueryRedeploymentIntegrationTest.cpp
@@ -884,5 +884,144 @@ TEST_P(QueryRedeploymentIntegrationTest, testMultipleUnplannedReconnects) {
     ASSERT_TRUE(retStopCord);
 }
 
+#ifdef S2DEF
+TEST_F(QueryRedeploymentIntegrationTest, testSequenceWithReconnecting) {
+    NES_INFO(" start coordinator");
+    std::string testFile = getTestResourceFolder() / "sequence_with_reconnecting_out.csv";
+
+    std::string compareString;
+    std::ostringstream oss;
+    oss << "seq$value:INTEGER(64 bits)" << std::endl;
+    for (int i = 1; i <= 10000; ++i) {
+        oss << std::to_string(i) << std::endl;
+        compareString = oss.str();
+    }
+
+    NES_INFO("rest port = {}", *restPort);
+
+    CoordinatorConfigurationPtr coordinatorConfig = CoordinatorConfiguration::createDefault();
+    coordinatorConfig->rpcPort.setValue(*rpcCoordinatorPort);
+    coordinatorConfig->restPort.setValue(*restPort);
+    NES_INFO("start coordinator")
+    NesCoordinatorPtr crd = std::make_shared<NesCoordinator>(coordinatorConfig);
+    uint64_t port = crd->startCoordinator(/**blocking**/ false);
+    ASSERT_NE(port, 0UL);
+    NES_INFO("coordinator started successfully")
+
+    TopologyPtr topology = crd->getTopology();
+    ASSERT_TRUE(waitForNodes(5, 1, topology));
+
+    //TopologyNodePtr node = topology->getRootTopologyNodeId();
+    std::vector<NES::Spatial::DataTypes::Experimental::GeoLocation> locVec = {
+        {52.53024925374664, 13.440408001670573},  {52.44959193751221, 12.994693532702838},
+        {52.58394737653231, 13.404557656002641},  {52.48534029037908, 12.984138457171484},
+        {52.37433823627218, 13.558651957244951},  {52.51533875315059, 13.241771507925069},
+        {52.55973107205912, 13.015653271890772},  {52.63119966549814, 13.441159505328082},
+        {52.52554704888443, 13.140415389311752},  {52.482596286130494, 13.292443465145574},
+        {52.54298642356826, 13.73191525503437},   {52.42678133005856, 13.253118169911525},
+        {52.49621174869779, 13.660943763979146},  {52.45590365225229, 13.683553731893118},
+        {52.62859441558, 13.135969230535936},     {52.49564618880393, 13.333672868668472},
+        {52.58790396655713, 13.283405589901832},  {52.43730546215479, 13.288472865017477},
+        {52.452625895558846, 13.609715377620118}, {52.604381034747234, 13.236153100778251},
+        {52.52406858008703, 13.202905224067974},  {52.48532771063918, 13.248322218507269},
+        {52.50023010173765, 13.35516100143647},   {52.5655774963026, 13.416236069617133},
+        {52.56839177666675, 13.311990021109548},  {52.42881523569258, 13.539510531504995},
+        {52.55745803205775, 13.521177091034348},  {52.378590211721814, 13.39387224077735},
+        {52.45968932886132, 13.466172426273232},  {52.60131778672673, 13.6759151640276},
+        {52.59382248148305, 13.17751716953493},   {52.51690603363213, 13.627430091500505},
+        {52.40035318355461, 13.386405495784041},  {52.49369404130713, 13.503477002208028},
+        {52.52102316662499, 13.231109595273479},  {52.6264057419334, 13.239482930461145},
+        {52.45997462557177, 13.038370380285766},  {52.405581430754694, 12.994506535621692},
+        {52.5165220102255, 13.287867202522792},   {52.61937748717004, 13.607622490869543},
+        {52.620153404197254, 13.236774758123099}, {52.53095039302521, 13.150218024942914},
+        {52.60042748492653, 13.591960614892749},  {52.44688258081577, 13.091132219453291},
+        {52.44810624782493, 13.189186365976528},  {52.631904019035325, 13.099599387131189},
+        {52.51607843891218, 13.361003233097668},  {52.63920358795863, 13.365640690678045},
+        {52.51050545031392, 13.687455299147123},  {52.42516226249599, 13.597154340475155},
+        {52.585620728658185, 13.177440252255762}, {52.54251642039891, 13.270687079693818},
+        {52.62589583837628, 13.58922212327232},   {52.63840628658707, 13.336777486335386},
+        {52.382935034604074, 13.54689828854007},  {52.46173261319607, 13.637993027984113},
+        {52.45558349451082, 13.774558360650097},  {52.50660545385822, 13.171564805090318},
+        {52.38586011054127, 13.772290920473052},  {52.4010561708298, 13.426889487526187}};
+
+    std::vector<NesWorkerPtr> fieldNodes;
+    for (auto elem : locVec) {
+        WorkerConfigurationPtr wrkConf = WorkerConfiguration::create();
+        wrkConf->coordinatorPort.setValue(*rpcCoordinatorPort);
+        wrkConf->nodeSpatialType.setValue(NES::Spatial::Experimental::SpatialType::FIXED_LOCATION);
+        wrkConf->locationCoordinates.setValue(elem);
+        NesWorkerPtr wrk = std::make_shared<NesWorker>(std::move(wrkConf));
+        fieldNodes.push_back(wrk);
+        bool retStart = wrk->start(/**blocking**/ false, /**withConnect**/ true);
+        ASSERT_TRUE(retStart);
+    }
+    ASSERT_TRUE(waitForNodes(5, 61, topology));
+    string singleLocStart = "52.55227464714949, 13.351743136322877";
+    crd->getSourceCatalog()->addLogicalSource("seq", Schema::create()->addField(createField("value", BasicType::UINT64)));
+
+    NES_INFO("start worker 1");
+    WorkerConfigurationPtr wrkConf1 = WorkerConfiguration::create();
+    wrkConf1->coordinatorPort.setValue(*rpcCoordinatorPort);
+
+    auto stype = CSVSourceType::create("seq", "test_stream");
+    stype->setFilePath(std::filesystem::path(TEST_DATA_DIRECTORY) / "sequence_long.csv");
+    stype->setNumberOfBuffersToProduce(1000);
+    stype->setNumberOfTuplesToProducePerBuffer(10);
+    stype->setGatheringInterval(1);
+    wrkConf1->physicalSourceTypes.add(stype);
+
+    wrkConf1->nodeSpatialType.setValue(NES::Spatial::Experimental::SpatialType::MOBILE_NODE);
+    wrkConf1->mobilityConfiguration.nodeInfoDownloadRadius.setValue(20000);
+    wrkConf1->mobilityConfiguration.nodeIndexUpdateThreshold.setValue(5000);
+    wrkConf1->mobilityConfiguration.mobilityHandlerUpdateInterval.setValue(10);
+    wrkConf1->mobilityConfiguration.locationBufferSaveRate.setValue(1);
+    wrkConf1->mobilityConfiguration.pathPredictionLength.setValue(40000);
+    wrkConf1->mobilityConfiguration.defaultCoverageRadius.setValue(5000);
+    wrkConf1->mobilityConfiguration.mobilityHandlerUpdateInterval.setValue(1000);
+    wrkConf1->mobilityConfiguration.locationProviderType.setValue(
+        NES::Spatial::Mobility::Experimental::LocationProviderType::CSV);
+    wrkConf1->mobilityConfiguration.locationProviderConfig.setValue(std::filesystem::path(TEST_DATA_DIRECTORY) / "path1.csv");
+
+    NesWorkerPtr wrk1 = std::make_shared<NesWorker>(std::move(wrkConf1));
+    bool retStart1 = wrk1->start(/**blocking**/ false, /**withConnect**/ true);
+    ASSERT_TRUE(retStart1);
+    ASSERT_TRUE(waitForNodes(5, 62, topology));
+
+    QueryId queryId = crd->getRequestHandlerService()->validateAndQueueAddQueryRequest(
+        R"(Query::from("seq").sink(FileSinkDescriptor::create(")" + testFile + R"(", "CSV_FORMAT", "APPEND"));)",
+        Optimizer::PlacementStrategy::BottomUp);
+
+    NES_INFO("Query ID: {}", queryId);
+    ASSERT_NE(queryId, INVALID_QUERY_ID);
+    size_t recv_tuples = 0;
+    auto startTimestamp = std::chrono::system_clock::now();
+    while (recv_tuples < 10000 && std::chrono::system_clock::now() < startTimestamp + defaultTimeoutInSec) {
+        std::ifstream inFile(testFile);
+        recv_tuples = std::count(std::istreambuf_iterator<char>(inFile), std::istreambuf_iterator<char>(), '\n');
+        NES_DEBUG("received: {}", recv_tuples)
+        sleep(1);
+    }
+
+    ASSERT_EQ(recv_tuples, 10001);
+    ASSERT_TRUE(TestUtils::checkOutputOrTimeout(compareString, testFile));
+
+    int response = remove(testFile.c_str());
+    ASSERT_TRUE(response == 0);
+
+    cout << "stopping worker" << endl;
+    bool retStopWrk = wrk1->stop(false);
+    ASSERT_TRUE(retStopWrk);
+
+    for (const auto& w : fieldNodes) {
+        bool stop = w->stop(false);
+        ASSERT_TRUE(stop);
+    }
+
+    cout << "stopping coordinator" << endl;
+    bool retStopCord = crd->stopCoordinator(false);
+    ASSERT_TRUE(retStopCord);
+}
+#endif
+
 INSTANTIATE_TEST_CASE_P(QueryRedeploymentIntegrationTestParam, QueryRedeploymentIntegrationTest, ::testing::Values(1, 4));
 }// namespace NES
