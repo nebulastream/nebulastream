953b001d25fc794dda722b6f5633fc71160a444f
Lukas Schwerdtfeger <lukas.schwerdtfeger@gmail.com>
2024-01-28 22:21:45 +0100

    Export: Move NoOp implementations

--- nes-unikernel-export/src/NoOp/NoOpQueryManager.cpp
+++ nes-unikernel-export/src/NoOp/NoOpQueryManager.cpp
@@ -1,4 +1,18 @@
 /*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+ */
+
+/*
      Licensed under the Apache License, Version 2.0 (the "License");
      you may not use this file except in compliance with the License.
      You may obtain a copy of the License at
@@ -11,7 +25,8 @@
      See the License for the specific language governing permissions and
      limitations under the License.
 */
-#include <NoOpQueryManager.h>
+
+#include <NoOp/NoOpQueryManager.h>
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wunused-parameter"
 bool NES::Runtime::AbstractQueryManager::registerQuery(const Execution::ExecutableQueryPlanPtr& qep) {
