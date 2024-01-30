42782732a95ec12b1e618122e01ee1daf58915d0
fkuzun <felix.s.lang@campus.tu-berlin.de>
2024-01-30 18:45:20 +0100

    commented execution plan cleanup out

--- nes-coordinator/src/Phases/QueryDeploymentPhase.cpp
+++ nes-coordinator/src/Phases/QueryDeploymentPhase.cpp
@@ -152,23 +152,24 @@ void QueryDeploymentPhase::execute(const SharedQueryPlanPtr& sharedQueryPlan) {
     startQuery(sharedQueryId, executionNodes);
 
     //remove subplans from global query plan if they were stopped due to migration
-    for (auto node : executionNodes) {
-        auto allDecomposedQueryPlans = node->getAllDecomposedQueryPlans(sharedQueryId);
-        for (auto decomposedQueryPlan : allDecomposedQueryPlans) {
-            auto subPlanStatus = decomposedQueryPlan->getState();
-            if (subPlanStatus == QueryState::MIGRATING || subPlanStatus == QueryState::MIGRATION_COMPLETED) {
-                NES_INFO("Removing migrated subplan with id  {} from execution node {}", decomposedQueryPlan->getDecomposedQueryPlanId(), node->getId())
-                //globalExecutionPlan->getExecutionNodeById(node->getId())->removeDecomposedQueryPlan(sharedQueryId, decomposedQueryPlan->getDecomposedQueryPlanId());
-                globalExecutionPlan->removeQuerySubPlanFromNode(node->getId(),
-                                                                sharedQueryId,
-                                                                decomposedQueryPlan->getDecomposedQueryPlanId());
-//                node->removeDecomposedQueryPlan(sharedQueryId,
+    //todo: why does this fail?
+//    for (auto node : executionNodes) {
+//        auto allDecomposedQueryPlans = node->getAllDecomposedQueryPlans(sharedQueryId);
+//        for (auto decomposedQueryPlan : allDecomposedQueryPlans) {
+//            auto subPlanStatus = decomposedQueryPlan->getState();
+//            if (subPlanStatus == QueryState::MIGRATING || subPlanStatus == QueryState::MIGRATION_COMPLETED) {
+//                NES_INFO("Removing migrated subplan with id  {} from execution node {}", decomposedQueryPlan->getDecomposedQueryPlanId(), node->getId())
+//                //globalExecutionPlan->getExecutionNodeById(node->getId())->removeDecomposedQueryPlan(sharedQueryId, decomposedQueryPlan->getDecomposedQueryPlanId());
+//                globalExecutionPlan->removeQuerySubPlanFromNode(node->getId(),
+//                                                                sharedQueryId,
 //                                                                decomposedQueryPlan->getDecomposedQueryPlanId());
-                auto resourceAmount = Optimizer::ExecutionNode::getOccupiedResourcesForDecomposedQueryPlan(decomposedQueryPlan);
-                node->getTopologyNode()->releaseSlots(resourceAmount);
-            }
-        }
-    }
+////                node->removeDecomposedQueryPlan(sharedQueryId,
+////                                                                decomposedQueryPlan->getDecomposedQueryPlanId());
+//                auto resourceAmount = Optimizer::ExecutionNode::getOccupiedResourcesForDecomposedQueryPlan(decomposedQueryPlan);
+//                node->getTopologyNode()->releaseSlots(resourceAmount);
+//            }
+//        }
+//    }
 }
 
 void QueryDeploymentPhase::deployQuery(SharedQueryId sharedQueryId,
