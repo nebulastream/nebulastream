fe32e67890f96ef971eb8b8eb12371951a086da7
Dwi Nugroho <d.nugroho@tu-berlin.de>
2024-02-12 10:11:08 +0100

    #4370: added a minimum reproducible error
    #4370: append argument types to the function name
    
    #4376: append external function name in c++ with arg types
    
    #4376: append function names with args at trace level & revert changes in the backend
    
    #4376: revert changes
    
    #4376: enable tests

--- nes-execution/tests/UnitTests/Execution/PipelineExecution/NonKeyedTimeWindowPipelineTest.cpp
+++ nes-execution/tests/UnitTests/Execution/PipelineExecution/NonKeyedTimeWindowPipelineTest.cpp
@@ -263,6 +263,109 @@ TEST_P(NonKeyedTimeWindowPipelineTest, windowWithMultiAggregates) {
 
 }// namespace NES::Runtime::Execution
 
+/**
+ * @brief Test running a pipeline containing a threshold window with two min aggregations with different data types
+ */
+TEST_P(NonKeyedTimeWindowPipelineTest, windowWithMultiAggregatesOnDifferentDataType) {
+    auto scanSchema = Schema::create(Schema::MemoryLayoutType::ROW_LAYOUT);
+    scanSchema->addField("f1", BasicType::INT64);
+    scanSchema->addField("f2", BasicType::INT64);
+    scanSchema->addField("f3", BasicType::FLOAT32);
+    scanSchema->addField("ts", BasicType::INT64);
+    auto scanMemoryLayout = Runtime::MemoryLayouts::RowLayout::create(scanSchema, bm->getBufferSize());
+
+    auto scanMemoryProviderPtr = std::make_unique<MemoryProvider::RowMemoryProvider>(scanMemoryLayout);
+    auto scanOperator = std::make_shared<Operators::Scan>(std::move(scanMemoryProviderPtr));
+
+    auto readF1 = std::make_shared<Expressions::ReadFieldExpression>("f1");
+    auto readF2 = std::make_shared<Expressions::ReadFieldExpression>("f2");
+    auto readF3 = std::make_shared<Expressions::ReadFieldExpression>("f3");
+    auto readTsField = std::make_shared<Expressions::ReadFieldExpression>("ts");
+    auto aggregationResultFieldName1 = "test$min_i64";
+    auto aggregationResultFieldName2 = "test$min_f32";
+    auto physicalTypeFactory = DefaultPhysicalTypeFactory();
+    PhysicalTypePtr integerType = physicalTypeFactory.getPhysicalType(DataTypeFactory::createInt64());
+    PhysicalTypePtr floatType = physicalTypeFactory.getPhysicalType(DataTypeFactory::createFloat());
+
+    std::vector<std::shared_ptr<Aggregation::AggregationFunction>> aggregationFunctions = {
+        std::make_shared<Aggregation::MinAggregationFunction>(integerType, integerType, readF2, aggregationResultFieldName1),
+        std::make_shared<Aggregation::MinAggregationFunction>(floatType, floatType, readF3, aggregationResultFieldName2)
+            };
+    auto slicePreAggregation =
+        std::make_shared<Operators::NonKeyedSlicePreAggregation>(0 /*handler index*/,
+                                                                 std::make_unique<Operators::EventTimeFunction>(readTsField),
+                                                                 aggregationFunctions);
+    scanOperator->setChild(slicePreAggregation);
+    auto preAggPipeline = std::make_shared<PhysicalOperatorPipeline>();
+    preAggPipeline->setRootOperator(scanOperator);
+    auto sliceMergingAction = std::make_unique<Operators::NonKeyedWindowEmitAction>(aggregationFunctions,
+                                                                                    "start",
+                                                                                    "end",
+                                                                                    /*origin id*/ 0);
+    auto sliceMerging = std::make_shared<Operators::NonKeyedSliceMerging>(0 /*handler index*/,
+                                                                          aggregationFunctions,
+                                                                          std::move(sliceMergingAction));
+    auto emitSchema = Schema::create(Schema::MemoryLayoutType::ROW_LAYOUT);
+    emitSchema = emitSchema->addField("test$min_i64", BasicType::INT64)
+                     ->addField("test$min_f32", BasicType::FLOAT32);
+    auto emitMemoryLayout = Runtime::MemoryLayouts::RowLayout::create(emitSchema, bm->getBufferSize());
+    auto emitMemoryProviderPtr = std::make_unique<MemoryProvider::RowMemoryProvider>(emitMemoryLayout);
+    auto emitOperator = std::make_shared<Operators::Emit>(std::move(emitMemoryProviderPtr));
+    sliceMerging->setChild(emitOperator);
+    auto sliceMergingPipeline = std::make_shared<PhysicalOperatorPipeline>();
+    sliceMergingPipeline->setRootOperator(sliceMerging);
+
+    auto buffer = bm->getBufferBlocking();
+    auto dynamicBuffer = Runtime::MemoryLayouts::DynamicTupleBuffer(scanMemoryLayout, buffer);
+
+    // Fill buffer
+    dynamicBuffer[0]["f1"].write(+1_s64);
+    dynamicBuffer[0]["f2"].write(+10_s64);
+    dynamicBuffer[0]["f3"].write((float) 0.5);
+    dynamicBuffer[0]["ts"].write(+1_s64);
+    dynamicBuffer[1]["f1"].write(+2_s64);
+    dynamicBuffer[1]["f2"].write(+20_s64);
+    dynamicBuffer[1]["f3"].write((float) 0.5);
+    dynamicBuffer[1]["ts"].write(+1_s64);
+    dynamicBuffer[2]["f1"].write(+3_s64);
+    dynamicBuffer[2]["f2"].write(+30_s64);
+    dynamicBuffer[2]["f3"].write((float) 0.5);
+    dynamicBuffer[2]["ts"].write(+2_s64);
+    dynamicBuffer[3]["f1"].write(+1_s64);
+    dynamicBuffer[3]["f2"].write(+40_s64);
+    dynamicBuffer[3]["f3"].write((float) 0.5);
+    dynamicBuffer[3]["ts"].write(+3_s64);
+    dynamicBuffer.setNumberOfTuples(4);
+    buffer.setWatermark(20);
+    buffer.setSequenceNumber(1);
+    buffer.setOriginId(0);
+
+    auto preAggExecutablePipeline = provider->create(preAggPipeline, options);
+    std::vector<OriginId> origins = {0};
+    auto preAggregationHandler = std::make_shared<Operators::NonKeyedSlicePreAggregationHandler>(10, 10, origins);
+
+    auto pipeline1Context = MockedPipelineExecutionContext({preAggregationHandler});
+    preAggExecutablePipeline->setup(pipeline1Context);
+
+    auto sliceMergingExecutablePipeline = provider->create(sliceMergingPipeline, options);
+    auto sliceMergingHandler = std::make_shared<Operators::NonKeyedSliceMergingHandler>();
+
+    auto pipeline2Context = MockedPipelineExecutionContext({sliceMergingHandler});
+    sliceMergingExecutablePipeline->setup(pipeline2Context);
+
+    preAggExecutablePipeline->execute(buffer, pipeline1Context, *wc);
+    EXPECT_EQ(pipeline1Context.buffers.size(), 1);
+    sliceMergingExecutablePipeline->execute(pipeline1Context.buffers[0], pipeline2Context, *wc);
+
+    EXPECT_EQ(pipeline2Context.buffers.size(), 1);
+    preAggExecutablePipeline->stop(pipeline1Context);
+    sliceMergingExecutablePipeline->stop(pipeline2Context);
+
+    auto resultDynamicBuffer = Runtime::MemoryLayouts::DynamicTupleBuffer(emitMemoryLayout, pipeline2Context.buffers[0]);
+    EXPECT_EQ(resultDynamicBuffer[0][0].read<int64_t>(), 10);
+    EXPECT_EQ(resultDynamicBuffer[0][1].read<float>(), 0.5);
+}// namespace NES::Runtime::Execution
+
 INSTANTIATE_TEST_CASE_P(testIfCompilation,
                         NonKeyedTimeWindowPipelineTest,
                         ::testing::Values("PipelineInterpreter", "PipelineCompiler", "CPPPipelineCompiler"),
