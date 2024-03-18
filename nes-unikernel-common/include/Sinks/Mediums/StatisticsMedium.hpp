5f74b3aa6ed5716b9076ae69a5fea610e9743c7d
lukas schwerdtfeger <lukas.schwerdtfeger@gmail.com>
2024-03-18 18:15:41 +0100

    Sink: Concurrent throughput

--- nes-unikernel-common/include/Sinks/Mediums/StatisticsMedium.hpp
+++ nes-unikernel-common/include/Sinks/Mediums/StatisticsMedium.hpp
@@ -1,6 +1,32 @@
 
 #ifndef STATISTICSMEDIUM_HPP
 #define STATISTICSMEDIUM_HPP
+
+struct Counter {
+    explicit Counter(const std::chrono::seconds& duration) : duration(duration) {}
+    std::chrono::seconds duration;
+    std::chrono::high_resolution_clock::time_point last_interval = std::chrono::high_resolution_clock::now();
+    std::atomic<uint64_t> counter = 0;
+    size_t last_counter = 0;
+    std::atomic_bool running = true;
+    std::jthread runner = std::jthread([this]() {
+        while (running.load()) {
+            std::this_thread::sleep_for(duration);
+            auto now = std::chrono::high_resolution_clock::now();
+            auto current_counter = counter.load();
+            if (current_counter > last_counter) {
+                const auto elapsedTime = std::chrono::duration_cast<std::chrono::duration<double>>(now - last_interval);
+                NES_INFO("Received {} tuples in {}s ({}tps)",
+                         (current_counter - last_counter),
+                         elapsedTime.count(),
+                         static_cast<double>((current_counter - last_counter)) / elapsedTime.count());
+            }
+            last_interval = now;
+            last_counter = current_counter;
+        }
+    });
+};
+
 namespace NES {
 class StatisticsMedium : public SinkMedium {
   public:
@@ -9,57 +35,24 @@ class StatisticsMedium : public SinkMedium {
                      QueryId queryId,
                      QuerySubPlanId querySubPlanId,
                      std::chrono::seconds duration)
-        : SinkMedium(std::move(sinkFormat), numOfProducers, queryId, querySubPlanId), duration(std::move(duration)) {}
+        : SinkMedium(std::move(sinkFormat), numOfProducers, queryId, querySubPlanId), counter(std::move(duration)) {}
     void setup() override;
-    void shutdown() override {
-        const auto current = std::chrono::high_resolution_clock::now();
-        const auto newCounter = totalCounter + newTupleCounter;
-        const auto elapsedTime = std::chrono::duration_cast<std::chrono::duration<double>>(current - last_interval);
-
-        NES_INFO("Received {} tuples in {}s ({}tps)",
-                 (newCounter - totalCounter),
-                 elapsedTime.count(),
-                 static_cast<double>((newCounter - totalCounter)) / elapsedTime.count());
-
-        last_interval = std::chrono::high_resolution_clock::now();
-        totalCounter = 0;
-        newTupleCounter = 0;
-    }
+    void shutdown() override {}
     bool writeData(Runtime::TupleBuffer& inputBuffer, Runtime::WorkerContext& workerContext) override;
     std::string toString() const override;
     SinkMediumTypes getSinkMediumType() override;
 
-    std::chrono::seconds duration;
-    std::chrono::high_resolution_clock::time_point last_interval = std::chrono::high_resolution_clock::now();
-    size_t totalCounter = 0;
-    size_t newTupleCounter = 0;
+    Counter counter;
 };
 
-inline void StatisticsMedium::setup() {
-    last_interval = std::chrono::high_resolution_clock::now();
-    totalCounter = 0;
-    newTupleCounter = 0;
-}
+inline void StatisticsMedium::setup() {}
 
 inline bool StatisticsMedium::writeData(Runtime::TupleBuffer& inputBuffer, Runtime::WorkerContext&) {
-    newTupleCounter += inputBuffer.getNumberOfTuples();
-    const auto current = std::chrono::high_resolution_clock::now();
-    if (last_interval + duration < current) {
-        const auto newCounter = totalCounter + newTupleCounter;
-        const auto elapsedTime = std::chrono::duration_cast<std::chrono::duration<double>>(current - last_interval);
-
-        NES_INFO("Received {} tuples in {}s ({}tps)",
-                 (newCounter - totalCounter),
-                 elapsedTime.count(),
-                 static_cast<double>((newCounter - totalCounter)) / elapsedTime.count());
-
-        last_interval = current;
-        newTupleCounter = 0;
-        totalCounter = newCounter;
-    }
-
+    auto numberOfTuples = inputBuffer.getNumberOfTuples();
+    counter.counter.fetch_add(numberOfTuples);
     return true;
 }
+
 inline std::string StatisticsMedium::toString() const { return "StatisticsMedium"; }
 inline SinkMediumTypes StatisticsMedium::getSinkMediumType() { return SinkMediumTypes::MONITORING_SINK; }
 }// namespace NES
