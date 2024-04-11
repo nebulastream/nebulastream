516342d10f12ed030790330eab44b0673a4bad83
Lukas Schwerdtfeger <lukas.schwerdtfeger@gmail.com>
2024-04-11 20:09:47 +0200

    [#4786] Annotates invokeErrorHandlers function as `noreturn` (#4787)
    ## What is the purpose of the change
    
    This removes false-positive possible nullpointer warnings following an
    assertion.
    
    ## Brief change log
    
    Annotates `NES::Exceptions::invokeErrorHandlers` with `[noreturn]` to
    tell the compiler, that a failed assertion will no allow subsequent code
    to be executed.
    
    ## Verifying this change
    
    This change is a trivial rework / code cleanup without any test
    coverage.
    
    ## Does this pull request potentially affect one of the following parts:
    
      - Dependencies (does it add or upgrade a dependency): no
      - The compiler: no
      - The threading model: no
      - The Runtime per-record code paths (performance sensitive): no
      - The network stack: no
    - Anything that affects deployment or recovery: Coordinator (and its
    components), NodeEngine (and its components): no
    
    ## Documentation
    
      - Does this pull request introduce a new feature? no
      - If yes, how is the feature documented? no
    
    ## Issue Closed by this pull request:
    
    This PR closes #4786

--- nes-common/include/Exceptions/SignalHandling.hpp
+++ nes-common/include/Exceptions/SignalHandling.hpp
@@ -15,6 +15,7 @@
 #define NES_COMMON_INCLUDE_EXCEPTIONS_SIGNALHANDLING_HPP_
 #include <exception>
 #include <memory>
+#include <string>
 
 namespace NES::Exceptions {
 class ErrorListener;
@@ -24,21 +25,21 @@ class ErrorListener;
  * @param signal which indicates the error
  * @param stacktrace the stacktrace of where the error was raised
  */
-void invokeErrorHandlers(int signal, std::string&& stacktrace);
+[[noreturn]] void invokeErrorHandlers(int signal, std::string&& stacktrace);
 
 /**
  * @brief calls to this function will pass an exception to all system-wide error listeners
  * @param exception which indicates the error
  * @param stacktrace the stacktrace of where the error was raised
  */
-void invokeErrorHandlers(std::shared_ptr<std::exception> exception, std::string&& stacktrace);
+[[noreturn]] void invokeErrorHandlers(std::shared_ptr<std::exception> exception, std::string&& stacktrace);
 
 /**
  * @brief calls to this function will create a RuntimeException that is passed to all system-wide error listeners
  * @param buffer the message of the exception
  * @param stacktrace the stacktrace of where the error was raised
  */
-void invokeErrorHandlers(const std::string& buffer, std::string&& stacktrace);
+[[noreturn]] void invokeErrorHandlers(const std::string& buffer, std::string&& stacktrace);
 
 /**
  * @brief make an error listener system-wide
