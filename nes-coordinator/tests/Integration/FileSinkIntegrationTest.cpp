400acf421e2ac024b37d09f1d8f8de451ec9635b
Viktor Rosenfeld <viktor.rosenfeld@tu-berlin.de>
2024-05-15 15:10:35 +0200

    [#4806] Extract logic from tests

--- nes-coordinator/tests/Integration/FileSinkIntegrationTest.cpp
+++ nes-coordinator/tests/Integration/FileSinkIntegrationTest.cpp
@@ -24,23 +24,33 @@ public:
     static void SetUpTestCase() {
         NES::Logger::setupLogging("FileSinkIntegrationTest.log", NES::LogLevel::LOG_DEBUG);
     }
+    TestHarness createTestHarness(const Query& query) const {
+        struct Input { uint64_t id; };
+        const auto schema = TestSchemas::getSchemaTemplate("id_u64")->updateSourceName(logicalSourceName);
+        TestHarness testHarness {query, *restPort, *rpcCoordinatorPort, getTestResourceFolder()};
+        testHarness.addLogicalSource(logicalSourceName, schema).attachWorkerWithMemorySourceToCoordinator(logicalSourceName);
+        testHarness.pushElement<Input>({1}, 2);
+        testHarness.validate().setupTopology();
+        return testHarness;
+    }
+    void runQueryAndVerifyFailureState(const TestHarness& testHarness, const Query& query) {
+        auto coordinator = testHarness.getCoordinator();
+        QueryId queryId = coordinator->getRequestHandlerService()->validateAndQueueAddQueryRequest(query.getQueryPlan(), NES::Optimizer::PlacementStrategy::BottomUp);
+        EXPECT_TRUE(TestUtils::checkFailedOrTimeout(queryId, coordinator->getQueryCatalog()));
+    }
+    const std::string logicalSourceName = "logicalSource";
 };
 
 TEST_F(FileSinkIntegrationTest, canWriteOutputFile) {
     // The test query copies the contents of the input source to the output CSV file of the test harness.
-    const std::string logicalSourceName = "logicalSource";
     const Query query = Query::from(logicalSourceName);
 
     // Setup a test harness with a memory source that contains a single input tuple.
-    struct Input { uint64_t id; };
-    const auto schema = TestSchemas::getSchemaTemplate("id_u64")->updateSourceName(logicalSourceName);
-    TestHarness testHarness {query, *restPort, *rpcCoordinatorPort, getTestResourceFolder()};
-    testHarness.addLogicalSource(logicalSourceName, schema).attachWorkerWithMemorySourceToCoordinator(logicalSourceName);
-    testHarness.pushElement<Input>({1}, 2);
+    TestHarness testHarness = createTestHarness(query);
 
     // Run the query and compare the output to the expected value.
     const std::string expectedOutput = "1\n";
-    auto actualBuffers = testHarness.validate().setupTopology().runQuery(NES::Util::countLines(expectedOutput)).getOutput();
+    auto actualBuffers = testHarness.runQuery(NES::Util::countLines(expectedOutput)).getOutput();
     auto tmpBuffer = TestUtils::createExpectedBufferFromCSVString(expectedOutput, testHarness.getOutputSchema(), testHarness.getBufferManager());
     auto expectedBuffers = TestUtils::createTestTupleBuffers(tmpBuffer, testHarness.getOutputSchema());
     EXPECT_TRUE(TestUtils::buffersContainSameTuples(expectedBuffers, actualBuffers));
@@ -48,23 +58,15 @@ TEST_F(FileSinkIntegrationTest, canWriteOutputFile) {
 
 TEST_F(FileSinkIntegrationTest, cannotWriteOutputFile) {
     // The test query tries to copy the contents of the input source to an output CSV file inside a folder that does not exist.
-    const std::string logicalSourceName = "logicalSource";
     const std::string outputFilePath = getTestResourceFolder() / "bad_folder" / "output.csv";
     const Query query = Query::from(logicalSourceName)
         .sink(FileSinkDescriptor::create(outputFilePath, "CSV_FORMAT", "APPEND"));
 
     // Setup a test harness with a memory source that contains a single input tuple.
-    struct Input { uint64_t id; };
-    const auto schema = TestSchemas::getSchemaTemplate("id_u64")->updateSourceName(logicalSourceName);
-    TestHarness testHarness {query, *restPort, *rpcCoordinatorPort, getTestResourceFolder()};
-    testHarness.addLogicalSource(logicalSourceName, schema).attachWorkerWithMemorySourceToCoordinator(logicalSourceName);
-    testHarness.pushElement<Input>({1}, 2);
-    testHarness.validate().setupTopology();
+    TestHarness testHarness = createTestHarness(query);
 
     // Run the query and verify that its status is FAILED in the query catalog
-    auto coordinator = testHarness.getCoordinator();
-    QueryId queryId = coordinator->getRequestHandlerService()->validateAndQueueAddQueryRequest(query.getQueryPlan(), NES::Optimizer::PlacementStrategy::BottomUp);
-    EXPECT_TRUE(TestUtils::checkFailedOrTimeout(queryId, coordinator->getQueryCatalog()));
+    runQueryAndVerifyFailureState(testHarness, query);
 }
 
 TEST_F(FileSinkIntegrationTest, cannotRemoveOutputFile) {
@@ -76,22 +78,14 @@ TEST_F(FileSinkIntegrationTest, cannotRemoveOutputFile) {
     dummyFile.close();
 
     // The test query outputs results to the folder, which cannot be removed.
-    const std::string logicalSourceName = "logicalSource";
     const Query query = Query::from(logicalSourceName)
         .sink(FileSinkDescriptor::create(folder, "CSV_FORMAT", "OVERWRITE"));
 
     // Setup a test harness with a memory source that contains a single input tuple.
-    struct Input { uint64_t id; };
-    const auto schema = TestSchemas::getSchemaTemplate("id_u64")->updateSourceName(logicalSourceName);
-    TestHarness testHarness {query, *restPort, *rpcCoordinatorPort, getTestResourceFolder()};
-    testHarness.addLogicalSource(logicalSourceName, schema).attachWorkerWithMemorySourceToCoordinator(logicalSourceName);
-    testHarness.pushElement<Input>({1}, 2);
-    testHarness.validate().setupTopology();
+    TestHarness testHarness = createTestHarness(query);
 
     // Run the query and verify that its status is FAILED in the query catalog
-    auto coordinator = testHarness.getCoordinator();
-    QueryId queryId = coordinator->getRequestHandlerService()->validateAndQueueAddQueryRequest(query.getQueryPlan(), NES::Optimizer::PlacementStrategy::BottomUp);
-    EXPECT_TRUE(TestUtils::checkFailedOrTimeout(queryId, coordinator->getQueryCatalog()));
+    runQueryAndVerifyFailureState(testHarness, query);
 }
 
 
