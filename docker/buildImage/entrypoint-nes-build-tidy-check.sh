c2a7531e9383f3bac44d1144b1d78358afe43623
Nils Schubert <nils.schubert@dima.tu-berlin.de>
2024-07-18 16:41:31 +0200

    [#100] [#100] Added pyyaml for installation in the docker image

--- docker/buildImage/entrypoint-nes-build-tidy-check.sh
+++ docker/buildImage/entrypoint-nes-build-tidy-check.sh
@@ -27,5 +27,11 @@ cd /nebulastream/build
 CLANG_TIDY_EXECUTABLE=$(cmake -DCMAKE_BUILD_TYPE=Release -DBoost_NO_SYSTEM_PATHS=TRUE -DNES_SELF_HOSTING=1 -DNES_USE_OPC=0 -DNES_USE_MQTT=1 -DNES_BUILD_PLUGIN_ONNX=1 -DNES_BUILD_PLUGIN_TENSOR_FLOW=1 -DNES_USE_S2=1 .. 2>&1 | grep "$CLANG_TIDY_EXECUTABLE" | cut -d '=' -f2)
 echo "CLANG_TIDY_EXECUTABLE: $CLANG_TIDY_EXECUTABLE"
 
+
+# For the script to work, we need to install pyyaml and for that we need pip
+apt-get update
+apt-get install -y python3-pip
+pip install pyyaml
+
 # run clang-tidy and pass the contents of the file to the script via stdin
-cat $GIT_DIFF_FILE_NAME | python3 /nebulastream/scripts/build/run_clang_tidy_diff.py -clang-tidy-binary "$CLANG_TIDY_EXECUTABLE" -p1 -j 4 -path /nebulastream/build -export-fixes /clang-tidy-result/fixes.yaml
+cat $GIT_DIFF_FILE_NAME | python3 /nebulastream/scripts/build/run_clang_tidy_diff.py -clang-tidy-binary "$CLANG_TIDY_EXECUTABLE" -p1 -j 4 -path /nebulastream/build -export-fixes /clang-tidy-result/fixes.yml
