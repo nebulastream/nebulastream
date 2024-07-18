a1f31858457282066453d0ac82737fa430cffaec
Nils Schubert <nils.schubert@dima.tu-berlin.de>
2024-07-18 14:54:50 +0200

    [#100] Added space in our fix-guards command so that our clang-format and fix-guard do not interfere with eachother.

--- cmake/GuardOnce.cmake
+++ cmake/GuardOnce.cmake
@@ -11,7 +11,7 @@ macro(project_enable_fixguards)
     # converts to `#pragma once` and then back to include guard, so that guarding variable is derived from file path
     add_custom_target(fix-guards
         COMMAND PYTHONPATH=${guardonce_SOURCE_DIR} python3 -m guardonce.guard2once -r -e="nes-common/include/Version/version.hpp" nes-*/
-        COMMAND PYTHONPATH=${guardonce_SOURCE_DIR} python3 -m guardonce.once2guard -r -e="nes-commom/include/Version/version.hpp" -p 'path | append _ | upper' -s '\#endif// %\\n' nes-*/
+        COMMAND PYTHONPATH=${guardonce_SOURCE_DIR} python3 -m guardonce.once2guard -r -e="nes-commom/include/Version/version.hpp" -p 'path | append _ | upper' -s '\#endif // %\\n' nes-*/
 
         WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
     )
