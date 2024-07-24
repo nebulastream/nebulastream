af85acaf9c873955a7c141269e0c96c8f186388d
Nils Schubert <nils.schubert@dima.tu-berlin.de>
2024-07-24 16:43:48 +0200

    [#137] feat(Docker on Demand) Created workflow for creating docker iamges on demand

--- docker/scripts/build_docker_image_executable_image.sh
+++ docker/scripts/build_docker_image_executable_image.sh
@@ -13,7 +13,7 @@ PACKAGE_NAME_DEFAULT="amd64"
 PLATFORM_NAME_DEFAULT="linux/amd64"
 BUILD_DIR_DEFAULT="./build_dir"
 CCACHE_DIR_DEFAULT="none"
-NEBULA_STREAM_PATH=$(cd .. && pwd) # We expect the script to be executed from the docker directory
+NEBULA_STREAM_PATH=$(realpath ..) # We expect the script to be executed from the docker directory
 DOCKERFILE_PATH_DEFAULT="./executableImage"
 DOCKERFILE_PATH_BUILD_IMAGE_DEFAULT="./buildImage"
 DOCKER_FILE_NAME_DEFAULT="Dockerfile-NES-Executable-Multi-Arch"
@@ -21,17 +21,31 @@ NUM_THREADS_DEFAULT=$(nproc)
 
 # Assign arguments with default fallbacks
 TAG="${1:-$TAG_DEFAULT}"
-BUILD_DIR="${2:-$BUILD_DIR_DEFAULT}"
-CCACHE_DIR="${3:-$CCACHE_DIR_DEFAULT}"
-NEBULA_STREAM_PATH="${4:-$NEBULA_STREAM_PATH}"
+ARCH="${2:-$ARCH_DEFAULT}"
+PACKAGE_NAME="${3:-$PACKAGE_NAME_DEFAULT}"
+PLATFORM_NAME="${4:-$PLATFORM_NAME_DEFAULT}"
 IMAGE_NAME="${5:-$IMAGE_NAME_DEFAULT}"
-ARCH="${6:-$ARCH_DEFAULT}"
-PACKAGE_NAME="${7:-$PACKAGE_NAME_DEFAULT}"
-PLATFORM_NAME="${8:-$PLATFORM_NAME_DEFAULT}"
+BUILD_DIR="${6:-$BUILD_DIR_DEFAULT}"
+CCACHE_DIR="${7:-$CCACHE_DIR_DEFAULT}"
+NEBULA_STREAM_PATH="${8:-$NEBULA_STREAM_PATH}"
 DOCKERFILE_PATH="${9:-$DOCKERFILE_PATH_DEFAULT}"
 DOCKER_FILE_NAME="${10:-$DOCKER_FILE_NAME_DEFAULT}"
 NUM_THREADS="${11:-$NUM_THREADS_DEFAULT}"
 DOCKERFILE_PATH_BUILD_IMAGE="${12:-$DOCKERFILE_PATH_BUILD_IMAGE_DEFAULT}"
+
+# Printing out all the arguments
+echo "TAG: $TAG"
+echo "ARCH: $ARCH"
+echo "PACKAGE_NAME: $PACKAGE_NAME"
+echo "PLATFORM_NAME: $PLATFORM_NAME"
+echo "BUILD_DIR: $BUILD_DIR"
+echo "CCACHE_DIR: $CCACHE_DIR"
+echo "NEBULA_STREAM_PATH: $NEBULA_STREAM_PATH"
+echo "IMAGE_NAME: $IMAGE_NAME"
+echo "DOCKERFILE_PATH: $DOCKERFILE_PATH"
+echo "DOCKER_FILE_NAME: $DOCKER_FILE_NAME"
+echo "NUM_THREADS: $NUM_THREADS"
+echo "DOCKERFILE_PATH_BUILD_IMAGE: $DOCKERFILE_PATH_BUILD_IMAGE"
 
 # Check if Dockerfile exists
 if [ ! -f "$DOCKERFILE_PATH/$DOCKER_FILE_NAME" ]; then
@@ -51,39 +65,34 @@ if [ "$CCACHE_DIR" != "NONE" ]; then
     mkdir -p $CCACHE_DIR
 fi
 
-# Ask the user if they want to proceed with building the Docker image
-echo "Do you want to build the Docker image with the name $IMAGE_NAME and tag $TAG from the file $DOCKERFILE_PATH?
-We first have to compile NebulaStream to create an executable image. We will use $NUM_THREADS threads for the compilation.
-Would you like to continue as this might take some time? (y/n)"
-read -r user_confirmation
+# Getting for all paths the absolute path
+BUILD_DIR=$(realpath $BUILD_DIR)
+CCACHE_DIR=$(realpath $CCACHE_DIR)
+NEBULA_STREAM_PATH=$(realpath $NEBULA_STREAM_PATH)
+DOCKERFILE_PATH=$(realpath $DOCKERFILE_PATH)
+DOCKERFILE_PATH_BUILD_IMAGE=$(realpath $DOCKERFILE_PATH_BUILD_IMAGE)
 
-if [[ $user_confirmation == "y" || $user_confirmation == "Y" ]]; then
-    # Build the Docker Image for Prepare Package
-    docker_image_name_creating_package="nes_package"
-    docker_runner_creating_package="nes_package_running_${ARCH}"
+echo "Building a docker image with the name $IMAGE_NAME and tag $TAG from the file $DOCKERFILE_PATH.
+We will first compile NebulaStream to create an executable image. We will use $NUM_THREADS threads for the compilation."
 
-    # Build the Docker image, but first cd to the directory where the Dockerfile is located and store the current directory
-    cur_dir=$(pwd)
-    cd "$DOCKERFILE_PATH_BUILD_IMAGE"
-    docker build  -t "$docker_image_name_creating_package" -f "Dockerfile-NES-Build" .
-    cd $cur_dir
+# Build the Docker Image for Prepare Package
+docker_image_name_creating_package="nes_package"
+docker_runner_creating_package="nes_package_running_${ARCH}_$(LC_ALL=C tr -dc 'a-zA-Z0-9' < /dev/urandom | head -c 6)"
 
-    # Run the Docker image to create the package
-    docker run --name $docker_runner_creating_package -v $BUILD_DIR:/build_dir -v $CCACHE_DIR:/cache_dir -v $NEBULA_STREAM_PATH:/nebulastream --privileged --cap-add SYS_NICE --entrypoint /nebulastream/docker/buildImage/entrypoint-prepare-nes-package.sh "$docker_image_name_creating_package" $NUM_THREADS
-    docker rm $docker_runner_creating_package
+# Build the Docker image, but first cd to the directory where the Dockerfile is located and store the current directory
+pushd "$DOCKERFILE_PATH_BUILD_IMAGE"
+docker build  -t "$docker_image_name_creating_package" -f "Dockerfile-NES-Build" .
+popd
 
-    # We have to rename the debian packages to either nes-amd64.deb or nes-arm64.deb, before we can build the docker image, as the dockerfile expects the names amd64 or arm64
-    cur_dir=$(pwd)
-    cd $DOCKERFILE_PATH
-    cp $BUILD_DIR/*.deb nes-$PACKAGE_NAME.deb
-    docker buildx build . -f $DOCKER_FILE_NAME --platform=$PLATFORM_NAME --tag nebulastream/nes-public-executable-image:nightly-$PACKAGE_NAME
-    cd $cur_dir
-else
-    echo "Docker image build cancelled."
-    exit 0
-fi
+# Run the Docker image to create the package
+docker run  --rm --name $docker_runner_creating_package -v $BUILD_DIR:/build_dir -v $CCACHE_DIR:/cache_dir -v $NEBULA_STREAM_PATH:/nebulastream --privileged --cap-add SYS_NICE --entrypoint /nebulastream/docker/buildImage/entrypoint-prepare-nes-package.sh "$docker_image_name_creating_package" $NUM_THREADS
+echo "NebulaStream has been compiled and the package has been created under $BUILD_DIR"
 
-#rm -rf $BUILD_DIR
-#if [ "$CCACHE_DIR" != "NONE" ]; then
-#    rm -rf $CCACHE_DIR
-#fi
+# We have to rename the debian packages to either nes-amd64.deb or nes-arm64.deb, before we can build the docker image, as the dockerfile expects the names amd64 or arm64
+pushd $DOCKERFILE_PATH
+cp $BUILD_DIR/*.deb $PACKAGE_NAME.deb
+echo "The debian package has been renamed to $PACKAGE_NAME.deb and copied from $BUILD_DIR to $(pwd)"
+docker buildx build . -f $DOCKER_FILE_NAME --platform=$PLATFORM_NAME --tag nebulastream/$IMAGE_NAME:$TAG
+popd
+
+rm -rf $BUILD_DIR
