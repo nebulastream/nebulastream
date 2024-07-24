6af4d748c3180bfdce603f0ce289d9dc57a9f6a0
Nils Schubert <nils.schubert@dima.tu-berlin.de>
2024-07-24 14:32:12 +0200

    [#91] Added a USE_CCACHE instead of having an else branch.

--- docker/buildImage/entrypoint-prepare-nes-package.sh
+++ docker/buildImage/entrypoint-prepare-nes-package.sh
@@ -27,21 +27,20 @@ num_threads=$1
 # Build NES
 python3 /nebulastream/scripts/build/check_license.py /nebulastream /nebulastream/.no-license-check || exit 1
 
-# Checking if a ccache_dir exists
+# Depending if the ccache_dir exists, we will instruct NebulaStream to compile with a ccache in mind or not
+USE_CCACHE = 0
 if [ -d /ccache_dir ]
 then
   # Compiling NebulaStream with ccache
   echo "Compiling NebulaStream with ccache..."
   ccache --set-config=cache_dir=/cache_dir/
   ccache -M 10G
-  cmake --fresh -B /build_dir -DCMAKE_BUILD_TYPE=Release -DNES_USE_CCACHE=1 -DNES_SELF_HOSTING=1 -DNES_ENABLES_TESTS=1 -DNES_USE_OPC=0 -DNES_ENABLE_EXPERIMENTAL_EXECUTION_ENGINE=1 -DNES_ENABLE_EXPERIMENTAL_EXECUTION_MLIR=1 -DNES_USE_KAFKA=1 -DNES_USE_MQTT=1 -DNES_ENABLE_EXPERIMENTAL_EXECUTION_JNI=1 -DNES_JAVA_UDF_UTILS_PATH=lib/NebulaStream/Java -DNES_BUILD_PLUGIN_TENSORFLOW=1 -DNES_BUILD_PLUGIN_ARROW=1 -DNES_USE_ADAPTIVE=0 -DNES_BUILD_PLUGIN_ONNX=1 -DNES_USE_S2=1 /nebulastream/
-  cmake --build /build_dir -j $num_threads
-else
-  # Compiling NebulaStream without ccache
-  echo "Compiling NebulaStream without ccache..."
-  cmake --fresh -B /build_dir -DCMAKE_BUILD_TYPE=Release -DNES_USE_CCACHE=0 -DNES_SELF_HOSTING=1 -DNES_ENABLES_TESTS=1 -DNES_USE_OPC=0 -DNES_ENABLE_EXPERIMENTAL_EXECUTION_ENGINE=1 -DNES_ENABLE_EXPERIMENTAL_EXECUTION_MLIR=1 -DNES_USE_KAFKA=1 -DNES_USE_MQTT=1 -DNES_ENABLE_EXPERIMENTAL_EXECUTION_JNI=1 -DNES_JAVA_UDF_UTILS_PATH=lib/NebulaStream/Java -DNES_BUILD_PLUGIN_TENSORFLOW=1 -DNES_BUILD_PLUGIN_ARROW=1 -DNES_USE_ADAPTIVE=0 -DNES_BUILD_PLUGIN_ONNX=1 -DNES_USE_S2=1 /nebulastream/
-  cmake --build /build_dir -j $num_threads
+  USE_CCACHE=1
 fi
+
+cmake --fresh -B /build_dir -DCMAKE_BUILD_TYPE=Release -DNES_USE_CCACHE=$USE_CCACHE -DNES_SELF_HOSTING=1 -DNES_ENABLES_TESTS=1 -DNES_USE_OPC=0 -DNES_ENABLE_EXPERIMENTAL_EXECUTION_ENGINE=1 -DNES_ENABLE_EXPERIMENTAL_EXECUTION_MLIR=1 -DNES_USE_KAFKA=1 -DNES_USE_MQTT=1 -DNES_ENABLE_EXPERIMENTAL_EXECUTION_JNI=1 -DNES_JAVA_UDF_UTILS_PATH=lib/NebulaStream/Java -DNES_BUILD_PLUGIN_TENSORFLOW=1 -DNES_BUILD_PLUGIN_ARROW=1 -DNES_USE_ADAPTIVE=0 -DNES_BUILD_PLUGIN_ONNX=1 -DNES_USE_S2=1 /nebulastream/
+cmake --build /build_dir -j $num_threads
+
 
 cd /build_dir
 rm *deb
