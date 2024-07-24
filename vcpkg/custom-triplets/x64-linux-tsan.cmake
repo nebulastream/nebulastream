ed712a09bfe92acd686145666c1d127c78691094
lukas schwerdtfeger <lukas.schwerdtfeger@gmail.com>
2024-07-24 11:21:33 +0200

    feat(Build) Integrate VCPKG into the project

---
+++ vcpkg/custom-triplets/x64-linux-tsan.cmake
@@ -0,0 +1,13 @@
+set(VCPKG_TARGET_ARCHITECTURE x64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE static)
+set(VCPKG_CMAKE_SYSTEM_NAME Linux)
+set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE ${CMAKE_CURRENT_LIST_DIR}/linux-clang.cmake)
+set(VCPKG_CXX_FLAGS -fsanitize=thread)
+set(VCPKG_C_FLAGS -fsanitize=thread)
+
+if (PORT STREQUAL llvm) 
+    set(VCPKG_CXX_FLAGS "")
+    set(VCPKG_C_FLAGS "")
+    set(VCPKG_CMAKE_CONFIGURE_OPTIONS -DLLVM_USE_SANITIZER="Thread")
+endif()
