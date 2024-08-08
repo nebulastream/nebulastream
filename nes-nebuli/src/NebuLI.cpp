f3009974c82240f0cb74da79ebf0849cf945aad0
Lukas Schwerdtfeger <lukas.schwerdtfeger@gmail.com>
2024-08-09 00:07:22 +0200

    feat(NebuLI) integrates the NebuLI Client

---
+++ nes-nebuli/src/NebuLI.cpp
@@ -0,0 +1,266 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include <fstream>
+#include <Catalogs/Source/PhysicalSource.hpp>
+#include <Catalogs/Source/SourceCatalog.hpp>
+#include <Catalogs/Source/SourceCatalogEntry.hpp>
+#include <Compiler/CPPCompiler/CPPCompiler.hpp>
+#include <Compiler/JITCompilerBuilder.hpp>
+#include <Configurations/Coordinator/CoordinatorConfiguration.hpp>
+#include <Configurations/Worker/PhysicalSourceTypes/CSVSourceType.hpp>
+#include <Configurations/Worker/PhysicalSourceTypes/PhysicalSourceType.hpp>
+#include <Configurations/Worker/PhysicalSourceTypes/TCPSourceType.hpp>
+#include <Identifiers/Identifiers.hpp>
+#include <Operators/LogicalOperators/Sources/CsvSourceDescriptor.hpp>
+#include <Operators/LogicalOperators/Sources/LogicalSourceDescriptor.hpp>
+#include <Operators/LogicalOperators/Sources/SourceLogicalOperator.hpp>
+#include <Operators/LogicalOperators/Sources/TCPSourceDescriptor.hpp>
+#include <Optimizer/Phases/MemoryLayoutSelectionPhase.hpp>
+#include <Optimizer/Phases/OriginIdInferencePhase.hpp>
+#include <Optimizer/Phases/QueryRewritePhase.hpp>
+#include <Optimizer/Phases/TypeInferencePhase.hpp>
+#include <Optimizer/QueryRewrite/LogicalSourceExpansionRule.hpp>
+#include <Plans/DecomposedQueryPlan/DecomposedQueryPlan.hpp>
+#include <QueryValidation/SemanticQueryValidation.hpp>
+#include <QueryValidation/SyntacticQueryValidation.hpp>
+#include <Services/QueryParsingService.hpp>
+#include <Util/Logger/Logger.hpp>
+#include <yaml-cpp/yaml.h>
+#include <ErrorHandling.hpp>
+#include <NebuLI.hpp>
+
+namespace NES::CLI
+{
+struct SchemaField
+{
+    std::string name;
+    BasicType type;
+};
+
+struct LogicalSource
+{
+    std::string name;
+    std::vector<SchemaField> schema;
+};
+
+struct PhysicalSource
+{
+    std::string logical;
+    std::map<std::string, std::string> config;
+};
+
+struct QueryConfig
+{
+    std::string query;
+    std::vector<LogicalSource> logical;
+    std::vector<PhysicalSource> physical;
+};
+} /// namespace NES::CLI
+
+namespace YAML
+{
+using namespace NES::CLI;
+template <>
+struct convert<SchemaField>
+{
+    static bool decode(const Node& node, SchemaField& rhs)
+    {
+        rhs.name = node["name"].as<std::string>();
+        rhs.type = *magic_enum::enum_cast<NES::BasicType>(node["type"].as<std::string>());
+        return true;
+    }
+};
+template <>
+struct convert<NES::CLI::LogicalSource>
+{
+    static bool decode(const Node& node, NES::CLI::LogicalSource& rhs)
+    {
+        rhs.name = node["name"].as<std::string>();
+        rhs.schema = node["schema"].as<std::vector<SchemaField>>();
+        return true;
+    }
+};
+template <>
+struct convert<NES::CLI::PhysicalSource>
+{
+    static bool decode(const Node& node, NES::CLI::PhysicalSource& rhs)
+    {
+        rhs.logical = node["logical"].as<std::string>();
+        rhs.config = node["config"].as<std::map<std::string, std::string>>();
+        return true;
+    }
+};
+template <>
+struct convert<NES::CLI::QueryConfig>
+{
+    static bool decode(const Node& node, NES::CLI::QueryConfig& rhs)
+    {
+        rhs.logical = node["logical"].as<std::vector<NES::CLI::LogicalSource>>();
+        rhs.physical = node["physical"].as<std::vector<NES::CLI::PhysicalSource>>();
+        rhs.query = node["query"].as<std::string>();
+        return true;
+    }
+};
+} /// namespace YAML
+
+namespace NES::CLI
+{
+
+SourceDescriptorPtr createSourceDescriptor(SchemaPtr schema, PhysicalSourceTypePtr physicalSourceType)
+{
+    auto logicalSourceName = physicalSourceType->getLogicalSourceName();
+    auto physicalSourceName = physicalSourceType->getPhysicalSourceName();
+    auto sourceType = physicalSourceType->getSourceType();
+    NES_DEBUG(
+        "PhysicalSourceConfig: create Actual source descriptor with physical source: {} {} ",
+        physicalSourceType->toString(),
+        magic_enum::enum_name(sourceType));
+
+    switch (sourceType)
+    {
+        case SourceType::CSV_SOURCE: {
+            auto csvSourceType = physicalSourceType->as<CSVSourceType>();
+            return CsvSourceDescriptor::create(schema, csvSourceType, logicalSourceName, physicalSourceName);
+        }
+        case SourceType::TCP_SOURCE: {
+            auto tcpSourceType = physicalSourceType->as<TCPSourceType>();
+            return TCPSourceDescriptor::create(schema, tcpSourceType, logicalSourceName, physicalSourceName);
+        }
+        default: {
+            NES_THROW_RUNTIME_ERROR("PhysicalSourceConfig:: source type " + physicalSourceType->getSourceTypeAsString() + " not supported");
+        }
+    }
+}
+
+PhysicalSourceTypePtr createPhysicalSourceType(
+    const std::string& logicalName, const std::string& physicalName, const std::map<std::string, std::string>& sourceConfiguration)
+{
+    if (!sourceConfiguration.contains(Configurations::SOURCE_TYPE_CONFIG))
+    {
+        NES_THROW_RUNTIME_ERROR("Missing `type` in source configuration");
+    }
+
+    auto modifiedSourceConfiguration = sourceConfiguration;
+    modifiedSourceConfiguration[Configurations::PHYSICAL_SOURCE_NAME_CONFIG] = physicalName;
+    modifiedSourceConfiguration[Configurations::LOGICAL_SOURCE_NAME_CONFIG] = logicalName;
+
+    return Configurations::PhysicalSourceTypeFactory::createFromString("", modifiedSourceConfiguration);
+}
+
+DecomposedQueryPlanPtr createFullySpecifiedQueryPlan(const QueryConfig& config)
+{
+    auto coordinatorConfig = Configurations::CoordinatorConfiguration::createDefault();
+    auto sourceCatalog = std::make_shared<Catalogs::Source::SourceCatalog>();
+
+    for (const auto& [logicalName, schemaFields] : config.logical)
+    {
+        NES_INFO("Adding logical source: {}", logicalName);
+        auto schema = Schema::create();
+        for (const auto& [name, type] : schemaFields)
+        {
+            schema = schema->addField(name, type);
+        }
+        sourceCatalog->addLogicalSource(logicalName, std::move(schema));
+    }
+
+    for (size_t i = 0; const auto& [logicalName, config] : config.physical)
+    {
+        auto sourceType = createPhysicalSourceType(logicalName, fmt::format("{}_phy{}", logicalName, i++), config);
+        sourceCatalog->addPhysicalSource(
+            logicalName,
+            Catalogs::Source::SourceCatalogEntry::create(
+                NES::PhysicalSource::create(std::move(sourceType)), sourceCatalog->getLogicalSource(logicalName), INITIAL<WorkerId>));
+    }
+
+    auto cppCompiler = Compiler::CPPCompiler::create();
+    auto jitCompiler = Compiler::JITCompilerBuilder().registerLanguageCompiler(cppCompiler).build();
+    auto parsingService = QueryParsingService::create(jitCompiler);
+    auto syntacticQueryValidation = Optimizer::SyntacticQueryValidation::create(parsingService);
+    auto semanticQueryValidation = Optimizer::SemanticQueryValidation::create(sourceCatalog);
+    auto logicalSourceExpansionRule = NES::Optimizer::LogicalSourceExpansionRule::create(sourceCatalog, false);
+    auto typeInference = Optimizer::TypeInferencePhase::create(sourceCatalog);
+    auto originIdInferencePhase = Optimizer::OriginIdInferencePhase::create();
+    auto memoryLayoutSelectionPhase = Optimizer::MemoryLayoutSelectionPhase::create(coordinatorConfig->optimizer.memoryLayoutPolicy);
+    auto queryRewritePhase = Optimizer::QueryRewritePhase::create(coordinatorConfig);
+
+    auto query = syntacticQueryValidation->validate(config.query);
+    semanticQueryValidation->validate(query);
+
+    logicalSourceExpansionRule->apply(query);
+    typeInference->execute(query);
+    originIdInferencePhase->execute(query);
+    memoryLayoutSelectionPhase->execute(query);
+    queryRewritePhase->execute(query);
+    typeInference->execute(query);
+
+    for (auto& sourceOperator : query->getSourceOperators())
+    {
+        if (auto sourceDescriptor = sourceOperator->getSourceDescriptor(); sourceDescriptor->instanceOf<LogicalSourceDescriptor>())
+        {
+            /// Fetch logical and physical source name in the descriptor
+            auto logicalSourceName = sourceDescriptor->getLogicalSourceName();
+            auto physicalSourceName = sourceDescriptor->getPhysicalSourceName();
+            /// Iterate over all available physical sources
+            bool foundPhysicalSource = false;
+            for (const auto& entry : sourceCatalog->getPhysicalSources(logicalSourceName))
+            {
+                NES_DEBUG("Replacing LogicalSourceDescriptor {} - {}", logicalSourceName, physicalSourceName);
+                if (auto physicalSourceType = entry->getPhysicalSource(); physicalSourceType->getPhysicalSourceName() == physicalSourceName)
+                {
+                    auto physicalDescriptor
+                        = createSourceDescriptor(sourceDescriptor->getSchema(), physicalSourceType->getPhysicalSourceType());
+                    NES_DEBUG("Replacement with: {}", physicalDescriptor->toString());
+                    sourceOperator->setSourceDescriptor(physicalDescriptor);
+                    foundPhysicalSource = true;
+                    break;
+                }
+            }
+            NES_ASSERT(
+                foundPhysicalSource,
+                "Logical source descriptor could not be replaced with the physical source descriptor: " + physicalSourceName);
+        }
+    }
+
+    NES_INFO("QEP:\n {}", query->toString());
+    NES_INFO("Sink Schema: {}", query->getRootOperators()[0]->getOutputSchema()->toString());
+    return DecomposedQueryPlan::create(INITIAL<QueryId>, INITIAL<WorkerId>, query->getRootOperators());
+}
+
+DecomposedQueryPlanPtr loadFromFile(const std::filesystem::path& filePath)
+{
+    std::ifstream file(filePath);
+    if (!file)
+    {
+        throw CouldNotReadQueryDescription(std::strerror(errno));
+    }
+
+    return loadFrom(file);
+}
+
+DecomposedQueryPlanPtr loadFrom(std::istream& inputStream)
+{
+    try
+    {
+        auto config = YAML::Load(inputStream).as<QueryConfig>();
+        return createFullySpecifiedQueryPlan(config);
+    }
+    catch (const YAML::ParserException& pex)
+    {
+        auto exception = CouldNotParseQueryDescription();
+        exception.what() += fmt::format(": {}.", pex.what());
+        throw exception;
+    }
+}
+}
---
+++ nes-nebuli/src/Services/QueryParsingService.cpp
@@ -0,0 +1,147 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+#include <iostream>
+#include <sstream>
+#include <API/Query.hpp>
+#include <API/Schema.hpp>
+#include <Compiler/CompilationRequest.hpp>
+#include <Compiler/CompilationResult.hpp>
+#include <Compiler/DynamicObject.hpp>
+#include <Compiler/JITCompiler.hpp>
+#include <Compiler/SourceCode.hpp>
+#include <Services/QueryParsingService.hpp>
+#include <Util/Common.hpp>
+#include <Util/Logger/Logger.hpp>
+#include <ErrorHandling.hpp>
+
+namespace NES
+{
+
+QueryParsingService::QueryParsingService(std::shared_ptr<Compiler::JITCompiler> jitCompiler) : jitCompiler(jitCompiler)
+{
+}
+
+std::shared_ptr<QueryParsingService> QueryParsingService::create(std::shared_ptr<Compiler::JITCompiler> jitCompiler)
+{
+    return std::make_shared<QueryParsingService>(jitCompiler);
+}
+
+SchemaPtr QueryParsingService::createSchemaFromCode(const std::string& queryCodeSnippet)
+{
+    try
+    {
+        /* translate user code to a shared library, load and execute function, then return query object */
+        std::stringstream code;
+        code << "#include <API/Schema.hpp>" << std::endl;
+        code << "#include <Common/DataTypes/DataTypeFactory.hpp>" << std::endl;
+        code << "namespace NES{" << std::endl;
+
+        code << "Schema createSchema(){" << std::endl;
+        code << "return " << queryCodeSnippet << ";";
+        code << "}" << std::endl;
+        code << "}" << std::endl;
+
+        NES_DEBUG("QueryParsingService : generated code= {}", code.str());
+        auto sourceCode = std::make_unique<Compiler::SourceCode>(Compiler::Language::CPP, code.str());
+        auto request = Compiler::CompilationRequest::create(std::move(sourceCode), "query", false, false, false, false);
+        auto result = jitCompiler->compile(std::move(request));
+        auto compiled_code = result.get().getDynamicObject();
+
+        using CreateSchemaFunctionPtr = Schema (*)();
+        auto func
+            = compiled_code->getInvocableMember<CreateSchemaFunctionPtr>("_ZN3NES12createSchemaEv"); /// was   _ZN5iotdb12createSchemaEv
+        if (!func)
+        {
+            NES_ERROR("QueryParsingService : Error retrieving function! Symbol not found!");
+        }
+        /* call loaded function to create query object */
+        Schema query((*func)());
+        return std::make_shared<Schema>(query);
+    }
+    catch (std::exception& exc)
+    {
+        NES_ERROR("QueryParsingService: Failed to create the query from input code string: {}", queryCodeSnippet);
+        throw;
+    }
+    catch (...)
+    {
+        NES_ERROR("QueryParsingService : Failed to create the query from input code string: {}", queryCodeSnippet);
+        throw "Failed to create the query from input code string";
+    }
+}
+
+QueryPlanPtr QueryParsingService::createQueryFromCodeString(const std::string& queryCodeSnippet)
+{
+    if (queryCodeSnippet.find("Source(") != std::string::npos || queryCodeSnippet.find("Schema::create()") != std::string::npos)
+    {
+        NES_ERROR("QueryParsingService: queryIdAndCatalogEntryMapping are not allowed to specify schemas anymore.");
+        auto ex = InvalidQuery();
+        ex.what() += "Queries are not allowed to define schemas anymore";
+        throw ex;
+    }
+
+    try
+    {
+        /* translate user code to a shared library, load and execute function, then return query object */
+        std::stringstream code;
+        code << "#include <API/QueryAPI.hpp>" << std::endl;
+        code << "namespace NES{" << std::endl;
+        code << "Query createQuery(){" << std::endl;
+
+        std::string sourceName = queryCodeSnippet.substr(queryCodeSnippet.find("::from("));
+        sourceName = sourceName.substr(7, sourceName.find(')') - 7);
+        NES_DEBUG(" QueryParsingService: source name = {}", sourceName);
+
+        std::string newQuery = queryCodeSnippet;
+        /// add return statement in front of input query
+        newQuery = NES::Util::replaceFirst(newQuery, "Query::from", "return Query::from");
+
+        NES_DEBUG("QueryParsingService: parsed query = {}", newQuery);
+        code << newQuery << std::endl;
+        code << "}" << std::endl;
+        code << "}" << std::endl;
+        NES_DEBUG("QueryParsingService: query code\n{}", code.str());
+        auto sourceCode = std::make_unique<Compiler::SourceCode>(Compiler::Language::CPP, code.str());
+        auto request = Compiler::CompilationRequest::create(std::move(sourceCode), "query", true, false, false, false);
+        auto result = jitCompiler->compile(std::move(request));
+        auto compiled_code = result.get().getDynamicObject();
+        if (!code)
+        {
+            NES_ERROR("Compilation of query code failed! Code: {}", code.str());
+        }
+
+        using CreateQueryFunctionPtr = Query (*)();
+        auto func = compiled_code->getInvocableMember<CreateQueryFunctionPtr>("_ZN3NES11createQueryEv");
+        if (!func)
+        {
+            NES_ERROR("QueryParsingService: Error retrieving function! Symbol not found!");
+        }
+        /* call loaded function to create query object */
+        Query query((*func)());
+
+        return query.getQueryPlan();
+    }
+    catch (std::exception& exc)
+    {
+        NES_ERROR("QueryParsingService: Failed to create the query from input code string: {} {}", queryCodeSnippet, exc.what());
+        throw;
+    }
+    catch (...)
+    {
+        NES_ERROR("QueryParsingService: Failed to create the query from input code string: {}", queryCodeSnippet);
+        throw Exceptions::RuntimeException("Failed to create the query from input code string");
+    }
+}
+
+} /// namespace NES
