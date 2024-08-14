nebulastream/nebulastream

commit 77a1e463de335934649b1d2d6a60ba8708952171

[#93] chore(Remove Statistic) Removed some leftovers from our statistics.

nes-execution/src/Execution/Operators/Streaming/EventTimeWatermarkAssignment.cpp
@@ -34,11 +34,6 @@ EventTimeWatermarkAssignment::EventTimeWatermarkAssignment(TimeFunctionPtr timeF
 
 void EventTimeWatermarkAssignment::open(ExecutionContext& executionCtx, RecordBuffer& recordBuffer) const
 {
-    /// We have to do this here, as we do not want to set the statistic id of this build operator in the execution context
-    if (hasChild())
-    {
-        child->open(executionCtx, recordBuffer);
-    }
     executionCtx.setLocalOperatorState(this, std::make_unique<WatermarkState>());
     timeFunction->open(executionCtx, recordBuffer);
 }

nes-execution/src/Execution/Operators/Streaming/IngestionTimeWatermarkAssignment.cpp
@@ -34,12 +34,6 @@ IngestionTimeWatermarkAssignment::IngestionTimeWatermarkAssignment(TimeFunctionP
 
 void IngestionTimeWatermarkAssignment::open(ExecutionContext& executionCtx, RecordBuffer& recordBuffer) const
 {
-    /// We have to do this here, as we do not want to set the statistic id of this build operator in the execution context
-    if (hasChild())
-    {
-        child->open(executionCtx, recordBuffer);
-    }
-
     timeFunction->open(executionCtx, recordBuffer);
     auto emptyRecord = Record();
     Value<> tsField = timeFunction->getTs(executionCtx, emptyRecord);

nes-execution/src/Execution/Operators/Streaming/Join/HashJoin/Slicing/HJBuildSlicing.cpp
@@ -159,10 +159,8 @@ void* getDefaultMemRef()
     return nullptr;
 }
 
-void HJBuildSlicing::open(ExecutionContext& ctx, RecordBuffer& recordBuffer) const
+void HJBuildSlicing::open(ExecutionContext& ctx, RecordBuffer&) const
 {
-    /// We override the Operator::open() and have to call it explicitly here, as we must set the statistic id
-    Operator::open(ctx, recordBuffer);
     auto operatorHandlerMemRef = ctx.getGlobalOperatorHandler(operatorHandlerIndex);
     Value<MemRef> dummyRef1 = Nautilus::FunctionCall("getDefaultMemRef", getDefaultMemRef);
     Value<MemRef> dummyRef2 = Nautilus::FunctionCall("getDefaultMemRef", getDefaultMemRef);

nes-execution/src/Execution/Operators/Streaming/Join/HashJoin/Slicing/HJBuildSlicingVarSized.cpp
@@ -135,10 +135,8 @@ void* getDefaultMemRefVarSized()
     return nullptr;
 }
 
-void HJBuildSlicingVarSized::open(ExecutionContext& ctx, RecordBuffer& recordBuffer) const
+void HJBuildSlicingVarSized::open(ExecutionContext& ctx, RecordBuffer&) const
 {
-    /// We override the Operator::open() and have to call it explicitly here, as we must set the statistic id
-    Operator::open(ctx, recordBuffer);
     auto operatorHandlerMemRef = ctx.getGlobalOperatorHandler(operatorHandlerIndex);
     Value<MemRef> dummyRef1 = FunctionCall("getDefaultMemRef", getDefaultMemRefVarSized);
     Value<MemRef> dummyRef2 = FunctionCall("getDefaultMemRef", getDefaultMemRefVarSized);

nes-execution/src/Execution/Operators/Streaming/Join/StreamJoinBuildBucketing.cpp
@@ -87,10 +87,8 @@ void* getDefaultMemRefProxy()
     return nullptr;
 }
 
-void StreamJoinBuildBucketing::open(ExecutionContext& ctx, RecordBuffer& recordBuffer) const
+void StreamJoinBuildBucketing::open(ExecutionContext& ctx, RecordBuffer&) const
 {
-    /// We override the Operator::open() and have to call it explicitly here, as we must set the statistic id
-    Operator::open(ctx, recordBuffer);
     auto opHandlerMemRef = ctx.getGlobalOperatorHandler(operatorHandlerIndex);
     Value<MemRef> allWindowsToFill = Nautilus::FunctionCall("getDefaultMemRef", getDefaultMemRefProxy);
     ctx.setLocalOperatorState(this, std::make_unique<LocalStateBucketing>(allWindowsToFill));

commit 4bca7f168bb5b3d8311d6fa0cbdb13f767f51bfa

fix(Join): fix small bug introduced with 77a1e46

nes-execution/src/Execution/Operators/Streaming/EventTimeWatermarkAssignment.cpp
@@ -34,6 +34,7 @@ EventTimeWatermarkAssignment::EventTimeWatermarkAssignment(TimeFunctionPtr timeF
 
 void EventTimeWatermarkAssignment::open(ExecutionContext& executionCtx, RecordBuffer& recordBuffer) const
 {
+    Operator::open(executionCtx, recordBuffer);
     executionCtx.setLocalOperatorState(this, std::make_unique<WatermarkState>());
     timeFunction->open(executionCtx, recordBuffer);
 }

nes-execution/src/Execution/Operators/Streaming/IngestionTimeWatermarkAssignment.cpp
@@ -34,6 +34,7 @@ IngestionTimeWatermarkAssignment::IngestionTimeWatermarkAssignment(TimeFunctionP
 
 void IngestionTimeWatermarkAssignment::open(ExecutionContext& executionCtx, RecordBuffer& recordBuffer) const
 {
+    Operator::open(executionCtx, recordBuffer);
     timeFunction->open(executionCtx, recordBuffer);
     auto emptyRecord = Record();
     Value<> tsField = timeFunction->getTs(executionCtx, emptyRecord);

nes-execution/src/Execution/Operators/Streaming/Join/HashJoin/Slicing/HJBuildSlicing.cpp
@@ -159,8 +159,9 @@ void* getDefaultMemRef()
     return nullptr;
 }
 
-void HJBuildSlicing::open(ExecutionContext& ctx, RecordBuffer&) const
+void HJBuildSlicing::open(ExecutionContext& ctx, RecordBuffer& recordBuffer) const
 {
+    Operator::open(ctx, recordBuffer);
     auto operatorHandlerMemRef = ctx.getGlobalOperatorHandler(operatorHandlerIndex);
     Value<MemRef> dummyRef1 = Nautilus::FunctionCall("getDefaultMemRef", getDefaultMemRef);
     Value<MemRef> dummyRef2 = Nautilus::FunctionCall("getDefaultMemRef", getDefaultMemRef);

nes-execution/src/Execution/Operators/Streaming/Join/HashJoin/Slicing/HJBuildSlicingVarSized.cpp
@@ -135,8 +135,9 @@ void* getDefaultMemRefVarSized()
     return nullptr;
 }
 
-void HJBuildSlicingVarSized::open(ExecutionContext& ctx, RecordBuffer&) const
+void HJBuildSlicingVarSized::open(ExecutionContext& ctx, RecordBuffer& recordBuffer) const
 {
+    Operator::open(ctx, recordBuffer);
     auto operatorHandlerMemRef = ctx.getGlobalOperatorHandler(operatorHandlerIndex);
     Value<MemRef> dummyRef1 = FunctionCall("getDefaultMemRef", getDefaultMemRefVarSized);
     Value<MemRef> dummyRef2 = FunctionCall("getDefaultMemRef", getDefaultMemRefVarSized);

nes-execution/src/Execution/Operators/Streaming/Join/StreamJoinBuildBucketing.cpp
@@ -87,8 +87,9 @@ void* getDefaultMemRefProxy()
     return nullptr;
 }
 
-void StreamJoinBuildBucketing::open(ExecutionContext& ctx, RecordBuffer&) const
+void StreamJoinBuildBucketing::open(ExecutionContext& ctx, RecordBuffer& recordBuffer) const
 {
+    Operator::open(ctx, recordBuffer);
     auto opHandlerMemRef = ctx.getGlobalOperatorHandler(operatorHandlerIndex);
     Value<MemRef> allWindowsToFill = Nautilus::FunctionCall("getDefaultMemRef", getDefaultMemRefProxy);
     ctx.setLocalOperatorState(this, std::make_unique<LocalStateBucketing>(allWindowsToFill));