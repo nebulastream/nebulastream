a6b67fd5d0feec24107425ff47a86305e204acd8
Nils Schubert <nils.schubert@dima.tu-berlin.de>
2024-09-03 19:42:24 +0200

    feat(Execution) Created and ported some expressions.
    We created the following expressions:
    - Logical: Equals and Negate
    - Arithmetical: Add, Sub, Mul, Div

---
+++ nes-execution/src/Execution/Expressions/LogicalExpressions/EqualsExpression.cpp
@@ -0,0 +1,30 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include <Execution/Expressions/LogicalExpressions/EqualsExpression.hpp>
+namespace NES::Runtime::Execution::Expressions
+{
+
+EqualsExpression::EqualsExpression(const ExpressionPtr& leftSubExpression, const ExpressionPtr& rightSubExpression)
+: leftSubExpression(leftSubExpression), rightSubExpression(rightSubExpression) {}
+
+VarVal EqualsExpression::execute(Record& record) const
+{
+    const auto leftValue = leftSubExpression->execute(record);
+    const auto rightValue = rightSubExpression->execute(record);
+    return leftValue == rightValue;
+}
+
+
+}
