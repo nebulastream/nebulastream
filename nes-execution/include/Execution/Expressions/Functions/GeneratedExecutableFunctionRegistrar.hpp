34c8ea4edefec9e200ad9f1f063d4f30fa595aea
Lukas Schwerdtfeger <lukas.schwerdtfeger@gmail.com>
2024-09-04 02:13:01 +0200

    refactor(Plugins) Uses new Functions registry

--- nes-execution/include/Execution/Expressions/Functions/GeneratedExecutableFunctionRegistrar.hpp
+++ nes-execution/include/Execution/Expressions/Functions/GeneratedExecutableFunctionRegistrar.hpp
@@ -15,81 +15,145 @@
 #pragma once
 #ifndef INCLUDED_FROM_EXECUTABLE_FUNCTION_REGISTRY
 #    error "This file should not be included directly! " \
-        "Include instead include <Execution/Expressions/Functions/ExecutableFunctionRegistry.hpp>"
+        "Include instead include <Execution/Functions/Functions/GeneratedExecutableFunctionRegistrar.hpp>"
 #endif
 
-namespace NES::Runtime::Execution::Expressions
+namespace NES::Runtime::Execution::Functions
 {
-std::unique_ptr<Expression> RegisterAbsExpression(const std::vector<ExpressionPtr>&);
-std::unique_ptr<Expression> RegisterAcosExpression(const std::vector<ExpressionPtr>&);
-std::unique_ptr<Expression> RegisterAsinExpression(const std::vector<ExpressionPtr>&);
-std::unique_ptr<Expression> RegisterAtan2Expression(const std::vector<ExpressionPtr>&);
-std::unique_ptr<Expression> RegisterAtanExpression(const std::vector<ExpressionPtr>&);
-std::unique_ptr<Expression> RegisterBitcounterExpression(const std::vector<ExpressionPtr>&);
-std::unique_ptr<Expression> RegisterCbrtExpression(const std::vector<ExpressionPtr>&);
-std::unique_ptr<Expression> RegisterCeilExpression(const std::vector<ExpressionPtr>&);
-std::unique_ptr<Expression> RegisterCosExpression(const std::vector<ExpressionPtr>&);
-std::unique_ptr<Expression> RegisterCotExpression(const std::vector<ExpressionPtr>&);
-std::unique_ptr<Expression> RegisterDegreesExpression(const std::vector<ExpressionPtr>&);
-std::unique_ptr<Expression> RegisterExpExpression(const std::vector<ExpressionPtr>&);
-std::unique_ptr<Expression> RegisterFactorialExpression(const std::vector<ExpressionPtr>&);
-std::unique_ptr<Expression> RegisterFloorExpression(const std::vector<ExpressionPtr>&);
-std::unique_ptr<Expression> RegisterGammaExpression(const std::vector<ExpressionPtr>&);
-std::unique_ptr<Expression> RegisterLGammaExpression(const std::vector<ExpressionPtr>&);
-std::unique_ptr<Expression> RegisterLnExpression(const std::vector<ExpressionPtr>&);
-std::unique_ptr<Expression> RegisterLog2Expression(const std::vector<ExpressionPtr>&);
-std::unique_ptr<Expression> RegisterLog10Expression(const std::vector<ExpressionPtr>&);
-std::unique_ptr<Expression> RegisterMaxExpression(const std::vector<ExpressionPtr>&);
-std::unique_ptr<Expression> RegisterMinExpression(const std::vector<ExpressionPtr>&);
-std::unique_ptr<Expression> RegisterModExpression(const std::vector<ExpressionPtr>&);
-std::unique_ptr<Expression> RegisterPowerExpression(const std::vector<ExpressionPtr>&);
-std::unique_ptr<Expression> RegisterRadiansExpression(const std::vector<ExpressionPtr>&);
-std::unique_ptr<Expression> RegisterRandomExpression(const std::vector<ExpressionPtr>&);
-std::unique_ptr<Expression> RegisterSignExpression(const std::vector<ExpressionPtr>&);
-std::unique_ptr<Expression> RegisterSinExpression(const std::vector<ExpressionPtr>&);
-std::unique_ptr<Expression> RegisterSqrtExpression(const std::vector<ExpressionPtr>&);
-std::unique_ptr<Expression> RegisterTanExpression(const std::vector<ExpressionPtr>&);
-std::unique_ptr<Expression> RegisterTruncExpression(const std::vector<ExpressionPtr>&);
-} /// namespace NES::Runtime::Execution::Expressions
+
+std::unique_ptr<Function> RegisterTanFunction(const std::vector<FunctionPtr>&);
+
+std::unique_ptr<Function> RegisterGammaFunction(const std::vector<FunctionPtr>&);
+
+std::unique_ptr<Function> RegisterLnFunction(const std::vector<FunctionPtr>&);
+
+std::unique_ptr<Function> RegisterCotFunction(const std::vector<FunctionPtr>&);
+
+std::unique_ptr<Function> RegisterLog2Function(const std::vector<FunctionPtr>&);
+
+std::unique_ptr<Function> RegisterCeilFunction(const std::vector<FunctionPtr>&);
+
+std::unique_ptr<Function> RegisterSqrtFunction(const std::vector<FunctionPtr>&);
+
+std::unique_ptr<Function> RegisterMinFunction(const std::vector<FunctionPtr>&);
+
+std::unique_ptr<Function> RegisterAsinFunction(const std::vector<FunctionPtr>&);
+
+std::unique_ptr<Function> RegisterLog10Function(const std::vector<FunctionPtr>&);
+
+std::unique_ptr<Function> RegisterAcosFunction(const std::vector<FunctionPtr>&);
+
+std::unique_ptr<Function> RegisterLGammaFunction(const std::vector<FunctionPtr>&);
+
+std::unique_ptr<Function> RegisterCosFunction(const std::vector<FunctionPtr>&);
+
+std::unique_ptr<Function> RegisterMaxFunction(const std::vector<FunctionPtr>&);
+
+std::unique_ptr<Function> RegisterAtanFunction(const std::vector<FunctionPtr>&);
+
+std::unique_ptr<Function> RegisterDegreesFunction(const std::vector<FunctionPtr>&);
+
+std::unique_ptr<Function> RegisterRandomFunction(const std::vector<FunctionPtr>&);
+
+std::unique_ptr<Function> RegisterAtan2Function(const std::vector<FunctionPtr>&);
+
+std::unique_ptr<Function> RegisterPowerFunction(const std::vector<FunctionPtr>&);
+
+std::unique_ptr<Function> RegisterSinFunction(const std::vector<FunctionPtr>&);
+
+std::unique_ptr<Function> RegisterBitcounterFunction(const std::vector<FunctionPtr>&);
+
+std::unique_ptr<Function> RegisterAbsFunction(const std::vector<FunctionPtr>&);
+
+std::unique_ptr<Function> RegisterCbrtFunction(const std::vector<FunctionPtr>&);
+
+std::unique_ptr<Function> RegisterFloorFunction(const std::vector<FunctionPtr>&);
+
+std::unique_ptr<Function> RegisterModFunction(const std::vector<FunctionPtr>&);
+std::unique_ptr<Function> RegisterRadiansFunction(const std::vector<FunctionPtr>&);
+} /// namespace NES::Runtime::Execution::Functions
 
 namespace NES
 {
 template <>
 inline void
-Registrar<std::string, Runtime::Execution::Expressions::Expression, const std::vector<Runtime::Execution::Expressions::ExpressionPtr>&>::
+Registrar<std::string, Runtime::Execution::Functions::Function, const std::vector<Runtime::Execution::Functions::FunctionPtr>&>::
     registerAll([[maybe_unused]] Registry<Registrar>& registry)
 {
-    using namespace NES::Runtime::Execution::Expressions;
+    using namespace NES::Runtime::Execution::Functions;
+    /// External register functions
+    ///-todo
 
-    registry.registerPlugin("abs", RegisterAbsExpression);
-    registry.registerPlugin("acos", RegisterAcosExpression);
-    registry.registerPlugin("asin", RegisterAsinExpression);
-    registry.registerPlugin("atan", RegisterAtanExpression);
-    registry.registerPlugin("atan2", RegisterAtan2Expression);
-    registry.registerPlugin("bitcounter", RegisterBitcounterExpression);
-    registry.registerPlugin("cbrt", RegisterCbrtExpression);
-    registry.registerPlugin("ceil", RegisterCeilExpression);
-    registry.registerPlugin("cos", RegisterCosExpression);
-    registry.registerPlugin("cot", RegisterCotExpression);
-    registry.registerPlugin("degrees", RegisterDegreesExpression);
-    registry.registerPlugin("exp", RegisterExpExpression);
-    registry.registerPlugin("factorial", RegisterFloorExpression);
-    registry.registerPlugin("floor", RegisterFloorExpression);
-    registry.registerPlugin("gamma", RegisterGammaExpression);
-    registry.registerPlugin("lGamma", RegisterLGammaExpression);
-    registry.registerPlugin("ln", RegisterLnExpression);
-    registry.registerPlugin("log2", RegisterLog2Expression);
-    registry.registerPlugin("log10", RegisterLog10Expression);
-    registry.registerPlugin("max", RegisterMaxExpression);
-    registry.registerPlugin("min", RegisterMinExpression);
-    registry.registerPlugin("mod", RegisterModExpression);
-    registry.registerPlugin("power", RegisterPowerExpression);
-    registry.registerPlugin("radians", RegisterRadiansExpression);
-    registry.registerPlugin("random", RegisterRandomExpression);
-    registry.registerPlugin("sign", RegisterSignExpression);
-    registry.registerPlugin("sin", RegisterSinExpression);
-    registry.registerPlugin("sqrt", RegisterSqrtExpression);
-    registry.registerPlugin("tan", RegisterTanExpression);
-    registry.registerPlugin("trunc", RegisterTruncExpression);
+    /// Internal register functions
+    registry.registerPlugin("tan", RegisterTanFunction);
+
+    registry.registerPlugin("gamma", RegisterGammaFunction);
+
+    registry.registerPlugin("ln", RegisterLnFunction);
+
+    registry.registerPlugin("cot", RegisterCotFunction);
+
+    registry.registerPlugin("log2", RegisterLog2Function);
+
+    registry.registerPlugin("ceil", RegisterCeilFunction);
+
+    registry.registerPlugin("sqrt", RegisterSqrtFunction);
+
+    registry.registerPlugin("min", RegisterMinFunction);
+
+    registry.registerPlugin("abs", RegisterAbsFunction);
+    registry.registerPlugin("acos", RegisterAcosFunction);
+    registry.registerPlugin("asin", RegisterAsinFunction);
+
+    registry.registerPlugin("log10", RegisterLog10Function);
+
+    registry.registerPlugin("acos", RegisterAcosFunction);
+
+    registry.registerPlugin("lGamma", RegisterLGammaFunction);
+
+    registry.registerPlugin("cos", RegisterCosFunction);
+
+    registry.registerPlugin("max", RegisterMaxFunction);
+
+    registry.registerPlugin("atan", RegisterAtanFunction);
+
+    registry.registerPlugin("degrees", RegisterDegreesFunction);
+
+    registry.registerPlugin("random", RegisterRandomFunction);
+
+    registry.registerPlugin("atan2", RegisterAtan2Function);
+
+    registry.registerPlugin("power", RegisterPowerFunction);
+
+    registry.registerPlugin("sin", RegisterSinFunction);
+
+    registry.registerPlugin("bitcounter", RegisterBitcounterFunction);
+
+    registry.registerPlugin("abs", RegisterAbsFunction);
+
+    registry.registerPlugin("cbrt", RegisterCbrtFunction);
+    registry.registerPlugin("floor", RegisterFloorFunction);
+    registry.registerPlugin("mod", RegisterModFunction);
+    registry.registerPlugin("radians", RegisterRadiansFunction);
+    registry.registerPlugin("random", RegisterRandomFunction);
+    registry.registerPlugin("sign", RegisterSignFunction);
+    registry.registerPlugin("sin", RegisterSinFunction);
+    registry.registerPlugin("sqrt", RegisterSqrtFunction);
+    registry.registerPlugin("tan", RegisterTanFunction);
+    registry.registerPlugin("trunc", RegisterTruncFunction);
+    registry.registerPlugin("ceil", RegisterCeilFunction);
+    registry.registerPlugin("cos", RegisterCosFunction);
+    registry.registerPlugin("cot", RegisterCotFunction);
+    registry.registerPlugin("degrees", RegisterDegreesFunction);
+    registry.registerPlugin("exp", RegisterExpFunction);
+    registry.registerPlugin("factorial", RegisterFloorFunction);
+    registry.registerPlugin("gamma", RegisterGammaFunction);
+    registry.registerPlugin("lGamma", RegisterLGammaFunction);
+    registry.registerPlugin("ln", RegisterLnFunction);
+    registry.registerPlugin("log2", RegisterLog2Function);
+    registry.registerPlugin("log10", RegisterLog10Function);
+    registry.registerPlugin("max", RegisterMaxFunction);
+    registry.registerPlugin("min", RegisterMinFunction);
+    registry.registerPlugin("power", RegisterPowerFunction);
 }
 }
