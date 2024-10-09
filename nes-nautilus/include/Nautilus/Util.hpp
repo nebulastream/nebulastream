ae895bef57d520f4f78b45250db0288ff484e5e6
Nils Schubert <nilslpschubert@gmail.com>
2024-10-09 16:16:15 +0200

    chore(Execution) Moved NES_LOG_PROXY from Runtime:Execution to Nautilus:Util
    We move the defines and proxy functions for calling our logging from inside nautilus-land.

---
+++ nes-nautilus/include/Nautilus/Util.hpp
@@ -0,0 +1,74 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#pragma once
+
+#include <Util/Logger/LogLevel.hpp>
+#include <nautilus/val.hpp>
+#include <nautilus/val_enum.hpp>
+
+namespace NES::Nautilus::Util
+{
+
+
+void logProxy(const char* message, const LogLevel logLevel);
+
+/// Allows to use our general logging calls from nautilus-land
+#define NES_TRACE_EXEC(TEXT) \
+    do \
+    { \
+        nautilus::stringstream ss; \
+        ss << TEXT; \
+        const nautilus::val<LogLevel> logLevel = (LogLevel::LOG_TRACE); \
+        nautilus::invoke(Util::logProxy, ss.str().c_str(), logLevel); \
+    } while (0)
+
+#define NES_DEBUG_EXEC(TEXT) \
+    do \
+    { \
+        nautilus::stringstream ss; \
+        ss << TEXT; \
+        const nautilus::val<LogLevel> logLevel = (LogLevel::LOG_DEBUG); \
+        nautilus::invoke(Util::logProxy, ss.str().c_str(), logLevel); \
+    } while (0)
+
+#define NES_INFO_EXEC(TEXT) \
+    do \
+    { \
+        nautilus::stringstream ss; \
+        ss << TEXT; \
+        const nautilus::val<LogLevel> logLevel = (LogLevel::LOG_INFO); \
+        nautilus::invoke(Util::logProxy, ss.str().c_str(), logLevel); \
+    } while (0)
+
+#define NES_WARNING_EXEC(TEXT) \
+    do \
+    { \
+        nautilus::stringstream ss; \
+        ss << TEXT; \
+        const nautilus::val<LogLevel> logLevel = (LogLevel::LOG_WARNING); \
+        nautilus::invoke(Util::logProxy, ss.str().c_str(), logLevel); \
+    } while (0)
+
+#define NES_ERROR_EXEC(TEXT) \
+    do \
+    { \
+        nautilus::stringstream ss; \
+        ss << TEXT; \
+        const nautilus::val<LogLevel> logLevel = (LogLevel::LOG_ERROR); \
+        nautilus::invoke(Util::logProxy, ss.str().c_str(), logLevel); \
+    } while (0)
+
+
+}
