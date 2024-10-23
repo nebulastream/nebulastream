0e88d7935a756736abe9ee55efeaa3ca0e2ebc3f
alepping <aljoschalepping@gmail.com>
2024-10-23 16:07:17 +0200

    Removed name of sink from serialization on.
    Since we don't need the name of the sink after serialization, we don't (de-)serialize it anymore.

--- nes-sinks/src/SinkProvider.cpp
+++ nes-sinks/src/SinkProvider.cpp
@@ -26,7 +26,8 @@ namespace NES::Sinks::SinkProvider
 
 std::shared_ptr<Sink> lower(const QueryId queryId, const SinkDescriptor& sinkDescriptor)
 {
-    if (auto sink = SinkRegistry::instance().tryCreate(sinkDescriptor.sinkType, queryId, sinkDescriptor); sink.has_value())
+    NES_DEBUG("The sinkDescriptor is: {}", sinkDescriptor);
+    if (auto sink = SinkRegistry::instance().create(sinkDescriptor.sinkType, queryId, sinkDescriptor); sink.has_value())
     {
         return std::move(sink.value());
     }
