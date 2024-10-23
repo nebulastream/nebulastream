7f4322782bebae7b4b98d5eff5a0f272afffb9d9
alepping <aljoschalepping@gmail.com>
2024-10-23 15:18:09 +0200

    Resolved toCSVString function in CSVFormat

--- nes-sinks/include/SinksParsing/CSVFormat.hpp
+++ nes-sinks/include/SinksParsing/CSVFormat.hpp
@@ -25,12 +25,11 @@ public:
     virtual ~CSVFormat() noexcept = default;
 
     /// Returns the schema of formatted according to the specific SinkFormat represented as string.
-    std::string getFormattedSchema();
+    std::string getFormattedSchema() const;
 
     /// return formatted content of TupleBuffer, contains timestamp if specified
     std::string getFormattedBuffer(Memory::TupleBuffer& inputBuffer);
 
-    static std::string toCSVString(const Schema& schema);
     static std::string printTupleBufferAsCSV(Memory::TupleBuffer tbuffer, const SchemaPtr& schema);
 
     friend std::ostream& operator<<(std::ostream& out, const CSVFormat& format);
