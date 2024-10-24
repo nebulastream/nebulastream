85914c46e3de6fc9576b0b2cf24369371340745b
alepping <aljoschalepping@gmail.com>
2024-10-24 14:36:46 +0200

    Fixed TestQueryStatusSimple integration test.
    The test was dependent on an old .bin file.

--- nes-single-node-worker/tests/Integration/SingleNodeIntegrationTestsQueryStatus.cpp
+++ nes-single-node-worker/tests/Integration/SingleNodeIntegrationTestsQueryStatus.cpp
@@ -86,7 +86,8 @@ TEST_F(SingleNodeIntegrationTest, DISABLED_TestQueryStatus)
 TEST_F(SingleNodeIntegrationTest, TestQueryStatusSimple)
 {
     const auto* const resultFileName = "TestQueryStatusSimple";
-    const std::string queryInputFile = fmt::format("{}.bin", "qOneSourceCSV");
+    /// Todo 396: as soon as system level tests support multiple sources, we get rid of the CSV integration tests and cannot depend on this .bin anymore.
+    const std::string queryInputFile = fmt::format("{}.bin", "qTwoSourcesCSVWithFilter");
     const std::string queryResultFile = fmt::format("{}.csv", resultFileName);
     IntegrationTestUtil::removeFile(queryResultFile); /// remove outputFile if exists
 
