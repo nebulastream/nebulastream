77480d3bb2d6531083ad82a518679f8e5c501816
Aljoscha <aljoschalepping@gmail.com>
2024-10-28 14:36:35 +0100

    Revert "Refactored validateAndFormatImpl in Source.hpp and Sink.hpp"
    This reverts commit 1979b46bb6262acbf0f0e02e1d33cd5c51202f3f.

--- nes-execution/src/QueryCompiler/Phases/Translations/LowerLogicalToPhysicalOperators.cpp
+++ nes-execution/src/QueryCompiler/Phases/Translations/LowerLogicalToPhysicalOperators.cpp
@@ -42,9 +42,8 @@ DecomposedQueryPlanPtr LowerLogicalToPhysicalOperators::apply(DecomposedQueryPla
 {
     auto isAlreadyLowered = [](const auto& node)
     {
-        return not(
-            Util::instanceOf<PhysicalOperators::PhysicalOperator>(node) or Util::instanceOf<SourceDescriptorLogicalOperator>(node)
-            or Util::instanceOf<SinkLogicalOperator>(node));
+        return Util::instanceOf<PhysicalOperators::PhysicalOperator>(node) or Util::instanceOf<SourceDescriptorLogicalOperator>(node)
+            or Util::instanceOf<SinkLogicalOperator>(node);
     };
     const std::vector<NodePtr> nodes = PlanIterator(decomposedQueryPlan).snapshot();
     for (const auto& node : nodes | std::views::filter(isAlreadyLowered))
