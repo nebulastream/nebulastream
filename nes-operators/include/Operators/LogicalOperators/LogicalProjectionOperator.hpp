c49d9e00faef55b164d2eff2012daa43ee551e2c
alepping <aljoschalepping@gmail.com>
2024-10-29 10:31:43 +0100

    refactor(projection): constructor: asserting functions are field access or assignment.
    We now assert that the LogicalProjectionOperator only takes field access or assignment functions in the constructor. Since the member `functions` is const and only set in the constructor, we don't need to check the types of functions in the other functions of the LogicalProjectionOperator.

--- nes-operators/include/Operators/LogicalOperators/LogicalProjectionOperator.hpp
+++ nes-operators/include/Operators/LogicalOperators/LogicalProjectionOperator.hpp
@@ -19,36 +19,20 @@
 namespace NES
 {
 
-/**
- * @brief projection operator, which contains an resets the output schema
- */
+/// The projection operator only narrows down the fields of an input schema to a smaller subset. The map operator handles renaming and adding new fields.
 class LogicalProjectionOperator : public LogicalUnaryOperator
 {
 public:
     explicit LogicalProjectionOperator(std::vector<NodeFunctionPtr> functions, OperatorId id);
     ~LogicalProjectionOperator() override = default;
 
-    /**
-     * @brief returns the list of fields that remain in the output schema.
-     * @return  std::vector<NodeFunctionPtr>
-     */
-    std::vector<NodeFunctionPtr> getFunctions() const;
+    const std::vector<NodeFunctionPtr>& getFunctions() const;
 
-    /**
-     * @brief check if two operators have the same output schema
-     * @param rhs the operator to compare
-     * @return bool true if they are the same otherwise false
-     */
     [[nodiscard]] bool equal(NodePtr const& rhs) const override;
     [[nodiscard]] bool isIdentical(NodePtr const& rhs) const override;
     [[nodiscard]] std::string toString() const override;
     void inferStringSignature() override;
 
-    /**
-    * @brief infers the input and out schema of this operator depending on its child.
-    * @param typeInferencePhaseContext needed for stamp inferring
-    * @return true if schema was correctly inferred
-    */
     bool inferSchema() override;
     OperatorPtr copy() override;
 
