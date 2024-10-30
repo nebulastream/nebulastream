10722e63c15568c8a77e99242f991371d8ca3f1f
Leo Raasch <raaschleo@gmail.com>
2024-10-30 16:28:55 +0100

    #420 : Resolved Clang-Tidy warnings in TupleBufferMemoryProvider.cpp

--- nes-data-types/src/Common/PhysicalTypes/PhysicalTypeUtil.cpp
+++ nes-data-types/src/Common/PhysicalTypes/PhysicalTypeUtil.cpp
@@ -12,6 +12,7 @@
     limitations under the License.
 */
 
+#include <Util/Common.hpp>
 #include <Common/PhysicalTypes/BasicPhysicalType.hpp>
 #include <Common/PhysicalTypes/PhysicalTypeUtil.hpp>
 #include <Common/PhysicalTypes/VariableSizedDataPhysicalType.hpp>
