530e4ff2820a4721a9eaa51a72019f715900434c
lukas schwerdtfeger <lukas.schwerdtfeger@gmail.com>
2024-11-22 14:58:32 +0100

    feat(Execution) Adds modulo function

---
+++ nes-execution/src/Execution/Functions/ArithmeticalFunctions/ExecutableFunctionMod.cpp
@@ -0,0 +1,45 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+#include <memory>
+#include <utility>
+#include <vector>
+#include <Execution/Functions/ArithmeticalFunctions/ExecutableFunctionMod.hpp>
+#include <Execution/Functions/Function.hpp>
+#include <Nautilus/DataTypes/VarVal.hpp>
+#include <ErrorHandling.hpp>
+
+namespace NES::Runtime::Execution::Functions
+{
+
+VarVal ExecutableFunctionMod::execute(Record& record) const
+{
+    const auto leftValue = leftExecutableFunctionSub->execute(record);
+    const auto rightValue = rightExecutableFunctionSub->execute(record);
+    return leftValue % rightValue;
+}
+
+ExecutableFunctionMod::ExecutableFunctionMod(
+    std::unique_ptr<Function> leftExecutableFunctionSub, std::unique_ptr<Function> rightExecutableFunctionSub)
+    : leftExecutableFunctionSub(std::move(leftExecutableFunctionSub)), rightExecutableFunctionSub(std::move(rightExecutableFunctionSub))
+{
+}
+
+
+std::unique_ptr<Function> RegisterExecutableFunctionMod(std::vector<std::unique_ptr<Functions::Function>> childFunctions)
+{
+    PRECONDITION(childFunctions.size() == 2, "Mod function must have exactly two sub-functions");
+    return std::make_unique<ExecutableFunctionMod>(std::move(childFunctions[0]), std::move(childFunctions[1]));
+}
+
+}
--- vcpkg/vcpkg-registry/ports/nautilus/portfile.cmake
+++ vcpkg/vcpkg-registry/ports/nautilus/portfile.cmake
@@ -4,6 +4,7 @@ vcpkg_from_github(
 		REF a48e9513def6000b49717a5e8dce954511b1e649
         SHA512 a343ad0e5d3babb1ed071eede64ea2047871f6f139adc094ae43b0fbff5cd950be9c62871ee7cf5068d5e56f05d7647ccdccef29107cfdd4ddc201d9d2c82971
 		PATCHES
+		mod_signess__Signed-off-by__Nils_Schubert__nilslpschubert@gmail_com_.patch
 )
 
 set(ADDITIONAL_CMAKE_OPTIONS "")
