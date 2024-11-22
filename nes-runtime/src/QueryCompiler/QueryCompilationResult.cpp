24b76c6248b8c5192b4ed5c08890b73f00a6a99e
lukas schwerdtfeger <lukas.schwerdtfeger@gmail.com>
2024-11-22 14:53:41 +0100

    chore(Runtime) Removes legacy runtime components

--- nes-runtime/src/QueryCompiler/QueryCompilationResult.cpp
+++ nes-runtime/src/QueryCompiler/QueryCompilationResult.cpp
@@ -11,25 +11,28 @@
     See the License for the specific language governing permissions and
     limitations under the License.
 */
+#include <cstdint>
+#include <memory>
 #include <utility>
 #include <QueryCompiler/QueryCompilationResult.hpp>
 #include <Util/Timer.hpp>
+#include <ExecutableQueryPlan.hpp>
 
 namespace NES::QueryCompilation
 {
 
-QueryCompilationResultPtr QueryCompilationResult::create(Runtime::Execution::ExecutableQueryPlanPtr qep, Timer<>&& timer)
+QueryCompilationResultPtr QueryCompilationResult::create(std::unique_ptr<Runtime::Execution::ExecutableQueryPlan> qep, Timer<>&& timer)
 {
     return std::make_shared<QueryCompilationResult>(QueryCompilationResult(std::move(qep), std::move(timer)));
 }
-QueryCompilationResult::QueryCompilationResult(Runtime::Execution::ExecutableQueryPlanPtr executableQueryPlan, Timer<> timer)
-    : executableQueryPlan(executableQueryPlan), timer(timer)
+QueryCompilationResult::QueryCompilationResult(std::unique_ptr<Runtime::Execution::ExecutableQueryPlan> executableQueryPlan, Timer<> timer)
+    : executableQueryPlan(std::move(executableQueryPlan)), timer(std::move(timer))
 {
 }
 
-Runtime::Execution::ExecutableQueryPlanPtr QueryCompilationResult::getExecutableQueryPlan()
+std::unique_ptr<Runtime::Execution::ExecutableQueryPlan> QueryCompilationResult::takeExecutableQueryPlan()
 {
-    return executableQueryPlan;
+    return std::move(executableQueryPlan);
 }
 
 uint64_t QueryCompilationResult::getCompilationTimeMilli() const
