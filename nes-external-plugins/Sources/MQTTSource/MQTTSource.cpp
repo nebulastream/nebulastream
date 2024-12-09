4d1f01500db1b8e18830b6e51780af6a902ca86e
yschroeder97 <schroeder_yannik@web.de>
2024-12-09 15:43:17 +0000

    Implement stashing mechanism to preserve partial messages between invocations of `fillTupleBuffer`, when the TB would overflow.

--- nes-external-plugins/Sources/MQTTSource/MQTTSource.cpp
+++ nes-external-plugins/Sources/MQTTSource/MQTTSource.cpp
@@ -16,6 +16,7 @@
 
 #include <memory>
 #include <ostream>
+#include <string_view>
 
 #include <Configurations/Descriptor.hpp>
 #include <Sources/Source.hpp>
@@ -24,12 +25,10 @@
 namespace NES::Sources
 {
 
-
 MQTTSource::MQTTSource(const SourceDescriptor& sourceDescriptor)
     : serverURI(sourceDescriptor.getFromConfig(ConfigParametersMQTT::SERVER_URI))
-    , clientId(sourceDescriptor.getFromConfig(ConfigParametersMQTT::CLIENT_ID))
-    , userName(sourceDescriptor.getFromConfig(ConfigParametersMQTT::USER_NAME))
     , topic(sourceDescriptor.getFromConfig(ConfigParametersMQTT::TOPIC))
+    , qos(sourceDescriptor.getFromConfig(ConfigParametersMQTT::QOS))
 {
 }
 
@@ -39,24 +38,27 @@ std::ostream& MQTTSource::toString(std::ostream& str) const
     str << "\n  serverURI: " << serverURI;
     str << "\n  clientId: " << clientId;
     str << "\n  topic: " << topic;
+    str << "\n  qos: " << qos;
     str << ")\n";
     return str;
 }
 
 void MQTTSource::open()
 {
-    mqttClient = std::make_unique<mqtt::async_client>(serverURI, clientId);
+    client = std::make_unique<mqtt::async_client>(serverURI, clientId);
 
     try
     {
-        auto connOpts = mqtt::connect_options_builder().user_name(userName).automatic_reconnect(true).clean_session(true).finalize();
-        auto token = mqttClient->connect(connOpts);
+        auto connOpts = mqtt::connect_options_builder().automatic_reconnect(true).clean_session(true).finalize();
 
+        client->start_consuming();
+
+        auto token = client->connect(connOpts);
         auto response = token->get_connect_response();
 
         if (response.is_session_present())
         {
-            mqttClient->subscribe(topic, 1)->wait();
+            client->subscribe(topic, 1)->wait();
         }
     }
     catch (const mqtt::exception& e)
@@ -64,13 +66,52 @@ void MQTTSource::open()
     }
 }
 
-size_t MQTTSource::fillTupleBuffer(NES::Memory::TupleBuffer& /*tupleBuffer*/)
+size_t MQTTSource::fillTupleBuffer(NES::Memory::TupleBuffer& tupleBuffer)
 {
-    return 0;
+    size_t tbOffset = 0;
+    const size_t tbSize = tupleBuffer.getBufferSize();
+
+    std::string_view payload;
+    /// If there is a stashed payload, consume it first
+    if (!payloadStash.empty())
+    {
+        payload = payloadStash.consume(tbSize);
+        tbOffset = writePayloadToBuffer(payload, tupleBuffer, tbOffset);
+    }
+
+    /// When the stashed payload is larger than a single TB, we skip the loop
+    while (tbOffset < tbSize)
+    {
+        auto message = client->consume_message();
+        payload = message->get_payload();
+        tbOffset = writePayloadToBuffer(payload, tupleBuffer, tbOffset);
+    }
+
+    return tbSize;
+}
+
+/// Write the message payload to the tuple buffer
+/// @return the offset in the TB after writing the payload
+size_t MQTTSource::writePayloadToBuffer(std::string_view payload, NES::Memory::TupleBuffer& tb, size_t tbOffset)
+{
+    /// If the payload fits into the TB, write it
+    if (tbOffset + payload.size() <= tb.getBufferSize())
+    {
+        std::memcpy(tb.getBuffer() + tbOffset, payload.data(), payload.size());
+        return tbOffset += payload.size();
+    }
+
+    /// Otherwise, split. Write the prefix to the TB and stash the remainder
+    auto prefixSize = tb.getBufferSize() - tbOffset;
+    std::memcpy(tb.getBuffer() + tbOffset, payload.data(), prefixSize);
+
+    payloadStash.stash(std::string_view{payload.data() + prefixSize, payload.size() - prefixSize});
 }
 
 void MQTTSource::close()
 {
+    client->unsubscribe(topic)->wait();
+    client->disconnect()->wait();
 }
 
 std::unique_ptr<NES::Configurations::DescriptorConfig::Config>
--- nes-external-plugins/Sources/MQTTSource/MQTTSource.hpp
+++ nes-external-plugins/Sources/MQTTSource/MQTTSource.hpp
@@ -16,6 +16,7 @@
 
 #include "Sources/Source.hpp"
 
+#include <iostream>
 #include <optional>
 #include <string>
 
@@ -27,38 +28,31 @@ namespace NES::Sources
 {
 
 
-/// Defines the names, (optional) default values, (optional) validation & config functions for all MQTT config parameters.
-struct ConfigParametersMQTT
-{
-    static inline const Configurations::DescriptorConfig::ConfigParameter<std::string> SERVER_URI{
-        "serverURI", std::nullopt, [](const std::unordered_map<std::string, std::string>& config) {
-            return Configurations::DescriptorConfig::tryGet(SERVER_URI, config);
-        }};
-
-    static inline const Configurations::DescriptorConfig::ConfigParameter<std::string> CLIENT_ID{
-        "clientId", std::nullopt, [](const std::unordered_map<std::string, std::string>& config) {
-            return Configurations::DescriptorConfig::tryGet(CLIENT_ID, config);
-        }};
-
-    static inline const Configurations::DescriptorConfig::ConfigParameter<std::string> USER_NAME{
-        "userName", std::nullopt, [](const std::unordered_map<std::string, std::string>& config) {
-            return Configurations::DescriptorConfig::tryGet(USER_NAME, config);
-        }};
-
-    static inline const Configurations::DescriptorConfig::ConfigParameter<std::string> TOPIC{
-        "topic", std::nullopt, [](const std::unordered_map<std::string, std::string>& config) {
-            return Configurations::DescriptorConfig::tryGet(TOPIC, config);
-        }};
-
-    static inline std::unordered_map<std::string, Configurations::DescriptorConfig::ConfigParameterContainer> parameterMap
-        = Configurations::DescriptorConfig::createConfigParameterContainerMap();
-};
-
-
 class MQTTSource : public Source
 {
 public:
     static inline const std::string NAME = "MQTT";
+
+    enum class QualityOfService : uint8_t
+    {
+        AT_MOST_ONCE = 0,
+        AT_LEAST_ONCE = 1,
+        EXACTLY_ONCE = 2
+    };
+    friend std::ostream& operator<<(std::ostream& str, const QualityOfService& qos)
+    {
+        switch (qos)
+        {
+            case QualityOfService::AT_MOST_ONCE:
+                return str << "AT_MOST_ONCE";
+            case QualityOfService::AT_LEAST_ONCE:
+                return str << "AT_LEAST_ONCE";
+            case QualityOfService::EXACTLY_ONCE:
+                return str << "EXACTLY_ONCE";
+            default:
+                return str << "UNKNOWN";
+        }
+    }
 
     explicit MQTTSource(const SourceDescriptor& sourceDescriptor);
     ~MQTTSource() override = default;
@@ -68,10 +62,11 @@ public:
     MQTTSource(MQTTSource&&) = delete;
     MQTTSource& operator=(MQTTSource&&) = delete;
 
-    size_t fillTupleBuffer(NES::Memory::TupleBuffer& tupleBuffer) override;
-
     /// Open connection to MQTT broker.
     void open() override;
+
+    size_t fillTupleBuffer(NES::Memory::TupleBuffer& tupleBuffer) override;
+
     /// Close connection to MQTT broker.
     void close() override;
 
@@ -83,9 +78,119 @@ public:
 private:
     std::string serverURI;
     std::string clientId;
-    std::string userName;
     std::string topic;
+    QualityOfService qos;
 
-    std::unique_ptr<mqtt::async_client> mqttClient;
+    std::unique_ptr<mqtt::async_client> client;
+
+    /// A simple stash for persisting partial payloads that did not fit into a TB between calls to `fillTupleBuffer`.
+    /// When a payload does not fit, stash the remainder.
+    /// On the next invocation, consume the stashed payload before reading new data.
+    /// The stash can be in two states:
+    /// 1. Empty: consumed == tail == 0
+    /// 2. Partly Consumed: consumed < tail
+    ///
+    /// The stash has the following two operations:
+    /// 1. Stash a new payload by placing it into the internal buffer. This operation is allowed only when the stash is in empty state.
+    /// 2. Consume up to `numBytes` from the stashed payload. This operation is allowed only when the stash is in partly consumed state.
+    ///
+    /// The consume method takes care of transferring the stash into the empty state when the whole payload has been consumed.
+    /// The caller has to take care of consuming before stashing more data.
+    class PayloadStash
+    {
+    public:
+        PayloadStash() { buf.reserve(STASH_TARGET_CAPACITY); }
+        ~PayloadStash() = default;
+
+        /// No copying or moving necessary.
+        [[nodiscard]] PayloadStash(const PayloadStash&) = delete;
+        PayloadStash(PayloadStash&&) = delete;
+        PayloadStash& operator=(const PayloadStash&) = delete;
+        PayloadStash& operator=(PayloadStash&&) = delete;
+
+        /// When the consumed pointer equals the tail pointer, everything has been consumed.
+        [[nodiscard]] bool empty() const { return consumed == tail; }
+
+        /// Consume up to `numBytes` from the stashed payload.
+        /// This is either the capacity of a TB, or the remaining part of the stashed payload, whatever is smaller.
+        /// @return a view to the consumed payload
+        [[nodiscard]] std::string_view consume(size_t numBytes)
+        {
+            PRECONDITION(numBytes > 0, "numBytes must be a positive integer.");
+            INVARIANT(consumed < tail)
+
+            numBytes = std::min(numBytes, tail - consumed);
+            auto consumeView = std::string_view{buf}.substr(consumed, numBytes);
+            consumed += numBytes;
+
+            /// If we consumed the whole payload, reset the stash
+            if (consumed == tail)
+            {
+                reset();
+            }
+            return consumeView;
+        }
+
+        /// Stash a new payload by copying it into the internal buffer.
+        /// Reset the consumed pointer and set the tail pointer to the end of the payload.
+        void stash(std::string_view payload)
+        {
+            PRECONDITION(payload.size() > 0, "Payload must not be empty.");
+            INVARIANT(consumed == 0 && tail == 0, "Consume stash before stashing a new payload.");
+
+            buf.assign(payload);
+            consumed = 0;
+            tail = payload.size();
+
+            /// If the last payload increased over the default capacity, shrink it back.
+            if (buf.capacity() > STASH_TARGET_CAPACITY)
+            {
+                buf.shrink_to_fit();
+            }
+        }
+
+    private:
+        void reset()
+        {
+            consumed = 0;
+            tail = 0;
+        }
+
+        const size_t STASH_TARGET_CAPACITY = 1024;
+
+        std::string buf;
+        size_t consumed = 0;
+        size_t tail = 0;
+    };
+
+    PayloadStash payloadStash;
+
+    void stashPayloadRemainder(std::string_view remainder);
+
+    size_t writePayloadToBuffer(std::string_view payload, NES::Memory::TupleBuffer& tb, size_t tbOffset);
 };
+
+/// Defines the names, (optional) default values, (optional) validation & config functions for all MQTT config parameters.
+struct ConfigParametersMQTT
+{
+    static inline const Configurations::DescriptorConfig::ConfigParameter<std::string> SERVER_URI{
+        "serverURI", std::nullopt, [](const std::unordered_map<std::string, std::string>& config) {
+            return Configurations::DescriptorConfig::tryGet(SERVER_URI, config);
+        }};
+
+    static inline const Configurations::DescriptorConfig::ConfigParameter<std::string> TOPIC{
+        "topic", std::nullopt, [](const std::unordered_map<std::string, std::string>& config) {
+            return Configurations::DescriptorConfig::tryGet(TOPIC, config);
+        }};
+
+    static inline const Configurations::DescriptorConfig::ConfigParameter<Configurations::EnumWrapper, MQTTSource::QualityOfService> QOS{
+        "qos", MQTTSource::QualityOfService::AT_LEAST_ONCE, [](const std::unordered_map<std::string, std::string>& config) {
+            return Configurations::DescriptorConfig::tryGet(QOS, config);
+        }};
+
+    static inline std::unordered_map<std::string, Configurations::DescriptorConfig::ConfigParameterContainer> parameterMap
+        = Configurations::DescriptorConfig::createConfigParameterContainerMap();
+};
+
+
 }
