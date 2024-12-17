982bbe3194cc85fd321a66190faa37db63db2d3a
lukas schwerdtfeger <lukas.schwerdtfeger@gmail.com>
2024-12-17 11:36:58 +0100

    fix(Nautilus) Adds patch to prevent concurrent llvm jit access

--- vcpkg/vcpkg-registry/ports/nautilus/portfile.cmake
+++ vcpkg/vcpkg-registry/ports/nautilus/portfile.cmake
@@ -5,6 +5,8 @@ vcpkg_from_github(
         SHA512 a343ad0e5d3babb1ed071eede64ea2047871f6f139adc094ae43b0fbff5cd950be9c62871ee7cf5068d5e56f05d7647ccdccef29107cfdd4ddc201d9d2c82971
 		PATCHES
 		mod_signess__Signed-off-by__Nils_Schubert__nilslpschubert@gmail_com_.patch
+		cache.patch
+		llvm.patch
 )
 
 set(ADDITIONAL_CMAKE_OPTIONS "")
