e18bb7a52d1015d9087c130d8a140d2ceb23a0b3
Nils Schubert <nilslpschubert@gmail.com>
2025-01-14 10:41:23 +0100

    feat(Aggregation) Chained Hash Map:
    We have added a chained hash map and a nautilus reference/wrapper around it. We have omitted the test for this hash map, as the test uses a new nautilus test utils that should be reviewed separately.

--- nes-nautilus/include/Nautilus/Interface/Hash/HashFunction.hpp
+++ nes-nautilus/include/Nautilus/Interface/Hash/HashFunction.hpp
@@ -13,47 +13,24 @@
 */
 
 #pragma once
+#include <vector>
 #include <Nautilus/DataTypes/VarVal.hpp>
 
 namespace NES::Nautilus::Interface
 {
 
-/**
- * @brief Interface for hash function on Nautilus values.
- * Sub classes can provide specific hash algorithms.
- */
+/// Interface for hash function on Nautilus values.
+/// Subclasses can provide specific hash algorithms.
 class HashFunction
 {
 public:
     using HashValue = nautilus::val<uint64_t>;
-
-    /**
-     * @brief Calculates the hash of one value.
-     * @param value a nautilus value
-     * @return the hash
-     */
-    HashValue calculate(VarVal value);
-
-    /**
-     * @brief Calculates the hash across a set of values.
-     * @param values vector of nautilus values.
-     * @return the hash
-     */
-    HashValue calculate(std::vector<VarVal>& values);
+    [[nodiscard]] HashValue calculate(const VarVal& value) const;
+    [[nodiscard]] HashValue calculate(const std::vector<VarVal>& values) const;
     virtual ~HashFunction() = default;
 
 protected:
-    /**
-     * @brief Initializes a hash value, e.g. a specific seed.
-     * @return HashValue
-     */
-    virtual HashValue init() = 0;
-    /**
-     * @brief Calculates the hash of a specific value.
-     * @param hash
-     * @param value
-     * @return HashValue
-     */
-    virtual HashValue calculate(HashValue& hash, VarVal& value) = 0;
+    [[nodiscard]] virtual HashValue init() const = 0;
+    virtual HashValue calculate(HashValue& hash, const VarVal& value) const = 0;
 };
 }
