ad5b22078da8de1c8c964e4cdf0776404369b533
Nils Schubert <nilslpschubert@gmail.com>
2025-01-14 10:21:53 +0100

    feat(Execution) Windowed Operator:
    We have created a WindowOperatorBuild and WindowOperatorProbe as two general executable operators for window operators. This allows us to reuse some code, i.e., triggering windows or flushing the state

--- nes-execution/include/Execution/Operators/Streaming/WindowBasedOperatorHandler.hpp
+++ nes-execution/include/Execution/Operators/Streaming/WindowBasedOperatorHandler.hpp
@@ -16,7 +16,9 @@
 
 #include <cstdint>
 #include <functional>
+#include <map>
 #include <memory>
+#include <optional>
 #include <vector>
 #include <Execution/Operators/SliceStore/WindowSlicesStoreInterface.hpp>
 #include <Execution/Operators/Watermark/MultiOriginWatermarkProcessor.hpp>
@@ -28,13 +30,14 @@
 #include <Runtime/AbstractBufferProvider.hpp>
 #include <Runtime/QueryTerminationType.hpp>
 #include <Util/Execution.hpp>
+#include <ErrorHandling.hpp>
 
 namespace NES::Runtime::Execution::Operators
 {
 /// This is the base class for all window-based operator handlers, e.g., join and aggregation.
 /// It assumes that they have a build and a probe phase.
-/// The build phase is the phase where the operator adds tuples to window(s)/state.
-/// The probe phase gets triggered from the build phase and is the phase where the operator processes the build-up state, e.g., perform the join or aggregation.
+/// The build phase is the phase where the operator adds tuples to window(s) / the state.
+/// The probe phase gets triggered by the build phase and is the phase where the operator processes the build-up state, e.g., performs the join or aggregation.
 class WindowBasedOperatorHandler : public OperatorHandler
 {
 public:
@@ -47,28 +50,38 @@ public:
 
     /// We can not call opHandler->start() from Nautilus, as we only get a pointer in the proxy function in Nautilus, e.g., setupProxy() in StreamJoinBuild
     void setWorkerThreads(uint64_t numberOfWorkerThreads);
-    void setBufferProvider(std::shared_ptr<Memory::AbstractBufferProvider> bufferProvider);
     void start(PipelineExecutionContext& pipelineExecutionContext, uint32_t localStateVariableId) override;
     void stop(QueryTerminationType queryTerminationType, PipelineExecutionContext& pipelineExecutionContext) override;
 
     WindowSlicesStoreInterface& getSliceAndWindowStore() const;
 
-    /// Updates the corresponding watermark processor and then garbage collect all slices and windows that are not valid anymore
+    /// Updates the corresponding watermark processor, and then garbage collects all slices and windows that are not valid anymore
     void garbageCollectSlicesAndWindows(const BufferMetaData& bufferMetaData) const;
 
-    /// Returns the sequence number for this window info. It is assumed that the first call to this method is the first window that is triggered.
-    /// This is important as a window-based operator acts as a source and thus needs to set the sequence number for the emitted tuples
-    [[nodiscard]] SequenceNumber getNextSequenceNumber(const WindowInfo& windowInfo);
+    /// Checks and triggers windows that are ready to be triggered, e.g., the watermark has passed the window end for time-based windows.
+    /// This method updates the watermarkProcessor and is thread-safe
+    virtual void checkAndTriggerWindows(const BufferMetaData& bufferMetaData, PipelineExecutionContext* pipelineCtx);
 
+    /// Triggers all windows that have not been already emitted to the probe
+    virtual void triggerAllWindows(PipelineExecutionContext* pipelineCtx);
 
-    virtual std::function<std::vector<std::shared_ptr<Slice>>(SliceStart, SliceEnd)> getCreateNewSlicesFunction() const = 0;
+    /// Gives the specific operator handler the chance to provide a function that creates new slices
+    /// This method is being called whenever a new slice is needed, e.g., receiving a timestamp that is not yet in the slice store.
+    virtual std::function<std::vector<std::shared_ptr<Slice>>(SliceStart, SliceEnd)>
+    getCreateNewSlicesFunction(const Memory::AbstractBufferProvider* bufferProvider) const = 0;
 
 protected:
+    /// Gets called if slices should be triggered once a window is ready to be emitted.
+    /// Each window operator can be specific about what to do if the given slices are ready to be emitted
+    virtual void triggerSlices(
+        const std::map<WindowInfoAndSequenceNumber, std::vector<std::shared_ptr<Slice>>>& slicesAndWindowInfo,
+        PipelineExecutionContext* pipelineCtx)
+        = 0;
+
     std::unique_ptr<WindowSlicesStoreInterface> sliceAndWindowStore;
     std::unique_ptr<MultiOriginWatermarkProcessor> watermarkProcessorBuild;
     std::unique_ptr<MultiOriginWatermarkProcessor> watermarkProcessorProbe;
     uint64_t numberOfWorkerThreads;
     const OriginId outputOriginId;
-    std::shared_ptr<Memory::AbstractBufferProvider> bufferProvider;
 };
 }
