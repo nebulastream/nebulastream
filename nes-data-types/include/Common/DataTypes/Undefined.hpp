7f7df80a91e643408f6ad1d69a646e2897382e1e
Tim Nacken <timnicknacken@gmail.com>
2025-01-15 17:23:48 +0100

    chore(replace-dataTypes-equals-with==) Removed requested doxygen comments. And also some other i found unnecassary in DataTypes folder.

--- nes-data-types/include/Common/DataTypes/Undefined.hpp
+++ nes-data-types/include/Common/DataTypes/Undefined.hpp
@@ -41,10 +41,6 @@ public:
      */
     DataTypePtr join(DataTypePtr otherDataType) override;
 
-    /**
-    * @brief Returns a string representation of the data type.
-    * @return string
-    */
     std::string toString() override;
 };
 
