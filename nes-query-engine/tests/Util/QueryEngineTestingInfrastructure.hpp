96983a75b895c5ed0407092461d46a3e948b9c65
Nils Schubert <nilslpschubert@gmail.com>
2025-01-17 08:36:04 +0100

    fix(TestSource) Increased default await time
    We have increased the default await, as otherwise the test is failing due to a timeout.

--- nes-query-engine/tests/Util/QueryEngineTestingInfrastructure.hpp
+++ nes-query-engine/tests/Util/QueryEngineTestingInfrastructure.hpp
@@ -69,8 +69,8 @@ static constexpr size_t NUMBER_OF_TUPLES_PER_BUFFER = 23;
 static constexpr size_t NUMBER_OF_BUFFERS_PER_SOURCE = 300;
 static constexpr size_t NUMBER_OF_THREADS = 2;
 static constexpr size_t LARGE_NUMBER_OF_THREADS = 8;
-constexpr std::chrono::milliseconds DEFAULT_AWAIT_TIMEOUT = std::chrono::milliseconds(100);
-constexpr std::chrono::milliseconds DEFAULT_LONG_AWAIT_TIMEOUT = std::chrono::milliseconds(1000);
+constexpr std::chrono::milliseconds DEFAULT_AWAIT_TIMEOUT = std::chrono::milliseconds(200);
+constexpr std::chrono::milliseconds DEFAULT_LONG_AWAIT_TIMEOUT = std::chrono::milliseconds(2000);
 
 /// Creates raw TupleBuffer data based on a recognizable pattern which can later be identified using `verifyIdentifier`.
 std::vector<std::byte> identifiableData(size_t identifier);
