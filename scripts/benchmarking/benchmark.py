c589a1eb44c1b812138f2eeb424b5b6acfd37ade
Klaas tom Dieck <klaas.tomdieck@gmail.com>
2025-01-24 11:15:51 +0100

    feat(Benchmarking) compile time is now uploaded to conbench

--- scripts/benchmarking/benchmark.py
+++ scripts/benchmarking/benchmark.py
@@ -1,4 +1,5 @@
 import json
+import subprocess
 import time
 import os
 import logging
@@ -9,6 +10,8 @@ log = logging.getLogger(__name__)
 from benchadapt import BenchmarkResult
 from benchadapt.adapters import CallableAdapter, BenchmarkAdapter, GoogleBenchmarkAdapter
 from benchrun import Benchmark, BenchmarkList, Iteration, CaseList
+
+import argparse
 
 DEFAULT_RESULT_DIR = "/tmp/nebulastream-public/cmake-build-debug-nes/nes-systests/result"
 DEFAULT_EXECUTABLE_PATH = "/tmp/nebulastream-public/cmake-build-debug-nes/nes-systests/systest/systest"
@@ -52,17 +55,70 @@ class SystestAdapter(BenchmarkAdapter):
 
         return benchmarkResults
 
-#os.environ["CONBENCH_URL"] = "https://bench.nebula.stream"
-#os.environ["CONBENCH_EMAIL"] = "user@example.com"
-#os.environ["CONBENCH_PASSWORD"] = "password"
-#os.environ["CONBENCH_PROJECT_REPOSITORY"] = "https://github.com/conbench/conbench"
-#os.environ["CONBENCH_PROJECT_COMMIT"] = "d7009ab93f1f4b79e3f95692a87a002da0465dd0"
-#os.environ["CONBENCH_RUN_REASON"] = "Testing"
 
+class CompileTimeBenchmark(BenchmarkAdapter):
+    """
+    Adapter that runs the build and times it
+    """
+    def __init__(
+            self,
+            result_fields_override: dict[str, Any] = None,
+            result_fields_append: dict[str, Any] = None,
+    ):
+        super().__init__(
+            command=[],
+            result_fields_append=result_fields_append,
+            result_fields_override=result_fields_override
+        )
 
-systest_adapter = SystestAdapter(
-    systest_result_dir="./build/nes-systests/result/",
-    result_fields_override={"run_reason": os.getenv("CONBENCH_RUN_REASON")},
-)
+    def run(self, params: List[str] = None) -> List[BenchmarkResult]:
+        if os.path.isdir("build"):
+            os.rmdir("build")
 
-systest_adapter.post_results()
+        log.info("Creating build directory")
+        #subprocess.run(["cmake", "-GNinja", "-B build"])
+        log.info("Starting build")
+        start_time = time.time()
+        #subprocess.run(["cmake", "--build", "build", "-j", "--", "--quiet"])
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        log.info(f"Build finished. Took: {elapsed_time:.2f} seconds")
+        self.results = [BenchmarkResult(
+            stats={
+                "data": [elapsed_time],
+                "unit": "s",
+            },
+            context={"benchmark_language": "CMake"}, # required else page won't load
+            tags={"name": "NebulaStream compile time"},
+        )]
+        self.results = self.transform_results()
+        return self.results
+    def _transform_results(self) -> List[BenchmarkResult]:
+        return self.results
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="NebulastreamBenchmarker",
+        description="A Utility to benchmark Nebulastream using various tools / upload benchmark result"
+    )
+    parser.add_argument("--systest", action="store_true", help="upload systest results")
+    parser.add_argument("--compile-time", action="store_true", help="Benchmark Nebulastream compile time and upload results")
+
+    args = parser.parse_args()
+
+    if args.systest:
+
+        systest_adapter = SystestAdapter(
+            systest_result_dir="./build/nes-systests/result/",
+            result_fields_override={"run_reason": os.getenv("CONBENCH_RUN_REASON")},
+        )
+
+        systest_adapter.post_results()
+    elif args.compile_time:
+        compile_time_adapter = CompileTimeBenchmark(
+            result_fields_override={"run_reason": os.getenv("CONBENCH_RUN_REASON")},
+        )
+        compile_time_adapter.run()
+        compile_time_adapter.post_results()
+    else:
+        log.fatal("No operation specified!")
