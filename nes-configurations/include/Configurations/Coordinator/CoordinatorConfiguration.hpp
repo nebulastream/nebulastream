e47e48b5241dcb17348197f6ddc97750e9ec8879
flang <flang@asian-lion.dima.tu-berlin.de>
2025-02-01 15:32:54 +0100

    included proactive deployment flag for compatibility with benchmark script

--- nes-configurations/include/Configurations/Coordinator/CoordinatorConfiguration.hpp
+++ nes-configurations/include/Configurations/Coordinator/CoordinatorConfiguration.hpp
@@ -86,6 +86,10 @@ class CoordinatorConfiguration : public BaseConfiguration {
         RequestProcessor::StorageHandlerType::TwoPhaseLocking,
         "The Storage Handler Type (TwoPhaseLocking, SerialHandler)"};
 
+    BoolOption enableProactiveDeployment = {ENABLE_PROACTIVE_DEPLOYMENT,
+                                             "false",
+                                             "Enable proactive deployment of migrated sub query plans. (Default: false)"};
+
     /**
      * @brief Configures different properties for the query optimizer.
      */
@@ -157,6 +161,7 @@ class CoordinatorConfiguration : public BaseConfiguration {
                 &rpcPort,
                 &restPort,
                 &logLevel,
+                &enableProactiveDeployment, //TODO remove
                 &enableMonitoring,
                 &configPath,
                 &worker,
