8b190020c5dac739a776e1f06355f4ce12c821e3
NES-CI <nebulastream@dima.tu-berlin.de>
2025-02-04 17:44:42 +0000

    GIT-CI: Updating NES version to 0.6.208

--- nes-common/include/Version/version.hpp
+++ nes-common/include/Version/version.hpp
@@ -18,7 +18,7 @@
 #define NES_VERSION_MAJOR 0
 #define NES_VERSION_MINOR 6
 #define NES_VERSION_PATCH 208
-#define NES_VERSION_POST_FIX -SNAPSHOT
-constexpr char const* NES_VERSION      = "0.6.208-SNAPSHOT";
+#define NES_VERSION_POST_FIX 
+constexpr char const* NES_VERSION      = "0.6.208";
 #endif // NES_NES_CORE_INCLUDE_VERSION_VERSION_HPP_
 // clang-format on
