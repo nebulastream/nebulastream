213eb0767b5dd5e6cc6caa75d89581a807c858c1
AKozar <kozar.anastasiia@gmail.com>
2025-02-11 15:59:26 +0100

    latency + loadBalancing as a parameter

--- nes-runtime/src/Sinks/Mediums/NullOutputSink.cpp
+++ nes-runtime/src/Sinks/Mediums/NullOutputSink.cpp
@@ -61,60 +61,60 @@ bool NullOutputSink::writeData(Runtime::TupleBuffer& inputBuffer, Runtime::Worke
     std::unique_lock lock(writeMutex);
     if (!duplicateDetectionCallback(inputBuffer)) {
         updateWatermarkCallback(inputBuffer);
-        // std::string str = "";
-    // auto now = std::chrono::system_clock::now();
-    // auto now_ms = std::chrono::time_point_cast<std::chrono::milliseconds>(now);
-    // auto epoch = now_ms.time_since_epoch();
-    // auto value = std::chrono::duration_cast<std::chrono::milliseconds>(epoch);
-    // auto ts = std::chrono::system_clock::now();
-    // timeval curTime;
-    // gettimeofday(&curTime, NULL);
-    // int milli = curTime.tv_usec / 1000;
-    // auto timeNow = std::chrono::system_clock::to_time_t(ts);
-    // char buffer[26];
-    // int millisec;
-    // struct tm* tm_info;
-    // struct timeval tv;
-    //
-    // gettimeofday(&tv, NULL);
-    //
-    // millisec = lrint(tv.tv_usec / 1000.0);// Round to nearest millisec
-    // if (millisec >= 1000) {               // Allow for rounding up to nearest second
-    //     millisec -= 1000;
-    //     tv.tv_sec++;
-    // }
-    //
-    // tm_info = localtime(&tv.tv_sec);
-    //
-    // strftime(buffer, 26, "%Y:%m:%d %H:%M:%S", tm_info);
-    // statisticsFile << std::put_time(std::localtime(&timeNow), "%Y-%m-%d %H:%M:%S") << ".";
-    // if (millisec < 10)
-    //     statisticsFile << "00" << millisec << ",";
-    // else if (millisec < 100)
-    //     statisticsFile << "0" << millisec << ",";
-    // else
-    //     statisticsFile << millisec << ",";
-    // unsigned lastDelimPos = str.find_last_of("|");
-    // unsigned firstDelimPos = str.find("|");
-    // unsigned nextDelimPosition = firstDelimPos;
-    // while (nextDelimPosition != lastDelimPos) {
-    //     std::cout << "firstDelimPos" << firstDelimPos;
-    //     firstDelimPos = nextDelimPosition;
-    //     nextDelimPosition = str.find_first_of("|", nextDelimPosition + 2);
-    //     std::cout << "nextDelimPosition" << nextDelimPosition;
-    // }
-    // if (!str.empty()) {
-    //     std::string creationTimestamp = str.substr(firstDelimPos + 1, nextDelimPosition - firstDelimPos - 1);
-    //     std::string::const_iterator it = creationTimestamp.begin();
-    //     while (it != creationTimestamp.end() && std::isdigit(*it)) ++it;
-    //     if (it == creationTimestamp.end()) {
-    //         statisticsFile << value.count() - std::atoll(creationTimestamp.c_str()) << "\n";
-    //         statisticsFile.flush();
-    //     }
-    // }
-    // else {
-    //     statisticsFile << value.count() - inputBuffer.getCreationTimestampInMS() << "\n";
-    // }
+        std::string str = "";
+    auto now = std::chrono::system_clock::now();
+    auto now_ms = std::chrono::time_point_cast<std::chrono::milliseconds>(now);
+    auto epoch = now_ms.time_since_epoch();
+    auto value = std::chrono::duration_cast<std::chrono::milliseconds>(epoch);
+    auto ts = std::chrono::system_clock::now();
+    timeval curTime;
+    gettimeofday(&curTime, NULL);
+    int milli = curTime.tv_usec / 1000;
+    auto timeNow = std::chrono::system_clock::to_time_t(ts);
+    char buffer[26];
+    int millisec;
+    struct tm* tm_info;
+    struct timeval tv;
+
+    gettimeofday(&tv, NULL);
+
+    millisec = lrint(tv.tv_usec / 1000.0);// Round to nearest millisec
+    if (millisec >= 1000) {               // Allow for rounding up to nearest second
+        millisec -= 1000;
+        tv.tv_sec++;
+    }
+
+    tm_info = localtime(&tv.tv_sec);
+
+    strftime(buffer, 26, "%Y:%m:%d %H:%M:%S", tm_info);
+    statisticsFile << std::put_time(std::localtime(&timeNow), "%Y-%m-%d %H:%M:%S") << ".";
+    if (millisec < 10)
+        statisticsFile << "00" << millisec << ",";
+    else if (millisec < 100)
+        statisticsFile << "0" << millisec << ",";
+    else
+        statisticsFile << millisec << ",";
+    unsigned lastDelimPos = str.find_last_of("|");
+    unsigned firstDelimPos = str.find("|");
+    unsigned nextDelimPosition = firstDelimPos;
+    while (nextDelimPosition != lastDelimPos) {
+        std::cout << "firstDelimPos" << firstDelimPos;
+        firstDelimPos = nextDelimPosition;
+        nextDelimPosition = str.find_first_of("|", nextDelimPosition + 2);
+        std::cout << "nextDelimPosition" << nextDelimPosition;
+    }
+    if (!str.empty()) {
+        std::string creationTimestamp = str.substr(firstDelimPos + 1, nextDelimPosition - firstDelimPos - 1);
+        std::string::const_iterator it = creationTimestamp.begin();
+        while (it != creationTimestamp.end() && std::isdigit(*it)) ++it;
+        if (it == creationTimestamp.end()) {
+            statisticsFile << value.count() - std::atoll(creationTimestamp.c_str()) << "\n";
+            statisticsFile.flush();
+        }
+    }
+    else {
+        statisticsFile << value.count() - inputBuffer.getCreationTimestampInMS() << "\n";
+    }
     }
 
     return true;
