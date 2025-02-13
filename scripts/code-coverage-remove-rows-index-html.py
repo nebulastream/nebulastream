d1b473701f4d8c4df072ccdee6287286958404f6
Matthis Gördel <matthis@posteo.de>
2025-02-13 17:07:45 +0100

    refactor/fix codecov html export

--- scripts/code-coverage-remove-rows-index-html.py
+++ scripts/code-coverage-remove-rows-index-html.py
@@ -2,42 +2,24 @@ import sys
 from bs4 import BeautifulSoup
 
 
-def split_wrapper(index, line):
-    return line.split("nebulastream-public/nebulastream-public", 1)[index]
-
-
 def main():
 
     if len(sys.argv) != 4:
-        print("Usage: python code-coverage-remove-rows-index-html.py <index_file> <output_file> <tested_files_path>")
-        exit(1)
+        print("Usage: python code-coverage-remove-rows-index-html.py <index_file> <output_file> <changed_files_path>")
+        sys.exit(1)
 
-    # We expect that this script is called with the following arguments
-    # Arg[1]: Absolute path to the index.html file
-    # Arg[2]: Absolute path where the script should write the output index.html file
-    # Arg[3]: Absolute path to the file that contains the changed files.
-    # We expect that each line contains the absolute path to the changed file
+    # path to the index.html file
     index_file = sys.argv[1]
+    # path where the script should write the output index.html file
     output_file = sys.argv[2]
-    tested_files = sys.argv[3]
+    # path to the file that contains the changed files, which will be used to filter index.html
+    changed_files_path = sys.argv[3]
 
-    # Read base path to nebulastream-public/nebulastream-public
-    with open(tested_files, "r", encoding="utf-8") as file:
-        line = file.readline().strip()
+    # Read the list of changed files
+    with open(changed_files_path, "r", encoding="utf-8") as file:
+        changed_files = [line.strip() for line in file.readlines()]
 
-        expected_path = "nebulastream-public/nebulastream-public"
-        assert expected_path in line, f"Expected '{expected_path}' in the line, but it was not found. Line content: '{line}'"
-
-        base_path = split_wrapper(0, line)
-
-        # removes __w as the rest of the script expects it to be gone
-        base_path = base_path.rstrip("__w")  # Removes "__w" from the end
-
-    # Read the list of tested files
-    with open(tested_files, "r", encoding="utf-8") as file:
-        tested_files_list = [split_wrapper(-1, line.strip()).rstrip(".html") for line in file.readlines()]
-
-    print(f"Tested files: {tested_files_list}")
+    print(f"Changed files: {changed_files}")
 
     # Read the index.html file and parse it with BeautifulSoup, so that we can later manipulate it
     with open(index_file, "r", encoding="utf-8") as file:
@@ -45,28 +27,23 @@ def main():
 
     # Locate the table containing the data
     table = soup.find('table')
-    assert table, "No table found in the HTML file."
+    assert table, "Expected table in the HTML file."
 
-    # Extract rows
-    rows = table.find_all('tr')[1:]
-
-    # We have to change the path of the style.css file in the index.html
-    # This is necessary because the index.html file is in a different relative path than the style.css file
-    table_style = soup.find('link')
-    table_style['href'] = "build/ccov/all-merged/style.css"
-
-    # Iterate over table rows and filter based on filenames
+    # Iterate over table rows and remove rows that report cov for unchanged files.
+    rows = table.find_all('tr')
     for row in rows:
-        filename_cell = row.find('td')  # Assuming the filename is in the first column
-        filename = filename_cell.text.strip()
-        filename = split_wrapper(-1, filename)
-        print(f"Filename: {filename}")
-        if filename not in tested_files_list:
+        a = row.find('a')
+
+        # skip table header, footer
+        if not a:
+            continue
+
+        filename = a.get("href")
+        if filename not in changed_files:
             row.decompose()  # Remove the row
-        else:
-            filename_cell_tag = filename_cell.find('a')
-            base_path = base_path.rstrip("__w/")
-            filename_cell_tag['href'] = base_path + "/" + filename_cell.text.strip() + ".html"
+
+        # remove path prefix /.../nebulastream-public/ (if in string)
+        a.string = a.text.split("nebulastream-public/")[-1]
 
     # Save the modified HTML to a new file
     with open(output_file, 'w', encoding='utf-8') as file:
