37c2159a9e7ce21f948d73b70bc4c40d3f207217
Philipp-Timm <philipptimm25@gmail.com>
2025-02-24 16:16:29 +0100

    feat(Antlr): added query control statements

--- nes-sql-parser/src/AntlrSQLQueryParser.cpp
+++ nes-sql-parser/src/AntlrSQLQueryParser.cpp
@@ -25,7 +25,7 @@
 namespace NES::AntlrSQLQueryParser
 {
 
-std::shared_ptr<QueryPlan> createLogicalQueryPlanFromSQLString(std::string_view queryString)
+QueryParseResult createLogicalQueryPlanFromSQLString(std::string_view queryString)
 {
     try
     {
@@ -38,9 +38,34 @@ std::shared_ptr<QueryPlan> createLogicalQueryPlanFromSQLString(std::string_view 
         AntlrSQLParser::QueryContext* tree = parser.query();
         Parsers::AntlrSQLQueryPlanCreator queryPlanCreator;
         antlr4::tree::ParseTreeWalker::DEFAULT.walk(&queryPlanCreator, tree);
+
+        if (auto* controlStmt = dynamic_cast<AntlrSQLParser::QueryControlStatementContext*>(tree->children[0]))
+        {
+            if (auto stopQueryContext = dynamic_cast<AntlrSQLParser::StopQueryContext*>(controlStmt))
+            {
+                auto queryIdString = stopQueryContext->INTEGER_VALUE()->getText();
+                return QueryParseResult{QueryControlStatement::STOP, nullptr, QueryId(std::stoi(queryIdString))};
+            }
+            if (auto statusQueryContext = dynamic_cast<AntlrSQLParser::StatusQueryContext*>(controlStmt))
+            {
+                auto queryIdString = statusQueryContext->INTEGER_VALUE()->getText();
+                return QueryParseResult{QueryControlStatement::STATUS, nullptr, QueryId(std::stoi(queryIdString))};
+            }
+            if (auto unregisterQueryContext = dynamic_cast<AntlrSQLParser::UnregisterQueryContext*>(controlStmt))
+            {
+                auto queryIdString = unregisterQueryContext->INTEGER_VALUE()->getText();
+                return QueryParseResult{QueryControlStatement::UNREGISTER, nullptr, QueryId(std::stoi(queryIdString))};
+            }
+            if (dynamic_cast<AntlrSQLParser::RegisterQueryContext*>(controlStmt) != nullptr)
+            {
+                auto queryPlan = queryPlanCreator.getQueryPlan();
+                return QueryParseResult{QueryControlStatement::REGISTER, queryPlan, std::nullopt};
+            }
+        }
         auto queryPlan = queryPlanCreator.getQueryPlan();
+
         NES_DEBUG("Created the following query from antlr AST: \n{}", queryPlan->toString());
-        return queryPlan;
+        return QueryParseResult{QueryControlStatement::START, queryPlan, std::nullopt};
     }
     catch (antlr4::RuntimeException antlrException)
     {
