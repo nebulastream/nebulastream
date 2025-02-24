2da832208d1ba0cef263e37ed0091e04cc3b5a3a
Ankit Chaudhary <ankit.chaudhary@tu-berlin.de>
2025-02-24 19:24:38 +0100

    added loggin in the deployment phase

--- nes-coordinator/src/Phases/DeploymentPhase.cpp
+++ nes-coordinator/src/Phases/DeploymentPhase.cpp
@@ -35,11 +35,13 @@ DeploymentPhase::DeploymentPhase(const Catalogs::Query::QueryCatalogPtr& queryCa
 
 void DeploymentPhase::execute(const std::set<Optimizer::DeploymentContextPtr>& deploymentContexts, RequestType requestType) {
     if (!deploymentContexts.empty()) {
-        NES_INFO("Register or unregister {} decomposed queries.", deploymentContexts.size());
+        NES_ERROR("Register or unregister {} decomposed queries.", deploymentContexts.size());
         registerOrStopDecomposedQueryPlan(deploymentContexts, requestType);
+        NES_ERROR("Register or unregister {} decomposed queries completed.", deploymentContexts.size());
 
-        NES_INFO("Start or stop {} decomposed queries.", deploymentContexts.size());
+        NES_ERROR("Start or stop {} decomposed queries.", deploymentContexts.size());
         startOrUnregisterDecomposedQueryPlan(deploymentContexts, requestType);
+        NES_ERROR("Start or stop {} decomposed queries completed.", deploymentContexts.size());
     }
 }
 
