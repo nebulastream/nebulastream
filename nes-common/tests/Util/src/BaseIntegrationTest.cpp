49238c7573c16e3e315219e0a454f91f4743a464
Matthis Gördel <matthis@posteo.de>
2025-03-07 01:46:21 +0100

    clang-tidy: fix all, make error: cppcoreguidelines-init-variables

--- nes-common/tests/Util/src/BaseIntegrationTest.cpp
+++ nes-common/tests/Util/src/BaseIntegrationTest.cpp
@@ -32,30 +32,29 @@ static std::uniform_int_distribution<> dis2(8, 11);
 std::string generateUUID()
 {
     std::stringstream ss;
-    int i;
     ss << std::hex;
-    for (i = 0; i < 8; i++)
+    for (int i = 0; i < 8; i++)
     {
         ss << dis(gen);
     }
     ss << "-";
-    for (i = 0; i < 4; i++)
+    for (int i = 0; i < 4; i++)
     {
         ss << dis(gen);
     }
     ss << "-4";
-    for (i = 0; i < 3; i++)
+    for (int i = 0; i < 3; i++)
     {
         ss << dis(gen);
     }
     ss << "-";
     ss << dis2(gen);
-    for (i = 0; i < 3; i++)
+    for (int i = 0; i < 3; i++)
     {
         ss << dis(gen);
     }
     ss << "-";
-    for (i = 0; i < 12; i++)
+    for (int i = 0; i < 12; i++)
     {
         ss << dis(gen);
     }
