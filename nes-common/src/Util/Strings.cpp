72dde0dd271c4fbee6309ab46bee45a7988d1440
alepping <aljoschalepping@gmail.com>
2025-03-07 11:28:22 +0100

    chore(Strings): from_chars_with_exception and 'char' support.
    Added a version of 'from_chars' that throws a meaningful exception. Also added 'char' support for both 'from_char' and 'from_char_with_exception'.

--- nes-common/src/Util/Strings.cpp
+++ nes-common/src/Util/Strings.cpp
@@ -30,6 +30,7 @@
 
 namespace NES::Util
 {
+
 template <>
 std::optional<float> from_chars<float>(const std::string_view input)
 {
@@ -59,6 +60,52 @@ std::optional<bool> from_chars<bool>(const std::string_view input)
     return {};
 }
 
+template <>
+std::optional<char> from_chars<char>(const std::string_view input)
+{
+    return (input.size() == 1) ? std::optional(input.front()) : std::nullopt;
+}
+
+template <>
+bool from_chars_with_exception(std::string_view input)
+{
+    if (const auto boolValue = from_chars<bool>(input); boolValue.has_value())
+    {
+        return boolValue.value();
+    }
+    throw CannotFormatMalformedStringValue("'{}' is not a supported boolean value.", input);
+}
+
+template <>
+float from_chars_with_exception<float>(std::string_view input)
+{
+    if (const auto floatValue = from_chars<float>(input); floatValue.has_value())
+    {
+        return floatValue.value();
+    }
+    throw CannotFormatMalformedStringValue("'{}' is not a supported float value.", input);
+}
+
+template <>
+double from_chars_with_exception<double>(std::string_view input)
+{
+    if (const auto doubleValue = from_chars<double>(input); doubleValue.has_value())
+    {
+        return doubleValue.value();
+    }
+    throw CannotFormatMalformedStringValue("'{}' is not a supported double value.", input);
+}
+
+template <>
+char from_chars_with_exception<char>(std::string_view input)
+{
+    if (const auto charValue = from_chars<char>(input); charValue.has_value())
+    {
+        return charValue.value();
+    }
+    throw CannotFormatMalformedStringValue("'{}' is not a supported char value.", input);
+}
+
 std::string formatFloat(std::floating_point auto value)
 {
     std::string formatted = fmt::format("{:.6f}", value);
@@ -76,9 +123,6 @@ std::string formatFloat(std::floating_point auto value)
 
     return formatted.substr(0, lastNonZero + 1);
 }
-/// explicit instantiations
-template std::string formatFloat(float);
-template std::string formatFloat(double);
 
 template <>
 std::optional<double> from_chars<double>(const std::string_view input)
@@ -93,6 +137,10 @@ std::optional<double> from_chars<double>(const std::string_view input)
         return {};
     }
 }
+
+/// explicit instantiations
+template std::string formatFloat(float);
+template std::string formatFloat(double);
 
 template <>
 std::optional<std::string> from_chars<std::string>(const std::string_view input)
