406d113784a207d758b3d537af85d2ea749ea771
Klaas tom Dieck <klaas.tom-dieck@campus.tu-berlin.de>
2025-03-14 14:35:08 +0100

    chore(clang-tidy)
    added check if error has value, chrono header to directly include, changed declarations from const auto to auto *const
    
    added gtest header to directly include stuff
    
    removed redundant initializers in QuerýRunSummary
    
    added missing headers and a check if error has a value
    
    format script

--- nes-systests/systest/include/SystestRunner.hpp
+++ nes-systests/systest/include/SystestRunner.hpp
@@ -22,7 +22,6 @@
 #include <string_view>
 #include <utility>
 #include <vector>
-#include <Operators/Serialization/DecomposedQueryPlanSerializationUtil.hpp>
 #include <Plans/DecomposedQueryPlan/DecomposedQueryPlan.hpp>
 #include <nlohmann/json_fwd.hpp>
 #include <SingleNodeWorkerConfiguration.hpp>
