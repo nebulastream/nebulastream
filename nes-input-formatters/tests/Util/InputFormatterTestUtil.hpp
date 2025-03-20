5818de65beb7f84cbedabb7305d3c708be9ca39c
alepping <aljoschalepping@gmail.com>
2025-03-20 18:18:10 +0100

    chore(SequentialTestTaskQueue): Is now single-threaded

--- nes-input-formatters/tests/Util/InputFormatterTestUtil.hpp
+++ nes-input-formatters/tests/Util/InputFormatterTestUtil.hpp
@@ -57,21 +57,18 @@ enum class TestDataTypes : uint8_t
 struct ThreadInputBuffers
 {
     SequenceNumber sequenceNumber;
-    WorkerThreadId workerThreadId;
     std::string rawBytes;
 };
 
 struct TaskPackage
 {
     SequenceNumber sequenceNumber;
-    WorkerThreadId workerThreadId;
     Memory::TupleBuffer rawByteBuffer;
 };
 
 template <typename TupleSchemaTemplate>
 struct WorkerThreadResults
 {
-    WorkerThreadId::Underlying workerThreadId;
     std::vector<std::vector<TupleSchemaTemplate>> expectedResultsForThread;
 };
 
@@ -79,7 +76,6 @@ template <typename TupleSchemaTemplate>
 struct TestConfig
 {
     size_t numRequiredBuffers{};
-    size_t numThreads{};
     uint64_t bufferSize{};
     Sources::ParserConfig parserConfig;
     std::vector<TestDataTypes> testSchema;
@@ -124,7 +120,7 @@ struct TestHandle
     std::shared_ptr<Memory::BufferManager> testBufferManager;
     std::shared_ptr<std::vector<std::vector<NES::Memory::TupleBuffer>>> resultBuffers;
     std::shared_ptr<Schema> schema;
-    std::unique_ptr<Runtime::Execution::SequentialTestTaskQueue> testTaskQueue;
+    std::unique_ptr<Runtime::Execution::SingleThreadedTestTaskQueue> testTaskQueue;
     std::vector<TaskPackage> inputBuffers;
     std::vector<std::vector<Memory::TupleBuffer>> expectedResultVectors;
 
@@ -185,15 +181,14 @@ bool validateResult(const TestHandle<TupleSchemaTemplate>& testHandle)
 template <typename TupleSchemaTemplate, bool PrintDebug>
 std::vector<std::vector<Memory::TupleBuffer>> createExpectedResults(const TestHandle<TupleSchemaTemplate>& testHandle)
 {
-    std::vector<std::vector<Memory::TupleBuffer>> expectedTupleBuffers(testHandle.testConfig.numThreads);
+    std::vector<std::vector<Memory::TupleBuffer>> expectedTupleBuffers(1);
     for (const auto workerThreadResultVector : testHandle.testConfig.expectedResults)
     {
         /// expectedBuffersVector: vector<TupleSchemaTemplate>
         for (const auto& expectedBuffersVector : workerThreadResultVector.expectedResultsForThread)
         {
-            expectedTupleBuffers.at(workerThreadResultVector.workerThreadId)
-                .emplace_back(TestUtil::createTupleBufferFromTuples<TupleSchemaTemplate, false, PrintDebug>(
-                    testHandle.schema, *testHandle.testBufferManager, expectedBuffersVector));
+            expectedTupleBuffers.at(0).emplace_back(TestUtil::createTupleBufferFromTuples<TupleSchemaTemplate, false, PrintDebug>(
+                testHandle.schema, *testHandle.testBufferManager, expectedBuffersVector));
         }
     }
     return expectedTupleBuffers;
@@ -205,14 +200,14 @@ TestHandle<TupleSchemaTemplate> setupTest(const TestConfig<TupleSchemaTemplate>&
     std::shared_ptr<Memory::BufferManager> testBufferManager
         = Memory::BufferManager::create(testConfig.bufferSize, 2 * testConfig.numRequiredBuffers);
     std::shared_ptr<std::vector<std::vector<NES::Memory::TupleBuffer>>> resultBuffers
-        = std::make_shared<std::vector<std::vector<NES::Memory::TupleBuffer>>>(testConfig.numThreads);
+        = std::make_shared<std::vector<std::vector<NES::Memory::TupleBuffer>>>(1);
     std::shared_ptr<Schema> schema = createSchema(testConfig.testSchema);
     return {
         testConfig,
         testBufferManager,
         resultBuffers,
         std::move(schema),
-        std::make_unique<Runtime::Execution::SequentialTestTaskQueue>(testConfig.numThreads, testBufferManager, resultBuffers),
+        std::make_unique<Runtime::Execution::SingleThreadedTestTaskQueue>(testBufferManager, resultBuffers),
         {},
         {}};
 }
@@ -229,8 +224,8 @@ std::vector<Runtime::Execution::TestablePipelineTask> createTasks(const TestHand
     std::vector<Runtime::Execution::TestablePipelineTask> tasks;
     for (const auto& inputBuffer : testHandle.inputBuffers)
     {
-        tasks.emplace_back(createInputFormatterTask(
-            inputBuffer.sequenceNumber, inputBuffer.workerThreadId, inputBuffer.rawByteBuffer, inputFormatterTask));
+        tasks.emplace_back(
+            createInputFormatterTask(inputBuffer.sequenceNumber, WorkerThreadId(0), inputBuffer.rawByteBuffer, inputFormatterTask));
     }
     return tasks;
 }
@@ -244,9 +239,7 @@ std::vector<TaskPackage> createTestTupleBuffers(const TestHandle<TupleSchemaTemp
         auto tupleBuffer = testHandle.testBufferManager->getBufferNoBlocking();
         INVARIANT(tupleBuffer, "Couldn't get buffer from bufferManager. Configure test to use more buffers.");
         rawTupleBuffers.emplace_back(TaskPackage{
-            rawInputBuffer.sequenceNumber,
-            rawInputBuffer.workerThreadId,
-            TestUtil::copyStringDataToTupleBuffer(rawInputBuffer.rawBytes, std::move(tupleBuffer.value()))});
+            rawInputBuffer.sequenceNumber, TestUtil::copyStringDataToTupleBuffer(rawInputBuffer.rawBytes, std::move(tupleBuffer.value()))});
     }
     return rawTupleBuffers;
 }
