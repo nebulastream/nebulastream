aa554e68987da5a14e15ba5a0363d6b8f6ad1193
Adrian Michalke <michalke@tu-berlin.de>
2025-03-28 15:50:22 +0100

    chore(LegacyOptimizer): LogicalSourceExpansionRule.cpp

---
+++ nes-nebuli/src/LegacyOptimizer/QueryRewrite/LogicalSourceExpansionRule.cpp
@@ -0,0 +1,49 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include <string>
+#include <vector>
+#include <unordered_set>
+#include <Operators/Sources/SourceDescriptorLogicalOperator.hpp>
+#include <Operators/Sources/SourceNameLogicalOperator.hpp>
+#include <LegacyOptimizer/QueryRewrite/LogicalSourceExpansionRule.hpp>
+#include <Plans/QueryPlan.hpp>
+#include <SourceCatalogs/PhysicalSource.hpp>
+#include <ErrorHandling.hpp>
+
+namespace NES::LegacyOptimizer
+{
+
+void LogicalSourceExpansionRule::apply(QueryPlan& queryPlan, Catalogs::Source::SourceCatalog& sourceCatalog)
+{
+    auto sourceOperators = queryPlan.getOperatorByType<SourceNameLogicalOperator>();
+
+    for (SourceNameLogicalOperator sourceOp : sourceOperators)
+    {
+        std::string logicalSourceName(sourceOp.getName());
+        auto sourceCatalogEntries = sourceCatalog.getPhysicalSources(logicalSourceName);
+        if (sourceCatalogEntries.empty())
+        {
+            throw PhysicalSourceNotFoundInQueryDescription("LogicalSourceExpansionRule: Unable to find physical source locations for the logical source " + logicalSourceName);
+        }
+        for (const auto& sourceCatalogEntry : sourceCatalogEntries)
+        {
+            auto sourceDescriptor = sourceCatalogEntry->getPhysicalSource()->createSourceDescriptor(sourceOp.getSchema());
+            auto logicalDescOp = SourceDescriptorLogicalOperator(std::move(sourceDescriptor));
+            queryPlan.replaceOperator(sourceOp, logicalDescOp);
+        }
+    }
+}
+
+}
