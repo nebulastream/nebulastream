7caf08fa72c224591dfee86af0160efeb72e43e9
Adrian Michalke <michalke@tu-berlin.de>
2025-03-28 15:28:10 +0100

    chore(LowerToPhysicalSource)
    refactor(QueryOptimizer): add lowering for sink and source

---
+++ nes-query-optimizer/src/RewriteRules/LowerToPhysical/LowerToPhysicalSink.cpp
@@ -0,0 +1,42 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include <memory>
+#include <vector>
+#include <Functions/FunctionProvider.hpp>
+#include <Nautilus/Interface/MemoryProvider/RowTupleBufferMemoryProvider.hpp>
+#include <Operators/LogicalOperator.hpp>
+#include <Operators/Sinks/SinkLogicalOperator.hpp>
+#include <RewriteRules/AbstractRewriteRule.hpp>
+#include <RewriteRules/LowerToPhysical/LowerToPhysicalSink.hpp>
+#include <RewriteRuleRegistry.hpp>
+#include <SinkPhysicalOperator.hpp>
+
+namespace NES::Optimizer
+{
+
+std::vector<PhysicalOperatorWrapper> LowerToPhysicalSink::apply(LogicalOperator logicalOperator)
+{
+    auto sink = *logicalOperator.get<SinkLogicalOperator>();
+    auto physicalOperator = SinkPhysicalOperator(sink.sinkDescriptor);
+    auto wrapper = PhysicalOperatorWrapper(physicalOperator, sink.getInputSchemas()[0], sink.getOutputSchema());
+    return {wrapper};
+}
+
+std::unique_ptr<Optimizer::AbstractRewriteRule>
+RewriteRuleGeneratedRegistrar::RegisterSinkRewriteRule(RewriteRuleRegistryArguments argument)
+{
+    return std::make_unique<NES::Optimizer::LowerToPhysicalSink>(argument.conf);
+}
+}
---
+++ nes-query-optimizer/src/RewriteRules/LowerToPhysical/LowerToPhysicalSource.cpp
@@ -0,0 +1,45 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include <memory>
+#include <Operators/Sinks/SinkLogicalOperator.hpp>
+#include <Operators/Sources/SourceDescriptorLogicalOperator.hpp>
+#include <RewriteRules/AbstractRewriteRule.hpp>
+#include <RewriteRules/LowerToPhysical/LowerToPhysicalSource.hpp>
+#include <RewriteRuleRegistry.hpp>
+#include <SinkPhysicalOperator.hpp>
+
+namespace NES::Optimizer
+{
+
+RewriteRuleResult LowerToPhysicalSource::apply(LogicalOperator logicalOperator)
+{
+    PRECONDITION(logicalOperator.tryGet<SourceDescriptorLogicalOperator>(), "Expected a SourceDescriptorLogicalOperator");
+    auto source = *logicalOperator.get<SourceDescriptorLogicalOperator>();
+
+    auto outputOriginIds = source.getOutputOriginIds();
+    PRECONDITION(outputOriginIds.size() == 1, "SourceDescriptorLogicalOperator has exactly one origin id");
+    auto physicalOperator = SourcePhysicalOperator(source.getSourceDescriptor(), outputOriginIds[0]);
+
+    auto inputSchemas = logicalOperator.getInputSchemas();
+    PRECONDITION(inputSchemas.size() == 1, "SourceDescriptorLogicalOperator has exactly one schema");
+    auto wrapper = std::make_shared<PhysicalOperatorWrapper>(physicalOperator, inputSchemas[0], logicalOperator.getOutputSchema());
+    return {.root = wrapper, .leafOperators = {wrapper}};
+}
+
+RewriteRuleRegistryReturnType RewriteRuleGeneratedRegistrar::RegisterSourceRewriteRule(RewriteRuleRegistryArguments argument)
+{
+    return std::make_unique<NES::Optimizer::LowerToPhysicalSource>(argument.conf);
+}
+}
