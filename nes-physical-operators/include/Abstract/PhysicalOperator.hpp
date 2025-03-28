775bab9e5984efcaeceb947fcfc826548b1782dd
Adrian Michalke <michalke@tu-berlin.de>
2025-03-28 15:37:48 +0100

    Constructor for PhysicalOperatorWrapper

--- nes-physical-operators/include/Abstract/PhysicalOperator.hpp
+++ nes-physical-operators/include/Abstract/PhysicalOperator.hpp
@@ -98,6 +98,10 @@ public:
         self->setChild(child);
     }
 
+    bool operator==(const PhysicalOperator &other) const {
+        return self->equals(*other.self);
+    }
+
     void setup(ExecutionContext& executionCtx) const
     {
         self->setup(executionCtx);
@@ -122,13 +126,15 @@ public:
     {
         self->execute(executionCtx, record);
     }
-private:
+
+
     std::string toString() const {
         return self->toString();
     }
 
     struct Concept : PhysicalOperatorConcept {
         [[nodiscard]] virtual std::unique_ptr<Concept> clone() const = 0;
+        [[nodiscard]] virtual bool equals(const Concept& other) const = 0;
     };
 
     template<typename T>
@@ -175,6 +181,14 @@ private:
             data.execute(executionCtx, record);
         }
 
+        [[nodiscard]] bool equals(const Concept& other) const override {
+            if (auto p = dynamic_cast<const Model<T>*>(&other)) {
+                return data == p->data;
+            }
+            return false;
+        }
+    };
+
         std::string toString() const override
         {
             return "PhysicalOperator(" + std::string(typeid(T).name()) + ")";
@@ -191,36 +205,9 @@ struct PhysicalOperatorWrapper
 
     PhysicalOperator physicalOperator;
     std::optional<Schema> inputSchema, outputSchema;
-    std::vector<std::shared_ptr<PhysicalOperatorWrapper>> children {};
+    std::vector<std::unique_ptr<PhysicalOperatorWrapper>> children;
 
-    bool isPipelineBreaker = false;
-    std::optional<std::shared_ptr<OperatorHandler>> handler;
-    std::optional<OperatorHandlerId> handlerId;
-
-    bool isScan = false;
-    bool isEmit = false;
-
-    /// @brief Returns a string representation of the wrapper.
-    std::string toString() const {
-        std::ostringstream oss;
-        oss << "PhysicalOperatorWrapper(";
-        oss << "Operator: " << physicalOperator.toString() << ", ";
-        oss << "InputSchema: " << (inputSchema ? "present" : "none") << ", ";
-        oss << "OutputSchema: " << (outputSchema ? "present" : "none") << ", ";
-        oss << "isScan: " << std::boolalpha << isScan << ", ";
-        oss << "isEmit: " << std::boolalpha << isEmit;
-        if (!children.empty()) {
-            oss << ", Children: [";
-            for (size_t i = 0; i < children.size(); ++i)
-            {
-                oss << children[i]->toString();
-                if (i + 1 < children.size())
-                    oss << ", ";
-            }
-            oss << "]";
-        }
-        oss << ")";
-        return oss.str();
-    }
+};
 }
-}
+
+FMT_OSTREAM(NES::PhysicalOperator);
