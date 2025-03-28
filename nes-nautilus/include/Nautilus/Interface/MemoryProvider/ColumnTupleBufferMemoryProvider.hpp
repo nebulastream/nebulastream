7170577ddbe33adefc76bb13e00c4d55bc0f6d14
Adrian Michalke <michalke@tu-berlin.de>
2025-03-28 15:47:56 +0100

    ColumnTupleBufferMemoryProvider.cpp return MemoryLayout as sharedptr

--- nes-nautilus/include/Nautilus/Interface/MemoryProvider/ColumnTupleBufferMemoryProvider.hpp
+++ nes-nautilus/include/Nautilus/Interface/MemoryProvider/ColumnTupleBufferMemoryProvider.hpp
@@ -30,7 +30,11 @@ public:
     ColumnTupleBufferMemoryProvider(std::shared_ptr<Memory::MemoryLayouts::ColumnLayout> columnMemoryLayoutPtr);
     ~ColumnTupleBufferMemoryProvider() override = default;
 
-    std::shared_ptr<Memory::MemoryLayouts::MemoryLayout> getMemoryLayout() override;
+    std::shared_ptr<Memory::MemoryLayouts::MemoryLayout> getMemoryLayout() const override;
+    ColumnTupleBufferMemoryProvider(const ColumnTupleBufferMemoryProvider& other)
+        : columnMemoryLayout(other.columnMemoryLayout)
+    {
+    }
 
     Record readRecord(
         const std::vector<Record::RecordFieldIdentifier>& projections,
