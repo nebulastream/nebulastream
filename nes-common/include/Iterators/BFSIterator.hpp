d0e39e34f8f63f1a0c8db4623d3be78838ee7b34
Adrian Michalke <michalke@tu-berlin.de>
2025-03-29 16:40:53 +0100

    feat: create templated DFSInterator and BFSInterator classes

---
+++ nes-common/include/Iterators/BFSIterator.hpp
@@ -0,0 +1,93 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#pragma once
+
+#include <iterator>
+#include <cstddef>
+#include <memory>
+#include <queue>
+
+namespace NES
+{
+
+template <typename T>
+class BFSIterator
+{
+public:
+    using iterator_category = std::forward_iterator_tag;
+    using value_type        = std::shared_ptr<T>;
+    using difference_type   = std::ptrdiff_t;
+    using pointer           = std::shared_ptr<T>*;
+    using reference         = std::shared_ptr<T>&;
+
+    BFSIterator() = default;
+
+    explicit BFSIterator(std::shared_ptr<T> root) {
+        if (root) {
+            nodeQueue.push(std::move(root));
+        }
+    }
+
+    BFSIterator& operator++() {
+        if (!nodeQueue.empty()) {
+            auto current = nodeQueue.front();
+            nodeQueue.pop();
+
+            for (auto& child : current->children) {
+                nodeQueue.push(child);
+            }
+        }
+        return *this;
+    }
+
+    bool operator==(const BFSIterator& other) const {
+        return nodeQueue.empty() && other.nodeQueue.empty();
+    }
+    bool operator!=(const BFSIterator& other) const {
+        return !(*this == other);
+    }
+
+    std::shared_ptr<T> operator*() const {
+        if (!nodeQueue.empty()) {
+            return nodeQueue.front();
+        }
+        return nullptr;
+    }
+
+private:
+    std::queue<std::shared_ptr<T>> nodeQueue;
+};
+
+
+/// Wrapper for running ranges
+template <typename T>
+class BFSRange {
+public:
+    explicit BFSRange(std::shared_ptr<T> root)
+        : root(std::move(root))
+    { }
+
+    BFSIterator<T> begin() const {
+        return BFSIterator<T>(root);
+    }
+
+    BFSIterator<T> end() const {
+        return BFSIterator<T>();
+    }
+
+private:
+    std::shared_ptr<T> root;
+};
+}
---
+++ nes-common/include/Iterators/DFSIterator.hpp
@@ -0,0 +1,91 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#pragma once
+
+#include <memory>
+#include <vector>
+#include <stack>
+
+namespace NES
+{
+
+
+template <typename T>
+class DFSIterator {
+public:
+    using iterator_category = std::forward_iterator_tag;
+    using value_type        = std::shared_ptr<T>;
+    using difference_type   = std::ptrdiff_t;
+    using pointer           = std::shared_ptr<T>*;
+    using reference         = std::shared_ptr<T>&;
+
+    DFSIterator() = default;
+
+    explicit DFSIterator(std::shared_ptr<T> root) {
+        if (root) {
+            nodeStack.push(std::move(root));
+        }
+    }
+
+    DFSIterator& operator++() {
+        if (!nodeStack.empty()) {
+            auto current = nodeStack.top();
+            nodeStack.pop();
+
+            auto& children = current->children;
+            for (auto it = children.rbegin(); it != children.rend(); ++it) {
+                nodeStack.push(*it);
+            }
+        }
+        return *this;
+    }
+
+    bool operator==(const DFSIterator& other) const {
+        return nodeStack.empty() && other.nodeStack.empty();
+    }
+    bool operator!=(const DFSIterator& other) const {
+        return !(*this == other);
+    }
+
+    std::shared_ptr<T> operator*() const {
+        if (!nodeStack.empty()) {
+            return nodeStack.top();
+        }
+        return nullptr;
+    }
+
+private:
+    std::stack<std::shared_ptr<T>> nodeStack;
+};
+
+template <typename T>
+class DFSRange {
+public:
+    explicit DFSRange(std::shared_ptr<T> root)
+        : root(std::move(root)) {}
+
+    DFSIterator<T> begin() const {
+        return DFSIterator<T>(root);
+    }
+
+    DFSIterator<T> end() const {
+        return DFSIterator<T>();
+    }
+
+private:
+    std::shared_ptr<T> root;
+};
+
+}
