ffc381ff3a955a2239dcb0ee0ef452b68adc0a55
Adrian Michalke <michalke@tu-berlin.de>
2025-03-29 20:14:32 +0100

    includes

--- nes-sql-parser/private/AntlrSQLParser/AntlrSQLHelper.hpp
+++ nes-sql-parser/private/AntlrSQLParser/AntlrSQLHelper.hpp
@@ -22,9 +22,7 @@
 #include <vector>
 #include <API/Query.hpp>
 #include <Functions/FieldAccessLogicalFunction.hpp>
-#include <Operators/LogicalOperators/Windows/Aggregations/WindowAggregationDescriptor.hpp>
-#include <Operators/LogicalOperators/Windows/Joins/LogicalJoinDescriptor.hpp>
-#include <Plans/Query/QueryPlan.hpp>
+#include <Operators/LogicalOperators/Windows/Aggregations/WindowAggregationFunction.hpp>
 #include <Sinks/SinkDescriptor.hpp>
 
 namespace NES::Parsers
