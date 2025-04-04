430053b1d8a865cb30e6d9d514a944c67067b21e
alepping <aljoschalepping@gmail.com>
2025-04-04 17:47:57 +0200

    feat(FieldAccessFunction): abstract function for field access
    Enables, e.g., binary formats that simply calculate the tuple/field offsets to use(and implement) such field access functions without runtime overhead. Prior, InputFormatter implementations needed to fill TupleBuffers with offsets to fields. The more general FieldAccessFunction simply demands that an InputFormatter returns an object that can return a pointer to a field in the tuple that the object currently points to and to increment the pointer to the beginning of the next tuple. This allows to 'FieldAccessFunctions' that are, e.g., purely algebraic, which is a useful, e.g., for our native format.

--- nes-input-formatters/src/CSVInputFormatIndexer.cpp
+++ nes-input-formatters/src/CSVInputFormatIndexer.cpp
@@ -24,81 +24,91 @@
 #include <utility>
 
 #include <InputFormatters/InputFormatIndexer.hpp>
-#include <InputFormatters/InputFormatterTask.hpp>
+#include <InputFormatters/InputFormatterTaskPipeline.hpp>
 #include <Sources/SourceDescriptor.hpp>
 #include <Util/Logger/Logger.hpp>
 #include <fmt/format.h>
 #include <ErrorHandling.hpp>
 #include <FieldOffsets.hpp>
 #include <InputFormatIndexerRegistry.hpp>
+#include <InputFormatterTask.hpp>
+
+namespace
+{
+void initializeIndexFunctionForTuple(
+    NES::InputFormatters::FieldOffsets& fieldOffsets,
+    const std::string_view tuple,
+    const NES::InputFormatters::FieldOffsetsType startIdxOfTuple,
+    const NES::ParserConfig& config,
+    size_t numberOfFieldsInSchema)
+{
+    /// The start of the tuple is the offset of the first field of the tuple
+    size_t fieldIdx = 0;
+    fieldOffsets.writeOffsetAt(startIdxOfTuple, fieldIdx++);
+    /// Find field delimiters, until reaching the end of the tuple
+    /// The position of the field delimiter (+ size of field delimiter) is the beginning of the next field
+    for (size_t nextFieldOffset = tuple.find(config.fieldDelimiter, 0); nextFieldOffset != std::string_view::npos;
+         nextFieldOffset = tuple.find(config.fieldDelimiter, nextFieldOffset))
+    {
+        nextFieldOffset += config.fieldDelimiter.size();
+        fieldOffsets.writeOffsetAt(startIdxOfTuple + nextFieldOffset, fieldIdx); ///NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+        ++fieldIdx;
+    }
+    /// The last delimiter is the size of the tuple itself, which allows the next phase to determine the last field without any extra calculations
+    fieldOffsets.writeOffsetAt(startIdxOfTuple + tuple.size(), numberOfFieldsInSchema);
+    if (fieldIdx != numberOfFieldsInSchema)
+    {
+        throw NES::CannotFormatSourceData(
+            "Number of parsed fields does not match number of fields in schema (parsed {} vs {} schema", fieldIdx, numberOfFieldsInSchema);
+    }
+}
+}
 
 namespace NES::InputFormatters
 {
 
-CSVInputFormatIndexer::CSVInputFormatIndexer(InputFormatIndexerRegistryArguments args)
-    : config(std::move(args.inputFormatIndexerConfig)), numberOfFieldsInSchema(args.numberOfFieldsInSchema)
+CSVInputFormatIndexer::CSVInputFormatIndexer(ParserConfig config, const size_t numberOfFieldsInSchema)
+    : config(std::move(config)), numberOfFieldsInSchema(numberOfFieldsInSchema)
 {
 }
 
-void CSVInputFormatIndexer::indexTuple(
-    const std::string_view tuple, FieldOffsetsType* fieldOffsets, const FieldOffsetsType startIdxOfTuple) const
+void CSVInputFormatIndexer::indexRawBuffer(FieldOffsets& fieldOffsets, const RawTupleBuffer& rawBuffer, const TupleMetaData&) const
 {
-    PRECONDITION(fieldOffsets != nullptr, "FieldOffsets cannot be null.");
+    fieldOffsets.startSetup(numberOfFieldsInSchema, this->config.fieldDelimiter.size());
 
-    /// The start of the tuple is the offset of the first field of the tuple
-    *fieldOffsets = startIdxOfTuple;
-    size_t fieldIdx = 1;
-    /// Find field delimiters, until reaching the end of the tuple
-    /// The position of the field delimiter (+ size of field delimiter) is the beginning of the next field
-    for (size_t nextFieldOffset = tuple.find(this->config.fieldDelimiter, 0); nextFieldOffset != std::string_view::npos;
-         nextFieldOffset = tuple.find(this->config.fieldDelimiter, nextFieldOffset))
-    {
-        nextFieldOffset += this->config.fieldDelimiter.size();
-        *(fieldOffsets + fieldIdx) = startIdxOfTuple + nextFieldOffset; ///NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
-        ++fieldIdx;
-    }
-    if (fieldIdx != numberOfFieldsInSchema)
-    {
-        throw CannotFormatSourceData(
-            "Number of parsed fields does not match number of fields in schema (parsed {} vs {} schema", fieldIdx, numberOfFieldsInSchema);
-    }
-}
-
-InputFormatIndexer::FirstAndLastTupleDelimiterOffsets
-CSVInputFormatIndexer::indexBuffer(std::string_view bufferView, FieldOffsets& fieldOffsets) const
-{
     const auto sizeOfTupleDelimiter = this->config.tupleDelimiter.size();
-    const auto offsetOfFirstTupleDelimiter = static_cast<FieldOffsetsType>(bufferView.find(this->config.tupleDelimiter));
+    const auto offsetOfFirstTupleDelimiter = static_cast<FieldOffsetsType>(rawBuffer.getBufferView().find(this->config.tupleDelimiter));
 
     /// If the buffer does not contain a delimiter, set the 'offsetOfFirstTupleDelimiter' to a value larger than the buffer size to tell
     /// the InputFormatIndexerTask that there was no tuple delimiter in the buffer and return
     if (offsetOfFirstTupleDelimiter == static_cast<FieldOffsetsType>(std::string::npos))
     {
-        return {
-            .offsetOfFirstTupleDelimiter = std::numeric_limits<FieldOffsetsType>::max(),
-            .offsetOfLastTupleDelimiter = std::numeric_limits<FieldOffsetsType>::max()};
+        fieldOffsets.markNoTupleDelimiters();
+        return;
     }
 
     /// If the buffer contains at least one delimiter, check if it contains more and index all tuples between the tuple delimiters
-    size_t startIdxOfCurrentTuple = offsetOfFirstTupleDelimiter + sizeOfTupleDelimiter;
-    size_t endIdxOfCurrentTuple = bufferView.find(this->config.tupleDelimiter, startIdxOfCurrentTuple);
-    while (endIdxOfCurrentTuple != std::string::npos)
+    auto startIdxOfNextTuple = offsetOfFirstTupleDelimiter + sizeOfTupleDelimiter;
+    size_t endIdxOfNextTuple = rawBuffer.getBufferView().find(this->config.tupleDelimiter, startIdxOfNextTuple);
+
+    while (endIdxOfNextTuple != std::string::npos)
     {
-        INVARIANT(startIdxOfCurrentTuple <= endIdxOfCurrentTuple, "The start index of a tuple cannot be larger than the end index.");
-        auto* tupleOffsetPtr = fieldOffsets.writeOffsetsOfNextTuple();
-        const auto sizeOfCurrentTuple = endIdxOfCurrentTuple - startIdxOfCurrentTuple;
-        /// WE ALWAYS skip the first partial tuple and start with the first full tuple delimiter, thus, we can ALWAYS add the size of the tuple delimiter
-        const auto currentTuple = std::string_view(bufferView.begin() + startIdxOfCurrentTuple, sizeOfCurrentTuple);
+        /// Get a string_view for the next tuple, by using the start and the size of the next tuple
+        INVARIANT(startIdxOfNextTuple <= endIdxOfNextTuple, "The start index of a tuple cannot be larger than the end index.");
+        const auto sizeOfNextTuple = endIdxOfNextTuple - startIdxOfNextTuple;
+        const auto nextTuple = rawBuffer.getBufferView().substr(startIdxOfNextTuple, sizeOfNextTuple);
 
-        indexTuple(currentTuple, tupleOffsetPtr, startIdxOfCurrentTuple);
-        /// The last delimiter is the size of the tuple itself, which allows the next phase to determine the last field without any extra calculations
-        *(tupleOffsetPtr + numberOfFieldsInSchema) = endIdxOfCurrentTuple; ///NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+        /// Determine the offsets to the individual fields of the next tuple, including the start of the first and the end of the last field
+        initializeIndexFunctionForTuple(fieldOffsets, nextTuple, startIdxOfNextTuple, this->config, this->numberOfFieldsInSchema);
+        fieldOffsets.writeOffsetsOfNextTuple(); ///NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
 
-        startIdxOfCurrentTuple = endIdxOfCurrentTuple + sizeOfTupleDelimiter;
-        endIdxOfCurrentTuple = bufferView.find(this->config.tupleDelimiter, startIdxOfCurrentTuple);
+        /// Update the start and the end index for the next tuple (if no more tuples in buffer, endIdx is 'std::string::npos')
+        startIdxOfNextTuple = endIdxOfNextTuple + sizeOfTupleDelimiter;
+        endIdxOfNextTuple = rawBuffer.getBufferView().find(this->config.tupleDelimiter, startIdxOfNextTuple);
     }
-    const auto offsetOfLastTupleDelimiter = static_cast<FieldOffsetsType>(startIdxOfCurrentTuple - sizeOfTupleDelimiter);
-    return {.offsetOfFirstTupleDelimiter = offsetOfFirstTupleDelimiter, .offsetOfLastTupleDelimiter = offsetOfLastTupleDelimiter};
+    /// Since 'endIdxOfNextTuple == std::string::npos', we use the startIdx to determine the offset of the last tuple
+    const auto offsetOfLastTupleDelimiter = static_cast<FieldOffsetsType>(startIdxOfNextTuple - sizeOfTupleDelimiter);
+    fieldOffsets.markWithTupleDelimiters(offsetOfFirstTupleDelimiter, offsetOfLastTupleDelimiter);
 }
 
 std::ostream& CSVInputFormatIndexer::toString(std::ostream& os) const
@@ -110,7 +120,9 @@ std::ostream& CSVInputFormatIndexer::toString(std::ostream& os) const
 InputFormatIndexerRegistryReturnType InputFormatIndexerGeneratedRegistrar::RegisterCSVInputFormatIndexer(
     InputFormatIndexerRegistryArguments arguments) ///NOLINT(performance-unnecessary-value-param)
 {
-    return std::make_unique<CSVInputFormatIndexer>(arguments);
+    auto inputFormatter
+        = std::make_unique<CSVInputFormatIndexer>(arguments.inputFormatIndexerConfig, arguments.getNumberOfFieldsInSchema());
+    return arguments.createInputFormatterTaskPipeline<CSVInputFormatIndexer, FieldOffsets, true>(std::move(inputFormatter));
 }
 
 }
