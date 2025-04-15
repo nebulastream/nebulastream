f2276a79cbf86adb1918ce271a8e2692d192f135
lukas schwerdtfeger <lukas.schwerdtfeger@gmail.com>
2025-04-15 16:56:46 +0200

    fix(Sanitizer): Fix ubsan nullptr report

--- nes-input-formatters/src/CSVInputFormatter.cpp
+++ nes-input-formatters/src/CSVInputFormatter.cpp
@@ -571,8 +571,11 @@ CSVInputFormatter::FormattedTupleIs CSVInputFormatter::processPartialTuple(
         "Partial tuple is larger {} than buffer size {}",
         sizeOfLeadingPartialTuple,
         firstBuffer.buffer.getBufferSize());
-    const std::string_view firstBytesOfPartialTuple = std::string_view(
-        firstBuffer.buffer.getBuffer<const char>() + (firstBuffer.offsetOfLastTupleDelimiter + 1), sizeOfLeadingPartialTuple);
+
+    const std::string_view firstBytesOfPartialTuple = firstBuffer.buffer.getBuffer()
+        ? std::string_view(
+              firstBuffer.buffer.getBuffer<const char>() + (firstBuffer.offsetOfLastTupleDelimiter + 1), sizeOfLeadingPartialTuple)
+        : std::string_view();
 
     std::string partialTuple(firstBytesOfPartialTuple);
     /// 2. Process all buffers in-between the first and the last
