cd3b1dd2f5e264aca7e5a5b38c6922162021f435
Adrian Michalke <michalke@tu-berlin.de>
2025-04-17 19:00:37 +0200

    comments

--- nes-memory/include/MemoryLayout/RowLayoutField.hpp
+++ nes-memory/include/MemoryLayout/RowLayoutField.hpp
@@ -36,25 +36,9 @@ template <class T, bool boundaryChecks = true>
 class RowLayoutField
 {
 public:
-    /**
-     *  Factory to create a RowLayoutField for a specific memory layout and a specific tuple buffer.
-     * @param fieldIndex
-     * @param layoutBuffer
-     * @tparam boundaryChecks if true will check if access is allowed
-     * @tparam T type of field
-     * @return field handler via a fieldIndex and a layoutBuffer
-     */
     static inline RowLayoutField<T, boundaryChecks>
     create(uint64_t fieldIndex, std::shared_ptr<RowLayout> layout, Memory::TupleBuffer& buffer);
 
-    /**
-    * Factory to create a RowLayoutField for a specific memory layout and a specific tuple buffer.
-    * @param fieldIndex the field which is accessed.
-    * @param layout the memory layout
-    * @tparam buffer the tuple buffer
-    * @tparam T type of field
-    * @return field handler
-    */
     static inline RowLayoutField<T, boundaryChecks>
     create(const std::string& fieldName, std::shared_ptr<RowLayout> layout, Memory::TupleBuffer& buffer);
 
@@ -66,13 +50,6 @@ public:
     inline T& operator[](size_t recordIndex);
 
 private:
-    /**
-     * @brief Constructor for RowLayoutField
-     * @param dynamicRowLayoutBuffer
-     * @param basePointer
-     * @param fieldIndex
-     * @param recordSize
-     */
     RowLayoutField(std::shared_ptr<RowLayout> layout, uint8_t* basePointer, uint64_t fieldIndex, uint64_t recordSize)
         : fieldIndex(fieldIndex), recordSize(recordSize), basePointer(basePointer), layout(std::move(layout)) {};
 
