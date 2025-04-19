462f225799dc467bf6db9a709ed72014e93ab291
Adrian Michalke <michalke@tu-berlin.de>
2025-04-19 22:02:47 +0200

    refactor(DataTypes): AttributeField.hpp chore

--- nes-data-types/include/API/AttributeField.hpp
+++ nes-data-types/include/API/AttributeField.hpp
@@ -30,23 +30,16 @@ class AttributeField
 {
 public:
     AttributeField() = default;
+    AttributeField(std::string name, std::shared_ptr<DataType> dataType);
 
-    /**
-     * @brief Factory method to create a new field
-     * @param name name of the field
-     * @param dataType data type
-     * @return std::shared_ptr<AttributeField>
-     */
-    static std::shared_ptr<AttributeField> create(const std::string& name, const std::shared_ptr<DataType>& dataType);
-
-    [[nodiscard]] std::shared_ptr<DataType> getDataType() const;
+    [[nodiscard]] DataType& getDataType() const;
 
     [[nodiscard]] const std::string& getName() const;
 
     void setName(std::string newName);
 
     [[nodiscard]] std::string toString() const;
-    [[nodiscard]] bool isEqual(const std::shared_ptr<AttributeField>& attr) const;
+    [[nodiscard]] bool isEqual(const AttributeField& attr) const;
 
     uint64_t hash() const;
     /**
@@ -56,8 +49,6 @@ public:
     [[nodiscard]] std::shared_ptr<AttributeField> deepCopy() const;
 
 private:
-    AttributeField(std::string name, std::shared_ptr<DataType> dataType);
-
     std::string name;
     std::shared_ptr<DataType> dataType;
 };
