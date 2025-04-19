2fe5a5cf75f0515ab2740c0122c9c7053453ecf8
Adrian Michalke <michalke@tu-berlin.de>
2025-04-19 20:44:30 +0200

    refactor(PhysicalOperators): executable function to physical function

--- nes-physical-operators/include/Functions/ExecutableFunctionConcat.hpp
+++ nes-physical-operators/include/Functions/ExecutableFunctionConcat.hpp
@@ -13,23 +13,25 @@
 */
 
 #pragma once
+
 #include <memory>
-#include <Execution/Functions/Function.hpp>
-#include <Execution/Operators/ExecutionContext.hpp>
+#include <Functions/PhysicalFunction.hpp>
+#include <ExecutionContext.hpp>
 #include <Nautilus/DataTypes/VarVal.hpp>
 #include <Nautilus/Interface/Record.hpp>
+
 namespace NES::Functions
 {
 
-class ExecutableFunctionConcat final : public Function
+class ConcatPhysicalFunction final : public PhysicalFunction
 {
 public:
-    ExecutableFunctionConcat(std::unique_ptr<Function> leftExecutableFunction, std::unique_ptr<Function> rightExecutableFunction);
+    ConcatPhysicalFunction(std::unique_ptr<PhysicalFunction> leftExecutableFunction, std::unique_ptr<PhysicalFunction> rightExecutableFunction);
     [[nodiscard]] VarVal execute(const Record& record, ArenaRef& arena) const override;
 
 private:
-    const std::unique_ptr<Function> leftExecutableFunction;
-    const std::unique_ptr<Function> rightExecutableFunction;
+    const std::unique_ptr<PhysicalFunction> leftPhysicalFunction;
+    const std::unique_ptr<PhysicalFunction> rightPhysicalFunction;
 };
 
 }
