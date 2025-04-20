9f103a6c48402a6ffbc9ac3d7772ab2c375f4262
Adrian Michalke <michalke@tu-berlin.de>
2025-04-21 00:31:40 +0200

    refactor(QueryOptimizer):

--- nes-optimizer/src/RewriteRules/LowerToPhysical/LowerToPhysicalNLJoin.cpp
+++ nes-optimizer/src/RewriteRules/LowerToPhysical/LowerToPhysicalNLJoin.cpp
@@ -16,30 +16,23 @@
 #include <RewriteRules/AbstractRewriteRule.hpp>
 #include <RewriteRules/LowerToPhysical/LowerToPhysicalSelection.hpp>
 #include <RewriteRuleRegistry.hpp>
-#include <memory>
-#include <utility>
-#include <Traits/QueryForSubtree.hpp>
-#include <Traits/TraitSet.hpp>
-#include <RewriteRules/AbstractRewriteRule.hpp>
 #include <Functions/FunctionProvider.hpp>
 #include <Operators/SelectionLogicalOperator.hpp>
-#include <Nautilus/Interface/MemoryProvider/RowTupleBufferMemoryProvider.hpp>
-#include <Plans/Operator.hpp>
 #include <SelectionPhysicalOperator.hpp>
+#include <Abstract/PhysicalOperator.hpp>
 
 namespace NES::Optimizer
 {
 
-std::vector<std::shared_ptr<PhysicalOperator>> LowerToPhysicalSelection::applyToPhysical(DynamicTraitSet<QueryForSubtree, Operator>* traitSet)
+RewriteRuleResult LowerToPhysicalSelection::apply(LogicalOperator logicalOperator)
 {
-    auto op = traitSet->get<Operator>();
-    const auto ops = dynamic_cast<SelectionLogicalOperator*>(op);
-    auto function = ops->getPredicate();
+    PRECONDITION(logicalOperator.tryGet<SelectionLogicalOperator>(), "Expected a SelectionLogicalOperator");
+    auto selection = *logicalOperator.get<SelectionLogicalOperator>();
+    auto function = selection.getPredicate();
     auto func = QueryCompilation::FunctionProvider::lowerFunction(function);
-    auto layout = std::make_shared<Memory::MemoryLayouts::RowLayout>(ops->getInputSchema(), conf.bufferSize.getValue());
-    auto memoryProvider = std::make_unique<RowTupleBufferMemoryProvider>(layout);
-    auto phyOp = std::make_shared<SelectionPhysicalOperator>(std::vector<std::shared_ptr<TupleBufferMemoryProvider>>{std::move(memoryProvider)}, std::move(func));
-    return {phyOp};
+    auto physicalOperator = SelectionPhysicalOperator(func);
+    auto wrapper = std::make_shared<PhysicalOperatorWrapper>(physicalOperator, logicalOperator.getInputSchemas()[0], logicalOperator.getOutputSchema());
+    return {wrapper, {wrapper}};
 };
 
 std::unique_ptr<AbstractRewriteRule> RewriteRuleGeneratedRegistrar::RegisterSelectionRewriteRule(RewriteRuleRegistryArguments argument)
--- nes-query-optimizer/private/RewriteRules/AbstractRewriteRule.hpp
+++ nes-query-optimizer/private/RewriteRules/AbstractRewriteRule.hpp
@@ -15,15 +15,22 @@
 #pragma once
 
 #include <Plans/QueryPlan.hpp>
-#include <Traits/Trait.hpp>
 #include <Abstract/PhysicalOperator.hpp>
 
 namespace NES::Optimizer
 {
 
+struct RewriteRuleResult
+{
+    /// Top-level physical operator
+    std::shared_ptr<PhysicalOperatorWrapper> root;
+    /// Points where the following operators can attach
+    std::vector<std::shared_ptr<PhysicalOperatorWrapper>> leafOperators;
+};
+
 struct AbstractRewriteRule
 {
-    virtual void apply(QueryPlan& queryPlan) = 0;
+    virtual RewriteRuleResult apply(LogicalOperator logicalOperator) = 0;
     virtual ~AbstractRewriteRule() = default;
 };
 
