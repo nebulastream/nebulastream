a6ac9efa651cd8d63c07576b5ac438dc41370dd8
Adrian Michalke <michalke@tu-berlin.de>
2025-04-20 23:25:03 +0200

    refactor(LogicalOperators): removes obsolete `RenameSourceLogicalOperator`

--- nes-operators/tests/UnitTests/Plans/Query/QueryPlanBuilderTest.cpp
+++ nes-operators/tests/UnitTests/Plans/Query/QueryPlanBuilderTest.cpp
@@ -20,7 +20,6 @@
 #include <Functions/LogicalFunctions/EqualsBinaryLogicalFunction.hpp>
 #include <Operators/LogicalOperators/MapLogicalOperator.hpp>
 #include <Operators/LogicalOperators/ProjectionLogicalOperator.hpp>
-#include <Operators/LogicalOperators/RenameSourceLogicalOperator.hpp>
 #include <Operators/LogicalOperators/SelectionLogicalOperator.hpp>
 #include <Operators/LogicalOperators/Sinks/SinkLogicalOperator.hpp>
 #include <Operators/LogicalOperators/UnionLogicalOperator.hpp>
@@ -52,10 +51,6 @@ TEST_F(QueryPlanBuilderTest, testHasOperator)
     ///test createQueryPlan
     auto queryPlan = QueryPlanBuilder::createQueryPlan("test_stream");
     EXPECT_EQ(queryPlan->getSourceConsumed(), "test_stream");
-    ///test addRename
-    queryPlan = QueryPlanBuilder::addRename("testStream", queryPlan);
-    EXPECT_TRUE(queryPlan->getOperatorByType<RenameSourceLogicalOperator>().size() == 1);
-    EXPECT_EQ(queryPlan->getOperatorByType<RenameSourceLogicalOperator>()[0]->getNewSourceName(), "testStream");
     ///test addSelection
     auto filterFunction = std::shared_ptr<LogicalFunction>(
         EqualsBinaryLogicalFunction::create(NES::Attribute("a").getLogicalFunction(), NES::Attribute("b").getLogicalFunction()));
