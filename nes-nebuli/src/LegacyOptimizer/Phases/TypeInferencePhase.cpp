3e45404493c5181d3c2aa1cb33ac59b40d639a10
Adrian Michalke <michalke@tu-berlin.de>
2025-04-21 00:22:31 +0200

    refactor(LegacyOptimizer): TypeInferencePhase

--- nes-nebuli/src/LegacyOptimizer/Phases/TypeInferencePhase.cpp
+++ nes-nebuli/src/LegacyOptimizer/Phases/TypeInferencePhase.cpp
@@ -24,26 +24,28 @@
 namespace NES::LegacyOptimizer
 {
 
-void apply(std::vector<SourceNameLogicalOperator>& sourceOperators,  Catalogs::Source::SourceCatalog& sourceCatalog)
+void TypeInferencePhase::apply(LogicalPlan& queryPlan,  Catalogs::Source::SourceCatalog& sourceCatalog)
 {
+    auto sourceOperators = queryPlan.getOperatorByType<SourceNameLogicalOperator>();
+
     PRECONDITION(not sourceOperators.empty(), "Query plan did not contain sources during type inference.");
 
+    std::vector<LogicalOperator> newSources;
     /// first we have to check if all source operators have a correct source descriptors
     for (auto& source : sourceOperators)
     {
         /// if the source descriptor has no schema set and is only a logical source we replace it with the correct
         /// source descriptor form the catalog.
-        auto logicalSourceName = source.getName();
-        Schema schema = Schema();
-        if (!sourceCatalog.containsLogicalSource(std::string(logicalSourceName)))
+        auto schema = Schema();
+        if (!sourceCatalog.containsLogicalSource(source.getLogicalSourceName()))
         {
-            NES_ERROR("Source name: {} not registered.", logicalSourceName);
-            throw LogicalSourceNotFoundInQueryDescription(fmt::format("Logical source not registered. Source Name: {}", logicalSourceName));
+            NES_ERROR("Source name: {} not registered.", source.getLogicalSourceName());
+            throw LogicalSourceNotFoundInQueryDescription("Logical source not registered. Source Name: {}", source.getLogicalSourceName());
         }
-        auto originalSchema = sourceCatalog.getSchemaForLogicalSource(std::string(logicalSourceName));
+        auto originalSchema = sourceCatalog.getSchemaForLogicalSource(source.getLogicalSourceName());
         schema = schema.copyFields(originalSchema);
         schema.setLayoutType(originalSchema.getLayoutType());
-        auto qualifierName = std::string(logicalSourceName) + Schema::ATTRIBUTE_NAME_SEPARATOR;
+        auto qualifierName = source.getLogicalSourceName() + Schema::ATTRIBUTE_NAME_SEPARATOR;
         /// perform attribute name resolution
         for (auto& field : schema)
         {
@@ -53,8 +55,9 @@ void apply(std::vector<SourceNameLogicalOperator>& sourceOperators,  Catalogs::S
             }
         }
         source.setSchema(schema);
-        NES_DEBUG("TypeInferencePhase: update source descriptor for source {} with schema: {}", logicalSourceName, schema.toString());
+        newSources.push_back(source);
     }
+    queryPlan.rootOperators = newSources;
 }
 
 void TypeInferencePhase::apply(QueryPlan& queryPlan)
@@ -64,13 +67,12 @@ void TypeInferencePhase::apply(QueryPlan& queryPlan)
 
     /// now we have to infer the input and output schemas for the whole query.
     /// to this end we call at each sink the infer method to propagate the schemata across the whole query.
+    std::vector<LogicalOperator> newSources;
     for (auto& source : sourceOperators)
     {
-        if (!source.inferSchema())
-        {
-            throw TypeInferenceException("TypeInferencePhase failed for query with id: {}", queryPlan.getQueryId());
-        }
+        newSources.push_back(source.withInferredSchema(Schema()));
     }
+    queryPlan.rootOperators = newSources;
 }
 
 
