d77501776bb6f57e7334c4de708a7c47c56b9e3c
Adrian Michalke <michalke@tu-berlin.de>
2025-04-21 23:00:47 +0200

    chore(LogicalOperators)

--- nes-logical-operators/src/Operators/LogicalOperator.cpp
+++ nes-logical-operators/src/Operators/LogicalOperator.cpp
@@ -11,13 +11,159 @@
     See the License for the specific language governing permissions and
     limitations under the License.
 */
-#pragma once
 
+#include <algorithm>
 #include <memory>
-#include <QueryCompiler/Operators/PipelineQueryPlan.hpp>
-#include <CompiledQueryPlan.hpp>
+#include <string>
+#include <Configurations/Descriptor.hpp>
+#include <Operators/LogicalOperator.hpp>
+#include <Serialization/SchemaSerializationUtil.hpp>
+#include <SerializableOperator.pb.h>
+#include <ErrorHandling.hpp>
+#include <Util/Logger/Logger.hpp>
 
-namespace NES::QueryCompilation::LowerToExecutableQueryPlanPhase
+namespace NES
 {
-std::unique_ptr<CompiledQueryPlan> apply(const std::shared_ptr<PipelineQueryPlan>& pipelineQueryPlan);
+
+LogicalOperatorConcept::LogicalOperatorConcept() : id(getNextLogicalOperatorId()) {
+}
+LogicalOperatorConcept::LogicalOperatorConcept(OperatorId existingId) : id(existingId) {}
+
+std::string NullLogicalOperator::toString() const  {
+    PRECONDITION(false, "Calls in NullLogicalOperator are undefined");
+}
+
+std::vector<NES::LogicalOperator> NullLogicalOperator::getChildren() const  {
+    PRECONDITION(false, "Calls in NullLogicalOperator are undefined");
+}
+
+LogicalOperator NullLogicalOperator::withChildren(std::vector<NES::LogicalOperator>) const {
+    PRECONDITION(false, "Calls in NullLogicalOperator are undefined");
+}
+
+bool NullLogicalOperator::operator==(NES::LogicalOperatorConcept const&) const  {
+    return false;
+}
+
+std::string_view NullLogicalOperator::getName() const noexcept  {
+    PRECONDITION(false, "Calls in NullLogicalOperator are undefined");
+}
+NES::SerializableOperator NullLogicalOperator::serialize() const  {
+    PRECONDITION(false, "Calls in NullLogicalOperator are undefined");
+}
+Optimizer::TraitSet NullLogicalOperator::getTraitSet() const  {
+    PRECONDITION(false, "Calls in NullLogicalOperator are undefined");
+}
+std::vector<Schema> NullLogicalOperator::getInputSchemas() const  {
+    PRECONDITION(false, "Calls in NullLogicalOperator are undefined");
+}
+Schema NullLogicalOperator::getOutputSchema() const  {
+    PRECONDITION(false, "Calls in NullLogicalOperator are undefined");
+}
+std::vector<std::vector<OriginId>> NullLogicalOperator::getInputOriginIds() const  {
+    PRECONDITION(false, "Calls in NullLogicalOperator are undefined");
+}
+std::vector<OriginId> NullLogicalOperator::getOutputOriginIds() const  {
+    PRECONDITION(false, "Calls in NullLogicalOperator are undefined");
+}
+
+LogicalOperator NullLogicalOperator::withInputOriginIds(std::vector<std::vector<OriginId>>) const
+{
+    PRECONDITION(false, "Calls in NullLogicalOperator are undefined");
+}
+
+LogicalOperator NullLogicalOperator::withOutputOriginIds(std::vector<OriginId>) const
+{
+    PRECONDITION(false, "Calls in NullLogicalOperator are undefined");
+}
+
+LogicalOperator NullLogicalOperator::withInferredSchema(std::vector<Schema>) const
+{
+    PRECONDITION(false, "Calls in NullLogicalOperator are undefined");
+}
+
+LogicalOperator::LogicalOperator() : self(std::make_unique<Model<NullLogicalOperator>>(NullLogicalOperator{})) {}
+
+LogicalOperator::LogicalOperator(const LogicalOperator& other) : self(other.self->clone()) {}
+
+LogicalOperator::LogicalOperator(LogicalOperator&&) noexcept = default;
+
+LogicalOperator& LogicalOperator::operator=(const LogicalOperator& other) {
+    if (this != &other)
+    {
+        self = other.self->clone();
+    }
+    return *this;
+}
+
+std::string LogicalOperator::toString() const
+{
+    return self->toString();
+}
+
+std::vector<LogicalOperator> LogicalOperator::getChildren() const {
+    return self->getChildren();
+}
+
+LogicalOperator LogicalOperator::withChildren(std::vector<LogicalOperator> children) const {
+    return self->withChildren(children);
+}
+
+OperatorId LogicalOperator::getId() const {
+    return self->id;
+}
+
+bool LogicalOperator::operator==(const LogicalOperator &other) const {
+    return self->equals(*other.self);
+}
+
+std::string_view LogicalOperator::getName() const noexcept
+{
+    return self->getName();
+}
+
+SerializableOperator LogicalOperator::serialize() const
+{
+    return self->serialize();
+}
+
+Optimizer::TraitSet LogicalOperator::getTraitSet() const
+{
+    return self->getTraitSet();
+}
+
+std::vector<Schema> LogicalOperator::getInputSchemas() const
+{
+    return self->getInputSchemas();
+}
+
+Schema LogicalOperator::getOutputSchema() const
+{
+    return self->getOutputSchema();
+}
+
+std::vector<std::vector<OriginId>> LogicalOperator::getInputOriginIds() const
+{
+        return self->getInputOriginIds();
+}
+
+std::vector<OriginId> LogicalOperator::getOutputOriginIds() const
+{
+    return self->getOutputOriginIds();
+}
+
+LogicalOperator LogicalOperator::withInputOriginIds(std::vector<std::vector<OriginId>> ids) const
+{
+    return self->withInputOriginIds(ids);
+}
+
+LogicalOperator LogicalOperator::withOutputOriginIds(std::vector<OriginId> ids) const
+{
+    return self->withOutputOriginIds(ids);
+}
+
+LogicalOperator LogicalOperator::withInferredSchema(std::vector<Schema> inputSchemas) const
+{
+    return self->withInferredSchema(std::move(inputSchemas));
+}
 }
