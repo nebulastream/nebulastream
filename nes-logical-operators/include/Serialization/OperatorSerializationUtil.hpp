1cf84d245c600ffe5eaf52236a37df9d10676558
Adrian Michalke <michalke@tu-berlin.de>
2025-04-21 16:21:30 +0200

    refactor(LogicalOperators): remove obsolete WindowOperator class

--- nes-logical-operators/include/Serialization/OperatorSerializationUtil.hpp
+++ nes-logical-operators/include/Serialization/OperatorSerializationUtil.hpp
@@ -25,7 +25,6 @@
 #include <Operators/Sources/SourceDescriptorLogicalOperator.hpp>
 #include <Operators/Windows/Aggregations/WindowAggregationLogicalFunction.hpp>
 #include <Operators/Windows/JoinLogicalOperator.hpp>
-#include <Operators/Windows/WindowOperator.hpp>
 #include <SerializableOperator.pb.h>
 
 namespace NES
