0590601621ab19f87d1f272036625901c34d8a1b
Adrian Michalke <michalke@tu-berlin.de>
2025-04-22 20:40:58 +0200

    chore(LogicalOperators): minor changes

--- nes-nebuli/src/Optimizer/QueryRewrite/BinaryOperatorSortRule.cpp
+++ nes-nebuli/src/Optimizer/QueryRewrite/BinaryOperatorSortRule.cpp
@@ -13,8 +13,8 @@
 */
 #include <memory>
 #include <API/Schema.hpp>
-#include <Operators/LogicalOperators/LogicalUnionOperator.hpp>
-#include <Operators/LogicalOperators/Windows/Joins/LogicalJoinOperator.hpp>
+#include <Operators/LogicalOperators/UnionLogicalOperator.hpp>
+#include <Operators/LogicalOperators/Windows/Joins/JoinLogicalOperator.hpp>
 #include <Optimizer/QueryRewrite/BinaryOperatorSortRule.hpp>
 #include <Plans/Query/QueryPlan.hpp>
 #include <Util/Logger/Logger.hpp>
@@ -36,13 +36,13 @@ std::shared_ptr<QueryPlan> BinaryOperatorSortRule::apply(std::shared_ptr<QueryPl
 {
     NES_INFO("Apply BinaryOperatorSortRule ");
     /// Find all join operators in the query plan and sort children individually.
-    auto joinOperators = queryPlan->getOperatorByType<LogicalJoinOperator>();
+    auto joinOperators = queryPlan->getOperatorByType<JoinLogicalOperator>();
     for (const auto& joinOperator : joinOperators)
     {
         sortChildren(joinOperator);
     }
     /// Find all Union operators in the query plan and sort children individually.
-    auto unionOperators = queryPlan->getOperatorByType<LogicalUnionOperator>();
+    auto unionOperators = queryPlan->getOperatorByType<UnionLogicalOperator>();
     for (const auto& unionOperator : unionOperators)
     {
         sortChildren(unionOperator);
@@ -51,7 +51,7 @@ std::shared_ptr<QueryPlan> BinaryOperatorSortRule::apply(std::shared_ptr<QueryPl
     return queryPlan;
 }
 
-void BinaryOperatorSortRule::sortChildren(const std::shared_ptr<BinaryOperator>& binaryOperator)
+void BinaryOperatorSortRule::sortChildren(const std::shared_ptr<BinaryLogicalOperator>& binaryOperator)
 {
     /// Extract the children operators
     auto children = binaryOperator->getChildren();
