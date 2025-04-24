17aec0fc790781996141deec1cc935272e19ee5e
Klaas tom Dieck <klaas.tomdieck@gmail.com>
2025-04-24 14:39:04 +0200

    feat(Plugin): Adds GeneratorSource plugin
    The generator source generates synthetic tuples based on
    per field parameters.
    
    This commit adds the basic functionality of creating data for
    Sequences: SEQUENCE UINT64 0 100000 1
    Normal Distribution: NORMAL_DISTRIBUTION FLOAT64 0 15
    
    Currently the generator only supports (U)INT64 and FLOAT(32|64)

---
+++ nes-plugins/Sources/GeneratorSource/GeneratorFields.hpp
@@ -0,0 +1,97 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#pragma once
+#include <array>
+#include <cstdint>
+#include <functional>
+#include <ostream>
+#include <random>
+#include <string_view>
+#include <variant>
+
+namespace NES::Sources::GeneratorFields
+{
+
+static constexpr std::string_view SEQUENCE_IDENTIFIER = "SEQUENCE";
+static constexpr std::string_view NORMAL_DISTRIBUTION_IDENTIFIER = "NORMAL_DISTRIBUTION";
+
+/// @brief Variant containing the types that a field can generate
+using FieldType = std::variant<uint64_t, int64_t, float, double>;
+
+/// @brief Base class for all types of fields for the generator
+class BaseGeneratorField
+{
+public:
+    virtual ~BaseGeneratorField() = default;
+    virtual std::ostream& generate(std::ostream& os, std::default_random_engine& /*randEng*/) = 0;
+};
+
+class BaseStoppableGeneratorField : public BaseGeneratorField
+{
+public:
+    bool stop{false};
+};
+
+constexpr auto NUM_PARAMETERS_SEQUENCE_FIELD = 5;
+/// @brief generates sequential records based on the sequence start, end and step size
+class SequenceField : public BaseStoppableGeneratorField
+{
+public:
+    SequenceField(FieldType start, FieldType end, FieldType step);
+    explicit SequenceField(std::string_view rawSchemaLine);
+
+    std::ostream& generate(std::ostream& os, std::default_random_engine& randEng) override;
+
+    static void validate(std::string_view rawSchemaLine);
+
+    FieldType sequencePosition;
+    FieldType sequenceStart;
+    FieldType sequenceEnd;
+    FieldType sequenceStepSize;
+
+private:
+    template <class T>
+    void parse(std::string_view start, std::string_view end, std::string_view step);
+};
+
+constexpr auto NUM_PARAMETERS_NORMAL_DISTRIBUTION_FIELD = 4;
+/// @brief generates normally distríbuted floating point records
+class NormalDistributionField final : public BaseGeneratorField
+{
+public:
+    NormalDistributionField(double mean, double stddev);
+    explicit NormalDistributionField(std::string_view rawSchemaLine);
+    std::ostream& generate(std::ostream& os, std::default_random_engine& randEng) override;
+    static void validate(std::string_view rawSchemaLine);
+
+private:
+    std::normal_distribution<double> distribution;
+};
+
+/// @brief Variant containing the types of base generator fields
+using GeneratorFieldType = std::variant<SequenceField, NormalDistributionField>;
+
+struct FieldValidator
+{
+    std::string_view identifier;
+    std::function<void(std::string_view)> validator; /// Validator function throws an Exception if field is invalid
+};
+/// @brief Array containing functions paired with the fields identifier used to validate the fields syntax
+static const std::array<FieldValidator, 2> Validators
+    = {{{.identifier = SEQUENCE_IDENTIFIER, .validator = SequenceField::validate},
+        {.identifier = NORMAL_DISTRIBUTION_IDENTIFIER, .validator = NormalDistributionField::validate}}};
+
+
+}
---
+++ nes-plugins/Sources/GeneratorSource/GeneratorSource.cpp
@@ -0,0 +1,137 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include <GeneratorSource.hpp>
+
+#include <chrono>
+#include <cstddef>
+#include <exception>
+#include <iostream>
+#include <memory>
+#include <stop_token>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+#include <utility>
+#include <Configurations/Descriptor.hpp>
+#include <Runtime/TupleBuffer.hpp>
+#include <Sources/SourceDescriptor.hpp>
+#include <Util/Logger/Logger.hpp>
+#include <Generator.hpp>
+#include <SourceRegistry.hpp>
+#include <SourceValidationRegistry.hpp>
+
+
+namespace NES::Sources
+{
+
+GeneratorSource::GeneratorSource(const SourceDescriptor& sourceDescriptor)
+    : seed(sourceDescriptor.getFromConfig(ConfigParametersGenerator::SEED))
+    , maxRuntime(sourceDescriptor.getFromConfig(ConfigParametersGenerator::MAX_RUNTIME_MS))
+    , generatorSchemaRaw(sourceDescriptor.getFromConfig(ConfigParametersGenerator::GENERATOR_SCHEMA))
+    , generatorStartTime(std::chrono::system_clock::now())
+    , generator(
+          seed,
+          sourceDescriptor.getFromConfig(ConfigParametersGenerator::SEQUENCE_STOPS_GENERATOR),
+          sourceDescriptor.getFromConfig(ConfigParametersGenerator::GENERATOR_SCHEMA))
+
+{
+    NES_TRACE("Init GeneratorSource.")
+}
+
+void GeneratorSource::open()
+{
+    NES_TRACE("Opening GeneratorSource.");
+}
+
+void GeneratorSource::close()
+{
+    NES_TRACE("Closing GeneratorSource.");
+}
+
+size_t GeneratorSource::fillTupleBuffer(NES::Memory::TupleBuffer& tupleBuffer, const std::stop_token& stopToken)
+{
+    NES_INFO("Filling buffer in GeneratorSource.");
+    try
+    {
+        size_t writtenBytes = 0;
+        const auto elapsedTime
+            = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - generatorStartTime).count();
+        if (maxRuntime >= 0 && elapsedTime >= maxRuntime)
+        {
+            NES_INFO("Reached max runtime! Stopping Source");
+            return 0;
+        }
+        const size_t rawTBSize = tupleBuffer.getBufferSize();
+        while (writtenBytes < rawTBSize and not this->generator.shouldStop() and not stopToken.stop_requested())
+        {
+            auto insertedBytes = tuplesStream.tellp();
+            if (!orphanTuples.empty())
+            {
+                tuplesStream << orphanTuples;
+                orphanTuples.clear();
+            }
+            this->generator.generateTuple(tuplesStream);
+            insertedBytes = tuplesStream.tellp() - insertedBytes;
+            if (writtenBytes + insertedBytes > rawTBSize)
+            {
+                tuplesStream.read(tupleBuffer.getBuffer<char>(), writtenBytes);
+                generatedBuffers++;
+                this->orphanTuples = tuplesStream.str().substr(writtenBytes, tuplesStream.str().length() - writtenBytes);
+                tuplesStream.str("");
+                return writtenBytes;
+            }
+            writtenBytes += insertedBytes;
+        }
+        tuplesStream.read(tupleBuffer.getBuffer<char>(), writtenBytes);
+        ++generatedBuffers;
+        tuplesStream.str("");
+        NES_INFO("Wrote {} bytes", writtenBytes);
+        return writtenBytes;
+    }
+    catch (const std::exception& e)
+    {
+        NES_ERROR("Failed to fill the TupleBuffer. Error: {}", e.what());
+        throw e;
+    }
+}
+
+std::ostream& GeneratorSource::toString(std::ostream& str) const
+{
+    str << "\nGeneratorSource(";
+    str << "\n\tgenerated buffers: " << this->generatedBuffers;
+    str << "\n\tschema: " << this->generatorSchemaRaw;
+    str << "\n\tseed: " << this->seed;
+    str << ")\n";
+    return str;
+}
+
+NES::Configurations::DescriptorConfig::Config GeneratorSource::validateAndFormat(std::unordered_map<std::string, std::string> config)
+{
+    return Configurations::DescriptorConfig::validateAndFormat<ConfigParametersGenerator>(std::move(config), NAME);
+}
+
+SourceValidationRegistryReturnType
+///NOLINTNEXTLINE (performance-unnecessary-value-param)
+SourceValidationGeneratedRegistrar::RegisterGeneratorSourceValidation(SourceValidationRegistryArguments sourceConfig)
+{
+    return GeneratorSource::validateAndFormat(sourceConfig.config);
+}
+
+///NOLINTNEXTLINE (performance-unnecessary-value-param)
+SourceRegistryReturnType SourceGeneratedRegistrar::RegisterGeneratorSource(SourceRegistryArguments sourceRegistryArguments)
+{
+    return std::make_unique<GeneratorSource>(sourceRegistryArguments.sourceDescriptor);
+}
+}
