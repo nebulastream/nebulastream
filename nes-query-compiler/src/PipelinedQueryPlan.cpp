84ca3df25d3ad7435fdcb0547fc01c325de8eb40
Adrian Michalke <michalke@tu-berlin.de>
2025-04-28 14:32:58 +0200

    chore:

---
+++ nes-query-compiler/src/PipelinedQueryPlan.cpp
@@ -0,0 +1,70 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include <Phases/LowerToCompiledQueryPlanPhase.hpp>
+#include <PipelinedQueryPlan.hpp>
+#include <Runtime/NodeEngine.hpp>
+#include <ErrorHandling.hpp>
+#include <QueryCompiler.hpp>
+
+namespace NES
+{
+
+PipelinedQueryPlan::PipelinedQueryPlan(QueryId id) : queryId(id) {};
+
+void printPipelineRecursive(const NES::Pipeline* pipe, std::stringstream& ss, int indentLevel)
+{
+    std::string indent(indentLevel * 2, ' ');
+    ss << indent << pipe->toString() << "\n";
+    for (const auto& succ : pipe->getSuccessors())
+    {
+        ss << indent << "Successor Pipeline:\n";
+        printPipelineRecursive(succ.get(), ss, indentLevel + 1);
+    }
+}
+
+std::string PipelinedQueryPlan::toString() const
+{
+    std::stringstream ss;
+    ss << "PipelinedQueryPlan for Query: " << queryId << "\n";
+    ss << "Number of root pipelines: " << pipelines.size() << "\n";
+    for (size_t i = 0; i < pipelines.size(); ++i)
+    {
+        ss << "------------------\n";
+        ss << "Root Pipeline " << i << ":\n";
+        printPipelineRecursive(pipelines[i].get(), ss, 1);
+    }
+    return ss.str();
+}
+
+std::vector<std::shared_ptr<Pipeline>> PipelinedQueryPlan::getSourcePipelines()
+{
+    std::vector<std::shared_ptr<Pipeline>> sourcePipelines;
+    auto it = pipelines.begin();
+    while (it != pipelines.end())
+    {
+        if (it->get()->isSourcePipeline())
+        {
+            sourcePipelines.push_back(*it);
+            it = pipelines.erase(it);
+        }
+        else
+        {
+            ++it;
+        }
+    }
+    return sourcePipelines;
+}
+
+}
