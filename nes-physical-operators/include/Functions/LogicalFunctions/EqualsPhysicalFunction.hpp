c43d06ef679b9a22841b2ef6c374a7e5f73a5592
Adrian Michalke <michalke@tu-berlin.de>
2025-04-29 23:59:39 +0200

    feat(PhysicalOperators)

---
+++ nes-physical-operators/include/Functions/LogicalFunctions/EqualsPhysicalFunction.hpp
@@ -0,0 +1,34 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#pragma once
+
+#include <memory>
+#include <Functions/PhysicalFunction.hpp>
+
+namespace NES::Functions
+{
+
+class EqualsPhysicalFunction final : public PhysicalFunction
+{
+public:
+    EqualsPhysicalFunction(
+        std::unique_ptr<PhysicalFunction> leftExecutableFunction, std::unique_ptr<PhysicalFunction> rightExecutableFunction);
+    [[nodiscard]] VarVal execute(const Record& record, ArenaRef& arena) const override;
+
+private:
+    const std::unique_ptr<PhysicalFunction> leftExecutableFunction;
+    const std::unique_ptr<PhysicalFunction> rightExecutableFunction;
+};
+}
---
+++ nes-physical-operators/src/Functions/Comparison/LessPhysicalFunction.cpp
@@ -0,0 +1,49 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include <memory>
+#include <utility>
+#include <vector>
+#include <Functions/Comparison/LessPhysicalFunction.hpp>
+#include <Functions/PhysicalFunction.hpp>
+#include <Nautilus/DataTypes/VarVal.hpp>
+#include <Nautilus/Interface/Record.hpp>
+#include <ErrorHandling.hpp>
+#include <ExecutionContext.hpp>
+#include <PhysicalFunctionRegistry.hpp>
+
+
+namespace NES::Functions
+{
+
+VarVal LessPhysicalFunction::execute(const Record& record, ArenaRef& arena) const
+{
+    const auto leftValue = leftPhysicalFunction.execute(record, arena);
+    const auto rightValue = rightPhysicalFunction.execute(record, arena);
+    return leftValue < rightValue;
+}
+
+LessPhysicalFunction::LessPhysicalFunction(PhysicalFunction leftPhysicalFunction, PhysicalFunction rightPhysicalFunction)
+    : leftPhysicalFunction(leftPhysicalFunction), rightPhysicalFunction(rightPhysicalFunction)
+{
+}
+
+PhysicalFunctionRegistryReturnType
+PhysicalFunctionGeneratedRegistrar::RegisterLessPhysicalFunction(PhysicalFunctionRegistryArguments PhysicalFunctionRegistryArguments)
+{
+    PRECONDITION(PhysicalFunctionRegistryArguments.childFunctions.size() == 2, "Less function must have exactly two sub-functions");
+    return LessPhysicalFunction(PhysicalFunctionRegistryArguments.childFunctions[0], PhysicalFunctionRegistryArguments.childFunctions[1]);
+}
+
+}
