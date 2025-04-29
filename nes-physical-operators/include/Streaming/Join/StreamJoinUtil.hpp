b389c2cb662e03d905499c67c844ad94c43d4dab
Adrian Michalke <michalke@tu-berlin.de>
2025-04-30 00:02:11 +0200

    feat(PhysicalOperators)

---
+++ nes-physical-operators/include/Streaming/Join/StreamJoinUtil.hpp
@@ -0,0 +1,42 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#pragma once
+
+#include <memory>
+#include <API/Schema.hpp>
+
+namespace NES
+{
+
+
+/// This stores the left, right and output schema for a binary join
+struct JoinSchema
+{
+    JoinSchema(const Schema& leftSchema, const Schema& rightSchema, const Schema& joinSchema)
+        : leftSchema(leftSchema), rightSchema(rightSchema), joinSchema(joinSchema)
+    {
+    }
+
+    Schema leftSchema;
+    Schema rightSchema;
+    Schema joinSchema;
+};
+
+namespace Util
+{
+/// Creates the join schema from the left and right schema
+Schema createJoinSchema(const Schema& leftSchema, const Schema& rightSchema);
+}
+}
