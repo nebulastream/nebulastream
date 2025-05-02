fb270e7305f817ae1b4f7bcedb24491d543f234b
fkuzun <felix.s.lang@campus.tu-berlin.de>
2025-05-02 13:48:57 +0200

    more logging

--- nes-runtime/src/Sinks/Mediums/FileSink.cpp
+++ nes-runtime/src/Sinks/Mediums/FileSink.cpp
@@ -235,7 +235,8 @@ bool FileSink::writeData(Runtime::TupleBuffer& inputBuffer, Runtime::WorkerConte
         const auto bufferOriginId = inputBuffer.getOriginId();
 
         auto record = inputBuffer.getBuffer<Record>();
-        std::tuple<uint64_t, uint64_t> key = {record->value_1, record->value_2};//FIXME
+        std::tuple key = {record->value_1, record->value_2};
+        NES_ERROR("key ({}, {})", std::get<0>(key), std::get<1>(key))
 
         buffersStorageMap[key].push_back(inputBuffer);
 
@@ -284,6 +285,7 @@ bool FileSink::writeData(Runtime::TupleBuffer& inputBuffer, Runtime::WorkerConte
         std::vector<Runtime::TupleBuffer> vec;
         NES_ERROR("writing and erasing elements on write data");
         for (auto& [id, bufferVec] : buffersStorageMap) {
+            NES_ERROR("there are {} saved buffers for key ({}, {})", bufferVec.size(), std::get<0>(key), std::get<1>(key))
             auto it = std::remove_if(bufferVec.begin(), bufferVec.end(), [&](const Runtime::TupleBuffer& buf) {
                 if (buf.getWatermark() < currentWatermarkAfterAdding) {
                     vec.push_back(buf);
