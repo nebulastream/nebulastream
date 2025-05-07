a8a542454a2bacd99ca003480339cd967e2d8d4a
lukas schwerdtfeger <lukas.schwerdtfeger@gmail.com>
2025-05-07 13:30:34 +0200

    feat(Functions): Adds logical function registry

---
+++ nes-functions/registry/include/LogicalFunctionRegistry.hpp
@@ -0,0 +1,40 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#pragma once
+#include <memory>
+#include <string>
+#include <vector>
+#include <Functions/NodeFunction.hpp>
+#include <Util/Registry.hpp>
+
+namespace NES
+{
+
+using LogicalFunctionRegistryReturnType = std::shared_ptr<NodeFunction>;
+struct LogicalFunctionRegistryArguments
+{
+    std::vector<LogicalFunctionRegistryReturnType> childFunctions;
+};
+
+class LogicalFunctionRegistry
+    : public BaseRegistry<LogicalFunctionRegistry, std::string, LogicalFunctionRegistryReturnType, LogicalFunctionRegistryArguments>
+{
+};
+}
+
+
+#define INCLUDED_FROM_REGISTRY_FUNCTION_LOGICAL
+#include <LogicalFunctionGeneratedRegistrar.inc>
+#undef INCLUDED_FROM_REGISTRY_FUNCTION_LOGICAL
