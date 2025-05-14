bd4e8a65a612f2907944eab9f8b069f962124f50
Eric Benschneider <e.benschneider@campus.tu-berlin.de>
2025-05-14 13:43:54 +0200

    reverted hardcoding

--- nes-execution/src/QueryCompiler/Phases/Translations/LowerPhysicalToNautilusOperators.cpp
+++ nes-execution/src/QueryCompiler/Phases/Translations/LowerPhysicalToNautilusOperators.cpp
@@ -348,25 +348,19 @@ std::shared_ptr<Runtime::Execution::Operators::Operator> LowerPhysicalToNautilus
 std::shared_ptr<Runtime::Execution::Operators::Operator>
 LowerPhysicalToNautilusOperators::lowerScan(const std::shared_ptr<Schema>& schema, size_t bufferSize)
 {
-    std::vector<std::string> fieldNames;
-    if (schema->getFieldNames().size() != 0)
-    {
-
-        fieldNames.emplace_back(schema->getFieldNames()[2]);
-    }
     switch (schema->getLayoutType())
     {
         case Schema::MemoryLayoutType::COLUMNAR_LAYOUT: {
             auto layout = std::make_shared<Memory::MemoryLayouts::ColumnLayout>(schema, bufferSize);
             std::unique_ptr<Nautilus::Interface::MemoryProvider::TupleBufferMemoryProvider> memoryProvider
                 = std::make_unique<Nautilus::Interface::MemoryProvider::ColumnTupleBufferMemoryProvider>(layout);
-            return std::make_shared<Runtime::Execution::Operators::Scan>(std::move(memoryProvider), fieldNames);
+            return std::make_shared<Runtime::Execution::Operators::Scan>(std::move(memoryProvider), schema->getFieldNames());
         }
         case Schema::MemoryLayoutType::ROW_LAYOUT: {
             auto layout = std::make_shared<Memory::MemoryLayouts::RowLayout>(schema, bufferSize);
             std::unique_ptr<Nautilus::Interface::MemoryProvider::TupleBufferMemoryProvider> memoryProvider
                 = std::make_unique<Nautilus::Interface::MemoryProvider::RowTupleBufferMemoryProvider>(layout);
-            return std::make_shared<Runtime::Execution::Operators::Scan>(std::move(memoryProvider), fieldNames);
+            return std::make_shared<Runtime::Execution::Operators::Scan>(std::move(memoryProvider), schema->getFieldNames());
         }
     }
 }
