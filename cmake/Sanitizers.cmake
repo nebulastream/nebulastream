f3cc61717173bce6259e0640eda375f32ccc5052
Matthis Gördel <matthis@posteo.de>
2025-05-14 17:18:24 +0200

    hackily add codecov flags

--- cmake/Sanitizers.cmake
+++ cmake/Sanitizers.cmake
@@ -42,3 +42,6 @@ endif ()
 
 add_compile_options(-fsanitize=fuzzer-no-link)
 add_link_options(-fsanitize=fuzzer-no-link)
+
+add_compile_options(-fprofile-instr-generate -fcoverage-mapping)
+add_link_options(-fprofile-instr-generate -fcoverage-mapping)
