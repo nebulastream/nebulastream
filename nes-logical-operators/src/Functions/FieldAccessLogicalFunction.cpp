41ce38e32874c04e9dfd24846a1600b51f7784f0
Adrian Michalke <michalke@tu-berlin.de>
2025-05-16 20:35:03 +0200

    refactor(LogicalOperators): Migrate FieldAccess and ConstantValue functions to LogicalFunction concept

---
+++ nes-logical-operators/src/Functions/FieldAccessLogicalFunction.cpp
@@ -0,0 +1,138 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+#include <format>
+#include <memory>
+#include <string>
+#include <utility>
+#include <API/Schema.hpp>
+#include <Functions/FieldAccessLogicalFunction.hpp>
+#include <Serialization/DataTypeSerializationUtil.hpp>
+#include <ErrorHandling.hpp>
+#include <LogicalFunctionRegistry.hpp>
+#include <Common/DataTypes/DataType.hpp>
+#include <Common/DataTypes/DataTypeProvider.hpp>
+
+namespace NES
+{
+FieldAccessLogicalFunction::FieldAccessLogicalFunction(std::string fieldName)
+    : fieldName(std::move(fieldName)), dataType(DataTypeProvider::provideDataType(LogicalType::UNDEFINED)) { };
+
+FieldAccessLogicalFunction::FieldAccessLogicalFunction(std::shared_ptr<DataType> dataType, std::string fieldName)
+    : fieldName(std::move(fieldName)), dataType(dataType) { };
+
+bool FieldAccessLogicalFunction::operator==(const LogicalFunctionConcept& rhs) const
+{
+    if (auto other = dynamic_cast<const FieldAccessLogicalFunction*>(&rhs))
+    {
+        return *this == *other;
+    }
+    return false;
+}
+
+bool operator==(const FieldAccessLogicalFunction& lhs, const FieldAccessLogicalFunction& rhs)
+{
+    bool fieldNamesMatch = rhs.fieldName == lhs.fieldName;
+    const bool dataTypesMatch = *rhs.dataType == *lhs.dataType;
+    return fieldNamesMatch and dataTypesMatch;
+}
+
+bool operator!=(const FieldAccessLogicalFunction& lhs, const FieldAccessLogicalFunction& rhs)
+{
+    return !(lhs == rhs);
+}
+
+std::string FieldAccessLogicalFunction::getFieldName() const
+{
+    return fieldName;
+}
+
+LogicalFunction FieldAccessLogicalFunction::withFieldName(std::string fieldName) const
+{
+    auto copy = *this;
+    copy.fieldName = std::move(fieldName);
+    return copy;
+}
+
+std::string FieldAccessLogicalFunction::explain(ExplainVerbosity verbosity) const
+{
+    if (verbosity == ExplainVerbosity::Debug)
+    {
+        return fmt::format("FieldAccessLogicalFunction({}{})", fieldName, dataType ? fmt::format(" [{}]", dataType->toString()) : "");
+    }
+    return fieldName;
+}
+
+LogicalFunction FieldAccessLogicalFunction::withInferredDataType(const Schema& schema) const
+{
+    const auto existingField = schema.getFieldByName(fieldName);
+    INVARIANT(existingField, "field is not part of the schema");
+
+    auto copy = *this;
+    copy.dataType = existingField.value().getDataType();
+    copy.fieldName = existingField.value().getName();
+    return copy;
+}
+
+std::shared_ptr<DataType> FieldAccessLogicalFunction::getDataType() const
+{
+    return dataType;
+};
+
+LogicalFunction FieldAccessLogicalFunction::withDataType(std::shared_ptr<DataType> newStamp) const
+{
+    PRECONDITION(newStamp != nullptr, "newStamp is null in FieldAccessLogicalFunction::withStamp");
+    auto copy = *this;
+    copy.dataType = newStamp;
+    return copy;
+}
+
+std::vector<LogicalFunction> FieldAccessLogicalFunction::getChildren() const
+{
+    return {};
+};
+
+LogicalFunction FieldAccessLogicalFunction::withChildren(const std::vector<LogicalFunction>&) const
+{
+    return *this;
+};
+
+std::string_view FieldAccessLogicalFunction::getType() const
+{
+    return NAME;
+}
+
+SerializableFunction FieldAccessLogicalFunction::serialize() const
+{
+    SerializableFunction serializedFunction;
+    serializedFunction.set_function_type(NAME);
+
+    NES::Configurations::DescriptorConfig::ConfigType configVariant = getFieldName();
+    SerializableVariantDescriptor variantDescriptor = descriptorConfigTypeToProto(configVariant);
+    (*serializedFunction.mutable_config())["FieldName"] = variantDescriptor;
+
+    DataTypeSerializationUtil::serializeDataType(dataType, serializedFunction.mutable_data_type());
+
+    return serializedFunction;
+}
+
+LogicalFunctionRegistryReturnType
+LogicalFunctionGeneratedRegistrar::RegisterFieldAccessLogicalFunction(LogicalFunctionRegistryArguments arguments)
+{
+    PRECONDITION(arguments.config.contains("FieldName"), "FieldAccessLogicalFunction requires a FieldName in its config");
+    auto fieldName = get<std::string>(arguments.config["FieldName"]);
+    PRECONDITION(!fieldName.empty(), "FieldName cannot be empty");
+    return FieldAccessLogicalFunction(arguments.dataType, fieldName);
+}
+
+}
