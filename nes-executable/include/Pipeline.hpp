caa7aa55ad67f9a7ec9b2a0d4976dae25579aebc
Adrian Michalke <michalke@tu-berlin.de>
2025-05-16 21:25:19 +0200

    refactor(runtime): Adapt ExecutableQueryPlan, CompiledQueryPlan, and Pipeline structures

---
+++ nes-executable/include/Pipeline.hpp
@@ -0,0 +1,77 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+#pragma once
+
+#include <vector>
+#include <Identifiers/Identifiers.hpp>
+#include <Nautilus/NautilusBackend.hpp>
+#include <Runtime/Execution/OperatorHandler.hpp>
+#include <PhysicalOperator.hpp>
+#include <SinkPhysicalOperator.hpp>
+#include <SourcePhysicalOperator.hpp>
+
+namespace NES
+{
+
+PipelineId getNextPipelineId();
+
+/// @brief Defines a single pipeline, which contains of a query plan of operators.
+/// Each pipeline can have N successor and predecessor pipelines.
+/// We make use of shared_ptr reference counting of Pipelines to track the lifetime of query plans
+struct Pipeline
+{
+    explicit Pipeline(PhysicalOperator op);
+    explicit Pipeline(SourcePhysicalOperator op);
+    explicit Pipeline(SinkPhysicalOperator op);
+    Pipeline() = delete;
+
+    [[nodiscard]] bool isSourcePipeline() const;
+    [[nodiscard]] bool isOperatorPipeline() const;
+    [[nodiscard]] bool isSinkPipeline() const;
+
+    void appendOperator(PhysicalOperator newOp);
+    void prependOperator(PhysicalOperator newOp);
+
+    friend std::ostream& operator<<(std::ostream& os, const Pipeline& p);
+
+    [[nodiscard]] Nautilus::Configurations::ExecutionMode getExecutionMode() const;
+    void setExecutionMode(Nautilus::Configurations::ExecutionMode mode);
+
+    [[nodiscard]] const PhysicalOperator& getRootOperator() const;
+    void setRootOperator(const PhysicalOperator& op);
+
+    [[nodiscard]] PipelineId getPipelineId() const;
+
+    [[nodiscard]] const std::unordered_map<OperatorHandlerId, std::shared_ptr<OperatorHandler>>& getOperatorHandlers() const;
+    std::unordered_map<OperatorHandlerId, std::shared_ptr<OperatorHandler>>& getOperatorHandlers();
+
+    void addSuccessor(const std::shared_ptr<Pipeline>& successor, const std::weak_ptr<Pipeline>& self);
+
+    void removePredecessor(const Pipeline& pipeline);
+    [[nodiscard]] const std::vector<std::shared_ptr<Pipeline>>& getSuccessors() const;
+
+    void clearSuccessors();
+    void clearPredecessors();
+    void removeSuccessor(const Pipeline& pipeline);
+
+private:
+    Nautilus::Configurations::ExecutionMode executionMode = Nautilus::Configurations::ExecutionMode::INTERPRETER;
+    PhysicalOperator rootOperator;
+    const PipelineId pipelineId;
+    std::unordered_map<OperatorHandlerId, std::shared_ptr<OperatorHandler>> operatorHandlers;
+    std::vector<std::shared_ptr<Pipeline>> successorPipelines;
+    std::vector<std::weak_ptr<Pipeline>> predecessorPipelines;
+};
+}
+FMT_OSTREAM(NES::Pipeline);
