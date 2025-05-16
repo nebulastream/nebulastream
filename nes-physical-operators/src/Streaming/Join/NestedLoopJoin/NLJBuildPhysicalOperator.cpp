f233d7d19ce3d84b5e57f3ce65f4c8cb0490ac7b
Adrian Michalke <michalke@tu-berlin.de>
2025-05-16 20:08:00 +0200

    refactor(PhysicalOperators): Migrate StreamJoin components to nes-physical-operators

---
+++ nes-physical-operators/src/Streaming/Join/NestedLoopJoin/NLJBuildPhysicalOperator.cpp
@@ -0,0 +1,83 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include <cstdint>
+#include <memory>
+#include <utility>
+#include <Nautilus/Interface/MemoryProvider/TupleBufferMemoryProvider.hpp>
+#include <Nautilus/Interface/PagedVector/PagedVectorRef.hpp>
+#include <Nautilus/Interface/Record.hpp>
+#include <Nautilus/Interface/RecordBuffer.hpp>
+#include <Nautilus/Interface/TimestampRef.hpp>
+#include <Runtime/Execution/OperatorHandler.hpp>
+#include <Streaming/Join/NestedLoopJoin/NLJBuildPhysicalOperator.hpp>
+#include <Streaming/Join/NestedLoopJoin/NLJOperatorHandler.hpp>
+#include <Streaming/Join/NestedLoopJoin/NLJSlice.hpp>
+#include <Streaming/Join/StreamJoinBuildPhysicalOperator.hpp>
+#include <Streaming/WindowBuildPhysicalOperator.hpp>
+#include <Time/Timestamp.hpp>
+#include <Watermark/TimeFunction.hpp>
+#include <nautilus/val_enum.hpp>
+#include <ErrorHandling.hpp>
+#include <ExecutionContext.hpp>
+#include <function.hpp>
+
+namespace NES
+{
+SliceStart getNLJSliceStartProxy(const NLJSlice* nljSlice)
+{
+    PRECONDITION(nljSlice != nullptr, "nlj slice pointer should not be null!");
+    return nljSlice->getSliceStart();
+}
+
+SliceEnd getNLJSliceEndProxy(const NLJSlice* nljSlice)
+{
+    PRECONDITION(nljSlice != nullptr, "nlj slice pointer should not be null!");
+    return nljSlice->getSliceEnd();
+}
+
+NLJBuildPhysicalOperator::NLJBuildPhysicalOperator(
+    OperatorHandlerId operatorHandlerIndex,
+    const JoinBuildSideType joinBuildSide,
+    std::unique_ptr<TimeFunction> timeFunction,
+    std::shared_ptr<TupleBufferMemoryProvider> memoryProvider)
+    : StreamJoinBuildPhysicalOperator(operatorHandlerIndex, joinBuildSide, std::move(timeFunction), memoryProvider)
+{
+}
+
+void NLJBuildPhysicalOperator::execute(ExecutionContext& executionCtx, Record& record) const
+{
+    /// Get the current slice / pagedVector that we have to insert the tuple into
+    const auto timestamp = timeFunction->getTs(executionCtx, record);
+    const auto operatorHandlerRef = executionCtx.getGlobalOperatorHandler(operatorHandlerIndex);
+    const auto sliceReference = invoke(
+        +[](OperatorHandler* ptrOpHandler, const Timestamp timestamp)
+        {
+            PRECONDITION(ptrOpHandler != nullptr, "opHandler context should not be null!");
+            const auto* opHandler = dynamic_cast<NLJOperatorHandler*>(ptrOpHandler);
+            const auto createFunction = opHandler->getCreateNewSlicesFunction();
+            return dynamic_cast<NLJSlice*>(opHandler->getSliceAndWindowStore().getSlicesOrCreate(timestamp, createFunction)[0].get());
+        },
+        operatorHandlerRef,
+        timestamp);
+    const auto nljPagedVectorMemRef
+        = invoke(getNLJPagedVectorProxy, sliceReference, executionCtx.getWorkerThreadId(), nautilus::val<JoinBuildSideType>(joinBuildSide));
+
+
+    /// Write record to the pagedVector
+    const Interface::PagedVectorRef pagedVectorRef(
+        nljPagedVectorMemRef, memoryProvider, executionCtx.pipelineMemoryProvider.bufferProvider);
+    pagedVectorRef.writeRecord(record, executionCtx.pipelineMemoryProvider.bufferProvider);
+}
+}
