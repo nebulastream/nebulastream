566f8da77ebb3de1be49ef83d7c6682e43144fb9
Adrian Michalke <michalke@tu-berlin.de>
2025-05-16 21:24:17 +0200

    feat(optimizer): Add LowerToPhysical rules for Source, Sink, and core operators

---
+++ nes-query-optimizer/src/RewriteRules/LowerToPhysical/LowerToPhysicalSink.cpp
@@ -0,0 +1,42 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include <memory>
+#include <Operators/LogicalOperator.hpp>
+#include <Operators/Sinks/SinkLogicalOperator.hpp>
+#include <RewriteRules/AbstractRewriteRule.hpp>
+#include <RewriteRules/LowerToPhysical/LowerToPhysicalSink.hpp>
+#include <RewriteRuleRegistry.hpp>
+#include <SinkPhysicalOperator.hpp>
+
+namespace NES
+{
+
+RewriteRuleResultSubgraph LowerToPhysicalSink::apply(LogicalOperator logicalOperator)
+{
+    PRECONDITION(logicalOperator.tryGet<SinkLogicalOperator>(), "Expected a SinkLogicalOperator");
+    auto sink = logicalOperator.get<SinkLogicalOperator>();
+    auto physicalOperator = SinkPhysicalOperator(sink.sinkDescriptor);
+    auto wrapper = std::make_shared<PhysicalOperatorWrapper>(physicalOperator, sink.getInputSchemas()[0], sink.getOutputSchema());
+
+    /// Creates a physical leaf for each logical leaf. Required, as this operator can have any number of sources.
+    std::vector leafes(logicalOperator.getChildren().size(), wrapper);
+    return {wrapper, {leafes}};
+}
+
+std::unique_ptr<AbstractRewriteRule> RewriteRuleGeneratedRegistrar::RegisterSinkRewriteRule(RewriteRuleRegistryArguments argument)
+{
+    return std::make_unique<LowerToPhysicalSink>(argument.conf);
+}
+}
---
+++ nes-query-optimizer/src/RewriteRules/LowerToPhysical/LowerToPhysicalUnion.cpp
@@ -0,0 +1,45 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include <memory>
+#include <Nautilus/Interface/MemoryProvider/RowTupleBufferMemoryProvider.hpp>
+#include <Operators/UnionLogicalOperator.hpp>
+#include <RewriteRules/AbstractRewriteRule.hpp>
+#include <RewriteRules/LowerToPhysical/LowerToPhysicalUnion.hpp>
+#include <ErrorHandling.hpp>
+#include <PhysicalOperator.hpp>
+#include <RewriteRuleRegistry.hpp>
+#include <UnionPhysicalOperator.hpp>
+
+namespace NES
+{
+
+RewriteRuleResultSubgraph LowerToPhysicalUnion::apply(LogicalOperator logicalOperator)
+{
+    PRECONDITION(logicalOperator.tryGet<UnionLogicalOperator>(), "Expected a UnionLogicalOperator");
+    auto source = logicalOperator.get<UnionLogicalOperator>();
+
+    auto inputSchemas = logicalOperator.getInputSchemas();
+    PRECONDITION(inputSchemas.size() == 2, "UnionLogicalOperator should have exactly two schema, but has {}", inputSchemas.size());
+    auto physicalOperator = UnionPhysicalOperator();
+
+    auto wrapper = std::make_shared<PhysicalOperatorWrapper>(physicalOperator, inputSchemas[0], logicalOperator.getOutputSchema());
+    return {wrapper, {wrapper, wrapper}};
+}
+
+RewriteRuleRegistryReturnType RewriteRuleGeneratedRegistrar::RegisterUnionRewriteRule(RewriteRuleRegistryArguments argument)
+{
+    return std::make_unique<LowerToPhysicalUnion>(argument.conf);
+}
+}
