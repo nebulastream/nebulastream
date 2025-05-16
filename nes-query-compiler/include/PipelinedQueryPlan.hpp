a5b38ac8c7bbc27ddc1bfe02acb0ac35f502686b
Adrian Michalke <michalke@tu-berlin.de>
2025-05-16 21:23:57 +0200

    feat(compiler): Introduce QueryCompiler and PipelinedQueryPlan

---
+++ nes-query-compiler/include/PipelinedQueryPlan.hpp
@@ -0,0 +1,47 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+#pragma once
+
+#include <memory>
+#include <ostream>
+#include <vector>
+#include <Identifiers/Identifiers.hpp>
+#include <Nautilus/NautilusBackend.hpp>
+#include <Runtime/Execution/OperatorHandler.hpp>
+#include <PhysicalPlan.hpp>
+#include <Pipeline.hpp>
+
+namespace NES
+{
+struct PipelinedQueryPlan final
+{
+    explicit PipelinedQueryPlan(QueryId id, Nautilus::Configurations::ExecutionMode executionMode);
+
+    friend std::ostream& operator<<(std::ostream& os, const PipelinedQueryPlan& plan);
+
+    [[nodiscard]] QueryId getQueryId() const;
+    [[nodiscard]] Nautilus::Configurations::ExecutionMode getExecutionMode() const;
+
+    [[nodiscard]] std::vector<std::shared_ptr<Pipeline>> getSourcePipelines() const;
+    [[nodiscard]] const std::vector<std::shared_ptr<Pipeline>>& getPipelines() const;
+    void addPipeline(const std::shared_ptr<Pipeline>& pipeline);
+    void removePipeline(Pipeline& pipeline);
+
+private:
+    QueryId queryId;
+    Nautilus::Configurations::ExecutionMode executionMode;
+    std::vector<std::shared_ptr<Pipeline>> pipelines;
+};
+}
+FMT_OSTREAM(NES::PipelinedQueryPlan);
---
+++ nes-query-compiler/src/PipelinedQueryPlan.cpp
@@ -0,0 +1,93 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include <Phases/LowerToCompiledQueryPlanPhase.hpp>
+#include <Runtime/NodeEngine.hpp>
+#include <ErrorHandling.hpp>
+#include <PipelinedQueryPlan.hpp>
+#include <QueryCompiler.hpp>
+
+namespace NES
+{
+
+PipelinedQueryPlan::PipelinedQueryPlan(QueryId id, Nautilus::Configurations::ExecutionMode executionMode)
+    : queryId(id), executionMode(executionMode) { };
+
+void printPipelineRecursive(const Pipeline* pipeline, std::ostream& os, int indentLevel)
+{
+    std::string indent(indentLevel * 2, ' ');
+    os << indent << *pipeline << "\n";
+    for (const auto& succ : pipeline->getSuccessors())
+    {
+        os << indent << "Successor Pipeline:\n";
+        printPipelineRecursive(succ.get(), os, indentLevel + 1);
+    }
+}
+
+std::ostream& operator<<(std::ostream& os, const PipelinedQueryPlan& plan)
+{
+    os << "PipelinedQueryPlan for Query: " << plan.getQueryId() << "\n";
+    os << "Number of root pipelines: " << plan.getPipelines().size() << "\n";
+    for (size_t i = 0; i < plan.getPipelines().size(); ++i)
+    {
+        os << "------------------\n";
+        os << "Root Pipeline " << i << ":\n";
+        printPipelineRecursive(plan.getPipelines()[i].get(), os, 1);
+    }
+    return os;
+}
+
+void PipelinedQueryPlan::removePipeline(Pipeline& pipeline)
+{
+    pipeline.clearSuccessors();
+    pipeline.clearPredecessors();
+    std::erase_if(pipelines, [&pipeline](const auto& ptr) { return ptr->getPipelineId() == pipeline.getPipelineId(); });
+}
+
+std::vector<std::shared_ptr<Pipeline>> PipelinedQueryPlan::getSourcePipelines() const
+{
+    std::vector<std::shared_ptr<Pipeline>> sourcePipelines;
+    auto it = pipelines.begin();
+    while (it != pipelines.end())
+    {
+        if (it->get()->isSourcePipeline())
+        {
+            sourcePipelines.push_back(*it);
+        }
+        ++it;
+    }
+    return sourcePipelines;
+}
+
+QueryId PipelinedQueryPlan::getQueryId() const
+{
+    return queryId;
+}
+
+Nautilus::Configurations::ExecutionMode PipelinedQueryPlan::getExecutionMode() const
+{
+    return executionMode;
+}
+
+const std::vector<std::shared_ptr<Pipeline>>& PipelinedQueryPlan::getPipelines() const
+{
+    return pipelines;
+}
+
+void PipelinedQueryPlan::addPipeline(const std::shared_ptr<Pipeline>& pipeline)
+{
+    pipelines.push_back(pipeline);
+}
+
+}
