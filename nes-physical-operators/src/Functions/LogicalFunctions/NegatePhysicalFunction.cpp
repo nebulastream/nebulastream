7cccaa6eaca1918dc493302efe98f6d1cb0f9ae5
Adrian Michalke <michalke@tu-berlin.de>
2025-05-16 20:07:49 +0200

    refactor(PhysicalOperators): Migrate logical functions to PhysicalFunction and remove old versions

---
+++ nes-physical-operators/src/Functions/LogicalFunctions/NegatePhysicalFunction.cpp
@@ -0,0 +1,43 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+#include <memory>
+#include <Functions/LogicalFunctions/NegatePhysicalFunction.hpp>
+#include <Functions/PhysicalFunction.hpp>
+#include <Nautilus/DataTypes/VarVal.hpp>
+#include <Nautilus/Interface/Record.hpp>
+#include <ErrorHandling.hpp>
+#include <ExecutionContext.hpp>
+#include <PhysicalFunctionRegistry.hpp>
+
+namespace NES::Functions
+{
+
+VarVal NegatePhysicalFunction::execute(const Record& record, ArenaRef& arena) const
+{
+    const auto value = childFunction.execute(record, arena);
+    return !value;
+}
+
+NegatePhysicalFunction::NegatePhysicalFunction(PhysicalFunction childFunction) : childFunction(childFunction)
+{
+}
+
+PhysicalFunctionRegistryReturnType
+PhysicalFunctionGeneratedRegistrar::RegisterNegatePhysicalFunction(PhysicalFunctionRegistryArguments PhysicalFunctionRegistryArguments)
+{
+    PRECONDITION(PhysicalFunctionRegistryArguments.childFunctions.size() == 1, "Negate function must have exactly one sub-function");
+    return NegatePhysicalFunction(PhysicalFunctionRegistryArguments.childFunctions[0]);
+}
+
+}
