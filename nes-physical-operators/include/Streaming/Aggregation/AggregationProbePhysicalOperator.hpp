d01d704ea325b94860c2db543cdc0b289b6bdfe2
Adrian Michalke <michalke@tu-berlin.de>
2025-05-16 20:07:58 +0200

    refactor(PhysicalOperators): Migrate Aggregation components and functions to nes-physical-operators

---
+++ nes-physical-operators/include/Streaming/Aggregation/AggregationProbePhysicalOperator.hpp
@@ -0,0 +1,38 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#pragma once
+
+#include <cstdint>
+#include <memory>
+#include <string>
+#include <Nautilus/Interface/RecordBuffer.hpp>
+#include <Streaming/Aggregation/WindowAggregation.hpp>
+#include <Streaming/WindowProbePhysicalOperator.hpp>
+
+namespace NES
+{
+
+class AggregationProbePhysicalOperator final : public WindowAggregation, public WindowProbePhysicalOperator
+{
+public:
+    AggregationProbePhysicalOperator(
+        std::shared_ptr<WindowAggregation> windowAggregationOperator,
+        OperatorHandlerId operatorHandlerIndex,
+        WindowMetaData windowMetaData);
+    AggregationProbePhysicalOperator(const AggregationProbePhysicalOperator& other);
+    void open(ExecutionContext& executionCtx, RecordBuffer& recordBuffer) const override;
+};
+
+}
---
+++ nes-physical-operators/src/Streaming/Aggregation/Function/AggregationFunction.cpp
@@ -0,0 +1,38 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include <memory>
+#include <utility>
+#include <Functions/PhysicalFunction.hpp>
+#include <Nautilus/Interface/Record.hpp>
+#include <Streaming/Aggregation/Function/AggregationFunction.hpp>
+#include <Common/PhysicalTypes/PhysicalType.hpp>
+
+namespace NES
+{
+
+AggregationFunction::AggregationFunction(
+    std::unique_ptr<PhysicalType> inputType,
+    std::unique_ptr<PhysicalType> resultType,
+    Functions::PhysicalFunction inputFunction,
+    Nautilus::Record::RecordFieldIdentifier resultFieldIdentifier)
+    : inputType(std::move(inputType))
+    , resultType(std::move(resultType))
+    , inputFunction(std::move(inputFunction))
+    , resultFieldIdentifier(std::move(resultFieldIdentifier))
+{
+}
+
+AggregationFunction::~AggregationFunction() = default;
+}
