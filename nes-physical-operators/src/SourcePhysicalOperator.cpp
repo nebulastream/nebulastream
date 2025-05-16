949d84438ba06afa9922ba7dfa227a2f1d78577d
Adrian Michalke <michalke@tu-berlin.de>
2025-05-16 20:07:35 +0200

    feat(PhysicalOperators): Add Source, Sink, and Union physical operators

---
+++ nes-physical-operators/src/SourcePhysicalOperator.cpp
@@ -0,0 +1,48 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include <SourcePhysicalOperator.hpp>
+
+namespace NES
+{
+
+SourcePhysicalOperator::SourcePhysicalOperator(std::shared_ptr<Sources::SourceDescriptor> descriptor, OriginId id)
+    : originId(id), descriptor(descriptor) { };
+
+std::shared_ptr<Sources::SourceDescriptor> SourcePhysicalOperator::getDescriptor() const
+{
+    return descriptor;
+};
+
+OriginId SourcePhysicalOperator::getOriginId() const
+{
+    return originId;
+};
+
+bool SourcePhysicalOperator::operator==(const SourcePhysicalOperator& other) const
+{
+    return *descriptor == *other.descriptor;
+}
+
+std::optional<PhysicalOperator> SourcePhysicalOperator::getChild() const
+{
+    return child;
+}
+
+void SourcePhysicalOperator::setChild(PhysicalOperator child)
+{
+    this->child = std::move(child);
+}
+
+}
