cd98f07cb6bffea7dfcbb8c6a9fe38d816ad1663
Adrian Michalke <michalke@tu-berlin.de>
2025-05-16 21:24:17 +0200

    feat(optimizer): Add LowerToPhysical rules for Source, Sink, and core operators

---
+++ nes-query-optimizer/src/RewriteRules/LowerToPhysical/LowerToPhysicalProjection.cpp
@@ -0,0 +1,64 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include <memory>
+#include <Functions/FunctionProvider.hpp>
+#include <MemoryLayout/RowLayout.hpp>
+#include <Nautilus/Interface/MemoryProvider/RowTupleBufferMemoryProvider.hpp>
+#include <Operators/ProjectionLogicalOperator.hpp>
+#include <RewriteRules/AbstractRewriteRule.hpp>
+#include <RewriteRules/LowerToPhysical/LowerToPhysicalProjection.hpp>
+#include <EmitOperatorHandler.hpp>
+#include <EmitPhysicalOperator.hpp>
+#include <RewriteRuleRegistry.hpp>
+#include <ScanPhysicalOperator.hpp>
+
+namespace NES
+{
+
+RewriteRuleResultSubgraph LowerToPhysicalProjection::apply(LogicalOperator projectionLogicalOperator)
+{
+    auto handlerId = getNextOperatorHandlerId();
+    auto inputSchema = projectionLogicalOperator.getInputSchemas()[0];
+    auto outputSchema = projectionLogicalOperator.getOutputSchema();
+    auto bufferSize = conf.pageSize.getValue();
+
+    auto scanLayout = std::make_shared<Memory::MemoryLayouts::RowLayout>(inputSchema, bufferSize);
+    auto scanMemoryProvider = std::make_shared<Interface::MemoryProvider::RowTupleBufferMemoryProvider>(scanLayout);
+    auto scan = ScanPhysicalOperator(scanMemoryProvider, outputSchema.getFieldNames());
+    auto scanWrapper = std::make_shared<PhysicalOperatorWrapper>(
+        scan, outputSchema, outputSchema, std::nullopt, std::nullopt, PhysicalOperatorWrapper::PipelineEndpoint::Scan);
+
+    auto emitLayout = std::make_shared<Memory::MemoryLayouts::RowLayout>(outputSchema, bufferSize);
+    auto emitMemoryProvider = std::make_shared<RowTupleBufferMemoryProvider>(emitLayout);
+    auto emit = EmitPhysicalOperator(handlerId, emitMemoryProvider);
+    auto emitWrapper = std::make_shared<PhysicalOperatorWrapper>(
+        emit,
+        outputSchema,
+        outputSchema,
+        handlerId,
+        std::make_shared<EmitOperatorHandler>(),
+        PhysicalOperatorWrapper::PipelineEndpoint::Emit,
+        std::vector<std::shared_ptr<PhysicalOperatorWrapper>>{scanWrapper});
+
+    /// Creates a physical leaf for each logical leaf. Required, as this operator can have any number of sources.
+    std::vector leafs(projectionLogicalOperator.getChildren().size(), scanWrapper);
+    return {.root = emitWrapper, .leafs = {scanWrapper}};
+}
+
+std::unique_ptr<AbstractRewriteRule> RewriteRuleGeneratedRegistrar::RegisterProjectionRewriteRule(RewriteRuleRegistryArguments argument)
+{
+    return std::make_unique<LowerToPhysicalProjection>(argument.conf);
+}
+}
