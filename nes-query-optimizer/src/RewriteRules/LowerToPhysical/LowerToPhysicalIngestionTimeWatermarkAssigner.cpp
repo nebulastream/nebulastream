06ba138b445f174d72a11af6e8264abd1719a01d
Adrian Michalke <michalke@tu-berlin.de>
2025-05-16 21:24:33 +0200

    feat(optimizer): Add LowerToPhysical rules for WatermarkAssigner, Join, and WindowedAggregation

---
+++ nes-query-optimizer/src/RewriteRules/LowerToPhysical/LowerToPhysicalIngestionTimeWatermarkAssigner.cpp
@@ -0,0 +1,45 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include <memory>
+#include <Functions/FunctionProvider.hpp>
+#include <Operators/IngestionTimeWatermarkAssignerLogicalOperator.hpp>
+#include <RewriteRules/AbstractRewriteRule.hpp>
+#include <RewriteRules/LowerToPhysical/LowerToPhysicalIngestionTimeWatermarkAssigner.hpp>
+#include <Watermark/IngestionTimeWatermarkAssignerPhysicalOperator.hpp>
+#include <Watermark/TimeFunction.hpp>
+#include <MapPhysicalOperator.hpp>
+#include <RewriteRuleRegistry.hpp>
+
+namespace NES
+{
+
+RewriteRuleResultSubgraph LowerToPhysicalIngestionTimeWatermarkAssigner::apply(LogicalOperator logicalOperator)
+{
+    PRECONDITION(logicalOperator.tryGet<IngestionTimeWatermarkAssignerLogicalOperator>(), "Expected a IngestionTimeWatermarkAssigner");
+    auto physicalOperator = IngestionTimeWatermarkAssignerPhysicalOperator(IngestionTimeFunction());
+    auto wrapper = std::make_shared<PhysicalOperatorWrapper>(
+        physicalOperator, logicalOperator.getInputSchemas()[0], logicalOperator.getOutputSchema());
+
+    /// Creates a physical leaf for each logical leaf. Required, as this operator can have any number of sources.
+    std::vector leafes(logicalOperator.getChildren().size(), wrapper);
+    return {wrapper, {leafes}};
+}
+
+std::unique_ptr<AbstractRewriteRule>
+RewriteRuleGeneratedRegistrar::RegisterIngestionTimeWatermarkAssignerRewriteRule(RewriteRuleRegistryArguments argument)
+{
+    return std::make_unique<LowerToPhysicalIngestionTimeWatermarkAssigner>(argument.conf);
+}
+}
