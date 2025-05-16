41209df89c69a19a8700bfd45c2accfdfaf0657c
Adrian Michalke <michalke@tu-berlin.de>
2025-05-16 20:07:47 +0200

    refactor(PhysicalOperators): Migrate comparison functions to PhysicalFunction and remove old versions

---
+++ nes-physical-operators/src/Functions/ComparisonFunctions/GreaterPhysicalFunction.cpp
@@ -0,0 +1,46 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include <memory>
+#include <utility>
+#include <vector>
+#include <Functions/ComparisonFunctions/GreaterPhysicalFunction.hpp>
+#include <Functions/PhysicalFunction.hpp>
+#include <ErrorHandling.hpp>
+#include <PhysicalFunctionRegistry.hpp>
+
+
+namespace NES::Functions
+{
+
+VarVal GreaterPhysicalFunction::execute(const Record& record, ArenaRef& arena) const
+{
+    const auto leftValue = leftPhysicalFunction.execute(record, arena);
+    const auto rightValue = rightPhysicalFunction.execute(record, arena);
+    return leftValue > rightValue;
+}
+
+GreaterPhysicalFunction::GreaterPhysicalFunction(PhysicalFunction leftPhysicalFunction, PhysicalFunction rightPhysicalFunction)
+    : leftPhysicalFunction(leftPhysicalFunction), rightPhysicalFunction(rightPhysicalFunction)
+{
+}
+PhysicalFunctionRegistryReturnType
+PhysicalFunctionGeneratedRegistrar::RegisterGreaterPhysicalFunction(PhysicalFunctionRegistryArguments PhysicalFunctionRegistryArguments)
+{
+    PRECONDITION(PhysicalFunctionRegistryArguments.childFunctions.size() == 2, "Greater function must have exactly two sub-functions");
+    return GreaterPhysicalFunction(
+        PhysicalFunctionRegistryArguments.childFunctions[0], PhysicalFunctionRegistryArguments.childFunctions[1]);
+}
+
+}
