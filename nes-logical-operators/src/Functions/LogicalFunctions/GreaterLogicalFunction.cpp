00ac4b65951edc14cbe51350d334e77c6444336e
Adrian Michalke <michalke@tu-berlin.de>
2025-05-16 20:58:40 +0200

    refactor(LogicalOperators): Add TimeUnit and related changes

---
+++ nes-logical-operators/src/Functions/LogicalFunctions/GreaterLogicalFunction.cpp
@@ -0,0 +1,111 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include <Functions/LogicalFunctions/GreaterLogicalFunction.hpp>
+#include <Serialization/DataTypeSerializationUtil.hpp>
+#include <Util/Common.hpp>
+#include <fmt/format.h>
+#include <LogicalFunctionRegistry.hpp>
+#include <Common/DataTypes/DataType.hpp>
+#include <Common/DataTypes/DataTypeProvider.hpp>
+
+namespace NES
+{
+
+GreaterLogicalFunction::GreaterLogicalFunction(const GreaterLogicalFunction& other)
+    : dataType(other.dataType), left(other.left), right(other.right)
+{
+}
+
+GreaterLogicalFunction::GreaterLogicalFunction(LogicalFunction left, LogicalFunction right)
+    : dataType(DataTypeProvider::provideDataType(LogicalType::BOOLEAN)), left(left), right(right)
+{
+}
+
+bool GreaterLogicalFunction::operator==(const LogicalFunctionConcept& rhs) const
+{
+    if (auto other = dynamic_cast<const GreaterLogicalFunction*>(&rhs))
+    {
+        const bool simpleMatch = left == other->left and right == other->right;
+        const bool commutativeMatch = left == other->right and right == other->left;
+        return simpleMatch or commutativeMatch;
+    }
+    return false;
+}
+
+std::string GreaterLogicalFunction::explain(ExplainVerbosity verbosity) const
+{
+    return fmt::format("{} > {}", left.explain(verbosity), right.explain(verbosity));
+}
+
+std::shared_ptr<DataType> GreaterLogicalFunction::getDataType() const
+{
+    return dataType;
+};
+
+LogicalFunction GreaterLogicalFunction::withDataType(std::shared_ptr<DataType> dataType) const
+{
+    auto copy = *this;
+    copy.dataType = dataType;
+    return copy;
+};
+
+LogicalFunction GreaterLogicalFunction::withInferredDataType(const Schema& schema) const
+{
+    std::vector<LogicalFunction> newChildren;
+    for (auto& child : getChildren())
+    {
+        newChildren.push_back(child.withInferredDataType(schema));
+    }
+    return this->withChildren(newChildren);
+}
+
+std::vector<LogicalFunction> GreaterLogicalFunction::getChildren() const
+{
+    return {left, right};
+};
+
+LogicalFunction GreaterLogicalFunction::withChildren(const std::vector<LogicalFunction>& children) const
+{
+    PRECONDITION(children.size() == 2, "GreaterLogicalFunction requires exactly two children, but got {}", children.size());
+    auto copy = *this;
+    copy.left = children[0];
+    copy.right = children[1];
+    return copy;
+};
+
+std::string_view GreaterLogicalFunction::getType() const
+{
+    return NAME;
+}
+
+SerializableFunction GreaterLogicalFunction::serialize() const
+{
+    SerializableFunction serializedFunction;
+    serializedFunction.set_function_type(NAME);
+    serializedFunction.add_children()->CopyFrom(left.serialize());
+    serializedFunction.add_children()->CopyFrom(right.serialize());
+    DataTypeSerializationUtil::serializeDataType(this->getDataType(), serializedFunction.mutable_data_type());
+    return serializedFunction;
+}
+
+LogicalFunctionRegistryReturnType
+LogicalFunctionGeneratedRegistrar::RegisterGreaterLogicalFunction(LogicalFunctionRegistryArguments arguments)
+{
+    PRECONDITION(
+        arguments.children.size() == 2, "GreaterLogicalFunction requires exactly two children, but got {}", arguments.children.size());
+    return GreaterLogicalFunction(arguments.children[0], arguments.children[1]);
+}
+
+}
