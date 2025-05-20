87e6c428bad009749d196e04a714e59010c213d8
Matthis Gördel <matthis@posteo.de>
2025-05-20 20:06:21 +0200

    fuzz engine with SerQePlan and LPM

---
+++ nes-single-node-worker/tests/Integration/ProtoFuzz.cpp
@@ -0,0 +1,46 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include <chrono>
+#include <iostream>
+#include <thread>
+#include <libprotobuf-mutator/src/libfuzzer/libfuzzer_macro.h>
+#include "Operators/Serialization/DecomposedQueryPlanSerializationUtil.hpp"
+#include "Runtime/Execution/QueryStatus.hpp"
+#include "Runtime/QueryTerminationType.hpp"
+#include "SerializableDecomposedQueryPlan.pb.h"
+#include "SingleNodeWorker.hpp"
+#include "SingleNodeWorkerConfiguration.hpp"
+
+DEFINE_PROTO_FUZZER(const NES::SerializableDecomposedQueryPlan& sdqp)
+{
+    std::cout << sdqp.DebugString() << std::endl;
+    auto dqp = NES::DecomposedQueryPlanSerializationUtil::deserializeDecomposedQueryPlan(&sdqp);
+    NES::SingleNodeWorker snw{NES::Configuration::SingleNodeWorkerConfiguration{}};
+    auto qid = snw.registerQuery(dqp);
+    snw.startQuery(qid);
+    snw.stopQuery(qid, NES::Runtime::QueryTerminationType::Graceful);
+    while (true)
+    {
+        if (snw.getQuerySummary(qid)->currentStatus <= NES::Runtime::Execution::QueryStatus::Running)
+        {
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));
+            continue;
+        }
+        else
+        {
+            return;
+        }
+    }
+}
