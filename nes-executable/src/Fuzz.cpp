7f1a3f73df2a33bf062092dd1ab6890161acfd4f
Matthis Gördel <matthis@posteo.de>
2025-05-29 12:36:16 +0200

    not stuck but slow

--- nes-executable/src/Fuzz.cpp
+++ nes-executable/src/Fuzz.cpp
@@ -15,7 +15,6 @@
 #include <cpptrace/from_current.hpp>
 #include "ErrorHandling.hpp"
 #include "SQLQueryParser/AntlrSQLQueryParser.hpp"
-#include "Util/PlanRenderer.hpp"
 
 int main()
 {
@@ -30,15 +29,14 @@ int main()
         std::string query{
             "SELECT * FROM (SELECT * FROM stream0) JOIN (SELECT * FROM stream2) ON u_s1 = _s1 = i8_s2 "
             "and !!!!!!!!!!!!20Ants2 "
-            "and !!!!!!!!!!odeFunctionE_s1 = i8_s2 and !!!!f64Wsi8_s2 and !!!s2 and !!!!f64Wsi8_s2 and !!!!!!!!!!!!8_s1BOO_s2 "
-            "and u8N3NES19LogicalNodeFunctionE_s1 = i8_i8_s2 and !!!!f64Wsi8_s2 and !!!!!!!!!! Lists1 = i8_s2 and !!!!f64Wsi8_s2 "
-            "and !!!!!!!!!!!!8_s1BOO_s2 and u8N3NES19LogicalNodeFunctionE_s1 = i8_s2 and !!!!f64Wsi8_s2 "
-            "and !!!!!!!!!!!!8_sFunctionE_s1 = i8_s2 and !!!!f64Wsi8_s2 and !!!!!!!!!!!!8_s1BOO_s2 "
-            "and u8N3NES19LogicalNodeFunctionE_s1 = i8_i8_s2 and !!!!f64Wsi8_s2 and !!!!!!!!!!!!8_s1BOO_s2 "
-            "and u8N3NES19LogicalNodeFunctionE_s1 = i8_i8_s2 and !!!!f64Wsi8_s2 and !!!!!!!!!!!!8_s1BOO_s2 "
-            "and u8N3NES19LogicalNodeFunctionE_odeFunctionE_s1 = i8_s2 and !!!!f64Ws2 "
+            /// "and !!!!!!!!!!odeFunctionE_s1 = i8_s2 and !!!!f64Wsi8_s2 and !!!s2 and !!!!f64Wsi8_s2 and !!!!!!!!!!!!8_s1BOO_s2 "
+            /// "and u8N3NES19LogicalNodeFunctionE_s1 = i8_i8_s2 and !!!!f64Wsi8_s2 and !!!!!!!!!! Lists1 = i8_s2 and !!!!f64Wsi8_s2 "
+            /// "and !!!!!!!!!!!!8_s1BOO_s2 and u8N3NES19LogicalNodeFunctionE_s1 = i8_s2 and !!!!f64Wsi8_s2 "
+            /// "and !!!!!!!!!!!!8_sFunctionE_s1 = i8_s2 and !!!!f64Wsi8_s2 and !!!!!!!!!!!!8_s1BOO_s2 "
+            /// "and u8N3NES19LogicalNodeFunctionE_s1 = i8_i8_s2 and !!!!f64Wsi8_s2 and !!!!!!!!!!!!8_s1BOO_s2 "
+            /// "and u8N3NES19LogicalNodeFunctionE_s1 = i8_i8_s2 and !!!!f64Wsi8_s2 and !!!!!!!!!!!!8_s1BOO_s2 "
+            /// "and u8N3NES19LogicalNodeFunctionE_odeFunctionE_s1 = i8_s2 and !!!!f64Ws2 "
             "WINDOW TUMBLING (ts, size 1 sec) INTO sinkStream2Stream2"};
-        NES_INFO("foo");
         std::cout << "starting " << query.size() << std::endl;
         auto plan = NES::AntlrSQLQueryParser::createLogicalQueryPlanFromSQLString(query);
         std::cout << "done" << std::endl;
