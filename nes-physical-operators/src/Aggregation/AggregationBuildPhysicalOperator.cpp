cadced37d364bd019b7bcad47bad09d3a4403684
nikla44 <n.tantow@tu-berlin.de>
2025-05-31 17:26:59 +0200

    WIP

--- nes-physical-operators/src/Aggregation/AggregationBuildPhysicalOperator.cpp
+++ nes-physical-operators/src/Aggregation/AggregationBuildPhysicalOperator.cpp
@@ -73,8 +73,7 @@ Interface::HashMap* getAggHashMapProxy(
             return createdSlices;
         });
 
-    const auto hashMap = operatorHandler->getSliceAndWindowStore().getSlicesOrCreate(
-        timestamp, workerThreadId, JoinBuildSideType::Left, wrappedCreateFunction);
+    const auto hashMap = operatorHandler->getSliceAndWindowStore().getSlicesOrCreate(timestamp, wrappedCreateFunction);
     INVARIANT(
         hashMap.size() == 1,
         "We expect exactly one slice for the given timestamp during the AggregationBuild, as we currently solely support "
