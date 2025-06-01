c18e488a80f275c7d6d7466e4f2847e8f47f995c
lukas schwerdtfeger <lukas.schwerdtfeger@gmail.com>
2025-06-01 18:10:39 +0200

    fix(Execution): Fail if child operator has not been set
    Previously the physical operator would silently do nothing
    if no child operator has been set.

--- nes-physical-operators/src/PhysicalOperator.cpp
+++ nes-physical-operators/src/PhysicalOperator.cpp
@@ -27,6 +27,7 @@
 #include <Util/PlanRenderer.hpp>
 #include <fmt/format.h>
 #include <magic_enum/magic_enum.hpp>
+#include <ErrorHandling.hpp>
 #include <ExecutionContext.hpp>
 
 namespace NES
@@ -67,39 +68,29 @@ void PhysicalOperatorConcept::execute(ExecutionContext& executionCtx, Record& re
 
 void PhysicalOperatorConcept::setupChild(ExecutionContext& executionCtx) const
 {
-    if (const auto child = getChild())
-    {
-        child.value().setup(executionCtx);
-    }
+    INVARIANT(getChild().has_value(), "Child operator is not set");
+    getChild().value().setup(executionCtx);
 }
 void PhysicalOperatorConcept::openChild(ExecutionContext& executionCtx, RecordBuffer& recordBuffer) const
 {
-    if (const auto child = getChild())
-    {
-        child.value().open(executionCtx, recordBuffer);
-    }
+    INVARIANT(getChild().has_value(), "Child operator is not set");
+    getChild().value().open(executionCtx, recordBuffer);
 }
 void PhysicalOperatorConcept::closeChild(ExecutionContext& executionCtx, RecordBuffer& recordBuffer) const
 {
-    if (const auto child = getChild())
-    {
-        child.value().close(executionCtx, recordBuffer);
-    }
+    INVARIANT(getChild().has_value(), "Child operator is not set");
+    getChild().value().close(executionCtx, recordBuffer);
 }
 void PhysicalOperatorConcept::executeChild(ExecutionContext& executionCtx, Record& record) const
 {
-    if (const auto child = getChild())
-    {
-        child.value().execute(executionCtx, record);
-    }
+    INVARIANT(getChild().has_value(), "Child operator is not set");
+    getChild().value().execute(executionCtx, record);
 }
 
 void PhysicalOperatorConcept::terminateChild(ExecutionContext& executionCtx) const
 {
-    if (const auto child = getChild())
-    {
-        child.value().terminate(executionCtx);
-    }
+    INVARIANT(getChild().has_value(), "Child operator is not set");
+    getChild().value().terminate(executionCtx);
 }
 
 PhysicalOperator::PhysicalOperator() = default;
