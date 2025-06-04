34328b1eda39d40166063dde55ea7e3c7a20702c
Matthis Gördel <matthis@posteo.de>
2025-06-04 19:01:13 +0200

    vcpkg: refactor custom triplets

--- vcpkg/custom-triplets/bits/arch/x64.cmake
+++ vcpkg/custom-triplets/bits/arch/x64.cmake
@@ -10,6 +10,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-include(${CMAKE_CURRENT_LIST_DIR}/bits/arch/x64.cmake)
-
-set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE ${CMAKE_CURRENT_LIST_DIR}/toolchains/libcxx.cmake)
+set(VCPKG_TARGET_ARCHITECTURE x64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE static)
+set(VCPKG_CMAKE_SYSTEM_NAME Linux)
