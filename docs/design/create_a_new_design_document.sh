1caf5d7859f7d48c9ef7057b0b3412c7c9bbc066
Matthis Gördel <matthis@posteo.de>
2025-06-05 15:28:53 +0200

    add missing license preambles

--- docs/design/create_a_new_design_document.sh
+++ docs/design/create_a_new_design_document.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#    https://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 # This script takes a mandatory design document title.
 # It interprets all arguments as the title string, so quotes are not required.
 # It then creates a new file using the following schema: `YYYYMMDD_NAME_OF_THE_DESIGN_DOCUMENT.md`
