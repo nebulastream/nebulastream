af9ea620e7f16a4155d36dd4130e4dabce497c69
Tim Nacken <timnicknacken@gmail.com>
2025-06-06 14:53:09 +0200

    removed not used functions

--- nes-common/include/Util/Common.hpp
+++ nes-common/include/Util/Common.hpp
@@ -47,65 +47,7 @@ inline std::ostream& operator<<(std::ostream& os, const VerbosityLevel& level)
 
 namespace NES::Util
 {
-/// @brief escapes all non text characters in a input string, such that the string could be processed as json.
-std::string escapeJson(const std::string& str);
 
-/// Update the source names by sorting and then concatenating the source names from the sub- and query plan
-std::string updateSourceName(std::string queryPlanSourceConsumed, std::string subQueryPlanSourceConsumed);
-
-/// Truncates the file and then writes the header string as is to the file
-void writeHeaderToCsvFile(const std::string& csvFileName, const std::string& header);
-
-/// Appends the row as is to the csv file
-void writeRowToCsvFile(const std::string& csvFileName, const std::string& row);
-
-/// Partition a vector in n chunks, e.g., ([1, 2, 3, 4, 5], 3) -> [[1, 2], [3, 4], [5]]
-template <typename T>
-std::vector<std::vector<T>> partition(const std::vector<T>& vec, size_t n)
-{
-    std::vector<std::vector<T>> outVec;
-    size_t length = vec.size() / n;
-    size_t remain = vec.size() % n;
-
-    size_t begin = 0;
-    size_t end = 0;
-    for (size_t i = 0; i < std::min(n, vec.size()); ++i)
-    {
-        end += (remain > 0) ? (length + !!(remain--)) : length;
-        outVec.push_back(std::vector<T>(vec.begin() + begin, vec.begin() + end));
-        begin = end;
-    }
-    return outVec;
-}
-
-/// appends newValue until the vector contains a minimum of newSize elements
-template <typename T>
-void padVectorToSize(std::vector<T>& vector, size_t newSize, T newValue)
-{
-    while (vector.size() < newSize)
-    {
-        vector.push_back(newValue);
-    }
-}
-
-/// hashes the key with murmur hash
-uint64_t murmurHash(uint64_t key);
-
-/// Counts the number of lines of a string
-uint64_t countLines(const std::string& str);
-
-/// Counts the number of lines of a stream, e.g., a file
-uint64_t countLines(std::istream& stream);
-
-/// Tries to update curVal until it succeeds or curVal is larger then newVal
-template <typename T>
-void updateAtomicMax(std::atomic<T>& curVal, const T& newVal)
-{
-    T prev_value = curVal;
-    while (prev_value < newVal && !curVal.compare_exchange_weak(prev_value, newVal))
-    {
-    }
-};
 
 /// check if the given object is an instance of the specified type.
 template <typename Out, typename In>
