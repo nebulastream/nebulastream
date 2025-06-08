31128eea42e543961081831e415b7107fa419696
Eric Benschneider <eric.benschneider@tu-berlin.de>
2025-06-08 09:04:37 +0000

    added layout swapping to selection operator

--- nes-query-optimizer/src/RewriteRules/LowerToPhysical/LowerToPhysicalSelection.cpp
+++ nes-query-optimizer/src/RewriteRules/LowerToPhysical/LowerToPhysicalSelection.cpp
@@ -23,6 +23,12 @@
 #include <RewriteRuleRegistry.hpp>
 #include <SelectionPhysicalOperator.hpp>
 
+#include <EmitPhysicalOperator.hpp>
+#include <ScanPhysicalOperator.hpp>
+#include <Nautilus/Interface/MemoryProvider/ColumnTupleBufferMemoryProvider.hpp>
+#include <Nautilus/Interface/MemoryProvider/RowTupleBufferMemoryProvider.hpp>
+
+
 namespace NES
 {
 
@@ -33,6 +39,66 @@ RewriteRuleResultSubgraph LowerToPhysicalSelection::apply(LogicalOperator logica
     auto function = selection.getPredicate();
     auto func = QueryCompilation::FunctionProvider::lowerFunction(function);
     auto physicalOperator = SelectionPhysicalOperator(func);
+    auto inputSchema = logicalOperator.getInputSchemas()[0];
+    auto outputSchema = logicalOperator.getOutputSchema();
+    if (conf.useSingleMemoryLayout.getValue() &&conf.memoryLayout.getValue() != conf.memoryLayout.getDefaultValue())
+    {
+
+        auto schema = *std::make_shared<Schema>(logicalOperator.getInputSchemas()[0]);
+        auto colSchema = *std::make_shared<Schema>(schema); //hopefully deep copy
+        colSchema.memoryLayoutType = Schema::MemoryLayoutType::COLUMNAR_LAYOUT;
+
+        auto rowLayout = std::make_shared<Memory::MemoryLayouts::RowLayout>(
+            conf.pageSize.getValue(), schema);
+        auto rowMemoryProvider = std::make_shared<Nautilus::Interface::MemoryProvider::RowTupleBufferMemoryProvider>(rowLayout);
+        auto handlerId = getNextOperatorHandlerId();
+        auto scanRow = ScanPhysicalOperator(rowMemoryProvider, schema.getFieldNames());
+        auto emitRow = EmitPhysicalOperator(handlerId, rowMemoryProvider);
+
+        auto scanWrapperRow = std::make_shared<PhysicalOperatorWrapper>(
+            scanRow, schema, schema, PhysicalOperatorWrapper::PipelineLocation::INTERMEDIATE);
+
+        auto emitWrapperRow = std::make_shared<PhysicalOperatorWrapper>(
+            emitRow, schema, schema, PhysicalOperatorWrapper::PipelineLocation::INTERMEDIATE);
+
+        auto columnLayout = std::make_shared<Memory::MemoryLayouts::ColumnLayout>(
+                   conf.pageSize.getValue(), schema);
+        auto columnMemoryProvider = std::make_shared<Nautilus::Interface::MemoryProvider::ColumnTupleBufferMemoryProvider>(columnLayout);
+        auto newHandlerId = getNextOperatorHandlerId();
+        auto scanCol = ScanPhysicalOperator(columnMemoryProvider, colSchema.getFieldNames());
+        auto emitCol = EmitPhysicalOperator(newHandlerId, columnMemoryProvider);
+
+
+
+        auto scanWrapperCol = std::make_shared<PhysicalOperatorWrapper>(
+            scanCol, colSchema, colSchema, PhysicalOperatorWrapper::PipelineLocation::INTERMEDIATE);
+        auto emitWrapperCol = std::make_shared<PhysicalOperatorWrapper>(
+            emitCol, colSchema, colSchema, PhysicalOperatorWrapper::PipelineLocation::INTERMEDIATE);
+
+
+        inputSchema.memoryLayoutType = conf.memoryLayout.getValue();///TODO: do for all operators besides sink and source
+        outputSchema.memoryLayoutType = conf.memoryLayout.getValue();
+
+        auto wrapper = std::make_shared<PhysicalOperatorWrapper>(
+        physicalOperator,
+        inputSchema,
+        outputSchema,
+        PhysicalOperatorWrapper::PipelineLocation::INTERMEDIATE);
+
+
+        emitWrapperRow->addChild(scanWrapperCol);
+        scanWrapperCol->addChild(wrapper);
+        wrapper->addChild(emitWrapperCol);
+        emitWrapperCol->addChild(scanWrapperRow);
+
+        // scanWrapperRow->addChild(emitWrapperCol);
+        // emitWrapperCol->addChild(wrapper);
+        // wrapper->addChild(scanWrapperCol);
+        // scanWrapperCol->addChild(emitWrapperRow);
+        return {.root = emitWrapperRow, .leafs = {scanWrapperRow}};
+
+    }
+
     auto wrapper = std::make_shared<PhysicalOperatorWrapper>(
         physicalOperator,
         logicalOperator.getInputSchemas()[0],
