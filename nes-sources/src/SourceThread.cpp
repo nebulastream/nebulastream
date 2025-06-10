4750ae8c3a8fd3f556dcf912cb343a4645b8b7aa
lukas schwerdtfeger <lukas.schwerdtfeger@gmail.com>
2025-06-10 09:40:43 +0200

    fix(Source): Fixes unresponsiveness when waiting for buffers

--- nes-sources/src/SourceThread.cpp
+++ nes-sources/src/SourceThread.cpp
@@ -118,7 +118,13 @@ dataSourceThreadRoutine(const std::stop_token& stopToken, Source& source, Abstra
         ///    The thread exits with `EndOfStream`
         /// 4. Failure. The fillTupleBuffer method will throw an exception, the exception is propagted to the SourceThread via the return promise.
         ///    The thread exists with an exception
-        auto emptyBuffer = bufferProvider.getBufferBlocking();
+        auto emptyBufferOpt = bufferProvider.getBufferWithTimeout(std::chrono::milliseconds(25));
+        if (!emptyBufferOpt)
+        {
+            continue;
+        }
+        auto emptyBuffer = *emptyBufferOpt;
+
         const auto numReadBytes = source.fillTupleBuffer(emptyBuffer, stopToken);
 
         if (numReadBytes != 0)
