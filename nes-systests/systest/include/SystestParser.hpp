keyseven123/engine-statemanagement-paper

chore(SysTest): Renamed SystestParser::Sink to SystestSink

nes-systests/systest/include/SystestParser.hpp
@@ -70,11 +70,11 @@ public:
         bool operator==(const SLTSource& other) const = default;
     };
 
-    struct Sink
+    struct SystestSink
     {
         std::string name;
         SystestSchema fields;
-        bool operator==(const Sink& other) const = default;
+        bool operator==(const SystestSink& other) const = default;
     };
 
     using ResultTuples = std::vector<std::string>;
@@ -83,13 +83,13 @@ public:
     using ResultTuplesCallback = std::function<void(ResultTuples&&)>;
     using SLTSourceCallback = std::function<void(SLTSource&&)>;
     using AttachSourceCallback = std::function<void(SystestAttachSource attachSource)>;
-    using SinkCallback = std::function<void(Sink&&)>;
+    using SystestSinkCallback = std::function<void(SystestSink&&)>;
 
     /// Register callbacks to be called when the respective section is parsed
     void registerOnQueryCallback(QueryCallback callback);
     void registerOnSLTSourceCallback(SLTSourceCallback callback);
     void registerOnAttachSourceCallback(AttachSourceCallback callback);
-    void registerOnSinkCallBack(SinkCallback callback);
+    void registerOnSystestSystestSinkCallback(SystestSinkCallback callback);
 
 
     void parse(SystestStarterGlobals& systestStarterGlobals, std::string_view testFileName);
@@ -109,7 +109,7 @@ private:
 
     [[nodiscard]] SLTSource expectSLTSource();
     [[nodiscard]] SystestAttachSource expectAttachSource();
-    [[nodiscard]] Sink expectSink() const;
+    [[nodiscard]] SystestSink expectSink() const;
     [[nodiscard]] ResultTuples expectTuples(bool ignoreFirst = false);
     [[nodiscard]] std::filesystem::path expectFilePath();
     [[nodiscard]] std::string expectQuery();
@@ -117,7 +117,7 @@ private:
     QueryCallback onQueryCallback;
     SLTSourceCallback onSLTSourceCallback;
     AttachSourceCallback onAttachSourceCallback;
-    SinkCallback onSinkCallback;
+    SystestSinkCallback onSystestSinkCallback;
 
     bool firstToken = true;
     size_t currentLine = 0;

nes-systests/systest/src/SystestParser.cpp
@@ -203,9 +203,9 @@ void SystestParser::registerOnAttachSourceCallback(AttachSourceCallback callback
     this->onAttachSourceCallback = std::move(callback);
 }
 
-void SystestParser::registerOnSinkCallBack(SinkCallback callback)
+void SystestParser::registerOnSystestSystestSinkCallback(SystestSinkCallback callback)
 {
-    this->onSinkCallback = std::move(callback);
+    this->onSystestSinkCallback = std::move(callback);
 }
 
 void SystestParser::parse(SystestStarterGlobals& systestStarterGlobals, const std::string_view testFileName)
@@ -232,9 +232,9 @@ void SystestParser::parse(SystestStarterGlobals& systestStarterGlobals, const st
             }
             case TokenType::SINK: {
                 auto sink = expectSink();
-                if (onSinkCallback)
+                if (onSystestSinkCallback)
                 {
-                    onSinkCallback(std::move(sink));
+                    onSystestSinkCallback(std::move(sink));
                 }
                 break;
             }
@@ -331,11 +331,11 @@ std::optional<TokenType> SystestParser::nextToken()
     return getTokenIfValid(potentialToken);
 }
 
-SystestParser::Sink SystestParser::expectSink() const
+SystestParser::SystestSink SystestParser::expectSink() const
 {
     INVARIANT(currentLine < lines.size(), "current parse line should exist");
 
-    Sink sink;
+    SystestSink sink;
     const auto& line = lines[currentLine];
     std::istringstream lineAsStream(line);
 

nebulastream/nebulastream

nes-systests/systest/include/SystestParser.hpp
@@ -79,11 +79,11 @@ public:
         bool operator==(const SLTSource& other) const = default;
     };
 
-    struct Sink
+    struct SystestSink
     {
         std::string name;
         SystestSchema fields;
-        bool operator==(const Sink& other) const = default;
+        bool operator==(const SystestSink& other) const = default;
     };
 
     struct ErrorExpectation
@@ -97,7 +97,7 @@ public:
     using ResultTuplesCallback = std::function<void(std::vector<std::string>&&, SystestQueryId correspondingQueryId)>;
     using SLTSourceCallback = std::function<void(const SLTSource&)>;
     using CSVSourceCallback = std::function<void(const CSVSource&)>;
-    using SinkCallback = std::function<void(Sink&&)>;
+    using SystestSinkCallback = std::function<void(SystestSink&&)>;
     using ErrorExpectationCallback = std::function<void(const ErrorExpectation&)>;
 
     /// Register callbacks to be called when the respective section is parsed
@@ -105,7 +105,7 @@ public:
     void registerOnResultTuplesCallback(ResultTuplesCallback callback);
     void registerOnSLTSourceCallback(SLTSourceCallback callback);
     void registerOnCSVSourceCallback(CSVSourceCallback callback);
-    void registerOnSinkCallBack(SinkCallback callback);
+    void registerOnSystestSinkCallback(SystestSinkCallback callback);
     void registerOnErrorExpectationCallback(ErrorExpectationCallback callback);
 
     void parse();
@@ -126,7 +126,7 @@ private:
 
     [[nodiscard]] SLTSource expectSLTSource();
     [[nodiscard]] CSVSource expectCSVSource() const;
-    [[nodiscard]] Sink expectSink() const;
+    [[nodiscard]] SystestSink expectSink() const;
     [[nodiscard]] std::vector<std::string> expectTuples(bool ignoreFirst);
     [[nodiscard]] std::string expectQuery();
     [[nodiscard]] ErrorExpectation expectError() const;
@@ -135,7 +135,7 @@ private:
     ResultTuplesCallback onResultTuplesCallback;
     SLTSourceCallback onSLTSourceCallback;
     CSVSourceCallback onCSVSourceCallback;
-    SinkCallback onSinkCallback;
+    SystestSinkCallback onSystestSinkCallback;
     ErrorExpectationCallback onErrorExpectationCallback;
 
     bool firstToken = true;

nes-systests/systest/src/SystestParser.cpp
@@ -34,7 +34,6 @@
 #include <fmt/ranges.h>
 #include <magic_enum/magic_enum.hpp>
 #include <ErrorHandling.hpp>
-#include <SystestParser.hpp>
 #include <SystestState.hpp>
 
 namespace
@@ -168,9 +167,9 @@ void SystestParser::registerOnSLTSourceCallback(SLTSourceCallback callback)
     this->onSLTSourceCallback = std::move(callback);
 }
 
-void SystestParser::registerOnSinkCallBack(SinkCallback callback)
+void SystestParser::registerOnSystestSinkCallback(SystestSinkCallback callback)
 {
-    this->onSinkCallback = std::move(callback);
+    this->onSystestSinkCallback = std::move(callback);
 }
 
 void SystestParser::registerOnCSVSourceCallback(CSVSourceCallback callback)
@@ -209,9 +208,9 @@ void SystestParser::parse()
             }
             case TokenType::SINK: {
                 auto sink = expectSink();
-                if (onSinkCallback)
+                if (onSystestSinkCallback)
                 {
-                    onSinkCallback(std::move(sink));
+                    onSystestSinkCallback(std::move(sink));
                 }
                 break;
             }
@@ -350,11 +349,11 @@ std::optional<TokenType> SystestParser::peekToken() const
     return getTokenIfValid(potentialToken);
 }
 
-SystestParser::Sink SystestParser::expectSink() const
+SystestParser::SystestSink SystestParser::expectSink() const
 {
     INVARIANT(currentLine < lines.size(), "current parse line should exist");
 
-    Sink sink;
+    SystestSink sink;
     const auto& line = lines[currentLine];
     std::istringstream lineAsStream(line);
 