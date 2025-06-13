keyseven123/engine-statemanagement-paper

chore(SysTest): Renamed SystestParser::Source to SystestLogicalSource

nes-systests/systest/include/SystestParser.hpp
@@ -63,11 +63,11 @@ public:
     [[nodiscard]] bool loadString(const std::string& str);
 
     /// Type definitions ///
-    struct SLTSource
+    struct SystestLogicalSource
     {
         std::string name;
         SystestSchema fields;
-        bool operator==(const SLTSource& other) const = default;
+        bool operator==(const SystestLogicalSource& other) const = default;
     };
 
     struct SystestSink
@@ -81,13 +81,13 @@ public:
 
     using QueryCallback = std::function<void(std::string, size_t)>;
     using ResultTuplesCallback = std::function<void(ResultTuples&&)>;
-    using SLTSourceCallback = std::function<void(SLTSource&&)>;
+    using SystestLogicalSourceCallback = std::function<void(SystestLogicalSource&&)>;
     using AttachSourceCallback = std::function<void(SystestAttachSource attachSource)>;
     using SystestSinkCallback = std::function<void(SystestSink&&)>;
 
     /// Register callbacks to be called when the respective section is parsed
     void registerOnQueryCallback(QueryCallback callback);
-    void registerOnSLTSourceCallback(SLTSourceCallback callback);
+    void registerOnSystestLogicalSourceCallback(SystestLogicalSourceCallback callback);
     void registerOnAttachSourceCallback(AttachSourceCallback callback);
     void registerOnSystestSystestSinkCallback(SystestSinkCallback callback);
 
@@ -107,7 +107,7 @@ private:
     /// Got the next token. Returns false if reached end of file.
     [[nodiscard]] bool moveToNextToken();
 
-    [[nodiscard]] SLTSource expectSLTSource();
+    [[nodiscard]] SystestLogicalSource expectSystestLogicalSource();
     [[nodiscard]] SystestAttachSource expectAttachSource();
     [[nodiscard]] SystestSink expectSink() const;
     [[nodiscard]] ResultTuples expectTuples(bool ignoreFirst = false);
@@ -115,7 +115,7 @@ private:
     [[nodiscard]] std::string expectQuery();
 
     QueryCallback onQueryCallback;
-    SLTSourceCallback onSLTSourceCallback;
+    SystestLogicalSourceCallback onSystestLogicalSourceCallback;
     AttachSourceCallback onAttachSourceCallback;
     SystestSinkCallback onSystestSinkCallback;
 

nebulastream/nebulastream

nes-systests/systest/include/SystestParser.hpp
@@ -71,12 +71,12 @@ public:
         bool operator==(const CSVSource& other) const = default;
     };
 
-    struct SLTSource
+    struct SystestLogicalSource
     {
         std::string name;
         SystestSchema fields;
         std::vector<std::string> tuples;
-        bool operator==(const SLTSource& other) const = default;
+        bool operator==(const SystestLogicalSource& other) const = default;
     };
 
     struct SystestSink
@@ -95,15 +95,15 @@ public:
 
     using QueryCallback = std::function<void(std::string, SystestQueryId)>;
     using ResultTuplesCallback = std::function<void(std::vector<std::string>&&, SystestQueryId correspondingQueryId)>;
-    using SLTSourceCallback = std::function<void(const SLTSource&)>;
-    using CSVSourceCallback = std::function<void(const CSVSource&)>;
+    using SystestLogicalSourceCallback = std::function<void(const SystestLogicalSource&)>;
+    using CSVSourceCallback = std::function<void(CSVSource&&)>;
     using SystestSinkCallback = std::function<void(SystestSink&&)>;
     using ErrorExpectationCallback = std::function<void(const ErrorExpectation&)>;
 
     /// Register callbacks to be called when the respective section is parsed
     void registerOnQueryCallback(QueryCallback callback);
     void registerOnResultTuplesCallback(ResultTuplesCallback callback);
-    void registerOnSLTSourceCallback(SLTSourceCallback callback);
+    void registerOnSystestLogicalSourceCallback(SystestLogicalSourceCallback callback);
     void registerOnCSVSourceCallback(CSVSourceCallback callback);
     void registerOnSystestSinkCallback(SystestSinkCallback callback);
     void registerOnErrorExpectationCallback(ErrorExpectationCallback callback);
@@ -124,7 +124,7 @@ private:
     /// Look ahead at the next token without consuming it
     [[nodiscard]] std::optional<TokenType> peekToken() const;
 
-    [[nodiscard]] SLTSource expectSLTSource();
+    [[nodiscard]] SystestLogicalSource expectSystestLogicalSource();
     [[nodiscard]] CSVSource expectCSVSource() const;
     [[nodiscard]] SystestSink expectSink() const;
     [[nodiscard]] std::vector<std::string> expectTuples(bool ignoreFirst);
@@ -133,7 +133,7 @@ private:
 
     QueryCallback onQueryCallback;
     ResultTuplesCallback onResultTuplesCallback;
-    SLTSourceCallback onSLTSourceCallback;
+    SystestLogicalSourceCallback onSystestLogicalSourceCallback;
     CSVSourceCallback onCSVSourceCallback;
     SystestSinkCallback onSystestSinkCallback;
     ErrorExpectationCallback onErrorExpectationCallback;