5d1499a9b5388a58dbf3189c8fcd4afc51c71517
Tim Nacken <timnicknacken@gmail.com>
2025-06-27 20:51:49 +0200

    chore(config): changed config to snake case
    Changed back test udate and changed UIntOptions

--- nes-runtime/include/Configuration/WorkerConfiguration.hpp
+++ nes-runtime/include/Configuration/WorkerConfiguration.hpp
@@ -40,7 +40,7 @@ public:
     StringOption localWorkerHost = {"local_worker_host", "127.0.0.1", "Worker IP or hostname."};
 
     /// Port for the RPC server of the Worker. This is used to receive control messages from the coordinator or other workers .
-    UIntOption rpcPort = {"rpcPort", "0", "RPC server port of the NES Worker.", {std::make_shared<NumberValidation>()}};
+    UIntOption rpcPort = {"rpc_port", "0", "RPC server port of the NES Worker.", {std::make_shared<NumberValidation>()}};
 
     EnumOption<LogLevel> logLevel
         = {"log_level", LogLevel::LOG_INFO, "The log level (LOG_NONE, LOG_WARNING, LOG_DEBUG, LOG_INFO, LOG_TRACE)"};
@@ -49,21 +49,21 @@ public:
 
     /// The number of buffers in the global buffer manager. Controls how much memory is consumed by the system.
     UIntOption numberOfBuffersInGlobalBufferManager
-        = {"numberOfBuffersInGlobalBufferManager", "1024", "Number buffers in global buffer pool.", {std::make_shared<NumberValidation>()}};
+        = {"number_of_buffers_in_global_buffer_manager", "1024", "Number buffers in global buffer pool.", {std::make_shared<NumberValidation>()}};
 
     UIntOption numberOfBuffersPerWorker
-        = {"numberOfBuffersPerWorker", "128", "Number buffers in task local buffer pool.", {std::make_shared<NumberValidation>()}};
+        = {"number_of_buffers_per_worker", "128", "Number buffers in task local buffer pool.", {std::make_shared<NumberValidation>()}};
 
 
     /// Indicates how many buffers a single data source can allocate. This property controls the backpressure mechanism as a data source that can't allocate new records can't ingest more data.
     UIntOption numberOfBuffersInSourceLocalPools
-        = {"numberOfBuffersInSourceLocalPools",
+        = {"number_of_buffers_in_source_local_pools",
            "64",
            "Number buffers in source local buffer pool. May be overwritten by a source-specific configuration (see SourceDescriptor).",
            {std::make_shared<NumberValidation>()}};
 
     /// Configures the buffer size of individual TupleBuffers in bytes. This property has to be the same over a whole deployment.
-    UIntOption bufferSizeInBytes = {"bufferSizeInBytes", "4096", "BufferSizeInBytes.", {std::make_shared<NumberValidation>()}};
+    UIntOption bufferSizeInBytes = {"buffer_size_in_bytes", "4096", "BufferSizeInBytes.", {std::make_shared<NumberValidation>()}};
 
     QueryOptimizerConfiguration queryOptimizer = {"queryOptimizer", "Configuration for the query optimizer"};
     StringOption configPath = {CONFIG_PATH, "", "Path to configuration file."};
