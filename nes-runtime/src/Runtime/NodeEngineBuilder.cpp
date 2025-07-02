e944c3830924e3eb04ac65644cb6f27cfd6c3109
Adrian Michalke <michalke@tu-berlin.de>
2025-07-02 16:24:16 +0200

    chore: cleanup WorkerConfiguration

--- nes-runtime/src/Runtime/NodeEngineBuilder.cpp
+++ nes-runtime/src/Runtime/NodeEngineBuilder.cpp
@@ -47,8 +47,7 @@ std::unique_ptr<NodeEngine> NodeEngineBuilder::build()
     auto queryLog = std::make_shared<QueryLog>();
 
 
-    auto queryEngine
-        = std::make_unique<QueryEngine>(workerConfiguration.queryEngineConfiguration, statisticEventListener, queryLog, bufferManager);
+    auto queryEngine = std::make_unique<QueryEngine>(workerConfiguration.queryEngine, statisticEventListener, queryLog, bufferManager);
 
     return std::make_unique<NodeEngine>(
         std::move(bufferManager),
