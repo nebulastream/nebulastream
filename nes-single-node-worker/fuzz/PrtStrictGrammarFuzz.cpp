cba67526d4059d2b193e6cfa962937886e25ee9b
Matthis Gördel <matthis@posteo.de>
2025-07-04 18:11:17 +0200

    WIP: strict-fuzz: always CSV+FILE src

--- nes-single-node-worker/fuzz/PrtStrictGrammarFuzz.cpp
+++ nes-single-node-worker/fuzz/PrtStrictGrammarFuzz.cpp
@@ -98,11 +98,44 @@ LogicalFunction toFn(const FPredicate& pred)
     throw CannotDeserialize("foo");
 }
 
+SourceDescriptor toSd(const SerializableSourceDescriptor& sourceDescriptor)
+{
+    auto schema = SchemaSerializationUtil::deserializeSchema(sourceDescriptor.sourceschema());
+    const LogicalSource logicalSource{sourceDescriptor.logicalsourcename(), std::make_shared<Schema>(schema)};
+
+    /// TODO #815 the serializer would also a catalog to register/create source descriptors/logical sources
+    const auto physicalSourceId = PhysicalSourceId{sourceDescriptor.physicalsourceid()};
+    const auto& sourceType = "File"s;
+    const auto workerIdInt = sourceDescriptor.workerid();
+    const auto workerId = WorkerId{workerIdInt};
+    const auto buffersInLocalPool = sourceDescriptor.numberofbuffersinlocalpool();
+
+    /// Deserialize the parser config.
+    const auto& serializedParserConfig = sourceDescriptor.parserconfig();
+    auto deserializedParserConfig = ParserConfig{.parserType = "CSV", .tupleDelimiter = "\n", .fieldDelimiter = ","};
+
+    /// Deserialize SourceDescriptor config. Convert from protobuf variant to SourceDescriptor::ConfigType.
+    NES::Configurations::DescriptorConfig::Config sourceDescriptorConfig{};
+    for (const auto& [key, value] : sourceDescriptor.config())
+    {
+        sourceDescriptorConfig[key] = NES::Configurations::protoToDescriptorConfigType(value);
+    }
+
+    return SourceDescriptor{
+        logicalSource,
+        physicalSourceId,
+        workerId,
+        sourceType,
+        buffersInLocalPool,
+        (std::move(sourceDescriptorConfig)),
+        deserializedParserConfig};
+}
+
 LogicalOperator toOp(const FOp& op)
 {
     if (op.has_source())
     {
-        auto src = OperatorSerializationUtil::deserializeSourceDescriptor(op.source());
+        auto src = toSd(op.source());
         auto srcOp = SourceDescriptorLogicalOperator{std::move(src)};
         return srcOp.withOutputOriginIds({OriginId{op.outputoriginid()}});
     }
