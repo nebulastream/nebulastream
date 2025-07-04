9988138c65ef10bff061aecaa10c36eb3253410d
Grigorii Turchenko <turchenko.gregory@gmail.com>
2025-07-04 16:39:39 +0200

    add BatchingLogicalOperator

--- nes-nebuli/src/LegacyOptimizer/ModelInferenceCompilationRule.cpp
+++ nes-nebuli/src/LegacyOptimizer/ModelInferenceCompilationRule.cpp
@@ -16,6 +16,8 @@
 #include <utility>
 
 #include <LegacyOptimizer/ModelInferenceCompilationRule.hpp>
+#include "Operators/Windows/BatchingLogicalOperator.hpp"
+#include "WindowTypes/Types/CountBasedWindow.hpp"
 
 #include <InferModelLogicalOperator.hpp>
 #include <InferModelNameLogicalOperator.hpp>
@@ -35,11 +37,21 @@ void ModelInferenceCompilationRule::apply(LogicalPlan& queryPlan)
 
     for (auto modelNameOperator : NES::getOperatorByType<InferModel::InferModelNameLogicalOperator>(queryPlan))
     {
+        auto countBasedWindow = std::make_shared<Windowing::CountBasedWindow>(1);
+        auto batching = BatchingLogicalOperator(countBasedWindow);
+
         auto name = modelNameOperator.getModelName();
         auto model = catalog->load(name);
-        USED_IN_DEBUG auto shouldReplace = replaceOperator(
-            queryPlan, modelNameOperator, InferModel::InferModelLogicalOperator(model, modelNameOperator.getInputFields()));
-        queryPlan = std::move(shouldReplace.value());
+        auto inferModel = InferModel::InferModelLogicalOperator(model, modelNameOperator.getInputFields());
+        // USED_IN_DEBUG auto shouldReplace = replaceOperator(
+        //     queryPlan, modelNameOperator, InferModel::InferModelLogicalOperator(model, modelNameOperator.getInputFields()));
+        // queryPlan = std::move(shouldReplace.value());
+
+        queryPlan = replaceSubtree(
+                                queryPlan,
+                                modelNameOperator,
+                                inferModel.withChildren({batching.withChildren(modelNameOperator.getChildren())}))
+                                .value();
     }
 
     NES_DEBUG("ModelInferenceCompilationRule: Plan after\n{}", queryPlan);
