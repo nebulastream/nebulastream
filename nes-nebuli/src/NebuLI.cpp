60b99f297b89fb72c3051d9f5a0ebcd20a34700d
lukas schwerdtfeger <lukas.schwerdtfeger@gmail.com>
2025-07-07 18:05:51 +0200

    feat(Nebuli): Adds rule to eliminate redundant projections

--- nes-nebuli/src/NebuLI.cpp
+++ nes-nebuli/src/NebuLI.cpp
@@ -28,6 +28,7 @@
 #include <Identifiers/Identifiers.hpp>
 #include <LegacyOptimizer/LogicalSourceExpansionRule.hpp>
 #include <LegacyOptimizer/OriginIdInferencePhase.hpp>
+#include <LegacyOptimizer/RedundantProjectionRemovalRule.hpp>
 #include <LegacyOptimizer/RedundantUnionRemovalRule.hpp>
 #include <LegacyOptimizer/SourceInferencePhase.hpp>
 #include <LegacyOptimizer/TypeInferencePhase.hpp>
@@ -50,6 +51,7 @@ LogicalPlan LegacyOptimizer::optimize(const LogicalPlan& plan) const
     constexpr auto typeInference = NES::LegacyOptimizer::TypeInferencePhase{};
     constexpr auto originIdInferencePhase = NES::LegacyOptimizer::OriginIdInferencePhase{};
     constexpr auto redundantUnionRemovalRule = NES::LegacyOptimizer::RedundantUnionRemovalRule{};
+    constexpr auto redundantProjectionRemovalRule = NES::LegacyOptimizer::RedundantProjectionRemovalRule{};
 
     sourceInference.apply(newPlan);
     logicalSourceExpansionRule.apply(newPlan);
@@ -57,6 +59,9 @@ LogicalPlan LegacyOptimizer::optimize(const LogicalPlan& plan) const
     redundantUnionRemovalRule.apply(newPlan);
     NES_INFO("After Redundant Union Removal:\n{}", newPlan);
     typeInference.apply(newPlan);
+
+    redundantProjectionRemovalRule.apply(newPlan);
+    NES_INFO("After Redundant Projection Removal:\n{}", newPlan);
 
     originIdInferencePhase.apply(newPlan);
     typeInference.apply(newPlan);
