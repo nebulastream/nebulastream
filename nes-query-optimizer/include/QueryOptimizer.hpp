4259cfc238ff540a37c9735b91f299e5846304dc
Adrian Michalke <michalke@tu-berlin.de>
2025-07-09 15:58:00 +0200

    refactor: apply clang-tidy suggestions

--- nes-query-optimizer/include/QueryOptimizer.hpp
+++ nes-query-optimizer/include/QueryOptimizer.hpp
@@ -29,7 +29,7 @@ public:
         : defaultQueryExecution(std::move(defaultQueryExecution)) { };
     /// Takes the query plan as a logical plan and returns a fully physical plan
     [[nodiscard]] PhysicalPlan optimize(const LogicalPlan& plan);
-    [[nodiscard]] PhysicalPlan optimize(const LogicalPlan& plan, QueryExecutionConfiguration queryExecutionConfiguration);
+    [[nodiscard]] static PhysicalPlan optimize(const LogicalPlan& plan, const QueryExecutionConfiguration& defaultQueryExecution);
 
 private:
     QueryExecutionConfiguration defaultQueryExecution;
