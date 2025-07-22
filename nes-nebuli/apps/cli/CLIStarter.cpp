d97fdc9eccd413e5a92309ad943708b3d841f9e1
Yanikovic <schroeder_yannik@web.de>
2025-07-22 09:00:30 +0200

    feat(Distributed): adds network source/sink
    Signed-off-by: lukas schwerdtfeger <lukas.schwerdtfeger@gmail.com>

--- nes-nebuli/apps/cli/CLIStarter.cpp
+++ nes-nebuli/apps/cli/CLIStarter.cpp
@@ -423,6 +423,7 @@ void doStop(NES::QueryStatementHandler& queryStatementHandler, const std::unorde
     std::cout << result.dump(4) << '\n';
 }
 
+NES::HostAddr hostAddr{"localhost:9090"};
 NES::GrpcAddr grpcAddr{"localhost:8080"};
 
 NES::UniquePtr<NES::GRPCQuerySubmissionBackend> createGRPCBackend(const argparse::ArgumentParser& program)
@@ -437,7 +438,7 @@ NES::UniquePtr<NES::GRPCQuerySubmissionBackend> createGRPCBackend(const argparse
         return std::make_unique<NES::GRPCQuerySubmissionBackend>(NES::WorkerConfig{.grpc = NES::GrpcAddr(env)});
     }
 
-    return std::make_unique<NES::GRPCQuerySubmissionBackend>(NES::WorkerConfig{.grpc = grpcAddr});
+    return std::make_unique<NES::GRPCQuerySubmissionBackend>(NES::WorkerConfig{.host = hostAddr, .grpc = grpcAddr});
 }
 
 void doQueryManagement(const argparse::ArgumentParser& program, const argparse::ArgumentParser& subcommand)
