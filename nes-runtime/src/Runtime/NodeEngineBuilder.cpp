keyseven123/engine-statemanagement-paper

copy everything from niklas-bachelor-thesis

nes-runtime/src/Runtime/NodeEngineBuilder.cpp
@@ -30,15 +30,14 @@ namespace NES
 
 
 NodeEngineBuilder::NodeEngineBuilder(
-    const NES::Configurations::WorkerConfiguration& workerConfiguration,
+    const Configurations::WorkerConfiguration& workerConfiguration,
     std::shared_ptr<SystemEventListener> systemEventListener,
-    std::shared_ptr<QueryEngineStatisticListener> statisticEventListener)
+    std::vector<std::shared_ptr<QueryEngineStatisticListener>> statisticListener)
     : workerConfiguration(workerConfiguration)
     , systemEventListener(std::move(std::move(systemEventListener)))
-    , statisticEventListener(std::move(std::move(statisticEventListener)))
+    , statisticEventListener(std::move(std::move(statisticListener)))
 {
 }
-
 
 std::unique_ptr<NodeEngine> NodeEngineBuilder::build()
 {

nebulastream/nebulastream

nes-runtime/src/Runtime/NodeEngineBuilder.cpp
@@ -32,13 +32,12 @@ namespace NES
 NodeEngineBuilder::NodeEngineBuilder(
     const WorkerConfiguration& workerConfiguration,
     std::shared_ptr<SystemEventListener> systemEventListener,
-    std::shared_ptr<QueryEngineStatisticListener> statisticEventListener)
+    std::vector<std::shared_ptr<QueryEngineStatisticListener>> statisticEventListener)
     : workerConfiguration(workerConfiguration)
     , systemEventListener(std::move(systemEventListener))
     , statisticEventListener(std::move(statisticEventListener))
 {
 }
-
 
 std::unique_ptr<NodeEngine> NodeEngineBuilder::build()
 {