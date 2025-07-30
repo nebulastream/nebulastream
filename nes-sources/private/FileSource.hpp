80a7b6d46bed103b3fd5d9f90f502a4c53a3c4a0
lukas schwerdtfeger <lukas.schwerdtfeger@gmail.com>
2025-07-30 13:37:09 +0200

    refactor(Sources): Dedicated Type for FillTupleBufferResult
    Signed-off-by: lukas schwerdtfeger <lukas.schwerdtfeger@gmail.com>

--- nes-sources/private/FileSource.hpp
+++ nes-sources/private/FileSource.hpp
@@ -23,7 +23,7 @@
 #include <string>
 #include <string_view>
 #include <unordered_map>
-
+#include <Runtime/AbstractBufferProvider.hpp>
 #include <Runtime/TupleBuffer.hpp>
 #include <Sources/Source.hpp>
 #include <Sources/SourceDescriptor.hpp>
@@ -46,7 +46,7 @@ public:
     FileSource(FileSource&&) = delete;
     FileSource& operator=(FileSource&&) = delete;
 
-    size_t fillTupleBuffer(TupleBuffer& tupleBuffer, const std::stop_token& stopToken) override;
+    FillTupleBufferResult fillTupleBuffer(TupleBuffer& tupleBuffer, const std::stop_token& stopToken) override;
 
     /// Open file socket.
     void open() override;
