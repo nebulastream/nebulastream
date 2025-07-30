c1c9d8a885ce9a5c2f3918f8b7fb90839be4f33b
lukas schwerdtfeger <lukas.schwerdtfeger@gmail.com>
2025-07-30 13:37:09 +0200

    refactor(Sources): Dedicated Type for FillTupleBufferResult
    Signed-off-by: lukas schwerdtfeger <lukas.schwerdtfeger@gmail.com>

--- nes-sources/src/SourceThread.cpp
+++ nes-sources/src/SourceThread.cpp
@@ -33,6 +33,7 @@
 #include <Sources/SourceReturnType.hpp>
 #include <Time/Timestamp.hpp>
 #include <Util/Logger/Logger.hpp>
+#include <Util/Overloaded.hpp>
 #include <Util/ThreadNaming.hpp>
 #include <cpptrace/from_current.hpp>
 #include <fmt/format.h>
@@ -125,23 +126,22 @@ dataSourceThreadRoutine(const std::stop_token& stopToken, Source& source, Abstra
         /// 4. Failure. The fillTupleBuffer method will throw an exception, the exception is propagted to the SourceThread via the return promise.
         ///    The thread exists with an exception
         auto emptyBuffer = bufferProvider.getBufferBlocking();
-        const auto numReadBytes = source.fillTupleBuffer(emptyBuffer, stopToken);
+        const auto fillTupleResult = source.fillTupleBuffer(emptyBuffer, stopToken);
 
-        if (numReadBytes != 0)
+        if (const auto* numberOfTuples = std::get_if<Source::FillTupleBufferResult::Tuples>(&fillTupleResult.result))
         {
             /// The source read in raw bytes, thus we don't know the number of tuples yet.
             /// The InputFormatterTask expects that the source set the number of bytes this way and uses it to determine the number of tuples.
-            emptyBuffer.setNumberOfTuples(numReadBytes);
+            emptyBuffer.setNumberOfTuples(numberOfTuples->numTuples);
             emit(emptyBuffer, true);
         }
-
-        if (stopToken.stop_requested())
+        else
         {
-            return {SourceImplementationTermination::StopRequested};
-        }
+            if (stopToken.stop_requested())
+            {
+                return {SourceImplementationTermination::StopRequested};
+            }
 
-        if (numReadBytes == 0)
-        {
             return {SourceImplementationTermination::EndOfStream};
         }
     }
@@ -246,7 +246,7 @@ void SourceThread::stop()
 SourceReturnType::TryStopResult SourceThread::tryStop(std::chrono::milliseconds timeout)
 {
     PRECONDITION(thread.get_id() != std::this_thread::get_id(), "DataSrc Thread should never request the source termination");
-    NES_DEBUG("SourceThread  {} : attempting to stop source", originId);
+    NES_DEBUG("SourceThread {} : attempting to stop source", originId);
     thread.request_stop();
 
     try
