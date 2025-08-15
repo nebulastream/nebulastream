84a3eca20e24a4c55c1f00eace9f321dc0d1a7a6
Yanikovic <schroeder_yannik@web.de>
2025-08-15 11:56:56 +0200

    chore(SourceCatalog): add forwarding/producing source types to retrieve a descriptor with the name of the origin source

--- nes-sources/src/SourceCatalog.cpp
+++ nes-sources/src/SourceCatalog.cpp
@@ -36,25 +36,33 @@
 namespace NES
 {
 
-std::optional<LogicalSource> SourceCatalog::addLogicalSource(const std::string& logicalSourceName, const Schema& schema)
+
+Schema withSourceQualifier(const std::string& sourceName, const Schema& schema)
 {
     Schema newSchema;
     for (const auto& field : schema.getFields())
     {
-        newSchema.addField(logicalSourceName + Schema::ATTRIBUTE_NAME_SEPARATOR + field.name, field.dataType);
-        if (field.name.find(logicalSourceName) != std::string::npos)
+        newSchema.addField(sourceName + Schema::ATTRIBUTE_NAME_SEPARATOR + field.name, field.dataType);
+        if (field.name.find(sourceName) != std::string::npos)
         {
             NES_DEBUG(
                 "Trying to register logical source \"{}\" where passed field name {} already contained the source name as a prefix, which "
                 "is probably a mistake",
-                logicalSourceName,
+                sourceName,
                 field.name);
         }
     }
+    return newSchema;
+}
+
+std::optional<LogicalSource> SourceCatalog::addLogicalSource(
+    const std::string& logicalSourceName, const Schema& schema, const SourceIngestionType sourceType)
+{
     const std::unique_lock lock(catalogMutex);
     if (!containsLogicalSource(logicalSourceName))
     {
-        LogicalSource logicalSource{logicalSourceName, newSchema};
+        LogicalSource logicalSource{
+            logicalSourceName, sourceType == SourceIngestionType::Origin ? withSourceQualifier(logicalSourceName, schema) : schema};
         namesToLogicalSourceMapping.emplace(logicalSourceName, logicalSource);
         logicalToPhysicalSourceMapping.emplace(logicalSource, std::unordered_set<SourceDescriptor>{});
         NES_DEBUG("Added logical source {}", logicalSourceName);
