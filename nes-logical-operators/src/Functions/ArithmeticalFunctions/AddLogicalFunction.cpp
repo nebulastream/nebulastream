a386b199baab927f514344d6b6c8f0659631d574
Matthis Gördel <matthis@posteo.de>
2025-08-20 23:09:09 +0200

    fix include order

--- nes-logical-operators/src/Functions/ArithmeticalFunctions/AddLogicalFunction.cpp
+++ nes-logical-operators/src/Functions/ArithmeticalFunctions/AddLogicalFunction.cpp
@@ -12,12 +12,14 @@
     limitations under the License.
 */
 
+#include <Functions/ArithmeticalFunctions/AddLogicalFunction.hpp>
+
 #include <string>
 #include <string_view>
 #include <vector>
+
 #include <DataTypes/DataType.hpp>
 #include <DataTypes/Schema.hpp>
-#include <Functions/ArithmeticalFunctions/AddLogicalFunction.hpp>
 #include <Functions/LogicalFunction.hpp>
 #include <Serialization/DataTypeSerializationUtil.hpp>
 #include <Util/PlanRenderer.hpp>
