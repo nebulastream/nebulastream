6da2993c7abf7d0dda13ca8fd51cb4a82cd02c31
Matthis Gördel <matthis@posteo.de>
2025-08-27 19:55:04 +0200

    check_mutations: also check only single file

--- check_mutations.py
+++ check_mutations.py
@@ -20,44 +20,48 @@ import os
 
 from pathlib import Path
 
+
+def get_patched_filename(patch):
+    with open(patch, encoding="utf-8") as pfile:
+        first_line = pfile.read(1000).split("\n")[0]
+        filename = first_line.split(" ")[1]
+    return filename
+
+
+def check_patch(compile_commands, filename, orig_file, main_dir, patch):
+    dir = compile_commands[filename]["dir"]
+    cmd = compile_commands[filename]["cmd"]
+
+    subprocess.run(["patch", filename, patch], text=True, check=True, capture_output=True)
+    os.chdir(dir)
+
+    try:
+        subprocess.run(cmd.split(" "), text=True, check=True, capture_output=True)
+        compiles = True
+    except subprocess.CalledProcessError:
+        compiles = False
+
+    os.chdir(main_dir)
+    with open(filename, "w", encoding="utf-8") as reset_file:
+        reset_file.write(orig_file)
+    return compiles
+
+
 def check_patches(args):
     patches, compile_commands = args
 
-    with open(patches[0], encoding="utf-8") as pfile:
-        first_line = pfile.read(1000).split("\n")[0]
-        filename = first_line.split(" ")[1]
+    filename = get_patched_filename(patches[0])
     
     with open(filename, encoding="utf-8") as ofile:
         orig_file = ofile.read()
     main_dir = os.getcwd()
 
     for patch in patches:
-            
-        dir = compile_commands[filename]["dir"]
-        cmd = compile_commands[filename]["cmd"]
-
-        subprocess.run(["patch", filename, patch], text=True, check=True, capture_output=True)
-        os.chdir(dir)
-
-        try:
-            subprocess.run(cmd.split(" "), text=True, check=True, capture_output=True)
-            print("y", patch, flush=True)
-        except subprocess.CalledProcessError:
-            print("n", patch, flush=True)
-
-        os.chdir(main_dir)
-        with open(filename, "w", encoding="utf-8") as reset_file:
-            reset_file.write(orig_file)
+        compiles = check_patch(compile_commands, filename, orig_file, main_dir, patch)
+        print("y" if compiles else "n", patch, flush=True)
 
 
-def main():
-    mutation_dir = Path(sys.argv[1])
-
-    with open(sys.argv[2]) as compile_commands_json:
-        compile_commands = json.load(compile_commands_json)
-
-    compile_cmds = {cmd["file"]: {"dir": cmd["directory"], "cmd": cmd["command"]} for cmd in compile_commands}
-
+def check_mutation_dir(mutation_dir, compile_cmds):
     dirs = []
     for d in mutation_dir.iterdir():
         dirs.append(
@@ -66,6 +70,25 @@ def main():
 
     with multiprocessing.Pool() as mutation_dir:
         mutation_dir.map(check_patches, dirs)
+
+
+def main():
+    mutation_thingy = Path(sys.argv[1])
+
+    with open(sys.argv[2]) as compile_commands_json:
+        compile_commands = json.load(compile_commands_json)
+
+    compile_cmds = {cmd["file"]: {"dir": cmd["directory"], "cmd": cmd["command"]} for cmd in compile_commands}
+
+    if mutation_thingy.is_dir():
+        check_mutation_dir(mutation_thingy, compile_cmds)
+    elif mutation_thingy.is_file():
+        filename = get_patched_filename(mutation_thingy)
+        with open(filename, encoding="utf-8") as ofile:
+            orig_file = ofile.read()
+        compiles = check_patch(compile_cmds, filename, orig_file, os.getcwd(), mutation_thingy)
+        sys.exit(0 if compiles else 1)
+
 
 if __name__ == "__main__":
     main()
