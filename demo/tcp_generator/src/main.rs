6f70c53ead565d2bbb397602ce3e6cafaa9fab0b
lukas schwerdtfeger <lukas.schwerdtfeger@gmail.com>
2025-08-29 16:45:11 +0200

    demo
    Signed-off-by: lukas schwerdtfeger <lukas.schwerdtfeger@gmail.com>

---
+++ demo/tcp_generator/src/main.rs
@@ -0,0 +1,110 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+use tokio::net::{TcpListener, TcpStream};
+use tokio::io::{AsyncWriteExt, BufWriter};
+use rand::Rng;
+use std::time::Duration;
+use tokio::time::sleep;
+use csv::WriterBuilder;
+use std::io::Cursor;
+use tokio::signal;
+
+const HOST: &str = "0.0.0.0";
+const PORT: u16 = 9999;
+
+async fn handle_client(mut socket: TcpStream, client_address: std::net::SocketAddr) {
+    println!("[+] Accepted connection from {}", client_address);
+    let mut monotonic_counter: u64 = 0;
+
+    let mut writer = BufWriter::new(&mut socket);
+
+    // Initial row
+    let mut wtr = WriterBuilder::new()
+        .quote_style(csv::QuoteStyle::Never)
+        .escape(b'\\')
+        .from_writer(vec![]);
+    wtr.write_record(&[
+        monotonic_counter.to_string(),
+        monotonic_counter.to_string(),
+        monotonic_counter.to_string(),
+    ])
+        .unwrap();
+    let csv_line = String::from_utf8(wtr.into_inner().unwrap()).unwrap();
+    if let Err(e) = writer.write_all(csv_line.as_bytes()).await {
+        eprintln!("[-] Client {} disconnected: {}", client_address, e);
+        return;
+    }
+
+    for _i in 0..2 {
+        monotonic_counter += 1000;
+        let random_val = rand::thread_rng().gen_range(0.0..100.0);
+
+        let mut wtr = WriterBuilder::new()
+            .quote_style(csv::QuoteStyle::Never)
+            .escape(b'\\')
+            .from_writer(vec![]);
+        wtr.write_record(&[
+            monotonic_counter.to_string(),
+            format!("{:.4}", random_val),
+            monotonic_counter.to_string(),
+        ])
+            .unwrap();
+        let csv_line = String::from_utf8(wtr.into_inner().unwrap()).unwrap();
+
+        if let Err(e) = writer.write_all(csv_line.as_bytes()).await {
+            eprintln!("[-] Client {} disconnected: {}", client_address, e);
+            break;
+        }
+        if let Err(e) = writer.flush().await {
+            eprintln!("[-] Client {} disconnected during flush: {}", client_address, e);
+            break;
+        }
+    }
+    sleep(Duration::from_secs(1)).await;
+    println!("[*] Closing connection to {}", client_address);
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let listener = TcpListener::bind(format!("{}:{}", HOST, PORT)).await?;
+    println!("[*] Listening on {}:{}", HOST, PORT);
+
+    loop {
+        tokio::select! {
+            accepted = listener.accept() => {
+                match accepted {
+                    Ok((socket, addr)) => {
+                        tokio::spawn(async move {
+                            handle_client(socket, addr).await;
+                        });
+                    }
+                    Err(e) => {
+                        eprintln!("[!] Server accept error: {}", e);
+                        // If the listener encounters an error, it might be unrecoverable.
+                        // Consider breaking the loop or handling specific errors.
+                        break;
+                    }
+                }
+            }
+            _ = signal::ctrl_c() => {
+                println!("[*] Received Ctrl-C, shutting down server...");
+                break; // Exit the loop on Ctrl-C
+            }
+        }
+    }
+
+    println!("[*] Closing server socket.");
+    Ok(())
+}
