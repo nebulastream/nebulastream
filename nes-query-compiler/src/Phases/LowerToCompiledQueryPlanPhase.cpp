8e942e2ee4eee06992e94df37ab31d0e1fe75fbb
Benjamin Greb <benjamingreb01@gmail.com>
2025-09-09 11:34:04 +0200

    chore(compiler): enable function inlining during query compilation

--- nes-query-compiler/src/Phases/LowerToCompiledQueryPlanPhase.cpp
+++ nes-query-compiler/src/Phases/LowerToCompiledQueryPlanPhase.cpp
@@ -108,6 +108,7 @@ std::unique_ptr<ExecutablePipelineStage> LowerToCompiledQueryPlanPhase::getStage
     {
         case ExecutionMode::COMPILER: {
             options.setOption("engine.Compilation", true);
+            options.setOption("engine.Inline", true);
             break;
         }
         case ExecutionMode::INTERPRETER: {
