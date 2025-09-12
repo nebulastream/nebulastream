e1cacc2d7fff46a87c657b4b803de1215f0d25e9
Matthis Gördel <matthis@posteo.de>
2025-09-12 10:38:59 +0200

    add fetch_artifacts script

---
+++ scripts/fuzz/fetch_artifacts.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+if [ -z "$GITHUB_API_TOKEN" ]
+then
+    echo pls set GITHUB_API_TOKEN
+    exit 1
+fi
+
+if [ ! -d ma_results ]
+then
+    echo expexts ./ma_results to exits
+    exit 1
+fi
+
+res=./ma_results
+
+curl -L \
+    --silent \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer $GITHUB_API_TOKEN" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    https://api.github.com/repos/nebulastream/nebulastream/actions/artifacts \
+    > $res/artifacts.json
+
+downloaded=0
+for artifact_id in $(jq < $res/artifacts.json -r '.artifacts.[] | select(.workflow_run.head_branch == "fuzz") | .id')
+do
+    if [ -f $res/$artifact_id.zip ] && unzip -t $res/$artifact_id.zip > /dev/null
+    then
+        echo $artifact_id.zip already exists
+        continue
+    fi
+
+    curl -L \
+      --silent \
+      -H "Accept: application/vnd.github+json" \
+      -H "Authorization: Bearer $GITHUB_API_TOKEN" \
+      -H "X-GitHub-Api-Version: 2022-11-28" \
+      --output $res/$artifact_id.zip \
+      https://api.github.com/repos/nebulastream/nebulastream/actions/artifacts/$artifact_id/zip
+
+    downloaded=$(( downloaded + 1))
+done
+
+echo dowloaded $downloaded artifacts to $res
