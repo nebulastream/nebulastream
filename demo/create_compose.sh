7c3c6e35c28425ed7537b1cb838894eeb69dc51f
lukas schwerdtfeger <lukas.schwerdtfeger@gmail.com>
2025-09-16 18:55:23 +0200

    demo
    Signed-off-by: lukas schwerdtfeger <lukas.schwerdtfeger@gmail.com>

---
+++ demo/create_compose.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#    https://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -eo pipefail
+
+if [ $# -ne 1 ]; then
+  echo "Error: Exactly one argument required"
+  echo "Usage: $0 <filename>"
+  exit 1
+fi
+
+# Check if the argument is an existing file
+if [ ! -f "$1" ]; then
+  echo "Error: '$1' is not a valid file or does not exist"
+  exit 1
+fi
+
+# Check if yq is installed
+if ! command -v yq &>/dev/null; then
+  echo "Error: yq is required. Install with: sudo snap install yq"
+  exit 1
+fi
+
+CMAKE_BUILD_DIR=${CMAKE_BUILD_DIR:-"cmake-build-debug"}
+
+WORKERS_FILE=$(realpath $1)
+cd "$(git rev-parse --show-toplevel)"
+
+if [ ! -f "$(pwd)/${CMAKE_BUILD_DIR}/nes-single-node-worker/nes-single-node-worker" ]; then
+  echo "You have to build the nes-single-node-worker binary first for cmake target ${CMAKE_BUILD_DIR}"
+  exit 1
+fi
+
+# Start building the compose file
+cat <<EOF
+services:
+  worker-image:
+    profiles: [ build-only ]
+    image: worker-image
+    pull_policy: never
+    build:
+      context: $(pwd)/${CMAKE_BUILD_DIR}/nes-single-node-worker
+      dockerfile_inline: |
+        FROM ubuntu:24.04 AS app
+        ENV LLVM_TOOLCHAIN_VERSION=19
+        RUN apt update -y && apt install curl wget gpg -y
+        RUN curl -fsSL https://apt.llvm.org/llvm-snapshot.gpg.key | gpg --dearmor -o /etc/apt/keyrings/llvm-snapshot.gpg \
+        && chmod a+r /etc/apt/keyrings/llvm-snapshot.gpg \
+        && echo "deb [arch="\$\$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/llvm-snapshot.gpg] http://apt.llvm.org/"\$(. /etc/os-release && echo "\$\$VERSION_CODENAME")"/ llvm-toolchain-"\$(. /etc/os-release && echo "\$\$VERSION_CODENAME")"-\$\${LLVM_TOOLCHAIN_VERSION} main" > /etc/apt/sources.list.d/llvm-snapshot.list \
+        && echo "deb-src [arch="\$\$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/llvm-snapshot.gpg] http://apt.llvm.org/"\$(. /etc/os-release && echo "\$\$VERSION_CODENAME")"/ llvm-toolchain-"\$(. /etc/os-release && echo "\$\$VERSION_CODENAME")"-\$\${LLVM_TOOLCHAIN_VERSION} main" >> /etc/apt/sources.list.d/llvm-snapshot.list \
+        && apt update -y \
+        && apt install -y libc++1-\$\${LLVM_TOOLCHAIN_VERSION} libc++abi1-\$\${LLVM_TOOLCHAIN_VERSION}
+
+        RUN GRPC_HEALTH_PROBE_VERSION=v0.4.40 && \
+        wget -qO/bin/grpc_health_probe https://github.com/grpc-ecosystem/grpc-health-probe/releases/download/\$\${GRPC_HEALTH_PROBE_VERSION}/grpc_health_probe-linux-amd64 && \
+        chmod +x /bin/grpc_health_probe
+
+        COPY nes-single-node-worker /usr/bin
+        ENTRYPOINT ["nes-single-node-worker"]
+
+EOF
+
+# Read workers and generate services
+WORKER_COUNT=$(yq '.workers | length' "$WORKERS_FILE")
+
+for i in $(seq 0 $((WORKER_COUNT - 1))); do
+  # Extract worker data
+  HOST=$(yq -r ".workers[$i].host" "$WORKERS_FILE")
+  HOST_NAME=$(echo $HOST | cut -d':' -f1)
+  GRPC=$(yq -r ".workers[$i].grpc" "$WORKERS_FILE")
+  GRPC_PORT=$(echo $GRPC | cut -d':' -f2)
+
+  # Generate service definition
+  cat <<EOF
+  $HOST_NAME:
+    image: worker-image
+    pull_policy: never
+    healthcheck:
+      test: [ "CMD-SHELL", "/bin/grpc_health_probe", '-addr', 'localhost:$GRPC_PORT' ]
+    command: [
+      "--grpc=0.0.0.0:$GRPC_PORT",
+      "--connection=$HOST",
+    ]
+    volumes:
+      - $(pwd):$(pwd)
+EOF
+
+done
---
+++ demo/usage.md
@@ -0,0 +1,46 @@
+# Create Compose Script
+
+The create compose script converts a topology definition like this:
+
+```yaml
+workers:
+  - host: "localhost:9090"
+    grpc: "localhost:8080"
+    capacity: 10000
+```
+
+into a docker compose file
+
+In bash, you can directly invoke it like this:
+
+```bash 
+docker compose -f <(./create_compose.sh query.yaml) up
+```
+
+assuming you have previously built a recent worker image like this:
+
+```bash 
+docker compose -f <(./create_compose.sh query.yaml) --profile build-only build
+```
+
+By default, it picks the `cmake-build-debug` build directory which is the default debug cmake build directory, when
+using CLion.
+You can overwrite this behavior by setting the `CMAKE_BUILD_DIR` environment
+
+This will start multiple nebulastream workers according to the topology definition with their host names.
+You cannot access them because you are not in the same networking namespace as the docker compose container.
+
+You can enter the networking namespace using `nsenter`, but you have to get the `pid` of any docker compose container
+first.
+
+```bash 
+sudo nsenter -t $(docker inspect -f "{{.State.Pid}}" $(docker ps -q | head -n1)) <your-command>
+```
+
+```bash
+sudo nsenter -t $(
+  docker inspect -f "{{.State.Pid}}" $(
+    docker ps -q | head -n1
+  )
+) -n cmake-build-debug/nes-systests/systest/systest -r -n 100 -e large --clusterConfig=nes-systests/configs/topologies/single-node.yaml
+```
