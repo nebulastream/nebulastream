keyseven123/engine-statemanagement-paper

feat(Generator Source): Added output rates to generator source
It is now possible to specify a type of output / emit rate. For now, we have implemented a fixed and a sinus rate.

nes-plugins/Sources/GeneratorSource/GeneratorSource.cpp
@@ -21,7 +21,6 @@
 #include <memory>
 #include <stop_token>
 #include <string>
-#include <string_view>
 #include <unordered_map>
 #include <utility>
 #include <Configurations/Descriptor.hpp>
@@ -45,9 +44,20 @@ GeneratorSource::GeneratorSource(const SourceDescriptor& sourceDescriptor)
           seed,
           sourceDescriptor.getFromConfig(ConfigParametersGenerator::SEQUENCE_STOPS_GENERATOR),
           sourceDescriptor.getFromConfig(ConfigParametersGenerator::GENERATOR_SCHEMA))
-
+    , flushInterval(std::chrono::milliseconds{sourceDescriptor.getFromConfig(ConfigParametersGenerator::FLUSH_INTERVAL_MS)})
 {
     NES_TRACE("Init GeneratorSource.")
+    switch (sourceDescriptor.getFromConfig(ConfigParametersGenerator::GENERATOR_RATE_TYPE))
+    {
+        case GeneratorRate::Type::FIXED:
+            generatorRate
+                = std::make_unique<FixedGeneratorRate>(sourceDescriptor.getFromConfig(ConfigParametersGenerator::GENERATOR_RATE_CONFIG));
+            break;
+        case GeneratorRate::Type::SINUS:
+            generatorRate
+                = std::make_unique<SinusGeneratorRate>(sourceDescriptor.getFromConfig(ConfigParametersGenerator::GENERATOR_RATE_CONFIG));
+            break;
+    }
 }
 
 void GeneratorSource::open()
@@ -57,15 +67,15 @@ void GeneratorSource::open()
 
 void GeneratorSource::close()
 {
-    NES_TRACE("Closing GeneratorSource.");
+    auto totalElapsedTime = std::chrono::high_resolution_clock::now() - generatorStartTime;
+    NES_TRACE("Generated {} buffers in {}. Closing GeneratorSource.", generatedBuffers, totalElapsedTime);
 }
 
 size_t GeneratorSource::fillTupleBuffer(NES::Memory::TupleBuffer& tupleBuffer, const std::stop_token& stopToken)
 {
-    NES_INFO("Filling buffer in GeneratorSource.");
+    NES_DEBUG("Filling buffer in GeneratorSource.");
     try
     {
-        size_t writtenBytes = 0;
         const auto elapsedTime
             = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - generatorStartTime).count();
         if (maxRuntime >= 0 && elapsedTime >= maxRuntime)
@@ -73,31 +83,57 @@ size_t GeneratorSource::fillTupleBuffer(NES::Memory::TupleBuffer& tupleBuffer, c
             NES_INFO("Reached max runtime! Stopping Source");
             return 0;
         }
+
+        /// Asking the generatorRate how many tuples we should generate for this interval [now, now + flushInterval]
+        const auto startOfInterval = std::chrono::system_clock::now();
+        const auto endOfInterval = startOfInterval + flushInterval;
+        const auto numberOfTuplesToGenerate = generatorRate->calcNumberOfTuplesForInterval(startOfInterval, endOfInterval);
+        NES_DEBUG("numberOfTuplesToGenerate: {}", numberOfTuplesToGenerate);
+
+        /// Generating the required number of tuples. Any tuples that do not fit into the tuple buffer, we add to the orphanTuples and emit
+        /// a warning. Also, we first add the orphanTuples to the tuple buffer, before adding newly-created once.
         const size_t rawTBSize = tupleBuffer.getBufferSize();
-        while (writtenBytes < rawTBSize and not this->generator.shouldStop() and not stopToken.stop_requested())
+        uint64_t i = 0;
+        size_t writtenBytes = 0;
+        while (i < numberOfTuplesToGenerate and not this->generator.shouldStop() and not stopToken.stop_requested())
         {
             auto insertedBytes = tuplesStream.tellp();
-            if (!orphanTuples.empty())
+            if (not orphanTuples.empty())
             {
                 tuplesStream << orphanTuples;
                 orphanTuples.clear();
             }
             this->generator.generateTuple(tuplesStream);
+            ++generatedTuplesCounter;
             insertedBytes = tuplesStream.tellp() - insertedBytes;
             if (writtenBytes + insertedBytes > rawTBSize)
             {
-                tuplesStream.read(tupleBuffer.getBuffer<char>(), writtenBytes);
-                generatedBuffers++;
                 this->orphanTuples = tuplesStream.str().substr(writtenBytes, tuplesStream.str().length() - writtenBytes);
-                tuplesStream.str("");
-                return writtenBytes;
+                NES_WARNING("Not all required tuples fit into buffer of size {}. {} are left over", rawTBSize, tuplesStream.str().size());
+                break;
             }
             writtenBytes += insertedBytes;
+            ++i;
         }
         tuplesStream.read(tupleBuffer.getBuffer<char>(), writtenBytes);
         ++generatedBuffers;
         tuplesStream.str("");
-        NES_INFO("Wrote {} bytes", writtenBytes);
+        NES_DEBUG("Wrote {} bytes", writtenBytes);
+
+        /// Calculating how long to sleep. The whole method should take the duration of the flushInterval. If we have some time left, we
+        /// sleep for the remaining duration. If there is no time left, we print a warning.
+        const auto durationGeneratingTuples
+            = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - startOfInterval);
+        if (durationGeneratingTuples > flushInterval)
+        {
+            NES_WARNING(
+                "Can not produce all required tuples in the flushInterval of {} as it took us {}", flushInterval, durationGeneratingTuples);
+        }
+        else
+        {
+            const auto sleepDuration = flushInterval - durationGeneratingTuples;
+            std::this_thread::sleep_for(sleepDuration);
+        }
         return writtenBytes;
     }
     catch (const std::exception& e)

nebulastream/nebulastream

nes-plugins/Sources/GeneratorSource/GeneratorSource.cpp
@@ -21,13 +21,13 @@
 #include <memory>
 #include <stop_token>
 #include <string>
-#include <string_view>
 #include <unordered_map>
 #include <utility>
 #include <Configurations/Descriptor.hpp>
 #include <Runtime/TupleBuffer.hpp>
 #include <Sources/SourceDescriptor.hpp>
 #include <Util/Logger/Logger.hpp>
+#include <fmt/chrono.h>
 #include <Generator.hpp>
 #include <GeneratorDataRegistry.hpp>
 #include <SourceRegistry.hpp>
@@ -44,28 +44,43 @@ GeneratorSource::GeneratorSource(const SourceDescriptor& sourceDescriptor)
           seed,
           sourceDescriptor.getFromConfig(ConfigParametersGenerator::SEQUENCE_STOPS_GENERATOR),
           sourceDescriptor.getFromConfig(ConfigParametersGenerator::GENERATOR_SCHEMA))
-
+    , flushInterval(std::chrono::milliseconds{sourceDescriptor.getFromConfig(ConfigParametersGenerator::FLUSH_INTERVAL_MS)})
 {
     NES_TRACE("Init GeneratorSource.")
+    switch (sourceDescriptor.getFromConfig(ConfigParametersGenerator::GENERATOR_RATE_TYPE))
+    {
+        case GeneratorRate::Type::FIXED:
+            generatorRate
+                = std::make_unique<FixedGeneratorRate>(sourceDescriptor.getFromConfig(ConfigParametersGenerator::GENERATOR_RATE_CONFIG));
+            break;
+        case GeneratorRate::Type::SINUS:
+            /// We can assume that the parsing will work, as the config has been validated
+            auto [amplitude, frequency] = SinusGeneratorRate::parseAndValidateConfigString(
+                                              sourceDescriptor.getFromConfig(ConfigParametersGenerator::GENERATOR_RATE_CONFIG))
+                                              .value();
+            generatorRate = std::make_unique<SinusGeneratorRate>(frequency, amplitude);
+            break;
+    }
 }
 
 void GeneratorSource::open()
 {
     this->generatorStartTime = std::chrono::system_clock::now();
+    this->startOfInterval = std::chrono::system_clock::now();
     NES_TRACE("Opening GeneratorSource.");
 }
 
 void GeneratorSource::close()
 {
-    NES_TRACE("Closing GeneratorSource.");
+    auto totalElapsedTime = std::chrono::system_clock::now() - generatorStartTime;
+    NES_TRACE("Generated {} buffers in {}. Closing GeneratorSource.", generatedBuffers, totalElapsedTime);
 }
 
 size_t GeneratorSource::fillTupleBuffer(TupleBuffer& tupleBuffer, const std::stop_token& stopToken)
 {
-    NES_INFO("Filling buffer in GeneratorSource.");
+    NES_DEBUG("Filling buffer in GeneratorSource.");
     try
     {
-        size_t writtenBytes = 0;
         const auto elapsedTime
             = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - generatorStartTime).count();
         if (maxRuntime >= 0 && elapsedTime >= maxRuntime)
@@ -73,31 +88,70 @@ size_t GeneratorSource::fillTupleBuffer(TupleBuffer& tupleBuffer, const std::sto
             NES_INFO("Reached max runtime! Stopping Source");
             return 0;
         }
+
+        /// Asking the generatorRate how many tuples we should generate for this interval [now, now + flushInterval].
+        /// If we receive 0 tuples, we do not return but wait till another interval, as a return value of 0 tuples results in the query being terminated.
+        uint64_t numberOfTuplesToGenerate = 0;
+        uint64_t noIntervals = 1;
+        while (numberOfTuplesToGenerate == 0)
+        {
+            const auto endOfInterval = startOfInterval + (flushInterval * noIntervals);
+            numberOfTuplesToGenerate = generatorRate->calcNumberOfTuplesForInterval(startOfInterval, endOfInterval);
+            NES_DEBUG("numberOfTuplesToGenerate: {}", numberOfTuplesToGenerate);
+            if (numberOfTuplesToGenerate == 0)
+            {
+                std::this_thread::sleep_for(std::chrono::microseconds{flushInterval});
+                ++noIntervals;
+            }
+        }
+
+        /// Generating the required number of tuples. Any tuples that do not fit into the tuple buffer, we add to the orphanTuples and emit
+        /// a warning. Also, we first add the orphanTuples to the tuple buffer, before adding newly-created once.
         const size_t rawTBSize = tupleBuffer.getBufferSize();
-        while (writtenBytes < rawTBSize and not this->generator.shouldStop() and not stopToken.stop_requested())
+        uint64_t i = 0;
+        size_t writtenBytes = 0;
+        while (i < numberOfTuplesToGenerate and not this->generator.shouldStop() and not stopToken.stop_requested())
         {
             auto insertedBytes = tuplesStream.tellp();
-            if (!orphanTuples.empty())
+            if (not orphanTuples.empty())
             {
                 tuplesStream << orphanTuples;
                 orphanTuples.clear();
             }
             this->generator.generateTuple(tuplesStream);
+            ++generatedTuplesCounter;
             insertedBytes = tuplesStream.tellp() - insertedBytes;
             if (writtenBytes + insertedBytes > rawTBSize)
             {
-                tuplesStream.read(tupleBuffer.getBuffer<char>(), writtenBytes);
-                generatedBuffers++;
                 this->orphanTuples = tuplesStream.str().substr(writtenBytes, tuplesStream.str().length() - writtenBytes);
-                tuplesStream.str("");
-                return writtenBytes;
+                NES_WARNING("Not all required tuples fit into buffer of size {}. {} are left over", rawTBSize, tuplesStream.str().size());
+                break;
             }
             writtenBytes += insertedBytes;
+            ++i;
         }
         tuplesStream.read(tupleBuffer.getBuffer<char>(), writtenBytes);
         ++generatedBuffers;
         tuplesStream.str("");
-        NES_INFO("Wrote {} bytes", writtenBytes);
+        NES_DEBUG("Wrote {} bytes", writtenBytes);
+
+        /// Calculating how long to sleep. The whole method should take the duration of the flushInterval. If we have some time left, we
+        /// sleep for the remaining duration. If there is no time left, we print a warning.
+        const auto durationGeneratingTuples
+            = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - startOfInterval);
+        if (durationGeneratingTuples > (flushInterval * noIntervals))
+        {
+            NES_WARNING(
+                "Can not produce all required tuples in the flushInterval of {} as it took us {}",
+                (flushInterval * noIntervals),
+                durationGeneratingTuples);
+        }
+        else
+        {
+            const auto sleepDuration = (flushInterval * noIntervals) - durationGeneratingTuples;
+            std::this_thread::sleep_for(sleepDuration);
+        }
+        this->startOfInterval = std::chrono::system_clock::now();
         return writtenBytes;
     }
     catch (const std::exception& e)