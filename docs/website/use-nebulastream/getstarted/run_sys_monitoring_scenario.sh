c6f3d24cf7ea80a75bc2cef8015ac2b0811f9f1f
Tim Nacken <timnicknacken@gmail.com>
2025-09-17 16:34:48 +0200

    added a website subfolder which contains a the contents of the docs website

---
+++ docs/website/use-nebulastream/getstarted/run_sys_monitoring_scenario.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+DOCKER_COMPOSE_FILE="docker-compose.monitoring.yaml"
+DATA_SCRIPT="./generate_data/sys_resources.sh"
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 <input_yaml_filename>"
+  exit 1
+fi
+
+INPUT_FILENAME="$1"
+BASENAME=$(basename "$INPUT_FILENAME")
+CSV_NAME="${BASENAME%.*}.csv"  # .yaml → .csv
+
+if [ ! -f "$INPUT_FILENAME" ]; then
+  echo "File not found: $INPUT_FILENAME"
+  exit 1
+fi
+
+# Patch compose file
+TMP_COMPOSE_FILE="docker-compose.monitoring.generated.yml"
+
+sed -E "
+  s@(register -i )/app/[^ ]+@\1/app/$BASENAME@;
+  s@(/tmp/)<query_file_name>.csv@\1${CSV_NAME}@
+" ${DOCKER_COMPOSE_FILE} > ${TMP_COMPOSE_FILE}
+
+
+docker compose -f "$TMP_COMPOSE_FILE" down
+docker compose -f "$TMP_COMPOSE_FILE" pull
+docker compose -f "$TMP_COMPOSE_FILE" up &
+COMPOSE_PID=$!
+
+cleanup() {
+  docker compose -f "$TMP_COMPOSE_FILE" down
+  rm -f "$TMP_COMPOSE_FILE"
+}
+trap cleanup EXIT
+
+#bash "$DATA_SCRIPT"
+
+wait
