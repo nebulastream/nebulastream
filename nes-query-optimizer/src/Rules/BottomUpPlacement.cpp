0b6226994367a5eebf20937fae04163e594efec8
lukas schwerdtfeger <lukas.schwerdtfeger@gmail.com>
2025-09-23 13:53:20 +0200

    render topology
    Signed-off-by: lukas schwerdtfeger <lukas.schwerdtfeger@gmail.com>

--- nes-query-optimizer/src/Rules/BottomUpPlacement.cpp
+++ nes-query-optimizer/src/Rules/BottomUpPlacement.cpp
@@ -20,6 +20,7 @@
 #include <map>
 #include <optional>
 #include <ranges>
+#include <sstream>
 #include <string>
 #include <string_view>
 #include <unordered_map>
@@ -152,6 +153,9 @@ void validatePlan(const NetworkTopology& topology, const LogicalPlan& plan)
     {
         throw PlacementFailure(fmt::format("Found errors in query plan:\n{}", fmt::join(errors, "\n")));
     }
+    std::stringstream os;
+    renderTopology(topology, os);
+    NES_DEBUG("Performing Operator Placement on: {}", os.str());
 }
 
 /// Shared state for the ILP placement model
