003afebc6e7d399ddd9020fed1937aaa35205bac
Matthis Gördel <matthis@posteo.de>
2025-10-02 14:41:58 +0200

    add fuzzing cmake flag, harness for parser, worker-protobuf

---
+++ nes-single-node-worker/fuzz/WorkerProtobufFuzzer.cpp
@@ -0,0 +1,75 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include <chrono>
+#include <thread>
+
+#include <libprotobuf-mutator/src/libfuzzer/libfuzzer_macro.h>
+
+#include <Runtime/Execution/QueryStatus.hpp>
+#include <Runtime/QueryTerminationType.hpp>
+#include <Serialization/QueryPlanSerializationUtil.hpp>
+#include <cpptrace/from_current.hpp>
+#include <ErrorHandling.hpp>
+#include <SerializableQueryPlan.pb.h>
+#include <SingleNodeWorker.hpp>
+#include <SingleNodeWorkerConfiguration.hpp>
+
+DEFINE_PROTO_FUZZER(const NES::SerializableQueryPlan& sqp)
+{
+    NES::Logger::setupLogging("client.log", NES::LogLevel::LOG_ERROR);
+
+    CPPTRACE_TRY
+    {
+        auto dqp = NES::QueryPlanSerializationUtil::deserializeQueryPlan(sqp);
+        NES::SingleNodeWorker snw{NES::SingleNodeWorkerConfiguration{}};
+        auto qid = snw.registerQuery(dqp);
+        if (!qid)
+        {
+            throw qid.error();
+        }
+        auto r1 = snw.startQuery(*qid);
+        if (!r1)
+        {
+            throw r1.error();
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        auto r2 = snw.stopQuery(*qid, NES::QueryTerminationType::Graceful);
+        if (!r2)
+        {
+            throw r2.error();
+        }
+        while (true)
+        {
+            auto s = snw.getQueryStatus(*qid);
+            if (!s)
+            {
+                throw s.error();
+            }
+            if (s->state <= NES::QueryState::Running)
+            {
+                std::this_thread::sleep_for(std::chrono::milliseconds(1));
+                continue;
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+    CPPTRACE_CATCH(...)
+    {
+        return;
+    }
+}
