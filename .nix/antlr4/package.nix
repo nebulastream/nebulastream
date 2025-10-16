966e2e054682cb488ccf21e430bb57fafc367b51
Adrian Michalke <michalke@tu-berlin.de>
2025-10-16 12:15:40 +0200

    feat(build): nix support for sanitizer builds

--- .nix/antlr4/package.nix
+++ .nix/antlr4/package.nix
@@ -3,52 +3,58 @@ let
   lib = pkgs.lib;
   antlr4Version = "4.13.2";
 
-in pkgs.llvmPackages_19.stdenv.mkDerivation rec {
-  pname = "antlr4";
-  version = antlr4Version;
+  build = { extraBuildInputs ? [] }:
+    pkgs.llvmPackages_19.stdenv.mkDerivation rec {
+      pname = "antlr4";
+      version = antlr4Version;
 
-  src = pkgs.fetchFromGitHub {
-    owner = "antlr";
-    repo = "antlr4";
-    rev = antlr4Version;
-    hash = "sha512-dSzBW7T1Qo8S1StnIsRfW3dcXNdGYI19KZPtVX2sY6Uyd5j4mwfwXu5n/ncDaUd3Fs8KcjcLUNrXWxCZTEdTdg==";
-  };
+      src = pkgs.fetchFromGitHub {
+        owner = "antlr";
+        repo = "antlr4";
+        rev = antlr4Version;
+        hash = "sha512-dSzBW7T1Qo8S1StnIsRfW3dcXNdGYI19KZPtVX2sY6Uyd5j4mwfwXu5n/ncDaUd3Fs8KcjcLUNrXWxCZTEdTdg==";
+      };
 
-  sourceRoot = "source/runtime/Cpp";
+      sourceRoot = "source/runtime/Cpp";
 
-  nativeBuildInputs = [
-    pkgs.cmake
-    pkgs.ninja
-    pkgs.pkg-config
-  ];
-  buildInputs = [
-    pkgs.libuuid
-    pkgs.zlib
-  ];
+      nativeBuildInputs = [
+        pkgs.cmake
+        pkgs.ninja
+        pkgs.pkg-config
+      ];
+      buildInputs = [
+        pkgs.libuuid
+        pkgs.zlib
+      ] ++ extraBuildInputs;
 
-  postPatch = ''
-    substituteInPlace runtime/CMakeLists.txt \
-      --replace "target_compile_definitions(antlr4_shared PUBLIC ANTLR4CPP_EXPORTS)" "target_compile_definitions(antlr4_shared PRIVATE ANTLR4CPP_EXPORTS)"
-    substituteInPlace runtime/src/atn/ProfilingATNSimulator.cpp \
-      --replace "#include \"atn/ProfilingATNSimulator.h\"" "#include \"atn/ProfilingATNSimulator.h\"\n#include <chrono>"
-  '';
+      postPatch = ''
+        substituteInPlace runtime/CMakeLists.txt \
+          --replace "target_compile_definitions(antlr4_shared PUBLIC ANTLR4CPP_EXPORTS)" "target_compile_definitions(antlr4_shared PRIVATE ANTLR4CPP_EXPORTS)"
+        substituteInPlace runtime/src/atn/ProfilingATNSimulator.cpp \
+          --replace "#include \"atn/ProfilingATNSimulator.h\"" "#include \"atn/ProfilingATNSimulator.h\"\n#include <chrono>"
+      '';
 
-  cmakeFlags = [
-    "-G"
-    "Ninja"
-    "-DANTLR_BUILD_STATIC=ON"
-    "-DANTLR_BUILD_SHARED=ON"
-    "-DANTLR4_INSTALL=ON"
-    "-DANTLR_BUILD_CPP_TESTS=OFF"
-  ];
+      cmakeFlags = [
+        "-G"
+        "Ninja"
+        "-DANTLR_BUILD_STATIC=ON"
+        "-DANTLR_BUILD_SHARED=ON"
+        "-DANTLR4_INSTALL=ON"
+        "-DANTLR_BUILD_CPP_TESTS=OFF"
+      ];
 
-  enableParallelBuilding = true;
-  strictDeps = true;
+      enableParallelBuilding = true;
+      strictDeps = true;
 
-  meta = with lib; {
-    description = "ANTLR4 C++ runtime for NebulaStream";
-    homepage = "https://www.antlr.org";
-    license = licenses.bsd3;
-    platforms = platforms.linux;
-  };
+      meta = with lib; {
+        description = "ANTLR4 C++ runtime for NebulaStream";
+        homepage = "https://www.antlr.org";
+        license = licenses.bsd3;
+        platforms = platforms.linux;
+      };
+    };
+
+in {
+  default = build { };
+  withSanitizer = extraPackages: build { extraBuildInputs = extraPackages; };
 }
--- .nix/nix-cmake.sh
+++ .nix/nix-cmake.sh
@@ -16,7 +16,85 @@
 # If we are outside the dev shell, re-run this script inside the nix develop environment.
 if [ -z "$IN_NIX_RUN" ]; then
   SCRIPT_PATH="$(readlink -f "$0")"
-  exec "$(dirname "$0")"/nix-run.sh "$SCRIPT_PATH" "$@"
+
+  # Logic to enable building with -DUSE_SANITIZER=...
+  REQUESTED_SANITIZER=""
+  BUILD_DIR=""
+  PREVIOUS_ARG=""
+  for ARG in "$@"; do
+    if [ "$PREVIOUS_ARG" = "-D" ]; then
+      case "$ARG" in
+        USE_SANITIZER=*)
+          REQUESTED_SANITIZER="${ARG#USE_SANITIZER=}"
+          ;;
+      esac
+    elif [ "$PREVIOUS_ARG" = "--build" ]; then
+      BUILD_DIR="$ARG"
+    fi
+
+    case "$ARG" in
+      -DUSE_SANITIZER=*)
+        REQUESTED_SANITIZER="${ARG#-DUSE_SANITIZER=}"
+        ;;
+      --build)
+        PREVIOUS_ARG="--build"
+        continue
+        ;;
+      -D)
+        PREVIOUS_ARG="-D"
+        continue
+        ;;
+      --)
+        PREVIOUS_ARG=""
+        ;;
+      *)
+        PREVIOUS_ARG=""
+        ;;
+    esac
+  done
+
+  if [ -z "$REQUESTED_SANITIZER" ] && [ -n "$BUILD_DIR" ]; then
+    CACHE_FILE="$BUILD_DIR/CMakeCache.txt"
+    if [ -f "$CACHE_FILE" ]; then
+      CACHE_SANITIZER=$(sed -n 's/^USE_SANITIZER:STRING=\(.*\)$/\1/p' "$CACHE_FILE" | head -n 1)
+      if [ -n "$CACHE_SANITIZER" ]; then
+        REQUESTED_SANITIZER="$CACHE_SANITIZER"
+      fi
+    fi
+  fi
+
+  if [ -z "$REQUESTED_SANITIZER" ] && [ -n "${NES_SANITIZER:-}" ]; then
+    REQUESTED_SANITIZER="$NES_SANITIZER"
+  fi
+
+  SANITIZER_SELECTOR=""
+  if [ -n "$REQUESTED_SANITIZER" ]; then
+    LOWER_REQUEST=$(printf '%s' "$REQUESTED_SANITIZER" | tr '[:upper:]' '[:lower:]')
+    case "$LOWER_REQUEST" in
+      asan|address)
+        SANITIZER_SELECTOR="address"
+        ;;
+      tsan|thread)
+        SANITIZER_SELECTOR="thread"
+        ;;
+      ubsan|undefined)
+        SANITIZER_SELECTOR="undefined"
+        ;;
+      none|default)
+        SANITIZER_SELECTOR="none"
+        ;;
+      *)
+        printf 'nix-cmake.sh: warning: unrecognized sanitizer "%s", falling back to default\n' "$REQUESTED_SANITIZER" >&2
+        SANITIZER_SELECTOR=""
+        ;;
+    esac
+  fi
+
+  if [ -n "$SANITIZER_SELECTOR" ]; then
+    NES_SANITIZER_REQUEST="$SANITIZER_SELECTOR" exec "$(dirname "$0")"/nix-run.sh "$SCRIPT_PATH" "$@"
+  else
+    exec "$(dirname "$0")"/nix-run.sh "$SCRIPT_PATH" "$@"
+  fi
 fi
 
 # Use the cmakeFlags set by Nix - this doesn't work with --build
