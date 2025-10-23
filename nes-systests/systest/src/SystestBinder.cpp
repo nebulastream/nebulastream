d9161a651a02afcd4c8e281be7cafb70750837f5
Benedikt Maria Beckermann <beckermann@tu-berlin.de>
2025-10-23 16:58:12 +0200

    feat(nes-sql-parser): Created tests for inline sink and source operators

--- nes-systests/systest/src/SystestBinder.cpp
+++ nes-systests/systest/src/SystestBinder.cpp
@@ -199,8 +199,9 @@ public:
         const auto sinkOperatorOpt = this->optimizedPlan->getRootOperators().at(0).tryGetAs<SinkLogicalOperator>();
         INVARIANT(sinkOperatorOpt.has_value(), "The optimized plan should have a sink operator");
         INVARIANT(sinkOperatorOpt.value()->getSinkDescriptor().has_value(), "The sink operator should have a sink descriptor");
-        if (Util::toUpperCase(sinkOperatorOpt.value()->getSinkDescriptor().value().getSinkType())
-            == "CHECKSUM") /// NOLINT(bugprone-unchecked-optional-access)
+        if (Util::toUpperCase(
+                sinkOperatorOpt.value()->getSinkDescriptor().value().getSinkType()) /// NOLINT(bugprone-unchecked-optional-access)
+            == "CHECKSUM")
         {
             sinkOutputSchema = SLTSinkFactory::checksumSchema;
         }
