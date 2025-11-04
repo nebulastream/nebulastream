37a9cdb3cc69913daa002dcc37f227f03537d8db
lukas schwerdtfeger <lukas.schwerdtfeger@gmail.com>
2025-11-04 16:45:40 +0100

    refactor(Emit): use single map to track sequence progress and catch bad sequence numbers
    Signed-off-by: lukas schwerdtfeger <lukas.schwerdtfeger@gmail.com>

--- nes-physical-operators/src/EmitOperatorHandler.cpp
+++ nes-physical-operators/src/EmitOperatorHandler.cpp
@@ -16,34 +16,67 @@
 
 #include <cstdint>
 #include <Identifiers/Identifiers.hpp>
+#include <Identifiers/NESStrongType.hpp>
 #include <Runtime/QueryTerminationType.hpp>
+#include <Runtime/TupleBuffer.hpp>
 #include <Util/Logger/Logger.hpp>
+#include <ErrorHandling.hpp>
 #include <PipelineExecutionContext.hpp>
 
 namespace NES
 {
 
 void EmitOperatorHandler::setChunkNumber(
-    bool chunkCompleted, ChunkNumber currentChunkNumber, bool isCurrentBufferTheLastChunk, TupleBuffer& buffer)
+    bool isEndOfIncomingChunk, ChunkNumber incomingChunkNumber, bool isIncomingBufferTheLastChunk, TupleBuffer& buffer)
 {
+    /// The SequenceState tracks how many chunks have been seen per Sequence/OriginId pair.
+    /// This allows to reason about completeness of a SequenceNumber and assign unique ChunkNumbers to every produced buffer aswell
+    /// as assigning exactly one LastChunk flag once we have seen all chunks.
+
     SequenceNumberForOriginId seqNumberOriginId(buffer.getSequenceNumber(), buffer.getOriginId());
-    auto chunkNumberCounter = seqNumberOriginIdToOutputChunkNumber.wlock();
-    const auto newChunkNumber = ChunkNumber((*chunkNumberCounter)[seqNumberOriginId]++ + ChunkNumber::INITIAL);
-    buffer.setChunkNumber(newChunkNumber);
+    const auto lock = sequenceStates.wlock();
+
+    /// Assign a unique chunk number to the buffer and increment the internal counter
+    auto& [nextChunkNumberCounter, lastChunkNumber, seenChunks] = (*lock)[seqNumberOriginId];
+
+#ifndef NO_ASSERT
+    const auto completedSequencesLock = completedSequences.wlock();
+    INVARIANT(
+        !completedSequencesLock->contains(seqNumberOriginId),
+        "Received chunk for sequence {} that was already completed",
+        seqNumberOriginId);
+#endif
+
+
+    const auto nextChunkNumber = ChunkNumber(nextChunkNumberCounter);
+    nextChunkNumberCounter += 1;
+
+    buffer.setChunkNumber(nextChunkNumber);
     buffer.setLastChunk(false);
-    if (chunkCompleted)
+
+    /// Check for completeness of the sequence number. This is only relevant if this buffer is actually completing an incoming chunk.
+    if (isEndOfIncomingChunk)
     {
-        auto chunkStateInput = seqNumberOriginIdToChunkStateInput.wlock();
-        auto& [lastChunkNumber, seenChunks] = (*chunkStateInput)[seqNumberOriginId];
-        if (isCurrentBufferTheLastChunk)
+        /// As soon as the incomingBufferLastChunk is received we know how many incoming chunks to expect.
+        if (isIncomingBufferTheLastChunk)
         {
-            lastChunkNumber = currentChunkNumber.getRawValue();
+            INVARIANT(
+                lastChunkNumber == INVALID<ChunkNumber>,
+                "Received multiple last chunks for {}. Previous last chunk was {}",
+                seqNumberOriginId,
+                lastChunkNumber);
+            lastChunkNumber = incomingChunkNumber;
         }
         seenChunks++;
-        if (seenChunks == lastChunkNumber)
+
+        /// We have processed the expected number of chunks and this buffer is closing the input tuple buffer, so we assign the last flag
+        /// and erase the sequenceState.
+        if (lastChunkNumber != INVALID<ChunkNumber> && seenChunks - 1 == lastChunkNumber.getRawValue() - ChunkNumber::INITIAL)
         {
-            chunkNumberCounter->erase(seqNumberOriginId);
-            chunkStateInput->erase(seqNumberOriginId);
+            lock->erase(seqNumberOriginId);
+#ifndef NO_ASSERT
+            completedSequencesLock->emplace(seqNumberOriginId);
+#endif
             buffer.setLastChunk(true);
         }
     }
