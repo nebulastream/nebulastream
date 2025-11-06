7ada7b1b7255eefd6b6e54744edb9b055afaadf5
lukas schwerdtfeger <lukas.schwerdtfeger@gmail.com>
2025-11-06 14:31:38 +0100

    fix(test): emit operator test resets operator handler
    Signed-off-by: lukas schwerdtfeger <lukas.schwerdtfeger@gmail.com>

--- nes-physical-operators/tests/EmitPhysicalOperatorTest.cpp
+++ nes-physical-operators/tests/EmitPhysicalOperatorTest.cpp
@@ -118,7 +118,7 @@ public:
         auto schema = Schema{}.addField("A_FIELD", DataType::Type::UINT32);
         auto layout = std::make_shared<RowLayout>(512, schema);
         EmitPhysicalOperator emit{OperatorHandlerId(0), std::make_shared<Interface::BufferRef::RowTupleBufferRef>(layout)};
-        handlers.emplace(OperatorHandlerId(0), std::make_shared<EmitOperatorHandler>());
+        handlers.insert_or_assign(OperatorHandlerId(0), std::make_shared<EmitOperatorHandler>());
         return emit;
     }
 
