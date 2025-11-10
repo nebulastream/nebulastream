b53edce3244c104646f1ecd238224f96072ba43a
Nils L. Schubert <nilslpschubert@gmail.com>
2025-11-10 11:52:46 +0100

    chore(TestTupleBuffer): Removed the TestTupleBuffer
    Signed-off-by: Nils L. Schubert <nilslpschubert@gmail.com>

--- nes-data-types/src/Serialization/SchemaSerializationUtil.cpp
+++ nes-data-types/src/Serialization/SchemaSerializationUtil.cpp
@@ -33,45 +33,19 @@ SerializableSchema SchemaSerializationUtil::serializeSchema(const Schema& schema
         DataTypeSerializationUtil::serializeDataType(field.dataType, serializedField->mutable_type());
     }
 
-    /// Serialize layoutType
-    if (schema.memoryLayoutType == Schema::MemoryLayoutType::ROW_LAYOUT)
-    {
-        serializedSchema->set_layouttype(SerializableSchema_MemoryLayoutType_ROW_LAYOUT);
-    }
-    else if (schema.memoryLayoutType == Schema::MemoryLayoutType::COLUMNAR_LAYOUT)
-    {
-        serializedSchema->set_layouttype(SerializableSchema_MemoryLayoutType_COL_LAYOUT);
-    }
-
     return *serializedSchema;
 }
 
 Schema SchemaSerializationUtil::deserializeSchema(const SerializableSchema& serializedSchema)
 {
     /// de-serialize field from serialized schema to the schema object.
-    auto deserializedSchema = Schema{Schema::MemoryLayoutType::ROW_LAYOUT};
+    Schema deserializedSchema;
     for (const auto& serializedField : serializedSchema.fields())
     {
         const auto& fieldName = serializedField.name();
         /// de-serialize data type
         auto type = DataTypeSerializationUtil::deserializeDataType(serializedField.type());
         deserializedSchema.addField(fieldName, type);
-    }
-
-    /// Deserialize layoutType
-    switch (serializedSchema.layouttype())
-    {
-        case SerializableSchema_MemoryLayoutType_ROW_LAYOUT: {
-            deserializedSchema.memoryLayoutType = Schema::MemoryLayoutType::ROW_LAYOUT;
-            break;
-        }
-        case SerializableSchema_MemoryLayoutType_COL_LAYOUT: {
-            deserializedSchema.memoryLayoutType = Schema::MemoryLayoutType::COLUMNAR_LAYOUT;
-            break;
-        }
-        default: {
-            NES_ERROR("Wrong memory layout in serialization format");
-        }
     }
     return deserializedSchema;
 }
