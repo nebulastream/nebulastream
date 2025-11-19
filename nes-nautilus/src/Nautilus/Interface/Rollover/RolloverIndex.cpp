cf4994f0b812c42f04b5fe05fe68e6dff84cbca3
Tim Nacken <timnicknacken@gmail.com>
2025-11-19 15:04:46 +0100

    feature: added tests

--- nes-nautilus/src/Nautilus/Interface/Rollover/RolloverIndex.cpp
+++ nes-nautilus/src/Nautilus/Interface/Rollover/RolloverIndex.cpp
@@ -1,76 +1,72 @@
 /*
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
 
-    https://www.apache.org/licenses/LICENSE-2.0
+        https://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
 */
-
 #include <Nautilus/Interface/Rollover/RolloverIndex.hpp>
-#include <ErrorHandling.hpp>   // PRECONDITION, NES_WARNING etc. (if needed)
+#include <ErrorHandling.hpp>
 
 namespace NES::Nautilus::Interface
+{
+
+RolloverIndex::RolloverIndex(unsigned long rolloverValue, Callback onRollover)
+    : index_(0), rolloverValue_(rolloverValue), callback_(onRollover)
+{
+    PRECONDITION(rolloverValue_ > 0, "RolloverIndex: rolloverValue must be > 0");
+}
+
+void RolloverIndex::setIndex(unsigned long index)
+{
+    index_ = index;
+    if (index_ >= rolloverValue_)
     {
+        rollover();
+    }
+}
 
-        RolloverIndex::RolloverIndex(unsigned long rolloverValue, Callback onRollover)
-            : index_(0)
-            , rolloverValue_(rolloverValue)
-            , callback_(onRollover)
-        {
-            PRECONDITION(rolloverValue_ > 0, "RolloverIndex: rolloverValue must be > 0");
-        }
+unsigned long RolloverIndex::getIndex() const
+{
+    return index_;
+}
 
-        void RolloverIndex::setIndex(unsigned long index)
-        {
-            index_ = index;
-            if (index_ >= rolloverValue_)
-            {
-                rollover();
-            }
-        }
+void RolloverIndex::setRolloverValue(unsigned long rolloverValue)
+{
+    PRECONDITION(rolloverValue > 0, "RolloverIndex: rolloverValue must be > 0");
 
-        unsigned long RolloverIndex::getIndex() const
-        {
-            return index_;
-        }
+    rolloverValue_ = rolloverValue;
+}
 
-        void RolloverIndex::setRolloverValue(unsigned long rolloverValue)
-        {
-            PRECONDITION(rolloverValue > 0, "RolloverIndex: rolloverValue must be > 0");
+unsigned long RolloverIndex::getRolloverValue() const
+{
+    return rolloverValue_;
+}
 
-            // As per your notes: index remains unchanged when rollover value is updated.
-            rolloverValue_ = rolloverValue;
-        }
+void RolloverIndex::increment()
+{
+    ++index_;
 
-        unsigned long RolloverIndex::getRolloverValue() const
-        {
-            return rolloverValue_;
-        }
+    if (index_ >= rolloverValue_)
+    {
+        rollover();
+    }
+}
 
-        void RolloverIndex::increment()
-        {
-            ++index_;
+void RolloverIndex::rollover()
+{
+    index_ = 0;
 
-            if (index_ >= rolloverValue_)
-            {
-                rollover();
-            }
-        }
+    if (callback_)
+    {
+        callback_();
+    }
+}
 
-        void RolloverIndex::rollover()
-        {
-            index_ = 0;
-
-            if (callback_)
-            {
-                callback_();
-            }
-        }
-
-    } // namespace NES::Nautilus::Interface
+}
