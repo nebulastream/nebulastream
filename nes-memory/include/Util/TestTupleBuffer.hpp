063f7588ae4abbb81fe17e0f2402702f752003d4
Maxim Popov <maxim.popov@campus.tu-berlin.de>
2025-12-15 17:27:34 +0100

    fix(SmallFilesTest): fix SmallFilesTest, update expected results, add index getRawSize function

--- nes-memory/include/Util/TestTupleBuffer.hpp
+++ nes-memory/include/Util/TestTupleBuffer.hpp
@@ -67,7 +67,9 @@ public:
     {
         /// For VARSIZED, we access the field via uint64_t to read the @class VariableSizedAccess
         if (not physicalType.isSameDataType<Type>()
-            and not(physicalType.isType(DataType::Type::VARSIZED) and std::is_same_v<std::remove_cvref_t<Type>, std::uint64_t>))
+            and not(
+                physicalType.isType(DataType::Type::VARSIZED)
+                and std::is_same_v<std::remove_cvref_t<Type>, VariableSizedAccess::CombinedIndex>))
         {
             throw CannotAccessBuffer("Wrong field type passed. Field is of type {} but accessed as {}", physicalType, NAMEOF_TYPE(Type));
         }
@@ -84,7 +86,9 @@ public:
     {
         /// For VARSIZED, we access the field via uint64_t to read the @class VariableSizedAccess
         if (not physicalType.isSameDataType<Type>()
-            and not(physicalType.isType(DataType::Type::VARSIZED) and std::is_same_v<std::remove_cvref_t<Type>, std::uint64_t>))
+            and not(
+                physicalType.isType(DataType::Type::VARSIZED)
+                and std::is_same_v<std::remove_cvref_t<Type>, VariableSizedAccess::CombinedIndex>))
         {
             throw CannotAccessBuffer("Wrong field type passed. Field is of type {} but accessed as {}", physicalType, NAMEOF_TYPE(Type));
         }
@@ -101,7 +105,9 @@ public:
     {
         /// For VARSIZED, we access the field via uint64_t to read the @class VariableSizedAccess
         if (not physicalType.isSameDataType<Type>()
-            and not(physicalType.isType(DataType::Type::VARSIZED) and std::is_same_v<std::remove_cvref_t<Type>, std::uint64_t>))
+            and not(
+                physicalType.isType(DataType::Type::VARSIZED)
+                and std::is_same_v<std::remove_cvref_t<Type>, VariableSizedAccess::CombinedIndex>))
         {
             throw CannotAccessBuffer("Wrong field type passed. Field is of type {} but accessed as {}", physicalType, NAMEOF_TYPE(Type));
         }
