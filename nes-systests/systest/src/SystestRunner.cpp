0f3fed4264772c3c43851bd7fc838e9ddfdc5529
Klaas tom Dieck <klaas.tom-dieck@campus.tu-berlin.de>
2025-12-16 15:38:16 +0100

    feat(Systest): Add file level sequential execution of queries
    To run queries sequentially add the `SEQUENTIAL_EXECUTION` statement on a line. All queries parsed in the file until the next `SEQUENTIALL_EXECUTION` will be submitted to run sequentially by the the runQueries method
    
    To ensure this functionality within the benchmark mode, the order of queries to run is simply reversed. As the benchmark mode already runs single threaded and the reversed order is the order the queries are written in the file

--- nes-systests/systest/src/SystestRunner.cpp
+++ nes-systests/systest/src/SystestRunner.cpp
@@ -133,15 +133,54 @@ std::vector<RunningQuery> runQueries(
     SystestProgressTracker& progressTracker,
     const QueryPerformanceMessageBuilder& queryPerformanceMessage)
 {
+    using SystestKey = std::pair<TestName, SystestQueryId>;
+    std::unordered_set<SystestKey> completedQueries; /// Track which queries have completed
+
     std::queue<SystestQuery> pending;
+    std::vector<SystestQuery> dependentQueries;
     for (auto it = queries.rbegin(); it != queries.rend(); ++it)
     {
-        pending.push(*it);
+        if (it->runAfter.has_value() && it->runAfter.value().second.getRawValue() != 0)
+        {
+            dependentQueries.push_back(*it);
+        }
+        else
+        {
+            pending.push(*it);
+        }
     }
 
     std::unordered_map<QueryId, std::shared_ptr<RunningQuery>> active;
     std::unordered_map<QueryId, LocalQueryStatus> finishedDifferentialQueries;
     std::vector<std::shared_ptr<RunningQuery>> failed;
+
+    const auto canRunQuery = [&completedQueries](const SystestQuery& query) -> bool
+    {
+        if (!query.runAfter.has_value() || (query.runAfter.has_value() && query.runAfter.value().second.getRawValue() == 0))
+        {
+            return true;
+        }
+        return completedQueries.contains(query.runAfter.value());
+    };
+
+    const auto moveDependentsToPending = [&](const SystestQuery& parentQuery) -> void
+    {
+        completedQueries.emplace(std::make_pair(parentQuery.testName, parentQuery.queryIdInFile));
+        /// check if any dependent query can now be run
+        auto it = dependentQueries.begin();
+        while (it != dependentQueries.end())
+        {
+            if (canRunQuery(*it))
+            {
+                pending.emplace(std::move(*it));
+                it = dependentQueries.erase(it);
+            }
+            else
+            {
+                ++it;
+            }
+        }
+    };
 
     const auto startMoreQueries = [&] -> bool
     {
@@ -150,6 +189,12 @@ std::vector<RunningQuery> runQueries(
         {
             SystestQuery nextQuery = std::move(pending.front());
             pending.pop();
+
+            INVARIANT(
+                canRunQuery(nextQuery),
+                "Cannot run query from {} with the in file id {} as it's dependencies have not finished!",
+                nextQuery.testName,
+                nextQuery.queryIdInFile);
 
             if (nextQuery.differentialQueryPlan.has_value() and nextQuery.planInfoOrException.has_value())
             {
@@ -199,7 +244,7 @@ std::vector<RunningQuery> runQueries(
         return hasOneMoreQueryToStart;
     };
 
-    while (startMoreQueries() or not(active.empty() and pending.empty()))
+    while (startMoreQueries() or not(active.empty() and pending.empty() and dependentQueries.empty()))
     {
         for (const auto& queryStatus : querySubmitter.finishedQueries())
         {
@@ -268,6 +313,7 @@ std::vector<RunningQuery> runQueries(
                     finishedDifferentialQueries.erase(otherSummaryIt);
                     active.erase(it);
                     finishedDifferentialQueries.erase(queryStatus.queryId);
+                    moveDependentsToPending(runningQuery->systestQuery);
                 }
 
                 continue;
@@ -293,6 +339,7 @@ std::vector<RunningQuery> runQueries(
                     return std::string{};
                 },
                 queryPerformanceMessage);
+            moveDependentsToPending(runningQuery->systestQuery);
             active.erase(it);
         }
     }
@@ -337,8 +384,9 @@ std::vector<RunningQuery> runQueriesAndBenchmark(
     std::vector<std::shared_ptr<RunningQuery>> ranQueries;
     progressTracker.reset();
     progressTracker.setTotalQueries(queries.size());
-    for (const auto& queryToRun : queries)
+    for (auto it = queries.rbegin(); it != queries.rend(); ++it)
     {
+        const auto& queryToRun = *it;
         if (not queryToRun.planInfoOrException.has_value())
         {
             NES_ERROR("skip failing query: {}", queryToRun.testName);
--- nes-systests/systest/tests/SystestParserTests.cpp
+++ nes-systests/systest/tests/SystestParserTests.cpp
@@ -101,7 +101,7 @@ TEST_F(SystestParserTest, testCallbackQuery)
     std::vector<std::string> receivedResultTuples;
 
     parser.registerOnQueryCallback(
-        [&](const std::string& queryOut, SystestQueryId)
+        [&](const std::string& queryOut, SystestQueryId, bool)
         {
             ASSERT_EQ(queryIn, queryOut);
             queryCallbackCalled = true;
@@ -129,7 +129,7 @@ TEST_F(SystestParserTest, testResultTuplesWithoutQuery)
 
     const std::string str = delimiter + "\n" + tpl1 + "\n" + tpl2 + "\n";
 
-    parser.registerOnQueryCallback([&](const std::string&, SystestQueryId) { FAIL(); });
+    parser.registerOnQueryCallback([&](const std::string&, SystestQueryId, bool) { FAIL(); });
     parser.registerOnResultTuplesCallback(
         [&](const std::vector<std::string>&, const SystestQueryId)
         {
@@ -153,7 +153,7 @@ TEST_F(SystestParserTest, testDifferentialQueryCallbackFromFile)
     bool differentialQueryCallbackCalled = false;
 
     parser.registerOnQueryCallback(
-        [&](const std::string& queryOut, SystestQueryId)
+        [&](const std::string& queryOut, SystestQueryId, bool)
         {
             ASSERT_FALSE(differentialQueryCallbackCalled) << "Main query callback was called after the differential one.";
             ASSERT_FALSE(mainQueryCallbackCalled) << "Main query callback should only be called once.";
@@ -201,7 +201,7 @@ TEST_F(SystestParserTest, testDifferentialQueryCallbackInlineSyntax)
     bool differentialQueryCallbackCalled = false;
 
     parser.registerOnQueryCallback(
-        [&](const std::string& queryOut, SystestQueryId)
+        [&](const std::string& queryOut, SystestQueryId, bool)
         {
             ASSERT_FALSE(differentialQueryCallbackCalled) << "Main query callback was called after the differential one.";
             ASSERT_FALSE(mainQueryCallbackCalled) << "Main query callback should only be called once.";
