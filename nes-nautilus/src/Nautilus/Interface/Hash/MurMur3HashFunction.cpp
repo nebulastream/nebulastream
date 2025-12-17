ab2cd709f301f3821e84ca34b48aeb67b91495d8
alepping <aljoschalepping@gmail.com>
2025-12-17 19:43:01 +0100

    fix(VariableSizedData): renames getContentSize to getSize
    Since we don't use a 4 byte prefix that represents the size of the varsized anymore, we don't need to distinguish between the size of the content (payload) and the total size of the varsized (payload + 4byte size) anymore

--- nes-nautilus/src/Nautilus/Interface/Hash/MurMur3HashFunction.cpp
+++ nes-nautilus/src/Nautilus/Interface/Hash/MurMur3HashFunction.cpp
@@ -126,7 +126,7 @@ HashFunction::HashValue MurMur3HashFunction::calculate(HashValue& hash, const Va
                 if constexpr (std::is_same_v<T, VariableSizedData>)
                 {
                     const auto& varSizedContent = val;
-                    return hash ^ nautilus::invoke(hashBytes, varSizedContent.getContent(), varSizedContent.getContentSize());
+                    return hash ^ nautilus::invoke(hashBytes, varSizedContent.getContent(), varSizedContent.getSize());
                 }
                 else
                 {
