ygtmesci/nebulastreamBDSPRO

feat(Function): Added ceil physical function
Signed-off-by: Nils L. Schubert <nilslpschubert@gmail.com>

nes-physical-operators/include/Functions/ArithmeticalFunctions/CeilPhysicalFunction.hpp
@@ -13,29 +13,24 @@
 */
 
 #pragma once
-#include <string>
-#include <vector>
 #include <Functions/PhysicalFunction.hpp>
-#include <Util/Registry.hpp>
+#include <Nautilus/DataTypes/VarVal.hpp>
+#include <Nautilus/Interface/Record.hpp>
+#include <ExecutionContext.hpp>
 
 namespace NES
 {
 
-using PhysicalFunctionRegistryReturnType = PhysicalFunction;
-
-struct PhysicalFunctionRegistryArguments
+class CeilPhysicalFunction final : public PhysicalFunctionConcept
 {
-    std::vector<PhysicalFunction> childFunctions;
-    std::vector<DataType> inputTypes;
+public:
+    explicit CeilPhysicalFunction(PhysicalFunction childFunction, DataType inputType, DataType outputType);
+    [[nodiscard]] VarVal execute(const Record& record, ArenaRef& arena) const override;
+
+private:
+    PhysicalFunction childFunction;
+    DataType inputType;
     DataType outputType;
 };
 
-class PhysicalFunctionRegistry
-    : public BaseRegistry<PhysicalFunctionRegistry, std::string, PhysicalFunctionRegistryReturnType, PhysicalFunctionRegistryArguments>
-{
-};
 }
-
-#define INCLUDED_FROM_REGISTRY_PHYSICAL_FUNCTION
-#include <PhysicalFunctionGeneratedRegistrar.inc>
-#undef INCLUDED_FROM_REGISTRY_PHYSICAL_FUNCTION

nes-physical-operators/src/Functions/ArithmeticalFunctions/CeilPhysicalFunction.cpp
@@ -1,0 +1,56 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include <Functions/ArithmeticalFunctions/CeilPhysicalFunction.hpp>
+
+#include <utility>
+#include <Functions/PhysicalFunction.hpp>
+#include <Nautilus/DataTypes/VarVal.hpp>
+#include <Nautilus/Interface/Record.hpp>
+#include <std/cmath.h>
+#include <ErrorHandling.hpp>
+#include <ExecutionContext.hpp>
+#include <PhysicalFunctionRegistry.hpp>
+
+namespace NES
+{
+CeilPhysicalFunction::CeilPhysicalFunction(PhysicalFunction childFunction, DataType inputType, DataType outputType)
+    : childFunction(std::move(childFunction)), inputType(std::move(inputType)), outputType(std::move(outputType))
+{
+}
+
+VarVal CeilPhysicalFunction::execute(const Record& record, ArenaRef& arena) const
+{
+    const auto value = childFunction.execute(record, arena);
+    /// If the input type is a float, we need to ceil the value and returned the ceiled value.
+    /// If the input type is an integer, we do not need to do anything.
+    if (inputType.isFloat())
+    {
+        const auto ceiledValue = nautilus::ceil(value.cast<nautilus::val<double>>());
+        return VarVal{ceiledValue}.castToType(outputType.type);
+    }
+    return value.castToType(outputType.type);
+}
+
+PhysicalFunctionRegistryReturnType
+PhysicalFunctionGeneratedRegistrar::RegisterCeilPhysicalFunction(PhysicalFunctionRegistryArguments physicalFunctionRegistryArguments)
+{
+    PRECONDITION(physicalFunctionRegistryArguments.childFunctions.size() == 1, "Ceil function must have exactly one sub-function");
+    PRECONDITION(physicalFunctionRegistryArguments.inputTypes.size() == 1, "Ceil function must have exactly one input type");
+    return CeilPhysicalFunction(
+        physicalFunctionRegistryArguments.childFunctions[0],
+        physicalFunctionRegistryArguments.inputTypes[0],
+        physicalFunctionRegistryArguments.outputType);
+}
+}

nebulastream/nebulastream

nes-physical-operators/include/Functions/ArithmeticalFunctions/CeilPhysicalFunction.hpp
@@ -13,29 +13,25 @@
 */
 
 #pragma once
-#include <string>
-#include <vector>
+#include <DataTypes/DataType.hpp>
 #include <Functions/PhysicalFunction.hpp>
-#include <Util/Registry.hpp>
+#include <Nautilus/DataTypes/VarVal.hpp>
+#include <Nautilus/Interface/Record.hpp>
+#include <Arena.hpp>
 
 namespace NES
 {
 
-using PhysicalFunctionRegistryReturnType = PhysicalFunction;
-
-struct PhysicalFunctionRegistryArguments
+class CeilPhysicalFunction final : public PhysicalFunctionConcept
 {
-    std::vector<PhysicalFunction> childFunctions;
-    std::vector<DataType> inputTypes;
+public:
+    explicit CeilPhysicalFunction(PhysicalFunction childFunction, DataType inputType, DataType outputType);
+    [[nodiscard]] VarVal execute(const Record& record, ArenaRef& arena) const override;
+
+private:
+    PhysicalFunction childFunction;
+    DataType inputType;
     DataType outputType;
 };
 
-class PhysicalFunctionRegistry
-    : public BaseRegistry<PhysicalFunctionRegistry, std::string, PhysicalFunctionRegistryReturnType, PhysicalFunctionRegistryArguments>
-{
-};
 }
-
-#define INCLUDED_FROM_REGISTRY_PHYSICAL_FUNCTION
-#include <PhysicalFunctionGeneratedRegistrar.inc>
-#undef INCLUDED_FROM_REGISTRY_PHYSICAL_FUNCTION

nes-physical-operators/src/Functions/ArithmeticalFunctions/CeilPhysicalFunction.cpp
@@ -1,0 +1,58 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include <Functions/ArithmeticalFunctions/CeilPhysicalFunction.hpp>
+
+#include <utility>
+#include <DataTypes/DataType.hpp>
+#include <Functions/PhysicalFunction.hpp>
+#include <Nautilus/DataTypes/VarVal.hpp>
+#include <Nautilus/Interface/Record.hpp>
+#include <std/cmath.h>
+#include <Arena.hpp>
+#include <ErrorHandling.hpp>
+#include <PhysicalFunctionRegistry.hpp>
+#include <val.hpp>
+
+namespace NES
+{
+CeilPhysicalFunction::CeilPhysicalFunction(PhysicalFunction childFunction, DataType inputType, DataType outputType)
+    : childFunction(std::move(childFunction)), inputType(std::move(inputType)), outputType(std::move(outputType))
+{
+}
+
+VarVal CeilPhysicalFunction::execute(const Record& record, ArenaRef& arena) const
+{
+    const auto value = childFunction.execute(record, arena);
+    /// If the input type is a float, we need to ceil the value and returned the ceiled value.
+    /// If the input type is an integer, we do not need to do anything.
+    if (inputType.isFloat())
+    {
+        const auto ceiledValue = nautilus::ceil(value.cast<nautilus::val<double>>());
+        return VarVal{ceiledValue}.castToType(outputType.type);
+    }
+    return value.castToType(outputType.type);
+}
+
+PhysicalFunctionRegistryReturnType
+PhysicalFunctionGeneratedRegistrar::RegisterCeilPhysicalFunction(PhysicalFunctionRegistryArguments physicalFunctionRegistryArguments)
+{
+    PRECONDITION(physicalFunctionRegistryArguments.childFunctions.size() == 1, "Ceil function must have exactly one sub-function");
+    PRECONDITION(physicalFunctionRegistryArguments.inputTypes.size() == 1, "Ceil function must have exactly one input type");
+    return CeilPhysicalFunction(
+        physicalFunctionRegistryArguments.childFunctions[0],
+        physicalFunctionRegistryArguments.inputTypes[0],
+        physicalFunctionRegistryArguments.outputType);
+}
+}