3686d8edfa6898e1b1f50c91004127132e7d2ed2
Leo Raasch <raaschleo@gmail.com>
2026-01-02 19:36:15 +0100

    WIP: Created HTTP Sink
    The sink transforms the tuple buffers to newline-delimited json and sends it to the endpoint via curl POST

---
+++ testQuery.sql
@@ -0,0 +1,15 @@
+CREATE LOGICAL SOURCE heartRates(creationTS UINT64, patientId UINT32, heartRate FLOAT32);
+
+CREATE PHYSICAL SOURCE FOR heartRates TYPE FILE SET(
+       'nes-systests/testdata/small/matrix/heartrates.csv' AS `SOURCE`.`FILE_PATH`,
+       'CSV' as `PARSER`.`TYPE`
+);
+
+CREATE SINK highrates(HEARTRATES.CREATIONTS UINT64, HEARTRATES.PATIENTID UINT32, HEARTRATES.HEARTRATE FLOAT32) TYPE HTTP SET(
+       '3000' as `SINK`.PORT,
+       'JSON' as `SINK`.INPUT_FORMAT,
+       'localhost' as `SINK`.`IP_ADDRESS`,
+       'nesNotification' as `SINK`.ENDPOINT
+);
+
+SELECT * FROM HEARTRATES WHERE HEARTRATE > FLOAT32(100) INTO HIGHRATES;
