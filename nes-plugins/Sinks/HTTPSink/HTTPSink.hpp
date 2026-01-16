46cf553722082260e11acae4bb437f013701844f
Leo Raasch <raaschleo@gmail.com>
2026-01-16 12:43:58 +0100

    WIP: Transformed MatrixSink to more generalized HTTPSink
    The message building logic has been removed and should from now on be implemented by the receiving applications.
    The sink now only transforms the tuple buffers to newline-delimited json and sends it to the endpoint via curl POST

--- nes-plugins/Sinks/HTTPSink/HTTPSink.hpp
+++ nes-plugins/Sinks/HTTPSink/HTTPSink.hpp
@@ -28,33 +28,30 @@
 #include <folly/Synchronized.h>
 
 #include <Configurations/Descriptor.hpp>
+#include <DataTypes/Schema.hpp>
 #include <Runtime/TupleBuffer.hpp>
 #include <Sinks/Sink.hpp>
 #include <Sinks/SinkDescriptor.hpp>
-#include <DataTypes/Schema.hpp>
+#include <SinksParsing/Format.hpp>
+#include <BackpressureChannel.hpp>
 
 namespace NES
 {
 
-/// Sink that creates a customizable message out of query results and transmits it to a matrix bot.
-/// Needs the message and the ip address and port of the matrix bot.
-class MatrixSink final : public Sink
+/// Sink that sends off newline delimited json tuples to an HTTP endpoint at a certain port of an ip-address via POST.
+/// Currently, does not wait for any response, just sends and continues.
+/// Should probably use HTTPS at some point
+class HTTPSink final : public Sink
 {
 public:
-    struct fieldProperties
-    {
-        size_t offset;
-        DataType dataType;
-    };
+    static constexpr std::string_view NAME = "HTTP";
+    explicit HTTPSink(BackpressureController backpressureController, const SinkDescriptor& sinkDescriptor);
+    ~HTTPSink() override = default;
 
-    static constexpr std::string_view NAME = "Matrix";
-    explicit MatrixSink(BackpressureController backpressureController, const SinkDescriptor& sinkDescriptor);
-    ~MatrixSink() override = default;
-
-    MatrixSink(const MatrixSink&) = delete;
-    MatrixSink& operator=(const MatrixSink&) = delete;
-    MatrixSink(MatrixSink&&) = delete;
-    MatrixSink& operator=(MatrixSink&&) = delete;
+    HTTPSink(const HTTPSink&) = delete;
+    HTTPSink& operator=(const HTTPSink&) = delete;
+    HTTPSink(HTTPSink&&) = delete;
+    HTTPSink& operator=(HTTPSink&&) = delete;
 
     void start(PipelineExecutionContext& pipelineExecutionContext) override;
     void execute(const TupleBuffer& inputTupleBuffer, PipelineExecutionContext& pipelineExecutionContext) override;
@@ -68,13 +65,9 @@ protected:
 
 private:
     std::string url;
-    std::string message;
     CURL* curl;
-    char messageFieldDelimiter;
     bool isOpen = false;
-    /// Maps offsets in the message string to the field offsets of the field to insert into the message
-    std::unordered_map<size_t, fieldProperties> messageToBufferOffsets;
-    std::shared_ptr<const Schema> schema;
+    std::unique_ptr<Format> formatter;
 };
 
 struct ConfigParametersMatrix
@@ -85,29 +78,17 @@ struct ConfigParametersMatrix
         [](const std::unordered_map<std::string, std::string>& config) { return DescriptorConfig::tryGet(IPADDRESS, config); }};
 
     static inline const DescriptorConfig::ConfigParameter<std::string> PORT{
-        "port",
-        "3000",
-        [](const std::unordered_map<std::string, std::string>& config) { return DescriptorConfig::tryGet(PORT, config); }};
+        "port", "3000", [](const std::unordered_map<std::string, std::string>& config) { return DescriptorConfig::tryGet(PORT, config); }};
 
     static inline const DescriptorConfig::ConfigParameter<std::string> ENDPOINT{
         "endpoint",
         std::nullopt,
         [](const std::unordered_map<std::string, std::string>& config) { return DescriptorConfig::tryGet(ENDPOINT, config); }};
 
-    static inline const DescriptorConfig::ConfigParameter<std::string> MESSAGE{
-        "message",
-        std::nullopt,
-        [](const std::unordered_map<std::string, std::string>& config) { return DescriptorConfig::tryGet(MESSAGE, config); }};
-
-    static inline const DescriptorConfig::ConfigParameter<char> MESSAGE_FIELD_DELIMITER{
-        "message_field_delimiter",
-        '|',
-        [](const std::unordered_map<std::string, std::string>& config) { return DescriptorConfig::tryGet(MESSAGE_FIELD_DELIMITER, config); }};
-
     static inline std::unordered_map<std::string, DescriptorConfig::ConfigParameterContainer> parameterMap
-        = DescriptorConfig::createConfigParameterContainerMap(SinkDescriptor::parameterMap, IPADDRESS, PORT, ENDPOINT, MESSAGE, MESSAGE_FIELD_DELIMITER);
+        = DescriptorConfig::createConfigParameterContainerMap(SinkDescriptor::parameterMap, IPADDRESS, PORT, ENDPOINT);
 };
 
 }
 
-FMT_OSTREAM(NES::MatrixSink);
+FMT_OSTREAM(NES::HTTPSink);
