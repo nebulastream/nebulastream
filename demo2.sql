62912419dd51ff31807b174a9382a2b938f40145
saad9098-j <smnowar@gmail.com>
2026-01-27 14:34:35 +0100

    finalized the systemtests for the source

--- demo2.sql
+++ demo2.sql
@@ -1,11 +1,18 @@
-CREATE LOGICAL SOURCE single2(id UINT64, value UINT64, timestamp UINT64);
+CREATE LOGICAL SOURCE single5(id UINT64, status VARSIZED, value UINT64,type VARSIZED);
 
-CREATE PHYSICAL SOURCE FOR single2 TYPE LinuxProcess SET(
-  'python3 ./nes-systests/sources/LinuxProcess_Single2.py' AS `SOURCE`.command,
-  'CSV' AS PARSER.`TYPE`,
+CREATE PHYSICAL SOURCE FOR single5 TYPE LinuxProcess SET(
+  './nes-systests/sources/bashjson.sh' AS `SOURCE`.command,
+  'JSON' AS PARSER.`TYPE`,
   '\n' AS PARSER.TUPLE_DELIMITER,
   ','  AS PARSER.FIELD_DELIMITER
 );
 
-CREATE SINK result(single2.id UINT64, single2.value UINT64, single2.timestamp UINT64) TYPE File SET('./demo-output.csv' AS `SINK`.FILE_PATH, 'CSV' AS `SINK`.INPUT_FORMAT);
-SELECT * FROM single2 INTO result;
+CREATE SINK result9(
+  single5.id UINT64,
+  single5.status VARSIZED,
+  single5.value UINT64,
+  single5.type VARSIZED
+) TYPE File SET('./demo-output2.csv' AS `SINK`.FILE_PATH, 'CSV' AS `SINK`.INPUT_FORMAT);
+SELECT id, status, value, type FROM single5 INTO result9;
+
+--python3 ./nes-systests/sources/LinuxProcess_Source_JSON.py
