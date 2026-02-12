796a96c322b5207600341b92c867ed116b1bd22b
Nils L. Schubert <nilslpschubert@gmail.com>
2026-02-12 15:52:18 +0100

    fix(Address Sanitizer): Fixed a bug in our `ExecutionContext::allocateBuffer`
    Fixed a bug that could lead to leaked memory and subsequently a complaint from our address sanitizer in the `ExecutionContext::allocateBuffer`. The culprit was that we used `new TupleBuffer(buffer)` instead of storing the tuple buffer in a container. If the pipeline crashes, i.e., malformed string, the `emitBuffer` would not be called, resulting in no `delete` call.
    
    Signed-off-by: Nils L. Schubert <nilslpschubert@gmail.com>

--- nes-query-engine/QueryEngine.cpp
+++ nes-query-engine/QueryEngine.cpp
@@ -210,6 +210,9 @@ struct DefaultPEC final : PipelineExecutionContext
     size_t numberOfThreads;
     WorkerThreadId threadId;
     PipelineId pipelineId;
+    /// We want to ensure that the address of the TupleBuffer is always the same. If we would simply store the object directly in the vector,
+    /// the address might change as the vector might be resized and thus, the object have a different address.
+    std::vector<std::unique_ptr<TupleBuffer>> pinnedBuffers;
 
 #ifndef NO_ASSERT
     bool wasRepeated = false;
@@ -241,6 +244,13 @@ struct DefaultPEC final : PipelineExecutionContext
     {
         PRECONDITION(!wasRepeated, "A task should terminate after repeating");
         return bm->getBufferBlocking();
+    }
+
+    TupleBuffer& pinBuffer(TupleBuffer&& tupleBuffer) override
+    {
+        PRECONDITION(!wasRepeated, "A task should terminate after repeating");
+        pinnedBuffers.emplace_back(std::make_unique<TupleBuffer>(tupleBuffer));
+        return *pinnedBuffers.back();
     }
 
     [[nodiscard]] uint64_t getNumberOfWorkerThreads() const override
