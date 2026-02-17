ea8df7589c0ccedbf8cbf7d3e123fd88e47707c1
Leo Raasch <raaschleo@gmail.com>
2026-02-17 19:16:34 +0100

    WIP: Added LazyValueRepresentation to Nautilus Types

---
+++ nes-nautilus/include/Nautilus/DataTypes/LazyValueRepresentation.hpp
@@ -0,0 +1,33 @@
+/*
+Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#pragma once
+
+namespace NES
+{
+/// Like the VariableSizedData type, values of this data type consist of a pointer to the location of the value and the size of the value.
+/// We use this type of representation for fields in a pipeline, which are of a fixed sized data type but do not need to be in their fixed sized,
+/// internal memory-representation. This is the case, if the field is not used in a predicate function of a filter or in a scalar mapping expression in the first intermediate pipeline.
+/// By not parsing the value immediately into the internal format and instead just referring to the "raw string" value in the input-formatted buffer,
+/// we potentially avoid costly parsing operations.
+///
+/// Remaining values of this datatype can be converted into the internal format, if an intermediate pipeline further downstream requires them to be represeted this way.
+/// This still potentially saves us parsing operations, as some records might be filtered out already.
+/// Similarly, these values do not need to be "reverse-parsed" into their string format in the OutputFormatter, which especially saves parsing
+-/// costs in stateless queries with only one intermediate pipeline between source and sink.
+class LazyValueRepresentation
+{
+
+};
+}
