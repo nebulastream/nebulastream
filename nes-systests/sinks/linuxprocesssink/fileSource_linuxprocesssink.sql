fa385479d708f794040d4c0d0836047ab56851a1
saad9098-j <smnowar@gmail.com>
2026-03-12 03:03:19 +0100

    changed paths in the sink's test query to absolut

--- nes-systests/sinks/linuxprocesssink/fileSource_linuxprocesssink.sql
+++ nes-systests/sinks/linuxprocesssink/fileSource_linuxprocesssink.sql
@@ -1,12 +1,12 @@
 CREATE LOGICAL SOURCE fileSource_ourSink(value UINT64);
 CREATE PHYSICAL SOURCE FOR fileSource_ourSink TYPE File SET(
-    './demo-input.csv' AS `SOURCE`.FILE_PATH,
+    './nes-systests/sinks/linuxprocesssink/demo-input.csv' AS `SOURCE`.FILE_PATH,
     'CSV' AS PARSER.`TYPE`,
     '\n' AS PARSER.TUPLE_DELIMITER,
     ',' AS PARSER.FIELD_DELIMITER
 );
 CREATE SINK result(fileSource_ourSink.value UINT64) TYPE LinuxProcess SET(
-  'python3 ./consumer.py' AS `SINK`.command,
+  'python3 ./nes-systests/sinks/linuxprocesssink/consumer.py' AS `SINK`.command,
   'CSV' AS `SINK`.INPUT_FORMAT
 );
 
