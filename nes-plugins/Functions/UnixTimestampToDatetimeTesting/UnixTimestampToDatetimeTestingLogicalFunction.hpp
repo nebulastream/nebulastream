ebf6645c3ac0c24152230912f1d3a738b802556e
alepping <aljoschalepping@gmail.com>
2026-03-17 10:35:56 +0100

    chore(Alerts): refactored timestamping

---
+++ nes-plugins/Functions/UnixTimestampToDatetimeTesting/UnixTimestampToDatetimeTestingLogicalFunction.hpp
@@ -0,0 +1,81 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#pragma once
+
+#include <optional>
+#include <string>
+#include <string_view>
+#include <vector>
+#include <DataTypes/DataType.hpp>
+#include <DataTypes/Schema.hpp>
+#include <Functions/LogicalFunction.hpp>
+#include <Util/Logger/Formatter.hpp>
+#include <Util/PlanRenderer.hpp>
+#include <Util/Reflection.hpp>
+#include <SerializableVariantDescriptor.pb.h>
+
+namespace NES
+{
+class UnixTimestampToDatetimeTestingLogicalFunction final
+{
+public:
+    static constexpr std::string_view NAME = "UnixTimestampToDatetimeTesting";
+
+    explicit UnixTimestampToDatetimeTestingLogicalFunction(const LogicalFunction& child);
+
+    [[nodiscard]] bool operator==(const UnixTimestampToDatetimeTestingLogicalFunction& rhs) const;
+
+    [[nodiscard]] DataType getDataType() const;
+    [[nodiscard]] UnixTimestampToDatetimeTestingLogicalFunction withDataType(const DataType& dataType) const;
+    [[nodiscard]] LogicalFunction withInferredDataType(const Schema& schema) const;
+
+    [[nodiscard]] std::vector<LogicalFunction> getChildren() const;
+    [[nodiscard]] UnixTimestampToDatetimeTestingLogicalFunction withChildren(const std::vector<LogicalFunction>& children) const;
+
+    [[nodiscard]] std::string_view getType() const;
+    [[nodiscard]] std::string explain(ExplainVerbosity verbosity) const;
+
+private:
+    DataType dataType;
+    LogicalFunction child;
+
+    friend Reflector<UnixTimestampToDatetimeTestingLogicalFunction>;
+};
+
+template <>
+struct Reflector<UnixTimestampToDatetimeTestingLogicalFunction>
+{
+    Reflected operator()(const UnixTimestampToDatetimeTestingLogicalFunction& function) const;
+};
+
+template <>
+struct Unreflector<UnixTimestampToDatetimeTestingLogicalFunction>
+{
+    UnixTimestampToDatetimeTestingLogicalFunction operator()(const Reflected& reflected) const;
+};
+
+static_assert(LogicalFunctionConcept<UnixTimestampToDatetimeTestingLogicalFunction>);
+
+}
+
+namespace NES::detail
+{
+struct ReflectedUnixTimestampToDatetimeTestingLogicalFunction
+{
+    std::optional<LogicalFunction> child;
+};
+}
+
+FMT_OSTREAM(NES::UnixTimestampToDatetimeTestingLogicalFunction);
