1c4720ea3512800bd182878cde5931dc9791e6d7
Leo Raasch <raaschleo@gmail.com>
2026-03-29 21:16:06 +0200

    feat(ValueSerializerRegistry): Added ValueSerializer Registries and Default Serializers for each type.
    Replaces the writeValAsString function in OutputFormatterUtil as well as the other anonymous value serialization functions in the OutputFormatter files with a dedicated ValueSerializerRegistry and ValueSerializer interface.
    OutputFormatter now holds a datatype -> string unordered map as member, which specifies the types of the serializers for each DataType.
    The registry currently takes a quoted flag as parameter, which is important for the DefaultVARSIZEDValueSerializer. Should further configuration parameter be necessary in the future, we should integrate a dedicated ValueSerializerDescriptor.
    2 additional ValueSerializers have been added: JSONCHAR and JSONVARSIZED for chars/strings following JSON-delimitation rules. They are already added as plugin. JSONVARSIZED does not use the quoted parameter, as JSON standard enforces quoted strings.
    
    Co-Authored-By: Aljoscha Lepping <aljoschalepping@gmail.com>

--- nes-output-formatters/include/OutputFormatters/OutputFormatter.hpp
+++ nes-output-formatters/include/OutputFormatters/OutputFormatter.hpp
@@ -17,12 +17,16 @@
 #include <cstddef>
 #include <cstdint>
 #include <ostream>
+#include <string>
+#include <unordered_map>
 #include <vector>
 
 #include <DataTypes/DataType.hpp>
 #include <DataTypes/VarVal.hpp>
 #include <Interface/Record.hpp>
 #include <Interface/RecordBuffer.hpp>
+#include <magic_enum/magic_enum.hpp>
+
 #include <Runtime/AbstractBufferProvider.hpp>
 #include <fmt/base.h>
 #include <fmt/ostream.h>
@@ -65,9 +69,20 @@ public:
 
     friend std::ostream& operator<<(std::ostream& os, const OutputFormatter& obj);
 
+    [[nodiscard]] const std::string& getSerializerType(const DataType::Type& dataType) const
+    {
+        if (const auto it = serializerTypes.find(dataType); it != serializerTypes.end())
+        {
+            return it->second;
+        }
+        throw UnknownValueSerializerType("No ValueSerializer configured for DataType {}.", magic_enum::enum_name(dataType));
+    }
+
 protected:
     /// Identifiers of the fields of the output schema
     std::vector<Record::RecordFieldIdentifier> fieldNames;
+    /// Stores the configured serializer for each datatype.
+    std::unordered_map<DataType::Type, std::string> serializerTypes;
 };
 
 }
