08983f152ffbb6ac23482442449eb4b10ea01e13
Leo Raasch <raaschleo@gmail.com>
2026-03-30 15:00:41 +0200

    feat(ZMIJOutputParser): Added ZMIJOutputParser plugins for F32 and F64

---
+++ nes-plugins/OutputParsers/ZMIJOutputParser/ZMIJF64OutputParser.hpp
@@ -0,0 +1,40 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#pragma once
+#include <cstdint>
+#include <string>
+#include <Nautilus/DataTypes/VarVal.hpp>
+#include <Nautilus/Interface/RecordBuffer.hpp>
+#include <OutputFormatters/OutputParser.hpp>
+#include <val_arith.hpp>
+#include <val_concepts.hpp>
+#include <val_ptr.hpp>
+#include <Runtime/AbstractBufferProvider.hpp>
+
+namespace NES
+{
+/// Uses the ZMIJ Float parser from https://github.com/vitaut/zmij/tree/88292bd8d624364ceef68889d164ab48564220d9
+class ZMIJF64OutputParser final : public OutputParser
+{
+public:
+    explicit ZMIJF64OutputParser() noexcept = default;
+    [[nodiscard]] nautilus::val<uint64_t> parseAndWrite(
+        const VarVal& value,
+        const nautilus::val<uint64_t>& remainingSize,
+        const RecordBuffer& recordBuffer,
+        const nautilus::val<AbstractBufferProvider*>& bufferProvider,
+        const nautilus::val<int8_t*>& startingAddress) const override;
+};
+}
