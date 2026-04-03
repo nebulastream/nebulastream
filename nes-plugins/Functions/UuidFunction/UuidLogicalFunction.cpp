5b1a8788dea5ccbff9970c0dd58ea03edcd22d2c
lukas schwerdtfeger <lukas.schwerdtfeger@gmail.com>
2026-04-03 21:17:39 +0200

    fmt function
    Signed-off-by: lukas schwerdtfeger <lukas.schwerdtfeger@gmail.com>

--- nes-plugins/Functions/UuidFunction/UuidLogicalFunction.cpp
+++ nes-plugins/Functions/UuidFunction/UuidLogicalFunction.cpp
@@ -13,9 +13,206 @@
 */
 
 #include <Functions/FunctionPluginMacros.hpp>
+#include "Functions/ConstantValueLogicalFunction.hpp"
 
 /// Converts two UINT64 values (high bits, low bits) into a UUID string (VARSIZED).
 namespace NES
 {
-NES_LOGICAL_FUNCTION(Uuid, UINT64, UINT64, VARSIZED)
+NES_LOGICAL_FUNCTION(Uuid, UINT64, UINT64, VARSIZED);
+
+NES_LOGICAL_FUNCTION_IMPL(To_String, 1)
+{
+    switch (children.at(0).getDataType().type)
+    {
+        case DataType::Type::UINT8:
+        case DataType::Type::UINT16:
+        case DataType::Type::UINT32:
+        case DataType::Type::UINT64:
+        case DataType::Type::INT8:
+        case DataType::Type::INT16:
+        case DataType::Type::INT32:
+        case DataType::Type::INT64:
+        case DataType::Type::FLOAT32:
+        case DataType::Type::FLOAT64:
+        case DataType::Type::BOOLEAN:
+        case DataType::Type::CHAR:
+        case DataType::Type::VARSIZED:
+            return DataType::Type::VARSIZED;
+        case DataType::Type::UNDEFINED:
+            INVARIANT(children.at(0).getDataType().type != DataType::Type::UNDEFINED, "Data type should have been resolved");
+    }
+    std::unreachable();
+}
+
+static std::string getFormatString(const LogicalFunction& function)
+{
+    auto constantFunction = function.tryGetAs<ConstantValueLogicalFunction>();
+    if (!constantFunction)
+    {
+        throw CannotInferSchema("FMT requires a constant string as its first argument");
+    }
+    if (constantFunction->getDataType() != DataTypeProvider::provideDataType("VARSIZED"))
+    {
+        throw CannotInferSchema("FMT requires a constant string as its first argument");
+    }
+    return constantFunction->get().getConstantValue();
+}
+
+class FMTLogicalFunction final
+{
+public:
+    static constexpr std::string_view NAME = "FMT";
+    explicit FMTLogicalFunction(std::vector<LogicalFunction> children);
+    [[nodiscard]] bool operator==(const FMTLogicalFunction& rhs) const;
+    [[nodiscard]] DataType getDataType() const;
+    [[nodiscard]] FMTLogicalFunction withDataType(const DataType& dataType) const;
+    [[nodiscard]] LogicalFunction withInferredDataType(const Schema& schema) const;
+    [[nodiscard]] std::vector<LogicalFunction> getChildren() const;
+    [[nodiscard]] FMTLogicalFunction withChildren(const std::vector<LogicalFunction>& children) const;
+    [[nodiscard]] std::string_view getType() const;
+    [[nodiscard]] std::string explain(ExplainVerbosity verbosity) const;
+
+private:
+    DataType dataType;
+    std::vector<LogicalFunction> children_;
+    friend Reflector<FMTLogicalFunction>;
+};
+
+static_assert(LogicalFunctionConcept<FMTLogicalFunction>);
+
+template <>
+struct Reflector<FMTLogicalFunction>
+{
+    Reflected operator()(const FMTLogicalFunction& function) const;
+};
+
+template <>
+struct Unreflector<FMTLogicalFunction>
+{
+    FMTLogicalFunction operator()(const Reflected& reflected) const;
+};
+}
+
+namespace NES::detail
+{
+struct ReflectedFMTLogicalFunction
+{
+    std::vector<LogicalFunction> children;
+};
+}
+
+FMT_OSTREAM(NES::FMTLogicalFunction);
+
+namespace NES
+{
+FMTLogicalFunction::FMTLogicalFunction(std::vector<LogicalFunction> children)
+    : dataType(DataTypeProvider::provideDataType(DataType::Type::UNDEFINED)), children_(std::move(children))
+{
+}
+
+bool FMTLogicalFunction::operator==(const FMTLogicalFunction& rhs) const
+{
+    return children_ == rhs.children_;
+}
+
+DataType FMTLogicalFunction::getDataType() const
+{
+    return dataType;
+}
+
+FMTLogicalFunction FMTLogicalFunction::withDataType(const DataType& dataType) const
+{
+    auto copy = *this;
+    copy.dataType = dataType;
+    return copy;
+}
+
+std::string_view FMTLogicalFunction::getType() const
+{
+    return NAME;
+}
+
+std::vector<LogicalFunction> FMTLogicalFunction::getChildren() const
+{
+    return children_;
+}
+
+FMTLogicalFunction FMTLogicalFunction::withChildren(const std::vector<LogicalFunction>& children) const
+{
+    auto copy = *this;
+    copy.children_ = children;
+    return copy;
+}
+
+std::string FMTLogicalFunction::explain(ExplainVerbosity verbosity) const
+{
+    return detail::explainFunction(NAME, children_, verbosity);
+}
+
+LogicalFunction FMTLogicalFunction::withInferredDataType(const Schema& schema) const
+{
+    const auto inferredChildren = children_
+        | std::views::transform([&schema](const auto& child) { return child.withInferredDataType(schema); })
+        | std::ranges::to<std::vector>();
+    auto newDataType = DataTypeProvider::provideDataType(DataType::Type::VARSIZED);
+    newDataType.nullable = std::ranges::any_of(inferredChildren, [](const auto& c) { return c.getDataType().nullable; });
+
+    std::vector<LogicalFunction> newChildren;
+    newChildren.reserve(inferredChildren.size() + 1);
+    newChildren.emplace_back(inferredChildren[0]);
+
+    auto formatString = getFormatString(newChildren[0]);
+    size_t inArgument = false;
+    size_t argCount = 1;
+    for (auto c : formatString)
+    {
+        if (!inArgument && c == '{')
+        {
+            inArgument = true;
+        }
+        else if (inArgument && c == '}')
+        {
+            inArgument = false;
+            auto inputFunction = inferredChildren.at(argCount++);
+            if (inputFunction.getDataType().type != DataType::Type::VARSIZED)
+            {
+                newChildren.emplace_back(To_StringLogicalFunction({inputFunction}).withInferredDataType(schema));
+            }
+            else
+            {
+                newChildren.emplace_back(inputFunction);
+            }
+        }
+    }
+
+    return withDataType(newDataType).withChildren(newChildren);
+}
+
+Reflected Reflector<FMTLogicalFunction>::operator()(const FMTLogicalFunction& function) const
+{
+    return reflect(detail::ReflectedFMTLogicalFunction{.children = function.children_});
+}
+
+FMTLogicalFunction Unreflector<FMTLogicalFunction>::operator()(const Reflected& reflected) const
+{
+    auto result = unreflect<detail::ReflectedFMTLogicalFunction>(reflected);
+    return FMTLogicalFunction{std::move(result.children)};
+}
+
+LogicalFunctionRegistryReturnType LogicalFunctionGeneratedRegistrar::RegisterFMTLogicalFunction(LogicalFunctionRegistryArguments arguments)
+{
+    if (!arguments.reflected.isEmpty())
+    {
+        return unreflect<FMTLogicalFunction>(arguments.reflected);
+    }
+    if (arguments.children.size() < 1)
+    {
+        throw CannotDeserialize(
+            "FMT"
+            "LogicalFunction requires at least {} children, but got {}",
+            1,
+            arguments.children.size());
+    }
+    return FMTLogicalFunction(std::move(arguments.children));
+}
 }
--- nes-plugins/Functions/UuidFunction/UuidPhysicalFunction.cpp
+++ nes-plugins/Functions/UuidFunction/UuidPhysicalFunction.cpp
@@ -18,9 +18,13 @@
 #include <cstdint>
 #include <cstring>
 #include <Nautilus/DataTypes/VariableSizedData.hpp>
+#include <ryu/ryu.h>
+#include <std/cstring.h>
 #include <uuid/uuid.h>
 #include <function.hpp>
 #include <val.hpp>
+#include "Functions/ConstantValueVariableSizePhysicalFunction.hpp"
+#include "Util/Overloaded.hpp"
 
 namespace NES
 {
@@ -62,6 +66,287 @@ NES_PHYSICAL_FUNCTION(2, Uuid)
         payload);
 
     return VarVal{uuidData, isNullable, false};
+}
+
+#define TO_STRING(Type) \
+    class ToString_##Type##_PhysicalFunction final \
+    { \
+    public: \
+        explicit ToString_##Type##_PhysicalFunction(PhysicalFunction valueFunction); \
+        [[nodiscard]] VarVal execute(const Record& record, ArenaRef& arena) const; \
+\
+    private: \
+        ConstantValueVariableSizePhysicalFunction trueFn{reinterpret_cast<const int8_t*>("true"), 4}; \
+        ConstantValueVariableSizePhysicalFunction falseFn{reinterpret_cast<const int8_t*>("false"), 5}; \
+        PhysicalFunction valueFunction; \
+    }; \
+\
+    static_assert(PhysicalFunctionConcept<ToString_##Type##_PhysicalFunction>); \
+\
+    ToString_##Type##_PhysicalFunction::ToString_##Type##_PhysicalFunction(PhysicalFunction valueFunction) \
+        : valueFunction(std::move(valueFunction)) \
+    { \
+    } \
+\
+    VarVal ToString_##Type##_PhysicalFunction::execute([[maybe_unused]] const Record& record, [[maybe_unused]] ArenaRef& arena) const
+#define INTER_TO_STRING_IMPL(Type) \
+    { \
+        auto varsized = arena.allocateVariableSizedData(int_str_buf_size<Type>()); \
+        auto value = valueFunction.execute(record, arena); \
+        nautilus::val<uint32_t> actualSize \
+            = nautilus::invoke(int_to_str<Type>, value.getRawValueAs<nautilus::val<Type>>(), varsized.getContent()); \
+        return VariableSizedData{ \
+            varsized.getContent() + (nautilus::val<uint32_t>(int_str_buf_size<Type>()) - actualSize - 1), actualSize}; \
+    }
+
+template <typename T>
+constexpr size_t int_str_buf_size()
+{
+    return sizeof(T) * 3 + 2;
+}
+
+template <typename T>
+uint32_t int_to_str(T val, char* buf)
+{
+    static_assert(std::is_integral_v<T>, "int_to_str: T must be an integral type");
+
+    constexpr size_t N = int_str_buf_size<T>();
+    char* p = buf + N - 1;
+    *p = '\0';
+
+    if (val == 0)
+    {
+        *--p = '0';
+        return 1;
+    }
+
+    bool neg = false;
+    std::make_unsigned_t<T> uval;
+    if constexpr (std::is_signed_v<T>)
+    {
+        if (val < 0)
+        {
+            neg = true;
+            uval = static_cast<std::make_unsigned_t<T>>(-val);
+        }
+        else
+        {
+            uval = static_cast<std::make_unsigned_t<T>>(val);
+        }
+    }
+    else
+    {
+        uval = val;
+    }
+
+    uint32_t size = 0;
+    while (uval != 0)
+    {
+        *--p = '0' + static_cast<char>(uval % 10);
+        uval /= 10;
+        ++size;
+    }
+    if (neg)
+    {
+        *--p = '-';
+        ++size;
+    }
+    return size;
+}
+
+TO_STRING(uint8_t) INTER_TO_STRING_IMPL(uint8_t);
+TO_STRING(uint16_t) INTER_TO_STRING_IMPL(uint16_t);
+TO_STRING(uint32_t) INTER_TO_STRING_IMPL(uint32_t);
+TO_STRING(uint64_t) INTER_TO_STRING_IMPL(uint64_t);
+TO_STRING(int8_t) INTER_TO_STRING_IMPL(int8_t);
+TO_STRING(int16_t) INTER_TO_STRING_IMPL(int16_t);
+TO_STRING(int32_t) INTER_TO_STRING_IMPL(int32_t);
+TO_STRING(int64_t) INTER_TO_STRING_IMPL(int64_t);
+
+TO_STRING(float)
+{
+    auto varsized = arena.allocateVariableSizedData(16);
+    auto value = valueFunction.execute(record, arena);
+    nautilus::val<uint32_t> actualSize = nautilus::invoke(
+        +[](float value, char* buf) { return f2s_buffered_n(value, buf); },
+        value.getRawValueAs<nautilus::val<float>>(),
+        varsized.getContent());
+    return VariableSizedData{varsized.getContent(), actualSize};
+}
+
+TO_STRING(double)
+{
+    auto varsized = arena.allocateVariableSizedData(25);
+    auto value = valueFunction.execute(record, arena);
+    nautilus::val<uint32_t> actualSize = nautilus::invoke(
+        +[](double value, char* buf) { return d2s_buffered_n(value, buf); },
+        value.getRawValueAs<nautilus::val<double>>(),
+        varsized.getContent());
+    return VariableSizedData{varsized.getContent(), actualSize};
+}
+
+TO_STRING(bool)
+{
+    auto value = valueFunction.execute(record, arena);
+    if (value.getRawValueAs<nautilus::val<bool>>())
+    {
+        return trueFn.execute(record, arena);
+    }
+    else
+    {
+        return falseFn.execute(record, arena);
+    }
+}
+
+TO_STRING(char)
+{
+    auto varsized = arena.allocateVariableSizedData(1);
+    auto value = valueFunction.execute(record, arena);
+    *varsized.getContent() = value.getRawValueAs<nautilus::val<char>>();
+    return VariableSizedData{varsized.getContent(), 1};
+}
+
+TO_STRING(VarSized)
+{
+    return valueFunction.execute(record, arena);
+}
+
+PhysicalFunctionRegistryReturnType
+PhysicalFunctionGeneratedRegistrar::RegisterTo_StringPhysicalFunction(PhysicalFunctionRegistryArguments args)
+{
+    PRECONDITION(args.childFunctions.size() == (1), "TO_STRING PhysicalFunction must have exactly 1 child function(s)");
+    auto child = std::move(args.childFunctions[0]);
+    switch (args.inputTypes.at(0).type)
+    {
+        case DataType::Type::UINT8:
+            return PhysicalFunction(ToString_uint8_t_PhysicalFunction(std::move(child)));
+        case DataType::Type::UINT16:
+            return PhysicalFunction(ToString_uint16_t_PhysicalFunction(std::move(child)));
+        case DataType::Type::UINT32:
+            return PhysicalFunction(ToString_uint32_t_PhysicalFunction(std::move(child)));
+        case DataType::Type::UINT64:
+            return PhysicalFunction(ToString_uint64_t_PhysicalFunction(std::move(child)));
+        case DataType::Type::INT8:
+            return PhysicalFunction(ToString_int8_t_PhysicalFunction(std::move(child)));
+        case DataType::Type::INT16:
+            return PhysicalFunction(ToString_int16_t_PhysicalFunction(std::move(child)));
+        case DataType::Type::INT32:
+            return PhysicalFunction(ToString_int32_t_PhysicalFunction(std::move(child)));
+        case DataType::Type::INT64:
+            return PhysicalFunction(ToString_int64_t_PhysicalFunction(std::move(child)));
+        case DataType::Type::FLOAT32:
+            return PhysicalFunction(ToString_float_PhysicalFunction(std::move(child)));
+        case DataType::Type::FLOAT64:
+            return PhysicalFunction(ToString_double_PhysicalFunction(std::move(child)));
+        case DataType::Type::BOOLEAN:
+            return PhysicalFunction(ToString_bool_PhysicalFunction(std::move(child)));
+        case DataType::Type::CHAR:
+            return PhysicalFunction(ToString_char_PhysicalFunction(std::move(child)));
+        case DataType::Type::VARSIZED:
+            return PhysicalFunction(ToString_VarSized_PhysicalFunction(std::move(child)));
+        case DataType::Type::UNDEFINED:
+            INVARIANT(args.inputTypes.at(0).type != DataType::Type::UNDEFINED, "Undefined should have been resolved");
+    }
+    std::unreachable();
+}
+
+class FMTPhysicalFunction final
+{
+public:
+    explicit FMTPhysicalFunction(
+        std::vector<std::variant<std::pair<std::shared_ptr<const std::byte[]>, size_t>, PhysicalFunction>> fragments);
+    [[nodiscard]] VarVal execute(const Record& record, ArenaRef& arena) const;
+
+private:
+    size_t baseAllocation = 0;
+    std::vector<std::variant<std::pair<std::shared_ptr<const std::byte[]>, size_t>, PhysicalFunction>> fragments;
+};
+
+static_assert(PhysicalFunctionConcept<FMTPhysicalFunction>);
+
+FMTPhysicalFunction::FMTPhysicalFunction(
+    std::vector<std::variant<std::pair<std::shared_ptr<const std::byte[]>, size_t>, PhysicalFunction>> fragments)
+    : fragments(std::move(fragments))
+{
+    for (const auto& fragment : this->fragments)
+    {
+        if (auto textFragment = std::get_if<std::pair<std::shared_ptr<const std::byte[]>, size_t>>(&fragment))
+        {
+            baseAllocation += textFragment->second;
+        }
+    }
+}
+
+PhysicalFunctionRegistryReturnType PhysicalFunctionGeneratedRegistrar::RegisterFMTPhysicalFunction(PhysicalFunctionRegistryArguments args)
+{
+    auto fmt = args.childFunctions.at(0).tryGetAs<ConstantValueVariableSizePhysicalFunction>();
+    std::vector<std::variant<std::pair<std::shared_ptr<const std::byte[]>, size_t>, PhysicalFunction>> fragments;
+    const auto& data = fmt->get().getData();
+
+    size_t argumentCounter = 1;
+    bool inArgument = false;
+    size_t lastFragmentStart = 0;
+    for (size_t i = 0; i < data.size(); i++)
+    {
+        if (!inArgument && data[i] == '{')
+        {
+            if (i > lastFragmentStart)
+            {
+                auto fragment = std::make_unique<std::byte[]>(i - lastFragmentStart);
+                std::memcpy(fragment.get(), data.data() + lastFragmentStart, i - lastFragmentStart);
+                fragments.emplace_back(std::pair{std::move(fragment), i - lastFragmentStart});
+            }
+            inArgument = true;
+        }
+        else if (inArgument && data[i] == '}')
+        {
+            fragments.emplace_back(args.childFunctions[argumentCounter++]);
+            lastFragmentStart = i + 1;
+            inArgument = false;
+        }
+    }
+    INVARIANT(inArgument == false, "Missing closing bracket in FMT string");
+    if (lastFragmentStart < data.size())
+    {
+        auto fragment = std::make_unique<std::byte[]>(data.size() - lastFragmentStart);
+        std::memcpy(fragment.get(), data.data() + lastFragmentStart, data.size() - lastFragmentStart);
+        fragments.emplace_back(std::pair{std::move(fragment), data.size() - lastFragmentStart});
+    }
+
+
+    return FMTPhysicalFunction(std::move(fragments));
+}
+
+VarVal FMTPhysicalFunction::execute([[maybe_unused]] const Record& record, [[maybe_unused]] ArenaRef& arena) const
+{
+    nautilus::val<uint32_t> totalSize = nautilus::val<uint32_t>(baseAllocation);
+    std::vector<VariableSizedData> args;
+    for (const auto& fragment : nautilus::static_iterable(fragments))
+    {
+        if (auto argument = std::get_if<PhysicalFunction>(&fragment))
+        {
+            args.emplace_back(argument->execute(record, arena).getRawValueAs<VariableSizedData>());
+            totalSize += args.back().getSize();
+        }
+    }
+    auto resultString = arena.allocateVariableSizedData(totalSize);
+    nautilus::val<size_t> offset = 0;
+    size_t argument = 0;
+    for (const auto& fragment : nautilus::static_iterable(fragments))
+    {
+        if (auto textFragment = std::get_if<std::pair<std::shared_ptr<const std::byte[]>, size_t>>(&fragment))
+        {
+            nautilus::memcpy(resultString.getContent() + offset, textFragment->first.get(), textFragment->second);
+            offset += textFragment->second;
+        }
+        else if (std::holds_alternative<PhysicalFunction>(fragment))
+        {
+            nautilus::memcpy(resultString.getContent() + offset, args.at(argument).getContent(), args.at(argument).getSize());
+            offset += args[argument].getSize();
+            argument++;
+        }
+    }
+    return resultString;
 }
 
 }
