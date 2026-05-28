7e8d4a220779e6b3b9e2780ee48452e01c95a267
Benedikt Maria Beckermann <beckermann@tu-berlin.de>
2026-05-28 18:07:13 +0200

    feat(optimizer) created simple library of helper methods to simplify testing of rules

---
+++ nes-query-optimizer/tests/Util/OptimizerTestUtils.cpp
@@ -0,0 +1,117 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include <OptimizerTestUtils.hpp>
+
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+#include <DataTypes/DataType.hpp>
+#include <DataTypes/DataTypeProvider.hpp>
+#include <DataTypes/Schema.hpp>
+#include <DataTypes/SchemaFwd.hpp>
+#include <DataTypes/UnboundField.hpp>
+#include <Identifiers/Identifier.hpp>
+#include <Operators/LogicalOperator.hpp>
+#include <Operators/LogicalOperatorFwd.hpp>
+#include <Operators/Sinks/SinkLogicalOperator.hpp>
+#include <Operators/Sources/SourceDescriptorLogicalOperator.hpp>
+#include <Plans/LogicalPlan.hpp>
+#include <Sinks/SinkDescriptor.hpp>
+#include <Sources/SourceDescriptor.hpp>
+#include <ErrorHandling.hpp>
+#include <QueryId.hpp>
+
+namespace NES
+{
+/// NOLINTNEXTLINE(readability-convert-member-functions-to-static)
+Schema<UnboundFieldBase<1>, Ordered> OptimizerTestUtils::createSchema(const std::vector<std::string>& names)
+{
+    std::vector<UnqualifiedUnboundField> fields = {};
+    fields.reserve(names.size());
+    for (const auto& name : names)
+    {
+        fields.emplace_back(Identifier::parse(name), DataTypeProvider::provideDataType(DataType::Type::UINT64));
+    }
+
+    return Schema<UnqualifiedUnboundField, Ordered>{fields};
+}
+
+TypedLogicalOperator<SourceDescriptorLogicalOperator>
+OptimizerTestUtils::createSource(std::string name, const Schema<UnqualifiedUnboundField, Ordered>& schema)
+{
+    auto descriptor = createSourceDescriptor(Identifier::parse(std::move(name)), schema);
+    return SourceDescriptorLogicalOperator::create(std::move(descriptor));
+}
+
+TypedLogicalOperator<SourceDescriptorLogicalOperator>
+OptimizerTestUtils::createSource(std::string name, const std::vector<std::string>& fieldNames)
+{
+    return createSource(std::move(name), createSchema(fieldNames));
+}
+
+SourceDescriptor
+OptimizerTestUtils::createSourceDescriptor(const Identifier& identifier, const Schema<UnqualifiedUnboundField, Ordered>& schema)
+{
+    auto source = sourceCatalog.addLogicalSource(identifier, schema);
+
+    if (!source.has_value())
+    {
+        throw TestException();
+    }
+    const std::unordered_map<Identifier, std::string> sourceConfig{{Identifier::parse("FILE_PATH"), "/dev/null"}};
+    const std::unordered_map<Identifier, std::string> parserConfig{{Identifier::parse("TYPE"), "CSV"}};
+    auto result = sourceCatalog.addPhysicalSource(source.value(), Identifier::parse("file"), Host{"localhost"}, sourceConfig, parserConfig);
+
+    if (!result.has_value())
+    {
+        throw TestException();
+    }
+    return result.value();
+}
+
+SinkDescriptor OptimizerTestUtils::createSinkDescriptor(const Identifier& sinkName, const Schema<UnqualifiedUnboundField, Ordered>& schema)
+{
+    const std::unordered_map<Identifier, std::string> sinkConfig{
+        {Identifier::parse("FILE_PATH"), "/dev/null"}, {Identifier::parse("OUTPUT_FORMAT"), "CSV"}};
+
+    auto sinkDescriptor = sinkCatalog.addSinkDescriptor(sinkName, schema, Identifier::parse("file"), Host{"localhost"}, sinkConfig, {});
+    if (!sinkDescriptor.has_value())
+    {
+        throw TestException();
+    }
+    return sinkDescriptor.value();
+}
+
+TypedLogicalOperator<SinkLogicalOperator>
+OptimizerTestUtils::createSink(LogicalOperator child, std::string name, const Schema<UnqualifiedUnboundField, Ordered>& schema)
+{
+    return SinkLogicalOperator::create(createSinkDescriptor(Identifier::parse(std::move(name)), schema), std::move(child));
+}
+
+TypedLogicalOperator<SinkLogicalOperator>
+OptimizerTestUtils::createSink(LogicalOperator child, std::string name, const std::vector<std::string>& fieldNames)
+{
+    return createSink(std::move(child), std::move(name), createSchema(fieldNames));
+}
+
+/// NOLINTNEXTLINE(readability-convert-member-functions-to-static)
+LogicalPlan OptimizerTestUtils::createPlan(LogicalOperator sink)
+{
+    return LogicalPlan{
+        QueryId::create(LocalQueryId{LocalQueryId::INVALID}, DistributedQueryId{DistributedQueryId::INVALID}), {std::move(sink)}};
+}
+}
