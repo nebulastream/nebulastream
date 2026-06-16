c1b3a6c003aba7ee858c4d889c33dcd62f77e9e8
Leo Raasch <raaschleo@gmail.com>
2026-06-16 21:35:13 +0200

    feat(ValueSerializers): Added VALUE_SERIALIZERS parameter to OutputFormatterDescriptor to override value serializer types for specific fields
    Co-Authored-By: Aljoscha Lepping <aljoschalepping@gmail.com>

--- nes-plugins/OutputFormatters/JSONOutputFormatter/JSONOutputFormatter.hpp
+++ nes-plugins/OutputFormatters/JSONOutputFormatter/JSONOutputFormatter.hpp
@@ -42,7 +42,7 @@ namespace NES
 class JSONOutputFormatter : public OutputFormatter
 {
 public:
-    explicit JSONOutputFormatter(const std::vector<Record::RecordFieldIdentifier>& fieldNames);
+    explicit JSONOutputFormatter(const std::vector<Record::RecordFieldIdentifier>& fieldNames, const OutputFormatterDescriptor& descriptor);
 
     [[nodiscard]] nautilus::val<uint64_t> writeFormattedValue(
         const VarVal& value,
@@ -73,7 +73,7 @@ namespace NES::OutputFormatterConfig
 struct ConfigParametersJSON
 {
     static inline std::unordered_map<std::string, DescriptorConfig::ConfigParameterContainer> parameterMap
-        = DescriptorConfig::createConfigParameterContainerMap();
+        = DescriptorConfig::createConfigParameterContainerMap(OutputFormatterDescriptor::parameterMap);
 };
 }
 
