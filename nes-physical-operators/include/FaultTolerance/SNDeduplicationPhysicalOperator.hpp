cf4099af4dc45cfb4fdf017b33aa48bdc1645559
Benjamin Greb <benjamingreb01@gmail.com>
2026-07-08 12:06:35 +0200

    add SND operator

---
+++ nes-physical-operators/include/FaultTolerance/SNDeduplicationPhysicalOperator.hpp
@@ -0,0 +1,61 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+#pragma once
+#include <ScanPhysicalOperator.hpp>
+
+#include <optional>
+#include <utility>
+#include <Functions/PhysicalFunction.hpp>
+#include <PhysicalOperator.hpp>
+
+namespace NES
+{
+
+namespace SND
+{
+class DeduplicationLocalState : public OperatorState
+{
+public:
+    explicit DeduplicationLocalState() : skipBuffer(true) { }
+
+    bool skipBuffer;
+};
+}
+
+class SNDeduplicationPhysicalOperator final : public ScanPhysicalOperator
+{
+public:
+    explicit SNDeduplicationPhysicalOperator(
+        std::shared_ptr<TupleBufferRef> bufferRef,
+        std::vector<Record::RecordFieldIdentifier> projections,
+        OperatorHandlerId operatorHandlerId,
+        std::string filePath)
+        : ScanPhysicalOperator(bufferRef, projections), operatorHandlerId(operatorHandlerId), filePath(std::move(filePath)) { };
+    void setup(ExecutionContext& executionCtx, CompilationContext& compilationContext) const override;
+    void open(ExecutionContext& executionCtx, RecordBuffer& recordBuffer) const override;
+    void close(ExecutionContext& executionCtx, RecordBuffer& recordBuffer) const override;
+    void execute(ExecutionContext& ctx, Record& record) const override;
+    void terminate(ExecutionContext& executionCtx) const override;
+
+    [[nodiscard]] std::optional<PhysicalOperator> getChild() const override;
+    void setChild(PhysicalOperator child) override;
+
+private:
+    std::optional<PhysicalOperator> child;
+    std::shared_ptr<TupleBufferRef> bufferRef;
+    std::vector<Record::RecordFieldIdentifier> projections;
+    OperatorHandlerId operatorHandlerId;
+    const std::string filePath;
+};
+}
