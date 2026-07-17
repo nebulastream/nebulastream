ff22a6f5549e318016dfd00502c40d86652dc40c
Lukas Schwerdtfeger <lukas.schwerdtfeger@gmail.com>
2026-07-17 20:45:41 +0200

    arm fixes

--- vcpkg/custom-triplets/bits/arch/arm64.cmake
+++ vcpkg/custom-triplets/bits/arch/arm64.cmake
@@ -15,7 +15,8 @@ set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_CMAKE_SYSTEM_NAME Linux)
 
-# boost-context does not recognize arm64
-if (PORT STREQUAL "boost-context")
+# boost-context and libpng use CMake architecture detection that does not
+# recognize vcpkg's arm64 architecture name.
+if (PORT STREQUAL "boost-context" OR PORT STREQUAL "libpng")
     SET(VCPKG_CMAKE_CONFIGURE_OPTIONS -DCMAKE_SYSTEM_PROCESSOR=aarch64)
 endif ()
