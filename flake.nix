37870130e374f056c7a5be95d8aa092b87b87c0e
ThanGeo <thanatasgoj@gmail.com>
2026-09-10 11:15:05 +0200

    switched to jobs 2 parallelism to not exceed timeouts for kafka broker claude's recommended fix on the CI for proper preparation fixed nix and set the jobs for kafka sink to be ran sequentially to avoid (possible) port collision that makes the CI fail nix fix

--- flake.nix
+++ flake.nix
@@ -264,6 +264,8 @@
                 pahoMqttPkg.c
                 pahoMqttPkg.cpp
                 pkgs.rdkafka
+                pkgs.curl
+                pkgs.cyrus_sasl
               ];
           in {
             inherit fmtPkg spdlogPkg follyPkg baseThirdPartyDeps;
@@ -841,7 +843,7 @@
             ];
             runtimeLibraryPath = lib.makeLibraryPath (allBuildInputs ++ transitiveRuntimeDeps);
             runtimeLibraryPathCMake = lib.concatStringsSep ";" (
-              lib.unique (map (dep: "${dep}/lib") (allBuildInputs ++ transitiveRuntimeDeps))
+              lib.unique (map (dep: "${lib.getLib dep}/lib") (allBuildInputs ++ transitiveRuntimeDeps))
             );
             envVars =
               sanitizer.extraEnv
@@ -869,7 +871,7 @@
               cmakeFlags = cmakeFlagsList;
               shellHook = ''
                 unset NES_PREBUILT_VCPKG_ROOT
-                export LD_BINARY_PATH="${runtimeLibraryPath}''${LD_LIBRARY_PATH:+$LD_LIBRARY_PATH}"
+                export LD_LIBRARY_PATH="${runtimeLibraryPath}''${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
               '' + ccacheShellHook;
             }
           );
